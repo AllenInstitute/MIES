AllenInstitute/MIES

commit 35db5f573985020b41a89043625a8642eb1f046c

Whitespace and code cleanup
Original-CommitID: b4c344d9f312542f03664344250e0452ffb3ee58

Packages/MIES/TJ_MIES_DataManagementNew.ipf
@@ -83,48 +83,38 @@ Function DM_CreateScaleTPHoldWaveChunk(panelTitle,startPoint, NoOfPointsInTP)// 
 End
 
 Function DM_ADScaling(WaveToScale, panelTitle)
-wave WaveToScale
-string panelTitle
-string WavePath = HSU_DataFullFolderPathString(panelTitle)
-wave ITCChanConfigWave = $WavePath + ":ITCChanConfigWave"
-string ADChannelList  =  SCOPE_RefToPullDatafrom2DWave(0,0, 1, ITCChanConfigWave)
-variable NoOfADColumns = DC_NoOfChannelsSelected("ad", panelTitle)
-variable StartOfADColumns = DC_NoOfChannelsSelected("da", panelTitle)
-string ADGainControlName
-variable gain, i
-Wave ChannelClampMode    = GetChannelClampMode(panelTitle)
-Wave SweepData = DC_SweepDataWvRef(panelTitle)
-variable Headstage
+	wave WaveToScale
+	string panelTitle
 
-for(i = 0; i < (itemsinlist(ADChannelList)); i += 1)
-//Gain_AD_00
-	if(str2num(stringfromlist(i, ADChannelList, ";")) < 10)
-		ADGainControlName = "Gain_AD_0" + stringfromlist(i, ADChannelList, ";")
-	else
-		ADGainControlName = "Gain_AD_" + stringfromlist(i, ADChannelList, ";")
-	endif
-	
-	gain = getSetVariable(panelTitle, ADGainControlName)
-	
-	// document AD parameters into SweepData wave
-	Headstage = TP_HeadstageUsingADC(panelTitle, i)
-	if(IsFinite(Headstage))
-		SweepData[0][1][HeadStage] = i // document the AD channel
-		SweepData[0][3][HeadStage] = gain // document the AD gain
-	endif
-	
-	if(ChannelClampMode[str2num(stringfromlist(i, ADChannelList, ";"))][1] == V_CLAMP_MODE)
-		gain *= 3200 // itc output will be multiplied by 1000 to convert to pA then divided by the gain
-		WaveToScale[][(StartOfADColumns + i)] /= gain
-	endif
-	
-	if(ChannelClampMode[str2num(stringfromlist(i, ADChannelList, ";"))][1] == I_CLAMP_MODE)
-		gain *=3200
-		WaveToScale[][(StartOfADColumns + i)]/=gain
-	endif
-	
-endfor
+	string WavePath = HSU_DataFullFolderPathString(panelTitle)
+	wave ITCChanConfigWave = $WavePath + ":ITCChanConfigWave"
+	string ADChannelList  =  SCOPE_RefToPullDatafrom2DWave(0,0, 1, ITCChanConfigWave)
+	variable StartOfADColumns = DC_NoOfChannelsSelected("da", panelTitle)
+	variable gain, i, numEntries, adc
+	Wave ChannelClampMode    = GetChannelClampMode(panelTitle)
+	Wave SweepData = DC_SweepDataWvRef(panelTitle)
+	variable headstage
+	string ctrl
 
+	numEntries = ItemsInList(ADChannelList)
+	for(i = 0; i < numEntries; i += 1)
+		adc = str2num(StringFromList(i, ADChannelList))
+		headstage = TP_HeadstageUsingADC(panelTitle, i)
+	
+		sprintf ctrl, "Gain_AD_%02d", i
+		gain = GetSetVariable(panelTitle, ctrl)
+	
+		// document AD parameters into SweepData wave
+		if(IsFinite(Headstage))
+			SweepData[0][1][HeadStage] = i // document the AD channel
+			SweepData[0][3][HeadStage] = gain // document the AD gain
+		endif
+
+		if(ChannelClampMode[adc][1] == V_CLAMP_MODE || ChannelClampMode[adc][1] == I_CLAMP_MODE)
+			gain *= 3200
+			WaveToScale[][(StartOfADColumns + i)] /= gain
+		endif
+	endfor
 end
 
 Function DM_DAScaling(WaveToScale, panelTitle)

commit 93ca0d87afba4c33c93e7c161796f9c2f0f75a2c

Bugfix: Use the ad channel instead of the list index
Regression introduced in 35db5f57 (Whitespace and code cleanup, 2014-11-24)

Original-CommitID: a4b55f5a9f908f7b0a4af255533b7a9ff34bf66c

Packages/MIES/TJ_MIES_DataManagementNew.ipf
@@ -100,7 +100,7 @@ Function DM_ADScaling(WaveToScale, panelTitle)
 	for(i = 0; i < numEntries; i += 1)
 		adc = str2num(StringFromList(i, ADChannelList))
 
-		sprintf ctrl, "Gain_AD_%02d", i
+		sprintf ctrl, "Gain_AD_%02d", adc
 		gain = GetSetVariable(panelTitle, ctrl)
 
 		if(ChannelClampMode[adc][1] == V_CLAMP_MODE || ChannelClampMode[adc][1] == I_CLAMP_MODE)