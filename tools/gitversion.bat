defc314fbefdd308b885006dabcf0f93996bfe5b
Thomas Braun <thomas.braun@byte-physics.de>
2015-02-05 23:01:13 +0100

    Quote the repository path
    As otherwise paths with spaces don't work
    
    Original-CommitID: 50488c8b2df7da79bdde60faa9de6719234a9656

--- tools/gitversion.bat
+++ tools/gitversion.bat
@@ -1,4 +1,4 @@
 REM This script assumes that .. of its location is the git top level directory
 
 SET REPO=%~dp0
-"c:\Program Files (x86)\Git\bin\git.exe" --git-dir=%REPO%..\\.git describe --always --tags > %REPO%..\\version.txt
+"c:\Program Files (x86)\Git\bin\git.exe" --git-dir="%REPO%..\\.git" describe --always --tags > "%REPO%..\\version.txt"
