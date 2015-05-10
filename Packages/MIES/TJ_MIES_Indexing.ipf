AllenInstitute/MIES

commit 7da6baec6d7907650af44fe5ebdd2e919b7a2653

Change the function argument order
As we nearly always have the panelTitle as first argument.
DC_ControlStatusWave was originally modeled after DC_ControlStatusListString
and that is the reason of the unusual argument order.

Original-CommitID: 1d2494c3871ea91da166fcc5a997d30de3d8fc70

Packages/MIES/TJ_MIES_Indexing.ipf
@@ -402,7 +402,7 @@ Function IDX_LongestITI(panelTitle, numActiveDAChannels)
 	for(i = 0; i < numPanels; i += 1)
 		panelTitle = StringFromList(i, panelList)
 
-		Wave DAChannelStatus = DC_ControlStatusWave("DA", panelTitle)
+		Wave DAChannelStatus = DC_ControlStatusWave(panelTitle, "DA")
 		if(i == 0) // this is either the lead panel or the first and only panel
 			numActiveDAChannels = sum(DAChannelStatus)
 		endif

Packages/MIES/TJ_MIES_DataConfiguratorITC.ipf
@@ -108,21 +108,21 @@ End
 
 /// @brief Returns a free wave of the status of the checkboxes specified by channelType
 ///
-/// @param channelType  one of DA, AD, or TTL
-/// @param panelTitle   panel title
-Function/Wave DC_ControlStatusWave(channelType, panelTitle)
-	string channelType
+/// @param type        one of DA, AD, TTL or DataAcq_HS
+/// @param panelTitle  panel title
+Function/Wave DC_ControlStatusWave(panelTitle, type)
+	string type
 	string panelTitle
 
 	string controlType = "CHECK"
 	string ctrl
-	variable numChannels = DC_TotNoOfControlType(controlType, channelType, panelTitle)
+	variable numChannels = DC_TotNoOfControlType(controlType, type, panelTitle)
 	variable i
 
 	Make/FREE/U/B/N=(numChannels) wv = 0
 
 	for(i = 0; i < numChannels; i += 1)
-		sprintf ctrl, "%s_%s_%.2d", controlType, channelType, i
+		sprintf ctrl, "%s_%s_%.2d", controlType, type, i
 		wv[i] = GetCheckboxState(panelTitle, ctrl)
 	endfor
 

commit 0756f889f5fbc18777314bea6ba89e2c2ad501d5

Indexing: Don't assert on invalid settings
For non active DA/TTL channels the stim set Wave and the indexing end
wave can be equal. As we enforce these two waves to be different in
DAP_CheckHeadStage for active channels, we don't have to do anything here.

Regression introduced in 7da6baec (Change the function argument order,
2014-10-21).

Original-CommitID: 52ab723bcc5bb72110f8ce60310145b29aee88c4

Packages/MIES/TJ_MIES_Indexing.ipf
@@ -55,7 +55,7 @@ Function IDX_IndexingDoIt(panelTitle)
 				PopUpMenu $ctrl win = $panelTitle, mode = DACIndexingStorageWave[0][i]
 			endif
 		else
-			ASSERT(0, "invalid channel type")
+			// do nothing
 		endif
 	endfor
 
@@ -76,7 +76,7 @@ Function IDX_IndexingDoIt(panelTitle)
 				PopUpMenu $ctrl win = $panelTitle, mode = TTLIndexingStorageWave[0][i]
 			endif
 		else
-			ASSERT(0, "invalid channel type")
+			// do nothing
 		endif
 	endfor
 