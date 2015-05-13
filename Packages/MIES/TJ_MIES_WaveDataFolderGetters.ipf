AllenInstitute/MIES

commit 7a886ae0445cea029a18ffafac3f3b4c62807440

Add wave getters for ITC and testpulse waves
Original-CommitID: d92f854a8447d3f0a402e2e67ffef8b83f2790cf

Packages/MIES/TJ_MIES_WaveDataFolderGetters.ipf
@@ -265,6 +265,91 @@ Function/S GetMiesPathAsString()
 	return "root:MIES"
 End
 
+/// @brief Return the ITC data wave
+Function/Wave GetITCDataWave(panelTitle)
+	string panelTitle
+
+	DFREF dfr = GetDevicePath(panelTitle)
+
+	WAVE/Z/SDFR=dfr wv = ITCDataWave
+
+	if(WaveExists(wv))
+		return wv
+	endif
+
+	Make/N=(1, NUM_DA_TTL_CHANNELS) dfr:ITCDataWave/Wave=wv
+
+	return wv
+End
+
+/// @brief Return the ITC channel config wave
+Function/Wave GetITCChanConfigWave(panelTitle)
+	string panelTitle
+
+	DFREF dfr = GetDevicePath(panelTitle)
+
+	WAVE/Z/SDFR=dfr wv = ITCChanConfigWave
+
+	if(WaveExists(wv))
+		return wv
+	endif
+
+	Make/N=(2, 4) dfr:ITCChanConfigWave/Wave=wv
+
+	return wv
+End
+
+/// @brief Return the ITC fifo available for all channels wave
+Function/Wave GetITCFIFOAvailAllConfigWave(panelTitle)
+	string panelTitle
+
+	DFREF dfr = GetDevicePath(panelTitle)
+
+	WAVE/Z/SDFR=dfr wv = ITCFIFOAvailAllConfigWave
+
+	if(WaveExists(wv))
+		return wv
+	endif
+
+	Make/N=(2, 4) dfr:ITCFIFOAvailAllConfigWave/Wave=wv
+
+	return wv
+End
+
+/// @brief Return the ITC fifo available for all channels wave
+Function/Wave GetITCFIFOPositionAllConfigWave(panelTitle)
+	string panelTitle
+
+	DFREF dfr = GetDevicePath(panelTitle)
+
+	WAVE/Z/SDFR=dfr wv = ITCFIFOPositionAllConfigWave
+
+	if(WaveExists(wv))
+		return wv
+	endif
+
+	Make/N=(2, 4) dfr:ITCFIFOPositionAllConfigWave/Wave=wv
+
+	return wv
+End
+
+/// @brief Return the ITC result data wave
+Function/Wave GetITCResultsWave(panelTitle)
+	string panelTitle
+
+	DFREF dfr = GetDevicePath(panelTitle)
+
+	WAVE/I/Z/SDFR=dfr wv = ResultsWave
+
+	if(WaveExists(wv))
+		return wv
+	endif
+
+	Make/I/N=(4) dfr:ResultsWave/Wave=wv
+
+	return wv
+End
+
 /// @name Experiment Documentation
 /// @{
 
@@ -756,6 +841,58 @@ End
 Function/S GetDeviceTestPulseAsString(panelTitle)
 	string panelTitle
 	return GetDevicePathAsString(panelTitle) + ":TestPulse"
+End
+
+/// @brief Return the ITC testpulse wave
+Function/Wave GetTestPulseITCWave(panelTitle)
+	string 	panelTitle
+
+	dfref dfr = GetDeviceTestPulse(panelTitle)
+	WAVE/Z/SDFR=dfr wv = TestPulseITC
+
+	if(WaveExists(wv))
+		return wv
+	endif
+
+	Make/N=(1, NUM_DA_TTL_CHANNELS) dfr:TestPulseITC/Wave=wv
+
+	return wv
+End
+
+/// @brief Return the testpulse instantenous resistance wave
+///
+/// The columns hold the *active* DA channels only and are subject to resizing.
+Function/Wave GetInstResistanceWave(panelTitle)
+	string 	panelTitle
+
+	dfref dfr = GetDeviceTestPulse(panelTitle)
+	WAVE/Z/SDFR=dfr wv = InstResistance
+
+	if(WaveExists(wv))
+		return wv
+	endif
+
+	Make/N=(1, NUM_DA_TTL_CHANNELS) dfr:InstResistance/Wave=wv
+
+	return wv
+End
+
+/// @brief Return the testpulse steady state resistance wave
+///
+/// The columns hold the *active* DA channels only and are subject to resizing.
+Function/Wave GetSSResistanceWave(panelTitle)
+	string 	panelTitle
+
+	dfref dfr = GetDeviceTestPulse(panelTitle)
+	WAVE/Z/SDFR=dfr wv = SSResistance
+
+	if(WaveExists(wv))
+		return wv
+	endif
+
+	Make/N=(1, NUM_DA_TTL_CHANNELS) dfr:SSResistance/Wave=wv
+
+	return wv
 End
 
 /// @}

commit fb032ea1fea93614acd77d3b7ee95733ac1442d3

Bugfix: Use the correct types for the ITC waves
Regression introduced in 7a886ae0 (Add wave getters for ITC
and testpulse waves, 2015-04-16).

Original-CommitID: 7d6ecc1c146b6c73667b072563b5f810fa3c8153

Packages/MIES/TJ_MIES_WaveDataFolderGetters.ipf
@@ -271,13 +271,13 @@ Function/Wave GetITCDataWave(panelTitle)
 
 	DFREF dfr = GetDevicePath(panelTitle)
 
-	WAVE/Z/SDFR=dfr wv = ITCDataWave
+	WAVE/W/Z/SDFR=dfr wv = ITCDataWave
 
 	if(WaveExists(wv))
 		return wv
 	endif
 
-	Make/N=(1, NUM_DA_TTL_CHANNELS) dfr:ITCDataWave/Wave=wv
+	Make/W/N=(1, NUM_DA_TTL_CHANNELS) dfr:ITCDataWave/Wave=wv
 
 	return wv
 End
@@ -288,13 +288,13 @@ Function/Wave GetITCChanConfigWave(panelTitle)
 
 	DFREF dfr = GetDevicePath(panelTitle)
 
-	WAVE/Z/SDFR=dfr wv = ITCChanConfigWave
+	WAVE/I/Z/SDFR=dfr wv = ITCChanConfigWave
 
 	if(WaveExists(wv))
 		return wv
 	endif
 
-	Make/N=(2, 4) dfr:ITCChanConfigWave/Wave=wv
+	Make/I/N=(2, 4) dfr:ITCChanConfigWave/Wave=wv
 
 	return wv
 End
@@ -305,13 +305,13 @@ Function/Wave GetITCFIFOAvailAllConfigWave(panelTitle)
 
 	DFREF dfr = GetDevicePath(panelTitle)
 
-	WAVE/Z/SDFR=dfr wv = ITCFIFOAvailAllConfigWave
+	WAVE/I/Z/SDFR=dfr wv = ITCFIFOAvailAllConfigWave
 
 	if(WaveExists(wv))
 		return wv
 	endif
 
-	Make/N=(2, 4) dfr:ITCFIFOAvailAllConfigWave/Wave=wv
+	Make/I/N=(2, 4) dfr:ITCFIFOAvailAllConfigWave/Wave=wv
 
 	return wv
 End
@@ -322,13 +322,13 @@ Function/Wave GetITCFIFOPositionAllConfigWave(panelTitle)
 
 	DFREF dfr = GetDevicePath(panelTitle)
 
-	WAVE/Z/SDFR=dfr wv = ITCFIFOPositionAllConfigWave
+	WAVE/I/Z/SDFR=dfr wv = ITCFIFOPositionAllConfigWave
 
 	if(WaveExists(wv))
 		return wv
 	endif
 
-	Make/N=(2, 4) dfr:ITCFIFOPositionAllConfigWave/Wave=wv
+	Make/I/N=(2, 4) dfr:ITCFIFOPositionAllConfigWave/Wave=wv
 
 	return wv
 End