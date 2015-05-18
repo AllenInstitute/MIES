AllenInstitute/MIES

commit 431164f2a0665515202b879d62512519d95ed470

Add forgotten checkbox for overlay channels feature
Since c67d14b4 (Implement overlay channels checkbox, 2015-04-24) the sweep browser
was supposed to have a new checkbox for the overlay channels feature.

This checkbox was forgotten and is now added.

Original-CommitID: b24752f273f25c39a6a0fd82d1168c72d8a41df8

Packages/MIES/TJ_MIES_AnalysisBrowser_SweepBrowser.ipf
@@ -403,11 +403,13 @@ Function/DF SB_CreateNewSweepBrowser()
 
 	NewPanel/HOST=#/EXT=1/W=(156,0,0,383) as " "
 	ModifyPanel fixedSize=0
-	CheckBox check_SweepBrowser_DisplayDAC,pos={17,7},size={116,14},title="Display DA channels"
-	CheckBox check_SweepBrowser_DisplayDAC,value= 0, proc=SB_CheckboxDisplayDAChannels
-	SetVariable setvar_SweepBrowser_OverlaySkip,pos={38,53},size={64,16},title="Step"
+	CheckBox check_SweepBrowser_DisplayDAC,pos={17,7},size={116,14},proc=SB_CheckboxDisplayDAChannels,title="Display DA channels"
+	CheckBox check_SweepBrowser_DisplayDAC,value= 0
+	SetVariable setvar_SweepBrowser_OverlaySkip,pos={38,49},size={64,16},title="Step"
 	SetVariable setvar_SweepBrowser_OverlaySkip,limits={1,inf,1},value= _NUM:1
-	CheckBox check_SweepBrowser_SweepOverlay,pos={18,36},size={95,14},title="Overlay Sweeps"
+	CheckBox check_sweepbrowser_OverlayChan,pos={19,69},size={101,14},proc=SB_CheckboxChangedSettings,title="Overlay Channels"
+	CheckBox check_sweepbrowser_OverlayChan,value= 1
+	CheckBox check_SweepBrowser_SweepOverlay,pos={17,30},size={95,14},title="Overlay Sweeps"
 	CheckBox check_SweepBrowser_SweepOverlay,value= 0
 	GroupBox group_sweep,pos={9,90},size={139,74},title="Sweep"
 	GroupBox group_postSynPot,pos={9,266},size={137,92},title="Post-synaptic potentials"

commit 639c2d8083d5573adcc4dde29f0ba5bbf602cb2d

Change to existing procedure and add procedure also for overlay channels checkbox
Regression introduced in 431164f2 (Add forgotten checkbox for overlay channels
feature, 2015-04-26).

Original-CommitID: 93a50fb703aafbb23bd7777f44be1b75cda09b6a

Packages/MIES/TJ_MIES_AnalysisBrowser_SweepBrowser.ipf
@@ -403,13 +403,13 @@ Function/DF SB_CreateNewSweepBrowser()
 
 	NewPanel/HOST=#/EXT=1/W=(156,0,0,383) as " "
 	ModifyPanel fixedSize=0
-	CheckBox check_SweepBrowser_DisplayDAC,pos={17,7},size={116,14},proc=SB_CheckboxDisplayDAChannels,title="Display DA channels"
+	CheckBox check_SweepBrowser_DisplayDAC,pos={17,7},size={116,14},proc=SB_CheckboxChangedSettings,title="Display DA channels"
 	CheckBox check_SweepBrowser_DisplayDAC,value= 0
 	SetVariable setvar_SweepBrowser_OverlaySkip,pos={38,49},size={64,16},title="Step"
 	SetVariable setvar_SweepBrowser_OverlaySkip,limits={1,inf,1},value= _NUM:1
 	CheckBox check_sweepbrowser_OverlayChan,pos={19,69},size={101,14},proc=SB_CheckboxChangedSettings,title="Overlay Channels"
 	CheckBox check_sweepbrowser_OverlayChan,value= 1
-	CheckBox check_SweepBrowser_SweepOverlay,pos={17,30},size={95,14},title="Overlay Sweeps"
+	CheckBox check_SweepBrowser_SweepOverlay,pos={17,30},size={95,14},proc=SB_CheckboxChangedSettings,title="Overlay Sweeps"
 	CheckBox check_SweepBrowser_SweepOverlay,value= 0
 	GroupBox group_sweep,pos={9,90},size={139,74},title="Sweep"
 	GroupBox group_postSynPot,pos={9,266},size={137,92},title="Post-synaptic potentials"