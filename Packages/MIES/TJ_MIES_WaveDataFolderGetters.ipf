AllenInstitute/MIES

commit 1fbbcd45d40e27e430c2a46b91260e0e50891fe5

Changes saved prior to testing WSE interactions
Original-CommitID: 6e9cd6ef5751e0aac2b832e5b0c416d9a9f09881

Packages/MIES/TJ_MIES_WaveDataFolderGetters.ipf
@@ -2946,4 +2946,37 @@ Function/S GetDevSpecConfigSttngsWaveAS(panelTitle)
 	string panelTitle
 	return GetDevSpecLabNBFolderAsString(panelTitle) + ":configSettings"
 End
-/// @}
+
+///@brief Returns a wave reference to the Asynch response wave
+Function/Wave GetAsynRspWaveRef(panelTitle)
+	string panelTitle
+
+	DFREF dfr = GetDevSpecAsynRspWavePath(panelTitle)
+
+	Wave/T/Z/SDFR=dfr wv = asynRspWave
+
+	if(WaveExists(wv))
+		return wv
+	endif
+
+	Make/T/N=(NUM_HEADSTAGES,1) dfr:asynRspWave/Wave=wv
+	
+	SetDimLabel 0, -1, HeadStage, wv	
+	SetDimLabel 1, 0, cmdID, wv
+
+	return wv
+End
+
+/// @brief Return the datafolder reference to the device specific text documentation
+Function/DF GetDevSpecAsynRspWavePath(panelTitle)
+	string panelTitle
+
+	return createDFWithAllParents(GetDevSpecAsynRspWaveAS(panelTitle))
+End
+
+/// @brief Return the full path to the device specific text documentation, e.g. root:mies:LabNoteBook:ITC18USB:Device0:analysisSettings
+Function/S GetDevSpecAsynRspWaveAS(panelTitle)
+	string panelTitle
+
+	return GetDevSpecLabNBFolderAsString(panelTitle) + ":cmdID"
+End

Packages/MIES/DR_MIES_AnalysisMaster.ipf
@@ -161,6 +161,9 @@ Function AM_PAA_adjustScaleFactor(panelTitle, headStage)
 	Wave/T analysisSettingsWave = GetAnalysisSettingsWaveRef(panelTitle)	
 	Wave actionScaleSettingsWave = GetActionScaleSettingsWaveRef(panelTitle)
 	
+	variable len 
+	string responseString
+	
 	// get the DA channel associated with the desired headstage
 	variable daChannel = TP_GetDAChannelFromHeadstage(panelTitle, headStage)
 	string scaleControlName
@@ -186,6 +189,24 @@ Function AM_PAA_adjustScaleFactor(panelTitle, headStage)
 		actionScaleSettingsWave[headStage][%result] = scaleFactor
 		print "scale factor that caused AP: ", scaleFactor	
 		
+		//  See if we need to send a response string to the WSE
+		// get the reference to the asyn response wave ref 
+		Wave/T asynRespWave = GetAsynRspWaveRef(panelTitle)
+		
+		//  See if there is anything in the cmdID space
+		len = strlen(asynRespWave[headstage][%cmdID])
+		
+		if (len >= 1)
+			// build up the response string
+			sprintf responseString, "scaleFactor:%d;", scaleFactor
+			writeAsyncResponse(asynRespWave[headstage][%cmdID], responseString)
+		else
+			print "No asyn response required..."
+		endif 
+		
+		// kill the asynRespWave
+		KillWaves asynRespWave
+		
 		// Now put the scale factor back to 1.0
 		SetSetVariable(panelTitle, scaleControlName, 1.0)	
 		return 1
@@ -199,6 +220,9 @@ Function AM_PAA_bracketScaleFactor(panelTitle, headStage)
 	
 	Wave/T analysisSettingsWave = GetAnalysisSettingsWaveRef(panelTitle)	
 	Wave actionScaleSettingsWave = GetActionScaleSettingsWaveRef(panelTitle)
+	
+	variable len
+	string responseString
 	
 	// get the DA channel associated with the desired headstage
 	variable daChannel = TP_GetDAChannelFromHeadstage(panelTitle, headStage)
@@ -244,6 +268,24 @@ Function AM_PAA_bracketScaleFactor(panelTitle, headStage)
 		// return the scale factor that caused the AP to fire
 		actionScaleSettingsWave[headStage][%result] = scaleFactor
 		print "scale factor that caused AP: ", scaleFactor	
+		
+		//  See if we need to send a response string to the WSE
+		// get the reference to the asyn response wave ref 
+		Wave/T asynRespWave = GetAsynRspWaveRef(panelTitle)
+		
+		//  See if there is anything in the cmdID space
+		len = strlen(asynRespWave[headstage][%cmdID])
+		
+		if (len >= 1)
+			// build up the response string
+			sprintf responseString, "scaleFactor:%d;", scaleFactor
+			writeAsyncResponse(asynRespWave[headstage][%cmdID], responseString)
+		else
+			print "No asyn response required..."
+		endif 
+		
+		// kill the asynRespWave
+		KillWaves asynRespWave
 		
 		// Now put the scale factor back to 1.0
 		SetSetVariable(panelTitle, scaleControlName, 1.0)			

Packages/MIES/DR_MIES_TangoInteract.ipf
@@ -371,17 +371,16 @@ Function TangoCommandInput(cmdString)
 	print "igorCmdPortion: ", igorCmdPortion
 	// and append the cmdNumber and the trailing ")"
 	string completeIgorCommand
-	sprintf completeIgorCommand, "%s, cmdID=%d)", igorCmdPortion, cmdID
+	sprintf completeIgorCommand, "%s, cmdID=\"%d\")", igorCmdPortion, cmdID
 	
 	print "completeIgorCommand: ", completeIgorCommand
 	// now call the command 
 	Execute/Z completeIgorCommand
 	if (V_Flag != 0)
 		print "Unable to run command....check command syntax..."
-		writeAck(cmdID, -1)
+		//writeAck(cmdID, -1)
 	else
 		print "Command ran successfully..."
-//		writeLog("Mies command ran successfully....")
 	endif
 End	
 	
@@ -407,7 +406,7 @@ End
 /// @brief Save Mies Experiment as a packed experiment
 Function TangoSave(saveFileName, [cmdID])
 	string saveFileName
-	variable cmdID
+	string cmdID
 	
 	//save as packed experiment
 	SaveExperiment/C/F={1,"",2}/P=home as saveFileName + ".pxp"
@@ -448,6 +447,13 @@ Function/S TI_runAdaptiveStim(stimWaveName, initScaleFactor, scaleFactor, thresh
 		
 		Wave actionScaleSettingsWave = GetActionScaleSettingsWaveRef(currentPanel)
 		Wave/T analysisSettingsWave = GetAnalysisSettingsWaveRef(currentPanel)
+		
+		// get the reference to the asyn response wave ref 
+		Wave/T asynRespWave = GetAsynRspWaveRef(currentPanel)
+		// and put the cmdID there, if you were passed one
+		if (ParamIsDefault(cmdID) == 0)
+			asynRespWave[headstage][%cmdID] = cmdID
+		endif
 		
 		// put the scaleDelta in the  actionscalesettings wave
 		actionScaleSettingsWave[headStage][%scaleValue] = scaleFactor
@@ -568,12 +574,22 @@ Function/S TI_runBracketingFunction(stimWaveName, coarseScaleFactor, fineScaleFa
 	variable headstage
 	string cmdID
 	
+	
 	// save the present data folder
 	string savedDataFolder = GetDataFolder(1)
 	
 	// get the da_ephys panel names
 	string lockedDevList = DAP_ListOfLockedDevs()
 	variable noLockedDevs = ItemsInList(lockedDevList)
+	
+	string waveSelect 
+	string psaMenu
+	string paaMenu
+	string psaCheck
+	string paaCheck
+	string scaleWidgetName
+	
+	print "headStage: ", headStage
 	
 	variable n
 	for (n = 0; n<noLockedDevs; n+= 1)
@@ -597,21 +613,20 @@ Function/S TI_runBracketingFunction(stimWaveName, coarseScaleFactor, fineScaleFa
 		// reset the result value before starting the cycle
 		actionScaleSettingsWave[headStage][%result] = 0
 		
-		// push the waveSet to the ephys panel
-		// first, build up the control name by using the headstage value
-		string waveSelect 
-		string psaMenu
-		string paaMenu
-		string psaCheck
-		string paaCheck
-		string scaleWidgetName
+		// get the reference to the asyn response wave ref 
+		Wave/T asynRespWave = GetAsynRspWaveRef(currentPanel)
+		// and put the cmdID there, if passed one
+		if (ParamIsDefault(cmdID) == 0)
+			asynRespWave[headstage][%cmdID] = cmdID
+		endif
 		
-		sprintf waveSelect, "Wave_DA_%02d", headstage
+		// push the waveSet to the ephys panel		
+		sprintf waveSelect, "Wave_DA_0%d", headstage
 		sprintf psaMenu, "PSA_headStage%d", headstage
 		sprintf paaMenu, "PAA_headStage%d", headstage
 		sprintf psaCheck, "headStage%d_postSweepAnalysisOn", headstage
 		sprintf paaCheck, "headStage%d_postAnalysisActionOn", headstage
-		sprintf scaleWidgetName, "Scale_DA_%02d", headStage
+		sprintf scaleWidgetName, "Scale_DA_0%0d", headStage
 		
 		string FolderPath
 		string folder
@@ -719,6 +734,11 @@ Function/T TI_runStimWave(stimWaveName, scaleFactor, headstage, [cmdID])
 		
 		Wave actionScaleSettingsWave = GetActionScaleSettingsWaveRef(currentPanel)
 		Wave/T analysisSettingsWave = GetAnalysisSettingsWaveRef(currentPanel)
+		
+		// get the reference to the asyn response wave ref 
+		Wave/T asynRespWave = GetAsynRspWaveRef(currentPanel)
+		// and put the cmdID there
+		asynRespWave[headstage][%cmdID] = cmdID
 		
 		// push the waveSet to the ephys panel
 		// first, build up the control name by using the headstage value
@@ -842,7 +862,7 @@ End
 ///@brief routine to be called from the WSE to start and stop the test pulse
 Function TI_runTestPulse(tpCmd, [cmdID])
 	variable tpCmd
-	variable cmdID
+	string cmdID
 	
 	print "cmdID: ", cmdID
 	
@@ -884,7 +904,7 @@ END
 
 ///@brief Routine to test starting and stopping acquisition by remotely hitting the start/stop button on the DA_Ephys panel
 Function TI_runStopStart([cmdID])
-	variable cmdID
+	string cmdID
 	
 	// get the da_ephys panel names
 	string lockedDevList = DAP_ListOfLockedDevs()
@@ -916,13 +936,13 @@ End
 
 /// @brief function to write the acknowledgement string back to the WSE
 Function writeAck(cmdID, returnValue)
-	Variable cmdID
+	string cmdID
 	Variable returnValue
 	
 	String logMessage
 		
 	// put the response string together...
-	sprintf logMessage, "cmd_id:%d;response:%d", cmdID, returnValue 
+	sprintf logMessage, "cmd_id:%s;response:%d", cmdID, returnValue 
 	print "logMessage: ", logMessage
 	
 	//- function arg: the name of the device on which the commands will be executed 
@@ -991,14 +1011,15 @@ End
 
 /// @brief function to allow for writing async responses back to the WSE
 Function writeAsyncResponse(cmdID, returnString)
-	variable cmdID
+	String cmdID
 	String returnString
 	
 	String responseMessage	
 	variable numberOfReturnItems = ItemsInList(returnString)
+	print "numberOfReturnItems: ", numberOfReturnItems
 	
 	// put the response string together...
-	sprintf logMessage, "cmd_id:%d;%d;%s", cmdID, numberOfReturnItems, returnString
+	sprintf responseMessage, "cmd_id:%s;%d;%s", cmdID, numberOfReturnItems, returnString
 	
 	//- function arg: the name of the device on which the commands will be executed 
 	String dev_name = "mies_device/MiesDevice/test"

commit 5c3f0589601930f154ae27fb2d8392893b92b950

Readd doxygen group closing
Regression introduced in 1fbbcd45 (Changes saved prior to testing WSE
interactions, 2015-04-15).

Original-CommitID: c2ceaa10447b85530408a997f38591429f22b0f1

Packages/MIES/TJ_MIES_WaveDataFolderGetters.ipf
@@ -2918,6 +2918,8 @@ Function/Wave GetAnalysisSweepWave(expFolder, device, sweep)
 	return wv
 End
 
+/// @}
+
 ///@brief Returns a wave reference to the new config settings Wave
 Function/Wave GetConfigSettingsWaveRef(panelTitle)
 	string panelTitle