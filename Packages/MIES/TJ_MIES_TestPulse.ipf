AllenInstitute/MIES

commit 9a065769a373fdd0b34fa5c899501c0eff3e565f

Introduce a constant for the minimum sampling interval
The replaced 0.005 equals 5 microseconds as we have 1 ms per point.

Original-CommitID: 53fc70223a5080b780ec755bf190f22c35799169

Packages/MIES/TJ_MIES_TestPulse.ipf
@@ -214,7 +214,7 @@ Function TP_ButtonProc_DataAcq_TestPulse(ba) : ButtonControl
 			string TestPulsePath = "root:MIES:WaveBuilder:SavedStimulusSets:DA:TestPulse"
 			Make/O/N=0 $TestPulsePath
 			WAVE TestPulse = $TestPulsePath
-			SetScale /P x 0,0.005,"ms", TestPulse
+			SetScale /P x 0, MINIMUM_SAMPLING_INTERVAL, "ms", TestPulse
 
 			TP_UpdateTPBufferSizeGlobal(panelTitle)
 			TP_UpdateTestPulseWave(TestPulse, panelTitle)
@@ -743,7 +743,7 @@ Function TP_CreateSquarePulseWave(panelTitle, Frequency, Amplitude, TPWave)
 	variable 	amplitude
 	Wave 	TPWave
 	variable 	numberOfSquarePulses
-	variable  	longestSweepPoints = (((1000 / Frequency) * 2) / 0.005)  * (1 / (DC_ITCMinSamplingInterval(panelTitle) / 0.005))
+	variable  	longestSweepPoints = (((1000 / Frequency) * 2) / MINIMUM_SAMPLING_INTERVAL)  * (1 / (DC_ITCMinSamplingInterval(panelTitle) / MINIMUM_SAMPLING_INTERVAL))
 	//print "longest sweep =", longestSweepPoints
 	variable 	exponent = ceil(log(longestSweepPoints)/log(2))
 	if(exponent < 17) // prevents FIFO underrun overrun errors by keepint the wave a minimum size
@@ -754,13 +754,13 @@ Function TP_CreateSquarePulseWave(panelTitle, Frequency, Amplitude, TPWave)
 	make /FREE /n = (2 ^ exponent)  CosBuildWave
 	make /FREE /n = (2 ^ exponent)  BuildWave
 
-	SetScale /P x 0,0.005, "ms", SinBuildWave
-	SetScale /P x 0,0.005, "ms", CosBuildWave
-	SetScale /P x 0,0.005, "ms", BuildWave
+	SetScale /P x 0, MINIMUM_SAMPLING_INTERVAL,  "ms", SinBuildWave
+	SetScale /P x 0, MINIMUM_SAMPLING_INTERVAL,  "ms", CosBuildWave
+	SetScale /P x 0, MINIMUM_SAMPLING_INTERVAL,  "ms", BuildWave
 	
 	Frequency /= 1.5
 	// the point offset is 1/4 of a cos wave cycle in points. The is used to make the baseline before the first pulse the same length as the interpulse interval
-	variable PointOffset = ((1 / Frequency) / 0.000005) * 0.25
+	variable PointOffset = ((1 / Frequency) / MINIMUM_SAMPLING_INTERVAL) * 0.25
 	Multithread SinBuildWave =  .49 * - sin(2 * Pi * (Frequency * 1000) * (5 / 1000000000) * (p + PointOffset))
 	Multithread CosBuildWave = 0.49 * - cos(2 * Pi * ((Frequency* 2) * 1000) * (5 / 1000000000) * (p + PointOffset))
 	Multithread BuildWave = SinBuildWave + CosBuildWave

Packages/MIES/TJ_MIES_Constants.ipf
@@ -218,3 +218,6 @@ StrConstant NUMERALS = "First;Second;Third;Fourth;Fifth;Sixth;Seventh;Eighth"
 
 /// Generic axis name for graphs using split axis
 StrConstant AXIS_BASE_NAME = "col"
+
+/// Minimum possible sampling interval for our ITC DACs in milliseconds (1e-3s)
+Constant MINIMUM_SAMPLING_INTERVAL = 0.005

Packages/MIES/TJ_MIES_DataAcqMgmt.ipf
@@ -445,7 +445,7 @@ Function TP_TPSetUp(panelTitle) // prepares device for TP - use this procedure j
 	TestPulsePath = GetWBSvdStimSetDAPathAsString() + ":TestPulse"
 	make /o /n = 0 $TestPulsePath
 	wave TestPulse = $TestPulsePath
-	SetScale /P x 0,0.005,"ms", TestPulse // test pulse wave made at max possible samp frequency
+	SetScale /P x 0, MINIMUM_SAMPLING_INTERVAL, "ms", TestPulse // test pulse wave made at max possible samp frequency
 
 	TP_UpdateTestPulseWaveChunks(TestPulse, panelTitle) // makes the test pulse wave that contains enought test pulses to fill the min ITC DAC wave size 2^17
 
@@ -459,7 +459,7 @@ Function TP_TPSetUp(panelTitle) // prepares device for TP - use this procedure j
 	DC_ConfigureDataForITC(panelTitle, TEST_PULSE_MODE)
 	// special mod for test pulse to ITC data wave that makes sure the entire TP is filled with test pulses because of how data is placed into the ITCDataWave based on sampling frequency
 	wave ITCDataWave = $WavePath + ":ITCDataWave"
-	variable NewNoOfPoints = floor(dimsize(ITCDataWave, 0) / (deltaX(ITCDataWave) / 0.005))
+	variable NewNoOfPoints = floor(dimsize(ITCDataWave, 0) / (deltaX(ITCDataWave) / MINIMUM_SAMPLING_INTERVAL))
 
 	if(NewNoOfPoints ==   43690) // extra special exceptions for 3 channels - super BS coding right here.
 		NewNoOfPoints = 2^15

Packages/MIES/TJ_MIES_PressureControl.ipf
@@ -928,7 +928,7 @@ Function P_DAforManPpulse(panelTitle, Headstage)
 	variable 	lastPressureCom		= pressureDataWv[Headstage][%LastPressureCommand]
 	variable 	DAGain				= pressureDataWv[Headstage][%DAC_Gain]
 	variable 	PressureCom		= pressureDataWv[headStage][%ManPPPressure]
-	variable 	PPEndPoint			= PRESSURE_PULSE_STARTpt + (pressureDataWv[headStage][%ManPPDuration] / 0.005)
+	variable 	PPEndPoint			= PRESSURE_PULSE_STARTpt + (pressureDataWv[headStage][%ManPPDuration] / MINIMUM_SAMPLING_INTERVAL)
 
 	if((PressureCom) < MAX_REGULATOR_PRESSURE && PressureCom > MIN_REGULATOR_PRESSURE)
 		ITCData[][%DA] = (PRESSURE_OFFSET * BITS_PER_VOLT)

Packages/MIES/TJ_MIES_RepeatedAcquisition.ipf
@@ -53,7 +53,7 @@ Function RA_Start(panelTitle)
 		string TestPulsePath = "root:MIES:WaveBuilder:SavedStimulusSets:DA:TestPulse"
 		make /o /n = 0 $TestPulsePath
 		wave TestPulse = $TestPulsePath
-		SetScale /P x 0, 0.005, "ms", TestPulse
+		SetScale /P x 0, MINIMUM_SAMPLING_INTERVAL, "ms", TestPulse
 		TP_UpdateTestPulseWave(TestPulse,panelTitle)
 
 		make /free /N=(NUM_DA_TTL_CHANNELS) SelectedDACWaveList
@@ -156,7 +156,7 @@ Function RA_Counter(panelTitle)
 				string TestPulsePath = "root:MIES:WaveBuilder:SavedStimulusSets:DA:TestPulse"
 				make /o /n = 0 $TestPulsePath
 				wave TestPulse = root:MIES:WaveBuilder:SavedStimulusSets:DA:TestPulse
-				SetScale /P x 0, 0.005, "ms", TestPulse
+				SetScale /P x 0, MINIMUM_SAMPLING_INTERVAL, "ms", TestPulse
 				TP_UpdateTestPulseWave(TestPulse, panelTitle)
 				
 				make /free /N=(NUM_DA_TTL_CHANNELS) SelectedDACWaveList
@@ -224,7 +224,7 @@ Function RA_BckgTPwithCallToRACounter(panelTitle)
 		string TestPulsePath = "root:MIES:WaveBuilder:SavedStimulusSets:DA:TestPulse"
 		make /o /n = 0 $TestPulsePath
 		wave TestPulse = root:MIES:WaveBuilder:SavedStimulusSets:DA:TestPulse
-		SetScale/P x 0, 0.005, "ms", TestPulse
+		SetScale/P x 0, MINIMUM_SAMPLING_INTERVAL, "ms", TestPulse
 		TP_UpdateTestPulseWave(TestPulse, panelTitle)
 		
 		make /free /N=(NUM_DA_TTL_CHANNELS) SelectedDACWaveList

Packages/MIES/TJ_MIES_WaveBuilder.ipf
@@ -318,7 +318,7 @@ static Function WB_MakeWaveBuilderWave(WP, stepCount, numEpochs, wvName)
 
 	AddEntryIntoWaveNoteAsList(WaveBuilderWave, "ITI", var=SegWvType[99], appendCR=1)
 
-	SetScale /P x 0, 0.005, "ms", WaveBuilderWave
+	SetScale /P x 0, MINIMUM_SAMPLING_INTERVAL, "ms", WaveBuilderWave
 	// although we are not creating these globals anymore, we still try to kill them
 	KillVariables/Z ParameterHolder
 	KillStrings/Z StringHolder
@@ -330,7 +330,7 @@ static Function/Wave WB_GetSegmentWave(duration)
 	variable duration
 
 	DFREF dfr = GetWaveBuilderDataPath()
-	variable numPoints = duration / 0.005
+	variable numPoints = duration / MINIMUM_SAMPLING_INTERVAL
 	Wave/Z/SDFR=dfr SegmentWave
 
 	if(duration > MAX_SWEEP_DURATION_IN_MS)
@@ -344,7 +344,7 @@ static Function/Wave WB_GetSegmentWave(duration)
 		Redimension/N=(numPoints) SegmentWave
 	endif
 
-	SetScale/P x 0,0.005, "ms", SegmentWave
+	SetScale/P x 0, MINIMUM_SAMPLING_INTERVAL, "ms", SegmentWave
 
 	return SegmentWave
 End
@@ -361,7 +361,7 @@ End
 static Function WB_RampSegment(pa)
 	struct SegmentParameters &pa
 
-	variable amplitudeIncrement = pa.amplitude * 0.005 / pa.duration
+	variable amplitudeIncrement = pa.amplitude * MINIMUM_SAMPLING_INTERVAL / pa.duration
 
 	Wave SegmentWave = WB_GetSegmentWave(pa.duration)
 	MultiThread SegmentWave = amplitudeIncrement * p
@@ -472,7 +472,7 @@ static Function WB_SquarePulseTrainSegment(pa, mode)
 
 	// We remove one point from the duration.
 	// This is done in order to create, for situations with t = 1000, f = 5, p = 100, the expected five pulses (n = 5)
-	interPulseInterval = ((pa.duration/0.005 - 1) * 0.005 - pa.numberOfPulses * pa.pulseDuration) / (pa.numberOfPulses - 1)
+	interPulseInterval = ((pa.duration/MINIMUM_SAMPLING_INTERVAL - 1) * MINIMUM_SAMPLING_INTERVAL - pa.numberOfPulses * pa.pulseDuration) / (pa.numberOfPulses - 1)
 
 	WAVE segmentWave = WB_GetSegmentWave(pa.duration)
 	segmentWave = 0
@@ -480,26 +480,26 @@ static Function WB_SquarePulseTrainSegment(pa, mode)
 
 	if(!GetCheckBoxState("Wavebuilder", "check_SPT_Poisson_P44"))
 		for(;;)
-			endIndex = floor((pulseStartTime + pa.pulseDuration) / 0.005)
+			endIndex = floor((pulseStartTime + pa.pulseDuration) / MINIMUM_SAMPLING_INTERVAL)
 
 			if(endIndex >= numRows || endIndex < 0)
 				break
 			endif
 
-			startIndex = floor(pulseStartTime / 0.005)
+			startIndex = floor(pulseStartTime / MINIMUM_SAMPLING_INTERVAL)
 			segmentWave[startIndex, endIndex] = pa.amplitude
 			pulseStartTime += interPulseInterval + pa.pulseDuration
 		endfor
 	else
 		for(;;)
 			pulseStartTime += -ln(abs(enoise(1))) / pa.frequency * 1000
-			endIndex = floor((pulseStartTime + pa.pulseDuration) / 0.005)
+			endIndex = floor((pulseStartTime + pa.pulseDuration) / MINIMUM_SAMPLING_INTERVAL)
 
 			if(endIndex >= numRows || endIndex < 0)
 				break
 			endif
 
-			startIndex = floor(pulseStartTime / 0.005)
+			startIndex = floor(pulseStartTime / MINIMUM_SAMPLING_INTERVAL)
 			segmentWave[startIndex, endIndex] = pa.amplitude
 		endfor
 	endif
@@ -508,14 +508,14 @@ static Function WB_SquarePulseTrainSegment(pa, mode)
 	FindValue/V=(0)/S=(startIndex) segmentWave
 	if(V_Value != -1)
 		Redimension/N=(V_Value) segmentWave
-		pa.duration = V_Value * 0.005
+		pa.duration = V_Value * MINIMUM_SAMPLING_INTERVAL
 	endif
 
 	segmentWave += pa.offset
 
 	DEBUGPRINT("interPulseInterval", var=interPulseInterval)
 	DEBUGPRINT("numberOfPulses", var=pa.numberOfPulses)
-	DEBUGPRINT("Real duration", var=DimSize(segmentWave, ROWS) * 0.005, format="%.6f")
+	DEBUGPRINT("Real duration", var=DimSize(segmentWave, ROWS) * MINIMUM_SAMPLING_INTERVAL, format="%.6f")
 End
 
 static Function WB_PSCSegment(pa)
@@ -526,11 +526,11 @@ static Function WB_PSCSegment(pa)
 	Wave SegmentWave = WB_GetSegmentWave(pa.duration)
 
 	pa.TauRise = 1 / pa.TauRise
-	pa.TauRise *= 0.005
+	pa.TauRise *= MINIMUM_SAMPLING_INTERVAL
 	pa.TauDecay1 = 1 / pa.TauDecay1
-	pa.TauDecay1 *= 0.005
+	pa.TauDecay1 *= MINIMUM_SAMPLING_INTERVAL
 	pa.TauDecay2 = 1 / pa.TauDecay2
-	pa.TauDecay2 *= 0.005
+	pa.TauDecay2 *= MINIMUM_SAMPLING_INTERVAL
 
 	MultiThread SegmentWave[] = pa.amplitude * ((1 - exp(-pa.TauRise * p)) + exp(-pa.TauDecay1 * p) * (1 - pa.TauDecay2Weight) + exp(-pa.TauDecay2 * p) * pa.TauDecay2Weight)
 
@@ -566,8 +566,8 @@ static Function WB_PinkAndBrownNoise(pa, pinkOrBrown)
 		return NaN
 	endif
 
-	Make/FREE/n=(pa.duration / 0.005, NumberOfBuildWaves) BuildWave
-	SetScale/P x 0,0.005,"ms", BuildWave
+	Make/FREE/n=(pa.duration / MINIMUM_SAMPLING_INTERVAL, NumberOfBuildWaves) BuildWave
+	SetScale/P x 0, MINIMUM_SAMPLING_INTERVAL, "ms", BuildWave
 	variable frequency = pa.highPassCutOff
 	variable i
 	variable localAmplitude
@@ -586,7 +586,7 @@ static Function WB_PinkAndBrownNoise(pa, pinkOrBrown)
 	endfor
 
 	MatrixOp/O/NTHR=0   SegmentWave = sumRows(BuildWave)
-	SetScale/P x 0, 0.005,"ms", SegmentWave
+	SetScale/P x 0, MINIMUM_SAMPLING_INTERVAL, "ms", SegmentWave
 
 	WaveStats/Q SegmentWave
 	SegmentWave *= pa.amplitude / V_sdev

commit 9cfca745a8752736d6d4c4d0f1983665e858f456

Bugfix: Calculate point offset for MD test pulses correctly
Regression introduced in 9a065769 (Introduce a constant for the minimum
sampling interval, 2015-05-08).

Original-CommitID: 22d730738f16b4efeef19145ddcf08544b70e090

Packages/MIES/TJ_MIES_TestPulse.ipf
@@ -756,7 +756,7 @@ Function TP_CreateSquarePulseWave(panelTitle, Frequency, Amplitude, TPWave)
 	
 	Frequency /= 1.5
 	// the point offset is 1/4 of a cos wave cycle in points. The is used to make the baseline before the first pulse the same length as the interpulse interval
-	variable PointOffset = ((1 / Frequency) / MINIMUM_SAMPLING_INTERVAL) * 0.25
+	variable PointOffset = ((1 / Frequency) / 0.000005) * 0.25
 	Multithread SinBuildWave =  .49 * - sin(2 * Pi * (Frequency * 1000) * (5 / 1000000000) * (p + PointOffset))
 	Multithread CosBuildWave = 0.49 * - cos(2 * Pi * ((Frequency* 2) * 1000) * (5 / 1000000000) * (p + PointOffset))
 	Multithread BuildWave = SinBuildWave + CosBuildWave