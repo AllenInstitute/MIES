AllenInstitute/MIES

commit b376efa3e8add0e30f0dfa0252bf5337c81ab00e

Enhance MDSort to be able to sort three columns
Original-CommitID: 86645b42907d9a24e02b1fe7d9dbc267a1acefb1

Packages/MIES/TJ_MIES_Utilities.ipf
@@ -945,13 +945,14 @@ End
 /// @param w                          wave of arbitrary type
 /// @param keyColPrimary              column of the primary key
 /// @param keyColSecondary [optional] column of the secondary key
+/// @param keyColTertiary [optional]  column of the tertiary key
 /// @param reversed [optional]        do an descending sort instead of an ascending one
 ///
 /// Taken from http://www.igorexchange.com/node/599 with some cosmetic changes and extended for
-/// the secondary key
-Function MDsort(w, keyColPrimary, [keyColSecondary, reversed])
+/// the two key
+Function MDsort(w, keyColPrimary, [keyColSecondary, keyColTertiary, reversed])
 	WAVE w
-	variable keyColPrimary, keyColSecondary, reversed
+	variable keyColPrimary, keyColSecondary, keyColTertiary, reversed
 
 	variable numRows, type
 
@@ -962,7 +963,7 @@ Function MDsort(w, keyColPrimary, [keyColSecondary, reversed])
 		return NaN
 	endif
 
-	Make/Y=(type)/Free/N=(numRows) keyPrimary, keySecondary
+	Make/Y=(type)/Free/N=(numRows) keyPrimary, keySecondary, keyTertiary
 	Make/Free/N=(numRows)/I/U valindex = p
 
 	if(type == 0)
@@ -971,33 +972,42 @@ Function MDsort(w, keyColPrimary, [keyColSecondary, reversed])
 		output[] = indirectSourceText[p][keyColPrimary]
 		WAVE/T output = keySecondary
 		output[] = indirectSourceText[p][keyColSecondary]
+		WAVE/T output = keyTertiary
+		output[] = indirectSourceText[p][keyColTertiary]
 	else
-		WAVE indirectSource = w
+		WAVE indirectSource        = w
 		MultiThread keyPrimary[]   = indirectSource[p][keyColPrimary]
 		MultiThread keySecondary[] = indirectSource[p][keyColSecondary]
+		MultiThread keyTertiary[]  = indirectSource[p][keyColTertiary]
 	endif
 
-	if(ParamIsDefault(keyColSecondary))
+	if(ParamIsDefault(keyColSecondary) && ParamIsDefault(keyColTertiary))
 		if(reversed)
 			Sort/A/R keyPrimary, valindex
 		else
 			Sort/A keyPrimary, valindex
 		endif
-	else
+	elseif(!ParamIsDefault(keyColSecondary))
 		if(reversed)
 			Sort/A/R {keyPrimary, keySecondary}, valindex
 		else
 			Sort/A {keyPrimary, keySecondary}, valindex
 		endif
+	else
+		if(reversed)
+			Sort/A/R {keyPrimary, keySecondary, keyTertiary}, valindex
+		else
+			Sort/A {keyPrimary, keySecondary, keyTertiary}, valindex
+		endif
 	endif
 
 	if(type == 0)
 		Duplicate/FREE/T indirectSourceText, newtoInsertText
-		newtoInsertText[][] = indirectSourceText[valindex[p]][q]
+		newtoInsertText[][][][] = indirectSourceText[valindex[p]][q][r][s]
 		indirectSourceText = newtoInsertText
 	else
 		Duplicate/FREE indirectSource, newtoInsert
-		MultiThread newtoinsert[][] = indirectSource[valindex[p]][q]
+		MultiThread newtoinsert[][][][] = indirectSource[valindex[p]][q][r][s]
 		MultiThread indirectSource = newtoinsert
 	endif
 End

commit 7ab2a10cc31279206bca35ec49e72b04dbdf4bb5

MDSort: Fix sorting with three columns
Regression introduced in b376efa3 (Enhance MDSort to be able to sort three columns, 2015-02-25).
Currently no callers use three columns

Original-CommitID: 1749973dbd6e299fbdcd7bb098dd438e8f2875fc

Packages/MIES/TJ_MIES_Utilities.ipf
@@ -915,7 +915,7 @@ Function MDsort(w, keyColPrimary, [keyColSecondary, keyColTertiary, reversed])
 		else
 			Sort/A keyPrimary, valindex
 		endif
-	elseif(!ParamIsDefault(keyColSecondary))
+	elseif(!ParamIsDefault(keyColSecondary) && ParamIsDefault(keyColTertiary))
 		if(reversed)
 			Sort/A/R {keyPrimary, keySecondary}, valindex
 		else