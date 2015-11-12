AllenInstitute/MIES

commit 102c38138eb9fedfff8cc47308991fdf573c890a

Fix TP_Teardown usage and move more common code their
The function must be called only after the testpulse finished. There is
no use in calling it after starting a background task for the testpulse.

Original-CommitID: f5c4c0ba633ea3d576c178963ed81453c43a74ae

Packages/MIES/MIES_TestPulse.ipf
@@ -238,8 +238,6 @@ End
 Function TP_StartTestPulseSingleDevice(panelTitle)
 	string panelTitle
 
-	variable headstage
-
 	AbortOnValue DAP_CheckSettings(panelTitle, TEST_PULSE_MODE),1
 
 	DisableControl(panelTitle, "StartTestPulseButton")
@@ -255,20 +253,14 @@ Function TP_StartTestPulseSingleDevice(panelTitle)
 		ITC_StartBackgroundTestPulse(panelTitle)
 	else
 		ITC_StartTestPulse(panelTitle)
-		SCOPE_KillScopeWindowIfRequest(panelTitle)
+		TP_Teardown(panelTitle)
 	endif
-
-	TP_Teardown(panelTitle)
-
-	headStage = GetSliderPositionIndex(panelTitle, "slider_DataAcq_ActiveHeadstage")
-	P_LoadPressureButtonState(panelTitle, headStage)
 End
 
 /// @brief Start a multi device test pulse, always done in background mode
 Function TP_StartTestPulseMultiDevice(panelTitle)
 	string panelTitle
 
-	variable headstage
 	AbortOnValue DAP_CheckSettings(panelTitle, TEST_PULSE_MODE),1
 
 	DAP_StopOngoingDataAcqMD(panelTitle)
@@ -732,10 +724,23 @@ End
 Function TP_Teardown(panelTitle)
 	string panelTitle
 
+	variable headstage
+
 	DFREF dfr = GetDevicePath(panelTitle)
 
 	WAVE/SDFR=dfr SelectedDACWaveList
 	TP_ResetSelectedDACWaves(SelectedDACWaveList, panelTitle)
 	WAVE/SDFR=dfr SelectedDACScale
 	TP_RestoreDAScale(SelectedDACScale, panelTitle)
+
+	DAP_RestoreTTLState(panelTitle)
+
+	EnableControl(panelTitle, "StartTestPulseButton")
+
+	ED_TPDocumentation(panelTitle)
+
+	SCOPE_KillScopeWindowIfRequest(panelTitle)
+
+	headStage = GetSliderPositionIndex(panelTitle, "slider_DataAcq_ActiveHeadstage")
+	P_LoadPressureButtonState(panelTitle, headStage)
 End

Packages/MIES/MIES_DataAcqITC.ipf
@@ -333,15 +333,8 @@ Function ITC_StopTestPulseSingleDevice(panelTitle)
 	variable headstage
 
 	CtrlNamedBackground TestPulse, stop
-	SCOPE_KillScopeWindowIfRequest(panelTitle)
 
-	DAP_RestoreTTLState(panelTitle)
-	EnableControl(panelTitle, "StartTestPulseButton")
-
-	headStage = GetSliderPositionIndex(panelTitle, "slider_DataAcq_ActiveHeadstage")
-	P_LoadPressureButtonState(panelTitle, headStage)
-
-	ED_TPDocumentation(panelTitle)
+	TP_Teardown(panelTitle)
 End
 
 static Constant DEFAULT_MAXAUTOBIASCURRENT = 500e-12 /// Unit: Amps
@@ -513,14 +506,8 @@ Function ITC_StartTestPulse(panelTitle)
 
 		i += 1	
 	while(!(GetKeyState(0) & ESCAPE_KEY))
-	
-	DAP_RestoreTTLState(panelTitle)
-	ED_TPDocumentation(panelTitle)
-	EnableControl(panelTitle,"StartTestPulseButton")
-	
-	// Update pressure buttons
-	variable headStage = GetSliderPositionIndex(panelTitle, "slider_DataAcq_ActiveHeadstage") // determine the selected MIES headstage
-	P_LoadPressureButtonState(panelTitle, headStage)
+
+	TP_Teardown(panelTitle)
 END
 
 Function ITC_SingleADReading(Channel, panelTitle)//channels 16-23 are asynch channels on ITC1600

Packages/MIES/MIES_DataAcqMgmt.ipf
@@ -124,7 +124,6 @@ Function DAM_StartTestPulseMD(panelTitle)
 			print "TP Started on independent ITC1600"
 			TP_Setup(panelTitle, multiDevice=1)
 			ITC_BkrdTPMD(0, panelTitle) // START TP DATA ACQUISITION
-			TP_Teardown(panelTitle)
 		elseif(DAP_DeviceCanLead(panelTitle))
 			SVAR/Z ListOfFollowerDevices = $GetFollowerList(doNotCreateSVAR=1)
 			if(SVAR_exists(ListOfFollowerDevices)) // ITC1600 device with the potential for yoked devices - need to look in the list of yoked devices to confirm, but the list does exist
@@ -144,14 +143,11 @@ Function DAM_StartTestPulseMD(panelTitle)
 					//Lead board commands
 					TP_Setup(panelTitle, multiDevice=1)
 					ITC_BkrdTPMD(TriggerMode, panelTitle) // Sets lead board in wait for trigger mode
-					// restores lead board settings
-					TP_Teardown(panelTitle)
 					
 					//Follower board commands
 					do
 						followerPanelTitle = stringfromlist(i,ListOfFollowerDevices, ";")
 						ITC_BkrdTPMD(TriggerMode, followerPanelTitle) // Sets lead board in wait for trigger mode
-						TP_Teardown(followerPanelTitle)
 						i += 1
 					while(i < numberOfFollowerDevices)
 
@@ -161,18 +157,15 @@ Function DAM_StartTestPulseMD(panelTitle)
 				elseif(numberOfFollowerDevices == 0)
 					TP_Setup(panelTitle, multiDevice=1)
 					ITC_BkrdTPMD(0, panelTitle) // START TP DATA ACQUISITION
-					TP_Teardown(panelTitle)
 				endif
 			else
 				TP_Setup(panelTitle, multiDevice=1)
 				ITC_BkrdTPMD(0, panelTitle) // START TP DATA ACQUISITION
-				TP_Teardown(panelTitle)
 			endif
 		endif
 	else
 		TP_Setup(panelTitle, multiDevice=1)
 		ITC_BkrdTPMD(0, panelTitle) // START TP DATA ACQUISITION
-		TP_Teardown(panelTitle)
 	endif
 End
 

Packages/MIES/MIES_RepeatedAcquisition.ipf
@@ -43,8 +43,6 @@ static Function RA_HandleITI(panelTitle)
 
 	ITC_StartBackgroundTestPulse(panelTitle)
 	ITC_StartBackgroundTimer(ITI, "ITC_STOPTestPulseSingleDevice(\"" + panelTitle + "\")", "RA_Counter(\"" + panelTitle + "\")", "", panelTitle)
-
-	TP_Teardown(panelTitle)
 End
 
 /// @brief Function gets called after the first trial is already

Packages/MIES/MIES_TPBackgroundMD.ipf
@@ -172,13 +172,7 @@ Function ITC_StopTPMD(panelTitle)
 		endif
 	endif
 
-	SCOPE_KillScopeWindowIfRequest(panelTitle)
-	ED_TPDocumentation(panelTitle)
-	EnableControl(panelTitle, "StartTestPulseButton")
-	DAP_RestoreTTLState(panelTitle)
-
-	headstage = GetSliderPositionIndex(panelTitle, "slider_DataAcq_ActiveHeadstage")
-	P_LoadPressureButtonState(panelTitle, headStage)
+	TP_Teardown(panelTitle)
 End
 
 static Function ITC_MakeOrUpdateTPDevLstWave(panelTitle, ITCDeviceIDGlobal, ADChannelToMonitor, StopCollectionPoint, AddorRemoveDevice)

commit ff72709117bb6c6ffd39d0a54eabc104720fd623

TP_StartTestPulseSingleDevice: Fix left out call to P_LoadPressureButtonState
Broken since 102c3813 (Fix TP_Teardown usage and move more common code
their, 2015-10-28).

Original-CommitID: ea0ab97610a6abafb05562f34f22f260efdcad2d

Packages/MIES/MIES_TestPulse.ipf
@@ -31,6 +31,8 @@ End
 Function TP_StartTestPulseSingleDevice(panelTitle)
 	string panelTitle
 
+	variable headstage
+
 	AbortOnValue DAP_CheckSettings(panelTitle, TEST_PULSE_MODE),1
 
 	DAP_StopOngoingDataAcquisition(panelTitle)
@@ -39,6 +41,9 @@ Function TP_StartTestPulseSingleDevice(panelTitle)
 	if(GetCheckBoxState(panelTitle, "Check_Settings_BkgTP"))
 		TP_Setup(panelTitle, TEST_PULSE_BG_SINGLE_DEVICE)
 		ITC_StartBackgroundTestPulse(panelTitle)
+
+		headStage = GetSliderPositionIndex(panelTitle, "slider_DataAcq_ActiveHeadstage")
+		P_LoadPressureButtonState(panelTitle, headStage)
 	else
 		TP_Setup(panelTitle, TEST_PULSE_FG_SINGLE_DEVICE)
 		ITC_StartTestPulse(panelTitle)