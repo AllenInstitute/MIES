AllenInstitute/MIES

commit e349661bfd11e38782d2620d4e4bd6716c61906d

Versioning of manipulator wave getter functions. Update to DA_ephys panel macro to include Manipulator selection popup Moved find duplicate function to MIES_Utilities
Original-CommitID: 8cea741547842e6b76e2a88de5e0f955cebf16bd

Packages/MIES/MIES_PanelITC.ipf
@@ -439,7 +439,7 @@ Window DA_Ephys() : Panel
 	SetVariable SetVar_DataAcq_Comment,userdata(ResizeControlsInfo) += A"zzzzzzzzzzzz!!#u:Du]k<zzzzzzzzzzz"
 	SetVariable SetVar_DataAcq_Comment,userdata(ResizeControlsInfo) += A"zzz!!#u:Du]k<zzzzzzzzzzzzzz!!!"
 	SetVariable SetVar_DataAcq_Comment,fSize=8,value= _STR:""
-	Button DataAcquireButton,pos={44,711},size={395,42},disable=1,proc=DAP_ButtonProc_AcquireData,title="\\Z14\\f01Acquire\rData"
+	Button DataAcquireButton,pos={44,711},size={395,42},disable=1,proc=DAP_ButtonProc_AcquireDataMD,title="\\Z14\\f01Acquire\rData"
 	Button DataAcquireButton,userdata(tabnum)=  "0",userdata(tabcontrol)=  "ADC"
 	Button DataAcquireButton,userdata(ResizeControlsInfo)= A"!!,Ch!!#C6J,hsRJ,ho(z!!#](Aon\"Qzzzzzzzzzzzzzz!!#](Aon\"Qzz"
 	Button DataAcquireButton,userdata(ResizeControlsInfo) += A"zzzzzzzzzzzz!!#u:Du]k<zzzzzzzzzzz"
@@ -875,7 +875,7 @@ Window DA_Ephys() : Panel
 	CheckBox Check_Settings_Append,userdata(ResizeControlsInfo) += A"zzzzzzzzzzzz!!#u:Du]k<zzzzzzzzzzz"
 	CheckBox Check_Settings_Append,userdata(ResizeControlsInfo) += A"zzz!!#u:Du]k<zzzzzzzzzzzzzz!!!"
 	CheckBox Check_Settings_Append,value= 0
-	CheckBox Check_Settings_BkgTP,pos={34,86},size={93,14},disable=1,title="Background TP"
+	CheckBox Check_Settings_BkgTP,pos={34,86},size={93,14},disable=3,title="Background TP"
 	CheckBox Check_Settings_BkgTP,help={"Use cautiously - intended primarily for software development"}
 	CheckBox Check_Settings_BkgTP,userdata(tabnum)=  "5"
 	CheckBox Check_Settings_BkgTP,userdata(tabcontrol)=  "ADC"
@@ -883,7 +883,7 @@ Window DA_Ephys() : Panel
 	CheckBox Check_Settings_BkgTP,userdata(ResizeControlsInfo) += A"zzzzzzzzzzzz!!#u:Du]k<zzzzzzzzzzz"
 	CheckBox Check_Settings_BkgTP,userdata(ResizeControlsInfo) += A"zzz!!#u:Du]k<zzzzzzzzzzzzzz!!!"
 	CheckBox Check_Settings_BkgTP,value= 1
-	CheckBox Check_Settings_BackgrndDataAcq,pos={34,193},size={156,14},disable=1,title="Background Data Acquisition"
+	CheckBox Check_Settings_BackgrndDataAcq,pos={34,193},size={156,14},disable=3,title="Background Data Acquisition"
 	CheckBox Check_Settings_BackgrndDataAcq,help={"You may notice that onscreen update isn't as smooth with background data acquisition. This is normal and unavoidable."}
 	CheckBox Check_Settings_BackgrndDataAcq,userdata(tabnum)=  "5"
 	CheckBox Check_Settings_BackgrndDataAcq,userdata(tabcontrol)=  "ADC"
@@ -2023,7 +2023,7 @@ Window DA_Ephys() : Panel
 	TitleBox Title_Hardware_IC_AD_Div1,pos={153,440},size={15,13},title="V /"
 	TitleBox Title_Hardware_IC_AD_Div1,userdata(tabnum)=  "6"
 	TitleBox Title_Hardware_IC_AD_Div1,userdata(tabcontrol)=  "ADC",frame=0
-	GroupBox GroupBox_Hardware_Associations,pos={23,305},size={400,300},title="DAC Channel and Device Associations"
+	GroupBox GroupBox_Hardware_Associations,pos={23,305},size={400,350},title="DAC Channel and Device Associations"
 	GroupBox GroupBox_Hardware_Associations,userdata(tabnum)=  "6"
 	GroupBox GroupBox_Hardware_Associations,userdata(tabcontrol)=  "ADC"
 	GroupBox group_Settings_DatAcq,pos={20,169},size={424,258},disable=1,title="Data Acquisition"
@@ -2101,7 +2101,7 @@ Window DA_Ephys() : Panel
 	SetVariable setvar_Hardware_YokeList,userdata(tabnum)=  "6"
 	SetVariable setvar_Hardware_YokeList,userdata(tabcontrol)=  "ADC"
 	SetVariable setvar_Hardware_YokeList,labelBack=(60928,60928,60928),frame=0
-	SetVariable setvar_Hardware_YokeList,value= _STR:"No Yoked Devices",noedit= 1
+	SetVariable setvar_Hardware_YokeList,value= _STR:"Device is not yokeable",noedit= 1
 	Button button_Hardware_RemoveYoke,pos={335,240},size={80,21},disable=2,proc=DAP_ButtonProc_YokeRelease,title="Release"
 	Button button_Hardware_RemoveYoke,userdata(tabnum)=  "6"
 	Button button_Hardware_RemoveYoke,userdata(tabcontrol)=  "ADC"
@@ -2349,7 +2349,7 @@ Window DA_Ephys() : Panel
 	PopupMenu popup_Settings_Pressure_ITCdev,help={"List of available ITC devices for pressure control"}
 	PopupMenu popup_Settings_Pressure_ITCdev,userdata(tabnum)=  "6"
 	PopupMenu popup_Settings_Pressure_ITCdev,userdata(tabcontrol)=  "ADC"
-	PopupMenu popup_Settings_Pressure_ITCdev,mode=1,popvalue="- none -",value= #"\"- none -;ITC1600_Dev_1;ITC1600_Dev_2;ITC1600_Dev_3;\""
+	PopupMenu popup_Settings_Pressure_ITCdev,mode=3,popvalue="ITC1600_Dev_2",value= #"\"- none -;ITC1600_Dev_1;ITC1600_Dev_2;ITC1600_Dev_3;\""
 	TitleBox Title_settings_Hardware_Pressur,pos={31,481},size={41,13},title="Pressure"
 	TitleBox Title_settings_Hardware_Pressur,userdata(tabnum)=  "6"
 	TitleBox Title_settings_Hardware_Pressur,userdata(tabcontrol)=  "ADC",frame=0
@@ -2364,19 +2364,19 @@ Window DA_Ephys() : Panel
 	SetVariable setvar_Settings_Pressure_DAgain,pos={98,532},size={50,16},proc=DAP_SetVarProc_CAA
 	SetVariable setvar_Settings_Pressure_DAgain,userdata(tabnum)=  "6"
 	SetVariable setvar_Settings_Pressure_DAgain,userdata(tabcontrol)=  "ADC"
-	SetVariable setvar_Settings_Pressure_DAgain,value= _NUM:nan
+	SetVariable setvar_Settings_Pressure_DAgain,value= _NUM:0.5
 	SetVariable setvar_Settings_Pressure_ADgain,pos={98,557},size={50,16},proc=DAP_SetVarProc_CAA
 	SetVariable setvar_Settings_Pressure_ADgain,userdata(tabnum)=  "6"
 	SetVariable setvar_Settings_Pressure_ADgain,userdata(tabcontrol)=  "ADC"
-	SetVariable setvar_Settings_Pressure_ADgain,value= _NUM:nan
+	SetVariable setvar_Settings_Pressure_ADgain,value= _NUM:2
 	SetVariable SetVar_Hardware_Pressur_DA_Unit,pos={156,532},size={30,16},proc=DAP_SetVarProc_CAA
 	SetVariable SetVar_Hardware_Pressur_DA_Unit,userdata(tabnum)=  "6"
 	SetVariable SetVar_Hardware_Pressur_DA_Unit,userdata(tabcontrol)=  "ADC"
-	SetVariable SetVar_Hardware_Pressur_DA_Unit,value= _STR:""
+	SetVariable SetVar_Hardware_Pressur_DA_Unit,value= _STR:"psi"
 	SetVariable SetVar_Hardware_Pressur_AD_Unit,pos={177,557},size={30,16},proc=DAP_SetVarProc_CAA
 	SetVariable SetVar_Hardware_Pressur_AD_Unit,userdata(tabnum)=  "6"
 	SetVariable SetVar_Hardware_Pressur_AD_Unit,userdata(tabcontrol)=  "ADC"
-	SetVariable SetVar_Hardware_Pressur_AD_Unit,value= _STR:""
+	SetVariable SetVar_Hardware_Pressur_AD_Unit,value= _STR:"psi"
 	TitleBox Title_Hardware_Pressure_DA_Div,pos={190,534},size={15,13},title="/ V"
 	TitleBox Title_Hardware_Pressure_DA_Div,userdata(tabnum)=  "6"
 	TitleBox Title_Hardware_Pressure_DA_Div,userdata(tabcontrol)=  "ADC",frame=0
@@ -2400,7 +2400,7 @@ Window DA_Ephys() : Panel
 	SetVariable setvar_Settings_InBathP,help={"Set the (positive) pressure applied to the pipette when the pipette is in the bath."}
 	SetVariable setvar_Settings_InBathP,userdata(tabnum)=  "5"
 	SetVariable setvar_Settings_InBathP,userdata(tabcontrol)=  "ADC"
-	SetVariable setvar_Settings_InBathP,limits={-10,10,0.1},value= _NUM:0.549999952316284
+	SetVariable setvar_Settings_InBathP,limits={-10,10,0.1},value= _NUM:0.550000011920929
 	SetVariable setvar_Settings_InSliceP,pos={287,679},size={105,16},disable=1,proc=DAP_SetVarProc_CAA,title="In slice P (psi)"
 	SetVariable setvar_Settings_InSliceP,help={"Set the (positive) pressure applied to the pipette when the pipette is in the tissue specimen."}
 	SetVariable setvar_Settings_InSliceP,userdata(tabnum)=  "5"
@@ -2435,7 +2435,7 @@ Window DA_Ephys() : Panel
 	Button button_Settings_UpdateDACList,help={"Updates the popup menu contents to show the available ITC devices"}
 	Button button_Settings_UpdateDACList,userdata(tabnum)=  "6"
 	Button button_Settings_UpdateDACList,userdata(tabcontrol)=  "ADC"
-	Button button_Hardware_P_Enable,pos={285,527},size={60,46},proc=P_ButtonProc_Enable,title="Enable"
+	Button button_Hardware_P_Enable,pos={285,527},size={60,46},disable=2,proc=P_ButtonProc_Enable,title="Enable"
 	Button button_Hardware_P_Enable,help={"Enable ITC devices used for pressure regulation."}
 	Button button_Hardware_P_Enable,userdata(tabnum)=  "6"
 	Button button_Hardware_P_Enable,userdata(tabcontrol)=  "ADC",fSize=14
@@ -2445,13 +2445,13 @@ Window DA_Ephys() : Panel
 	Button button_Hardware_P_Disable,userdata(tabcontrol)=  "ADC",fSize=14
 	ValDisplay valdisp_DataAcq_P_0,pos={53,331},size={102,17},bodyWidth=35,disable=1,title="\\Z10Pressure (psi)"
 	ValDisplay valdisp_DataAcq_P_0,userdata(tabcontrol)=  "tab_DataAcq_Pressure"
-	ValDisplay valdisp_DataAcq_P_0,fSize=14,fStyle=0,valueColor=(65535,65535,65535)
-	ValDisplay valdisp_DataAcq_P_0,valueBackColor=(0,0,0)
+	ValDisplay valdisp_DataAcq_P_0,fSize=14,fStyle=0
+	ValDisplay valdisp_DataAcq_P_0,valueBackColor=(65535,65535,65535)
 	ValDisplay valdisp_DataAcq_P_0,limits={0,0,0},barmisc={0,1000},value= #"0.00"
 	ValDisplay valdisp_DataAcq_P_1,pos={158,331},size={35,17},bodyWidth=35,disable=1
 	ValDisplay valdisp_DataAcq_P_1,userdata(tabcontrol)=  "tab_DataAcq_Pressure"
-	ValDisplay valdisp_DataAcq_P_1,fSize=14,fStyle=0
-	ValDisplay valdisp_DataAcq_P_1,valueBackColor=(65535,65535,65535)
+	ValDisplay valdisp_DataAcq_P_1,fSize=14,fStyle=0,valueColor=(65535,65535,65535)
+	ValDisplay valdisp_DataAcq_P_1,valueBackColor=(0,0,0)
 	ValDisplay valdisp_DataAcq_P_1,limits={0,0,0},barmisc={0,1000},value= #"0.00"
 	ValDisplay valdisp_DataAcq_P_2,pos={198,331},size={35,17},bodyWidth=35,disable=1
 	ValDisplay valdisp_DataAcq_P_2,userdata(tabcontrol)=  "tab_DataAcq_Pressure"
@@ -2570,6 +2570,14 @@ Window DA_Ephys() : Panel
 	PopupMenu Popup_Settings_SampIntMult,userdata(ResizeControlsInfo) += A"zzzzzzzzzzzz!!#u:Du]k<zzzzzzzzzzz"
 	PopupMenu Popup_Settings_SampIntMult,userdata(ResizeControlsInfo) += A"zzz!!#u:Du]k<zzzzzzzzzzzzzz!!!"
 	PopupMenu Popup_Settings_SampIntMult,mode=1,popvalue="1",value= #"\"1;2;4;8;16;32;64\""
+	TitleBox Title_settings_Hardware_Manipul,pos={31,588},size={60,13},title="Manipulators"
+	TitleBox Title_settings_Hardware_Manipul,userdata(tabnum)=  "6"
+	TitleBox Title_settings_Hardware_Manipul,userdata(tabcontrol)=  "ADC",frame=0
+	PopupMenu popup_Settings_Manip_MSSMnipLst,pos={75,605},size={176,21},bodyWidth=150,proc=DAP_PopMenuProc_CAA,title="MSS"
+	PopupMenu popup_Settings_Manip_MSSMnipLst,help={"List of available Scientifica micromanipulators"}
+	PopupMenu popup_Settings_Manip_MSSMnipLst,userdata(tabnum)=  "6"
+	PopupMenu popup_Settings_Manip_MSSMnipLst,userdata(tabcontrol)=  "ADC"
+	PopupMenu popup_Settings_Manip_MSSMnipLst,mode=2,popvalue="mg1",value= #"\"- none -;\" + M_GetListOfAttachedManipulators()"
 	DefineGuide UGV0={FR,-25},UGH0={FB,-27},UGV1={FL,481}
 	SetWindow kwTopWin,hook(cleanup)=DAP_WindowHook
 	SetWindow kwTopWin,userdata(ResizeControlsInfo)= A"!!*'\"z!!#Du5QF1NJ,fQL!!*'\"zzzzzzzzzzzzzzzzzzz"

Packages/MIES/MIES_Manipulator.ipf
@@ -6,8 +6,8 @@
 // relevant wave and DF getters:
 // GetManipulatorPathAsString()
 // GetManipulatorPath()
-// GetHSManipulatorName(panelTitle)
-// GetHSManipulatorAssignments(panelTitle)
+// GetHSManipulatorName(panelTitle) - returns manipulator name strings assigned to headstage
+// GetHSManipulatorAssignments(panelTitle) - returns manipulator number assigned to headstage
 
 // server commands (without manipulator name, where required)
 Static StrConstant DEVICE_LIST = "http://localhost:8889/geom_config?fn=list_geom_iids"
@@ -34,7 +34,6 @@ Function/S M_ExecuteMSSCommand(cmd, [deviceName])
 		endif
 		return M_ExecuteMSSServerCall(cmd)
 	endif
-	
 	ASSERT(0, "Server command failed")
 End
 
@@ -195,7 +194,7 @@ Function M_ManipulatorGizmoPlot(panelTitle, [sweep])
 	string setting
 	DFREF ManipulatorDF = GetManipulatorPath()
 	DFREF settingsHistoryDFR = GetDevSpecLabNBSettHistFolder(panelTitle)
-	WAVE/D/Z/SDFR=settingsHistoryDFR Settingshistory
+	WAVE/D/Z/SDFR=settingsHistoryDFR SettingsHistory
 	WAVE WaveForGizmo = GetManipulatorPos(panelTitle)
 	if(paramIsDefault(sweep))
 		sweep = DM_ReturnLastSweepAcquired(panelTitle)
@@ -212,27 +211,10 @@ Function M_ManipulatorGizmoPlot(panelTitle, [sweep])
 	Execute cmd
 End		
 
-/// @brief Detects duplicate values in a 1d wave.
-///
-/// Returns -1 if duplicate is NOT found. Will not report NaNs as duplicates
-/// Igor 7 will have a findDuplicates command
-Function SearchForDuplicates(Wv)
-	WAVE Wv
-	ASSERT(dimsize(Wv,1) <= 1, (nameofwave(Wv) + " is not a 1D wave")) // make sure wave passed in is 1d
-	Duplicate/FREE Wv WvCopyOne WvCopyTwo // make two copies. One to store duplicate search results, the other to sort and search for duplicates.
-	variable Rows = (dimSize(Wv,0) // create a variable so dimSize is only called once instead of twice.
-	WvCopyOne[Rows- 1] = 0 // Set last point to 0 because if it by chance was 1 it would come up as a duplicate, even when the penultimate value in Wv was not 1
-	Sort WvCopyTwo, WvCopyTwo // sort so that duplicates will be in adjacent rows
- 	WvCopyOne[0, Rows - 2] = WvCopyTwo[p] != WvCopyTwo[p + 1] ? 0 : 1 // could multithread but, MIES use case will be with short 1d waves.
-	FindValue/V=1 WvCopyOne
-	return V_value
-End
-
 /// @brief Steps manipulators on approach axis for headstages that are active and in approach pressure mode
-///
+/// Waiting for Approach to be implemented in MSS server
 Function M_ApproachStep(panelTitle, stepSize, [headstage])
 	string panelTitle
 	variable stepSize
 	variable headstage
-	
 End

Packages/MIES/MIES_Utilities.ipf
@@ -1902,3 +1902,19 @@ Function/S AddPrefixToEachListItem(prefix, list)
 
 	return result
 End
+
+/// @brief Detects duplicate values in a 1d wave.
+///
+/// Returns -1 if duplicate is NOT found. Will not report NaNs as duplicates
+/// Igor 7 will have a findDuplicates command
+Function SearchForDuplicates(Wv)
+	WAVE Wv
+	ASSERT(dimsize(Wv,1) <= 1, (nameofwave(Wv) + " is not a 1D wave")) // make sure wave passed in is 1d
+	Duplicate/FREE Wv WvCopyOne WvCopyTwo // make two copies. One to store duplicate search results, the other to sort and search for duplicates.
+	variable Rows = (dimSize(Wv,0) // create a variable so dimSize is only called once instead of twice.
+	WvCopyOne[Rows- 1] = 0 // Set last point to 0 because if it by chance was 1, it would come up as a duplicate, even when the penultimate value in Wv was not 1
+	Sort WvCopyTwo, WvCopyTwo // sort so that duplicates will be in adjacent rows
+ 	WvCopyOne[0, Rows - 2] = WvCopyTwo[p] != WvCopyTwo[p + 1] ? 0 : 1 // could multithread but, MIES use case will be with short 1d waves.
+	FindValue/V=1 WvCopyOne
+	return V_value
+End

Packages/MIES/MIES_WaveDataFolderGetters.ipf
@@ -86,16 +86,24 @@ Function/Wave GetHSManipulatorAssignments(panelTitle)
 	string panelTitle
 
 	DFREF dfr = GetManipulatorPath()
+	variable versionOfNewWave = 1
 	
 	Wave/Z/SDFR=dfr wv = $panelTitle
-
-	if(WaveExists(wv))
+	
+	if(ExistsWithCorrectLayoutVersion(wv, versionOfNewWave))
 		return wv
+	elseif(WaveExists(wv))
+	    // change the required dimensions and leave all others untouched with -1
+ 	    // the extended dimensions are initialized with zero
+ 		Redimension/N=(NUM_HEADSTAGES,1, -1, -1) wv
+	else
+		Make/N=(NUM_HEADSTAGES, 1) dfr:$panelTitle/Wave=wv
+		wv = NaN
+		SetDimLabel COLS, 0, ManipulatorNumber, wv
 	endif
-
-	Make/N=(NUM_HEADSTAGES, 1) dfr:$panelTitle/Wave=wv
-	wv = NaN
-	SetDimLabel COLS, 0, ManipulatorNumber, wv
+	
+	SetWaveVersion(wv, versionOfNewWave)
+	
 	return wv
 End
 
@@ -111,16 +119,22 @@ Function/Wave GetHSManipulatorName(panelTitle)
 	string panelTitle
 
 	DFREF dfr = GetManipulatorPath()
-
+	variable versionOfNewWave = 1
+	
 	Wave/T/Z/SDFR=dfr wv = $panelTitle + "_S"
 
-	if(WaveExists(wv))
+	if(ExistsWithCorrectLayoutVersion(wv, versionOfNewWave))
 		return wv
+	elseif(WaveExists(wv))
+ 		Redimension/N=(NUM_HEADSTAGES,1, -1, -1) wv
+ 	else 
+ 		Make/T/N=(NUM_HEADSTAGES, 1) dfr:$panelTitle+"_S"/Wave=wv
+		wv = ""
+		SetDimLabel COLS, 0, ManipulatorName, wv
 	endif
 
-	Make/T/N=(NUM_HEADSTAGES, 1) dfr:$panelTitle+"_S"/Wave=wv
-	wv = ""
-	SetDimLabel COLS, 0, ManipulatorName, wv
+	SetWaveVersion(wv, versionOfNewWave)
+	
 	return wv
 End
 
@@ -138,20 +152,23 @@ Function/Wave GetManipulatorPos(panelTitle)
 	string panelTitle
 	string Name = "Gizmo_" + panelTitle
 	DFREF dfr = GetManipulatorPath()
+	variable versionOfNewWave = 1
 
 	Wave/Z/SDFR=dfr wv =$Name
 
-	if(WaveExists(wv))
+	if(ExistsWithCorrectLayoutVersion(wv, versionOfNewWave))
 		return wv
+	elseif(WaveExists(wv))
+ 		Redimension/N=(NUM_HEADSTAGES,3, -1, -1) wv
+ 		wv = NaN
+	else
+		Make/N=(NUM_HEADSTAGES, 3) dfr:$Name/Wave=wv
+		SetDimLabel COLS, 0, Xpos, wv
+		SetDimLabel COLS, 1, Ypos, wv
+		SetDimLabel COLS, 2, Zpos, wv
 	endif
 
-	Make/N=(NUM_HEADSTAGES, 3) dfr:$Name/Wave=wv
-	wv = NaN
-
-	SetDimLabel COLS, 0, Xpos, wv
-	SetDimLabel COLS, 1, Ypos, wv
-	SetDimLabel COLS, 2, Zpos, wv
-
+	SetWaveVersion(wv, versionOfNewWave)
 	return wv
 End
 

commit ef55747946481fa8f7363887a474d7cd2cf7bb6d

Bugfix: Fix initial state of pressure buttons
Broken since e349661b (Versioning of manipulator wave getter functions.
Update to DA_ephys panel macro to include Manipulator selecti
on popup Moved find duplicate function to MIES_Utilities, 2015-08-30).

Original-CommitID: d4cdc74b6fb6ed09cd832506e0543038495c7122

Packages/MIES/MIES_PanelITC.ipf
@@ -2435,7 +2435,7 @@ Window DA_Ephys() : Panel
 	Button button_Settings_UpdateDACList,help={"Updates the popup menu contents to show the available ITC devices"}
 	Button button_Settings_UpdateDACList,userdata(tabnum)=  "6"
 	Button button_Settings_UpdateDACList,userdata(tabcontrol)=  "ADC"
-	Button button_Hardware_P_Enable,pos={285,527},size={60,46},disable=2,proc=P_ButtonProc_Enable,title="Enable"
+	Button button_Hardware_P_Enable,pos={285,527},size={60,46},proc=P_ButtonProc_Enable,title="Enable"
 	Button button_Hardware_P_Enable,help={"Enable ITC devices used for pressure regulation."}
 	Button button_Hardware_P_Enable,userdata(tabnum)=  "6"
 	Button button_Hardware_P_Enable,userdata(tabcontrol)=  "ADC",fSize=14
@@ -2977,6 +2977,9 @@ Function DAP_EphysPanelStartUpSettings(panelTitle)
 	CheckBox check_Settings_TP_SaveTPRecord WIN = $panelTitle, value = 0
 	CheckBox check_settings_TP_show_steady WIN = $panelTitle, value = 1
 	CheckBox check_settings_TP_show_peak WIN = $panelTitle, value = 1
+
+	EnableControl(panelTitle, "button_Hardware_P_Enable")
+	DisableControl(panelTitle, "button_Hardware_P_Disable")
 
 	return 0
 End