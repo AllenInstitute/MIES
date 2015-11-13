AllenInstitute/MIES

commit dfe2d8629ab206683d4b2c83129b65fe43905eea

Make the function AB_SplitTTLWaveIntoComponents available for all
Rename it therefore to SplitTTLWaveIntoComponents and move the "_"
character into the wavePrefix parameter.

Original-CommitID: 4ada04a26444dee907fc9d6d01ed48894b094599

Packages/MIES/MIES_AnalysisBrowser.ipf
@@ -838,12 +838,12 @@ static Function AB_SplitSweepIntoComponents(expFolder, device, sweep, sweepWave)
 		ASSERT(!isEmpty(channelType), "empty channel type")
 		channelNumber = config[i][1]
 		ASSERT(IsFinite(channelNumber), "non-finite channel number")
-		str = channelType + "_" + num2istr(channelNumber)
+		str = channelType + "_" + num2istr(channelNumber) + "_"
 
 		WAVE data = ExtractOneDimDataFromSweep(config, sweepWave, i)
 
 		if(!cmpstr(channelType, "TTL"))
-			AB_SplitTTLWaveIntoComponents(data, GetTTLBits(numericValues, sweep, channelNumber), sweepFolder, str)
+			SplitTTLWaveIntoComponents(data, GetTTLBits(numericValues, sweep, channelNumber), sweepFolder, str)
 		endif
 
 		MoveWave data, sweepFolder:$str
@@ -853,30 +853,6 @@ static Function AB_SplitSweepIntoComponents(expFolder, device, sweep, sweepWave)
 	KillOrMoveToTrash(wv=sweepWave)
 
 	return 0
-End
-
-static Function AB_SplitTTLWaveIntoComponents(data, ttlBits, sweepFolder, wvName)
-	WAVE data
-	variable ttlBits
-	DFREF sweepFolder
-	string wvName
-
-	if(!IsFinite(ttlBits))
-		return NaN
-	endif
-
-	variable i, bit
-
-	for(i = 0; i < NUM_TTL_BITS_PER_RACK; i += 1)
-
-		bit = 2^i
-		if(!(ttlBits & bit))
-			continue
-		endif
-
-		Duplicate data, sweepFolder:$(wvName + "_" + num2str(i))/Wave=dest
-		MultiThread dest[] = dest[p] & bit
-	endfor
 End
 
 Function AB_ScanFolder(win)

Packages/MIES/MIES_MiesUtilities.ipf
@@ -2059,3 +2059,32 @@ Function/S ExtractAnalysisFuncFromStimSet(stimSet, eventType)
 
 	return StringByKey(eventName, wvnote, "=", ";")
 End
+
+/// @brief Split TTL data into a single wave for each channel
+/// @param data       1D channel data extracted by #ExtractOneDimDataFromSweep
+/// @param ttlBits    bit mask of the active TTL channels form e.g. #GetTTLBits
+/// @param targetDFR  datafolder where to put the waves, can be a free datafolder
+/// @param wavePrefix prefix of the created wave names
+Function SplitTTLWaveIntoComponents(data, ttlBits, targetDFR, wavePrefix)
+	WAVE data
+	variable ttlBits
+	DFREF targetDFR
+	string wavePrefix
+
+	variable i, bit
+
+	if(!IsFinite(ttlBits))
+		return NaN
+	endif
+
+	for(i = 0; i < NUM_TTL_BITS_PER_RACK; i += 1)
+
+		bit = 2^i
+		if(!(ttlBits & bit))
+			continue
+		endif
+
+		Duplicate data, targetDFR:$(wavePrefix + num2str(i))/Wave=dest
+		MultiThread dest[] = dest[p] & bit
+	endfor
+End

commit b41acdf23a10ee1ece4cecf8bc8d3e6fa38fcf28

AB_SplitSweepIntoComponents: Fix wave splitting
Broken since dfe2d862 (Make the function AB_SplitTTLWaveIntoComponents
available for all, 2015-10-07). The underline "_" must only be added for
TTL stimsets.

Original-CommitID: 6b56d8801187dff7a22147a97f97f51d0f5f9b12

Packages/MIES/MIES_AnalysisBrowser.ipf
@@ -838,12 +838,12 @@ static Function AB_SplitSweepIntoComponents(expFolder, device, sweep, sweepWave)
 		ASSERT(!isEmpty(channelType), "empty channel type")
 		channelNumber = config[i][1]
 		ASSERT(IsFinite(channelNumber), "non-finite channel number")
-		str = channelType + "_" + num2istr(channelNumber) + "_"
+		str = channelType + "_" + num2istr(channelNumber)
 
 		WAVE data = ExtractOneDimDataFromSweep(config, sweepWave, i)
 
 		if(!cmpstr(channelType, "TTL"))
-			SplitTTLWaveIntoComponents(data, GetTTLBits(numericValues, sweep, channelNumber), sweepFolder, str)
+			SplitTTLWaveIntoComponents(data, GetTTLBits(numericValues, sweep, channelNumber), sweepFolder, str + "_")
 		endif
 
 		MoveWave data, sweepFolder:$str