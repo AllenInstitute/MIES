AllenInstitute/MIES

commit 9f3808810a6b225f6fbe0ce67af2036881419a83

DAP_CheckSettings: Find duplicate headstage <-> channel associations
For functions like AFH_GetHeadstageFromADC it is crucial that the
headstage AD/DA channel mapping is one to one.

Until now we have never enforced this. The symptom of a violation of this
precondition is that sweep dependent data for the labnotebook might end up
in the wrong layer, i.e. is connected with the wrong headstage.

Check for these before for DAQ and TP.

Original-CommitID: 044ede098f0131b40f620984170762f64d54d878

Packages/MIES/MIES_PanelITC.ipf
@@ -4230,7 +4230,7 @@ Function DAP_CheckSettings(panelTitle, mode)
 	string panelTitle
 	variable mode
 
-	variable numDACs, numADCs, numHS, numEntries, i, indexingEnabled
+	variable numDACs, numADCs, numHS, numEntries, i, indexingEnabled, ctrlNo, clampMode
 	string ctrl, endWave, ttlWave, dacWave, refDacWave
 	string list, msg
 
@@ -4352,6 +4352,49 @@ Function DAP_CheckSettings(panelTitle, mode)
 					endif
 				endfor
 			endif
+		endif
+
+		// avoid having different headstages reference the same amplifiers
+		// and/or DA/AD channels in the "DAC Channel and Device Associations" menu
+		Make/FREE/N=(NUM_HEADSTAGES) DACs, ADCs
+
+		WAVE chanAmpAssign = GetChanAmpAssign(panelTitle)
+
+		for(i = 0; i < NUM_HEADSTAGES; i += 1)
+
+			sprintf ctrl, "Check_DataAcq_HS_%02d", i
+			DAP_GetInfoFromControl(panelTitle, ctrl, ctrlNo, clampMode, i)
+
+			if(clampMode == V_CLAMP_MODE)
+				DACs[i] = ChanAmpAssign[0][i]
+				ADCs[i] = ChanAmpAssign[2][i]
+			elseif(clampMode == I_CLAMP_MODE)
+				DACs[i] = ChanAmpAssign[4][i]
+				ADCs[i] = ChanAmpAssign[6][i]
+			else
+				printf "(%s) Unhandled mode %d\r", panelTitle, clampMode
+				return 1
+			endif
+		endfor
+
+		if(SearchForDuplicates(DACs))
+			printf "(%s) Different headstages in the \"DAC Channel and Device Associations\" menu reference the same DA channels.\r", panelTitle
+			printf "Please clear the associations for unused headstages.\r"
+			return 1
+		endif
+
+		if(SearchForDuplicates(ADCs))
+			printf "(%s) Different headstages in the \"DAC Channel and Device Associations\" menu reference the same AD channels.\r", panelTitle
+			printf "Please clear the associations for unused headstages.\r"
+			return 1
+		endif
+
+		MatrixOP/FREE ampIndex = row(chanAmpAssign, 10)
+
+		if(SearchForDuplicates(ampIndex))
+			printf "(%s) Different headstages in the \"DAC Channel and Device Associations\" menu reference the same amplifier-channel-combination.\r", panelTitle
+			printf "Please clear the associations for unused headstages.\r"
+			return 1
 		endif
 
 		// check all active headstages

commit 0f5eaaf28c621617b8c59321ce7f766de4fd69cf

Bugfix: Pass a 1D wave with no columns to SearchForDuplicates
Bug introduced in 9f380881 (DAP_CheckSettings: Find duplicate headstage <-> channel
associations, 2015-12-15). Our IP6 version of SearchForDuplicates does not
accept waves with columns.

Original-CommitID: 825e998f898e69c8e127be6cc2f1a4818c91b82f

Packages/MIES/MIES_PanelITC.ipf
@@ -4387,7 +4387,7 @@ Function DAP_CheckSettings(panelTitle, mode)
 			return 1
 		endif
 
-		MatrixOP/FREE ampIndex = row(chanAmpAssign, 10)
+		MatrixOP/FREE ampIndex = row(chanAmpAssign, 10)^t
 
 		if(SearchForDuplicates(ampIndex))
 			printf "(%s) Different headstages in the \"DAC Channel and Device Associations\" menu reference the same amplifier-channel-combination.\r", panelTitle