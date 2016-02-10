AllenInstitute/MIES

commit a46f8c29088ccd8debf29652f847a3ed14781b23

WBP_UpdateITCPanelPopUps: Allow to update all locked panels
And also make it faster as we don't have to recalculate the list
of stim sets for each channel.

Original-CommitID: 2d314852dd7404dab5828346ea9ea22733f7005d

Packages/MIES/MIES_WaveBuilderPanel.ipf
@@ -965,14 +965,14 @@ Function WBP_ButtonProc_DeleteSet(ba) : ButtonControl
 					popupMenuSelectedItemsStart = WBP_PopupMenuWaveNameList(DAorTTL, 0, panelTitle)
 					popupMenuSelectedItemsEnd = WBP_PopupMenuWaveNameList(DAorTTL, 1, panelTitle)
 					WBP_DeleteSet()
-					WBP_UpdateITCPanelPopUps(panelTitle)
 					WBP_RestorePopupMenuSelection(popupMenuSelectedItemsStart, DAorTTL, 0, panelTitle)
 					WBP_RestorePopupMenuSelection(popupMenuSelectedItemsEnd, DAorTTL, 1, panelTitle)
-					WBP_UpdateITCPanelPopUps(panelTitle)
 				endfor
 			else
 				WBP_DeleteSet()
 			endif
+
+			WBP_UpdateITCPanelPopUps()
 
 			ControlUpdate/W=$panel popup_WaveBuilder_SetList
 			PopupMenu popup_WaveBuilder_SetList win=$panel, mode = 1
@@ -1054,15 +1054,7 @@ Function WBP_ButtonProc_SaveSet(ctrlName) : ButtonControl
 
 	RemoveTracesFromGraph(WaveBuilderGraph)
 	WBP_SaveSetParam()
-
-	SVAR/Z/SDFR=GetITCDevicesFolder() ITCPanelTitleList
-	if(SVAR_Exists(ITCPanelTitleList))
-		numPanels = ItemsInList(ITCPanelTitleList)
-		for(i = 0; i < numPanels; i += 1)
-			panelTitle = StringFromList(i, ITCPanelTitleList)
-			WBP_UpdateITCPanelPopUps(panelTitle)
-		endfor
-	endif
+	WBP_UpdateITCPanelPopUps()
 
 	SetSetVariableString(panel, "setvar_WaveBuilder_baseName", "InsertBaseName")
 	ControlUpdate/W=$panel popup_WaveBuilder_SetList
@@ -1628,27 +1620,45 @@ Function WBP_PopMenuProc_FolderSelect(ctrlName,popNum,popStr) : PopupMenuControl
 	ControlUpdate/A/W=$panel
 End
 
-/// @brief Used after a new set has been saved to the DA or TTL folder.
+/// @brief Update the popup menus and its `MenuExp` user data after stim set changes
 ///
-/// It repopulates the popup menus in the ITC control Panel to reflect the new waves
-Function WBP_UpdateITCPanelPopUps(panelTitle)
+/// @param panelTitle [optional, defaults to all locked devices] device
+Function WBP_UpdateITCPanelPopUps([panelTitle])
 	string panelTitle
 
-	variable i
-	string ctrlWave, ctrlIndexEnd, list
+	variable i, numPanels
+	string ctrlWave, ctrlIndexEnd, DAlist, TTLlist, listOfPanels
 
-	for(i=0; i < NUM_DA_TTL_CHANNELS; i+=1)
-		ctrlWave     = GetPanelControl(panelTitle, i, CHANNEL_TYPE_DAC, CHANNEL_CONTROL_WAVE)
-		ctrlIndexEnd = GetPanelControl(panelTitle, i, CHANNEL_TYPE_DAC, CHANNEL_CONTROL_INDEX_END)
-		list = ReturnListOfAllStimSets(CHANNEL_TYPE_DAC, CHANNEL_DA_SEARCH_STRING)
-		SetControlUserData(panelTitle, ctrlWave, "MenuExp", list)
-		SetControlUserData(panelTitle, ctrlIndexEnd, "MenuExp", list)
+	if(ParamIsDefault(panelTitle))
+		SVAR/Z/SDFR=GetITCDevicesFolder() ITCPanelTitleList
+		if(!SVAR_Exists(ITCPanelTitleList))
+			return NaN
+		endif
+		listOfPanels = ITCPanelTitleList
+	else
+		listOfPanels = panelTitle
+	endif
 
-		ctrlWave     = GetPanelControl(panelTitle, i, CHANNEL_TYPE_TTL, CHANNEL_CONTROL_WAVE)
-		ctrlIndexEnd = GetPanelControl(panelTitle, i, CHANNEL_TYPE_TTL, CHANNEL_CONTROL_INDEX_END)
-		list = ReturnListOfAllStimSets(CHANNEL_TYPE_TTL, CHANNEL_TTL_SEARCH_STRING)
-		SetControlUserData(panelTitle, ctrlWave, "MenuExp", list)
-		SetControlUserData(panelTitle, ctrlIndexEnd, "MenuExp", list)
+	DEBUGPRINT("Updating", str=listOfPanels)
+
+	DAlist  = ReturnListOfAllStimSets(CHANNEL_TYPE_DAC, CHANNEL_DA_SEARCH_STRING)
+	TTLlist = ReturnListOfAllStimSets(CHANNEL_TYPE_TTL, CHANNEL_TTL_SEARCH_STRING)
+
+	numPanels = ItemsInList(listOfPanels)
+	for(i = 0; i < numPanels; i += 1)
+		panelTitle = StringFromList(i, listOfPanels)
+
+		for(i = 0; i < NUM_DA_TTL_CHANNELS; i += 1)
+			ctrlWave     = GetPanelControl(panelTitle, i, CHANNEL_TYPE_DAC, CHANNEL_CONTROL_WAVE)
+			ctrlIndexEnd = GetPanelControl(panelTitle, i, CHANNEL_TYPE_DAC, CHANNEL_CONTROL_INDEX_END)
+			SetControlUserData(panelTitle, ctrlWave, "MenuExp", DAlist)
+			SetControlUserData(panelTitle, ctrlIndexEnd, "MenuExp", DAlist)
+
+			ctrlWave     = GetPanelControl(panelTitle, i, CHANNEL_TYPE_TTL, CHANNEL_CONTROL_WAVE)
+			ctrlIndexEnd = GetPanelControl(panelTitle, i, CHANNEL_TYPE_TTL, CHANNEL_CONTROL_INDEX_END)
+			SetControlUserData(panelTitle, ctrlWave, "MenuExp", TTLlist)
+			SetControlUserData(panelTitle, ctrlIndexEnd, "MenuExp", TTLlist)
+		endfor
 	endfor
 End
 

Packages/MIES/MIES_HardwareSetUp.ipf
@@ -99,7 +99,7 @@ Function HSU_LockDevice(panelTitle)
 	DAP_UpdateAllYokeControls()
 	// create the amplifier settings waves
 	GetAmplifierParamStorageWave(panelTitleLocked)
-	WBP_UpdateITCPanelPopUps(panelTitleLocked)
+	WBP_UpdateITCPanelPopUps(panelTitle=panelTitleLocked)
 	DAP_UnlockCommentNotebook(panelTitleLocked)
 	DAP_ToggleAcquisitionButton(panelTitleLocked, DATA_ACQ_BUTTON_TO_DAQ)
 	SI_CalculateMinSampInterval(panelTitleLocked)

Packages/MIES/MIES_PanelITC.ipf
@@ -3617,7 +3617,7 @@ Function DAP_CheckProc_IndexingState(cba) : CheckBoxControl
 
 		panelTitle = cba.win
 		// makes sure user data for controls is up to date
-		WBP_UpdateITCPanelPopUps(panelTitle)
+		WBP_UpdateITCPanelPopUps(panelTitle=panelTitle)
 		DAP_UpdateSweepSetVariables(panelTitle)
 		DAP_UpdateITIAcrossSets(panelTitle)
 		break

commit 81e8d270efc03b7080516c5ae26eb344ad8001d6

WBP_UpdateITCPanelPopUps: Fix updating multiple panels
If no optional argument panelTitle is passed to WBP_UpdateITCPanelPopUps
we don't iterate over all locked ITC devices, because the inner loop index
is the same as the outer loop index.

Fix that by using j as inner index.

Broken since a46f8c29 (WBP_UpdateITCPanelPopUps: Allow to update all
locked panels, 2015-09-17).

Original-CommitID: e51c29bb25924da8f4e3144daf5723568a2993db

Packages/MIES/MIES_WaveBuilderPanel.ipf
@@ -1835,7 +1835,7 @@ End
 Function WBP_UpdateITCPanelPopUps([panelTitle])
 	string panelTitle
 
-	variable i, numPanels
+	variable i, j, numPanels
 	string ctrlWave, ctrlIndexEnd, DAlist, TTLlist, listOfPanels
 
 	if(ParamIsDefault(panelTitle))
@@ -1857,14 +1857,14 @@ Function WBP_UpdateITCPanelPopUps([panelTitle])
 	for(i = 0; i < numPanels; i += 1)
 		panelTitle = StringFromList(i, listOfPanels)
 
-		for(i = 0; i < NUM_DA_TTL_CHANNELS; i += 1)
-			ctrlWave     = GetPanelControl(panelTitle, i, CHANNEL_TYPE_DAC, CHANNEL_CONTROL_WAVE)
-			ctrlIndexEnd = GetPanelControl(panelTitle, i, CHANNEL_TYPE_DAC, CHANNEL_CONTROL_INDEX_END)
+		for(j = 0; j < NUM_DA_TTL_CHANNELS; j += 1)
+			ctrlWave     = GetPanelControl(panelTitle, j, CHANNEL_TYPE_DAC, CHANNEL_CONTROL_WAVE)
+			ctrlIndexEnd = GetPanelControl(panelTitle, j, CHANNEL_TYPE_DAC, CHANNEL_CONTROL_INDEX_END)
 			SetControlUserData(panelTitle, ctrlWave, "MenuExp", DAlist)
 			SetControlUserData(panelTitle, ctrlIndexEnd, "MenuExp", DAlist)
 
-			ctrlWave     = GetPanelControl(panelTitle, i, CHANNEL_TYPE_TTL, CHANNEL_CONTROL_WAVE)
-			ctrlIndexEnd = GetPanelControl(panelTitle, i, CHANNEL_TYPE_TTL, CHANNEL_CONTROL_INDEX_END)
+			ctrlWave     = GetPanelControl(panelTitle, j, CHANNEL_TYPE_TTL, CHANNEL_CONTROL_WAVE)
+			ctrlIndexEnd = GetPanelControl(panelTitle, j, CHANNEL_TYPE_TTL, CHANNEL_CONTROL_INDEX_END)
 			SetControlUserData(panelTitle, ctrlWave, "MenuExp", TTLlist)
 			SetControlUserData(panelTitle, ctrlIndexEnd, "MenuExp", TTLlist)
 		endfor