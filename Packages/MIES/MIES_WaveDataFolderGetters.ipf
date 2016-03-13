AllenInstitute/MIES

commit 00401dcfe38ee5bad9c61ddb4ce8190f597aee16

Wavebuilder SegWvType: Add dimension labels
Introduce also wave versioning.

Original-CommitID: 371408bde457a69f0a7399fb1a66d62d7f306c34

Packages/MIES/MIES_WaveDataFolderGetters.ipf
@@ -1889,6 +1889,39 @@ Function/WAVE GetWaveBuilderWaveTextParam()
 	return wv
 End
 
+static Constant SEGWVTYPE_WAVE_LAYOUT_VERSION = 1
+
+/// @brief Upgrade the wave layout of `SegWvType` to the most recent one
+///        as defined in `SEGWVTYPE_WAVE_LAYOUT_VERSION`
+Function UpgradeSegWvType(wv)
+	WAVE wv
+
+	if(ExistsWithCorrectLayoutVersion(wv, WPT_WAVE_LAYOUT_VERSION))
+		return NaN
+	endif
+
+	AddDimLabelsToSegWvType(wv)
+	SetWaveVersion(wv, WPT_WAVE_LAYOUT_VERSION)
+End
+
+/// @brief Add dimension labels to the WaveBuilder `SegWvType` wave
+static Function AddDimLabelsToSegWvType(wv)
+	WAVE wv
+
+	variable i
+
+	for(i = 0; i <= SEGMENT_TYPE_WAVE_LAST_IDX; i += 1)
+	 SetDimLabel ROWS, i, $("Type of Epoch " + num2str(i)), wv
+	endfor
+
+	SetDimLabel ROWS, SEGMENT_TYPE_WAVE_LAST_IDX + 1, $("Flip time axis")        , wv
+	SetDimLabel ROWS, SEGMENT_TYPE_WAVE_LAST_IDX + 2, $("Inter trial interval")  , wv
+	SetDimLabel ROWS, SEGMENT_TYPE_WAVE_LAST_IDX + 3, $("Total number of epochs"), wv
+	SetDimLabel ROWS, SEGMENT_TYPE_WAVE_LAST_IDX + 4, $("Total number of steps") , wv
+
+	SetWaveVersion(wv, SEGWVTYPE_WAVE_LAYOUT_VERSION)
+End
+
 /// @brief Returns the segment type wave used by the wave builder panel
 /// Remember to change #SEGMENT_TYPE_WAVE_LAST_IDX if changing the wave layout
 /// - Rows
@@ -1899,17 +1932,19 @@ End
 ///   - 101: total number of steps
 Function/Wave GetSegmentTypeWave()
 
-	dfref dfr = GetWaveBuilderDataPath()
-	Wave/Z/SDFR=dfr wv = SegWvType
+	DFREF dfr = GetWaveBuilderDataPath()
+	WAVE/Z/SDFR=dfr wv = SegWvType
 
 	if(WaveExists(wv))
-		return wv
+		UpgradeSegWvType(wv)
+	else
+		Make/N=102 dfr:SegWvType/Wave=wv
+
+		wv[100] = 1
+		wv[101] = 1
+		AddDimLabelsToSegWvType(wv)
+		SetWaveVersion(wv, SEGWVTYPE_WAVE_LAYOUT_VERSION)
 	endif
-
-	Make/N=102 dfr:SegWvType/Wave=wv
-
-	wv[100] = 1
-	wv[101] = 1
 
 	return wv
 End

Packages/MIES/MIES_WaveBuilder.ipf
@@ -160,6 +160,7 @@ Function/Wave WB_GetStimSet([setName])
 
 		UpgradeWaveParam(WP)
 		UpgradeWaveTextParam(WPT)
+		UpgradeSegWvType(SegWvType)
 	endif
 
 	// WB_AddDelta modifies WP so we pass a copy instead

commit 9011a0518ec08601b4c91398d148bed3d911bc68

UpgradeSegWvType: Use correct constants
Bug introduced in 00401dcf (Wavebuilder SegWvType: Add dimension labels,
2016-02-23).

The WPT_WAVE_LAYOUT_VERSION is 4 so we now also set the
SEGWVTYPE_WAVE_LAYOUT_VERSION constant to 4 in order to neither do an
unnecessary upgrade nor use decrementing wave version numbers.

Original-CommitID: d16e5ace6e20b0e767ff81c13a11338a195884ad

Packages/MIES/MIES_WaveDataFolderGetters.ipf
@@ -1988,19 +1988,19 @@ Function/WAVE GetWaveBuilderWaveTextParam()
 	return wv
 End
 
-static Constant SEGWVTYPE_WAVE_LAYOUT_VERSION = 1
+static Constant SEGWVTYPE_WAVE_LAYOUT_VERSION = 4
 
 /// @brief Upgrade the wave layout of `SegWvType` to the most recent one
 ///        as defined in `SEGWVTYPE_WAVE_LAYOUT_VERSION`
 Function UpgradeSegWvType(wv)
 	WAVE wv
 
-	if(ExistsWithCorrectLayoutVersion(wv, WPT_WAVE_LAYOUT_VERSION))
+	if(ExistsWithCorrectLayoutVersion(wv, SEGWVTYPE_WAVE_LAYOUT_VERSION))
 		return NaN
 	endif
 
 	AddDimLabelsToSegWvType(wv)
-	SetWaveVersion(wv, WPT_WAVE_LAYOUT_VERSION)
+	SetWaveVersion(wv, SEGWVTYPE_WAVE_LAYOUT_VERSION)
 End
 
 /// @brief Add dimension labels to the WaveBuilder `SegWvType` wave