AllenInstitute/MIES

commit 76838ea58ad919312eec01b8cf8f5dcdbf432b73

Code cleanup: Use constant NONE
Original-CommitID: 7b36afe96ab0d70fa607f84826bc862ac5cda557

Packages/MIES/MIES_PressureControl.ipf
@@ -1593,7 +1593,7 @@ Function ButtonProc_Hrdwr_P_UpdtDAClist(ba) : ButtonControl
 
 	switch(ba.eventCode)
 		case 2: // mouse up
-			string DeviceList = "- none -;" + HSU_ListDevices()
+			string DeviceList = NONE + HSU_ListDevices()
 			SetPopupMenuVal(ba.win, "popup_Settings_Pressure_ITCdev", DeviceList)
 			break
 	endswitch

Packages/MIES/MIES_BackgroundMD.ipf
@@ -127,7 +127,6 @@ Function ITC_FIFOMonitorMD(s)
 		if(fifoPos >= ActiveDeviceList[i][2])
 			print "stopped data acq on " + panelTitle, "device ID global = ", ActiveDeviceList[i][0]
 			DeviceIDGlobal = ActiveDeviceList[i][0]
-			ITC_MakeOrUpdateActivDevLstWave(panelTitle, DeviceIDGlobal, 0, 0, -1)
 			ITC_StopDataAcqMD(panelTitle, DeviceIDGlobal)
 			i = 0
 			continue

Packages/MIES/MIES_Manipulator.ipf
@@ -114,7 +114,7 @@ Function M_SetManipulatorAssociation(panelTitle)
 	WAVE ManipulatorDataWave = GetHSManipulatorAssignments(panelTitle)
 	WAVE/T ManipulatorTextWave = GetHSManipulatorName(panelTitle)
 	string ManipulatorName = GetPopupMenuString(panelTitle, "popup_Settings_Manip_MSSMnipLst")
-	if(cmpstr("- none -", ManipulatorName) == 0)
+	if(cmpstr(NONE, ManipulatorName) == 0)
 		ManipulatorDataWave[headStage][%ManipulatorNumber] = NaN
 		ManipulatorTextWave[headStage][%ManipulatorName] = ""
 	else

Packages/MIES/MIES_WaveDataFolderGetters.ipf
@@ -3196,7 +3196,7 @@ Function/WAVE P_PressureDataTxtWaveRef(panelTitle)
 	SetDimLabel ROWS, 6, Headstage_6, PressureDataTextWv
 	SetDimLabel ROWS, 7, Headstage_7, PressureDataTextWv
 
-	PressureDataTextWv[][0] = "- none -"
+	PressureDataTextWv[][0] = NONE
 
 	return PressureDataTextWv
 End

commit eb727f69fdd69fc4322d745d42533f76cd6184e6

Add forgotten list separator
Bug introduced in 76838ea5 (Code cleanup: Use constant NONE, 2015-11-27).

Original-CommitID: ffccc0861d10d507ee4f9736abac75ab9628372c

Packages/MIES/MIES_PressureControl.ipf
@@ -1576,7 +1576,7 @@ Function ButtonProc_Hrdwr_P_UpdtDAClist(ba) : ButtonControl
 
 	switch(ba.eventCode)
 		case 2: // mouse up
-			string DeviceList = NONE + HSU_ListDevices()
+			string DeviceList = AddListItem(HSU_ListDevices(), NONE, ";", Inf)
 			SetPopupMenuVal(ba.win, "popup_Settings_Pressure_ITCdev", DeviceList)
 			break
 	endswitch