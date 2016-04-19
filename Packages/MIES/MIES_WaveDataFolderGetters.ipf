AllenInstitute/MIES

commit 934e823e6c1b0b1009555f4504cfd93e6d8984d7

Extend GUIStateWave to hold all unique controls
Generate list of control names and formatted control names
Added common control count constant
Get control type as string function added to GUIUtilities
Added function to update the state of the unique controls in the GUIStateWave

Original-CommitID: d967bff5d501cf5d9b1763abf577617a8c42bc0b

Packages/MIES/MIES_WaveDataFolderGetters.ipf
@@ -3588,22 +3588,30 @@ End
 /// 14: AlarmState State of control check_AsyncAlarm_RowNum. 0 = UnChecked, 1 = Checked
 /// 15: AlarmMin Internal number stored in control min_AsyncAD__RowNum. The minium value alarm trigger.
 /// 16: AlarmMax Internal number stored in control max_AsyncAD_RowNum. The max value alarm trigger.
+/// 17+: Unique controls
 Function/Wave GetDA_EphysGuiStateNum(panelTitle)
 	string panelTitle
-	DFREF dfr= GetDevicePath(panelTitle)
-	variable versionOfNewWave = 1
-	Wave/Z/SDFR=dfr wv = DA_EphysGuiState
 
- 	if(ExistsWithCorrectLayoutVersion(wv, versionOfNewWave))
- 		return wv
- 	elseif(WaveExists(wv)) // handle upgrade
- 	    // change the required dimensions and leave all others untouched with -1
- 	    // the extended dimensions are initialized with zero
-		Redimension/N=(NUM_MAX_CHANNELS, 17, -1, -1) wv
- 	else
-		Make/N=(NUM_MAX_CHANNELS, 17) dfr:DA_EphysGuiState/Wave=wv
+	DFREF dfr= GetDevicePath(panelTitle)
+	Wave/Z/SDFR=dfr wv = DA_EphysGuiState
+	variable uniqueCtrlCount
+	string uniqueCtrlList
+
+	if(ExistsWithCorrectLayoutVersion(wv, DA_EPHYS_PANEL_VERSION))
+		return wv
+	elseif(WaveExists(wv)) // handle upgrade
+		// change the required dimensions and leave all others untouched with -1
+		// the extended dimensions are initialized with zero
+		uniqueCtrlList = GetUniqueSpecCtrlTypeList(panelTitle)
+		uniqueCtrlCount = itemsInList(uniqueCtrlList)
+		Redimension/N=(NUM_MAX_CHANNELS, COMMON_CONTROL_GROUP_COUNT + uniqueCtrlCount, -1, -1) wv
 		wv = Nan
- 	endif
+	else
+		uniqueCtrlList = GetUniqueSpecCtrlTypeList(panelTitle)
+		uniqueCtrlCount = itemsInList(uniqueCtrlList)
+		Make/N=(NUM_MAX_CHANNELS, COMMON_CONTROL_GROUP_COUNT + uniqueCtrlCount) dfr:DA_EphysGuiState/Wave=wv
+		wv = Nan
+	endif
 
 	SetDimLabel COLS, 0, HSState, wv
 	SetDimLabel COLS, 1, HSMode, wv
@@ -3623,9 +3631,76 @@ Function/Wave GetDA_EphysGuiStateNum(panelTitle)
 	SetDimLabel COLS, 15, AlarmMin, wv
 	SetDimLabel COLS, 16, AlarmMax, wv
 
- 	SetWaveVersion(wv, versionOfNewWave)
+	SetWaveDimLabel(wv, uniqueCtrlList, COLS, startPos = COMMON_CONTROL_GROUP_COUNT)
+	SetWaveVersion(wv, DA_EPHYS_PANEL_VERSION)
+	// needs to be called after setting the wave version in order to avoid infinite recursion
+	DAP_RecordDA_EphysGuiState(panelTitle, GUISTATE=wv)
+	return wv
+End
 
- 	return wv
+/// @brief Returns a list of unique and type specific controls
+///
+Function/S GetUniqueSpecCtrlTypeList(panelTitle)
+	string panelTitle
+
+	return GetSpecificCtrlTypes(panelTitle, GetUniqueCtrlList(panelTitle))
+End
+
+/// @brief Parses a list of controls in the panelTitle and returns a list of unique controls
+///
+Function/S GetUniqueCtrlList(panelTitle)
+	string panelTitle
+
+	string list = controlNameList(panelTitle)
+	string relatedSetVar   = "Gain_*;Scale_*;Unit_*;Min_*;Max_*;Search_DA_*;Search_TTL_*"
+	string relatedCheckBox = "Check_AD_*;Check_DA_*;Check_TTL_*;Check_AsyncAlarm_*;Check_AsyncAD_*;Check_DataAcqHS_*;Radio_ClampMode_*;"
+	string relatedPopUp    = "IndexEnd_*;Wave_*;"
+	string relatedValDisp  = "ValDisp_DataAcq_P_*;"
+	string ctrlToRemove    = relatedSetVar + relatedCheckBox + relatedPopUp + relatedValDisp
+	string prunedList
+	variable i,j
+
+	for(i=0;i<itemsinlist(ctrlToRemove);i+=1)
+		prunedList = ListMatch(list, stringfromlist(i, ctrlToRemove))
+		list = removefromlist(prunedList, list)
+	endfor
+
+	return list
+End
+
+/// @brief Parses a list of controls and returns numeric checkBox, valDisplay, setVariable, popUpMenu, and slider controls
+///
+Function/S GetSpecificCtrlTypes(panelTitle,list)
+	string panelTitle
+	string list
+
+	string subtypeCtrlList = ""
+	variable i, type
+	string controlName
+
+	for(i=0;i<itemsinlist(list);i+=1)
+		controlName = stringfromlist(i, list)
+		controlInfo/W=$panelTitle $controlName
+		type = abs(V_flag)
+		switch(type)
+			case CONTROL_TYPE_CHECKBOX:
+			case CONTROL_TYPE_POPUPMENU:
+			case CONTROL_TYPE_SLIDER: // fallthrough by design
+				subtypeCtrlList = AddListItem(controlName, subtypeCtrlList)
+				break
+			case CONTROL_TYPE_VALDISPLAY:
+			case CONTROL_TYPE_SETVARIABLE:  // fallthrough by design
+				if(!DoesControlHaveInternalString(panelTitle, controlName))
+					subtypeCtrlList = AddListItem(controlName, subtypeCtrlList)
+				endif
+				break
+			default:
+				// do nothing
+				break
+		endswitch
+	endfor
+
+	return subtypeCtrlList
 End
 
 /// @brief Return the datafolder reference to the NeuroDataWithoutBorders folder,

Packages/MIES/MIES_Constants.ipf
@@ -60,6 +60,8 @@ StrConstant LAST_SWEEP_USER_DATA = "lastSweep"
 Constant MINIMUM_WAVE_SIZE = 64
 Constant MAXIMUM_WAVE_SIZE = 16384 // 2^14
 
+/// @name Wave dimension constants
+/// @anchor WaveDimensions
 /// Convenience definition to nicify expressions like DimSize(wv, ROWS)
 /// easier to read than DimSize(wv, 0).
 /// @{
@@ -371,6 +373,9 @@ Constant INDEP_HEADSTAGE = 8
 
 StrConstant UNKNOWN_MIES_VERSION = "unknown version"
 
+/// Number of common control groups in the DA_EPHYS panel
+Constant COMMON_CONTROL_GROUP_COUNT = 17
+
 /// Equals 2^5 from `GetKeyState`
 Constant ESCAPE_KEY = 32
 
@@ -399,3 +404,6 @@ Constant HARDWARE_ITC_BITS_PER_VOLT = 3200
 Constant HARDWARE_DAC_DEFAULT_TRIGGER  = 0x0
 Constant HARDWARE_DAC_EXTERNAL_TRIGGER = 0x1
 /// @}
+
+/// Used to upgrade the GuiStateWave as well as the DA Ephys panel
+Constant DA_EPHYS_PANEL_VERSION = 4

Packages/MIES/MIES_GuiUtilities.ipf
@@ -1040,3 +1040,177 @@ Function EqualizeCheckBoxes(win, checkBoxIn, checkBoxPartner, checkBoxInState)
 	SetCheckBoxState(win, checkBoxIn, checkBoxInState)
 	SetCheckBoxState(win, checkBoxPartner, checkBoxInState)
 End
+
+///@brief Return the control type as string
+///
+/// @param win     window name
+/// @param control name of control
+/// @return type of control as string or empty string
+Function/S GetControlTypeAsString(win, control)
+	string win
+	string control
+
+	controlInfo/W=$win $control
+	ASSERT(V_flag != 0, "Non-existing control or window")
+	variable controlType = abs(V_flag)
+	variable checkBoxMode
+	switch(controlType)
+		case 1:
+			return "Button"
+			break
+		case 2:
+			checkBoxMode = GetCheckBoxMode(win, control)
+			if(!checkBoxMode)
+				return "Check"
+			elseif(checkBoxMode == 1)
+				return "Radio"
+			elseif(checkBoxMode == 2)
+				return "Triangle"
+			else
+				ASSERT(0, "Impossible case")
+			endif
+			break
+		case 3:
+			return "PopUp"
+			break
+		case 4:
+			return "ValDisp"
+			break
+		case 5:
+			return "SetVar"
+			break
+		case 6:
+			return "Chart"
+			break
+		case 7:
+			return "Slider"
+			break
+		case 8:
+			return "TabCtrl"
+			break
+		case 9:
+			return "Group"
+			break
+		case 10:
+			return "Title"
+			break
+		case 11:
+			return "List"
+			break
+		case 12:
+			return "Custom"
+			break
+		default:
+			ASSERT(0, "Impossible case")
+			break
+	endswitch
+End
+
+/// @brief Determines if control stores numeric or text data
+Function DoesControlHaveInternalString(win, control)
+	string win, control
+
+	variable internalString
+	ControlInfo/W=$win $control
+	ASSERT(V_flag != 0, "invalid or non existing control")
+	return strsearch(S_recreation, "_STR:", 0) != -1
+End
+
+/// @brief Returns checkbox mode
+Function GetCheckBoxMode(win, checkBoxName)
+	string win, checkBoxName
+
+	variable first, mode
+	string modeString
+	ControlInfo/W=$win $checkBoxName
+	ASSERT(V_flag == 2, "not a checkBox control")
+	first = strsearch(S_recreation, "mode=", 0,2)
+	if(first == -1)
+		return 0
+	else
+		sscanf S_recreation[first, first + 5], "mode=%d", mode
+	endif
+	ASSERT(IsFinite(mode), "Unexpected checkbox mode")
+	return mode
+End
+
+#if (IgorVersion() >= 7.0)
+
+///@brief Returns formatted control name
+	Function/S GetFormattedControlName(win, control)
+		string win, control
+
+		string savedCtrlName = control
+		string newPrefix = GetControlTypeAsString(Win, control)
+		control = trimstring(control)
+		variable stringLocation = strsearch(control,newPrefix,0,2)
+		if(stringLocation == 0)
+			control = replacestring(newPrefix, control, newPrefix) // returns case correct formatting
+		elseif(stringLocation > 0)
+			control = replacestring(newPrefix, control, "") // removes incorrectly placed ctrl type string
+			control = newPrefix + control
+			control = replacestring("__", control, "_") // remove double underscores
+		elseif(stringLocation == -1)
+			control = newPrefix + "_" + control // adds ctrl type string prefix to ctrl name with missing ctrl type string
+		endif
+
+		if(DoesControlHaveInternalString(win, savedCtrlName)) // adds txt suffix string to string setting ctrl
+			control = control + "_txt"
+		endif
+		return control
+	End
+
+
+///@ brief Returns a wave of formatted control names
+Function/WAVE GetFormattedCtrlNames(win)
+	string win
+
+	string listOfControlNames = sortList(controlNameList(win),";",8)
+	variable ctrlCount = itemsInList(listOfControlNames)
+	variable i
+	string ctrl, ctrlFormatted
+
+	make/T/O/N=(ctrlCount,3) controlNames
+	setDimLabel COLS, 0, unformatted, controlNames
+	setDimLabel COLS, 1, formatted,   controlNames
+	setDimLabel COLS, 2, maxLdiff,    controlNames
+
+	for(i = 0; i < ctrlCount; i +=1)
+		ctrl = stringFromList(i, listOfControlNames)
+		ctrlFormatted = GetFormattedControlName(win, ctrl)
+		controlNames[i][0] = ctrl
+		controlNames[i][1] = ctrlFormatted
+		controlNames[i][2] = num2str(31 - strlen(ctrlFormatted))
+	endfor
+	return controlNames
+End
+
+#endif
+
+/// @brief Sets the dimensionlabes of a wave
+///
+/// @param wv       Wave to add dimLables
+/// @param list     List of dimension labels, semicolon separated.
+/// @param dim      Wave dimension, see, @ref WaveDimensions
+/// @param startPos [optional, defaults to 0] First dimLabel index
+Function SetWaveDimLabel(wv, list, dim, [startPos])
+	WAVE wv
+	string list
+	variable dim
+	variable startPos
+
+	string labelName
+	variable i
+	variable dimlabelCount = itemsinlist(list)
+
+	if(paramIsDefault(startPos))
+		startPos = 0
+	endif
+
+	ASSERT(startPos >= 0, "Illegal negative startPos")
+	ASSERT(dimlabelCount <= dimsize(wv, dim) + startPos, "Dimension label count exceeds dimension size")
+	for(i = 0; i < dimlabelCount;i += 1)
+		labelName = stringfromlist(i, list)
+		setDimLabel dim, i + startPos, $labelName, Wv
+	endfor
+End

Packages/MIES/MIES_PanelITC.ipf
@@ -13,8 +13,6 @@ static StrConstant LEADER                 = "Leader"
 static StrConstant COMMENT_PANEL          = "UserComments"
 static StrConstant COMMENT_PANEL_NOTEBOOK = "NB"
 
-static Constant DA_EPHYS_PANEL_VERSION    = 3
-
 static StrConstant AMPLIFIER_DEF_FORMAT   = "AmpNo %d Chan %d"
 
 Window DA_Ephys() : Panel
@@ -6418,9 +6416,13 @@ Function DAP_SetVarProc_TPAmp(sva) : SetVariableControl
 End
 
 /// @brief Records the state of the DA_ephys panel into the GUI state wave
-Function DAP_RecordDA_EphysGuiState(panelTitle)
+Function DAP_RecordDA_EphysGuiState(panelTitle, [GUIState])
 	string panelTitle
-	Wave GUIState = GetDA_EphysGuiStateNum(panelTitle)
+	WAVE GUIState
+
+	if(ParamIsDefault(GuiState))
+		Wave GUIState = GetDA_EphysGuiStateNum(panelTitle)
+	endif
 
 	GUIState[0, NUM_HEADSTAGES - 1][%HSState] = DC_ControlStatusWave(panelTitle, CHANNEL_TYPE_HEADSTAGE)[p]
 	GUIState[0, NUM_HEADSTAGES - 1][%HSMode] = DAP_GetAllHSMode(panelTitle)[p]
@@ -6444,6 +6446,25 @@ Function DAP_RecordDA_EphysGuiState(panelTitle)
 	GUIState[0, NUM_ASYNC_CHANNELS - 1][%AlarmState] = DC_ControlStatusWave(panelTitle, CHANNEL_TYPE_ALARM)[p]
 	GUIState[0, NUM_ASYNC_CHANNELS - 1][%AlarmMin] = GetAllDAEphysSetVar(panelTitle, CHANNEL_TYPE_ASYNC, CHANNEL_CONTROL_ALARM_MIN)[p]
 	GUIState[0, NUM_ASYNC_CHANNELS - 1][%AlarmMax] = GetAllDAEphysSetVar(panelTitle, CHANNEL_TYPE_ASYNC, CHANNEL_CONTROL_ALARM_MAX)[p]
+
+	DAP_GetDA_Ephys_UniqueCtrlState(panelTitle, GUIState)
+End
+
+/// @brief Records the state of unique controls in the DA_ephys panel into the GUI state wave
+Static Function DAP_GetDA_Ephys_UniqueCtrlState(panelTitle, GuiState)
+	string panelTitle
+	WAVE GUIState
+
+	string ctrlName
+	variable col
+	variable lastCol = dimSize(GuiState, COLS)
+	for(col = COMMON_CONTROL_GROUP_COUNT; col < lastCol; col+=1)
+		ctrlName = getDimLabel(GUIState, COLS, col)
+		controlInfo/w=$panelTitle $ctrlName
+		ASSERT(V_flag != 0, "invalid or non existing control")
+		GUIState[0][col] = V_Value
+	endfor
+
 End
 
 /// @brief Return the mode of all DA_Ephys panel headstages

Packages/Testing-MIES/MIES_MiscTesting.ipf
@@ -1,0 +1,37 @@
+#pragma TextEncoding = "UTF-8"		// For details execute DisplayHelpTopic "The TextEncoding Pragma"
+#pragma rtGlobals=3		// Use modern global access method and strict wave access.
+
+Function GetDimLabelVsCtrlInfo(win)
+	string win
+	string CtrlList = GetUniqueCtrlList(controlNameList(win))
+	variable ctrlCount = itemsInList(CtrlList)
+	print "ctrl count:", ctrlCount
+	variable i, dimIndex, value
+	WAVE GuiState = GetDA_EphysGuiStateNum(win)
+	Variable timerRefNum
+	Variable microSeconds
+	Variable n
+	timerRefNum = startMSTimer
+	if (timerRefNum == -1)
+		Abort "All timers are in use"
+	endif
+
+	for(i=0; i<ctrlCount; i+=1)
+	value = GuiState[0][%$stringFromList(i, ctrlList)]
+	endfor
+
+	microSeconds = stopMSTimer(timerRefNum)
+	Print microSeconds/1000/ctrlCount, "milliseconds for dimLabelSearch"
+
+	timerRefNum = startMSTimer
+	if (timerRefNum == -1)
+		Abort "All timers are in use"
+	endif
+
+	for(i=0; i<=ctrlCount; i+=1)
+		controlInfo/W=$win $stringFromList(i, ctrlList)
+	endfor
+
+	microSeconds = stopMSTimer(timerRefNum)
+	Print microSeconds/1000/ctrlCount, "milliseconds for controlInfo"
+End

commit a7306731e2fdc7df1fa69baf549f0524e0550444

Fix single module compilation
Bug introduced in 934e823e (Extend GUIStateWave to hold all unique
controls, 2016-03-13).

Original-CommitID: 93a5aa76de404ecae277ebf5eb2281d6ec604a14

Packages/MIES/MIES_WaveDataFolderGetters.ipf
@@ -3671,8 +3671,25 @@ Function/Wave GetDA_EphysGuiStateNum(panelTitle)
 	SetWaveDimLabel(wv, uniqueCtrlList, COLS, startPos = COMMON_CONTROL_GROUP_COUNT)
 	SetWaveVersion(wv, DA_EPHYS_PANEL_VERSION)
 	// needs to be called after setting the wave version in order to avoid infinite recursion
-	DAP_RecordDA_EphysGuiState(panelTitle, GUISTATE=wv)
+	RecordDA_EphysGuiStateWrapper(panelTitle, wv)
 	return wv
+End
+
+/// @brief Calls `DAP_RecordDA_EphysGuiState` if it can be found,
+/// otherwise calls `RecordDA_EphysGuiStateProto` which aborts.
+static Function RecordDA_EphysGuiStateWrapper(str, wv)
+	string str
+	WAVE wv
+
+	FUNCREF RecordDA_EphysGuiStateProto f = $"DAP_RecordDA_EphysGuiState"
+	f(str, GUISTATE=wv)
+End
+
+Function RecordDA_EphysGuiStateProto(str, [GUISTATE])
+	string str
+	WAVE GUISTATE
+
+	Abort "Prototype function can not be called"
 End
 
 /// @brief Returns a list of unique and type specific controls