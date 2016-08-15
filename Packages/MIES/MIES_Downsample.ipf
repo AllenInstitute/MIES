AllenInstitute/MIES

commit 64ecb1ffcbcf7d6092c4fb0be61c77242c36ae60

GetFollowerList: Remove doNotCreateSVAR hack
Since 7497cf00 (Introduce wrapper for ListOfFollowerITC1600s, 2014-10-07)
the ListOfFollowerITC1600s global string was special as it was not created
on demand.

Remove that hack and create it on demand just like all other global
variables/strings.

Add also a convenience wrapper GetListofLeaderAndPossFollower.

Original-CommitID: 5d9e37edcbed761f81cd4a9f428f16ecdedd5e4c

Packages/MIES/MIES_Downsample.ipf
@@ -134,22 +134,24 @@ Function/S GetPopupMenuDeviceListWithData()
 
 			str = deviceString
 
-			SVAR/Z listOfFollowerDevices = $GetFollowerList(doNotCreateSVAR=1)
-			if(SVAR_Exists(listOfFollowerDevices))
-				allFollowerDevices = AddListItem(listOfFollowerDevices, allFollowerDevices, ";", inf)
+			if(!DeviceHasFollower(str))
+				continue
+			endif
 
-				numFollower = ItemsInList(listOfFollowerDevices)
+			SVAR listOfFollowerDevices = $GetFollowerList(str)
+			allFollowerDevices = AddListItem(listOfFollowerDevices, allFollowerDevices, ";", inf)
 
-				followerDeviceIDList = ""
-				for(k=0; k<numFollower ;k+=1)
-					follower = StringFromList(k, listOfFollowerDevices)
-					ParseDeviceString(follower, followerDeviceType, followerDeviceNumber)
-					followerDeviceIDList = AddListItem(followerDeviceNumber, followerDeviceIDList, ",", inf)
-				endfor
+			numFollower = ItemsInList(listOfFollowerDevices)
 
-				if(numFollower > 0)
-					str += " (" + RemoveEnding(followerDeviceIDList, ",") + ")"
-				endif
+			followerDeviceIDList = ""
+			for(k=0; k<numFollower ;k+=1)
+				follower = StringFromList(k, listOfFollowerDevices)
+				ParseDeviceString(follower, followerDeviceType, followerDeviceNumber)
+				followerDeviceIDList = AddListItem(followerDeviceNumber, followerDeviceIDList, ",", inf)
+			endfor
+
+			if(numFollower > 0)
+				str += " (" + RemoveEnding(followerDeviceIDList, ",") + ")"
 			endif
 
 			if(WhichListItem(deviceString, allFollowerDevices) == -1)

Packages/MIES/MIES_BackgroundMD.ipf
@@ -28,7 +28,7 @@ Function ITC_StartDAQMultiDeviceLowLevel(panelTitle)
 		return NaN
 	endif
 
-	SVAR listOfFollowerDevices = $GetFollowerList(doNotCreateSVAR=1)
+	SVAR listOfFollowerDevices = $GetFollowerList(panelTitle)
 	numFollower = ItemsInList(listOfFollowerDevices)
 
 	// configure follower devices
@@ -329,15 +329,6 @@ Function ITC_CallFuncForDevicesMDYoked(panelTitle, func)
 	string panelTitle
 	FUNCREF CALL_FUNCTION_LIST_PROTOTYPE func
 
-	if(!DeviceHasFollower(panelTitle))
-		func(panelTitle)
-		return NaN
-	endif
-
-	func(panelTitle)
-
-	SVAR/Z listOfFollowerDevices = $GetFollowerList(doNotCreateSVAR=1)
-	if(SVAR_Exists(listOfFollowerDevices) && ItemsInList(listOfFollowerDevices) > 0)
-		CallFunctionForEachListItem(func, listOfFollowerDevices)
-	endif
+	string list = GetListofLeaderAndPossFollower(panelTitle)
+	CallFunctionForEachListItem(func, list)
 End

Packages/MIES/MIES_ExperimentDocumentation.ipf
@@ -382,10 +382,8 @@ Function ED_createWaveNoteTags(panelTitle, sweepCount)
 	values = ""
 
 	if(DeviceCanLead(panelTitle))
-		SVAR/Z listOfFollowerDevices = $GetFollowerList(doNotCreateSVAR=1)
-		if(SVAR_Exists(listOfFollowerDevices))
-			values[0][0][INDEP_HEADSTAGE] = listOfFollowerDevices
-		endif
+		SVAR listOfFollowerDevices = $GetFollowerList(panelTitle)
+		values[0][0][INDEP_HEADSTAGE] = listOfFollowerDevices
 	endif
 
 	SVAR miesVersion = $GetMiesVersion()

Packages/MIES/MIES_GlobalStringAndVariableAccess.ipf
@@ -144,26 +144,13 @@ Function/S GetDataAcqState(panelTitle)
 	return GetNVARAsString(GetDevicePath(panelTitle), "DataAcqState", initialValue=0)
 End
 
-/// @brief Returns the list of follower devices of a ITC1600 Device 0, aka the Lead Device
+/// @brief Return the list of follower devices of a lead device
 ///
-/// For backward compatibility the string is not created if it does not exist
-/// This is also the reason why callers have to call it as
-/// @code
-/// GetFollowerList(doNotCreateSVAR=1)
-/// @endcode
-/// so that they remember that.
-/// @todo remove the doNotCreateSVAR-hack
-Function/S GetFollowerList([doNotCreateSVAR])
-	variable doNotCreateSVAR
+/// @sa GetListofLeaderAndPossFollower()
+Function/S GetFollowerList(leadPanel)
+	string leadPanel
 
-	ASSERT(!ParamIsDefault(doNotCreateSVAR) && doNotCreateSVAR == 1, "Wrong parameter, read the function documentation")
-	string path = GetDevicePathAsString(ITC1600_FIRST_DEVICE)
-
-	if(!DataFolderExists(path))
-		return ""
-	endif
-
-	return path + ":ListOfFollowerITC1600s"
+	return GetSVARAsString(GetDevicePath(leadPanel), "ListOfFollowerITC1600s", initialValue="")
 End
 
 /// @brief Returns the absolute path to the ITC device ID

Packages/MIES/MIES_HardwareSetUp.ipf
@@ -231,10 +231,8 @@ Function HSU_UnlockDevice(panelTitle)
 		HSU_ClearWaveIfExists(ActiveDevTimeParam)
 		HSU_ClearWaveIfExists(TimerFunctionListWave)
 
-		SVAR/Z listOfFollowers = $GetFollowerList(doNotCreateSVAR=1)
-		if(SVAR_Exists(listOfFollowers))
-			listOfFollowers = ""
-		endif
+		SVAR listOfFollowers = $GetFollowerList(ITC1600_FIRST_DEVICE)
+		listOfFollowers = ""
 
 		KillOrMoveToTrash(wv = GetDeviceMapping())
 	endif
@@ -350,28 +348,11 @@ Function HSU_UpdateChanAmpAssignPanel(panelTitle)
 	endif
 End
 
-/// Create, if it does not exist, the global variable ListOfFollowerITC1600s storing the ITC follower list
-/// @todo merge with GetFollowerList once the doNotCreateSVAR-hack is removed
-static Function/S HSU_CreateITCFollowerList(panelTitle)
-	string panelTitle
-
-	// ensure that the device folder exists
-	dfref dfr = GetDevicePath(panelTitle)
-	SVAR/Z/SDFR=dfr list = ListOfFollowerITC1600s
-	if(!SVAR_Exists(list))
-		string/G dfr:ListOfFollowerITC1600s = ""
-	endif
-
-	// now we can return the absolute path to the SVAR
-	// as we know it exists
-	return GetFollowerList(doNotCreateSVAR=1)
-End
-
 /// This function sets a ITC1600 device as a follower, ie. The internal clock is used to synchronize 2 or more PCI-1600
 Function HSU_SetITCDACasFollower(leadDAC, followerDAC)
 	string leadDAC, followerDAC
 
-	SVAR listOfFollowerDevices = $HSU_CreateITCFollowerList(leadDAC)
+	SVAR listOfFollowerDevices = $GetFollowerList(leadDAC)
 	NVAR followerITCDeviceIDGlobal = $GetITCDeviceIDGlobal(followerDAC)
 	
 	if(WhichListItem(followerDAC, listOfFollowerDevices) == -1)

Packages/MIES/MIES_Indexing.ipf
@@ -371,14 +371,7 @@ Function IDX_LongestITI(panelTitle, numActiveDAChannels)
 	variable numPanels, i, j, k, iti, maxITI, numDACs, lockedIndexing, numSets
 	string panelList, setName, setList
 
-	panelList = panelTitle
-
-	if(DeviceCanLead(panelTitle))
-		SVAR/Z listOfFollowerDevices = $GetFollowerList(doNotCreateSVAR=1)
-		if(SVAR_Exists(listOfFollowerDevices))
-			panelList = AddListItem(panelList, listOfFollowerDevices, ";", inf)
-		endif
-	endif
+	panelList = GetListofLeaderAndPossFollower(panelTitle)
 
 	maxITI = -INF
 	numPanels = ItemsInList(panelList)

Packages/MIES/MIES_Manipulator.ipf
@@ -225,14 +225,7 @@ Function M_CheckSettings(panelTitle)
 		return 1
 	endif	
 
-	list = panelTitle
-
-	if(DeviceCanLead(panelTitle))
-		SVAR/Z listOfFollowerDevices = $GetFollowerList(doNotCreateSVAR=1)
-		if(SVAR_Exists(listOfFollowerDevices))
-			list = AddListItem(list, listOfFollowerDevices, ";", inf)
-		endif
-	endif
+	list = GetListofLeaderAndPossFollower(panelTitle)
 
 	DEBUGPRINT("Checking the panelTitle list: ", str=list)
 

Packages/MIES/MIES_MiesUtilities.ipf
@@ -2741,9 +2741,9 @@ End
 Function DeviceIsFollower(panelTitle)
 	string panelTitle
 
-	SVAR/Z listOfFollowerDevices = $GetFollowerList(doNotCreateSVAR=1)
+	SVAR listOfFollowerDevices = $GetFollowerList(ITC1600_FIRST_DEVICE)
 
-	return SVAR_Exists(listOfFollowerDevices) && WhichListItem(panelTitle, listOfFollowerDevices) != -1
+	return WhichListItem(panelTitle, listOfFollowerDevices) != -1
 End
 
 /// @brief Check that the device can act as a leader
@@ -2757,7 +2757,22 @@ End
 Function DeviceHasFollower(panelTitle)
 	string panelTitle
 
-	SVAR/Z listOfFollowerDevices = $GetFollowerList(doNotCreateSVAR=1)
+	SVAR listOfFollowerDevices = $GetFollowerList(panelTitle)
 
-	return DeviceCanLead(panelTitle) && SVAR_Exists(listOfFollowerDevices) && ItemsInList(listOfFollowerDevices) > 0
+	return DeviceCanLead(panelTitle) && ItemsInList(listOfFollowerDevices) > 0
+End
+
+/// @brief Convenience wrapper for GetFollowerList()
+///
+/// For iterating over a list of all followers and the leader. Returns just
+/// panelTitle if the device can not lead.
+Function/S GetListofLeaderAndPossFollower(panelTitle)
+	string panelTitle
+
+	if(!DeviceCanLead(panelTitle))
+		return panelTitle
+	endif
+
+	SVAR followerList = $GetFollowerList(panelTitle)
+	return AddListItem(panelTitle, followerList, ";", 0)
 End

commit 460207f6e58177d238f8450a50d96ad28b30b637

Downsample: Fix not finding any device data
Bug introduced in 64ecb1ff (GetFollowerList: Remove doNotCreateSVAR hack,
2016-07-06).

Original-CommitID: b71f7dc18371cd39aef659ab9073009a5a84e5a4

Packages/MIES/MIES_Downsample.ipf
@@ -134,24 +134,22 @@ Function/S GetPopupMenuDeviceListWithData()
 
 			str = deviceString
 
-			if(!DeviceHasFollower(str))
-				continue
-			endif
+			if(DeviceHasFollower(str))
+				SVAR listOfFollowerDevices = $GetFollowerList(str)
+				allFollowerDevices = AddListItem(listOfFollowerDevices, allFollowerDevices, ";", inf)
 
-			SVAR listOfFollowerDevices = $GetFollowerList(str)
-			allFollowerDevices = AddListItem(listOfFollowerDevices, allFollowerDevices, ";", inf)
+				numFollower = ItemsInList(listOfFollowerDevices)
 
-			numFollower = ItemsInList(listOfFollowerDevices)
+				followerDeviceIDList = ""
+				for(k=0; k<numFollower ;k+=1)
+					follower = StringFromList(k, listOfFollowerDevices)
+					ParseDeviceString(follower, followerDeviceType, followerDeviceNumber)
+					followerDeviceIDList = AddListItem(followerDeviceNumber, followerDeviceIDList, ",", inf)
+				endfor
 
-			followerDeviceIDList = ""
-			for(k=0; k<numFollower ;k+=1)
-				follower = StringFromList(k, listOfFollowerDevices)
-				ParseDeviceString(follower, followerDeviceType, followerDeviceNumber)
-				followerDeviceIDList = AddListItem(followerDeviceNumber, followerDeviceIDList, ",", inf)
-			endfor
-
-			if(numFollower > 0)
-				str += " (" + RemoveEnding(followerDeviceIDList, ",") + ")"
+				if(numFollower > 0)
+					str += " (" + RemoveEnding(followerDeviceIDList, ",") + ")"
+				endif
 			endif
 
 			if(WhichListItem(deviceString, allFollowerDevices) == -1)