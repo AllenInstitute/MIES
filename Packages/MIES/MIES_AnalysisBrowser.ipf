AllenInstitute/MIES

commit b16df2917ba985d984fcd41c6348eec4f5a8fb80

Streamline labnotebook naming
The names of the four labnotebook wave are historically grown to be either
settingsHistory, numDocWave, numericValues or numericalValues (that is
only for the data wave of the numerical labnotebook).

This makes the code quite hard to read.

Therefore we now rename the labnotebook waves to:
- numericalValues
- numericalKeys
- textualValues
- textualKeys

For the AnalysisBrowser new wave getter functions were introduced for
handling the upgrade using UpgradeWaveLocationAndGetIt.

The sweep timestamp column wave is renamed to numericalValuesDat

Original-CommitID: fb83404116a77aa5f17ff85fbec5072941f3cd5a

Packages/MIES/MIES_AnalysisBrowser.ipf
@@ -157,9 +157,10 @@ static Function AB_FillListWave(expFolder, expName, device)
 
 	string str, name, listOfSweepConfigWaves
 
-	DFREF expDataDFR = GetAnalysisDeviceConfigFolder(expFolder, device)
-	DFREF dfr = GetAnalysisLabNBFolder(expFolder, device)
-	WAVE/SDFR=dfr numericValues, textValues
+	DFREF expDataDFR   = GetAnalysisDeviceConfigFolder(expFolder, device)
+	WAVE numericValues = GetAnalysLBNumericalValues(expFolder, device)
+	WAVE textValues    = GetAnalysLBTextualValues(expFolder, device)
+
 	WAVE/T list = GetExperimentBrowserGUIList()
 
 	index = GetNumberFromWaveNote(list, NOTE_INDEX)
@@ -377,8 +378,9 @@ static Function/S AB_LoadLabNotebookFromFile(expFilePath)
 	expName   = experimentMap[V_Value][%ExperimentName]
 	expFolder = experimentMap[V_Value][%ExperimentFolder]
 
-	labNotebookWaves = "settingsHistory;keyWave;txtDocWave;txtDocKeyWave"
-	labNotebookPath  = GetLabNotebookFolderAsString()
+	labNotebookWaves  = "settingsHistory;keyWave;txtDocWave;txtDocKeyWave;"
+	labNotebookWaves += "numericalKeys;textualKeys;numericalValues;textualValues"
+	labNotebookPath = GetLabNotebookFolderAsString()
 	DFREF saveDFR = GetDataFolderDFR()
 	DFREF newDFR = UniqueDataFolder(GetAnalysisFolder(), "temp")
 	numWavesLoaded = AB_LoadDataWrapper(newDFR, expFilePath, labNotebookPath, labNotebookWaves)
@@ -410,21 +412,42 @@ static Function/S AB_LoadLabNotebookFromFile(expFilePath)
 				continue
 			endif
 
-			basepath = path + ":KeyWave"
-			Wave/Z/SDFR=$basepath keyWave
+			// first try the new wave names and then as fallback
+			// the old ones
+			// Supports old/new wavename mixes although these should not
+			// exist in the wild.
 
-			basepath = path + ":settingsHistory"
-			Wave/Z/SDFR=$basepath settingsHistory
+			Wave/Z/SDFR=$path numericalKeys
 
-			basepath = path + ":TextDocKeyWave"
-			Wave/Z/SDFR=$basepath txtDocKeyWave
+			if(!WaveExists(numericalKeys))
+				basepath = path + ":KeyWave"
+				Wave/Z/SDFR=$basepath numericalKeys = keyWave
+			endif
 
-			basepath = path + ":textDocumentation"
-			Wave/Z/SDFR=$basepath txtDocWave
+			Wave/Z/SDFR=$path numericalValues
+
+			if(!WaveExists(numericalValues))
+				basepath = path + ":settingsHistory"
+				Wave/Z/SDFR=$basepath numericalValues = settingsHistory
+			endif
+
+			Wave/Z/SDFR=$path textualKeys
+
+			if(!WaveExists(textualKeys))
+				basepath = path + ":TextDocKeyWave"
+				Wave/Z/SDFR=$basepath textualKeys = txtDocKeyWave
+			endif
+
+			Wave/Z/SDFR=$path textualValues
+
+			if(!WaveExists(textualValues))
+				basepath = path + ":textDocumentation"
+				Wave/Z/SDFR=$basepath textualValues = txtDocWave
+			endif
 
 			device = BuildDeviceString(type, number)
 
-			if(!WaveExists(keyWave) || !WaveExists(settingsHistory) || !WaveExists(txtDocKeyWave) || !WaveExists(txtDocWave))
+			if(!WaveExists(numericalKeys) || !WaveExists(numericalValues) || !WaveExists(textualKeys) || !WaveExists(textualValues))
 				printf "Could not find all four labnotebook waves, dropping all data from device %s\r", device
 				continue
 			endif
@@ -435,24 +458,24 @@ static Function/S AB_LoadLabNotebookFromFile(expFilePath)
 
 			DFREF dfr = GetAnalysisLabNBFolder(expFolder, device)
 
-			Duplicate/O keyWave, dfr:numericKeys/Wave=numericKeys
-			Duplicate/O settingsHistory, dfr:numericValues/Wave=numericValues
-			Duplicate/O txtDocKeyWave, dfr:textKeys/Wave=textKeys
-			Duplicate/O txtDocWave, dfr:textValues/Wave=textValues
+			Duplicate/O numericalKeys, dfr:numericalKeys/Wave=numericalKeys
+			Duplicate/O numericalValues, dfr:numericalValues/Wave=numericalValues
+			Duplicate/O textualKeys, dfr:textualKeys/Wave=textualKeys
+			Duplicate/O textualValues, dfr:textualValues/Wave=textualValues
 
 			// add some forgotten dimension labels in older versions of MIES
 			// and overwrite invalid dim labels (labnotebook waves created with versions prior to a8f0f43)
-			str = GetDimLabel(textValues, COLS, 0)
+			str = GetDimLabel(textualValues, COLS, 0)
 			if(isEmpty(str) || !cmpstr(str, "dimLabelText"))
-				SetDimensionLabels(textKeys, textValues)
+				SetDimensionLabels(textualKeys, textualValues)
 			endif
 
-			str = GetDimLabel(numericKeys, COLS, 0)
+			str = GetDimLabel(numericalKeys, COLS, 0)
 			if(isEmpty(str) || !cmpstr(str, "dimLabelText"))
-				SetDimensionLabels(numericKeys, numericValues)
+				SetDimensionLabels(numericalKeys, textualValues)
 			endif
 
-			highestSweepNumber = AB_GetHighestPossibleSweepNum(numericValues)
+			highestSweepNumber = AB_GetHighestPossibleSweepNum(numericalValues)
 
 			AB_LoadSweepConfigData(expFilePath, expFolder, device, highestSweepNumber)
 			AB_LoadTPStorageFromFile(expFilePath, expFolder, device)

Packages/MIES/MIES_AnalysisBrowser_LabNotebookTPStorageBrowser.ipf
@@ -266,11 +266,10 @@ Function LBN_PopMenuProc_LBNViewableCols(pa) : PopupMenuControl
 				break
 			endif
 
-			DFREF dfr = $folder
-			WAVE/T/SDFR=dfr numericKeys
-			WAVE/T/SDFR=dfr numericValues
+			WAVE/T numericalKeys = GetAnalysLBNumericalKeys(expFolder, device)
+			WAVE numericalValues = GetAnalysLBNumericalValues(expFolder, device)
 
-			AddTraceToLBGraph(graph, numericKeys, numericValues, popStr)
+			AddTraceToLBGraph(graph, numericalKeys, numericalValues, popStr)
 			break
 	endswitch
 
@@ -322,10 +321,9 @@ Function LBN_ButtonProc_SwitchXaxisType(ba) : ButtonControl
 			expFolder = LBN_GetExpFolderFromPopup(graph)
 			device = GetPopupMenuString(panel, "popup_select_device")
 
-			DFREF dfr = GetAnalysisLabNBFolder(expFolder, device)
-			WAVE/T/SDFR=dfr numericValues
+			WAVE numericalValues = GetAnalysLBNumericalValues(expFolder, device)
 
-			SwitchLBGraphXAxis(graph, numericValues)
+			SwitchLBGraphXAxis(graph, numericalValues)
 			break
 	endswitch
 
@@ -347,15 +345,8 @@ Function LBN_PopMenuProc_ExpDevSelector(pa) : PopupMenuControl
 			expFolder = LBN_GetExpFolderFromPopup(graph)
 			device = GetPopupMenuString(panel, "popup_select_device")
 
-			DFREF dfr = GetAnalysisLabNBFolder(expFolder, device)
-
-			WAVE/Z/T/SDFR=dfr numericKeys
-			WAVE/Z/T/SDFR=dfr numericValues
-
-			if(!WaveExists(numericKeys) || !WaveExists(numericValues))
-				print "BUG: missing labnotebook, handle that earlier"
-				break
-			endif
+			WAVE/T numericalKeys = GetAnalysLBNumericalKeys(expFolder, device)
+			WAVE numericalValues = GetAnalysLBNumericalValues(expFolder, device)
 
 			traceList = TraceNameList(graph, ";", 0 + 1)
 			numTraces = ItemsInList(traceList)
@@ -376,7 +367,7 @@ Function LBN_PopMenuProc_ExpDevSelector(pa) : PopupMenuControl
 			numKeys = ItemsInList(keyList)
 			for(i = 0; i < numKeys; i += 1)
 				key = StringFromList(i, keyList)
-				AddTraceToLBGraph(graph, numericKeys, numericValues, key)
+				AddTraceToLBGraph(graph, numericalKeys, numericalValues, key)
 			endfor
 			break
 	endswitch
@@ -498,10 +489,9 @@ Function/S LBN_GetLabNotebookViewAbleCols(graph)
 		return NONE
 	endif
 
-	DFREF dfr = GetAnalysisLabNBFolder(expFolder, device)
-	WAVE/T/Z/SDFR=dfr numericKeys
+	WAVE/T numericalKeys = GetAnalysLBNumericalKeys(expFolder, device)
 
-	return GetLabNotebookSortedKeys(numericKeys)
+	return GetLabNotebookSortedKeys(numericalKeys)
 End
 
 Function/S LBN_TPStorageViewAbleCols(graph)

Packages/MIES/MIES_AnalysisBrowser_SweepBrowser.ipf
@@ -208,10 +208,9 @@ static Function/WAVE SB_GetSweepPropertyFromNumLBN(graph, mapIndex, key)
 	sweep     = str2num(sweepMap[mapIndex][%Sweep])
 	expFolder = sweepMap[mapIndex][%ExperimentFolder]
 
-	DFREF dfr = GetAnalysisLabNBFolder(expFolder, device)
-	WAVE/SDFR=dfr numericValues
+	WAVE numericalValues = GetAnalysLBNumericalValues(expFolder, device)
 
-	return GetLastSetting(numericValues, sweep, key)
+	return GetLastSetting(numericalValues, sweep, key)
 End
 
 /// @brief Duplicate the sweep browser graph to a user given folder and name
@@ -533,9 +532,8 @@ Function SB_PlotSweep(sweepBrowserDFR, currentMapIndex, newMapIndex)
 
 	WAVE configWave = GetAnalysisConfigWave(expFolder, device, sweep)
 
-	DFREF dfr = GetAnalysisLabNBFolder(expFolder, device)
-	WAVE/SDFR=dfr numericValues
-	WAVE/T/SDFR=dfr textValues
+	WAVE numericalValues = GetAnalysLBNumericalValues(expFolder, device)
+	WAVE textualValues   = GetAnalysLBTextualValues(expFolder, device)
 
 	STRUCT TiledGraphSettings tgs
 	tgs.displayDAC      = GetCheckBoxState(panel, "check_SweepBrowser_DisplayDAC")
@@ -547,7 +545,7 @@ Function SB_PlotSweep(sweepBrowserDFR, currentMapIndex, newMapIndex)
 	tgs.dDAQDisplayMode = GetCheckBoxState(panel, "check_sweepbrowser_dDAQ")
 	WAVE channelSelWave = SB_GetChannelSelWave(graph)
 
-	CreateTiledChannelGraph(graph, configWave, sweep, numericValues, textValues, tgs, sweepDFR=newSweepDFR, channelSelWave=channelSelWave)
+	CreateTiledChannelGraph(graph, configWave, sweep, numericalValues, textualValues, tgs, sweepDFR=newSweepDFR, channelSelWave=channelSelWave)
 
 	SetPopupMenuIndex(panel, "popup_sweep_selector", newMapIndex)
 	SB_SetFormerSweepNumber(panel, newMapIndex)

Packages/MIES/MIES_AnalysisFunctionHelpers.ipf
@@ -9,8 +9,8 @@
 /// ------------------------------|------------------------------------------------------
 /// GetADCListFromConfig()        | Free wave with all active AD channels as entries
 /// GetDACListFromConfig()        | Free wave with all active DA channels as entries
-/// GetNumDocWave()               | Wave reference to the labnotebook (numerical version)
-/// GetTextDocWave()              | Wave reference to the labnotebook (textual version)
+/// GetLBNumericalValues()        | Wave reference to the labnotebook (numerical version)
+/// GetLBTextualValues()          | Wave reference to the labnotebook (textual version)
 /// GetLastSetting()              | Last documented numerical value for headstages of a specific setting in the labnotebook for a given sweep number.
 /// GetLastSettingText()          | Last documented textual value for headstages of a specific setting in the labnotebook for a given sweep number.
 /// GetLastSweepWithSetting()     | Last documented numerical value for headstages of a specific setting in the labnotebook and the sweep number it was set last.

Packages/MIES/MIES_DataBrowser.ipf
@@ -273,25 +273,25 @@ End
 static Function/WAVE DB_GetSettingsHistory(panelTitle)
 	string panelTitle
 
-	return GetNumDocWave(GetPopupMenuString(panelTitle, "popup_DB_lockedDevices"))
+	return GetLBNumericalValues(GetPopupMenuString(panelTitle, "popup_DB_lockedDevices"))
 End
 
 static Function/WAVE DB_GetSettingsHistoryText(panelTitle)
 	string panelTitle
 
-	return GetTextDocWave(GetPopupMenuString(panelTitle, "popup_DB_lockedDevices"))
+	return GetLBTextualValues(GetPopupMenuString(panelTitle, "popup_DB_lockedDevices"))
 End
 
 static Function/WAVE DB_GetSettingsHistoryKeys(panelTitle)
 	string panelTitle
 
-	return GetNumDocKeyWave(GetPopupMenuString(panelTitle, "popup_DB_lockedDevices"))
+	return GetLBNumericalKeys(GetPopupMenuString(panelTitle, "popup_DB_lockedDevices"))
 End
 
 static Function/WAVE DB_GetSettingsHistoryTextKeys(panelTitle)
 	string panelTitle
 
-	return GetTextDocKeyWave(GetPopupMenuString(panelTitle, "popup_DB_lockedDevices"))
+	return GetLBTextualKeys(GetPopupMenuString(panelTitle, "popup_DB_lockedDevices"))
 End
 
 Function DB_UpdateToLastSweep(panel)

Packages/MIES/MIES_ExperimentDocumentation.ipf
@@ -19,8 +19,8 @@ Function ED_createWaveNotes(incomingSettingsWave, incomingKeyWave, sweepNo, pane
 
 	variable rowIndex, numCols, lastValidIncomingLayer, i
 
-	WAVE/T keyWave       = GetNumDocKeyWave(panelTitle)
-	WAVE settingsHistory = GetNumDocWave(panelTitle)
+	WAVE/T keyWave = GetLBNumericalKeys(panelTitle)
+	WAVE settingsHistory = GetLBNumericalValues(panelTitle)
 
 	ASSERT(!cmpstr(keyWave[0][2], "TimeStampSinceIgorEpochUTC"), "Labnotebook update failed")
 	ASSERT(DimSize(incomingSettingsWave, LAYERS) <= DimSize(settingsHistory, LAYERS), "Unexpected large layer count in the incoming settings wave")
@@ -31,7 +31,7 @@ Function ED_createWaveNotes(incomingSettingsWave, incomingKeyWave, sweepNo, pane
 	settingsHistory[rowIndex][1] = DateTime
 	settingsHistory[rowIndex][2] = DateTimeInUTC()
 
-	WAVE settingsHistoryDat = GetSettingsHistoryDateTime(settingsHistory)
+	WAVE settingsHistoryDat = GetLBNumericalValuesDat(settingsHistory)
 	EnsureLargeEnoughWave(settingsHistoryDat, minimumSize=rowIndex, dimension=ROWS, initialValue=NaN)
 	settingsHistoryDat[rowIndex] = settingsHistory[rowIndex][1]
 
@@ -295,8 +295,8 @@ Function ED_createTextNotes(incomingTextDocWave, incomingTextDocKeyWave, sweepNo
 
 	variable rowIndex, numCols, i
 
-	WAVE/T textDocWave = GetTextDocWave(panelTitle)
-	WAVE/T textDocKeyWave = GetTextDocKeyWave(panelTitle)
+	WAVE/T textDocWave = GetLBTextualValues(panelTitle)
+	WAVE/T textDocKeyWave = GetLBTextualKeys(panelTitle)
 
 	ASSERT(!cmpstr(textDocKeyWave[0][2], "TimeStampSinceIgorEpochUTC"), "Labnotebook update failed")
 	ASSERT(DimSize(incomingTextDocWave, ROWS)   == 1, "Mismatched row counts")

Packages/MIES/MIES_HDF5Ops.ipf
@@ -928,6 +928,9 @@ Function HD_LoadDataSet([incomingFileName, cmdID])
 		elseif(StringMatch(dataSet, "*savedLabNotebook/textDocumentation/*"))
 			SetDataFolder GetDevSpecLabNBTextDocFolder(panelName)
 			HDF5LoadData /O /IGOR=-1 fileID, dataSet
+		elseif(StringMatch(dataSet, "*savedLabNotebook/*")) // has to be the last in the list
+			SetDataFolder GetDevSpecLabNBFolder(panelName)
+			HDF5LoadData /O /IGOR=-1 fileID, dataSet
 		endif
 	endfor	
 

Packages/MIES/MIES_HardwareSetUp.ipf
@@ -84,6 +84,12 @@ Function HSU_LockDevice(panelTitle)
 	headstage = GetSliderPositionIndex(panelTitleLocked, "slider_DataAcq_ActiveHeadstage")
 	P_SaveUserSelectedHeadstage(panelTitleLocked, headstage)
 
+	// upgrade all four labnotebook waves in wanna-be atomic way
+	GetLBNumericalKeys(panelTitleLocked)
+	GetLBNumericalValues(panelTitleLocked)
+	GetLBTextualKeys(panelTitleLocked)
+	GetLBTextualValues(panelTitleLocked)
+
 	NVAR sessionStartTime = $GetSessionStartTime()
 	sessionStartTime = DateTimeInUTC()
 

commit c86c25ce75e99686f01f6a6786f05b3b07d341fc

AnalysisBrowser: Don't error out on non-existing datafolders
Introduced in b16df291 (Streamline labnotebook naming, 2016-04-13).

Original-CommitID: 4a7d8a18578d6e550623bc4b24efe3e1444fc73f

Packages/MIES/MIES_AnalysisBrowser.ipf
@@ -442,28 +442,36 @@ static Function/S AB_LoadLabNotebookFromFile(expFilePath)
 
 			if(!WaveExists(numericalKeys))
 				basepath = path + ":KeyWave"
-				Wave/Z/SDFR=$basepath numericalKeys = keyWave
+				if(DataFolderExists(basepath))
+					Wave/Z/SDFR=$basepath numericalKeys = keyWave
+				endif
 			endif
 
 			Wave/Z/SDFR=$path numericalValues
 
 			if(!WaveExists(numericalValues))
 				basepath = path + ":settingsHistory"
-				Wave/Z/SDFR=$basepath numericalValues = settingsHistory
+				if(DataFolderExists(basepath))
+					Wave/Z/SDFR=$basepath numericalValues = settingsHistory
+				endif
 			endif
 
 			Wave/Z/SDFR=$path textualKeys
 
 			if(!WaveExists(textualKeys))
 				basepath = path + ":TextDocKeyWave"
-				Wave/Z/SDFR=$basepath textualKeys = txtDocKeyWave
+				if(DataFolderExists(basepath))
+					Wave/Z/SDFR=$basepath textualKeys = txtDocKeyWave
+				endif
 			endif
 
 			Wave/Z/SDFR=$path textualValues
 
 			if(!WaveExists(textualValues))
 				basepath = path + ":textDocumentation"
-				Wave/Z/SDFR=$basepath textualValues = txtDocWave
+				if(DataFolderExists(basepath))
+					Wave/Z/SDFR=$basepath textualValues = txtDocWave
+				endif
 			endif
 
 			device = BuildDeviceString(type, number)