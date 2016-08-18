3730198e502301a17d1ac0e551b052fc485c44e6
Thomas Braun <thomas.braun@byte-physics.de>
2016-08-18 21:06:15 +0200

    Add XOPSupport from Toolkit 7.0
    Original-CommitID: be223455321a418b6c1e653ff94ea3ce2935dc2b

---
+++ Packages/ZeroMQ/src/XOPSupport/XOPSupportMac.h
@@ -0,0 +1,31 @@
+﻿// This file contains equates and prototypes that are needed on Macintosh only.
+
+
+#ifdef __cplusplus
+extern "C" {						/* This allows C++ to call the XOPSupport routines */
+#endif
+
+// Misc utilities.
+// IsMacOSX has been removed. Use #ifdef MACIGOR.
+#ifdef IGOR64
+	/*	These are defined in TextUtils.h and in the CarbonCore framework but
+		is not available in 64 bits. So this is a version of it for 64 bits.
+	*/
+	void CopyPascalStringToC(ConstStr255Param source, char* dest);
+	void CopyCStringToPascal(const char* source, Str255 dest);
+#endif
+void debugstr(const char* text);
+
+
+/* Resource routines (in XOPSupportMac.c) */
+int XOPRefNum(void);
+#ifdef IGOR64
+	// In 32-bits this is provided by the CarbonCore framework and declared in TextUtils.h
+	void GetIndString(Str255 theString, short strListID, short index);
+#endif
+Handle GetXOPResource(int resType, int resID);
+Handle GetXOPNamedResource(int resType, const char* name);
+
+#ifdef __cplusplus
+}
+#endif
