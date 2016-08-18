AllenInstitute/MIES

commit a116cca776b4aafb06ac17e561186c14639a67ed

CreateTiledChannelGraph: Add support for oodDAQ data
If the dDAQ checkbox is selected in the databrowser/sweepbrowser and
oodDAQ data is available only the smeared regions from the labnotebook are
shown.

Additionally the slider/popupmenu allows to only display regions from
selected headstages.

Original-CommitID: 79e52c6236175bb97501c41e9b831f17877c263c

Packages/MIES/MIES_MiesUtilities.ipf
@@ -637,16 +637,16 @@ Function CreateTiledChannelGraph(graph, config, sweepNo, numericalValues,  textu
 	WAVE/Z sweepWave
 	WAVE/Z channelSelWave
 
-	variable headstage, red, green, blue, splitSweepMode, axisIndex, numChannels
+	variable red, green, blue, splitSweepMode, axisIndex, numChannels
 	variable numDACs, numADCs, numTTLs, i, j, k, channelOffset, hasPhysUnit, slotMult
 	variable moreData, low, high, step, spacePerSlot, chan, numSlots, numHorizWaves, numVertWaves, idx, configIdx
 	variable numTTLBits, colorIndex, totalVertBlocks
-	variable delayOnsetUser, delayOnsetAuto, delayTermination, delaydDAQ, dDAQEnabled
-	variable stimSetLength, samplingInt, xRangeStart, xRangeEnd, left
-	variable numDACsOriginal, numADCsOriginal, numTTLsOriginal
+	variable delayOnsetUser, delayOnsetAuto, delayTermination, delaydDAQ, dDAQEnabled, oodDAQEnabled
+	variable stimSetLength, samplingInt, xRangeStart, xRangeEnd, left, first, last
+	variable numDACsOriginal, numADCsOriginal, numTTLsOriginal, numRegions, numEntries, numRangesPerEntry, totalXRange
 
-	string trace, traceType, channelID, axisLabel, existingLabel
-	string unit, configNote, name, wvName, str, vertAxis
+	string trace, traceType, channelID, axisLabel, existingLabel, entry, range
+	string unit, configNote, name, wvName, str, vertAxis, oodDAQRegionsAll, horizAxis
 
 	ASSERT(!isEmpty(graph), "Empty graph")
 	ASSERT(IsFinite(sweepNo), "Non-finite sweepNo")
@@ -758,13 +758,20 @@ Function CreateTiledChannelGraph(graph, config, sweepNo, numericalValues,  textu
 
 	high = 1.0
 
-	dDAQEnabled = GetLastSettingIndep(numericalValues, sweepNo, "Distributed DAQ")
+	dDAQEnabled   = GetLastSettingIndep(numericalValues, sweepNo, "Distributed DAQ")
+	oodDAQEnabled = GetLastSettingIndep(numericalValues, sweepNo, "Optimized Overlap dDAQ")
 
-	if(tgs.dDAQDisplayMode && !dDAQEnabled)
+	if(tgs.dDAQDisplayMode && !(dDAQEnabled || oodDAQEnabled))
 		printf "Distributed DAQ display mode turned off as no dDAQ data could be found.\r"
+		tgs.dDAQDisplayMode = 0
 	endif
 
-	tgs.dDAQDisplayMode = tgs.dDAQDisplayMode && dDAQEnabled
+	WAVE/Z/T oodDAQRegions = GetLastSettingText(textualValues, sweepNo, "oodDAQ regions")
+
+	if(tgs.dDAQDisplayMode && oodDAQEnabled && !WaveExists(oodDAQRegions))
+		printf "Distributed DAQ display mode turned off as no oodDAQ regions could be found in the labnotebook.\r"
+		tgs.dDAQDisplayMode = 0
+	endif
 
 	if(tgs.dDAQDisplayMode)
 		stimSetLength = GetLastSettingIndep(numericalValues, sweepNo, "Stim set length")
@@ -782,6 +789,33 @@ Function CreateTiledChannelGraph(graph, config, sweepNo, numericalValues,  textu
 
 		sprintf str, "delayOnsetUser=%g, delayOnsetAuto=%g, delayTermination=%g, delaydDAQ=%g", delayOnsetUser, delayOnsetAuto, delayTermination, delaydDAQ
 		DEBUGPRINT(str)
+
+		if(oodDAQEnabled)
+			numEntries = DimSize(oodDAQRegions, ROWS)
+			oodDAQRegionsAll = ""
+
+			for(i = 0; i < numEntries; i += 1)
+				// use only the selected region if requested
+				if(tgs.oodDAQHeadstageRegions >= 0 && tgs.oodDAQHeadstageRegions < NUM_HEADSTAGES && tgs.oodDAQHeadstageRegions != i)
+					continue
+				endif
+
+				entry = RemoveEnding(oodDAQRegions[i], ";")
+				numRangesPerEntry = ItemsInList(entry)
+				for(j = 0; j < numRangesPerEntry; j += 1)
+					range = StringFromList(j, entry)
+					oodDAQRegionsAll = AddListItem(range, oodDAQRegionsAll, ";", Inf)
+
+					xRangeStart = str2num(StringFromList(0, range, "-"))
+					xRangeEnd = str2num(StringFromList(1, range, "-"))
+					totalXRange += (xRangeEnd - XRangeStart) / samplingInt
+				endfor
+			endfor
+
+			numRegions = ItemsInList(oodDAQRegionsAll)
+			sprintf str, "oodDAQRegions (%d) concatenated: _%s_, totalRange=%g", numRegions, oodDAQRegionsAll, totalXRange
+			DEBUGPRINT(str)
+		endif
 	endif
 
 	WAVE/Z statusDAC = GetLastSetting(numericalValues, sweepNo, "DAC")
@@ -811,7 +845,7 @@ Function CreateTiledChannelGraph(graph, config, sweepNo, numericalValues,  textu
 					channelOffset    = 0
 					hasPhysUnit      = 1
 					slotMult         = 1
-					numHorizWaves    = tgs.dDAQDisplayMode ? numDACsOriginal : 1
+					numHorizWaves    = tgs.dDAQDisplayMode ? (oodDAQEnabled ? numRegions : numDACsOriginal) : 1
 					numVertWaves     = 1
 					numChannels      = numDACs
 					break
@@ -826,7 +860,7 @@ Function CreateTiledChannelGraph(graph, config, sweepNo, numericalValues,  textu
 					channelOffset    = numDACs
 					hasPhysUnit      = 1
 					slotMult         = ADC_SLOT_MULTIPLIER
-					numHorizWaves    = tgs.dDAQDisplayMode ? numADCsOriginal : 1
+					numHorizWaves    = tgs.dDAQDisplayMode ? (oodDAQEnabled ? numRegions : numADCsOriginal) : 1
 					numVertWaves     = 1
 					numChannels      = numADCs
 					break
@@ -883,6 +917,9 @@ Function CreateTiledChannelGraph(graph, config, sweepNo, numericalValues,  textu
 					axisIndex += 1
 				endif
 
+				DEBUGPRINT("")
+				first = 0
+
 				// number of horizontally distributed
 				// waves per channel type
 				for(k = 0; k < numHorizWaves; k += 1)
@@ -899,15 +936,28 @@ Function CreateTiledChannelGraph(graph, config, sweepNo, numericalValues,  textu
 
 					if(tgs.dDAQDisplayMode && channelTypes[i] != ITC_XOP_CHANNEL_TYPE_TTL) // TTL channels don't have dDAQ mode
 
-						// fallback to manual calculation
-						// for versions prior to 17b49b63 (DC_PlaceDataInITCDataWave: Document stim set length, 2016-05-12)
-						if(!IsFinite(stimSetLength))
-							stimSetLength = (DimSize(wv, ROWS) - (delayOnsetUser + delayOnsetAuto + delayTermination + delaydDAQ * (numADCs - 1))) /  numADCs
-							DEBUGPRINT("Stim set length (manually calculated)", var=stimSetLength)
-						endif
+						if(dDAQEnabled)
+							// fallback to manual calculation
+							// for versions prior to 17b49b63 (DC_PlaceDataInITCDataWave: Document stim set length, 2016-05-12)
+							if(!IsFinite(stimSetLength))
+								stimSetLength = (DimSize(wv, ROWS) - (delayOnsetUser + delayOnsetAuto + delayTermination + delaydDAQ * (numADCs - 1))) /  numADCs
+								DEBUGPRINT("Stim set length (manually calculated)", var=stimSetLength)
+							endif
 
-						xRangeStart = delayOnsetUser + delayOnsetAuto + k * (stimSetLength + delaydDAQ)
-						xRangeEnd   = xRangeStart + stimSetLength
+							xRangeStart = delayOnsetUser + delayOnsetAuto + k * (stimSetLength + delaydDAQ)
+							xRangeEnd   = xRangeStart + stimSetLength
+						elseif(oodDAQEnabled)
+							/// regions list format: $begin1-$end1;$begin2-$end2;...
+							/// the values are in x values of the ITCDataWave but we need points here ignored the onset delays
+							xRangeStart = str2num(StringFromList(0, StringFromList(k, oodDAQRegionsAll, ";"), "-"))
+							xRangeEnd   = str2num(StringFromList(1, StringFromList(k, oodDAQRegionsAll, ";"), "-"))
+
+							sprintf str, "begin[ms] = %g, end[ms] = %g", xRangeStart, xRangeEnd
+							DEBUGPRINT(str)
+
+							xRangeStart = delayOnsetUser + delayOnsetAuto + xRangeStart / samplingInt
+							xRangeEnd   = delayOnsetUser + delayOnsetAuto + xRangeEnd   / samplingInt
+						endif
 					else
 						xRangeStart = NaN
 						xRangeEnd   = NaN
@@ -921,16 +971,26 @@ Function CreateTiledChannelGraph(graph, config, sweepNo, numericalValues,  textu
 					if(!IsFinite(xRangeStart) && !IsFinite(XRangeEnd))
 						AppendToGraph/W=$graph/L=$vertAxis wv[][idx]/TN=$trace
 					else
-						AppendToGraph/W=$graph/L=$vertAxis wv[xRangeStart, xRangeEnd][idx]/TN=$trace
+						if(dDAQEnabled)
+							AppendToGraph/W=$graph/L=$vertAxis wv[xRangeStart, xRangeEnd][idx]/TN=$trace
 
-						left  = 1/numHorizWaves * k
+							left = 1/numHorizWaves * k
 
-						if(left != 0.0)
-							left += GRAPH_DIV_SPACING
+							if(left != 0.0)
+								left += GRAPH_DIV_SPACING
+							endif
+
+							ModifyGraph/W=$graph freePos($vertAxis)={left,kwFraction}, axisOnTop($vertAxis) = 1
+						elseif(oodDAQEnabled)
+							horizAxis = vertAxis + "_b"
+							AppendToGraph/W=$graph/L=$vertAxis/B=$horizAxis wv[xRangeStart, xRangeEnd][idx]/TN=$trace
+							first = first
+							last  = first + (xRangeEnd - xRangeStart) / totalXRange
+							ModifyGraph/W=$graph axisEnab($horizAxis)={first, last}
+							first += (xRangeEnd - xRangeStart) / totalXRange
 						endif
 
-						ModifyGraph/W=$graph freePos($vertAxis)={left,kwFraction}, axisOnTop($vertAxis) = 1
-						sprintf str, "horiz axis offset=[%g], stimset=[%d, %d]", left, xRangeStart, xRangeEnd
+						sprintf str, "horiz axis offset=[%g], stimset=[%d, %d] aka (%g, %g)", left, xRangeStart, xRangeEnd, pnt2x(wv,xRangeStart), pnt2x(wv,xRangeEnd)
 						DEBUGPRINT(str)
 					endif
 
@@ -967,6 +1027,13 @@ Function CreateTiledChannelGraph(graph, config, sweepNo, numericalValues,  textu
 						Label/W=$graph $vertAxis, "\\u#2"
 					endif
 
+					if(tgs.dDAQDisplayMode && oodDAQEnabled)
+						ModifyGraph/W=$graph axRGB($vertAxis)=(65535,65535,65535), tlblRGB($vertAxis)=(65535,65535,65535)
+						ModifyGraph/W=$graph axThick($vertAxis)=0
+						ModifyGraph/W=$graph axRGB($horizAxis)=(65535,65535,65535), tlblRGB($horizAxis)=(65535,65535,65535)
+						ModifyGraph/W=$graph alblRGB($horizAxis)=(65535,65535,65535), axThick($horizAxis)=0
+					endif
+
 					// Color scheme:
 					// 0-7:   Different headstages
 					// 8:     Unknown headstage
@@ -1001,6 +1068,12 @@ Function CreateTiledChannelGraph(graph, config, sweepNo, numericalValues,  textu
 			activeChanCount[i] += 1
 		endfor
 	while(moreData)
+
+	if(tgs.dDAQDisplayMode && oodDAQEnabled)
+		ModifyGraph/W=$graph margin(left)=28
+	else
+		ModifyGraph/W=$graph margin(left)=0
+	endif
 
 	SetAxesRanges(graph, ranges)
 End

Packages/MIES/MIES_AnalysisBrowser_SweepBrowser.ipf
@@ -543,6 +543,7 @@ Function SB_PlotSweep(sweepBrowserDFR, currentMapIndex, newMapIndex)
 	tgs.overlayChannels = GetCheckBoxState(panel, "check_sweepbrowser_OverlayChan")
 	tgs.splitTTLBits    = GetCheckBoxState(panel, "check_SweepBrowser_SplitTTL")
 	tgs.dDAQDisplayMode = GetCheckBoxState(panel, "check_sweepbrowser_dDAQ")
+	tgs.oodDAQHeadstageRegions = str2num(GetPopupMenuString(panel, "popup_oodDAQ_regions"))
 	WAVE channelSelWave = SB_GetChannelSelWave(graph)
 
 	CreateTiledChannelGraph(graph, configWave, sweep, numericalValues, textualValues, tgs, sweepDFR=newSweepDFR, channelSelWave=channelSelWave)
@@ -749,6 +750,9 @@ Function/DF SB_CreateNewSweepBrowser()
 	CheckBox check_sweepbrowser_dDAQ,pos={97.00,50.00},size={47.00,15.00},proc=SB_CheckboxChangedSettings,title="dDAQ"
 	CheckBox check_sweepbrowser_dDAQ,help={"Enable dedicated support for viewing distributed DAQ data"}
 	CheckBox check_sweepbrowser_dDAQ,value= 0
+	PopupMenu popup_oodDAQ_regions,pos={96.00,67.00},size={35.00,19.00},bodyWidth=35,disable=2,proc=SB_PopMenuProc_ChangedSettings
+	PopupMenu popup_oodDAQ_regions,help={"Allows to view only oodDAQ regions from the selected headstage. Choose -1 to display all."}
+	PopupMenu popup_oodDAQ_regions,mode=1,popvalue="-1",value= #"\"-1;0;1;2;3;4;5;6;7\""
 	RenameWindow #,P0
 	SetActiveSubwindow ##
 	NewPanel/HOST=#/EXT=0/W=(0,0,214,407)  as "Analysis Results"
@@ -809,6 +813,12 @@ Function SB_CheckboxChangedSettings(cba) : CheckBoxControl
 					DisableControls(win, SWEEP_OVERLAY_DEP_CTRLS)
 				else
 					EnableControls(win, SWEEP_OVERLAY_DEP_CTRLS)
+				endif
+			elseif(!cmpstr(ctrl, "check_sweepbrowser_dDAQ"))
+				if(checked)
+					EnableControl(win, "popup_oodDAQ_regions")
+				else
+					DisableControl(win, "popup_oodDAQ_regions")
 				endif
 			elseif(StringMatch(ctrl, "check_SB_channelSel_*"))
 				WAVE channelSel = SB_GetChannelSelWave(graph)
@@ -1097,6 +1107,25 @@ Function SB_ButtonProc_FindMinis(ba) : ButtonControl
 				EDC_FindMinis(workDFR, wv)
 			endfor
 			break
+	endswitch
+
+	return 0
+End
+
+Function SB_PopMenuProc_ChangedSettings(pa) : PopupMenuControl
+	STRUCT WMPopupAction &pa
+
+	variable idx
+	string graph, win
+
+	switch(pa.eventCode)
+		case 2: // mouse up
+			win   = pa.win
+			graph = GetMainWindow(win)
+			DFREF dfr = $SB_GetSweepBrowserFolder(graph)
+			idx = GetPopupMenuIndex(win, "popup_sweep_selector")
+			SB_PlotSweep(dfr, idx, idx)
+		break
 	endswitch
 
 	return 0

Packages/MIES/MIES_DataBrowser.ipf
@@ -259,6 +259,7 @@ static Function DB_TilePlotForDataBrowser(panelTitle, sweep, sweepNo)
 	tgs.overlaySweep    = GetCheckBoxState(panelTitle, "check_DataBrowser_SweepOverlay")
 	tgs.overlayChannels = GetCheckBoxState(panelTitle, "check_databrowser_OverlayChan")
 	tgs.dDAQDisplayMode = GetCheckBoxState(panelTitle, "check_databrowser_dDAQMode")
+	tgs.oodDAQHeadstageRegions = GetSliderPositionIndex(panelTitle, "slider_oodDAQ_regions")
 
 	return CreateTiledChannelGraph(graph, config, sweepNo, numericalValues, textualValues, tgs, sweepWave=sweep)
 End
@@ -438,6 +439,12 @@ Window DataBrowser() : Panel
 	CheckBox checkbox_DB_AutoScaleVertAxVisX,userdata(ResizeControlsInfo) += A"zzzzzzzzzzzz!!#?(FEDG<zzzzzzzzzzz"
 	CheckBox checkbox_DB_AutoScaleVertAxVisX,userdata(ResizeControlsInfo) += A"zzz!!#?(FEDG<zzzzzzzzzzzzzz!!!"
 	CheckBox checkbox_DB_AutoScaleVertAxVisX,value= 0
+	Slider slider_oodDAQ_regions,pos={657.00,12.00},size={233.00,54.00},disable=2,proc=DB_SliderProc_ChangedSetting
+	Slider slider_oodDAQ_regions,help={"Allows to view only oodDAQ regions from the selected headstage. Choose -1 to display all."}
+	Slider slider_oodDAQ_regions,userdata(ResizeControlsInfo)= A"!!,J55QF)8!!#B#!!#>fz!!#](Aon\"Qzzzzzzzzzzzzzz!!#](Aon\"Qzz"
+	Slider slider_oodDAQ_regions,userdata(ResizeControlsInfo) += A"zzzzzzzzzzzz!!#u:DuaGl<C]S7zzzzzzzzzz"
+	Slider slider_oodDAQ_regions,userdata(ResizeControlsInfo) += A"zzz!!#u:DuaGl<C]S7zzzzzzzzzzzzz!!!"
+	Slider slider_oodDAQ_regions,limits={-1,7,1},value= -1,vert= 0
 	DefineGuide UGV0={FR,-200},UGH1={FT,0.584722,FB},UGH0={UGH1,0.662207,FB}
 	SetWindow kwTopWin,hook(ResizeControls)=ResizeControls#ResizeControlsHook
 	SetWindow kwTopWin,userdata(ResizeControlsInfo)= A"!!*'\"z!!#ERTE%A:zzzzzzzzzzzzzzzzzzzzz"
@@ -610,6 +617,12 @@ Window DataBrowser_IP7() : Panel
 	SetWindow kwTopWin,userdata(ResizeControlsInfoUGV0)= A":-hTC3`S[N0KW?-:-(dOFC@LVDg-86EaMC72d\\:$<*<$d3`U64E]Zff;Ft%f:/jMQ3\\WWl:K'ha8P`)B0eb=</het@7o`,K756hm;EIBK8OQ!&3]g5.9MeM`8Q88W:-'s^0JGQ"
 	SetWindow kwTopWin,userdata(ResizeControlsInfoUGH1)= A":-hTC3`S[@0frH.:-(dOFC@LVDg-86EaMC72d\\:$<*<$d3`U64E]Zff;Ft%f:/jMQ3\\`]m:K'ha8P`)B1bpd<0JGRY<CoSI0fhd'4%E:B6q&jl7RB1778-NR;b9q[:JNr)/i>UF2_m-M"
 	SetWindow kwTopWin,userdata(ResizeControlsInfoUGH0)= A":-hTC3`S[@0KW?-:-(dOFC@LVDg-86EaMC72d\\:$<*<$d3`U64E]Zff;Ft%f:/jMQ3\\`]m:K'ha8P`)B2DI3E0JGRY<CoSI0fi<)8231r<CoSI1-.lk4&SL@:et\"]<(Tk\\3\\W0E2DR$A2`h"
+	Slider slider_oodDAQ_regions,pos={657.00,12.00},size={233.00,54.00},disable=2,proc=DB_SliderProc_ChangedSetting
+	Slider slider_oodDAQ_regions,help={"Allows to view only oodDAQ regions from the selected headstage. Choose -1 to display all."}
+	Slider slider_oodDAQ_regions,userdata(ResizeControlsInfo)= A"!!,J55QF)8!!#B#!!#>fz!!#](Aon\"Qzzzzzzzzzzzzzz!!#](Aon\"Qzz"
+	Slider slider_oodDAQ_regions,userdata(ResizeControlsInfo) += A"zzzzzzzzzzzz!!#u:DuaGl<C]S7zzzzzzzzzz"
+	Slider slider_oodDAQ_regions,userdata(ResizeControlsInfo) += A"zzz!!#u:DuaGl<C]S7zzzzzzzzzzzzz!!!"
+	Slider slider_oodDAQ_regions,limits={-1,7,1},value= -1,vert= 0
 	Display/W=(18,72,1039,362)/FG=($"",$"",UGV0,UGH1)/HOST=#
 	SetWindow kwTopWin,userdata(MiesPanelType)=  "DataBrowser"
 	RenameWindow #,DataBrowserGraph
@@ -669,6 +682,9 @@ Function DB_DataBrowserStartupSettings()
 	SetCheckBoxState(panelTitle, "check_databrowser_OverlayChan", CHECKBOX_SELECTED)
 	SetCheckBoxState(panelTitle, "check_DataBrowser_DisplayADChan", CHECKBOX_SELECTED)
 	EnableControls(panelTitle, "check_DataBrowser_DisplayDAchan;check_databrowser_OverlayChan;check_DataBrowser_DisplayADChan;check_DataBrowser_DisplayTTL")
+
+	SetSliderPositionIndex(panelTitle, "slider_oodDAQ_regions", -1)
+	DisableControl(panelTitle, "slider_oodDAQ_regions")
 
 	DB_ClearGraph(panelTitle)
 	SetPopupMenuIndex(panelTitle, "popup_labenotebookViewableCols", 0)
@@ -852,6 +868,19 @@ Function DB_ButtonProc_SwitchXAxis(ba) : ButtonControl
 	return 0
 End
 
+Function DB_SliderProc_ChangedSetting(spa) : SliderControl
+	STRUCT WMSliderAction &spa
+
+	string panelTitle
+
+	if(spa.eventCode & 0x1)
+		panelTitle = spa.win
+		DB_PlotSweep(panelTitle)
+	endif
+
+	return 0
+End
+
 Function DB_CheckProc_ChangedSetting(cba) : CheckBoxControl
 	STRUCT WMCheckboxAction &cba
 
@@ -864,13 +893,22 @@ Function DB_CheckProc_ChangedSetting(cba) : CheckBoxControl
 			ctrl       = cba.ctrlName
 			checked    = cba.checked
 
-			if(!cmpstr(ctrl, "check_DataBrowser_SweepOverlay"))
-				if(checked)
-					DisableControls(panelTitle, "check_DataBrowser_DisplayDAchan;check_databrowser_OverlayChan;check_DataBrowser_DisplayADChan;check_DataBrowser_DisplayTTL;check_databrowser_dDAQMode")
-				else
-					EnableControls(panelTitle, "check_DataBrowser_DisplayDAchan;check_databrowser_OverlayChan;check_DataBrowser_DisplayADChan;check_DataBrowser_DisplayTTL;check_databrowser_dDAQMode")
-				endif
-			endif
+			strswitch(ctrl)
+				case "check_DataBrowser_SweepOverlay":
+					if(checked)
+						DisableControls(panelTitle, "check_DataBrowser_DisplayDAchan;check_databrowser_OverlayChan;check_DataBrowser_DisplayADChan;check_DataBrowser_DisplayTTL;check_databrowser_dDAQMode")
+					else
+						EnableControls(panelTitle, "check_DataBrowser_DisplayDAchan;check_databrowser_OverlayChan;check_DataBrowser_DisplayADChan;check_DataBrowser_DisplayTTL;check_databrowser_dDAQMode")
+					endif
+				break
+				case "check_databrowser_dDAQMode":
+					if(checked)
+						EnableControl(panelTitle, "slider_oodDAQ_regions")
+					else
+						DisableControl(panelTitle, "slider_oodDAQ_regions")
+					endif
+				break
+			endswitch
 
 			DB_PlotSweep(panelTitle)
 			break

Packages/MIES/MIES_Structures.ipf
@@ -45,6 +45,7 @@ Structure TiledGraphSettings
 	int16 overlaySweep
 	int16 overlayChannels
 	int16 dDAQDisplayMode
+	int16 oodDAQHeadstageRegions
 EndStructure
 
 /// @brief Helper structure for formula parsing of the Wavebuilder combine epoch

commit e7eca331f44a0c9cabc9ccdd5b0258ddb3b30166

CreateTiledChannelGraph: Use zero as default value
We are using dDAQEnabled and oodDAQEnabled as boolean variables so we need
them to be not NaN. As NaN has a non consistent behaviour regarding if and
the ternary opertator.

Bug introduced in a116cca7 (CreateTiledChannelGraph: Add support for
oodDAQ data, 2016-08-10).

Original-CommitID: e88f743e827e6c75ba2d0a3eb90969ac33ecda67

Packages/MIES/MIES_MiesUtilities.ipf
@@ -758,8 +758,8 @@ Function CreateTiledChannelGraph(graph, config, sweepNo, numericalValues,  textu
 
 	high = 1.0
 
-	dDAQEnabled   = GetLastSettingIndep(numericalValues, sweepNo, "Distributed DAQ")
-	oodDAQEnabled = GetLastSettingIndep(numericalValues, sweepNo, "Optimized Overlap dDAQ")
+	dDAQEnabled   = GetLastSettingIndep(numericalValues, sweepNo, "Distributed DAQ", defValue=0)
+	oodDAQEnabled = GetLastSettingIndep(numericalValues, sweepNo, "Optimized Overlap dDAQ", defValue=0)
 
 	if(tgs.dDAQDisplayMode && !(dDAQEnabled || oodDAQEnabled))
 		printf "Distributed DAQ display mode turned off as no dDAQ data could be found.\r"