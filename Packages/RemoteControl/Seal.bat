33d9175f7a0f51eeaea0ef14d084dd66653ab341
Thomas Braun <thomas.braun@byte-physics.de>
2016-09-22 18:19:44 +0200

    Remove RemoteControl for IP6
    Original-CommitID: 083f43573e3050f407dfd6ee93e2004b27c50ca0

--- Packages/RemoteControl/Seal.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-SET IGOR="C:\Program Files (x86)\WaveMetrics\Igor Pro Folder\Igor.exe"
-%IGOR% /Q/X P_UpdatePressureMode("ITC1600_Dev_0", 1,"button_DataAcq_Seal",0)
--- Packages/RemoteControl/StopAllTestPulses.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-SET IGOR="C:\Program Files (x86)\WaveMetrics\Igor Pro Folder\Igor.exe"
-%IGOR% /Q/X TP_StopTestPulseOnAllDevices()
