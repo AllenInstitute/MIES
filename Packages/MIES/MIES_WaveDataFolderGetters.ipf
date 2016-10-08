AllenInstitute/MIES

commit 121a4632a7a9c73df2283e9e6d75fe16e2c943cb

Labnotebook: Reorganize Async data handling
- Turn all intermediate waves into free waves to remove clutter
- Streamline calling sequence in DM_SaveAndScaleITCData

Original-CommitID: bf2508ac5b6c054b9f404a7530b8d66c34edfaa6

Packages/MIES/MIES_WaveDataFolderGetters.ipf
@@ -2305,10 +2305,10 @@ End
 /// @name Asynchronous Measurements
 /// @}
 
-/// @brief Returns a wave reference to the asyncMeasurementWave
+/// @brief Return a *free* wave for the async settings, data wave
 ///
 /// asyncMeasurementWave is used to save the actual async measurement data
-/// for each data sweep 
+/// for each data sweep
 ///
 /// Rows:
 /// - One row
@@ -2322,22 +2322,10 @@ End
 /// - 5: Async Measurement 5
 /// - 6: Async Measurement 6
 /// - 7: Async Measurement 7
-///
-/// Layers:
-/// - Only one...all async measurements apply across all headstages, so no need to create multiple layers
-Function/Wave GetAsyncMeasurementWave(panelTitle)
-	string panelTitle
+Function/Wave GetAsyncMeasurementWave()
 
-	DFREF dfr =GetDevSpecLabNBSettHistFolder(panelTitle)
-
-	Wave/Z/SDFR=dfr wv = asyncMeasurementWave
-
-	if(WaveExists(wv))
-		return wv
-	endif
-
-	Make/N=(1,8) dfr:asyncMeasurementWave/Wave=wv
-	wv = Nan
+	Make/FREE/N=(1,8) wv
+	wv = NaN
 
 	SetDimLabel 1, 0, MeasVal0, wv
 	SetDimLabel 1, 1, MeasVal1, wv
@@ -2347,11 +2335,11 @@ Function/Wave GetAsyncMeasurementWave(panelTitle)
 	SetDimLabel 1, 5, MeasVal5, wv
 	SetDimLabel 1, 6, MeasVal6, wv
 	SetDimLabel 1, 7, MeasVal7, wv
-	
+
 	return wv
 End
 
-/// @brief Returns a wave reference to the asyncMeasurementKeyWave
+/// @brief Return a *free* wave for the async settings, key wave
 ///
 /// asyncMeasurementKeyWave is used to index async measurements for
 /// each data sweep and create waveNotes for tagging data sweeps
@@ -2370,27 +2358,15 @@ End
 /// - 5: Async Measurement 5
 /// - 6: Async Measurement 6
 /// - 7: Async Measurement 7
-///
-/// Layers:
-/// - Just one
-Function/Wave GetAsyncMeasurementKeyWave(panelTitle)
-	string panelTitle
+Function/Wave GetAsyncMeasurementKeyWave()
 
-	DFREF dfr = GetDevSpecLabNBSettKeyFolder(panelTitle)
-
-	Wave/Z/T/SDFR=dfr wv = asyncMeasurementKeyWave
-
-	if(WaveExists(wv))
-		return wv
-	endif
-
-	Make/T/N=(3,8) dfr:asyncMeasurementKeyWave/Wave=wv
+	Make/FREE/T/N=(3,8) wv
 	wv = ""
 
 	SetDimLabel 0, 0, Parameter, wv
 	SetDimLabel 0, 1, Units, wv
 	SetDimLabel 0, 2, Tolerance, wv
-	
+
 	wv[%Parameter][0] = "Async AD 0"
 	wv[%Units][0]     = ""
 	wv[%Tolerance][0] = ".0001"
@@ -2414,19 +2390,19 @@ Function/Wave GetAsyncMeasurementKeyWave(panelTitle)
 	wv[%Parameter][5] = "Async AD 5"
 	wv[%Units][5]     = ""
 	wv[%Tolerance][5] = ".0001"
-	
+
 	wv[%Parameter][6] = "Async AD 6"
 	wv[%Units][6]     = ""
 	wv[%Tolerance][6] = ".0001"
-	
+
 	wv[%Parameter][7] = "Async AD 7"
 	wv[%Units][7]     = ""
 	wv[%Tolerance][7] = ".0001"
-	
+
 	return wv
 End
 
-/// @brief Returns a wave reference to the asyncSettingsWave
+/// @brief Return a *free* wave for the asyncSettingsWave, data wave
 ///
 /// asyncSettingsWave is used to save async settings for each
 /// data sweep and create waveNotes for tagging data sweeps
@@ -2475,20 +2451,11 @@ End
 /// - 37: Async Alarm 5 Max
 /// - 38: Async Alarm 6 Max
 /// - 39: Async Alarm 7 Max
-Function/Wave GetAsyncSettingsWave(panelTitle)
-	string panelTitle
+Function/Wave GetAsyncSettingsWave()
 
-	DFREF dfr =GetDevSpecLabNBSettHistFolder(panelTitle)
-
-	Wave/Z/SDFR=dfr wv = asyncSettingsWave
-
-	if(WaveExists(wv))
-		return wv
-	endif
-
-	Make/N=(1,40) dfr:asyncSettingsWave/Wave=wv
+	Make/N=(1,40) wv
 	wv = Nan
-	
+
 	SetDimLabel 1, 0, ADOnOff0, wv
 	SetDimLabel 1, 1, ADOnOff1, wv
 	SetDimLabel 1, 2, ADOnOff2, wv
@@ -2533,7 +2500,7 @@ Function/Wave GetAsyncSettingsWave(panelTitle)
 	return wv
 End
 
-/// @brief Returns a wave reference to the asyncSettingsKeyWave
+/// @brief Return a *free* wave for the asyncSettingsKeyWave
 ///
 /// asyncSettingsKeyWave is used to index async settings for
 /// each data sweep and create waveNotes for tagging data sweeps
@@ -2584,18 +2551,9 @@ End
 /// - 37: Async Alarm 5 Max
 /// - 38: Async Alarm 6 Max
 /// - 39: Async Alarm 7 Max
-Function/Wave GetAsyncSettingsKeyWave(panelTitle)
-	string panelTitle
+Function/Wave GetAsyncSettingsKeyWave()
 
-	DFREF dfr = GetDevSpecLabNBSettKeyFolder(panelTitle)
-
-	Wave/Z/T/SDFR=dfr wv = asyncSettingsKeyWave
-
-	if(WaveExists(wv))
-		return wv
-	endif
-
-	Make/T/N=(3,40) dfr:asyncSettingsKeyWave/Wave=wv
+	Make/T/N=(3,40) wv
 	wv = ""
 
 	SetDimLabel 0, 0, Parameter, wv
@@ -2765,7 +2723,7 @@ Function/Wave GetAsyncSettingsKeyWave(panelTitle)
 	return wv
 End
 
-/// @brief Returns a wave reference to the AsyncSettingsTxtWave
+/// @brief Return a free wave for the AsyncSettingsTxtWave
 ///
 /// AsyncSettingsTxtData is used to store the async text settings used on a particular
 /// headstage and then create waveNotes for the sweep data
@@ -2790,24 +2748,15 @@ End
 /// - 13: Async 5 Units
 /// - 14: Async 6 Units
 /// - 15: Async 7 Units
-Function/Wave GetAsyncSettingsTextWave(panelTitle)
-	string panelTitle
+Function/Wave GetAsyncSettingsTextWave()
 
-	DFREF dfr = GetDevSpecLabNBTextDocFolder(panelTitle)
-
-	Wave/Z/T/SDFR=dfr wv = asyncSettingsTxtData
-
-	if(WaveExists(wv))
-		return wv
-	endif
-
-	Make/T/N=(1,16) dfr:asyncSettingsTxtData/Wave=wv
+	Make/T/N=(1,16) wv
 	wv = ""
 
 	return wv
 End
 
-/// @brief Returns a wave reference to the AsyncSettingsKeyTxtData
+/// @brief Return a *free* wave for the AsyncSettingsKeyTxtData
 ///
 /// AsyncSettingsKeyTxtData is used to index Txt Key Wave
 ///
@@ -2831,20 +2780,11 @@ End
 /// - 13: Async 5 Unit
 /// - 14: Async 6 Unit
 /// - 15: Async 7 Unit
-Function/Wave GetAsyncSettingsTextKeyWave(panelTitle)
-	string panelTitle
+Function/Wave GetAsyncSettingsTextKeyWave()
 
-	DFREF dfr = GetDevSpecLabNBTxtDocKeyFolder(panelTitle)
-
-	Wave/Z/T/SDFR=dfr wv = asyncSettingsKeyTxtData
-
-	if(WaveExists(wv))
-		return wv
-	endif
-
-	Make/T/N=(1,16) dfr:asyncSettingsKeyTxtData/Wave=wv
+	Make/T/N=(1,16) wv
 	wv = ""
-	
+
 	wv[0][0] = "Async AD0 Title"
 	wv[0][1] = "Async AD1 Title"
 	wv[0][2] = "Async AD2 Title"
@@ -2861,7 +2801,7 @@ Function/Wave GetAsyncSettingsTextKeyWave(panelTitle)
 	wv[0][13] = "Async AD5 Unit"
 	wv[0][14] = "Async AD6 Unit"
 	wv[0][15] = "Async AD7 Unit"
-	
+
 	return wv
 End
 

Packages/MIES/MIES_DataAcqITC.ipf
@@ -377,8 +377,8 @@ Function ITC_StartTestPulse(panelTitle)
 	TP_Teardown(panelTitle)
 END
 
-Function ITC_ADDataBasedWaveNotes(dataWave, panelTitle)
-	WAVE dataWave
+Function ITC_ADDataBasedWaveNotes(asyncMeasurementWave, panelTitle)
+	WAVE asyncMeasurementWave
 	string panelTitle
 
 	// This function takes about 0.9 seconds to run
@@ -387,10 +387,6 @@ Function ITC_ADDataBasedWaveNotes(dataWave, panelTitle)
 	string setvarTitle, setvarGain, title
 
 	NVAR ITCDeviceIDGlobal = $GetITCDeviceIDGlobal(panelTitle)
-
-	// Create the measurement wave that will hold the measurement values
-	WAVE asyncMeasurementWave = GetAsyncMeasurementWave(panelTitle)
-	asyncMeasurementWave[0][] = NaN
 
 	WAVE asyncChannelState = DC_ControlStatusWaveCache(panelTitle, CHANNEL_TYPE_ASYNC)
 	deviceChannelOffset = HW_ITC_CalculateDevChannelOff(panelTitle)
@@ -410,8 +406,7 @@ Function ITC_ADDataBasedWaveNotes(dataWave, panelTitle)
 
 		title = GetSetVariableString(panelTitle, setvarTitle)
 		gain  = GetSetVariable(panelTitle, setvarGain)
-		print "raw async", rawChannelValue
-		// put the measurement value into the async settings wave for creation of wave notes
+
 		asyncMeasurementWave[0][i] = rawChannelValue / gain // put the measurement value into the async settings wave for creation of wave notes
 		ITC_SupportSystemAlarm(i, asyncMeasurementWave[0][i], title, panelTitle)
 	endfor

Packages/MIES/MIES_DataManagementNew.ipf
@@ -45,16 +45,12 @@ Function DM_SaveAndScaleITCData(panelTitle)
 		// AI_createDummySettingsWave(panelTitle, SweepNo)
 	endif
 
-	// if option is checked, wave note containing single readings from (async) ADs is made
 	if(GetCheckboxState(panelTitle, "Check_Settings_Append"))
-		ITC_ADDataBasedWaveNotes(dataWave, panelTitle)
+		ED_createAsyncWaveNoteTags(panelTitle, sweepNo)
 	endif
 
 	// Add wave notes for the stim wave name and scale factor
 	ED_createWaveNoteTags(panelTitle, sweepNo)
-
-	// Add wave notes for the factors on the Asyn tab
-	ED_createAsyncWaveNoteTags(panelTitle, sweepNo)
 
 	// TP settings, especially useful if "global TP insertion" is active
 	ED_TPSettingsDocumentation(panelTitle)

Packages/MIES/MIES_ExperimentDocumentation.ipf
@@ -421,23 +421,15 @@ function ED_createAsyncWaveNoteTags(panelTitle, sweepCount)
 	string ctrl
 	variable minSettingValue, maxSettingValue
 
-	// Create the numerical wave for saving the numerical settings
-	Wave asyncSettingsWave = GetAsyncSettingsWave(panelTitle)
-	Wave/T asyncSettingsKey = GetAsyncSettingsKeyWave(panelTitle)
+	Wave asyncSettingsWave = GetAsyncSettingsWave()
+	Wave/T asyncSettingsKey = GetAsyncSettingsKeyWave()
 
-	// Create the txt wave to be used for saving the txt settings
-	Wave/T asyncSettingsTxtWave = GetAsyncSettingsTextWave(panelTitle)
-	Wave/T asyncSettingsTxtKey = GetAsyncSettingsTextKeyWave(panelTitle)
-	
-	// Create the measurement wave that will hold the measurement values
-	Wave asyncMeasurementWave = GetAsyncMeasurementWave(panelTitle)
-	Wave/T asyncMeasurementKey = GetAsyncMeasurementKeyWave(panelTitle)
-	
-	// fill the settings wave with NAN's...the asyncMeasurementWave will be filled with NAN's in another function
-	asyncSettingsWave[0][] = NAN
+	Wave/T asyncSettingsTxtWave = GetAsyncSettingsTextWave()
+	Wave/T asyncSettingsTxtKey = GetAsyncSettingsTextKeyWave()
 
-	// Now populate the aync Settings and measurement Waves
-	// first...determine if the head stage is being controlled
+	Wave asyncMeasurementWave = GetAsyncMeasurementWave()
+	Wave/T asyncMeasurementKey = GetAsyncMeasurementKeyWave()
+
 	variable asyncVariablesCounter
 	for(asyncVariablesCounter = 0;asyncVariablesCounter < NUM_ASYNC_CHANNELS ;asyncVariablesCounter += 1)
 		ctrl = GetPanelControl(asyncVariablesCounter, CHANNEL_TYPE_ASYNC, CHANNEL_CONTROL_CHECK)
@@ -483,15 +475,11 @@ function ED_createAsyncWaveNoteTags(panelTitle, sweepCount)
 		endif
 	endfor
 
-	if(GetCheckBoxState(panelTitle, "Check_Settings_Append"))
-		// call the function that will create the numerical wave notes
-		ED_createWaveNotes(asyncSettingsWave, asyncSettingsKey, SweepCount, panelTitle)
-	
-		// call the function that will create the measurement wave notes
-		ED_createWaveNotes(asyncMeasurementWave, asyncMeasurementKey, SweepCount, panelTitle)
+	ED_createTextNotes(asyncSettingsTxtWave, asyncSettingsTxtKey, sweepCount, panelTitle)
+	ED_createWaveNotes(asyncSettingsWave, asyncSettingsKey, SweepCount, panelTitle)
 
-		ED_createTextNotes(asyncSettingsTxtWave, asyncSettingsTxtKey, sweepCount, panelTitle)
-	endif
+	ITC_ADDataBasedWaveNotes(asyncMeasurementWave, panelTitle)
+	ED_createWaveNotes(asyncMeasurementWave, asyncMeasurementKey, SweepCount, panelTitle)
 End
 
 /// @brief Stores test pulse related data in the labnotebook

commit 9a58dee7a5f2aeacf2715a9a8bad3a88ed5401aa

Bugfix: Create temporary async data wave as free wave
Bug introduced in 121a4632 (Labnotebook: Reorganize Async data handling,
2016-04-03).

Original-CommitID: ad316b694a84cd522a72d73fb17c431d556b8fdf

Packages/MIES/MIES_WaveDataFolderGetters.ipf
@@ -2529,7 +2529,7 @@ End
 /// - 39: Async Alarm 7 Max
 Function/Wave GetAsyncSettingsWave()
 
-	Make/N=(1,40) wv
+	Make/N=(1,40)/FREE wv
 	wv = Nan
 
 	SetDimLabel 1, 0, ADOnOff0, wv
@@ -2629,7 +2629,7 @@ End
 /// - 39: Async Alarm 7 Max
 Function/Wave GetAsyncSettingsKeyWave()
 
-	Make/T/N=(3,40) wv
+	Make/T/N=(3,40)/FREE wv
 	wv = ""
 
 	SetDimLabel 0, 0, Parameter, wv
@@ -2799,7 +2799,7 @@ Function/Wave GetAsyncSettingsKeyWave()
 	return wv
 End
 
-/// @brief Return a free wave for the AsyncSettingsTxtWave
+/// @brief Return a *free* wave for the AsyncSettingsTxtWave
 ///
 /// AsyncSettingsTxtData is used to store the async text settings used on a particular
 /// headstage and then create waveNotes for the sweep data
@@ -2826,7 +2826,7 @@ End
 /// - 15: Async 7 Units
 Function/Wave GetAsyncSettingsTextWave()
 
-	Make/T/N=(1,16) wv
+	Make/T/N=(1,16)/FREE wv
 	wv = ""
 
 	return wv
@@ -2858,7 +2858,7 @@ End
 /// - 15: Async 7 Unit
 Function/Wave GetAsyncSettingsTextKeyWave()
 
-	Make/T/N=(1,16) wv
+	Make/T/N=(1,16)/FREE wv
 	wv = ""
 
 	wv[0][0] = "Async AD0 Title"