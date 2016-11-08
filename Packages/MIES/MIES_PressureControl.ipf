AllenInstitute/MIES

commit 9ad556808cffb87b012aec21414bfcc8eeddcf03

Hardware: Introduce two more flags
HARDWARE_PREVENT_ERROR_POPUP passes /Z=1 to all XOP calls and results in
no popup if an error is encountered.

HARDWARE_PREVENT_ERROR_MESSAGE avoids an error message to the command line
in case of an error. This flags does *not* prevent the popup from ITC XOP
calls.

Original-CommitID: 1a099cded4d0a9d2a82b2b1a9336db8eacff88f5

Packages/MIES/MIES_PressureControl.ipf
@@ -522,7 +522,7 @@ static Function P_CloseDeviceLowLevel(panelTitle, deviceToClose, refHeadstage)
 	deviceID = PressureDataWv[refHeadstage][%DAC_DevID]
 	hwType   = pressureDataWv[refHeadstage][%HW_DAC_Type]
 
-	if(IsFinite(deviceID) && IsFinite(hwType) && !HW_SelectDevice(hwType, deviceID))
+	if(IsFinite(deviceID) && IsFinite(hwType) && !HW_SelectDevice(HARDWARE_ITC_DAC, deviceID, flags=HARDWARE_PREVENT_ERROR_POPUP | HARDWARE_PREVENT_ERROR_MESSAGE))
 		HW_ResetDevice(hwType, deviceID)
 		doDeRegister = 1
 	endif

Packages/MIES/MIES_Constants.ipf
@@ -441,7 +441,9 @@ Constant HARDWARE_ITC_TTL_1600_RACK_ONE  = 3
 /// @name Flags for all hardware interaction functions from MIES_DAC-Hardware.ipf
 /// @anchor HardwareInteractionFlags
 /// @{
-Constant HARDWARE_ABORT_ON_ERROR = 0x01
+Constant HARDWARE_ABORT_ON_ERROR        = 0x01
+Constant HARDWARE_PREVENT_ERROR_POPUP   = 0x02
+Constant HARDWARE_PREVENT_ERROR_MESSAGE = 0x04
 /// @}
 
 /// List of different DAC hardware types

Packages/MIES/MIES_DAC-Hardware.ipf
@@ -796,7 +796,7 @@ Function HW_ITC_SelectDevice(deviceID, [flags])
 
 	DEBUGPRINTSTACKINFO()
 
-	ITCSelectDevice2/Z deviceID
+	ITCSelectDevice2/Z=(flags & HARDWARE_PREVENT_ERROR_POPUP) deviceID
 	return HW_ITC_HandleReturnValues(flags, V_ITCError, V_ITCXOPError)
 End
 
@@ -806,7 +806,7 @@ Function HW_ITC_EnableYoking([flags])
 
 	DEBUGPRINTSTACKINFO()
 
-	ITCInitialize2/M=1
+	ITCInitialize2/M=1/Z=(flags & HARDWARE_PREVENT_ERROR_POPUP)
 	HW_ITC_HandleReturnValues(flags, V_ITCError, V_ITCXOPError)
 End
 
@@ -816,7 +816,7 @@ Function HW_ITC_DisableYoking([flags])
 
 	DEBUGPRINTSTACKINFO()
 
-	ITCInitialize2/M=0
+	ITCInitialize2/M=0/Z=(flags & HARDWARE_PREVENT_ERROR_POPUP)
 	HW_ITC_HandleReturnValues(flags, V_ITCError, V_ITCXOPError)
 End
 
@@ -826,11 +826,11 @@ Function HW_ITC_StopAcq([prepareForDAQ, flags])
 
 	DEBUGPRINTSTACKINFO()
 
-	ITCStopAcq2
+	ITCStopAcq2/Z=(flags & HARDWARE_PREVENT_ERROR_POPUP)
 	HW_ITC_HandleReturnValues(flags, V_ITCError, V_ITCXOPError)
 
 	if(prepareForDAQ)
-		ITCConfigChannelUpload2
+		ITCConfigChannelUpload2/Z=(flags & HARDWARE_PREVENT_ERROR_POPUP)
 		HW_ITC_HandleReturnValues(flags, V_ITCError, V_ITCXOPError)
 	endif
 End
@@ -842,7 +842,7 @@ Function HW_ITC_GetCurrentDevice([flags])
 
 	DEBUGPRINTSTACKINFO()
 
-	ITCGetCurrentDevice2
+	ITCGetCurrentDevice2/Z=(flags & HARDWARE_PREVENT_ERROR_POPUP)
 	HW_ITC_HandleReturnValues(flags, V_ITCError, V_ITCXOPError)
 
 	return V_Value
@@ -870,7 +870,7 @@ Function HW_ITC_ResetFifo(deviceID, [fifoPos, flags])
 	endif
 
 	WAVE fifoPos_t = HW_ITC_TransposeAndToDouble(fifoPos)
-	ITCUpdateFIFOPositionAll2 fifoPos_t
+	ITCUpdateFIFOPositionAll2/Z=(flags & HARDWARE_PREVENT_ERROR_POPUP) fifoPos_t
 	HW_ITC_HandleReturnValues(flags, V_ITCError, V_ITCXOPError)
 End
 
@@ -882,10 +882,10 @@ Function HW_ITC_StartAcq(triggerMode, [flags])
 
 	switch(triggerMode)
 		case HARDWARE_DAC_EXTERNAL_TRIGGER:
-			ITCStartAcq2/EXT=256
+			ITCStartAcq2/EXT=256/Z=(flags & HARDWARE_PREVENT_ERROR_POPUP)
 			break
 		case HARDWARE_DAC_DEFAULT_TRIGGER:
-			ITCStartAcq2
+			ITCStartAcq2/Z=(flags & HARDWARE_PREVENT_ERROR_POPUP)
 			break
 		default:
 			ASSERT(0, "Unknown trigger mode")
@@ -915,7 +915,8 @@ Function/WAVE HW_ITC_GetState([flags])
 
 	DEBUGPRINTSTACKINFO()
 
-	ITCGetState2/R/O/C/E/FREE state
+	ITCGetState2/R/O/C/E/FREE/Z=(flags & HARDWARE_PREVENT_ERROR_POPUP) state
+
 	HW_ITC_HandleReturnValues(flags, V_ITCError, V_ITCXOPError)
 
 	return state
@@ -927,7 +928,7 @@ Function HW_ITC_ReadADC(deviceID, channel, [flags])
 
 	DEBUGPRINTSTACKINFO()
 
-	ITCReadADC2/C=1/V=1 channel
+	ITCReadADC2/C=1/V=1/Z=(flags & HARDWARE_PREVENT_ERROR_POPUP) channel
 	HW_ITC_HandleReturnValues(flags, V_ITCError, V_ITCXOPError)
 
 	return V_Value
@@ -939,7 +940,7 @@ Function HW_ITC_WriteDAC(deviceID, channel, value, [flags])
 
 	DEBUGPRINTSTACKINFO()
 
-	ITCSetDAC2 channel, value
+	ITCSetDAC2/Z=(flags & HARDWARE_PREVENT_ERROR_POPUP) channel, value
 	HW_ITC_HandleReturnValues(flags, V_ITCError, V_ITCXOPError)
 End
 
@@ -949,7 +950,7 @@ Function HW_ITC_ReadDigital(deviceID, xopChannel, [flags])
 
 	DEBUGPRINTSTACKINFO()
 
-	ITCReadDigital2 xopChannel
+	ITCReadDigital2/Z=(flags & HARDWARE_PREVENT_ERROR_POPUP) xopChannel
 	HW_ITC_HandleReturnValues(flags, V_ITCError, V_ITCXOPError)
 
 	return V_Value
@@ -961,7 +962,7 @@ Function HW_ITC_WriteDigital(deviceID, xopChannel, value, [flags])
 
 	DEBUGPRINTSTACKINFO()
 
-	ITCWriteDigital2 xopChannel, value
+	ITCWriteDigital2/Z=(flags & HARDWARE_PREVENT_ERROR_POPUP) xopChannel, value
 	HW_ITC_HandleReturnValues(flags, V_ITCError, V_ITCXOPError)
 End
 
@@ -971,7 +972,7 @@ Function HW_ITC_DebugMode(state, [flags])
 
 	DEBUGPRINTSTACKINFO()
 
-	ITCSetGlobals2/D=(state)
+	ITCSetGlobals2/D=(state)/Z=(flags & HARDWARE_PREVENT_ERROR_POPUP)
 End
 
 Function/Wave HW_WAVE_GETTER_PROTOTYPE(str)
@@ -1051,11 +1052,11 @@ Function HW_ITC_PrepareAcq(deviceID, [data, dataFunc, config, configFunc, fifoPo
 	endif
 
 	WAVE config_t = HW_ITC_TransposeAndToDouble(config)
-	ITCconfigAllchannels2 config_t, data
+	ITCconfigAllchannels2/Z=(flags & HARDWARE_PREVENT_ERROR_POPUP) config_t, data
 	HW_ITC_HandleReturnValues(flags, V_ITCError, V_ITCXOPError)
 
 #ifdef DEBUGGING_ENABLED
-	ITCGetAllChannelsConfig2/O config_t, settings
+	ITCGetAllChannelsConfig2/O/Z=(flags & HARDWARE_PREVENT_ERROR_POPUP) config_t, settings
 	HW_ITC_HandleReturnValues(flags, V_ITCError, V_ITCXOPError)
 
 	printf "xop: %d with alignment %d\r", settings[%FIFOPointer][0], GetAlignment(settings[%FIFOPointer][0])
@@ -1065,7 +1066,7 @@ Function HW_ITC_PrepareAcq(deviceID, [data, dataFunc, config, configFunc, fifoPo
 #endif // DEBUGGING_ENABLED
 
 	WAVE fifoPos_t = HW_ITC_TransposeAndToDouble(fifoPos)
-	ITCUpdateFIFOPositionAll2 fifoPos_t
+	ITCUpdateFIFOPositionAll2/Z=(flags & HARDWARE_PREVENT_ERROR_POPUP) fifoPos_t
 	HW_ITC_HandleReturnValues(flags, V_ITCError, V_ITCXOPError)
 End
 
@@ -1115,7 +1116,7 @@ Function HW_ITC_MoreData(deviceID, [ADChannelToMonitor, stopCollectionPoint, fif
 
 	wave config = GetITCChanConfigWave(panelTitle)
 	WAVE config_t = HW_ITC_TransposeAndToDouble(config)
-	ITCFIFOAvailableALL2/FREE config_t, fifoAvail_t
+	ITCFIFOAvailableALL2/FREE/Z=(flags & HARDWARE_PREVENT_ERROR_POPUP) config_t, fifoAvail_t
 	HW_ITC_HandleReturnValues(flags, V_ITCError, V_ITCXOPError)
 	WAVE fifoAvailNew = HW_ITC_TransposeAndToInt(fifoAvail_t)
 	fifoAvail[][] = fifoAvailNew[p][q]

Packages/MIES/MIES_Manipulator.ipf
@@ -241,7 +241,7 @@ Function M_CheckSettings(panelTitle)
 			return 1
 		endif
 
-		if(HW_SelectDevice(HARDWARE_ITC_DAC, ITCDeviceIDGlobal))
+		if(HW_SelectDevice(HARDWARE_ITC_DAC, ITCDeviceIDGlobal, flags=HARDWARE_PREVENT_ERROR_POPUP | HARDWARE_PREVENT_ERROR_MESSAGE))
 			printf "(%s) Device can not be selected. Please unlock and lock the device.\r", panelTitle
 			return 1
 		endif

Packages/MIES/MIES_PanelITC.ipf
@@ -4960,7 +4960,7 @@ Function DAP_CheckSettings(panelTitle, mode)
 		NVAR ITCDeviceIDGlobal = $GetITCDeviceIDGlobal(panelTitle)
 
 #ifndef EVIL_KITTEN_EATING_MODE
-		if(HW_SelectDevice(HARDWARE_ITC_DAC, ITCDeviceIDGlobal))
+		if(HW_SelectDevice(HARDWARE_ITC_DAC, ITCDeviceIDGlobal, flags=HARDWARE_PREVENT_ERROR_POPUP | HARDWARE_PREVENT_ERROR_MESSAGE))
 			printf "(%s) Device can not be selected. Please unlock and lock the device.\r", panelTitle
 			ControlWindowToFront()
 			return 1

commit d707135f9dc56d4a77cda5fb3d95cea853e9ffc6

P_CloseDeviceLowLevel: Use actual hardware type
Bug introduced in 9ad55680 (Hardware: Introduce two more flags,
2016-06-09).

Original-CommitID: 86104d4719283ce40eca4cdd91a39e3e9f6fbe50

Packages/MIES/MIES_PressureControl.ipf
@@ -522,7 +522,7 @@ static Function P_CloseDeviceLowLevel(panelTitle, deviceToClose, refHeadstage)
 	deviceID = PressureDataWv[refHeadstage][%DAC_DevID]
 	hwType   = pressureDataWv[refHeadstage][%HW_DAC_Type]
 
-	if(IsFinite(deviceID) && IsFinite(hwType) && !HW_SelectDevice(HARDWARE_ITC_DAC, deviceID, flags=HARDWARE_PREVENT_ERROR_POPUP | HARDWARE_PREVENT_ERROR_MESSAGE))
+	if(IsFinite(deviceID) && IsFinite(hwType) && !HW_SelectDevice(hwType, deviceID, flags=HARDWARE_PREVENT_ERROR_POPUP | HARDWARE_PREVENT_ERROR_MESSAGE))
 		HW_ResetDevice(hwType, deviceID)
 		doDeRegister = 1
 	endif