AllenInstitute/MIES

commit 9e6544b3e8f41c7fddf20a7d41f07a6b6c90839d

AB: IPNWB include
Original-CommitID: 51ab35112068cf2e504fa96de09a815febaf27c6

Packages/MIES/MIES_AnalysisBrowser.ipf
@@ -14,6 +14,12 @@
 #include ":ACL_UserdataEditor"
 #include ":FixScrolling"
 
+// NWB includes
+#include ":..:IPNWB:IPNWB_Include"
+#include ":MIES_NeuroDataWithoutBorders"
+#include ":MIES_WaveBuilder"
+#include ":MIES_WaveBuilderPanel"
+
 // our includes
 #include ":MIES_AnalysisFunctionHelpers"
 #include ":MIES_Constants"

commit 8ec50dd821160f2b64e9ee09bddd38247c295617

Remove duplicated Wavebuilder menu entry
Bug introduced in 9e6544b3 (AB: IPNWB include, 2016-08-19).

Original-CommitID: 384a467243cd6a1aa5564ccae275559e0decb140

Packages/MIES/MIES_AnalysisBrowser.ipf
@@ -19,7 +19,7 @@
 #include ":..:IPNWB:IPNWB_Include"
 #include ":MIES_NeuroDataWithoutBorders"
 #include ":MIES_WaveBuilder"
-#include ":MIES_WaveBuilderPanel"
+#include ":MIES_WaveBuilderPanel" menus=0
 
 // ZeroMQ procedures
 #include ":..:ZeroMQ:procedures:ZeroMQ_Interop"