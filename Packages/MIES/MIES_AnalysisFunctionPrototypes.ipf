AllenInstitute/MIES

commit 6828aa6085d9ddf9df729d0769d705cabbd7d1b1

Analysis functions to set initial stimulus set parameters and to switch holding potential midway through a stimulus set. Intended for multipatch pipeline use.
Original-CommitID: 5b016c9ca62cb347cc307e1b1748717357db0798

Packages/MIES/MIES_AnalysisFunctions.ipf
@@ -117,3 +117,166 @@ Function Enforce_IC(panelTitle, eventType, ITCDataWave, headStage, realDataLengt
 
 	return 0
 End
+
+// User Defined Analysis Functions
+// Functions which can be assigned to various epochs of a stimulus set
+// Starts with a pop-up menu to set initial parameters and then switches holding potential midway through total number of sweeps
+
+
+static strCONSTANT panelTitle = "ITC18USB_Dev_0"
+static strCONSTANT stimSetlocal = "PulseTrain_150Hz_DA_0"
+static CONSTANT Vm1local = -55
+static CONSTANT Vm2local = -85
+static CONSTANT scalelocal = 70
+static CONSTANT sweepslocal = 6
+static CONSTANT ITIlocal = 15
+ 
+
+// Force active headstages into voltage clamp
+Function SetStimConfig_Vclamp(panelTitle, eventType, ITCDataWave, headStage)
+	string panelTitle
+	variable eventType
+	Wave ITCDataWave
+	variable headstage
+	
+	setVClampMode(panelTitle)
+	
+	printf "Stimulus set running in V-Clamp on headstage: %d\r", headStage
+	
+End
+
+// Change holding potential midway through stim set
+Function ChangeHoldingPotential(panelTitle, eventType, ITCDataWave, headStage)
+	string panelTitle
+	variable eventType
+	Wave ITCDataWave
+	variable headstage
+	
+	variable StimRemaining = switchHolding(panelTitle,Vm2local)
+	
+	printf "Number of stimuli remaining is: %d on headstage: %d\r", StimRemaining, headStage
+End
+
+// GUI to set subset of initial stimulus parameters and begin data acquisition. NOTE: DATA ACQUISITION IS INTIATED AT THE END OF FUNCTION! 
+Function StimParamGUI()
+	
+	string StimSetList = ReturnListOfAllStimSets(CHANNEL_TYPE_DAC,"*DA*")
+	
+	variable Vm1 = Vm1local, Scale = Scalelocal, sweeps = sweepslocal, ITI = ITIlocal
+	string stimSet = stimSetlocal 
+	Prompt stimSet, "Choose which stimulus set to run:", popup, StimSetList
+	Prompt Vm1, "Enter initial holding potential: "
+	Prompt Scale, "Enter scale of stimulation [mV]: "
+	Prompt sweeps, "Enter number of sweeps to run: "
+	Prompt ITI, "Enter inter-trial interval [s]: "
+	
+	DoPrompt "Choose stimulus set and enter initial parameters", stimSet, Vm1,  Scale, sweeps, ITI
+	
+	SetStimParam(stimSet,Vm1,Scale,Sweeps,ITI)
+	
+	PGC_SetAndActivateControl(panelTitle,"DataAcquireButton")
+End
+
+// Setting of stimulus parameters	
+Function SetStimParam(stimSet, Vm1, Scale, Sweeps, ITI)
+	variable Vm1, scale, sweeps, ITI
+	string stimSet
+	
+	setHolding(panelTitle, Vm1)
+	
+	variable stimSetIndex = GetStimSet(stimSet)
+	
+	PGC_SetAndActivateControl(panelTitle,"Wave_DA_All", val = stimSetIndex + 1)
+	PGC_SetAndActivateControl(panelTitle,"Scale_DA_All", val = scale)
+	PGC_SetAndActivateControl(panelTitle,"SetVar_DataAcq_SetRepeats", val = sweeps)
+	PGC_SetAndActivateControl(panelTitle,"SetVar_DataAcq_ITI", val = ITI)
+	
+	WAVE GuiState = GetDA_EphysGuiStateNum(panelTitle)
+	
+   variable GuiControl = findDimLabel(GuiState,1,"Check_DataAcq1_DistribDaq")				// make sure dDAQ is enabled
+   if (GuiControl != 1)
+   		PGC_SetAndActivateControl(panelTitle,"Check_DataAcq1_DistribDaq", val = 1)
+   	endif
+   	
+   	GuiControl = findDimLabel(GuiState,1,"Check_DataAcq_Get_Set_ITI")						// make sure Get/Set ITI is disabled
+   	if (GuiControl != 0)
+   		PGC_SetAndActivateControl(panelTitle,"Check_DataAcq_Get_Set_ITI", val = 0)
+   	endif
+
+End
+
+// set holding potential for active headstages
+Function setHolding(panelTitle, Vm1)
+	string panelTitle
+	variable Vm1
+	
+	WAVE statusHS = DC_ControlStatusWaveCache(panelTitle, CHANNEL_TYPE_HEADSTAGE)
+	
+	variable i
+	
+	for (i=0; i<NUM_HEADSTAGES; i+=1)
+		if (statusHS[i] == 1)
+			PGC_SetAndActivateControl(panelTitle,"slider_DataAcq_ActiveHeadstage", val = i)
+			PGC_SetAndActivateControl(panelTitle,"setvar_DataAcq_Hold_VC", val = Vm1)
+			PGC_SetAndActivateControl(panelTitle,"setvar_DataAcq_Hold_IC", val = Vm1)
+		endif
+	endfor
+End
+
+//Set active headstages into V-clamp
+Function setVClampMode(panelTitle)
+	string panelTitle
+	
+	WAVE statusHS = DC_ControlStatusWaveCache(panelTitle, CHANNEL_TYPE_HEADSTAGE)
+	
+	variable i
+	
+	for (i=0; i<NUM_HEADSTAGES; i+=1)
+		if (statusHS[i] == 1)
+			PGC_SetAndActivateControl(panelTitle,"slider_DataAcq_ActiveHeadstage", val = i)
+			PGC_SetAndActivateControl(panelTitle,"Radio_ClampMode_0", val = 1)
+		endif
+	endfor
+End
+
+// change holding potential on active headstages to Vm2 after X/2 number of data sweeps. If X!/2 switchSweep = floor(X/2)
+Function switchHolding(panelTitle, Vm2)
+	string panelTitle
+	variable Vm2
+	
+	variable numSweeps = GetValDisplayAsNum(panelTitle,"valdisp_DataAcq_SweepsInSet")
+	variable switchSweep = floor(numSweeps/2)
+	
+	WAVE statusHS = DC_ControlStatusWaveCache(panelTitle, CHANNEL_TYPE_HEADSTAGE)
+	
+	WAVE GuiState = GetDA_EphysGuiStateNum(panelTitle)
+	
+	variable StimRemaining = GuiState[0][findDimLabel(GuiState,1,"valdisp_DataAcq_TrialsCountdown")]-1
+	
+	if (StimRemaining == switchSweep)
+		variable i
+		for (i=0; i<NUM_HEADSTAGES; i+=1)
+			if (statusHS[i] == 1)
+				PGC_SetAndActivateControl(panelTitle,"slider_DataAcq_ActiveHeadstage", val = i)
+				if (GuiState[i][1] == 0)
+					PGC_SetAndActivateControl(panelTitle,"setvar_DataAcq_Hold_VC", val = Vm2)
+				elseif (GuiState[i][1] == 1)
+					PGC_SetAndActivateControl(panelTitle,"setvar_DataAcq_Hold_IC", val = Vm2)
+				endif
+			endif
+		endfor
+		printf "Half-way through stim set, changing holding potential to: %d\r", Vm2  
+	endif
+	
+	return StimRemaining
+End
+
+// Get index of stim set from stim set list
+Function GetStimSet(stimSet)
+	string stimSet
+	
+	string StimSetList = ReturnListOfAllStimSets(CHANNEL_TYPE_DAC,"*DA*")
+	variable stimSetIndex = whichlistitem(stimSet,StimSetList)
+	
+	return stimSetIndex
+End

commit 7bcba0d090513d61b593aabe20d3a0a049887c05

Fix separate module compilation
We can not include MIES_AnalysisFunctions.ipf in the separate modules
anymore as it now relies on DC_ and DAP_ functions.

Instead we now factor out the analysis function prototypes which are
required for the wavebuilder module.

Bug introduced in 6828aa60 (Analysis functions to set initial stimulus set
parameters and to switch holding potential midway through a stimulus set.
Intended for multipatch pipeline use., 2016-10-26).

Original-CommitID: 5d802c1b7255fa0d393003b5a9ec12a8d9f3ad95

Packages/MIES/MIES_AnalysisFunctionPrototypes.ipf
@@ -1,0 +1,42 @@
+#pragma TextEncoding = "UTF-8"
+#pragma rtGlobals=3		// Use modern global access method and strict wave access.
+
+/// @file MIES_AnalysisFunctionPrototypes.ipf
+/// @brief __AF__ Analysis functions prototypes to be called during data acquisition
+///
+/// @sa MIES_AnalysisFunctions.ipf
+
+/// @deprecated Use AF_PROTO_ANALYSIS_FUNC_V2() instead
+///
+/// @param panelTitle  device
+/// @param eventType   eventType, one of @ref EVENT_TYPE_ANALYSIS_FUNCTIONS,
+///                    always compare `eventType` with the constants, never use the current numerical value directly
+/// @param ITCDataWave data wave (locked to prevent changes using `SetWaveLock`)
+/// @param headStage   active headstage index
+///
+/// @return ignored
+Function AF_PROTO_ANALYSIS_FUNC_V1(panelTitle, eventType, ITCDataWave, headStage)
+	string panelTitle
+	variable eventType
+	Wave ITCDataWave
+	variable headstage
+End
+
+/// @param panelTitle     device
+/// @param eventType      eventType, one of @ref EVENT_TYPE_ANALYSIS_FUNCTIONS,
+///                       always compare `eventType` with the constants, never use the current numerical value directly
+/// @param ITCDataWave    data wave (locked to prevent changes using `SetWaveLock`)
+/// @param headStage      active headstage index
+/// @param realDataLength number of rows in `ITCDataWave` with data, the total number of rows in `ITCDataWave` might be
+///                       higher due to alignment requirements of the data acquisition hardware
+///
+/// @return see @ref AnalysisFunctionEventDescriptionTable
+Function AF_PROTO_ANALYSIS_FUNC_V2(panelTitle, eventType, ITCDataWave, headStage, realDataLength)
+	string panelTitle
+	variable eventType
+	Wave ITCDataWave
+	variable headstage, realDataLength
+
+	// return value currently only honoured for `Pre DAQ` event
+	return 0
+End

Packages/MIES/MIES_AnalysisFunctions.ipf
@@ -4,7 +4,7 @@
 /// @file MIES_AnalysisFunctions.ipf
 /// @brief __AF__ Analysis functions to be called during data acquisition
 ///
-/// Function prototypes for analysis functions
+/// @sa MIES_AnalysisFunctionPrototypes.ipf
 ///
 /// Users can implement functions which are called at certain events for each
 /// data acquisition cycle. These functions should *never* abort, error out with a runtime error, or open dialogs!
@@ -37,41 +37,6 @@ static Constant ITI_LOCAL         = 15                      ///< Inter-trial-int
 static Constant POST_DELAY = 150									 ///< Delay after stimulation event in which no other event can occur in ms
 static Constant RESOLUTION = 25									 ///< Resolution of oodDAQ protocol in ms
 ///@}
-
-/// @deprecated Use AF_PROTO_ANALYSIS_FUNC_V2() instead
-///
-/// @param panelTitle  device
-/// @param eventType   eventType, one of @ref EVENT_TYPE_ANALYSIS_FUNCTIONS,
-///                    always compare `eventType` with the constants, never use the current numerical value directly
-/// @param ITCDataWave data wave (locked to prevent changes using `SetWaveLock`)
-/// @param headStage   active headstage index
-///
-/// @return ignored
-Function AF_PROTO_ANALYSIS_FUNC_V1(panelTitle, eventType, ITCDataWave, headStage)
-	string panelTitle
-	variable eventType
-	Wave ITCDataWave
-	variable headstage
-End
-
-/// @param panelTitle     device
-/// @param eventType      eventType, one of @ref EVENT_TYPE_ANALYSIS_FUNCTIONS,
-///                       always compare `eventType` with the constants, never use the current numerical value directly
-/// @param ITCDataWave    data wave (locked to prevent changes using `SetWaveLock`)
-/// @param headStage      active headstage index
-/// @param realDataLength number of rows in `ITCDataWave` with data, the total number of rows in `ITCDataWave` might be
-///                       higher due to alignment requirements of the data acquisition hardware
-///
-/// @return see @ref AnalysisFunctionEventDescriptionTable
-Function AF_PROTO_ANALYSIS_FUNC_V2(panelTitle, eventType, ITCDataWave, headStage, realDataLength)
-	string panelTitle
-	variable eventType
-	Wave ITCDataWave
-	variable headstage, realDataLength
-
-	// return value currently only honoured for `Pre DAQ` event
-	return 0
-End
 
 Function TestAnalysisFunction_V1(panelTitle, eventType, ITCDataWave, headStage)
 	string panelTitle

Packages/MIES/MIES_WaveBuilderPanel.ipf
@@ -16,8 +16,8 @@
 #include ":..:ZeroMQ:procedures:ZeroMQ_Interop"
 
 // our includes
-#include ":MIES_AnalysisFunctions"
 #include ":MIES_AnalysisFunctionHelpers"
+#include ":MIES_AnalysisFunctionPrototypes"
 #include ":MIES_ArtefactRemoval"
 #include ":MIES_Constants"
 #include ":MIES_Debugging"

Packages/MIES_Include.ipf
@@ -26,6 +26,7 @@
 #include "MIES_AnalysisBrowser_LabNotebookTPStorageBrowser"
 #include "MIES_AnalysisBrowser_SweepBrowser"
 #include "MIES_AnalysisFunctionHelpers"
+#include "MIES_AnalysisFunctionPrototypes"
 #include "MIES_AnalysisFunctions"
 #include "MIES_AnalysisMaster"
 #include "MIES_ArtefactRemoval"