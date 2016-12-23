da5f73b396e31cae773ce4eb588d9bec645fb2fb
Thomas Braun <thomas.braun@byte-physics.de>
2016-12-23 19:51:55 +0100

    Tools: Add script for install the release package on the CI server
    Original-CommitID: b1d332ecd17c5aa854800538fed7b5dcc7e022f6

---
+++ tools/compilation-testing/clean_mies_installation_from_release_package.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+# Perform a clean MIES installation on linux
+# Uses the files from the release package
+
+set -e
+
+git --version > /dev/null
+if [ $? -ne 0 ]
+then
+  echo "Could not find git executable"
+  exit 1
+fi
+
+top_level=$(git rev-parse --show-toplevel)
+
+if [ ! -d "$top_level" ]
+then
+  echo "Could not find git repository"
+  exit 1
+fi
+
+rm -rf ~/WaveMetrics
+
+user_proc="$HOME/WaveMetrics/Igor Pro 7 User Files/User Procedures"
+igor_proc="$HOME/WaveMetrics/Igor Pro 7 User Files/Igor Procedures"
+xops="$HOME/WaveMetrics/Igor Pro 7 User Files/Igor Extensions"
+xops_help="$HOME/WaveMetrics/Igor Pro 7 User Files/Igor Help Files"
+
+mkdir -p "$user_proc"
+mkdir -p "$igor_proc"
+mkdir -p "$xops"
+mkdir -p "$xops_help"
+
+# install unit-testing package
+cp -r  "$top_level"/Packages/unit-testing "$user_proc"
+
+release_pkg=`ls Release*.zip`
+
+if [ ! -e "$release_pkg" ]
+then
+  echo "Could not find a release package"
+  exit 1
+fi
+
+folder=release_zip_extracted
+
+rm -rf $folder
+
+# install files from release package
+unzip "$release_pkg" -d $folder
+
+rm -rf "$folder"/Packages/doc/html
+cp -r  "$folder"/Packages/*  "$user_proc"
+cp -r  "$folder"/XOPs-IP7/*  "$xops"
+cp -r  "$folder"/XOP-tango/* "$xops"
+cp -r  "$folder"/HelpFiles-IP7/* "$xops_help"
+
+# rename DLL, wine seems to be pickier than windows itself
+mv "$xops"/ITCMM32.dll "$xops"/ITCMM.dll
+
+exit 0
