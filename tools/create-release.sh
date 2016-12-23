AllenInstitute/MIES

commit c04929461e3fa7634b17a8075b6b80025cc101e2

tools/create-release.sh: Don't add ZeroMQ
Original-CommitID: 8eae640fe5607af00e5e3fefc3578e432970b40b

.gitattributes
@@ -26,6 +26,7 @@ Packages/ITC          export-ignore
 Packages/doc          export-ignore
 Packages/Testing-MIES export-ignore
 Packages/unit-testing export-ignore
+Packages/ZeroMQ       export-ignore
 Guidelines            export-ignore
 NIDAQmx.XOP           export-ignore
 Packages/Arduino/*zip export-ignore

tools/create-release.sh
@@ -49,6 +49,7 @@ echo $version > "$version_file"
 "$zip_exe" -qd $version.zip "Packages/unit-testing/*" > /dev/null
 "$zip_exe" -qd $version.zip "Packages/Arduino/*.zip" > /dev/null
 "$zip_exe" -qd $version.zip "Packages/Arduino/*.exe" > /dev/null
+"$zip_exe" -qd $version.zip "Packages/ZeroMQ/*" > /dev/null
 "$zip_exe" -qd $version.zip "tools/*" > /dev/null
 "$zip_exe" -qd $version.zip "Guidelines/*" > /dev/null
 "$zip_exe" -qd $version.zip "XOPs/NIDAQmx.XOP" > /dev/null

commit 6aa8afdfa0e9a3698404208408e6a13599d147bd

Bugfix: Include required ZeroMQ procedures in release package
Bug introduced in c0492946 (tools/create-release.sh: Don't add ZeroMQ,
2016-11-06).

Original-CommitID: 8bfa1ad8e79a80be22f899f6a1f73b8f6c86ead9

.gitattributes
@@ -20,17 +20,19 @@
 *.tex eol=lf
 *.awk eol=lf
 
-*.sh                  export-ignore
-tools                 export-ignore
-Packages/ITC          export-ignore
-Packages/doc          export-ignore
-Packages/Testing-MIES export-ignore
-Packages/unit-testing export-ignore
-Packages/ZeroMQ       export-ignore
-Guidelines            export-ignore
-NIDAQmx.XOP           export-ignore
-Packages/Arduino/*zip export-ignore
-Packages/Arduino/*exe export-ignore
+*.sh                               export-ignore
+tools                              export-ignore
+Packages/ITC                       export-ignore
+Packages/doc                       export-ignore
+Packages/Testing-MIES              export-ignore
+Packages/unit-testing              export-ignore
+Packages/ZeroMQ/src                export-ignore
+Packages/ZeroMQ/tests              export-ignore
+Packages/ZeroMQ/xop-stub-generator export-ignore
+Guidelines                         export-ignore
+NIDAQmx.XOP                        export-ignore
+Packages/Arduino/*zip              export-ignore
+Packages/Arduino/*exe              export-ignore
 
 .git* export-ignore
 

tools/create-release.sh
@@ -49,7 +49,9 @@ echo $version > "$version_file"
 "$zip_exe" -qd $version.zip "Packages/unit-testing/*" > /dev/null
 "$zip_exe" -qd $version.zip "Packages/Arduino/*.zip" > /dev/null
 "$zip_exe" -qd $version.zip "Packages/Arduino/*.exe" > /dev/null
-"$zip_exe" -qd $version.zip "Packages/ZeroMQ/*" > /dev/null
+"$zip_exe" -qd $version.zip "Packages/ZeroMQ/src/*" > /dev/null
+"$zip_exe" -qd $version.zip "Packages/ZeroMQ/tests/*" > /dev/null
+"$zip_exe" -qd $version.zip "Packages/ZeroMQ/xop-stub-generator/*" > /dev/null
 "$zip_exe" -qd $version.zip "tools/*" > /dev/null
 "$zip_exe" -qd $version.zip "Guidelines/*" > /dev/null
 "$zip_exe" -qd $version.zip "XOPs/NIDAQmx.XOP" > /dev/null