AllenInstitute/MIES

commit e955f718ecdd19841106c2837ed8912e08fc5406

WaveBuilder: Introduce constants for the epoch types
Original-CommitID: e4cd523c1e9bedbb7a735c2788007590717765cd

Packages/MIES/MIES_WaveBuilderPanel.ipf
@@ -972,7 +972,7 @@ static Function WBP_UpdatePanelIfAllowed()
 	endif
 
 	switch(GetTabID(panel, "WBP_WaveType"))
-		case 2:
+		case EPOCH_TYPE_NOISE:
 			if(GetCheckBoxState(panel,"check_Noise_Pink_P41"))
 				SetCheckBoxState(panel,"Check_Noise_Brown_P42", 0)
 				DisableControl(panel, "Check_Noise_Brown_P42")
@@ -995,14 +995,14 @@ static Function WBP_UpdatePanelIfAllowed()
 				DisableControl(panel, "SetVar_WaveBuilder_P30")
 			endif
 			break
-		case 3:
+		case EPOCH_TYPE_SIN_COS:
 			if(GetCheckBoxState(panel,"check_Sin_Chirp_P43"))
 				EnableControls(panel, "SetVar_WaveBuilder_P24;SetVar_WaveBuilder_P25")
 			else
 				DisableControls(panel, "SetVar_WaveBuilder_P24;SetVar_WaveBuilder_P25")
 			endif
 			break
-		case 5:
+		case EPOCH_TYPE_SQUARE_PULSE:
 			if(GetCheckBoxState(panel,"check_SPT_NumPulses_P46"))
 				DisableControl(panel, "SetVar_WaveBuilder_P0")
 				EnableControls(panel, "SetVar_WaveBuilder_P45;SetVar_WaveBuilder_P47")
@@ -1011,7 +1011,7 @@ static Function WBP_UpdatePanelIfAllowed()
 				DisableControls(panel, "SetVar_WaveBuilder_P45;SetVar_WaveBuilder_P47")
 			endif
 			break
-		case 8:
+		case EPOCH_TYPE_COMBINE:
 			WB_UpdateEpochCombineList(WBP_GetOutputType())
 			break
 		default:
@@ -1062,7 +1062,7 @@ static Function WBP_ParameterWaveToPanel(stimulusType)
 		SetSetVariableString(panel, control, data)
 	endfor
 
-	if(stimulusType == 7)
+	if(stimulusType == EPOCH_TYPE_CUSTOM)
 		customWaveName = WPT[0][segment]
 		WAVE/Z customWave = $customWaveName
 		if(WaveExists(customWave))
@@ -1234,11 +1234,11 @@ Function WBP_FinalTabHook(tca)
 
 	variable tabID = GetTabID(panel, "WBP_WaveType")
 
-	if(tabID != 5)
+	if(tabID != EPOCH_TYPE_SQUARE_PULSE_TRAIN)
 		EnableControl(panel, "SetVar_WaveBuilder_P0")
 	endif
 
-	if(tabID == 7 || tabID == 8)
+	if(tabID == EPOCH_TYPE_CUSTOM || tabID == EPOCH_TYPE_COMBINE)
 		HideControls(tca.win, "SetVar_WaveBuilder_P0;SetVar_WaveBuilder_P1;SetVar_WaveBuilder_P2;SetVar_WaveBuilder_P3;SetVar_WB_DurDeltaMult_P52;SetVar_WB_AmpDeltaMult_P50;popup_WaveBuilder_exp_P40")
 	endif
 
@@ -1310,11 +1310,11 @@ Function WBP_UpdateControlAndWP(control, value)
 	paramRow     = WBP_ExtractRowNumberFromControl(control, "P")
 	WP[paramRow][epoch][stimulusType] = value
 
-	if(stimulusType == 2)
+	if(stimulusType == EPOCH_TYPE_NOISE)
 		WBP_LowPassDeltaLimits()
 		WBP_HighPassDeltaLimits()
 		WBP_CutOffCrossOver()
-	elseif(stimulusType == 5)
+	elseif(tabID == EPOCH_TYPE_SQUARE_PULSE_TRAIN)
 		maxDuration = WBP_ReturnPulseDurationMax()
 		SetVariable SetVar_WaveBuilder_P8 win=$panel, limits = {0, maxDuration, 0.1}
 		if(GetSetVariable(panel, "SetVar_WaveBuilder_P8") > maxDuration)
@@ -1972,11 +1972,11 @@ Function WBP_CheckProc_PreventUpdate(ctrlName,checked) : CheckBoxControl
 
 	if(!checked)
 		tabID = GetTabID(panel, "WBP_WaveType")
-		if(tabID == 2)
+		if(tabID == EPOCH_TYPE_NOISE)
 			WBP_LowPassDeltaLimits()
 			WBP_HighPassDeltaLimits()
 			WBP_CutOffCrossOver()
-		elseif(tabID == 5)
+		elseif(tabID == EPOCH_TYPE_SQUARE_PULSE_TRAIN)
 			maxDur = WBP_ReturnPulseDurationMax()
 			SetVariable SetVar_WaveBuilder_P8 win=$panel, limits = {0, maxDur, 0.1}
 			if(GetSetVariable(panel, "SetVar_WaveBuilder_P8") > maxDur)

Packages/MIES/MIES_Constants.ipf
@@ -529,3 +529,18 @@ Constant PRESSURE_METHOD_MANUAL   = 4
 /// @}
 
 StrConstant POPUPMENU_DIVIDER = "\\M1(-"
+
+/// @name Constants for different WaveBuilder epochs
+/// Numbers are stored in the SegWvType waves, so they are part of our "API".
+/// @anchor WaveBuilderEpochTypes
+/// @{
+Constant EPOCH_TYPE_SQUARE_PULSE       = 0
+Constant EPOCH_TYPE_RAMP               = 1
+Constant EPOCH_TYPE_NOISE              = 2
+Constant EPOCH_TYPE_SIN_COS            = 3
+Constant EPOCH_TYPE_SAW_TOOTH          = 4
+Constant EPOCH_TYPE_SQUARE_PULSE_TRAIN = 5
+Constant EPOCH_TYPE_PSC                = 6
+Constant EPOCH_TYPE_CUSTOM             = 7
+Constant EPOCH_TYPE_COMBINE            = 8
+/// @}

Packages/MIES/MIES_WaveBuilder.ipf
@@ -425,7 +425,7 @@ static Function/WAVE WB_MakeWaveBuilderWave(WP, WPT, SegWvType, stepCount, numEp
 		endif
 
 		switch(type)
-			case 0:
+			case EPOCH_TYPE_SQUARE_PULSE:
 				WB_SquareSegment(params)
 				AddEntryIntoWaveNoteAsList(WaveBuilderWave, "Epoch"          , var=i)
 				AddEntryIntoWaveNoteAsList(WaveBuilderWave, "Type"           , str="Square pulse")
@@ -436,7 +436,7 @@ static Function/WAVE WB_MakeWaveBuilderWave(WP, WPT, SegWvType, stepCount, numEp
 				AddEntryIntoWaveNoteAsList(WaveBuilderWave, "Offset"         , var=params.Offset)
 				AddEntryIntoWaveNoteAsList(WaveBuilderWave, "Delta offset"   , var=params.DeltaOffset, appendCR=1)
 				break
-			case 1:
+			case EPOCH_TYPE_RAMP:
 				WB_RampSegment(params)
 				AddEntryIntoWaveNoteAsList(WaveBuilderWave, "Epoch"          , var=i)
 				AddEntryIntoWaveNoteAsList(WaveBuilderWave, "Type"           , str="Ramp")
@@ -447,7 +447,7 @@ static Function/WAVE WB_MakeWaveBuilderWave(WP, WPT, SegWvType, stepCount, numEp
 				AddEntryIntoWaveNoteAsList(WaveBuilderWave, "Offset"         , var=params.Offset)
 				AddEntryIntoWaveNoteAsList(WaveBuilderWave, "Delta offset"   , var=params.DeltaOffset, appendCR=1)
 				break
-			case 2:
+			case EPOCH_TYPE_NOISE:
 				// initialize the random seed value if not already done
 				if(WP[48][i][type] == 0)
 					WP[48][i][type] = GetNonReproducibleRandom()
@@ -483,7 +483,7 @@ static Function/WAVE WB_MakeWaveBuilderWave(WP, WPT, SegWvType, stepCount, numEp
 				AddEntryIntoWaveNoteAsList(WaveBuilderWave, "Random seed"            , var=params.randomSeed)
 				AddEntryIntoWaveNoteAsList(WaveBuilderWave, "Delta offset"           , var=params.DeltaOffset, appendCR=1)
 				break
-			case 3:
+			case EPOCH_TYPE_SIN_COS:
 				WB_TrigSegment(params)
 				AddEntryIntoWaveNoteAsList(WaveBuilderWave, "Epoch"              , var=i)
 				AddEntryIntoWaveNoteAsList(WaveBuilderWave, "Type"               , str="Sin Wave")
@@ -492,7 +492,7 @@ static Function/WAVE WB_MakeWaveBuilderWave(WP, WPT, SegWvType, stepCount, numEp
 				AddEntryIntoWaveNoteAsList(WaveBuilderWave, "End frequency"      , var=params.EndFrequency)
 				AddEntryIntoWaveNoteAsList(WaveBuilderWave, "End frequency delta", var=params.DeltaEndFrequency, appendCR=1)
 				break
-			case 4:
+			case EPOCH_TYPE_SAW_TOOTH:
 				WB_SawToothSegment(params)
 				AddEntryIntoWaveNoteAsList(WaveBuilderWave, "Epoch"          , var=i)
 				AddEntryIntoWaveNoteAsList(WaveBuilderWave, "Type"           , str="Saw tooth")
@@ -501,16 +501,16 @@ static Function/WAVE WB_MakeWaveBuilderWave(WP, WPT, SegWvType, stepCount, numEp
 				AddEntryIntoWaveNoteAsList(WaveBuilderWave, "Offset"         , var=params.Offset)
 				AddEntryIntoWaveNoteAsList(WaveBuilderWave, "Delta offset"   , var=params.DeltaOffset, appendCR=1)
 				break
-			case 5:
+			case EPOCH_TYPE_SQUARE_PULSE_TRAIN:
 				if(WP[46][i][type]) // "Number of pulses" checkbox
 					WB_SquarePulseTrainSegment(params, SQUARE_PULSE_TRAIN_MODE_PULSE)
-					if(windowExists("WaveBuilder") && GetTabID("WaveBuilder", "WBP_WaveType") == 5)
+					if(windowExists("WaveBuilder") && GetTabID("WaveBuilder", "WBP_WaveType") == EPOCH_TYPE_SQUARE_PULSE_TRAIN)
 						WBP_UpdateControlAndWP("SetVar_WaveBuilder_P0", params.duration)
 					endif
 					defMode = "Pulse"
 				else
 					WB_SquarePulseTrainSegment(params, SQUARE_PULSE_TRAIN_MODE_DUR)
-					if(windowExists("WaveBuilder") && GetTabID("WaveBuilder", "WBP_WaveType") == 5)
+					if(windowExists("WaveBuilder") && GetTabID("WaveBuilder", "WBP_WaveType") == EPOCH_TYPE_SQUARE_PULSE_TRAIN)
 						WBP_UpdateControlAndWP("SetVar_WaveBuilder_P45", params.numberOfPulses)
 					endif
 					defMode = "Duration"
@@ -527,7 +527,7 @@ static Function/WAVE WB_MakeWaveBuilderWave(WP, WPT, SegWvType, stepCount, numEp
 				AddEntryIntoWaveNoteAsList(WaveBuilderWave, "Number of pulses"    , var=params.NumberOfPulses)
 				AddEntryIntoWaveNoteAsList(WaveBuilderWave, "Definition mode"     , str=defMode, appendCR=1)
 				break
-			case 6:
+			case EPOCH_TYPE_PSC:
 				WB_PSCSegment(params)
 				AddEntryIntoWaveNoteAsList(WaveBuilderWave, "Epoch"             , var=i)
 				AddEntryIntoWaveNoteAsList(WaveBuilderWave, "Type"              , str="PSC")
@@ -538,7 +538,7 @@ static Function/WAVE WB_MakeWaveBuilderWave(WP, WPT, SegWvType, stepCount, numEp
 				AddEntryIntoWaveNoteAsList(WaveBuilderWave, "Offset"            , var=params.Offset)
 				AddEntryIntoWaveNoteAsList(WaveBuilderWave, "Delta offset"      , var=params.DeltaOffset, appendCR=1)
 				break
-			case 7:
+			case EPOCH_TYPE_CUSTOM:
 				WAVE/Z customWave = $""
 				customWaveName = WPT[0][i]
 
@@ -579,7 +579,7 @@ static Function/WAVE WB_MakeWaveBuilderWave(WP, WPT, SegWvType, stepCount, numEp
 					printf "Failed to recreate custom wave epoch %d as the referenced wave %s is missing\r", i, customWaveName
 				endif
 				break
-			case 8:
+			case EPOCH_TYPE_COMBINE:
 				WAVE segmentWave = WB_GetSegmentWave(0)
 
 				formula_for_note = WPT[6][i]

Packages/MIES/MIES_WaveDataFolderGetters.ipf
@@ -2396,7 +2396,7 @@ End
 /// @brief Returns the segment type wave used by the wave builder panel
 /// Remember to change #SEGMENT_TYPE_WAVE_LAST_IDX if changing the wave layout
 /// - Rows
-///   - 0 - 97: epoch types using the tabcontrol indizes
+///   - 0 - 97: epoch types using one of @ref WaveBuilderEpochTypes
 ///   - 98: Data flipping (1 or 0)
 ///   - 99: set ITI (s)
 ///   - 100: total number of segments/epochs

commit 2a220f488fbc090d12b95a479f17e0351be0bebb

WaveBuilderPanel/Square Pulse Train: Fix pulse/duration control enabling logic
Bug introduced in e955f718 (WaveBuilder: Introduce constants for the epoch
types, 2017-01-02).

Original-CommitID: b0813c0f437f350160d7a66a789c348376d50914

Packages/MIES/MIES_WaveBuilderPanel.ipf
@@ -973,7 +973,7 @@ static Function WBP_UpdatePanelIfAllowed()
 				DisableControls(panel, "SetVar_WaveBuilder_P24;SetVar_WaveBuilder_P25")
 			endif
 			break
-		case EPOCH_TYPE_SQUARE_PULSE:
+		case EPOCH_TYPE_SQUARE_PULSE_TRAIN:
 			if(GetCheckBoxState(panel,"check_SPT_NumPulses_P46"))
 				DisableControl(panel, "SetVar_WaveBuilder_P0")
 				EnableControls(panel, "SetVar_WaveBuilder_P45;SetVar_WaveBuilder_P47")