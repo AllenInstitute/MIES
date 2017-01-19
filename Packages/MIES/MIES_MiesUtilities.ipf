AllenInstitute/MIES

commit 10bde460d28254bda741cf7b986792c71d254aad

ReplaceWaveWithBackup: Extract code into GetBackupWave
Original-CommitID: c926060cd4bc27f9fa9fbda1fda2c946435eb894

Packages/MIES/MIES_MiesUtilities.ipf
@@ -2833,6 +2833,22 @@ Function/Wave CreateBackupWave(wv, [forceCreation])
 	return backup
 End
 
+/// @brief Return a wave reference to the possibly not existing backup wave
+Function/WAVE GetBackupWave(wv)
+	WAVE wv
+
+	string backupname
+
+	ASSERT(WaveExists(wv), "Found no original wave")
+
+	backupname = NameOfWave(wv) + WAVE_BACKUP_SUFFIX
+	DFREF dfr  = GetWavesDataFolderDFR(wv)
+
+	WAVE/Z/SDFR=dfr backup = $backupname
+
+	return backup
+End
+
 /// @brief Replace the wave wv with its backup. If possible the backup wave will be killed afterwards.
 ///
 /// @param wv                       wave to replace by its backup
@@ -2844,19 +2860,11 @@ Function/Wave ReplaceWaveWithBackup(wv, [nonExistingBackupIsFatal])
 	Wave wv
 	variable nonExistingBackupIsFatal
 
-	string backupname
-	dfref dfr
-
 	if(ParamIsDefault(nonExistingBackupIsFatal))
 		nonExistingBackupIsFatal = 1
 	endif
 
-	ASSERT(WaveExists(wv), "Found no original wave")
-
-	backupname = NameOfWave(wv) + WAVE_BACKUP_SUFFIX
-	dfr        = GetWavesDataFolderDFR(wv)
-
-	Wave/Z/SDFR=dfr backup = $backupname
+	WAVE backup = GetBackupWave(wv)
 
 	if(!WaveExists(backup))
 		if(nonExistingBackupIsFatal)

commit 17f5b99bff40c8f2631957983997a3066dd310c7

ReplaceWaveWithBackup: Handle non existing backup wave
Bug introduced in 10bde460 (ReplaceWaveWithBackup: Extract code into
GetBackupWave, 2017-01-17).

Original-CommitID: a33034840eb241f5364be0e6f952cab7ee67d20f

Packages/MIES/MIES_MiesUtilities.ipf
@@ -2864,7 +2864,7 @@ Function/Wave ReplaceWaveWithBackup(wv, [nonExistingBackupIsFatal])
 		nonExistingBackupIsFatal = 1
 	endif
 
-	WAVE backup = GetBackupWave(wv)
+	WAVE/Z backup = GetBackupWave(wv)
 
 	if(!WaveExists(backup))
 		if(nonExistingBackupIsFatal)