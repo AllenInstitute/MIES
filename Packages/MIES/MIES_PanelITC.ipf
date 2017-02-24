AllenInstitute/MIES

commit c4518cee4f8307cba6166eef6d3b939f4f89bdc8

DAQ: Rework count checking logic
Introduce a new function RA_IsFirstSweep to capture our questionable usage
of count being NaN.

Clarify the documentation of GetCount.

Original-CommitID: d89f62e0d2dca3f8fccec175f2e8a60c92ca98d2

Packages/MIES/MIES_PanelITC.ipf
@@ -6012,9 +6012,8 @@ Function DAP_StopOngoingDataAcquisition(panelTitle)
 		needsOTCAfterDAQ = needsOTCAfterDAQ | 1
 	else
 		// force a stop if invoked during a 'down' time, with nothing happening.
-		NVAR count = $GetCount(panelTitle)
-
-		if(IsFinite(count))
+		if(!RA_IsFirstSweep(panelTitle))
+			NVAR count = $GetCount(panelTitle)
 			count = GetValDisplayAsNum(panelTitle, "valdisp_DataAcq_SweepsInSet")
 			needsOTCAfterDAQ = needsOTCAfterDAQ | 1
 		endif

Packages/MIES/MIES_BackgroundMD.ipf
@@ -130,13 +130,11 @@ static Function ITC_StopDataAcqMD(panelTitle, ITCDeviceIDGlobal)
 	String panelTitle
 	Variable ITCDeviceIDGlobal
 
-	NVAR count = $GetCount(panelTitle)
-
 	HW_SelectDevice(HARDWARE_ITC_DAC, ITCDeviceIDGlobal, flags=HARDWARE_ABORT_ON_ERROR)
 	HW_StopAcq(HARDWARE_ITC_DAC, ITCDeviceIDGlobal, prepareForDAQ=1)
 
 	DM_SaveAndScaleITCData(panelTitle)
-	if(!IsFinite(count))
+	if(RA_IsFirstSweep(panelTitle))
 		if(GetCheckboxState(panelTitle, "Check_DataAcq1_RepeatAcq"))
 			RA_YokedRAStartMD(panelTitle)
 		else

Packages/MIES/MIES_DataAcqITC.ipf
@@ -59,8 +59,7 @@ Function ITC_StopDataAcq()
 
 	DM_SaveAndScaleITCData(panelTitleG)
 
-	NVAR count = $GetCount(panelTitleG)
-	if(!IsFinite(count))
+	if(RA_IsFirstSweep(panelTitleG))
 		if(GetCheckboxState(panelTitleG, "Check_DataAcq1_RepeatAcq"))
 			RA_Start(PanelTitleG)
 		else
@@ -201,8 +200,7 @@ Function ITC_TestPulseFunc(s)
 		SCOPE_UpdateGraph(panelTitle)
 	endif
 
-	NVAR count = $GetCount(panelTitle)
-	if(!IsFinite(count))
+	if(RA_IsFirstSweep(panelTitle))
 		if(GetKeyState(0) & ESCAPE_KEY)
 			beep
 			ITC_StopTestPulseSingleDevice(panelTitle)

Packages/MIES/MIES_DataConfiguratorITC.ipf
@@ -1151,14 +1151,14 @@ static Function/C DC_CalculateChannelColumnNo(panelTitle, SetName, channelNo, ch
 	string sequenceWaveName
 
 	DFREF devicePath = GetDevicePath(panelTitle)
-	NVAR count = $GetCount(panelTitle)
 
 	// wave exists only if random set sequence is selected
 	sequenceWaveName = SetName + num2str(channelType) + num2str(channelNo) + "_S"
 	WAVE/Z/SDFR=devicePath WorkingSequenceWave = $sequenceWaveName
 
 	// Below code calculates the variable local count which is then used to determine what column to select from a particular set
-	if(IsFinite(count))
+	if(!RA_IsFirstSweep(panelTitle))
+		NVAR count = $GetCount(panelTitle)
 		//thus the vairable "count" is used to determine if acquisition is on the first cycle
 		ControlInfo/W=$panelTitle Check_DataAcq_Indexing // check indexing status
 		if(v_value == 0)// if indexing is off...
@@ -1202,7 +1202,7 @@ static Function/C DC_CalculateChannelColumnNo(panelTitle, SetName, channelNo, ch
 				cycleCount = 1
 			endif
 		endif
-	else
+	else // first sweep
 		ControlInfo/W=$panelTitle check_DataAcq_RepAcqRandom
 		if(v_value == 0) // set step sequence is not random
 			column = 0

Packages/MIES/MIES_GlobalStringAndVariableAccess.ipf
@@ -190,8 +190,8 @@ End
 /// @brief Returns the absolute path to the global variable `count` storing the
 ///        number of data acquisition still left to perform.
 ///
-///        The initial value of NaN has the same semantics as the previous non existence
-///        of this variable. Both meaning that no data acquisition is done right now.
+/// If the return value is NaN it means either that this is the very
+/// first sweep or that no DAQ is ongoing.
 Function/S GetCount(panelTitle)
 	string panelTitle
 

Packages/MIES/MIES_RepeatedAcquisition.ipf
@@ -395,3 +395,13 @@ Function RA_YokedRABckgTPCallRACounter(panelTitle)
 		RA_BckgTPwithCallToRACounterMD(ITC1600_FIRST_DEVICE)
 	endif
 End
+
+/// @brief Return one if we are acquiring currently the very first sweep of a
+///        possible repeated acquisition cycle. Zero means that we acquire a later
+///        sweep than the first one in a repeated acquisition cycle.
+Function RA_IsFirstSweep(panelTitle)
+	string panelTitle
+
+	NVAR count = $GetCount(panelTitle)
+	return !IsFinite(count)
+End

Packages/MIES/MIES_TPBackgroundMD.ipf
@@ -147,8 +147,7 @@ Function ITC_BkrdTPFuncMD(s)
 			SCOPE_UpdateGraph(panelTitle)
 		endif
 
-		NVAR count = $GetCount(panelTitle)
-		if(!IsFinite(count))
+		if(RA_IsFirstSweep(panelTitle))
 			if(GetKeyState(0) & ESCAPE_KEY)
 				// only stop the currently active device
 				if(!cmpstr(panelTitle,GetMainWindow(GetCurrentWindow())))

commit 977b9b9a8a4315e36c644bee1768a68188dc2c62

DAP_OneTimeCallAfterDAQ: Fix "TP after DAQ"
We need to set the count variable to NaN after DAQ as otherwise we hit the
recursion assertion.

Bug introduced in c4518cee (DAQ: Rework count checking logic, 2017-01-09).

Original-CommitID: 4df2508e319b4e3addc84b028aa67a57ed44654c

Packages/MIES/MIES_PanelITC.ipf
@@ -4384,6 +4384,9 @@ Function DAP_OneTimeCallAfterDAQ(panelTitle)
 	NVAR dataAcqRunMode = $GetDataAcqRunMode(panelTitle)
 	dataAcqRunMode = DAQ_NOT_RUNNING
 
+	NVAR count = $GetCount(panelTitle)
+	count = NaN
+
 	// restore the selected sets before DAQ
 	if(GetCheckBoxState(panelTitle, "Check_DataAcq_Indexing"))
 		IDX_ResetStartFinishForIndexing(panelTitle)