AllenInstitute/MIES

commit 66d64ce103654a1509de6b24fb4447cb5cf4f2e2

Add rtFunctionErrors pragma
Original-CommitID: 4c56207403e174ff99f6fb267d82772216397e95

Packages/MIES/MIES_Downsample.ipf
@@ -1,5 +1,6 @@
 #pragma TextEncoding = "UTF-8"
 #pragma rtGlobals=3
+#pragma rtFunctionErrors=1
 #pragma igorVersion=7.0
 
 /// @file MIES_Downsample.ipf

Packages/MIES/MIES_AmplifierInteraction.ipf
@@ -1,5 +1,6 @@
 #pragma TextEncoding = "UTF-8"
 #pragma rtGlobals=3		// Use modern global access method and strict wave access.
+#pragma rtFunctionErrors=1
 
 /// @file MIES_AmplifierInteraction.ipf
 /// @brief __AI__ Interface with the Axon/MCC amplifiers

Packages/MIES/MIES_AnalysisBrowser.ipf
@@ -1,5 +1,6 @@
 #pragma TextEncoding = "UTF-8"
 #pragma rtGlobals=3		// Use modern global access method and strict wave access.
+#pragma rtFunctionErrors=1
 #pragma igorVersion=7.0
 
 /// @file MIES_AnalysisBrowser.ipf

Packages/MIES/MIES_AnalysisBrowser_LabNotebookTPStorageBrowser.ipf
@@ -1,5 +1,6 @@
 #pragma TextEncoding = "UTF-8"
 #pragma rtGlobals=3		// Use modern global access method and strict wave access.
+#pragma rtFunctionErrors=1
 
 /// @file MIES_AnalysisBrowser_LabNotebookTPStorageBrowser.ipf
 /// @brief __LBN__ Panels for browsing the labnotebook and the TPStorage waves

Packages/MIES/MIES_AnalysisBrowser_SweepBrowser.ipf
@@ -1,5 +1,6 @@
 #pragma TextEncoding = "UTF-8"
 #pragma rtGlobals=3		// Use modern global access method and strict wave access.
+#pragma rtFunctionErrors=1
 
 /// @file MIES_AnalysisBrowser_SweepBrowser.ipf
 /// @brief __SB__  Visualization of sweep data in the analysis browser

Packages/MIES/MIES_AnalysisFunctionHelpers.ipf
@@ -1,5 +1,6 @@
 #pragma TextEncoding = "UTF-8"
 #pragma rtGlobals=3		// Use modern global access method and strict wave access.
+#pragma rtFunctionErrors=1
 
 /// @file MIES_AnalysisFunctionHelpers.ipf
 /// @brief __AFH__ Helper functions for analysis function writers

Packages/MIES/MIES_AnalysisFunctionPrototypes.ipf
@@ -1,5 +1,6 @@
 #pragma TextEncoding = "UTF-8"
 #pragma rtGlobals=3		// Use modern global access method and strict wave access.
+#pragma rtFunctionErrors=1
 
 /// @file MIES_AnalysisFunctionPrototypes.ipf
 /// @brief __AF__ Analysis functions prototypes to be called during data acquisition

Packages/MIES/MIES_AnalysisFunctions.ipf
@@ -1,5 +1,6 @@
 #pragma TextEncoding = "UTF-8"
 #pragma rtGlobals=3		// Use modern global access method and strict wave access.
+#pragma rtFunctionErrors=1
 
 /// @file MIES_AnalysisFunctions.ipf
 /// @brief __AF__ Analysis functions to be called during data acquisition

commit dea3eb138de368c0cc7c270b92623b455dbcaedb

Downsample: Avoid erroring out on invalid target rate
Broken since 66d64ce1 (Add rtFunctionErrors pragma, 2017-01-04).
While we are at it we also pull out the logic into GetTargetRate.

Original-CommitID: 154d8f27f9926c729c16307525d5647a8254039d

Packages/MIES/MIES_Downsample.ipf
@@ -290,6 +290,20 @@ static Function UpdateCurrentSize(win)
 	SetValDisplay(win, valdisp_currentsize, var=size, format="%3.0f")
 End
 
+static Function GetTargetRate(win)
+	string win
+
+	string str
+
+	str = GetPopupMenuString(win, popup_targetrate)
+
+	if(!cmpstr(str, NONE))
+		return NaN
+	endif
+
+	return str2num(str)
+End
+
 static Function UpdateEstimatedSizeAfterwards(win)
 	string win
 
@@ -297,8 +311,8 @@ static Function UpdateEstimatedSizeAfterwards(win)
 	Wave      rate    = GetDownsampleRateWave()
 	variable i, size = 0
 	variable numWaves = DimSize(dataRef, ROWS)
+	variable targetRate = GetTargetRate(win)
 
-	variable targetRate = str2num(GetPopupMenuString(win, popup_targetrate))
 	for(i=0;i<numWaves;i+=1)
 		size += GetWaveSize(dataRef[i]) * targetRate / rate[i]
 	endfor
@@ -622,7 +636,7 @@ Function ButtonDoIt(ba) : ButtonControl
 			ASSERT(DimSize(dataRef, ROWS) == DimSize(rate, ROWS), "Unmatched wave sizes")
 			ASSERT(DimSize(list, ROWS)    == DimSize(rate, ROWS), "Unmatched wave sizes")
 
-			targetRate = str2num(GetPopupMenuString(win, popup_targetrate))
+			targetRate = GetTargetRate(win)
 
 			numWaves = DimSize(dataRef, ROWS)
 			for(i=0;i < numWaves; i+=1)