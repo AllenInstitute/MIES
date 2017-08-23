da206347a35c4edace9da25827768b9d62a04bbc
Thomas Braun <thomas.braun@byte-physics.de>
2017-08-23 20:37:19 +0200

    CIS: Sync startup script
    env has problems with expanding ~.
    
    Original-CommitID: bb2d6dcfa7df90b5668e8fbdd6cd1b4fded74a26

--- tools/start_IP7_32bit.sh
+++ tools/start_IP7_32bit.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-env WINEPREFIX=~/.wine-igor wine C:/Program\ Files/WaveMetrics/Igor\ Pro\ 7\ Folder/IgorBinaries_Win32/Igor.exe
+env WINEPREFIX=$HOME/.wine-igor wine C:/Program\ Files/WaveMetrics/Igor\ Pro\ 7\ Folder/IgorBinaries_Win32/Igor.exe
