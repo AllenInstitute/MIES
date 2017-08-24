AllenInstitute/MIES

commit 5616cdc32b403fea0cf300b8c397b9c05cd790a9

DAP_CheckProc_IndexingState: Skip calling WBP_UpdateITCPanelPopUps
It does not make sense to call it only for this GUI control.

Original-CommitID: 0108698761b000b9fcd83667a08d101c8a04b6fa

Packages/MIES/MIES_PanelITC.ipf
@@ -4623,9 +4623,6 @@ Function DAP_CheckProc_IndexingState(cba) : CheckBoxControl
 	switch(cba.eventCode)
 		case 2: // mouse up
 
-			panelTitle = cba.win
-			// makes sure user data for controls is up to date
-			WBP_UpdateITCPanelPopUps(panelTitle=panelTitle)
 			DAP_UpdateDAQControls(panelTitle, REASON_STIMSET_CHANGE)
 
 			if(cmpstr(cba.ctrlname, "Check_DataAcq1_IndexingLocked") == 0)

commit 5f09476f3d9d07d774c9415e15428a14f62d607f

Indexing: Fix null string error
Introduced in 5616cdc3 (DAP_CheckProc_IndexingState: Skip calling
WBP_UpdateITCPanelPopUps, 2017-07-18).

Original-CommitID: 518a3bbf458ee4b13f27646882a59b5428dc5311

Packages/MIES/MIES_PanelITC.ipf
@@ -4651,6 +4651,7 @@ Function DAP_CheckProc_IndexingState(cba) : CheckBoxControl
 	switch(cba.eventCode)
 		case 2: // mouse up
 
+			panelTitle = cba.win
 			DAP_UpdateDAQControls(panelTitle, REASON_STIMSET_CHANGE)
 
 			if(cmpstr(cba.ctrlname, "Check_DataAcq1_IndexingLocked") == 0)