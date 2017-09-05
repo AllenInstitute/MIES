AllenInstitute/MIES

commit 66d64ce103654a1509de6b24fb4447cb5cf4f2e2

Add rtFunctionErrors pragma
Original-CommitID: 4c56207403e174ff99f6fb267d82772216397e95

Packages/MIES/MIES_WaveBuilderPanel.ipf
@@ -1,5 +1,6 @@
 #pragma TextEncoding = "UTF-8"
 #pragma rtGlobals=3		// Use modern global access method and strict wave access.
+#pragma rtFunctionErrors=1
 #pragma igorVersion=7.0
 
 /// @file MIES_WaveBuilderPanel.ipf

Packages/MIES/MIES_AmplifierInteraction.ipf
@@ -1,5 +1,6 @@
 #pragma TextEncoding = "UTF-8"
 #pragma rtGlobals=3		// Use modern global access method and strict wave access.
+#pragma rtFunctionErrors=1
 
 /// @file MIES_AmplifierInteraction.ipf
 /// @brief __AI__ Interface with the Axon/MCC amplifiers

Packages/MIES/MIES_AnalysisBrowser.ipf
@@ -1,5 +1,6 @@
 #pragma TextEncoding = "UTF-8"
 #pragma rtGlobals=3		// Use modern global access method and strict wave access.
+#pragma rtFunctionErrors=1
 #pragma igorVersion=7.0
 
 /// @file MIES_AnalysisBrowser.ipf

Packages/MIES/MIES_AnalysisBrowser_LabNotebookTPStorageBrowser.ipf
@@ -1,5 +1,6 @@
 #pragma TextEncoding = "UTF-8"
 #pragma rtGlobals=3		// Use modern global access method and strict wave access.
+#pragma rtFunctionErrors=1
 
 /// @file MIES_AnalysisBrowser_LabNotebookTPStorageBrowser.ipf
 /// @brief __LBN__ Panels for browsing the labnotebook and the TPStorage waves

Packages/MIES/MIES_AnalysisBrowser_SweepBrowser.ipf
@@ -1,5 +1,6 @@
 #pragma TextEncoding = "UTF-8"
 #pragma rtGlobals=3		// Use modern global access method and strict wave access.
+#pragma rtFunctionErrors=1
 
 /// @file MIES_AnalysisBrowser_SweepBrowser.ipf
 /// @brief __SB__  Visualization of sweep data in the analysis browser

Packages/MIES/MIES_AnalysisFunctionHelpers.ipf
@@ -1,5 +1,6 @@
 #pragma TextEncoding = "UTF-8"
 #pragma rtGlobals=3		// Use modern global access method and strict wave access.
+#pragma rtFunctionErrors=1
 
 /// @file MIES_AnalysisFunctionHelpers.ipf
 /// @brief __AFH__ Helper functions for analysis function writers

Packages/MIES/MIES_AnalysisFunctionPrototypes.ipf
@@ -1,5 +1,6 @@
 #pragma TextEncoding = "UTF-8"
 #pragma rtGlobals=3		// Use modern global access method and strict wave access.
+#pragma rtFunctionErrors=1
 
 /// @file MIES_AnalysisFunctionPrototypes.ipf
 /// @brief __AF__ Analysis functions prototypes to be called during data acquisition

Packages/MIES/MIES_AnalysisFunctions.ipf
@@ -1,5 +1,6 @@
 #pragma TextEncoding = "UTF-8"
 #pragma rtGlobals=3		// Use modern global access method and strict wave access.
+#pragma rtFunctionErrors=1
 
 /// @file MIES_AnalysisFunctions.ipf
 /// @brief __AF__ Analysis functions to be called during data acquisition

commit 5f60932db21ddb16707e8d797da201a7b01724cb

WBP_GetFolderPath: Avoid runtime error with rtFunctionErrors=1
Altough the code we use resembles the example of GetIndexedObjNameDFR
this leads to an runtime error with rtFunctionErrors enabled.

Broken since 66d64ce1 (Add rtFunctionErrors pragma, 2017-01-04) and
reported to WM.

Now we use our GetListOfObjects wrapper.

Original-CommitID: 7dcf55a14d03fe848aa03347412b6b7879de9741

Packages/MIES/MIES_WaveBuilderPanel.ipf
@@ -1878,22 +1878,9 @@ End
 
 Function/S WBP_ReturnFoldersList()
 
-	string parent
-	string folderNameList = ""
-	string folderName
-	variable i = 0
-
 	DFREF dfr = WBP_GetFolderPath()
-	do
-		folderName = GetIndexedObjNameDFR(dfr, 4, i)
-		if (strlen(folderName) == 0)
-			break
-		endif
-		folderNameList = AddListItem(folderName, folderNamelist, ";", inf)
-		i += 1
-	while(1)
 
-	return NONE + ";root:;..;" + folderNameList
+	return NONE + ";root:;..;" + GetListOfObjects(dfr, ".*", typeFlag = COUNTOBJECTS_DATAFOLDER)
 End
 
 Function WBP_PopMenuProc_FolderSelect(pa) : PopupMenuControl