AllenInstitute/MIES

commit 54c9128111e9de7f33cd7bded9e92ad5ddac8585

Add ControlWindowToFront() to all error messages
Original-CommitID: e5067e6203adb5d7d319e41109d6b12750e34074

Packages/MIES/MIES_DAC-Hardware.ipf
@@ -728,6 +728,8 @@ Function HW_ITC_HandleReturnValues(flags, ITCError, ITCXOPError)
 		printf "Complete call stack: %s\r", GetRTStackInfo(3)
 	endif
 
+	ControlWindowToFront()
+
 #ifndef EVIL_KITTEN_EATING_MODE
 	if(ITCXOPError != 0 || ITCError != 0)
 		ASSERT(!(flags & HARDWARE_ABORT_ON_ERROR), "DAC error")
@@ -1603,6 +1605,7 @@ Function HW_NI_ReadDigital(device, [DIOPort, DIOLine, flags])
 	DAQmx_DIO_Config/DEV=device/DIR=1/LGRP=(lineGrouping) line
 	if (GetRTError(1))
 		print fDAQmx_ErrorString()
+		ControlWindowToFront()
 		if(flags & HARDWARE_ABORT_ON_ERROR)
 			ASSERT(0, "Error calling DAQmx_DIO_Config")
 		endif
@@ -1617,6 +1620,7 @@ Function HW_NI_ReadDigital(device, [DIOPort, DIOLine, flags])
 	if(ret)
 		print fDAQmx_ErrorString()
 		printf "Error %d: fDAQmx_DIO_Finished\r", ret
+		ControlWindowToFront()
 		if(flags & HARDWARE_ABORT_ON_ERROR)
 			ASSERT(0, "Error calling fDAQmx_DIO_Finished")
 		endif
@@ -1661,6 +1665,7 @@ Function HW_NI_WriteDigital(device, value, [DIOPort, DIOLine, flags])
 	DAQmx_DIO_Config/DEV=device/DIR=1/LGRP=(lineGrouping) line
 	if (GetRTError(1))
 		print fDAQmx_ErrorString()
+		ControlWindowToFront()
 		if(flags & HARDWARE_ABORT_ON_ERROR)
 			ASSERT(0, "Error calling DAQmx_DIO_Config")
 		endif
@@ -1674,6 +1679,7 @@ Function HW_NI_WriteDigital(device, value, [DIOPort, DIOLine, flags])
 	if(ret)
 		print fDAQmx_ErrorString()
 		printf "Error %d: fDAQmx_DIO_Write\r", ret
+		ControlWindowToFront()
 		if(flags & HARDWARE_ABORT_ON_ERROR)
 			ASSERT(0, "Error calling fDAQmx_DIO_Write")
 		endif
@@ -1683,6 +1689,7 @@ Function HW_NI_WriteDigital(device, value, [DIOPort, DIOLine, flags])
 	if(ret)
 		print fDAQmx_ErrorString()
 		printf "Error %d: fDAQmx_DIO_Finished\r", ret
+		ControlWindowToFront()
 		if(flags & HARDWARE_ABORT_ON_ERROR)
 			ASSERT(0, "Error calling fDAQmx_DIO_Finished")
 		endif
@@ -1794,6 +1801,7 @@ Function HW_NI_ResetDevice(device, [flags])
 	if(ret)
 		print fDAQmx_ErrorString()
 		printf "Error %d: fDAQmx_resetDevice\r", ret
+		ControlWindowToFront()
 		if(flags & HARDWARE_ABORT_ON_ERROR)
 			ASSERT(0, "Error calling fDAQmx_DIO_Finished")
 		endif

Packages/MIES/MIES_AmplifierInteraction.ipf
@@ -546,6 +546,7 @@ Function AI_SendToAmp(panelTitle, headStage, mode, func, value, [checkBeforeWrit
 
 	if(!IsFinite(ret))
 		print "Amp communication error. Check associations in hardware tab and/or use Query connected amps button"
+		ControlWindowToFront()
 	endif
 
 	// return value is only relevant for the getters

Packages/MIES/MIES_AnalysisBrowser.ipf
@@ -2284,6 +2284,7 @@ Function AB_ButtonProc_SelectStimSets(ba) : ButtonControl
 
 			if(!WaveExists(indizes) || DimSize(indizes, ROWS) != 1)
 				print "Please select exactly one row to use this feature"
+				ControlWindowToFront()
 				break
 			endif
 
@@ -2291,6 +2292,7 @@ Function AB_ButtonProc_SelectStimSets(ba) : ButtonControl
 
 			if(isEmpty(selectedStimSet))
 				print "Can not work with an empty stim set in the selection"
+				ControlWindowToFront()
 				break
 			endif
 
@@ -2369,6 +2371,7 @@ Function AB_ButtonProc_OpenCommentNB(ba) : ButtonControl
 
 			if(!WaveExists(indizes) || DimSize(indizes, ROWS) != 1)
 				print "Please select a sweep belonging to a device to use this feature"
+				ControlWindowToFront()
 				break
 			endif
 
@@ -2378,6 +2381,7 @@ Function AB_ButtonProc_OpenCommentNB(ba) : ButtonControl
 			device = GetLastNonEmptyEntry(expBrowserList, "device", row)
 			if(isEmpty(device))
 				print "Please select a sweep belonging to a device to use this feature"
+				ControlWindowToFront()
 				break
 			endif
 

Packages/MIES/MIES_DataAcqITC.ipf
@@ -333,6 +333,7 @@ Function ITC_ApplyAutoBias(panelTitle, BaselineSSAvg, SSResistance)
 
 		if(!IsFinite(actualCurrent))
 			print "Queried amplifier current is non-finite"
+			ControlWindowToFront()
 			continue
 		endif
 
@@ -340,6 +341,7 @@ Function ITC_ApplyAutoBias(panelTitle, BaselineSSAvg, SSResistance)
 
 		if( abs(current) > maximumAutoBiasCurrent)
 			printf "Headstage %d: Not applying autobias current shot of %gA as that would exceed the maximum allowed current of %gA\r", headStage, current, maximumAutoBiasCurrent
+			ControlWindowToFront()
 			continue
 		endif
 
@@ -440,6 +442,7 @@ static Function ITC_SupportSystemAlarm(Channel, Measurement, MeasurementTitle, p
 		if(Measurement >= ParamMax || Measurement <= ParamMin)
 			beep
 			print time() + " !!!!!!!!!!!!! " + MeasurementTitle + " has exceeded max/min settings" + " !!!!!!!!!!!!!"
+			ControlWindowToFront()
 			beep
 		endif
 	endif

Packages/MIES/MIES_DataBrowser.ipf
@@ -132,6 +132,7 @@ static Function DB_LockDBPanel(panelTitle, device)
 		endif
 
 		print "Please choose a device assignment for the data browser"
+		ControlWindowToFront()
 		DoWindow/W=$panelTitle/C $panelTitleNew
 		SetWindow $panelTitleNew, userdata($MIES_PANEL_TYPE_USER_DATA) = ""
 		SetWindow $panelTitleNew, userdata(DataFolderPath) = ""
@@ -614,6 +615,7 @@ Function DB_DataBrowserStartupSettings()
 
 	if(!windowExists(panelTitle))
 		print "A panel named \"DataBrowser\" does not exist"
+		ControlWindowToFront()
 		return NaN
 	endif
 

Packages/MIES/MIES_Debugging.ipf
@@ -298,6 +298,7 @@ Function SaveHistoryLog()
 	DoWindow HistoryCarbonCopy
 	if(V_flag == 0)
 		print "No log notebook found, please call CreateHistoryLog() before."
+		ControlWindowToFront()
 		return NaN
 	endif
 

Packages/MIES/MIES_Downsample.ipf
@@ -586,6 +586,7 @@ Function ButtonRestoreBackup(ba) : ButtonControl
 
 			if(!success)
 				Print "Restoring backups failed. At least one wave did not have a backup."
+				ControlWindowToFront()
 			endif
 
 			UpdatePanel(win)

Packages/MIES/MIES_ExperimentConfig.ipf
@@ -125,6 +125,7 @@ Function ExpConfig_ConfigureMIES([middleOfExperiment])
 			PGC_SetAndActivateControl(win,GetPanelControl(CHANNEL_INDEX_ALL_V_CLAMP,CHANNEL_TYPE_DAC,CHANNEL_CONTROL_SCALE), val = str2numSafe(UserSettings[V_value][%SettingValue]))
 		else
 			print "Stim set failed to load, check file path"
+			ControlWindowToFront()
 		endif
 		
 		PGC_SetAndActivateControl(win,"ADC", val = DA_EPHYS_PANEL_DATA_ACQUISITION)

commit c4b76f52b606f9cabaefb63f836c5bcdebc0c632

HW_ITC_HandleReturnValues: Avoid bringing the control window to front unconditionally
Broken since 54c91281 (Add ControlWindowToFront() to all error messages,
2017-08-17).

Original-CommitID: a8e7a65399fb9961fed752f9484c659c4fd9377d

Packages/MIES/MIES_DAC-Hardware.ipf
@@ -728,10 +728,9 @@ Function HW_ITC_HandleReturnValues(flags, ITCError, ITCXOPError)
 		printf "Complete call stack: %s\r", GetRTStackInfo(3)
 	endif
 
-	ControlWindowToFront()
-
 #ifndef EVIL_KITTEN_EATING_MODE
 	if(ITCXOPError != 0 || ITCError != 0)
+		ControlWindowToFront()
 		ASSERT(!(flags & HARDWARE_ABORT_ON_ERROR), "DAC error")
 	endif
 