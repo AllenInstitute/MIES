AllenInstitute/MIES

commit 1f7cfa6591065b71f5d788fa9a51ea4f1af1ef2c

Make the baseline length of the testpulse configurable
Use the user given baseline length instead of using a fixed one of 25%
(single device) or 33 1/3% (multidevice).
Write baselineFrac, AmplitudeIC, AmplitudeVC, pulseduration into the
labnotebook after testpulse and DAQ (because of global TP insertion).

We don't need the calculation for the testpulse wave length in the multi device case
any more. The reason is that the old implementation only created waves larger than 2^17 if
the testpulse duration execeeded 2e5 ms. And this will never be the case.

Original-CommitID: 8a3ca5f10f33f2ec2e9fa2218f499d38e26b643e

Packages/MIES/MIES_TestPulse.ipf
@@ -123,8 +123,10 @@ end
 Function TP_UpdateGlobals(panelTitle)
 	string panelTitle
 
-	variable pulseDuration
 	DFREF testPulseDFR = GetDeviceTestPulse(panelTitle)
+
+	variable/G testPulseDFR:pulseDuration
+	NVAR/SDFR=testPulseDFR pulseDuration
 
 	variable/G testPulseDFR:duration
 	NVAR/SDFR=testPulseDFR duration
@@ -135,6 +137,9 @@ Function TP_UpdateGlobals(panelTitle)
 	variable/G testPulseDFR:AmplitudeIC
 	NVAR/SDFR=testPulseDFR AmplitudeIC
 
+	variable/G testPulseDFR:baselineFrac
+	NVAR/SDFR=testPulseDFR baselineFrac
+
 	WAVE ITCChanConfigWave = GetITCChanConfigWave(panelTitle)
 
 	string/G testPulseDFR:ADChannelList  = Convert1DWaveToList(GetADCListFromConfig(ITCChanConfigWave))
@@ -142,6 +147,7 @@ Function TP_UpdateGlobals(panelTitle)
 
 	pulseDuration = GetSetVariable(panelTitle, "SetVar_DataAcq_TPDuration")
 	duration = pulseDuration / (SI_CalculateMinSampInterval(panelTitle) / 1000)
+	baselineFrac = GetSetVariable(panelTitle, "SetVar_DataAcq_TPBaselinePerc") / 100
 
 	// need to deal with units here to ensure that resistance is calculated correctly
 	AmplitudeVC = GetSetVariable(panelTitle, "SetVar_DataAcq_TPAmplitude")
@@ -152,42 +158,71 @@ Function TP_UpdateGlobals(panelTitle)
 	n = GetSetVariable(panelTitle, "setvar_Settings_TPBuffer")
 End
 
-Function TP_UpdateTestPulseWave(TestPulse, panelTitle)
-	Wave TestPulse
+/// @brief Return the total length of a single testpulse with baseline
+///
+/// @param pulseDuration duration of the high portion of the testpulse in points or time
+/// @param baselineFrac  fraction, *not* percentage, of the baseline
+Function TP_CalculateTestPulseLength(pulseDuration, baselineFrac)
+	variable pulseDuration, baselineFrac
+
+	ASSERT(baselineFrac > 0 && baselineFrac < 0.5, "baselineFrac is out of range")
+	return pulseDuration / (1 - 2 * baselineFrac)
+End
+
+/// @brief Return the total length of a single testpulse with baseline, equal to one chunk for the MD case, in points
+Function TP_GetTestPulseLengthInPoints(panelTitle)
 	string panelTitle
 
-	variable pointsInTPWave, pulseDuration
+	NVAR duration     = $GetTestpulseDuration(panelTitle)
+	NVAR baselineFrac = $GetTestpulseBaselineFraction(panelTitle)
+
+	return TP_CalculateTestPulseLength(duration, baselineFrac)
+End
+
+Function TP_UpdateTestPulseWave(panelTitle, TestPulse)
+	string panelTitle
+	WAVE TestPulse
+
+	variable length
 	TP_UpdateGlobals(panelTitle)
 
 	DFREF testPulseDFR = GetDeviceTestPulse(panelTitle)
-	NVAR/SDFR=testPulseDFR amplitudeVC
+	NVAR/SDFR=testPulseDFR amplitudeVC, baselineFrac, pulseDuration
 
-	pulseDuration = GetSetVariable(panelTitle, "SetVar_DataAcq_TPDuration")
-	pointsInTPWave = (2 * pulseDuration) / MINIMUM_SAMPLING_INTERVAL
-	Redimension/N=(pointsInTPWave) TestPulse
+	// this length here is with minimum sampling interval, it will
+	// later be downsampled to match the return value of TP_GetTestPulseLengthInPoints
+	length = ceil(TP_CalculateTestPulseLength(pulseDuration , baselineFrac) / MINIMUM_SAMPLING_INTERVAL)
+	Redimension/N=(length) TestPulse
 	FastOp TestPulse = 0
-	TestPulse[round(0.25 * pointsInTPWave), round(0.75 * pointsInTPWave)] = amplitudeVC
+	// TP_SetDAScaleToOne adapts to the different clamp modes
+	// so setting it here unconditionally for V_CLAMP is correct
+	TestPulse[baselineFrac * length, (1 - baselineFrac) * length] = amplitudeVC
 End
 
 /// @brief MD-variant of #TP_UpdateTestPulseWave
-Function TP_UpdateTestPulseWaveChunks(TestPulse, panelTitle)
-	wave TestPulse
+Function TP_UpdateTestPulseWaveMD(panelTitle, TestPulse)
 	string panelTitle
+	WAVE TestPulse
 
-	variable frequency, pulseDuration
-	TP_UpdateGlobals(panelTitle)
-
+	variable length
 	DFREF testPulseDFR = GetDeviceTestPulse(panelTitle)
-	NVAR/SDFR=testPulseDFR amplitudeVC
+
+	Make/FREE singlePulse
+	TP_UpdateTestPulseWave(panelTitle, singlePulse)
 
 	variable/G testPulseDFR:TPPulseCount
 	NVAR/SDFR=testPulseDFR TPPulseCount
 
-	NVAR duration = $GetTestpulseDuration(panelTitle)
+	length = 2^MINIMUM_ITCDATAWAVE_EXPONENT
+	Redimension/N=0 TestPulse
 
-	pulseDuration = GetSetVariable(panelTitle, "SetVar_DataAcq_TPDuration")
-	frequency = 1000 / (pulseDuration * 2)
-	TPPulseCount = TP_CreateSquarePulseWave(panelTitle, frequency, amplitudeVC, TestPulse)
+	TPPulseCount = 0
+	do
+		Concatenate/NP=0 {singlePulse}, TestPulse
+		TPPulseCount += 1
+	while(DimSize(TestPulse, ROWS) < length)
+
+	Redimension/N=(length) TestPulse
 End
 
 /// @brief Start a single device test pulse, either in background
@@ -215,7 +250,7 @@ Function TP_StartTestPulseSingleDevice(panelTitle)
 
 	TP_UpdateGlobals(panelTitle)
 	WAVE TestPulse = GetTestPulse()
-	TP_UpdateTestPulseWave(TestPulse, panelTitle)
+	TP_UpdateTestPulseWave(panelTitle, TestPulse)
 
 	Make/FREE/N=8 SelectedDACWaveList
 	TP_StoreSelectedDACWaves(SelectedDACWaveList, panelTitle)
@@ -278,9 +313,9 @@ Function TP_Delta(panelTitle)
 	DFREF dfr = GetDeviceTestPulse(panelTitle)
 
 	WAVE/SDFR=dfr TestPulseITC
-	NVAR/SDFR=dfr durationG = duration
 	NVAR/SDFR=dfr amplitudeIC
 	NVAR/SDFR=dfr amplitudeVC
+	NVAR/SDFR=dfr baselineFrac
 	NVAR/SDFR=dfr noOfActiveDA
 	SVAR/SDFR=dfr clampModeString
 
@@ -289,13 +324,13 @@ Function TP_Delta(panelTitle)
 	amplitudeIC = abs(amplitudeIC)
 	amplitudeVC = abs(amplitudeVC)
 
-	variable duration = (durationG * 2 * deltaX(TestPulseITC)) // total duration of TP in ms
-	variable BaselineSteadyStateStartTime =(0.1 * duration)
-	variable BaselineSteadyStateEndTime = (0.24 * Duration)
-	variable TPSSEndTime = (0.74 * duration)
-	variable TPInstantaneouseOnsetTime = (0.252 * Duration)
 	variable DimOffsetVar = DimOffset(TestPulseITC, ROWS)
 	variable DimDeltaVar = DimDelta(TestPulseITC, ROWS)
+	variable duration = DimSize(TestPulseITC, ROWS) * DimDeltaVar // total duration of TP in ms
+	variable BaselineSteadyStateStartTime = 0.1 * duration
+	variable BaselineSteadyStateEndTime = (baselineFrac - 0.01) * duration
+	variable TPSSEndTime = (1 - (baselineFrac + 0.01)) * duration
+	variable TPInstantaneouseOnsetTime = (baselineFrac + 0.002) * duration
 	variable PointsInSteadyStatePeriod = (((BaselineSteadyStateEndTime - DimOffsetVar) / DimDeltaVar) - ((BaselineSteadyStateStartTime - DimOffsetVar) / DimDeltaVar))
 	variable BaselineSSStartPoint = ((BaselineSteadyStateStartTime - DimOffsetVar) / DimDeltaVar)
 	variable BaslineSSEndPoint = BaselineSSStartPoint + PointsInSteadyStatePeriod
@@ -660,50 +695,6 @@ Function TP_GetDAChannelFromHeadstage(panelTitle, headstage)
 		endif
 	endfor
 	return NaN
-End
-
-/// @brief Creates a square pulse wave where the duration of the pulse is equal to what the user inputs. The interpulse interval is twice the pulse duration.
-/// The interpulse is twice as long as the pulse to give the cell membrane sufficient time to recover between pulses
-static Function TP_CreateSquarePulseWave(panelTitle, Frequency, Amplitude, TPWave)
-	string 	panelTitle
-	variable 	frequency
-	variable 	amplitude
-	Wave 	TPWave
-	variable 	numberOfSquarePulses
-	variable  	longestSweepPoints = (((1000 / Frequency) * 2) / MINIMUM_SAMPLING_INTERVAL)  * (1 / (SI_CalculateMinSampInterval(panelTitle) / MINIMUM_SAMPLING_INTERVAL))
-	//print "longest sweep =", longestSweepPoints
-	variable 	exponent = ceil(log(longestSweepPoints)/log(2))
-	if(exponent < 17) // prevents FIFO underrun overrun errors by keepint the wave a minimum size
-		exponent = 17
-	endif 
-
-	make /FREE /n = (2 ^ exponent)  SinBuildWave
-	make /FREE /n = (2 ^ exponent)  CosBuildWave
-	make /FREE /n = (2 ^ exponent)  BuildWave
-
-	SetScale /P x 0, MINIMUM_SAMPLING_INTERVAL,  "ms", SinBuildWave
-	SetScale /P x 0, MINIMUM_SAMPLING_INTERVAL,  "ms", CosBuildWave
-	SetScale /P x 0, MINIMUM_SAMPLING_INTERVAL,  "ms", BuildWave
-	
-	Frequency /= 1.5
-	// the point offset is 1/4 of a cos wave cycle in points. The is used to make the baseline before the first pulse the same length as the interpulse interval
-	variable PointOffset = ((1 / Frequency) / 0.000005) * 0.25
-	Multithread SinBuildWave =  .49 * - sin(2 * Pi * (Frequency * 1000) * (5 / 1000000000) * (p + PointOffset))
-	Multithread CosBuildWave = 0.49 * - cos(2 * Pi * ((Frequency* 2) * 1000) * (5 / 1000000000) * (p + PointOffset))
-	Multithread BuildWave = SinBuildWave + CosBuildWave
-	Multithread BuildWave = Ceil(Buildwave)
-
-	duplicate /o BuildWave TPWave
-
-	TPWave *= Amplitude
-	FindLevels /Q BuildWave, 0.5
-	numberOfSquarePulses = V_LevelsFound
-	if(mod(numberOfSquarePulses, 2) == 0)
-		return (numberOfSquarePulses / 2) 
-	else
-		numberOfSquarePulses -= 1
-		return (numberOfSquarePulses / 2)
-	endif
 End
 
 /// @brief Returns the column of any of the TP results waves (TPBaseline, TPInstResistance, TPSSResistance) associated with a headstage.

Packages/MIES/MIES_DataAcqMgmt.ipf
@@ -436,12 +436,8 @@ static Function DAM_TPSetup(panelTitle)
 	TP_SetDAScaleToOne(panelTitle)
 
 	WAVE TestPulse = GetTestPulse()
-	TP_UpdateTestPulseWaveChunks(TestPulse, panelTitle) // makes the test pulse wave that contains enought test pulses to fill the min ITC DAC wave size 2^17
+	TP_UpdateTestPulseWaveMD(panelTitle, TestPulse)
 
-	NVAR duration = $GetTestpulseDuration(panelTitle)
-	DM_CreateScaleTPHoldWaveChunk(panelTitle,0, duration)  // first TP so start point = 0
-
-	// configures data for ITC with testpulse wave selected
 	DC_ConfigureDataForITC(panelTitle, TEST_PULSE_MODE)
 	// special mod for test pulse to ITC data wave that makes sure the entire TP is filled with test pulses because of how data is placed into the ITCDataWave based on sampling frequency
 	WAVE ITCDataWave = GetITCDataWave(panelTitle)

Packages/MIES/MIES_DataConfiguratorITC.ipf
@@ -399,7 +399,7 @@ static Function DC_PlaceDataInITCDataWave(panelTitle, dataAcqOrTP, multiDevice)
 	string setNameList, setName
 	string ctrl, firstSetName, str, list
 	variable DAGain, DAScale, setColumn, insertStart, setLength, oneFullCycle, val
-	variable channelMode, TPDuration, TPAmpVClamp, TPAmpIClamp, TPStartPoint, TPEndPoint
+	variable channelMode, TPAmpVClamp, TPAmpIClamp, testPulseLength, testPulseAmplitude
 	variable GlobalTPInsert, ITI, scalingZero, indexingLocked, indexing, distributedDAQ
 	variable distributedDAQDelay, onSetDelay, indexActiveHeadStage, decimationFactor
 	variable/C ret
@@ -414,11 +414,10 @@ static Function DC_PlaceDataInITCDataWave(panelTitle, dataAcqOrTP, multiDevice)
 
 	if(globalTPInsert)
 		Wave ChannelClampMode = GetChannelClampMode(panelTitle)
-		TPDuration   = 2 * GetSetVariable(panelTitle, "SetVar_DataAcq_TPDuration")
+		testPulseLength = TP_GetTestPulseLengthInPoints(panelTitle)
+		NVAR baselineFrac = $GetTestpulseBaselineFraction(panelTitle)
 		TPAmpVClamp  = GetSetVariable(panelTitle, "SetVar_DataAcq_TPAmplitude")
 		TPAmpIClamp  = GetSetVariable(panelTitle, "SetVar_DataAcq_TPAmplitudeIC")
-		TPStartPoint = x2pnt(ITCDataWave, TPDuration / 4)
-		TPEndPoint   = x2pnt(ITCDataWave, TPDuration / 2) + TPStartPoint
 	endif
 
 	WAVE sweepDataLNB      = GetSweepSettingsWave(panelTitle)
@@ -512,13 +511,15 @@ static Function DC_PlaceDataInITCDataWave(panelTitle, dataAcqOrTP, multiDevice)
 		// of the onset delay
 		if(dataAcqOrTP == DATA_ACQUISITION_MODE && globalTPInsert)
 			channelMode = ChannelClampMode[i][%DAC]
+			testPulseAmplitude = NaN
 			if(channelMode == V_CLAMP_MODE)
-				ITCDataWave[TPStartPoint, TPEndPoint][itcDataColumn] = TPAmpVClamp * DAGain
+				testPulseAmplitude = TPAmpVClamp * DAGain
 			elseif(channelMode == I_CLAMP_MODE)
-				ITCDataWave[TPStartPoint, TPEndPoint][itcDataColumn] = TPAmpIClamp * DAGain
+				testPulseAmplitude = TPAmpIClamp * DAGain
 			else
 				ASSERT(0, "Unknown clamp mode")
 			endif
+			ITCDataWave[baselineFrac * testPulseLength, (1 - baselineFrac) * testPulseLength][itcDataColumn] = testPulseAmplitude
 		endif
 
 		// put the insert test pulse checkbox status into the sweep data wave

Packages/MIES/MIES_DataManagementNew.ipf
@@ -85,33 +85,27 @@ Function DM_AfterSweepDataSaveHook(panelTitle)
 	endfor
 End
 
-Function DM_CreateScaleTPHoldingWave(panelTitle)
+Function DM_CreateScaleTPHoldingWave(panelTitle, [chunk])
 	string panelTitle
+	variable chunk
 
-	dfref testPulseDFR = GetDeviceTestPulse(panelTitle)
+	variable length, first, last
 
-	NVAR duration = $GetTestpulseDuration(panelTitle)
-	WAVE ITCDataWave = GetITCDataWave(panelTitle)
-
-
-	Duplicate/O/R=[0, (duration * 2)][] ITCDataWave, testPulseDFR:TestPulseITC/Wave=TestPulseITC
-	Redimension/Y=(GetRawDataFPType(panelTitle)) TestPulseITC
-	DM_ADScaling(TestPulseITC, panelTitle)
-End
-
-Function DM_CreateScaleTPHoldWaveChunk(panelTitle,startPoint, NoOfPointsInTP)
-	string panelTitle
-	variable startPoint, NoOfPointsInTP
-
-	variable RowsToCopy = NoOfPointsInTP
 	WAVE ITCDataWave = GetITCDataWave(panelTitle)
 	WAVE TestPulseITC = GetTestPulseITCWave(panelTitle)
+	length = TP_GetTestPulseLengthInPoints(panelTitle)
 
-	ITCDataWave[0][0] += 0
-	startPoint += RowsToCopy / 4
-	Duplicate/O/R=[startPoint,(startPoint + RowsToCopy)][] ITCDataWave, TestPulseITC
+	if(ParamIsDefault(chunk))
+		chunk = 0
+	endif
+
+	first = chunk * length
+	last  = first + length
+	ASSERT(first >= 0 && last < DimSize(ITCDataWave, ROWS), "Invalid wave subrange")
+
+	Duplicate/O/R=[first, last][] ITCDataWave, TestPulseITC
 	Redimension/Y=(GetRawDataFPType(panelTitle)) TestPulseITC
-	SetScale/P x 0,deltax(TestPulseITC),"ms", TestPulseITC
+	SetScale/P x, 0, DimDelta(TestPulseITC, ROWS), "ms", TestPulseITC
 	DM_ADScaling(TestPulseITC, panelTitle)
 End
 

Packages/MIES/MIES_GlobalStringAndVariableAccess.ipf
@@ -207,6 +207,16 @@ Function/S GetTestpulseDuration(panelTitle)
 	return GetNVARAsString(GetDeviceTestPulse(panelTitle), "duration", initialValue=NaN)
 End
 
+/// @brief Return the absolute path to the testpulse baseline fraction variable
+///
+/// The returned value is the fraction which the baseline occupies relative to the total
+/// testpulse length, before and after the pulse itself.
+Function/S GetTestpulseBaselineFraction(panelTitle)
+	string panelTitle
+
+	return GetNVARAsString(GetDeviceTestPulse(panelTitle), "baselineFrac", initialValue=NaN)
+End
+
 /// @brief Return the absolute path to the user comment string
 Function/S GetUserComment(panelTitle)
 	string panelTitle

Packages/MIES/MIES_Oscilloscope.ipf
@@ -223,8 +223,7 @@ Function SCOPE_CreateGraph(plotData, panelTitle)
 	Label/W=$graph bottom "Time (\\U)"
 
 	if(!cmpstr(dataName, "TestPulseITC"))
-		NVAR duration = $GetTestpulseDuration(panelTitle)
-		SetAxis/W=$graph bottom 0, duration * (SI_CalculateMinSampInterval(panelTitle) / 1000) * 2 // use for MD TP plotting
+		SetAxis/W=$graph bottom 0, TP_GetTestPulseLengthInPoints(panelTitle) * (SI_CalculateMinSampInterval(panelTitle) / 1000)
 	else
 		SetAxis/W=$graph bottom 0, (DC_GetStopCollectionPoint(panelTitle, DATA_ACQUISITION_MODE)) * (SI_CalculateMinSampInterval(panelTitle) / 1000)
 	endif

Packages/MIES/MIES_PanelITC.ipf
@@ -5239,44 +5239,67 @@ Function DAP_CheckProc_InsertTP(cba) : CheckBoxControl
 	struct WMCheckBoxAction &cba
 
 	string panelTitle
-	variable testPulseDuration, existingOnsetDelay, onsetDelayResetValue
 
 	switch(cba.eventCode)
 		case 2:
-			panelTitle = cba.win
-			testPulseDuration = 2 * GetSetVariable(panelTitle, "SetVar_DataAcq_TPDuration")
-			existingOnsetDelay = GetSetVariable(panelTitle, "setvar_DataAcq_OnsetDelay")
-
-			if(cba.checked)
-				if(ExistingOnsetDelay < testPulseDuration) // only increases onset delay if it is not big enough to for the TP
-					Setvariable setvar_DataAcq_OnsetDelay WIN = $panelTitle, value =_NUM:testPulseDuration
-				endif
-
-				SetVariable setvar_DataAcq_OnsetDelay win=$panelTitle, limits={testPulseDuration, inf, 1}
-			else
-				onsetDelayResetValue = max(0, (ExistingOnsetDelay - testPulseDuration)) // makes sure onset delay is never less than 0
-				Setvariable setvar_DataAcq_OnsetDelay WIN = $panelTitle, value =_NUM:OnsetDelayResetValue, limits = {0, inf, 1}
-			endif
+			DAP_UpdateOnsetDelay(cba.win, reset_after_uncheck=!cba.checked)
 		break
 	endswitch
 
 	return 0
 End
 
+/// @brief Update the onset delay
+///
+/// @param panelTitle          device
+/// @param reset_after_uncheck [optional, defaults to false] handle the special transition between "global TP insertion" on -> off
+Function DAP_UpdateOnsetDelay(panelTitle, [reset_after_uncheck])
+	string panelTitle
+	variable reset_after_uncheck
+
+	variable pulseDuration, baselineFrac, existingOnsetDelay, onsetDelayResetValue
+	variable testPulseDurWithBL, globalTPinsertion
+
+	if(ParamIsDefault(reset_after_uncheck))
+		reset_after_uncheck = 0
+	else
+		reset_after_uncheck = !!reset_after_uncheck
+	endif
+
+	globalTPinsertion = GetCheckBoxState(panelTitle, "Check_Settings_InsertTP")
+
+	if(!globalTPinsertion && !reset_after_uncheck)
+		return NaN
+	endif
+
+	ASSERT(globalTPinsertion + reset_after_uncheck == 1, "Can not handle that situation")
+
+	pulseDuration = GetSetVariable(panelTitle, "SetVar_DataAcq_TPDuration")
+	baselineFrac = GetSetVariable(panelTitle, "SetVar_DataAcq_TPBaselinePerc") / 100
+	testPulseDurWithBL = TP_CalculateTestPulseLength(pulseDuration, baselineFrac)
+
+	existingOnsetDelay = GetSetVariable(panelTitle, "setvar_DataAcq_OnsetDelay")
+
+	if(globalTPinsertion)
+		if(existingOnsetDelay < testPulseDurWithBL) // only increases onset delay if it is not big enough to for the TP
+			SetSetVariable(paneltitle, "setvar_DataAcq_OnsetDelay", testPulseDurWithBL)
+		endif
+
+		SetVariable setvar_DataAcq_OnsetDelay win=$panelTitle, limits={testPulseDurWithBL, inf, 1}
+	elseif(reset_after_uncheck)
+		onsetDelayResetValue = max(0, existingOnsetDelay - testPulseDurWithBL) // makes sure onset delay is never less than 0
+		Setvariable setvar_DataAcq_OnsetDelay win=$panelTitle, value=_NUM:onsetDelayResetValue, limits={0, inf, 1}
+	endif
+End
+
 Function DAP_SetVarProc_TestPulseSett(sva) : SetVariableControl
 	struct WMSetVariableAction &sva
 
-	string panelTitle
-	variable val
-
 	switch(sva.eventCode)
-		case 2:
-			panelTitle = sva.win
-			val = sva.dval
-
-			if(GetCheckBoxState(panelTitle, "Check_Settings_InsertTP"))
-				Setvariable setvar_DataAcq_OnsetDelay WIN = $panelTitle, value =_NUM:(val * 2), limits = {(val * 2), inf, 1}
-			endif
+		case 1: // mouse up
+		case 2: // Enter key
+		case 3: // Live update
+			DAP_UpdateOnsetDelay(sva.win)
 			break
 	endswitch
 

Packages/MIES/MIES_RepeatedAcquisition.ipf
@@ -37,7 +37,7 @@ static Function RA_HandleITI(panelTitle)
 	DAP_TurnOffAllTTLs(panelTitle)
 
 	WAVE TestPulse = GetTestPulse()
-	TP_UpdateTestPulseWave(TestPulse,panelTitle)
+	TP_UpdateTestPulseWave(panelTitle, TestPulse)
 
 	MAKE/FREE/N=(NUM_DA_TTL_CHANNELS) SelectedDACWaveList
 	TP_StoreSelectedDACWaves(SelectedDACWaveList,panelTitle)

commit f5be05a690a8313b29e8b30d5d0df5a532560c4d

TP_Delta: Fix Steady state resistance calculation for non-default baselines
The old code assumed that the same amount of time taken for the baseline
calculation can be used for the elevated calculation as well. This works
for the default (25%) baseline as then the baseline is as long as the
elevated part. But for higher baseline values this breaks as the
evaluated elevated part is getting larger than the real pulse.

Fix that by using the same timespan, 20% of the minimum of baseline and
duration, for evaluation for the elevated part as already done for the
baselines. For very long durations we just take 5ms.

Broken since 1f7cfa65 (Make the baseline length of the testpulse
configurable, 2015-07-04).

Original-CommitID: a3095cc93a6155ec69b7a10eb223bbfbaf003f9e

Packages/MIES/MIES_TestPulse.ipf
@@ -13,6 +13,7 @@ static Constant TP_MAX_VALID_RESISTANCE       = 3000 ///< Units MOhm
 static Constant TP_TPSTORAGE_WRITE_INTERVAL   = 0.18
 static Constant TP_FIT_POINTS                 = 5
 static Constant TP_DIMENSION_SCALING_INTERVAL = 100  ///< Interval in steps of #TP_TPSTORAGE_WRITE_INTERVAL for recalculating the time axis
+static Constant TP_EVAL_POINT_OFFSET          = 5
 
 /// @brief Return the total length of a single testpulse with baseline
 ///
@@ -128,10 +129,11 @@ Function TP_Delta(panelTitle)
 	string 	panelTitle
 
 	variable amplitudeIC, amplitudeVC, referenceTime
-	variable duration, BaselineSteadyStateStartTime, BaselineSteadyStateEndTime, TPSSEndTime
-	variable TPInstantaneouseOnsetTime, PointsInSteadyStatePeriod, BaselineSSStartPoint
-	variable BaslineSSEndPoint, TPSSEndPoint, TPSSStartPoint, TPInstantaneousOnsetPoint
-	variable columns, i, columnsInWave, OndDBaseline, TPInstantaneousEndPoint
+	variable BaselineSSStartPoint
+	variable BaselineSSEndPoint, TPSSEndPoint, TPSSStartPoint, TPInstantaneousOnsetPoint
+	variable columns, i, columnsInWave, OndDBaseline, durationInTime, baselineInTime
+	variable lengthTPInPoints, evalRangeInPoints, refPoint, TPInstantaneousEndPoint
+	string msg
 
 	referenceTime = DEBUG_TIMER_START()
 
@@ -146,31 +148,43 @@ Function TP_Delta(panelTitle)
 	WAVE OscilloscopeData = GetOscilloscopeWave(panelTitle)
 	NVAR/SDFR=dfr amplitudeICGlobal = amplitudeIC
 	NVAR/SDFR=dfr amplitudeVCGlobal = amplitudeVC
-	NVAR/SDFR=dfr baselineFrac
 	NVAR ADChannelToMonitor = $GetADChannelToMonitor(panelTitle)
 	WAVE activeHSProp = GetActiveHSProperties(panelTitle)
+
+	NVAR duration     = $GetTestpulseDuration(panelTitle)
+	NVAR baselineFrac = $GetTestpulseBaselineFraction(panelTitle)
+	lengthTPInPoints  = TP_GetTestPulseLengthInPoints(panelTitle)
 
 	NVAR tpBufferSize = $GetTPBufferSizeGlobal(panelTitle)
 
 	amplitudeIC = abs(amplitudeICGlobal)
 	amplitudeVC = abs(amplitudeVCGlobal)
 
-	duration = DimSize(OscilloscopeData, ROWS) * DimDelta(OscilloscopeData, ROWS) // total duration of TP in ms
-	BaselineSteadyStateStartTime = 0.1 * duration
-	BaselineSteadyStateEndTime = (baselineFrac - 0.01) * duration
-	TPSSEndTime = (1 - (baselineFrac + 0.01)) * duration
-	TPInstantaneouseOnsetTime = (baselineFrac + 0.002) * duration
-	PointsInSteadyStatePeriod = ScaleToIndex(OscilloscopeData, BaselineSteadyStateEndTime, ROWS) - ScaleToIndex(OscilloscopeData, BaselineSteadyStateStartTime, ROWS)
-	BaselineSSStartPoint = ScaleToIndex(OscilloscopeData, BaselineSteadyStateStartTime, ROWS)
-	BaslineSSEndPoint = ScaleToIndex(OscilloscopeData, BaselineSteadyStateStartTime, ROWS)
-	TPSSEndPoint = ScaleToIndex(OscilloscopeData, TPSSEndTime, ROWS)
-	TPSSStartPoint = TPSSEndPoint - PointsInSteadyStatePeriod
-	TPInstantaneousOnsetPoint = ScaleToIndex(OscilloscopeData, TPInstantaneouseOnsetTime, ROWS)
+	durationInTime = duration * DimDelta(OscilloscopeData, ROWS)
+	baselineInTime = baseLineFrac * lengthTPInPoints * DimDelta(OscilloscopeData, ROWS)
+
+	// Normal durations: 20% of the minimum of duration and baseline
+	// Long durations:   5ms
+	evalRangeInPoints = min(5, 0.2 * min(durationInTime, baselineInTime)) / DimDelta(OscilloscopeData, ROWS)
+
+	// Use data immediately before elevated onset
+	refPoint = baselineFrac * lengthTPInPoints - TP_EVAL_POINT_OFFSET
+	BaselineSSStartPoint = refPoint - evalRangeInPoints
+	BaselineSSEndPoint   = refPoint
+	// Use data before the end of the elevation
+	refPoint = (1 - baselineFrac) * lengthTPInPoints - TP_EVAL_POINT_OFFSET
+	TPSSStartPoint = refPoint - evalRangeInPoints
+	TPSSEndPoint   = refPoint
+
+	TPInstantaneousOnsetPoint = (baselineFrac + 0.002) * lengthTPInPoints
 	TPInstantaneousEndPoint   = 50
+
+	sprintf msg, "%g ms/point,TP length %g ms, duration %g, baseline range [%g, %g], elevated range [%g, %g], instanenous range [%g, %g]", DimDelta(OscilloscopeData ,ROWS), IndexToScale(OscilloscopeData, lengthTPInPoints, ROWS), IndexToScale(OscilloscopeData, duration, ROWS), IndexToScale(OscilloscopeData, BaselineSSStartPoint, ROWS), IndexToScale(OscilloscopeData, BaselineSSEndPoint, ROWS), IndexToScale(OscilloscopeData, TPSSStartPoint, ROWS), IndexToScale(OscilloscopeData, TPSSEndPoint, ROWS), IndexToScale(OscilloscopeData, TPInstantaneousOnsetPoint, ROWS), IndexToScale(OscilloscopeData, TPInstantaneousEndPoint, ROWS)
+	DEBUGPRINT(msg)
 
 	//	duplicate chunks of TP wave in regions of interest: Baseline, Onset, Steady state
 	// 	OscilloscopeData has the AD columns in the order of active AD channels, not the order of active headstages
-	Duplicate/FREE/R=[BaselineSSStartPoint, BaslineSSEndPoint][] OscilloscopeData, BaselineSS
+	Duplicate/FREE/R=[BaselineSSStartPoint, BaselineSSEndPoint][] OscilloscopeData, BaselineSS
 	Duplicate/FREE/R=[TPSSStartPoint, TPSSEndPoint][] OscilloscopeData, TPSS
 	Duplicate/FREE/R=[TPInstantaneousOnsetPoint, TPInstantaneousEndPoint][] OscilloscopeData, Instantaneous
 	//	average the steady state wave
@@ -213,10 +227,10 @@ Function TP_Delta(panelTitle)
 	Multithread InstAvg = abs(InstAvg)
 
 	Duplicate/O/R=[][ADChannelToMonitor, dimsize(TPSS,1) - 1] AvgDeltaSS dfr:SSResistance/Wave=SSResistance
-	SetScale/P x TPSSEndTime,1,"ms", SSResistance // this line determines where the value sit on the bottom axis of the oscilloscope
+	SetScale/P x IndexToScale(OscilloscopeData, TPSSEndPoint, ROWS),1,"ms", SSResistance // this line determines where the value sit on the bottom axis of the oscilloscope
 
 	Duplicate/O/R=[][(ADChannelToMonitor), (dimsize(TPSS,1) - 1)] InstAvg dfr:InstResistance/Wave=InstResistance
-	SetScale/P x TPInstantaneouseOnsetTime,1,"ms", InstResistance
+	SetScale/P x IndexToScale(OscilloscopeData, TPInstantaneousOnsetPoint, ROWS),1,"ms", InstResistance
 
 	i = 0
 	do