AllenInstitute/MIES

commit 4cf6a98a5dd0e334fcc09cf827bd83344fd5584e

Analysis functions for Mid Sweep Event: Support early abort without repurposing the time to the ITI
Original-CommitID: 65aa7dcbd618b877970d38bcaa8fcab5fdd4aa39

Packages/MIES/MIES_BackgroundMD.ipf
@@ -169,6 +169,8 @@ Function ITC_FIFOMonitorMD(s)
 		if(!isFinished && AFM_CallAnalysisFunctions(panelTitle, MID_SWEEP_EVENT) == ANALYSIS_FUNC_RET_REPURP_TIME)
 			UpdateLeftOverSweepTime(panelTitle, fifoPos)
 			isFinished = 1
+		elseif(!isFinished && AFM_CallAnalysisFunctions(panelTitle, MID_SWEEP_EVENT) == ANALYSIS_FUNC_RET_EARLY_STOP)
+			isFinished = 1
 		endif
 
 		if(isFinished)

Packages/MIES/MIES_DataAcqITC.ipf
@@ -101,6 +101,8 @@ Function ITC_FIFOMonitor(s)
 	if(moreData && AFM_CallAnalysisFunctions(panelTitleG, MID_SWEEP_EVENT) == ANALYSIS_FUNC_RET_REPURP_TIME)
 		UpdateLeftOverSweepTime(panelTitleG, fifoPos)
 		moreData = 0
+	elseif(moreData && AFM_CallAnalysisFunctions(panelTitleG, MID_SWEEP_EVENT) == ANALYSIS_FUNC_RET_EARLY_STOP)
+		moreData = 0
 	endif
 
 	if(!moreData)

Packages/MIES/MIES_AnalysisFunctionManagement.ipf
@@ -89,7 +89,7 @@ Function AFM_CallAnalysisFunctions(panelTitle, eventType)
 
 		if(eventType == PRE_DAQ_EVENT && ret == 1)
 			return ret
-		elseif(eventType == MID_SWEEP_EVENT && ret == ANALYSIS_FUNC_RET_REPURP_TIME)
+		elseif(eventType == MID_SWEEP_EVENT && (ret == ANALYSIS_FUNC_RET_REPURP_TIME || ret == ANALYSIS_FUNC_RET_EARLY_STOP))
 			return ret
 		endif
 	endfor

Packages/MIES/MIES_AnalysisFunctions.ipf
@@ -40,6 +40,7 @@
 /// 0                                 | All         | Nothing
 /// 1                                 | Pre DAQ     | DAQ is prevented to start
 /// #ANALYSIS_FUNC_RET_REPURP_TIME    | Mid Sweep   | Current sweep is immediately stopped. Left over time is repurposed for ITI.
+/// #ANALYSIS_FUNC_RET_EARLY_STOP     | Mid Sweep   | Current sweep is immediately stopped without honouring the left over time in a special way.
 
 /// @name Initial parameters for stimulation
 ///@{
@@ -417,7 +418,7 @@ Function TestPrematureSweepStop(panelTitle, eventType, ITCDataWave, headStage, r
 		temp = num2str(str2num(temp) + 1)
 
 		if(str2num(temp) > 20)
-			return ANALYSIS_FUNC_RET_REPURP_TIME
+			return ANALYSIS_FUNC_RET_EARLY_STOP
 		endif
 	endif
 

Packages/MIES/MIES_Constants.ipf
@@ -426,6 +426,7 @@ StrConstant EVENT_NAME_LIST = "Pre DAQ;Mid Sweep;Post Sweep;Post Set;Post DAQ"
 /// @anchor AnalysisFuncReturnTypesConstants
 /// @{
 Constant ANALYSIS_FUNC_RET_REPURP_TIME = -100
+Constant ANALYSIS_FUNC_RET_EARLY_STOP  = -101
 /// @}
 
 /// Number of layers in the labnotebook

commit b69a3cfd30d562427597d5987f66e5a9ad35bc08

Avoid calling analysis functions twice on mid sweep event
The if/else logic would call AFM_CallAnalysisFunctions twice if the
first if check fails.

Bug introduced in 4cf6a98a (Analysis functions for Mid Sweep Event:
Support early abort without repurposing the time to the ITI,
2017-08-28).

Original-CommitID: 1bae38906d4ac7652742284fe9b328e7f95a3fa2

Packages/MIES/MIES_BackgroundMD.ipf
@@ -153,7 +153,7 @@ Function ITC_FIFOMonitorMD(s)
 	DFREF activeDevices = GetActiveITCDevicesFolder()
 	WAVE/SDFR=activeDevices ActiveDeviceList
 	variable deviceID, isFinished
-	variable i, fifoPos
+	variable i, fifoPos, result
 	string panelTitle
 
 	for(i = 0; i < DimSize(ActiveDeviceList, ROWS); i += 1)
@@ -166,11 +166,15 @@ Function ITC_FIFOMonitorMD(s)
 
 		SCOPE_UpdateOscilloscopeData(panelTitle, DATA_ACQUISITION_MODE, fifoPos=fifoPos)
 
-		if(!isFinished && AFM_CallAnalysisFunctions(panelTitle, MID_SWEEP_EVENT) == ANALYSIS_FUNC_RET_REPURP_TIME)
-			UpdateLeftOverSweepTime(panelTitle, fifoPos)
-			isFinished = 1
-		elseif(!isFinished && AFM_CallAnalysisFunctions(panelTitle, MID_SWEEP_EVENT) == ANALYSIS_FUNC_RET_EARLY_STOP)
-			isFinished = 1
+		if(!isFinished)
+			result = AFM_CallAnalysisFunctions(panelTitle, MID_SWEEP_EVENT)
+
+			if(result == ANALYSIS_FUNC_RET_REPURP_TIME)
+				UpdateLeftOverSweepTime(panelTitle, fifoPos)
+				isFinished = 1
+			elseif(result == ANALYSIS_FUNC_RET_EARLY_STOP)
+				isFinished = 1
+			endif
 		endif
 
 		if(isFinished)

Packages/MIES/MIES_DataAcqITC.ipf
@@ -84,7 +84,7 @@ Function ITC_FIFOMonitor(s)
 	STRUCT WMBackgroundStruct &s
 
 	string oscilloscopeSubwindow
-	variable fifoPos, moreData, anaFuncReturn
+	variable fifoPos, moreData, anaFuncReturn, result
 
 	SVAR panelTitleG       = $GetPanelTitleGlobal()
 	NVAR ITCDeviceIDGlobal = $GetITCDeviceIDGlobal(panelTitleG)
@@ -98,11 +98,15 @@ Function ITC_FIFOMonitor(s)
 
 	AM_analysisMasterMidSweep(panelTitleG)
 
-	if(moreData && AFM_CallAnalysisFunctions(panelTitleG, MID_SWEEP_EVENT) == ANALYSIS_FUNC_RET_REPURP_TIME)
-		UpdateLeftOverSweepTime(panelTitleG, fifoPos)
-		moreData = 0
-	elseif(moreData && AFM_CallAnalysisFunctions(panelTitleG, MID_SWEEP_EVENT) == ANALYSIS_FUNC_RET_EARLY_STOP)
-		moreData = 0
+	if(moreData)
+		result = AFM_CallAnalysisFunctions(panelTitleG, MID_SWEEP_EVENT)
+
+		if(result == ANALYSIS_FUNC_RET_REPURP_TIME)
+			UpdateLeftOverSweepTime(panelTitleG, fifoPos)
+			moreData = 0
+		elseif(result == ANALYSIS_FUNC_RET_EARLY_STOP)
+			moreData = 0
+		endif
 	endif
 
 	if(!moreData)