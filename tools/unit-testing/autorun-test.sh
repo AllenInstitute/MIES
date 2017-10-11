8de3a93b0284b5d896bd21ce14ddd0763b36cacc
Thomas Braun <thomas.braun@byte-physics.de>
2017-10-11 22:28:27 +0200

    autorun-test.sh: Support windows as well
    Original-CommitID: 6be5290bbd46851dbcd79e7e2fff58b5a7b7d7ba

--- tools/unit-testing/autorun-test.sh
+++ tools/unit-testing/autorun-test.sh
@@ -15,7 +15,18 @@ fi
 for i in $experiments
 do
 	echo "Running experiment $i"
-	env WINEPREFIX=/home/thomasb/.wine-igor wine C:/Program\ Files/WaveMetrics/Igor\ Pro\ 7\ Folder/IgorBinaries_x64/Igor64.exe /N /I "$(pwd)/$i"
+
+  case $MSYSTEM in
+    MINGW*)
+      # we don't want MSYS path conversion, as that would break the /X options,
+      # see https://github.com/git-for-windows/build-extra/blob/master/ReleaseNotes.md
+      MSYS_NO_PATHCONV=1 "/c/Program Files/WaveMetrics/Igor Pro 7 Folder/IgorBinaries_x64/Igor64.exe" /N /I "$i"
+      ;;
+    *)
+      env WINEPREFIX=/home/thomasb/.wine-igor wine C:/Program\ Files/WaveMetrics/Igor\ Pro\ 7\ Folder/IgorBinaries_x64/Igor64.exe /N /I "$(pwd)/$i"
+        ;;
+  esac
+
 done
 
 rm -f $StateFile
