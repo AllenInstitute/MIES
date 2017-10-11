9af2eb5f394eb00d9e4b760fc299baff9bf20f23
Thomas Braun <thomas.braun@byte-physics.de>
2017-10-11 21:25:45 +0200

    Unit-testing: Use the new AUTORUN_PLAIN mode
    This skips the history log saving and also allows us in future commits
    to use the operation queue in the tests.
    
    Original-CommitID: f8edaa75fc2035ee7d56b9d000c3049b8fa161c5

--- tools/unit-testing/autorun-test.bat
+++ tools/unit-testing/autorun-test.bat
@@ -4,7 +4,7 @@ REM Script for automatic test execution and logging from the command line
 REM Opens all experiment files in the current directory in autorun mode
 
 set IgorPath="c:\Program Files\WaveMetrics\Igor Pro 7 Folder\IgorBinaries_x64\Igor64.exe"
-set StateFile="DO_AUTORUN.TXT"
+set StateFile="DO_AUTORUN_PLAIN.TXT"
 
 if exist %IgorPath% goto foundIgor
 echo Igor Pro could not be found in %IgorPath%, please adjust the variable IgorPath in the script
