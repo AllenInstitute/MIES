1f0cf17b4640461249e9adff5bf307933e40ae88
Thomas Braun <thomas.braun@byte-physics.de>
2017-11-10 17:08:50 +0100

    Add compilation testing to main UTF includes
    This avoids that we try to run tests on igor code which does not
    compile. Currently this hangs the build machine and we have to
    *manually* quit Igor Pro.
    
    We have to tweak the bamboo testing as well as we don't want to compile
    the test includes for the release package as they don't exist there.
    
    Original-CommitID: e5ffce4374d9e6e835d07b7c3703bb523a53d517

--- tools/compilation-testing/check_mies_compilation.sh
+++ tools/compilation-testing/check_mies_compilation.sh
@@ -12,6 +12,15 @@ echo MIES_Databrowser      >> input.txt
 echo MIES_WavebuilderPanel >> input.txt
 echo MIES_Downsample       >> input.txt
 
+# test UTF includes as well
+if [ "$#" -gt 0 -a "$1" = "all" ]
+then
+
+echo UTF_Main              >> input.txt
+echo UTF_HardwareMain      >> input.txt
+
+fi
+
 ./autorun-test.sh
 
 exit 0
