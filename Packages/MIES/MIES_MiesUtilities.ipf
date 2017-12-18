AllenInstitute/MIES

commit 9ec3ae7ac97d5eb761503ee7dd2caa26073f27d1

EqualizeVerticalAxesRanges: Add support for per clamp mode ranges
Original-CommitID: e72a509b1d14be7dcace436413572ebf672861aa

Packages/MIES/MIES_MiesUtilities.ipf
@@ -2355,24 +2355,31 @@ End
 
 /// @brief Equalize all vertical axes ranges so that they cover the same range
 ///
-/// @param graph graph
+/// @param graph                       graph
 /// @param ignoreAxesWithLevelCrossing [optional, defaults to false] ignore all vertical axis which
-/// cross the given level in the visible range
-/// @param level [optional, defaults to zero] level to be used for `ignoreAxesWithLevelCrossing=1`
-Function EqualizeVerticalAxesRanges(graph, [ignoreAxesWithLevelCrossing, level])
+///                                    cross the given level in the visible range
+/// @param level                       [optional, defaults to zero] level to be used for `ignoreAxesWithLevelCrossing=1`
+/// @param rangePerClampMode           [optional, defaults to false] use separate Y ranges per clamp mode
+Function EqualizeVerticalAxesRanges(graph, [ignoreAxesWithLevelCrossing, level, rangePerClampMode])
 	string graph
 	variable ignoreAxesWithLevelCrossing
-	variable level
+	variable level, rangePerClampMode
 
 	string axList, axis, traceList, trace, info
 	variable i, j, numAxes, axisOrient, xRangeBegin, xRangeEnd
-	variable beginY, endY
-	variable maxYRange, numTraces, err
+	variable beginY, endY, clampMode
+	variable maxYRange, numTraces , range, refClampMode, err
 
 	if(ParamIsDefault(ignoreAxesWithLevelCrossing))
 		ignoreAxesWithLevelCrossing = 0
 	else
 		ignoreAxesWithLevelCrossing = !!ignoreAxesWithLevelCrossing
+	endif
+
+	if(ParamIsDefault(rangePerClampMode))
+		rangePerClampMode = 0
+	else
+		rangePerClampMode = !!rangePerClampMode
 	endif
 
 	if(ParamIsDefault(level))
@@ -2395,7 +2402,10 @@ Function EqualizeVerticalAxesRanges(graph, [ignoreAxesWithLevelCrossing, level])
 	axList = AxisList(graph)
 	numAxes = ItemsInList(axList)
 
+	Make/FREE/D/N=(NUM_CLAMP_MODES) maxYRangeClampMode = 0
+	Make/FREE/D/N=(numAxes) axisClampMode = Nan
 	Make/FREE/D/N=(numAxes, 2) YValues = NaN
+
 	SetDimLabel COLS, 0, minimum, YValues
 	SetDimLabel COLS, 1, maximum, YValues
 
@@ -2408,6 +2418,8 @@ Function EqualizeVerticalAxesRanges(graph, [ignoreAxesWithLevelCrossing, level])
 		if(axisOrient != AXIS_ORIENTATION_LEFT && axisOrient != AXIS_ORIENTATION_RIGHT)
 			continue
 		endif
+
+		refClampMode = NaN
 
 		for(j = 0; j < numTraces; j += 1)
 			trace = StringFromList(j, traceList)
@@ -2430,12 +2442,26 @@ Function EqualizeVerticalAxesRanges(graph, [ignoreAxesWithLevelCrossing, level])
 				endif
 			endif
 
+			clampMode = str2num(GetUserData(graph, trace, "clampMode"))
+			AI_AssertOnInvalidClampMode(clampMode)
+
+			if(!IsFinite(refClampMode))
+				refClampMode = clampMode
+			else
+				axisClampMode[i] = refClampMode == clampMode ? clampMode : -1
+			endif
+
 			WaveStats/M=2/Q/R=(xRangeBegin, xRangeEnd) wv
 			YValues[i][%minimum] = V_min
 			YValues[i][%maximum] = V_max
-			if(abs(V_max - V_min) > maxYRange)
-				maxYRange = abs(V_max - V_min)
-				axisWithMaxYRange = i
+
+			range = abs(V_max - V_min)
+			if(range > maxYRange)
+				maxYRange = range
+			endif
+
+			if(range > maxYRangeClampMode[clampMode])
+				maxYRangeClampMode[clampMode] = range
 			endif
 		endfor
 	endfor
@@ -2458,7 +2484,13 @@ Function EqualizeVerticalAxesRanges(graph, [ignoreAxesWithLevelCrossing, level])
 		endif
 
 		beginY = YValues[i][%minimum]
-		endY   = YValues[i][%minimum] + maxYRange
+
+		if(rangePerClampMode && axisClampMode[i] >= 0)
+			endY = beginY + maxYRangeClampMode[axisClampMode[i]]
+		else
+			endY = beginY + maxYRange
+		endif
+
 		DebugPrint("Setting new axis ranges for:", str=axis)
 		DebugPrint("beginY:", var=beginY)
 		DebugPrint("endY:", var=endY)

Packages/MIES/MIES_Constants.ipf
@@ -206,6 +206,8 @@ Constant I_CLAMP_MODE      = 1
 Constant I_EQUAL_ZERO_MODE = 2
 /// @}
 
+Constant NUM_CLAMP_MODES = 3
+
 /// @name Possible values for the function parameter of AI_SendToAmp
 /// @anchor AI_SendToAmpConstants
 /// @{

commit 49b3dfec023d36f7ae7482dabaa10eff45837a96

EqualizeVerticalAxesRanges: Only calculate per clamp mode maximum if requested
This makes it possible again to use equalize y range with TTL data in
the sweep/databrowser.

Broken since 9ec3ae7a (EqualizeVerticalAxesRanges: Add support for per
clamp mode ranges, 2017-03-22).

Original-CommitID: 0742c3cca65211de482d1d3b2cf697eb3e9804f1

Packages/MIES/MIES_MiesUtilities.ipf
@@ -2885,7 +2885,7 @@ Function EqualizeVerticalAxesRanges(graph, [ignoreAxesWithLevelCrossing, level, 
 				maxYRange = range
 			endif
 
-			if(range > maxYRangeClampMode[clampMode])
+			if(rangePerClampMode && range > maxYRangeClampMode[clampMode])
 				maxYRangeClampMode[clampMode] = range
 			endif
 		endfor