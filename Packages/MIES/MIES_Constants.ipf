AllenInstitute/MIES

commit a31012bf4ab61c5455f31acba53f45043a4c3c0b

DA_Ephys: Replace GetSetVariable with DAP_GetValueFromNumStateWave
Original-CommitID: f96230c0a0c6a56828c149fba8e7942dc6dd3870

Packages/MIES/MIES_PressureControl.ipf
@@ -864,7 +864,7 @@ Function P_UpdatePressureDataStorageWv(panelTitle) /// @todo Needs to be reworke
 	PressureDataWv[settingHS][%TTL_A]          = idx == 0 ? NaN : --idx
 	idx = GetPopupMenuIndex(panelTitle, "Popup_Settings_Pressure_TTLB")
 	PressureDataWv[settingHS][%TTL_B]          = idx == 0 ? NaN : --idx
-	PressureDataWv[userHS][%ManSSPressure]     = GetSetVariable(panelTitle, "setvar_DataAcq_SSPressure")
+	PressureDataWv[userHS][%ManSSPressure]     = DAP_GetValueFromNumStateWave(panelTitle, "setvar_DataAcq_SSPressure")
 	PressureDataWv[][%PSI_air]                 = GetSetVariable(panelTitle, "setvar_Settings_InAirP")
 	PressureDataWv[][%PSI_solution]            = GetSetVariable(panelTitle, "setvar_Settings_InBathP")
 	PressureDataWv[][%PSI_slice]               = GetSetVariable(panelTitle, "setvar_Settings_InSliceP")
@@ -1744,7 +1744,7 @@ Function P_UpdatePressureModeTabs(panelTitle, headStage)
 		TabControl tab_DataAcq_Pressure win=$panelTitle, tabLabel(1) = "Manual"
 	endif
 
-	SetSetVariable(panelTitle, "setvar_DataAcq_SSPressure", pressureWave[headStage][%ManSSPressure])
+	PGC_SetAndActivateControl(panelTitle, "setvar_DataAcq_SSPressure", val = pressureWave[headStage][%ManSSPressure])
 End
 
 
@@ -2068,7 +2068,7 @@ Function P_SetPressureMode(panelTitle, headStage, pressureMode, [pressure])
 	if(!paramIsDefault(pressure) && pressureMode == PRESSURE_METHOD_MANUAL)
 		ASSERT(pressure > MIN_REGULATOR_PRESSURE && pressure < MAX_REGULATOR_PRESSURE, "Use pressure value greater than -10 psi and less than 10 psi")
 		if(UserSelectedHS == headStage)
-			SetSetVariable(panelTitle, "setvar_DataAcq_SSPressure", pressure)
+			PGC_SetAndActivateControl(panelTitle, "setvar_DataAcq_SSPressure", val = pressure)
 		endif
 		PressureDataWv[headStage][%ManSSPressure] = pressure
 	endif

Packages/MIES/MIES_AnalysisFunctions.ipf
@@ -68,7 +68,7 @@ Function TestAnalysisFunction_V1(panelTitle, eventType, ITCDataWave, headStage)
 	variable headstage
 
 	printf "Analysis function version 1 called: device %s, eventType \"%s\", headstage %d\r", panelTitle, StringFromList(eventType, EVENT_NAME_LIST), headStage
-	printf "Next sweep: %d\r", GetSetVariable(panelTitle, "SetVar_Sweep")
+	printf "Next sweep: %d\r", DAP_GetValueFromNumStateWave(panelTitle, "SetVar_Sweep")
 End
 
 Function TestAnalysisFunction_V2(panelTitle, eventType, ITCDataWave, headStage, realDataLength)
@@ -964,7 +964,7 @@ Function PatchSeqSubThreshold(panelTitle, eventType, ITCDataWave, headStage, rea
 				return 1
 			endif
 
-			val = GetSetVariable(panelTitle, "setvar_DataAcq_AutoBiasV")
+			val = DAP_GetValueFromNumStateWave(panelTitle, "setvar_DataAcq_AutoBiasV")
 
 			if(!IsFinite(val) || CheckIfSmall(val, tol = 1e-12))
 				printf "(%s): Autobias value is zero or non-finite\r", panelTitle
@@ -1065,7 +1065,7 @@ Function PatchSeqSubThreshold(panelTitle, eventType, ITCDataWave, headStage, rea
 	WAVE numericalValues = GetLBNumericalValues(panelTitle)
 
 	// we can't use AFH_GetLastSweepAcquired as the sweep is not yet acquired
-	sweepNo = GetSetVariable(panelTitle, "SetVar_Sweep")
+	sweepNo = DAP_GetValueFromNumStateWave(panelTitle, "SetVar_Sweep")
 	sweepPassed = GetLastSettingIndep(numericalValues, sweepNo, LABNOTEBOOK_USER_PREFIX + PATCHSEQ_ST_LBN_SWEEP_PASS, UNKNOWN_MODE, defValue = 0)
 
 	if(sweepPassed) // already done
@@ -1076,7 +1076,7 @@ Function PatchSeqSubThreshold(panelTitle, eventType, ITCDataWave, headStage, rea
 	WAVE OscilloscopeData = GetOscilloscopeWave(panelTitle)
 	lastFifoPos = GetNumberFromWaveNote(OscilloscopeData, "lastFifoPos") - 1
 
-	totalOnsetDelay = GetSetVariable(panelTitle, "setvar_DataAcq_OnsetDelayUser") \
+	totalOnsetDelay = DAP_GetValueFromNumStateWave(panelTitle, "setvar_DataAcq_OnsetDelayUser") \
 					  + GetValDisplayAsNum(panelTitle, "valdisp_DataAcq_OnsetDelayAuto")
 
 	fifoInStimsetPoint = lastFifoPos - totalOnsetDelay / DimDelta(OscilloscopeData, ROWS)
@@ -1215,7 +1215,7 @@ static Function EvaluateBaselineProperties(panelTitle, sweepNo, chunk, fifoInSti
 	Make/FREE/N = (LABNOTEBOOK_LAYER_COUNT) avgVoltage     = NaN
 	Make/FREE/N = (LABNOTEBOOK_LAYER_COUNT) targetVPassed  = NaN
 
-	targetV = GetSetVariable(panelTitle, "setvar_DataAcq_AutoBiasV")
+	targetV = DAP_GetValueFromNumStateWave(panelTitle, "setvar_DataAcq_AutoBiasV")
 
 	WAVE statusHS = DAP_ControlStatusWaveCache(panelTitle, CHANNEL_TYPE_HEADSTAGE)
 
@@ -1401,7 +1401,7 @@ static Function GetNumberOfChunks(panelTitle)
 
 	WAVE OscilloscopeData    = GetOscilloscopeWave(panelTitle)
 	NVAR stopCollectionPoint = $GetStopCollectionPoint(panelTitle)
-	totalOnsetDelay = GetSetVariable(panelTitle, "setvar_DataAcq_OnsetDelayUser") \
+	totalOnsetDelay = DAP_GetValueFromNumStateWave(panelTitle, "setvar_DataAcq_OnsetDelayUser") \
 					  + GetValDisplayAsNum(panelTitle, "valdisp_DataAcq_OnsetDelayAuto")
 
 	length = stopCollectionPoint * DimDelta(OscilloscopeData, ROWS)

Packages/MIES/MIES_DataConfiguratorITC.ipf
@@ -42,17 +42,17 @@ static Function DC_UpdateGlobals(panelTitle, dataAcqOrTP)
 	// can change due to indexing, etc.
 	AFM_UpdateAnalysisFunctionWave(panelTitle)
 
-	pulseDuration = GetSetVariable(panelTitle, "SetVar_DataAcq_TPDuration")
+	pulseDuration = DAP_GetValueFromNumStateWave(panelTitle, "SetVar_DataAcq_TPDuration")
 	duration = pulseDuration / (DAP_GetITCSampInt(panelTitle, TEST_PULSE_MODE) / 1000)
-	baselineFrac = GetSetVariable(panelTitle, "SetVar_DataAcq_TPBaselinePerc") / 100
+	baselineFrac = DAP_GetValueFromNumStateWave(panelTitle, "SetVar_DataAcq_TPBaselinePerc") / 100
 
 	// need to deal with units here to ensure that resistance is calculated correctly
-	AmplitudeVC = GetSetVariable(panelTitle, "SetVar_DataAcq_TPAmplitude")
-	AmplitudeIC = GetSetVariable(panelTitle, "SetVar_DataAcq_TPAmplitudeIC")
+	AmplitudeVC = DAP_GetValueFromNumStateWave(panelTitle, "SetVar_DataAcq_TPAmplitude")
+	AmplitudeIC = DAP_GetValueFromNumStateWave(panelTitle, "SetVar_DataAcq_TPAmplitudeIC")
 
 	NVAR n = $GetTPBufferSizeGlobal(panelTitle)
 	// n determines the number of TP cycles to average
-	n = GetSetVariable(panelTitle, "setvar_Settings_TPBuffer")
+	n = DAP_GetValueFromNumStateWave(panelTitle, "setvar_Settings_TPBuffer")
 
 	SVAR panelTitleG = $GetPanelTitleGlobal()
 	panelTitleG = panelTitle
@@ -618,11 +618,11 @@ static Function DC_PlaceDataInITCDataWave(panelTitle, numActiveChannels, dataAcq
 	indexing              = DAP_GetValueFromNumStateWave(panelTitle, "Check_DataAcq_Indexing")
 	distributedDAQ        = DAP_GetValueFromNumStateWave(panelTitle, "Check_DataAcq1_DistribDaq")
 	distributedDAQOptOv   = DAP_GetValueFromNumStateWave(panelTitle, "Check_DataAcq1_dDAQOptOv")
-	distributedDAQOptPre  = GetSetVariable(panelTitle, "Setvar_DataAcq_dDAQOptOvPre")
-	distributedDAQOptPost = GetSetVariable(panelTitle, "Setvar_DataAcq_dDAQOptOvPost")
-	distributedDAQOptRes  = GetSetVariable(panelTitle, "Setvar_DataAcq_dDAQOptOvRes")
-	TPAmpVClamp           = GetSetVariable(panelTitle, "SetVar_DataAcq_TPAmplitude")
-	TPAmpIClamp           = GetSetVariable(panelTitle, "SetVar_DataAcq_TPAmplitudeIC")
+	distributedDAQOptPre  = DAP_GetValueFromNumStateWave(panelTitle, "Setvar_DataAcq_dDAQOptOvPre")
+	distributedDAQOptPost = DAP_GetValueFromNumStateWave(panelTitle, "Setvar_DataAcq_dDAQOptOvPost")
+	distributedDAQOptRes  = DAP_GetValueFromNumStateWave(panelTitle, "Setvar_DataAcq_dDAQOptOvRes")
+	TPAmpVClamp           = DAP_GetValueFromNumStateWave(panelTitle, "SetVar_DataAcq_TPAmplitude")
+	TPAmpIClamp           = DAP_GetValueFromNumStateWave(panelTitle, "SetVar_DataAcq_TPAmplitudeIC")
 	powerSpectrum         = DAP_GetValueFromNumStateWave(panelTitle, "check_settings_show_power")
 	decimationFactor      = DC_GetDecimationFactor(panelTitle, dataAcqOrTP)
 	minSamplingInterval   = DAP_GetITCSampInt(panelTitle, dataAcqOrTP)
@@ -683,8 +683,8 @@ static Function DC_PlaceDataInITCDataWave(panelTitle, numActiveChannels, dataAcq
 			ASSERT(0, "Unknown clamp mode")
 		endif
 
-		ctrl = GetPanelControl(i, CHANNEL_TYPE_DAC, CHANNEL_CONTROL_SCALE)
-		DAScale[activeColumn] = GetSetVariable(panelTitle, ctrl)
+		ctrl = GetSpecialControlLabel(CHANNEL_TYPE_DAC, CHANNEL_CONTROL_SCALE)
+		DAScale[activeColumn] = DAP_GetValueFromNumStateWave(panelTitle, ctrl, index = i)
 
 		// DAScale tuning for special cases
 		if(dataAcqOrTP == DATA_ACQUISITION_MODE)
@@ -708,8 +708,8 @@ static Function DC_PlaceDataInITCDataWave(panelTitle, numActiveChannels, dataAcq
 
 		DC_DocumentChannelProperty(panelTitle, "DAC", headstageDAC[activeColumn], i, var=i)
 
-		ctrl = GetPanelControl(i, CHANNEL_TYPE_DAC, CHANNEL_CONTROL_GAIN)
-		val = GetSetVariable(panelTitle, ctrl)
+		ctrl = GetSpecialControlLabel(CHANNEL_TYPE_DAC, CHANNEL_CONTROL_GAIN)
+		val = DAP_GetValueFromNumStateWave(panelTitle, ctrl, index = i)
 		DAGain[activeColumn] = HARDWARE_ITC_BITS_PER_VOLT / val
 
 		DC_DocumentChannelProperty(panelTitle, "DA GAIN", headstageDAC[activeColumn], i, var=val)
@@ -837,19 +837,19 @@ static Function DC_PlaceDataInITCDataWave(panelTitle, numActiveChannels, dataAcq
 	DC_DocumentChannelProperty(panelTitle, "Sampling interval multiplier", INDEP_HEADSTAGE, NaN, var=multiplier)
 	DC_DocumentChannelProperty(panelTitle, "Minimum sampling interval", INDEP_HEADSTAGE, NaN, var=SI_CalculateMinSampInterval(panelTitle, dataAcqOrTP) * 1e-3)
 
-	DC_DocumentChannelProperty(panelTitle, "Inter-trial interval", INDEP_HEADSTAGE, NaN, var=GetSetVariable(panelTitle, "SetVar_DataAcq_ITI"))
-	DC_DocumentChannelProperty(panelTitle, "Delay onset user", INDEP_HEADSTAGE, NaN, var=GetSetVariable(panelTitle, "setvar_DataAcq_OnsetDelayUser"))
+	DC_DocumentChannelProperty(panelTitle, "Inter-trial interval", INDEP_HEADSTAGE, NaN, var=DAP_GetValueFromNumStateWave(panelTitle, "SetVar_DataAcq_ITI"))
+	DC_DocumentChannelProperty(panelTitle, "Delay onset user", INDEP_HEADSTAGE, NaN, var=DAP_GetValueFromNumStateWave(panelTitle, "setvar_DataAcq_OnsetDelayUser"))
 	DC_DocumentChannelProperty(panelTitle, "Delay onset auto", INDEP_HEADSTAGE, NaN, var=GetValDisplayAsNum(panelTitle, "valdisp_DataAcq_OnsetDelayAuto"))
-	DC_DocumentChannelProperty(panelTitle, "Delay termination", INDEP_HEADSTAGE, NaN, var=GetSetVariable(panelTitle, "setvar_DataAcq_TerminationDelay"))
-	DC_DocumentChannelProperty(panelTitle, "Delay distributed DAQ", INDEP_HEADSTAGE, NaN, var=GetSetVariable(panelTitle, "setvar_DataAcq_dDAQDelay"))
-	DC_DocumentChannelProperty(panelTitle, "oodDAQ Pre Feature", INDEP_HEADSTAGE, NaN, var=GetSetVariable(panelTitle, "Setvar_DataAcq_dDAQOptOvPre"))
-	DC_DocumentChannelProperty(panelTitle, "oodDAQ Post Feature", INDEP_HEADSTAGE, NaN, var=GetSetVariable(panelTitle, "Setvar_DataAcq_dDAQOptOvPost"))
-	DC_DocumentChannelProperty(panelTitle, "oodDAQ Resolution", INDEP_HEADSTAGE, NaN, var=GetSetVariable(panelTitle, "setvar_DataAcq_dDAQOptOvRes"))
+	DC_DocumentChannelProperty(panelTitle, "Delay termination", INDEP_HEADSTAGE, NaN, var=DAP_GetValueFromNumStateWave(panelTitle, "setvar_DataAcq_TerminationDelay"))
+	DC_DocumentChannelProperty(panelTitle, "Delay distributed DAQ", INDEP_HEADSTAGE, NaN, var=DAP_GetValueFromNumStateWave(panelTitle, "setvar_DataAcq_dDAQDelay"))
+	DC_DocumentChannelProperty(panelTitle, "oodDAQ Pre Feature", INDEP_HEADSTAGE, NaN, var=DAP_GetValueFromNumStateWave(panelTitle, "Setvar_DataAcq_dDAQOptOvPre"))
+	DC_DocumentChannelProperty(panelTitle, "oodDAQ Post Feature", INDEP_HEADSTAGE, NaN, var=DAP_GetValueFromNumStateWave(panelTitle, "Setvar_DataAcq_dDAQOptOvPost"))
+	DC_DocumentChannelProperty(panelTitle, "oodDAQ Resolution", INDEP_HEADSTAGE, NaN, var=DAP_GetValueFromNumStateWave(panelTitle, "setvar_DataAcq_dDAQOptOvRes"))
 
 	DC_DocumentChannelProperty(panelTitle, "TP Insert Checkbox", INDEP_HEADSTAGE, NaN, var=GlobalTPInsert)
 	DC_DocumentChannelProperty(panelTitle, "Distributed DAQ", INDEP_HEADSTAGE, NaN, var=distributedDAQ)
 	DC_DocumentChannelProperty(panelTitle, "Optimized Overlap dDAQ", INDEP_HEADSTAGE, NaN, var=distributedDAQOptOv)
-	DC_DocumentChannelProperty(panelTitle, "Repeat Sets", INDEP_HEADSTAGE, NaN, var=GetSetVariable(panelTitle, "SetVar_DataAcq_SetRepeats"))
+	DC_DocumentChannelProperty(panelTitle, "Repeat Sets", INDEP_HEADSTAGE, NaN, var=DAP_GetValueFromNumStateWave(panelTitle, "SetVar_DataAcq_SetRepeats"))
 	DC_DocumentChannelProperty(panelTitle, "Scaling zero", INDEP_HEADSTAGE, NaN, var=scalingZero)
 	DC_DocumentChannelProperty(panelTitle, "Indexing", INDEP_HEADSTAGE, NaN, var=indexing)
 	DC_DocumentChannelProperty(panelTitle, "Locked indexing", INDEP_HEADSTAGE, NaN, var=indexingLocked)
@@ -886,7 +886,7 @@ static Function DC_PlaceDataInITCDataWave(panelTitle, numActiveChannels, dataAcq
 		DC_DocumentChannelProperty(panelTitle, "ADC", headstage, i, var=i)
 
 		ctrl = GetPanelControl(i, CHANNEL_TYPE_ADC, CHANNEL_CONTROL_GAIN)
-		DC_DocumentChannelProperty(panelTitle, "AD Gain", headstage, i, var=GetSetVariable(panelTitle, ctrl))
+		DC_DocumentChannelProperty(panelTitle, "AD Gain", headstage, i, var=DAP_GetValueFromNumStateWave(panelTitle, ctrl))
 
 		ctrl = GetPanelControl(i, CHANNEL_TYPE_ADC, CHANNEL_CONTROL_UNIT)
 		DC_DocumentChannelProperty(panelTitle, "AD Unit", headstage, i, str=GetSetVariableString(panelTitle, ctrl))
@@ -1185,10 +1185,10 @@ static Function DC_ReturnTotalLengthIncrease(panelTitle, [onsetDelayUser, onsetD
 	numActiveDACs          = DC_NoOfChannelsSelected(panelTitle, CHANNEL_TYPE_DAC)
 	minSamplingInterval    = DAP_GetITCSampInt(panelTitle, DATA_ACQUISITION_MODE)
 	distributedDAQ         = DAP_GetValueFromNumStateWave(panelTitle, "Check_DataAcq1_DistribDaq")
-	onsetDelayUserVal      = round(GetSetVariable(panelTitle, "setvar_DataAcq_OnsetDelayUser") / (minSamplingInterval / 1000))
+	onsetDelayUserVal      = round(DAP_GetValueFromNumStateWave(panelTitle, "setvar_DataAcq_OnsetDelayUser") / (minSamplingInterval / 1000))
 	onsetDelayAutoVal      = round(GetValDisplayAsNum(panelTitle, "valdisp_DataAcq_OnsetDelayAuto") / (minSamplingInterval / 1000))
-	terminationDelayVal    = round(GetSetVariable(panelTitle, "setvar_DataAcq_TerminationDelay") / (minSamplingInterval / 1000))
-	distributedDAQDelayVal = round(GetSetVariable(panelTitle, "setvar_DataAcq_dDAQDelay") / (minSamplingInterval / 1000))
+	terminationDelayVal    = round(DAP_GetValueFromNumStateWave(panelTitle, "setvar_DataAcq_TerminationDelay") / (minSamplingInterval / 1000))
+	distributedDAQDelayVal = round(DAP_GetValueFromNumStateWave(panelTitle, "setvar_DataAcq_dDAQDelay") / (minSamplingInterval / 1000))
 
 	if(!ParamIsDefault(onsetDelayUser))
 		onsetDelayUser = onsetDelayUserVal

Packages/MIES/MIES_ExperimentDocumentation.ipf
@@ -692,7 +692,7 @@ Function ED_TPDocumentation(panelTitle)
 	TPKeyWave[1][10] = ""
 	TPKeyWave[1][11] = ""
 
-	RTolerance = GetSetVariable(panelTitle, "setvar_Settings_TP_RTolerance")
+	RTolerance = DAP_GetValueFromNumStateWave(panelTitle, "setvar_Settings_TP_RTolerance")
 	TPKeyWave[2][0]  = "1" // Assume a tolerance of 1 mV for V rest
 	TPKeyWave[2][1]  = "50" // Assume a tolerance of 50pA for I rest
 	TPKeyWave[2][2]  = num2str(RTolerance) // applies the same R tolerance for the instantaneous and steady state resistance

Packages/MIES/MIES_Indexing.ipf
@@ -703,7 +703,7 @@ Function IDX_CalculcateActiveSetCount(panelTitle)
 	variable value
 
 	value  = GetValDisplayAsNum(panelTitle, "valdisp_DataAcq_SweepsActiveSet")
-	value *= GetSetVariable(panelTitle, "SetVar_DataAcq_SetRepeats")
+	value *= DAP_GetValueFromNumStateWave(panelTitle, "SetVar_DataAcq_SetRepeats")
 
 	return value
 End

Packages/MIES/MIES_PanelITC.ipf
@@ -4827,7 +4827,7 @@ Function DAP_UpdateITIAcrossSets(panelTitle)
 	DEBUGPRINT("Maximum ITI across sets=", var=maxITI)
 
 	if(DAP_GetValueFromNumStateWave(panelTitle, "Check_DataAcq_Get_Set_ITI"))
-		SetSetVariable(panelTitle, "SetVar_DataAcq_ITI", maxITI)
+		PGC_SetAndActivateControl(panelTitle, "SetVar_DataAcq_ITI", val = maxITI)
 	elseif(maxITI == 0 && numActiveDAChannels > 0)
 		PGC_SetAndActivateControl(panelTitle, "Check_DataAcq_Get_Set_ITI", val = CHECKBOX_UNSELECTED)
 	endif
@@ -4967,7 +4967,7 @@ static Function DAP_UpdateSweepLimitsAndDisplay(panelTitle)
 	panelList = GetListofLeaderAndPossFollower(panelTitle)
 
 	if(DAP_DeviceIsLeader(panelTitle))
-		sweep = GetSetVariable(panelTitle, "SetVar_Sweep")
+		sweep = DAP_GetValueFromNumStateWave(panelTitle, "SetVar_Sweep")
 	else
 		sweep = NaN
 	endif
@@ -5027,7 +5027,7 @@ Function DAP_UpdateSweepSetVariables(panelTitle)
 	variable numSetRepeats
 
 	if(DAP_GetValueFromNumStateWave(panelTitle, "Check_DataAcq1_RepeatAcq"))
-		numSetRepeats = GetSetVariable(panelTitle, "SetVar_DataAcq_SetRepeats")
+		numSetRepeats = DAP_GetValueFromNumStateWave(panelTitle, "SetVar_DataAcq_SetRepeats")
 
 		if(DAP_GetValueFromNumStateWave(panelTitle, "Check_DataAcq1_IndexingLocked"))
 			numSetRepeats *= IDX_MaxSweepsLockedIndexing(panelTitle)
@@ -5565,7 +5565,7 @@ Function DAP_CheckSettings(panelTitle, mode)
 			endif
 		endfor
 
-		if(GetSetVariable(panelTitle, "SetVar_DataAcq_TPDuration") <= 0)
+		if(DAP_GetValueFromNumStateWave(panelTitle, "SetVar_DataAcq_TPDuration") <= 0)
 			print "The testpulse duration must be greater than 0 ms"
 			ControlWindowToFront()
 			return 1
@@ -5592,7 +5592,7 @@ static Function DAP_CheckHeadStage(panelTitle, headStage, mode)
 	string panelTitle
 	variable headStage, mode
 
-	string ctrl, dacWave, endWave, unit, func, info, str, ADUnit, DAUnit
+	string dacWave, endWave, unit, func, info, str, ADUnit, DAUnit
 	variable DACchannel, ADCchannel, DAheadstage, ADheadstage, DAGain, ADGain, realMode
 	variable gain, scale, clampMode, i, valid_f1, valid_f2, ampConnState, needResetting
 	variable DAGainMCC, ADGainMCC
@@ -5716,7 +5716,7 @@ static Function DAP_CheckHeadStage(panelTitle, headStage, mode)
 		return 1
 	endif
 
-	ctrl = GetPanelControl(DACchannel, CHANNEL_TYPE_DAC, CHANNEL_CONTROL_UNIT)
+	string ctrl = GetPanelControl(DACchannel, CHANNEL_TYPE_DAC, CHANNEL_CONTROL_UNIT)
 	unit = GetSetVariableString(panelTitle, ctrl)
 	if(isEmpty(unit))
 		printf "(%s) The unit for DACchannel %d is empty.\r", panelTitle, DACchannel
@@ -5730,8 +5730,7 @@ static Function DAP_CheckHeadStage(panelTitle, headStage, mode)
 		return 1
 	endif
 
-	ctrl = GetPanelControl(DACchannel, CHANNEL_TYPE_DAC, CHANNEL_CONTROL_GAIN)
-	gain = GetSetVariable(panelTitle, ctrl)
+	gain = DAP_GetValueFromNumStateWave(panelTitle, GetSpecialControlLabel(CHANNEL_TYPE_DAC, CHANNEL_CONTROL_GAIN), index = DACchannel)
 	if(!isFinite(gain) || gain == 0)
 		printf "(%s) The gain for DACchannel %d must be finite and non-zero.\r", panelTitle, DACchannel
 		ControlWindowToFront()
@@ -5745,8 +5744,8 @@ static Function DAP_CheckHeadStage(panelTitle, headStage, mode)
 	endif
 
 	// we allow the scale being zero
-	ctrl = GetPanelControl(DACchannel, CHANNEL_TYPE_DAC, CHANNEL_CONTROL_SCALE)
-	scale = GetSetVariable(panelTitle, ctrl)
+	scale = DAP_GetValueFromNumStateWave(panelTitle, GetSpecialControlLabel(CHANNEL_TYPE_DAC, CHANNEL_CONTROL_SCALE), index = DACchannel)
+	scale = DAP_GetValueFromNumStateWave(panelTitle, ctrl)
 	if(!isFinite(scale))
 		printf "(%s) The scale for DACchannel %d must be finite.\r", panelTitle, DACchannel
 		ControlWindowToFront()
@@ -5767,8 +5766,7 @@ static Function DAP_CheckHeadStage(panelTitle, headStage, mode)
 		return 1
 	endif
 
-	ctrl = GetPanelControl(ADCchannel, CHANNEL_TYPE_ADC, CHANNEL_CONTROL_GAIN)
-	gain = GetSetVariable(panelTitle, ctrl)
+	gain = DAP_GetValueFromNumStateWave(panelTitle, GetSpecialControlLabel(CHANNEL_TYPE_ADC, CHANNEL_CONTROL_GAIN), index = ADCchannel)
 	if(!isFinite(gain) || gain == 0)
 		printf "(%s) The gain for ADCchannel %d must be finite and non-zero.\r", panelTitle, ADCchannel
 		ControlWindowToFront()
@@ -6843,8 +6841,8 @@ Function DAP_UpdateOnsetDelay(panelTitle)
 	variable testPulseDurWithBL
 
 	if(DAP_GetValueFromNumStateWave(panelTitle, "Check_Settings_InsertTP"))
-		pulseDuration = GetSetVariable(panelTitle, "SetVar_DataAcq_TPDuration")
-		baselineFrac = GetSetVariable(panelTitle, "SetVar_DataAcq_TPBaselinePerc") / 100
+		pulseDuration = DAP_GetValueFromNumStateWave(panelTitle, "SetVar_DataAcq_TPDuration")
+		baselineFrac = DAP_GetValueFromNumStateWave(panelTitle, "SetVar_DataAcq_TPBaselinePerc") / 100
 		testPulseDurWithBL = TP_CalculateTestPulseLength(pulseDuration, baselineFrac)
 	else
 		testPulseDurWithBL = 0
@@ -8235,11 +8233,11 @@ Function DAP_AllChanDASettings(panelTitle, headStage)
 	endif
 
 	if(GuiState[headStage][%HSmode] == V_CLAMP_MODE)
-		scalar = GetSetVariable(panelTitle, GetPanelControl(CHANNEL_INDEX_ALL_V_CLAMP,CHANNEL_TYPE_DAC,CHANNEL_CONTROL_SCALE))
+		scalar = DAP_GetValueFromNumStateWave(panelTitle, GetPanelControl(CHANNEL_INDEX_ALL_V_CLAMP,CHANNEL_TYPE_DAC,CHANNEL_CONTROL_SCALE))
 		index = GetPopupMenuIndex(panelTitle, GetPanelControl(CHANNEL_INDEX_ALL_V_CLAMP,CHANNEL_TYPE_DAC,CHANNEL_CONTROL_WAVE))
 		indexEnd = GetPopupMenuIndex(panelTitle, GetPanelControl(CHANNEL_INDEX_ALL_V_CLAMP,CHANNEL_TYPE_DAC,CHANNEL_CONTROL_INDEX_END))
 	elseif(GuiState[headStage][%HSmode] == I_CLAMP_MODE)
-		scalar = GetSetVariable(panelTitle, GetPanelControl(CHANNEL_INDEX_ALL_I_CLAMP,CHANNEL_TYPE_DAC,CHANNEL_CONTROL_SCALE))
+		scalar = DAP_GetValueFromNumStateWave(panelTitle, GetPanelControl(CHANNEL_INDEX_ALL_I_CLAMP,CHANNEL_TYPE_DAC,CHANNEL_CONTROL_SCALE))
 		index = GetPopupMenuIndex(panelTitle, GetPanelControl(CHANNEL_INDEX_ALL_I_CLAMP,CHANNEL_TYPE_DAC,CHANNEL_CONTROL_WAVE))
 		indexEnd = GetPopupMenuIndex(panelTitle, GetPanelControl(CHANNEL_INDEX_ALL_I_CLAMP,CHANNEL_TYPE_DAC,CHANNEL_CONTROL_INDEX_END))
 	endif
@@ -8343,7 +8341,7 @@ End
 Function DAP_CheckSkipAhead(panelTitle)
 	string panelTitle
 
-	variable activeSkipAhead = getSetVariable(panelTitle, "SetVar_DataAcq_skipAhead")
+	variable activeSkipAhead = DAP_GetValueFromNumStateWave(panelTitle, "SetVar_DataAcq_skipAhead")
 	variable filteredSkipAhead = DAP_getFilteredSkipAhead(panelTitle, activeSkipAhead)
 	if(activeSkipAhead > filteredSkipAhead)
 		printf "Skip ahead value exceeds allowed limit for new selection and has been set to %d \r", filteredSkipAhead

Packages/MIES/MIES_RepeatedAcquisition.ipf
@@ -16,7 +16,7 @@ static Function RA_RecalculateITI(panelTitle)
 	variable ITI
 
 	NVAR repurposedTime = $GetRepurposedSweepTime(panelTitle)
-	ITI = GetSetVariable(panelTitle, "SetVar_DataAcq_ITI") - DQ_StopITCDeviceTimer(panelTitle) + repurposedTime
+	ITI = DAP_GetValueFromNumStateWave(panelTitle, "SetVar_DataAcq_ITI") - DQ_StopITCDeviceTimer(panelTitle) + repurposedTime
 	repurposedTime = 0
 
 	return ITI

Packages/MIES/MIES_SweepSaving.ipf
@@ -21,7 +21,7 @@ Function SWS_SaveAndScaleITCData(panelTitle, [forcedStop])
 
 	forcedStop = ParamIsDefault(forcedStop) ? 0 : !!forcedStop
 
-	sweepNo = GetSetVariable(panelTitle, "SetVar_Sweep")
+	sweepNo = DAP_GetValueFromNumStateWave(panelTitle, "SetVar_Sweep")
 
 	NVAR stopCollectionPoint = $GetStopCollectionPoint(panelTitle)
 	SCOPE_UpdateOscilloscopeData(panelTitle, DATA_ACQUISITION_MODE, fifoPos=stopCollectionPoint)
@@ -143,7 +143,7 @@ Function SWS_DeleteDataWaves(panelTitle)
 	string list, path, name
 	variable i, numItems, waveSweepNo, sweepNo
 
-	sweepNo   = GetSetVariable(panelTitle, "SetVar_Sweep")
+	sweepNo   = DAP_GetValueFromNumStateWave(panelTitle, "SetVar_Sweep")
 	path      = GetDeviceDataPathAsString(panelTitle)
 	DFREF dfr = GetDeviceDataPath(panelTitle)
 	list      = GetListOfObjects(dfr, DATA_SWEEP_REGEXP, waveProperty="MINCOLS:2")

commit 3ba07ed88416d3557511dad46c7948856d9a83bd

Pressure control: Fix manual pressure control
We have columns for the pressure settings from the data acquisition
panel in the guistate waves, but don't write into it and don't use the
querying functions when getting the values.

Now we write the pressure settings into the guistate wave and also read
them from it.

The bug manifested itself that for manual pressure not the current psi
value was used but the one set before. If you only changed the psi value
once this looked like that manual pressure did not work.

Broken since a31012bf (DA_Ephys: Replace GetSetVariable with
DAP_GetValueFromNumStateWave, 2017-11-02).

Original-CommitID: 623119d54424714367cd13e0ec35f6980829e9dc

Packages/MIES/MIES_Constants.ipf
@@ -473,7 +473,7 @@ Constant HARDWARE_DAC_EXTERNAL_TRIGGER = 0x1
 /// @}
 
 /// Used to upgrade the GuiStateWave as well as the DA Ephys panel
-Constant DA_EPHYS_PANEL_VERSION     = 25
+Constant DA_EPHYS_PANEL_VERSION     = 26
 Constant DATABROWSER_PANEL_VERSION  = 6
 Constant SWEEPBROWSER_PANEL_VERSION = 2
 Constant WAVEBUILDER_PANEL_VERSION  = 3

Packages/MIES/MIES_DAEphys.ipf
@@ -1640,6 +1640,14 @@ Function DAP_SetVarProc_CAA(sva) : SetVariableControl
 			panelTitle = sva.win
 			DAP_AbortIfUnlocked(panelTitle)
 
+			strswitch(sva.ctrlName)
+				case "setvar_DataAcq_SSPressure":
+				case "setvar_DataAcq_PPPressure":
+				case "setvar_DataAcq_PPDuration":
+					DAG_Update(sva.win, sva.ctrlName, val = sva.dval)
+					break
+			endswitch
+
 			DAP_UpdateChanAmpAssignStorWv(panelTitle)
 			P_UpdatePressureDataStorageWv(panelTitle)
 			break

Packages/MIES/MIES_PressureControl.ipf
@@ -872,8 +872,8 @@ Function P_UpdatePressureDataStorageWv(panelTitle) /// @todo Needs to be reworke
 	PressureDataWv[][%PSI_SealMax]             = GetSetVariable(panelTitle, "setvar_Settings_SealMaxP")
 	PressureDataWv[][%solutionZaxis]           = GetSetVariable(panelTitle, "setvar_Settings_SurfaceHeight")
 	PressureDataWv[][%sliceZaxis]              = GetSetVariable(panelTitle, "setvar_Settings_SliceSurfHeight")
-	PressureDataWv[][%ManPPPressure]           = GetSetVariable(panelTitle, "setvar_DataAcq_PPPressure")
-	PressureDataWv[][%ManPPDuration]           = GetSetVariable(panelTitle, "setvar_DataAcq_PPDuration")
+	PressureDataWv[][%ManPPPressure]           = DAG_GetNumericalValue(panelTitle, "setvar_DataAcq_PPPressure")
+	PressureDataWv[][%ManPPDuration]           = DAG_GetNumericalValue(panelTitle, "setvar_DataAcq_PPDuration")
 	PressureDataWv[][%ApproachNear]            = DAG_GetNumericalValue(panelTitle, "check_DatAcq_ApproachNear")
 	PressureDataWv[][%SealAtm]                 = DAG_GetNumericalValue(panelTitle, "check_DatAcq_SealAtm")
 