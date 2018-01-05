AllenInstitute/MIES

commit 5520a2fed07566daa835960e79c585ccef350a74

Disable dangerous controls during DAQ/TP
The controls for Multi Device support and background TP/DAQ should never
be changed during DAQ/TP as that can result in a huge mess.

For the testpulse we only change the controls enabled state if we are
*not* doing a TP during DAQ.

Original-CommitID: 496080c4ff5680d7906d3c9baaa9d6e00d2785a3

Packages/MIES/MIES_TestPulse.ipf
@@ -516,6 +516,7 @@ Function TP_Setup(panelTitle, runMode)
 
 	if(!(runMode & TEST_PULSE_DURING_RA_MOD))
 		DAP_ToggleTestpulseButton(panelTitle, TESTPULSE_BUTTON_TO_STOP)
+		DisableControls(panelTitle, CONTROLS_DISABLE_DURING_DAQ_TP)
 	endif
 
 	TP_ResetTPStorage(panelTitle)
@@ -534,13 +535,18 @@ End
 Function TP_Teardown(panelTitle)
 	string panelTitle
 
+	NVAR runMode = $GetTestpulseRunMode(panelTitle)
+
+	if(!(runMode & TEST_PULSE_DURING_RA_MOD))
+		EnableControls(panelTitle, CONTROLS_DISABLE_DURING_DAQ_TP)
+	endif
+
 	DAP_ToggleTestpulseButton(panelTitle, TESTPULSE_BUTTON_TO_START)
 
 	ED_TPDocumentation(panelTitle)
 
 	SCOPE_KillScopeWindowIfRequest(panelTitle)
 
-	NVAR runMode= $GetTestpulseRunMode(panelTitle)
 	runMode = TEST_PULSE_NOT_RUNNING
 
 	P_LoadPressureButtonState(panelTitle)

Packages/MIES/MIES_Constants.ipf
@@ -645,3 +645,6 @@ StrConstant POSITION_MCC				= "Position MCCs"
 
 StrConstant PULSE_START_TIMES_KEY     = "Pulse Train Pulses"
 StrConstant PULSE_TO_PULSE_LENGTH_KEY = "Pulse To Pulse Length"
+
+/// DA_Ephys controls which should be disabled during DAQ *and* TP
+StrConstant CONTROLS_DISABLE_DURING_DAQ_TP = "check_Settings_MD;Check_Settings_BkgTP;Check_Settings_BackgrndDataAcq"

Packages/MIES/MIES_PanelITC.ipf
@@ -4342,6 +4342,7 @@ Function DAP_OneTimeCallBeforeDAQ(panelTitle, runMode)
 	dataAcqRunMode = runMode
 
 	DAP_ToggleAcquisitionButton(panelTitle, DATA_ACQ_BUTTON_TO_STOP)
+	DisableControls(panelTitle, CONTROLS_DISABLE_DURING_DAQ_TP)
 
 	// turn off active pressure control modes
 	if(GetCheckboxState(panelTitle, "check_Settings_DisablePressure"))
@@ -4363,6 +4364,7 @@ Function DAP_ResetGUIAfterDAQ(panelTitle)
 	endfor
 
 	DAP_ToggleAcquisitionButton(panelTitle, DATA_ACQ_BUTTON_TO_DAQ)
+	EnableControls(panelTitle, CONTROLS_DISABLE_DURING_DAQ_TP)
 End
 
 /// @brief One time cleaning up after data acquisition

commit b3626cb39cbded7fccf2496022cb15975f602461

DAP_ResetGUIAfterDAQ/TP_Teardown : Redo the multi device mode dependent checkbox disabling
Broken since 5520a2fe (Disable dangerous controls during DAQ/TP,
2017-02-14).

Original-CommitID: a1e0d8bf302a93e3bbba42cbea524519606753ff

Packages/MIES/MIES_DAEphys.ipf
@@ -1083,6 +1083,9 @@ Function DAP_ResetGUIAfterDAQ(panelTitle)
 
 	DAP_ToggleAcquisitionButton(panelTitle, DATA_ACQ_BUTTON_TO_DAQ)
 	EnableControls(panelTitle, CONTROLS_DISABLE_DURING_DAQ_TP)
+
+	// fix multi device mode dependent checkbox disabling
+	DAP_SwitchSingleMultiMode(panelTitle)
 End
 
 /// @brief One time cleaning up after data acquisition

Packages/MIES/MIES_TestPulse.ipf
@@ -557,6 +557,7 @@ Function TP_Teardown(panelTitle)
 
 	if(!(runMode & TEST_PULSE_DURING_RA_MOD))
 		EnableControls(panelTitle, CONTROLS_DISABLE_DURING_DAQ_TP)
+		DAP_SwitchSingleMultiMode(panelTitle)
 	endif
 
 	DAP_ToggleTestpulseButton(panelTitle, TESTPULSE_BUTTON_TO_START)