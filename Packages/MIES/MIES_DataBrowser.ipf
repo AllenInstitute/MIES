AllenInstitute/MIES

commit 71c49cd5bf254f745e46527f3357b3b80facb170

Bugfix: Better binding of list waves
The device binding function is unified to have a more uniform look. The
listbox assignments are now done every time the device changes. On
startup it is made sure that the corresponding panel elements exist
prior to assigning the listbox waves.

Original-CommitID: 3109a986618975827566d17ab1ff4aed4c120697

Packages/MIES/MIES_DataBrowser.ipf
@@ -55,15 +55,15 @@ Function DB_OpenDataBrowser()
 	AddVersionToPanel(win, DATABROWSER_PANEL_VERSION)
 	BSP_SetDataBrowser(win)
 
+	BSP_OpenPanel(win)
+	DB_OpenSettingsHistory(win)
+
 	// immediately lock if we have only data from one device
 	devicesWithData = ListMatch(DB_GetAllDevicesWithData(), "!" + NONE)
 	if(ItemsInList(devicesWithData) == 1)
 		device = StringFromList(0, devicesWithData)
 		DB_LockDBPanel(win, device)
 	endif
-
-	BSP_OpenPanel(win)
-	DB_OpenSettingsHistory(win)
 End
 
 Function/S DB_GetMainGraph(win)
@@ -137,32 +137,29 @@ static Function DB_LockDBPanel(win, device)
 	string renameWin
 	variable first, last
 
-	if(!CmpStr(device,NONE))
+	renameWin = "DB_" + device
+	if(!cmpstr(device, NONE))
+		renameWin = "DataBrowser"
 		print "Please choose a device assignment for the data browser"
 		ControlWindowToFront()
-
-		renameWin = "DataBrowser"
-		if(windowExists(renameWin))
-			renameWin = UniqueName("DataBrowser", 9, 1)
-		endif
-		DoWindow/W=$win/C $renameWin
-		win = renameWin
-
-		DB_SetUserData(win, device)
-		DB_DynamicSettingsHistory(win)
-		DB_UpdateSweepPlot(win)
-		return NaN
 	endif
 
-	renameWin = UniqueName("DB_" + device, 9, 0)
+	if(windowExists(renameWin))
+		renameWin = UniqueName("DataBrowser", 9, 1)
+	endif
 	DoWindow/W=$win/C $renameWin
 	win = renameWin
 
 	DB_SetUserData(win, device)
+	if(windowExists(BSP_GetPanel(win)) && BSP_HasBoundDevice(win))
+		BSP_BindListBoxWaves(win)
+	endif
 
 	DB_DynamicSettingsHistory(win)
 	DB_FirstAndLastSweepAcquired(win, first, last)
 	DB_UpdateLastSweepControls(win, first, last)
+
+	DB_UpdateSweepPlot(win)
 End
 
 static Function DB_SetUserData(win, device)

Packages/MIES/MIES_BrowserSettingsPanel.ipf
@@ -151,25 +151,14 @@ static Function BSP_DynamicStartupSettings(mainPanel)
 	SetWindow $bsPanel, hook(main)=BSP_ClosePanelHook
 	AddVersionToPanel(bsPanel, BROWSERSETTINGS_PANEL_VERSION)
 
-	// overlay sweeps
 	SetControlProcedure(bsPanel, "check_BrowserSettings_OVS", BSP_AddBrowserPrefix(mainPanel, "CheckProc_OverlaySweeps"))
-	DFREF dfr = BSP_GetFolder(mainPanel, MIES_BSP_PANEL_FOLDER)
-	WAVE/T listBoxWave        = GetOverlaySweepsListWave(dfr)
-	WAVE listBoxSelWave       = GetOverlaySweepsListSelWave(dfr)
-	WAVE/WAVE sweepSelChoices = GetOverlaySweepSelectionChoices(dfr)
-	ListBox list_of_ranges, listWave=listBoxWave
-	ListBox list_of_ranges, selWave=listBoxSelWave
-	WaveClear listBoxWave
 	PopupMenu popup_overlaySweeps_select,value= #("OVS_GetSweepSelectionChoices(\"" + bsPanel + "\")")
 
-	// artefact removal
-	WAVE/T listBoxWave = GetArtefactRemovalListWave(dfr)
-	ListBox list_of_ranges1, listWave=listBoxWave
-
-	// bind the channel selection wave to the user controls of the external panel
-	WAVE channelSelection = BSP_GetChannelSelectionWave(mainPanel)
-	ChannelSelectionWaveToGUI(mainPanel, channelSelection)
 	BSP_SetCSButtonProc(bsPanel, BSP_AddBrowserPrefix(mainPanel, "CheckProc_ChangedSetting"))
+
+	if(!BSP_IsDataBrowser(mainPanel) || BSP_HasBoundDevice(mainPanel))
+		BSP_BindListBoxWaves(mainPanel)
+	endif
 
 	// settings tab
 	controls = "check_BrowserSettings_DAC;check_BrowserSettings_ADC;check_BrowserSettings_TTL;check_BrowserSettings_splitTTL;check_BrowserSettings_OChan;check_BrowserSettings_dDAQ;check_Calculation_AverageTraces;check_Calculation_ZeroTraces;"
@@ -185,7 +174,9 @@ static Function BSP_DynamicStartupSettings(mainPanel)
 	if(IsDataBrowser(mainPanel))
 		EnableControls(bsPanel, controlsDB)
 		DisableControls(bsPanel, controlsSB)
-		SetPopupMenuString(bsPanel, "popup_DB_lockedDevices", BSP_GetDevice(mainPanel))
+		if(BSP_HasBoundDevice(mainPanel))
+			SetPopupMenuString(bsPanel, "popup_DB_lockedDevices", BSP_GetDevice(mainPanel))
+		endif
 	else
 		EnableControls(bsPanel, controlsSB)
 		DisableControls(bsPanel, controlsDB)
@@ -195,6 +186,34 @@ static Function BSP_DynamicStartupSettings(mainPanel)
 	BSP_InitMainCheckboxes(bsPanel)
 
 	PGC_SetAndActivateControl(bsPanel, "Settings", val = 0)
+End
+
+Function BSP_BindListBoxWaves(win)
+	string win
+
+	string mainPanel, bsPanel
+
+	ASSERT(BSP_IsDataBrowser(win) && BSP_HasBoundDevice(win) || !BSP_IsDataBrowser(win), "DataBrowser needs bound device to bind listBox waves.")
+
+	mainPanel = GetMainWindow(win)
+	bsPanel = BSP_GetPanel(win)
+
+	// overlay sweeps
+	DFREF dfr = BSP_GetFolder(mainPanel, MIES_BSP_PANEL_FOLDER)
+	WAVE/T listBoxWave        = GetOverlaySweepsListWave(dfr)
+	WAVE listBoxSelWave       = GetOverlaySweepsListSelWave(dfr)
+	WAVE/WAVE sweepSelChoices = GetOverlaySweepSelectionChoices(dfr)
+	ListBox list_of_ranges, win=$bsPanel, listWave=listBoxWave
+	ListBox list_of_ranges, win=$bsPanel, selWave=listBoxSelWave
+	WaveClear listBoxWave
+
+	// artefact removal
+	WAVE/T listBoxWave = GetArtefactRemovalListWave(dfr)
+	ListBox list_of_ranges1, win=$bsPanel, listWave=listBoxWave
+
+	// channel selection
+	WAVE channelSelection = BSP_GetChannelSelectionWave(mainPanel)
+	ChannelSelectionWaveToGUI(bsPanel, channelSelection)
 End
 
 /// @brief add SB_* or DB_* prefix to the input string depending on current window
@@ -288,9 +307,10 @@ Function/S BSP_SetDevice(win, device)
 
 	string mainPanel
 
-	mainPanel = GetMainWindow(win)
-	ASSERT(WindowExists(mainPanel), "specified panel does not exist.")
+	ASSERT(WindowExists(win), "specified panel does not exist.")
+	ASSERT(BSP_IsDataBrowser(win), "device property only relevant in DB context")
 
+	mainPanel = GetMainWindow(win)
 	SetWindow $mainPanel, userdata($MIES_BSP_DEVICE) = device
 End
 

commit 897ff63456bd866c858d75bb970aee5a94d08da3

DB_LockDBPanel: Use correct naming scheme for locked databrowser graphs
Broken since 71c49cd5 (Bugfix: Better binding of list waves,
2017-12-14).

Original-CommitID: 2588bd5aa06e989b9cdcbace062c393f96b78381

Packages/MIES/MIES_DataBrowser.ipf
@@ -145,7 +145,7 @@ static Function DB_LockDBPanel(win, device)
 	endif
 
 	if(windowExists(renameWin))
-		renameWin = UniqueName("DataBrowser", 9, 1)
+		renameWin = UniqueName(renameWin, 9, 1)
 	endif
 	DoWindow/W=$win/C $renameWin
 	win = renameWin