AllenInstitute/MIES

commit 66fb97b89f4c6180c0926f09164a0a33b429d8cd

AnalysisBrowser module: Make the NWB menu entries available
Original-CommitID: b5b56605616058d2447276d14026db5cd9903ce6

Packages/MIES/MIES_AnalysisBrowser.ipf
@@ -59,9 +59,11 @@ static Constant AB_LOAD_SWEEP = 0
 static Constant AB_LOAD_STIMSET = 1
 
 Menu "Mies Panels"
-	"Analysis Browser"   , /Q, AB_OpenAnalysisBrowser()
-	"Labnotebook Browser", /Q, LBN_OpenLabnotebookBrowser()
-	"TPStorage Browser"  , /Q, LBN_OpenTPStorageBrowser()
+	"Analysis Browser"        , /Q, AB_OpenAnalysisBrowser()
+	"Labnotebook Browser"     , /Q, LBN_OpenLabnotebookBrowser()
+	"TPStorage Browser"       , /Q, LBN_OpenTPStorageBrowser()
+	"Export all data into NWB", /Q, NWB_ExportWithDialog()
+	"Load Stimsets from NWB"  , /Q, NWB_LoadAllStimsets()
 End
 
 static Function AB_ResetSelectionWave()

Packages/MIES_Include.ipf
@@ -61,7 +61,7 @@
 #include "MIES_Manipulator"
 #include "MIES_Menu"
 #include "MIES_MiesUtilities"
-#include "MIES_NeuroDataWithoutBorders"
+#include "MIES_NeuroDataWithoutBorders" menus=0
 #include "MIES_OptimzedOverlapDistributedAcquisition"
 #include "MIES_Oscilloscope"
 #include "MIES_OverlaySweeps"

commit 650eed8fbbb22365274494428bd25d9004037c16

AnalysisBrowser: Remove NWB export menu entry
It does not make sense to call it from the analysis browser. In addition
the signature of NWB_ExportWithDialog changed so it does not work either
at the moment.

Bug introduced in 66fb97b8 (AnalysisBrowser module: Make the NWB menu
entries available, 2017-06-23).

Original-CommitID: bd4142a2042762d25a07e0fbebb55042c0ffb4ee

Packages/MIES/MIES_AnalysisBrowser.ipf
@@ -62,7 +62,6 @@ Menu "Mies Panels"
 	"Analysis Browser"        , /Q, AB_OpenAnalysisBrowser()
 	"Labnotebook Browser"     , /Q, LBN_OpenLabnotebookBrowser()
 	"TPStorage Browser"       , /Q, LBN_OpenTPStorageBrowser()
-	"Export all data into NWB", /Q, NWB_ExportWithDialog()
 	"Load Stimsets from NWB"  , /Q, NWB_LoadAllStimsets()
 End
 