AllenInstitute/MIES

commit ef26970d6b1fc1c518254890e4f8c0761fed977d

WaveBuilder Panel: Sort the list of returned stimsets across channel types
In the DA_Ephys panel we show the stimsets sorted as well, but there we
never show both DA and TTL stimsets in the same list.

Here we now sort the total list of stimsets and also cleanup up the
logic when calling WBP_ReturnListSavedSets.

Now TTL stimsets are not located near the end anymore.

Original-CommitID: 2db73d24537f170d1931104ed513e567bc98ae95

Packages/MIES/MIES_WaveBuilderPanel.ipf
@@ -300,7 +300,7 @@ Window WaveBuilder() : Panel
 	PopupMenu popup_WaveBuilder_SetList,userdata(ResizeControlsInfo)= A"!!,J<J,htR^]6_;!!#<Pz!!#o2B4uAezzzzzzzzzzzzzz!!#o2B4uAezz"
 	PopupMenu popup_WaveBuilder_SetList,userdata(ResizeControlsInfo) += A"zzzzzzzzzzzz!!#?(FEDG<zzzzzzzzzzz"
 	PopupMenu popup_WaveBuilder_SetList,userdata(ResizeControlsInfo) += A"zzz!!#?(FEDG<zzzzzzzzzzzzzz!!!"
-	PopupMenu popup_WaveBuilder_SetList,mode=1,popvalue="- none -",value= #"\"- none -;\"+ WBP_ReturnListSavedSets(\"DA\") + WBP_ReturnListSavedSets(\"TTL\")"
+	PopupMenu popup_WaveBuilder_SetList,mode=1,popvalue="- none -",value= #"WBP_ReturnListSavedSets()"
 	Button button_WaveBuilder_KillSet,pos={839.00,606.00},size={152.00,23.00},proc=WBP_ButtonProc_DeleteSet,title="Delete Set"
 	Button button_WaveBuilder_KillSet,help={"Delete stimulus set selected in popup menu on the left."}
 	Button button_WaveBuilder_KillSet,userdata(tabcontrol)=  "WBP_WaveType"
@@ -1734,24 +1734,18 @@ Function WBP_GetOutputType()
 	return outputType
 End
 
-/// @brief Return a list of all stim sets for the given type
-/// @param setType One of `DA` or `TTL`
-Function/S WBP_ReturnListSavedSets(setType)
-	string setType
+/// @brief Return a sorted list of all stim sets created by the wavebuilder
+Function/S WBP_ReturnListSavedSets()
 
-	string path, list, stimSetList
-	variable numWaves, i
-	path= GetWBSvdStimSetParamPathAS() + ":" + setType
+	string list = ""
 
-	list = GetListOfObjects($path, "WP_.*" + setType + ".*")
-	stimSetList = ""
+	DFREF dfr = GetSetParamFolder(CHANNEL_TYPE_DAC)
+	list += GetListOfObjects(dfr, "WP_.*")
 
-	numWaves = ItemsInList(list)
-	for(i = 0; i < numWaves; i += 1)
-		stimSetList = AddListItem(RemovePrefix(StringFromList(i, list), startStr="WP_"), stimSetList, ";", Inf)
-	endfor
+	DFREF dfr = GetSetParamFolder(CHANNEL_TYPE_TTL)
+	list += GetListOfObjects(dfr, "WP_.*")
 
-	return SortList(stimSetList, ";", 16)
+	return SortList(RemovePrefixFromListItem("WP_", list), ";", 16)
 end
 
 /// @brief Return true if the given stimset is a builtin, false otherwise

Packages/MIES/MIES_Constants.ipf
@@ -488,7 +488,7 @@ Constant HARDWARE_DAC_EXTERNAL_TRIGGER = 0x1
 Constant DA_EPHYS_PANEL_VERSION     = 27
 Constant DATABROWSER_PANEL_VERSION  = 7
 Constant SWEEPBROWSER_PANEL_VERSION = 3
-Constant WAVEBUILDER_PANEL_VERSION  = 3
+Constant WAVEBUILDER_PANEL_VERSION  = 4
 
 /// Version of the labnotebooks (numerical and textual)
 ///

commit 355d910913d7f28d3d2fb20ff64349179a8c5245

WBP_ReturnListSavedSets: Add forgotten NONE entry
Broken since ef26970d (WaveBuilder Panel: Sort the list of returned
stimsets across channel types, 2018-01-26).

Original-CommitID: adc620f5d73fd55db023d8ba45cfb5bf923ddfa9

Packages/MIES/MIES_WaveBuilderPanel.ipf
@@ -1709,7 +1709,7 @@ Function/S WBP_ReturnListSavedSets()
 	DFREF dfr = GetSetParamFolder(CHANNEL_TYPE_TTL)
 	list += GetListOfObjects(dfr, "WP_.*")
 
-	return SortList(RemovePrefixFromListItem("WP_", list), ";", 16)
+	return NONE + ";" + SortList(RemovePrefixFromListItem("WP_", list), ";", 16)
 end
 
 /// @brief Return true if the given stimset is a builtin, false otherwise