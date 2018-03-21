8f0ecfacac97243c4eac58fd497cc01140c48695
Thomas Braun <thomas.braun@byte-physics.de>
2018-03-21 22:33:27 +0100

    CompilationTesting: Remove compilation testing for the separate modules
    Original-CommitID: a4fb7138e92495c46b6f037b2c67826d5df76a31

--- tools/compilation-testing/check_mies_compilation.bat
+++ tools/compilation-testing/check_mies_compilation.bat
@@ -1,12 +1,8 @@
 del/Q *.txt *.log *.xml
 
-echo MIES_Include          >> input.txt
-echo MIES_AnalysisBrowser  >> input.txt
-echo MIES_Databrowser      >> input.txt
-echo MIES_WavebuilderPanel >> input.txt
-echo MIES_Downsample       >> input.txt
-echo UTF_Main              >> input.txt
-echo UTF_HardwareMain      >> input.txt
+echo MIES_Include     >> input.txt
+echo UTF_Main         >> input.txt
+echo UTF_HardwareMain >> input.txt
 
 call autorun-test.bat
 pause
--- tools/compilation-testing/check_mies_compilation.sh
+++ tools/compilation-testing/check_mies_compilation.sh
@@ -6,18 +6,14 @@ export DISPLAY=:0
 
 rm -f *.txt *.log *.xml
 
-echo MIES_Include          >> input.txt
-echo MIES_AnalysisBrowser  >> input.txt
-echo MIES_Databrowser      >> input.txt
-echo MIES_WavebuilderPanel >> input.txt
-echo MIES_Downsample       >> input.txt
+echo MIES_Include >> input.txt
 
 # test UTF includes as well
 if [ "$#" -gt 0 -a "$1" = "all" ]
 then
 
-echo UTF_Main              >> input.txt
-echo UTF_HardwareMain      >> input.txt
+echo UTF_Main         >> input.txt
+echo UTF_HardwareMain >> input.txt
 
 fi
 
