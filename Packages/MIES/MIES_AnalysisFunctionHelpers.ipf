AllenInstitute/MIES

commit b32f7365df32784f3be1d47ad9a7282379b5c6b7

AFH_GetChannelUnit/AFH_GetChannelUnits: Add functions for querying the channel units
Original-CommitID: 5b1f3f870cbf9de209faf862d0f5a585e3cda650

Packages/MIES/MIES_AnalysisFunctionHelpers.ipf
@@ -158,6 +158,37 @@ Function AFH_GetITCDataColumn(ITCChanConfigWave, channelNumber, channelType)
 	return NaN
 End
 
+/// @brief Return all channel units as free text wave
+///
+/// @param ITCChanConfigWave ITC configuration wave, most users need to call
+///                          `GetITCChanConfigWave(panelTitle)` to get that wave.
+Function/WAVE AFH_GetChannelUnits(ITCChanConfigWave)
+	WAVE ITCChanConfigWave
+
+	string units
+
+		units = note(ITCChanConfigWave)
+		return ListToTextWave(units, ";")
+End
+
+/// @brief Return the channel unit
+///
+/// @param ITCChanConfigWave ITC configuration wave, most users need to call
+///                          `GetITCChanConfigWave(panelTitle)` to get that wave.
+/// @param channelNumber     channel number (0-based)
+/// @param channelType       channel type, one of @ref ITC_XOP_CHANNEL_CONSTANTS
+Function/S AFH_GetChannelUnit(ITCChanConfigWave, channelNumber, channelType)
+	WAVE ITCChanConfigWave
+	variable channelNumber, channelType
+
+	variable idx
+
+	idx = AFH_GetITCDataColumn(ITCChanConfigWave, channelNumber, channelType)
+	WAVE/T units = AFH_GetChannelUnits(ITCChanConfigWave)
+
+	return units[idx]
+End
+
 /// @brief Return the sweep number of the last acquired sweep
 ///
 /// Handles sweep number rollback properly.

commit 3ab4624de29a916d8425fc9cb615d4eafaa1f761

AFH_GetChannelUnit: Return an empty unit for out of range entries
Bug introduced in b32f7365 (AFH_GetChannelUnit/AFH_GetChannelUnits: Add
functions for querying the channel units, 2018-03-08).

Original-CommitID: a57ef066b507c95579fb7f6763505f51ae280186

Packages/MIES/MIES_AnalysisFunctionHelpers.ipf
@@ -191,6 +191,10 @@ Function/S AFH_GetChannelUnit(ITCChanConfigWave, channelNumber, channelType)
 	idx = AFH_GetITCDataColumn(ITCChanConfigWave, channelNumber, channelType)
 	WAVE/T units = AFH_GetChannelUnits(ITCChanConfigWave)
 
+	if(idx >= DimSize(units, ROWS))
+		return ""
+	endif
+
 	return units[idx]
 End
 