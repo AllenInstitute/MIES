AllenInstitute/MIES

commit b02565f416943df6272f4dbb53a6b8cfb6f8ce54

Add GetSweepsWithSetting
Original-CommitID: 17c5ee505ca7f9d4f1c1b49a9ade7305420f1fba

Packages/MIES/MIES_MiesUtilities.ipf
@@ -433,6 +433,7 @@ End
 /// - GetLastSweepWithSettingText()
 /// - GetLastSweepWithSettingTextI()
 /// - GetLastSweepWithSettingIndep()
+/// - GetSweepsWithSetting()
 
 /// @brief Return a headstage independent setting from the numerical labnotebook
 ///
@@ -1009,6 +1010,36 @@ static Function/WAVE GetNonEmptyLBNRows(labnotebookValues, setting)
 
    return FindIndizes(labnotebookValues, col = col, prop = PROP_NON_EMPTY, \
 					  startLayer = 0, endLayer = DimSize(labnotebookValues, LAYERS) - 1)
+End
+
+/// @brief Return a wave with all sweep numbers which have a non-empty entry for setting
+///
+/// @param labnotebookValues numerical/textual labnotebook
+/// @param setting           name of the value to search
+///
+/// @return a 1D free wave with the matching sweep numbers. In case
+/// the setting could not be found an invalid wave reference is returned.
+///
+/// @ingroup LabnotebookQueryFunctions
+Function/WAVE GetSweepsWithSetting(labnotebookValues, setting)
+	WAVE labnotebookValues
+	string setting
+
+	variable sweepCol
+
+	WAVE/Z indizes = GetNonEmptyLBNRows(labnotebookValues, setting)
+	if(!WaveExists(indizes))
+		return $""
+	endif
+
+	sweepCol = GetSweepColumn(labnotebookValues)
+
+	Make/FREE/N=(DimSize(indizes, ROWS)) sweeps = labnotebookValues[indizes[p]][sweepCol][0]
+
+	Make/N=0/FREE unique
+	FindDuplicates/RN=unique sweeps
+
+	return unique
 End
 
 /// @brief Return the last numerical value of a setting from the labnotebook

commit 571c8557c6826296a649564bba8e6dd2857ac49b

GetSweepsWithSetting: Prefer DeleteDuplicates to avoid error on wave with only one row
Bug introduced in b02565f4 (Add GetSweepsWithSetting, 2018-02-14).

Original-CommitID: ceda58b15620cfee7ea0042cda5a4cc28d85ff53

Packages/MIES/MIES_MiesUtilities.ipf
@@ -1059,10 +1059,7 @@ Function/WAVE GetSweepsWithSetting(labnotebookValues, setting)
 
 	Make/FREE/N=(DimSize(indizes, ROWS)) sweeps = labnotebookValues[indizes[p]][sweepCol][0]
 
-	Make/N=0/FREE unique
-	FindDuplicates/RN=unique sweeps
-
-	return unique
+	return DeleteDuplicates(sweeps)
 End
 
 /// @brief Return the last numerical value of a setting from the labnotebook