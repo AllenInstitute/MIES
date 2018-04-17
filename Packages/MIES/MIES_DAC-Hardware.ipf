AllenInstitute/MIES

commit 54d284f5d67957eac969c088828d60d8ee9804d9

Remove explicit HW_SelectDevice calls
Since d3810044 (Add completely rewritten ITC XOP, 2016-04-25) we are
using the new ITC XOP. And all operations therein which act on a device
have a flag deviceID which makes calling HW_SelectDevice before
superfluous.

This is not only faster but also more elegant and easier when we add
support for more hardware types.

Original-CommitID: 4b40d4eae8357cea309451acf9062044d7468285

Packages/MIES/MIES_DAC-Hardware.ipf
@@ -84,6 +84,9 @@ static Constant HW_ITC_RUNNING_STATE = 0x10
 
 /// @brief Select a device
 ///
+/// Only used in special cases for ITC hardware as all ITC operations use the
+/// `/DEV` flag nowadays.
+///
 /// @param hardwareType One of @ref HardwareDACTypeConstants
 /// @param deviceID identifier of the device
 /// @param flags    [optional, default none] One or multiple flags from @ref HardwareInteractionFlags
@@ -146,7 +149,7 @@ Function HW_CloseDevice(hardwareType, deviceID, [flags])
 
 	switch(hardwareType)
 		case HARDWARE_ITC_DAC:
-			HW_ITC_CloseDevice(flags=flags)
+			HW_ITC_CloseDevice(deviceID, flags=flags)
 			break
 		case HARDWARE_NI_DAC:
 			// nothing do be done
@@ -298,7 +301,7 @@ Function HW_EnableYoking(hardwareType, deviceID, [flags])
 
 	switch(hardwareType)
 		case HARDWARE_ITC_DAC:
-			HW_ITC_EnableYoking(flags=flags)
+			HW_ITC_EnableYoking(deviceID, flags=flags)
 			break
 		case HARDWARE_NI_DAC:
 			device = HW_GetInternalDeviceName(hardwareType, deviceID)
@@ -322,7 +325,7 @@ Function HW_DisableYoking(hardwareType, deviceID, [flags])
 
 	switch(hardwareType)
 		case HARDWARE_ITC_DAC:
-			HW_ITC_DisableYoking(flags=flags)
+			HW_ITC_DisableYoking(deviceID, flags=flags)
 			break
 		case HARDWARE_NI_DAC:
 			device = HW_GetInternalDeviceName(hardwareType, deviceID)
@@ -372,7 +375,7 @@ Function HW_IsRunning(hardwareType, deviceID, [flags])
 
 	switch(hardwareType)
 		case HARDWARE_ITC_DAC:
-			return HW_ITC_IsRunning(flags=flags)
+			return HW_ITC_IsRunning(deviceID, flags=flags)
 			break
 		case HARDWARE_NI_DAC:
 			device = HW_GetInternalDeviceName(hardwareType, deviceID)
@@ -399,7 +402,7 @@ Function HW_StartAcq(hardwareType, deviceID, [triggerMode, flags])
 
 	switch(hardwareType)
 		case HARDWARE_ITC_DAC:
-			HW_ITC_StartAcq(triggerMode, flags=flags)
+			HW_ITC_StartAcq(deviceID, triggerMode, flags=flags)
 			break
 		case HARDWARE_NI_DAC:
 			/// @todo add start acq NI code
@@ -653,8 +656,7 @@ Function/S HW_ITC_ListDevices()
 				if(V_ITCError == 0 && V_ITCXOPError == 0 && deviceID >= 0)
 					sprintf msg, "Found device type %s with number %s", type, number
 					DEBUGPRINT(msg)
-					HW_ITC_SelectDevice(deviceID)
-					HW_ITC_CloseDevice()
+					HW_ITC_CloseDevice(deviceID)
 					list = AddListItem(device, list, ";", inf)
 				endif
 			endfor
@@ -869,8 +871,7 @@ Function HW_ITC_CloseAllDevices([flags])
 
 	DEBUGPRINTSTACKINFO()
 
-	if(HW_ITC_IsRunning(flags=flags))
-		deviceID = HW_ITC_GetCurrentDevice()
+	if(HW_ITC_IsRunning(deviceID, flags=flags))
 		HW_ITC_StopAcq(deviceID, flags=flags)
 	endif
 
@@ -878,20 +879,17 @@ Function HW_ITC_CloseAllDevices([flags])
 End
 
 /// @see HW_CloseDevice
-Function HW_ITC_CloseDevice([flags])
-	variable flags
-
-	variable deviceID
+Function HW_ITC_CloseDevice(deviceID, [flags])
+	variable deviceID, flags
 
 	DEBUGPRINTSTACKINFO()
 
-	if(HW_ITC_IsRunning(flags=flags))
-		deviceID = HW_ITC_GetCurrentDevice()
+	if(HW_ITC_IsRunning(deviceID, flags=flags))
 		HW_ITC_StopAcq(deviceID, flags=flags)
 	endif
 
 	do
-		ITCCloseDevice2/Z
+		ITCCloseDevice2/DEV=(deviceID)/Z
 	while(V_ITCXOPError == SLOT_LOCKED_TO_OTHER_THREAD && V_ITCError == 0)
 End
 
@@ -909,26 +907,26 @@ Function HW_ITC_SelectDevice(deviceID, [flags])
 End
 
 /// @see HW_EnableYoking
-Function HW_ITC_EnableYoking([flags])
-	variable flags
+Function HW_ITC_EnableYoking(deviceID, [flags])
+	variable deviceID, flags
 
 	DEBUGPRINTSTACKINFO()
 
 	do
-		ITCInitialize2/M=1/Z=(HW_ITC_GetZValue(flags))
+		ITCInitialize2/DEV=(deviceID)/M=1/Z=(HW_ITC_GetZValue(flags))
 	while(V_ITCXOPError == SLOT_LOCKED_TO_OTHER_THREAD && V_ITCError == 0)
 
 	HW_ITC_HandleReturnValues(flags, V_ITCError, V_ITCXOPError)
 End
 
 /// @see HW_DisableYoking
-Function HW_ITC_DisableYoking([flags])
-	variable flags
+Function HW_ITC_DisableYoking(deviceID, [flags])
+	variable deviceID, flags
 
 	DEBUGPRINTSTACKINFO()
 
 	do
-		ITCInitialize2/M=0/Z=(HW_ITC_GetZValue(flags))
+		ITCInitialize2/DEV=(deviceID)/M=0/Z=(HW_ITC_GetZValue(flags))
 	while(V_ITCXOPError == SLOT_LOCKED_TO_OTHER_THREAD && V_ITCError == 0)
 
 	HW_ITC_HandleReturnValues(flags, V_ITCError, V_ITCXOPError)
@@ -972,7 +970,7 @@ Function HW_ITC_StopAcq(deviceID, [config, configFunc, prepareForDAQ, zeroDAC, f
 	DEBUGPRINTSTACKINFO()
 
 	do
-		ITCStopAcq2/Z=(HW_ITC_GetZValue(flags))
+		ITCStopAcq2/DEV=(deviceID)/Z=(HW_ITC_GetZValue(flags))
 	while(V_ITCXOPError == SLOT_LOCKED_TO_OTHER_THREAD && V_ITCError == 0)
 
 	HW_ITC_HandleReturnValues(flags, V_ITCError, V_ITCXOPError)
@@ -998,7 +996,7 @@ Function HW_ITC_StopAcq(deviceID, [config, configFunc, prepareForDAQ, zeroDAC, f
 
 	if(prepareForDAQ)
 		do
-			ITCConfigChannelUpload2/Z=(HW_ITC_GetZValue(flags))
+			ITCConfigChannelUpload2/DEV=(deviceID)/Z=(HW_ITC_GetZValue(flags))
 		while(V_ITCXOPError == SLOT_LOCKED_TO_OTHER_THREAD && V_ITCError == 0)
 
 		HW_ITC_HandleReturnValues(flags, V_ITCError, V_ITCXOPError)
@@ -1083,7 +1081,7 @@ Function HW_ITC_ResetFifo(deviceID, [config, configFunc, flags])
 	WAVE fifoPos_t = HW_ITC_GetFifoPosFromConfig(config_t)
 
 	do
-		ITCUpdateFIFOPositionAll2/Z=(HW_ITC_GetZValue(flags)) fifoPos_t
+		ITCUpdateFIFOPositionAll2/DEV=(deviceID)/Z=(HW_ITC_GetZValue(flags)) fifoPos_t
 	while(V_ITCXOPError == SLOT_LOCKED_TO_OTHER_THREAD && V_ITCError == 0)
 
 	HW_ITC_HandleReturnValues(flags, V_ITCError, V_ITCXOPError)
@@ -1115,21 +1113,21 @@ threadsafe Function HW_ITC_StartAcq_TS(deviceID, triggerMode, [flags])
 End
 
 /// @see HW_StartAcq
-Function HW_ITC_StartAcq(triggerMode, [flags])
-	variable triggerMode, flags
+Function HW_ITC_StartAcq(deviceID, triggerMode, [flags])
+	variable deviceID, triggerMode, flags
 
 	DEBUGPRINTSTACKINFO()
 
 	switch(triggerMode)
 		case HARDWARE_DAC_EXTERNAL_TRIGGER:
 			do
-				ITCStartAcq2/EXT=256/Z=(HW_ITC_GetZValue(flags))
+				ITCStartAcq2/DEV=(deviceID)/EXT=256/Z=(HW_ITC_GetZValue(flags))
 			while(V_ITCXOPError == SLOT_LOCKED_TO_OTHER_THREAD && V_ITCError == 0)
 
 			break
 		case HARDWARE_DAC_DEFAULT_TRIGGER:
 			do
-				ITCStartAcq2/Z=(HW_ITC_GetZValue(flags))
+				ITCStartAcq2/DEV=(deviceID)/Z=(HW_ITC_GetZValue(flags))
 			while(V_ITCXOPError == SLOT_LOCKED_TO_OTHER_THREAD && V_ITCError == 0)
 
 			break
@@ -1143,13 +1141,14 @@ End
 
 /// @brief Check wether DAQ is still ongoing
 ///
-/// @param flags [optional, default none] One or multiple flags from @ref HardwareInteractionFlags
-Function HW_ITC_IsRunning([flags])
-	variable flags
+/// @param deviceID device identifier
+/// @param flags    [optional, default none] One or multiple flags from @ref HardwareInteractionFlags
+Function HW_ITC_IsRunning(deviceID, [flags])
+	variable deviceID, flags
 
 	DEBUGPRINTSTACKINFO()
 
-	WAVE/Z state = HW_ITC_GetState(flags=flags)
+	WAVE/Z state = HW_ITC_GetState(deviceID, flags=flags)
 
 	if(!WaveExists(state))
 		return 0
@@ -1160,14 +1159,15 @@ End
 
 /// @brief Query the ITC device state
 ///
-/// @param flags [optional, default none] One or multiple flags from @ref HardwareInteractionFlags
-Function/WAVE HW_ITC_GetState([flags])
-	variable flags
+/// @param deviceID device identifier
+/// @param flags    [optional, default none] One or multiple flags from @ref HardwareInteractionFlags
+Function/WAVE HW_ITC_GetState(deviceID, [flags])
+	variable deviceID, flags
 
 	DEBUGPRINTSTACKINFO()
 
 	do
-		ITCGetState2/ALL/FREE/Z=(HW_ITC_GetZValue(flags)) state
+		ITCGetState2/DEV=(deviceID)/ALL/FREE/Z=(HW_ITC_GetZValue(flags)) state
 	while(V_ITCXOPError == SLOT_LOCKED_TO_OTHER_THREAD && V_ITCError == 0)
 
 
@@ -1183,7 +1183,7 @@ Function HW_ITC_ReadADC(deviceID, channel, [flags])
 	DEBUGPRINTSTACKINFO()
 
 	do
-		ITCReadADC2/C=1/V=1/Z=(HW_ITC_GetZValue(flags)) channel
+		ITCReadADC2/DEV=(deviceID)/C=1/V=1/Z=(HW_ITC_GetZValue(flags)) channel
 	while(V_ITCXOPError == SLOT_LOCKED_TO_OTHER_THREAD && V_ITCError == 0)
 
 	HW_ITC_HandleReturnValues(flags, V_ITCError, V_ITCXOPError)
@@ -1198,7 +1198,7 @@ Function HW_ITC_WriteDAC(deviceID, channel, value, [flags])
 	DEBUGPRINTSTACKINFO()
 
 	do
-		ITCSetDAC2/C=1/V=1/Z=(HW_ITC_GetZValue(flags)) channel, value
+		ITCSetDAC2/DEV=(deviceID)/C=1/V=1/Z=(HW_ITC_GetZValue(flags)) channel, value
 	while(V_ITCXOPError == SLOT_LOCKED_TO_OTHER_THREAD && V_ITCError == 0)
 
 	HW_ITC_HandleReturnValues(flags, V_ITCError, V_ITCXOPError)
@@ -1211,7 +1211,7 @@ Function HW_ITC_ReadDigital(deviceID, xopChannel, [flags])
 	DEBUGPRINTSTACKINFO()
 
 	do
-		ITCReadDigital2/Z=(HW_ITC_GetZValue(flags)) xopChannel
+		ITCReadDigital2/DEV=(deviceID)/Z=(HW_ITC_GetZValue(flags)) xopChannel
 	while(V_ITCXOPError == SLOT_LOCKED_TO_OTHER_THREAD && V_ITCError == 0)
 
 	HW_ITC_HandleReturnValues(flags, V_ITCError, V_ITCXOPError)
@@ -1226,7 +1226,7 @@ Function HW_ITC_WriteDigital(deviceID, xopChannel, value, [flags])
 	DEBUGPRINTSTACKINFO()
 
 	do
-		ITCWriteDigital2/Z=(HW_ITC_GetZValue(flags)) xopChannel, value
+		ITCWriteDigital2/DEV=(deviceID)/Z=(HW_ITC_GetZValue(flags)) xopChannel, value
 	while(V_ITCXOPError == SLOT_LOCKED_TO_OTHER_THREAD && V_ITCError == 0)
 
 	HW_ITC_HandleReturnValues(flags, V_ITCError, V_ITCXOPError)
@@ -1292,7 +1292,7 @@ Function HW_ITC_PrepareAcq(deviceID, [data, dataFunc, config, configFunc, flags,
 	WAVE config_t = HW_ITC_TransposeAndToDouble(config)
 
 	do
-		ITCconfigAllchannels2/Z=(HW_ITC_GetZValue(flags)) config_t, data
+		ITCconfigAllchannels2/DEV=(deviceID)/Z=(HW_ITC_GetZValue(flags)) config_t, data
 	while(V_ITCXOPError == SLOT_LOCKED_TO_OTHER_THREAD && V_ITCError == 0)
 
 	HW_ITC_HandleReturnValues(flags, V_ITCError, V_ITCXOPError)
@@ -1300,7 +1300,7 @@ Function HW_ITC_PrepareAcq(deviceID, [data, dataFunc, config, configFunc, flags,
 #ifdef DEBUGGING_ENABLED
 	if(DP_DebuggingEnabledForFile(GetFile(FunctionPath(""))))
 		do
-			ITCGetAllChannelsConfig2/O/Z=(HW_ITC_GetZValue(flags)) config_t, settings
+			ITCGetAllChannelsConfig2/DEV=(deviceID)/O/Z=(HW_ITC_GetZValue(flags)) config_t, settings
 		while(V_ITCXOPError == SLOT_LOCKED_TO_OTHER_THREAD && V_ITCError == 0)
 
 		HW_ITC_HandleReturnValues(flags, V_ITCError, V_ITCXOPError)
@@ -1315,7 +1315,7 @@ Function HW_ITC_PrepareAcq(deviceID, [data, dataFunc, config, configFunc, flags,
 	WAVE fifoPos_t = HW_ITC_GetFifoPosFromConfig(config_t)
 
 	do
-		ITCUpdateFIFOPositionAll2/Z=(HW_ITC_GetZValue(flags)) fifoPos_t
+		ITCUpdateFIFOPositionAll2/DEV=(deviceID)/Z=(HW_ITC_GetZValue(flags)) fifoPos_t
 	while(V_ITCXOPError == SLOT_LOCKED_TO_OTHER_THREAD && V_ITCError == 0)
 
 	HW_ITC_HandleReturnValues(flags, V_ITCError, V_ITCXOPError)
@@ -1404,7 +1404,7 @@ Function HW_ITC_MoreData(deviceID, [ADChannelToMonitor, stopCollectionPoint, con
 	WAVE config_t = HW_ITC_TransposeAndToDouble(config)
 
 	do
-		ITCFIFOAvailableALL2/FREE/Z=(HW_ITC_GetZValue(flags)) config_t, fifoAvail_t
+		ITCFIFOAvailableALL2/DEV=(deviceID)/FREE/Z=(HW_ITC_GetZValue(flags)) config_t, fifoAvail_t
 	while(V_ITCXOPError == SLOT_LOCKED_TO_OTHER_THREAD && V_ITCError == 0)
 
 	HW_ITC_HandleReturnValues(flags, V_ITCError, V_ITCXOPError)

Packages/MIES/MIES_DAEphys.ipf
@@ -3130,7 +3130,6 @@ Function DAP_RemoveYokedDAC(panelToDeYoke)
 	PGC_SetAndActivateControl(panelToDeYoke, "setvar_Hardware_YokeList", str = "None")
 
 	NVAR followerITCDeviceIDGlobal = $GetITCDeviceIDGlobal(panelToDeYoke)
-	HW_SelectDevice(HARDWARE_ITC_DAC, followerITCDeviceIDGlobal)
 	HW_DisableYoking(HARDWARE_ITC_DAC, followerITCDeviceIDGlobal)
 End
 
@@ -4241,7 +4240,6 @@ static Function DAP_UnlockDevice(panelTitle)
 	TFH_StopFIFODaemon(HARDWARE_ITC_DAC, ITCDeviceIDGlobal)
 
 	flags = HARDWARE_PREVENT_ERROR_POPUP | HARDWARE_PREVENT_ERROR_MESSAGE
-	HW_SelectDevice(HARDWARE_ITC_DAC, ITCDeviceIDGlobal, flags=flags)
 	HW_CloseDevice(HARDWARE_ITC_DAC, ITCDeviceIDGlobal, flags=flags)
 	HW_DeRegisterDevice(HARDWARE_ITC_DAC, ITCDeviceIDGlobal, flags=flags)
 
@@ -4407,7 +4405,6 @@ static Function DAP_SetITCDACasFollower(leadDAC, followerDAC)
 
 	if(WhichListItem(followerDAC, listOfFollowerDevices) == -1)
 		listOfFollowerDevices = AddListItem(followerDAC, listOfFollowerDevices,";",inf)
-		HW_SelectDevice(HARDWARE_ITC_DAC, followerITCDeviceIDGlobal)
 		HW_EnableYoking(HARDWARE_ITC_DAC, followerITCDeviceIDGlobal)
 		setvariable setvar_Hardware_YokeList Win = $leadDAC, value= _STR:listOfFollowerDevices, disable = 0
 	endif

Packages/MIES/MIES_DataAcquisition.ipf
@@ -83,7 +83,6 @@ static Function DQ_StopOngoingDAQHelper(panelTitle, [startTPAfterDAQ])
 		DQS_StopBackgroundFifoMonitor()
 
 		NVAR ITCDeviceIDGlobal = $GetITCDeviceIDGlobal(panelTitle)
-		HW_SelectDevice(HARDWARE_ITC_DAC, ITCDeviceIDGlobal)
 		HW_StopAcq(HARDWARE_ITC_DAC, ITCDeviceIDGlobal, zeroDAC = 1)
 
 		if(!discardData)

Packages/MIES/MIES_DataAcquisition_Multi.ipf
@@ -74,8 +74,7 @@ Function DQM_TerminateOngoingDAQHelper(panelTitle)
 	WAVE/T/SDFR=GetActiveITCDevicesFolder() ActiveDeviceList
 
 	TFH_StopFIFODaemon(HARDWARE_ITC_DAC, ITCDeviceIDGlobal)
-	HW_SelectDevice(HARDWARE_ITC_DAC, ITCDeviceIDGlobal, flags=HARDWARE_ABORT_ON_ERROR)
-	HW_StopAcq(HARDWARE_ITC_DAC, ITCDeviceIDGlobal, zeroDAC = 1)
+	HW_StopAcq(HARDWARE_ITC_DAC, ITCDeviceIDGlobal, zeroDAC = 1, flags=HARDWARE_ABORT_ON_ERROR)
 
 	// remove device passed in from active device lists
 	DQM_MakeOrUpdateActivDevLstWave(panelTitle, ITCDeviceIDGlobal, 0, 0, -1)
@@ -128,8 +127,7 @@ Function DQM_StartDAQMultiDevice(panelTitle, [initialSetupReq])
 
 	// configure passed device
 	NVAR ITCDeviceIDGlobal = $GetITCDeviceIDGlobal(panelTitle)
-	HW_SelectDevice(HARDWARE_ITC_DAC, ITCDeviceIDGlobal, flags=HARDWARE_ABORT_ON_ERROR)
-	HW_ITC_PrepareAcq(ITCDeviceIDGlobal)
+	HW_ITC_PrepareAcq(ITCDeviceIDGlobal, flags=HARDWARE_ABORT_ON_ERROR)
 
 	if(!DeviceHasFollower(panelTitle))
 		DQM_BkrdDataAcq(panelTitle)
@@ -169,8 +167,7 @@ Function DQM_StartDAQMultiDevice(panelTitle, [initialSetupReq])
 		followerPanelTitle = StringFromList(i, listOfFollowerDevices)
 
 		NVAR ITCDeviceIDGlobal = $GetITCDeviceIDGlobal(followerPanelTitle)
-		HW_SelectDevice(HARDWARE_ITC_DAC, ITCDeviceIDGlobal, flags=HARDWARE_ABORT_ON_ERROR)
-		HW_ITC_PrepareAcq(ITCDeviceIDGlobal)
+		HW_ITC_PrepareAcq(ITCDeviceIDGlobal, flags=HARDWARE_ABORT_ON_ERROR)
 	endfor
 
 	// start lead device
@@ -286,8 +283,7 @@ static Function DQM_StopDataAcq(panelTitle, ITCDeviceIDGlobal)
 	Variable ITCDeviceIDGlobal
 
 	TFH_StopFIFODaemon(HARDWARE_ITC_DAC, ITCDeviceIDGlobal)
-	HW_SelectDevice(HARDWARE_ITC_DAC, ITCDeviceIDGlobal, flags=HARDWARE_ABORT_ON_ERROR)
-	HW_StopAcq(HARDWARE_ITC_DAC, ITCDeviceIDGlobal, prepareForDAQ=1, zeroDAC = 1)
+	HW_StopAcq(HARDWARE_ITC_DAC, ITCDeviceIDGlobal, prepareForDAQ=1, zeroDAC = 1, flags=HARDWARE_ABORT_ON_ERROR)
 
 	SWS_SaveAndScaleITCData(panelTitle)
 	if(RA_IsFirstSweep(panelTitle))
@@ -312,8 +308,6 @@ static Function DQM_BkrdDataAcq(panelTitle, [triggerMode])
 	NVAR stopCollectionPoint = $GetStopCollectionPoint(panelTitle)
 	NVAR ADChannelToMonitor  = $GetADChannelToMonitor(panelTitle)
 	NVAR ITCDeviceIDGlobal   = $GetITCDeviceIDGlobal(panelTitle)
-
-	HW_SelectDevice(HARDWARE_ITC_DAC, ITCDeviceIDGlobal, flags=HARDWARE_ABORT_ON_ERROR)
 
 	if(triggerMode == HARDWARE_DAC_DEFAULT_TRIGGER && DAG_GetNumericalValue(panelTitle, "Check_DataAcq1_RepeatAcq"))
 		DQ_StartITCDeviceTimer(panelTitle)

Packages/MIES/MIES_DataAcquisition_Single.ipf
@@ -53,9 +53,7 @@ Function DQS_DataAcq(panelTitle)
 	string oscilloscopeSubwindow = SCOPE_GetGraph(panelTitle)
 
 	NVAR ITCDeviceIDGlobal = $GetITCDeviceIDGlobal(panelTitle)
-
-	HW_SelectDevice(HARDWARE_ITC_DAC, ITCDeviceIDGlobal, flags=HARDWARE_ABORT_ON_ERROR)
-	HW_ITC_PrepareAcq(ITCDeviceIDGlobal)
+	HW_ITC_PrepareAcq(ITCDeviceIDGlobal, flags=HARDWARE_ABORT_ON_ERROR)
 
 	if(DAG_GetNumericalValue(panelTitle, "Check_DataAcq1_RepeatAcq"))
 		DQ_StartITCDeviceTimer(panelTitle) // starts a timer for each ITC device. Timer is used to do real time ITI timing.
@@ -86,9 +84,7 @@ Function DQS_BkrdDataAcq(panelTitle)
 	string panelTitle
 
 	NVAR ITCDeviceIDGlobal = $GetITCDeviceIDGlobal(panelTitle)
-
-	HW_SelectDevice(HARDWARE_ITC_DAC, ITCDeviceIDGlobal, flags=HARDWARE_ABORT_ON_ERROR)
-	HW_ITC_PrepareAcq(ITCDeviceIDGlobal)
+	HW_ITC_PrepareAcq(ITCDeviceIDGlobal, flags=HARDWARE_ABORT_ON_ERROR)
 
 	if(DAG_GetNumericalValue(panelTitle, "Check_DataAcq1_RepeatAcq"))
 		DQ_StartITCDeviceTimer(panelTitle) // starts a timer for each ITC device. Timer is used to do real time ITI timing.
@@ -113,10 +109,7 @@ static Function DQS_StopDataAcq(panelTitle, [forcedStop])
 	endif
 
 	NVAR ITCDeviceIDGlobal = $GetITCDeviceIDGlobal(panelTitle)
-
-	HW_SelectDevice(HARDWARE_ITC_DAC, ITCDeviceIDGlobal)
-	HW_StopAcq(HARDWARE_ITC_DAC, ITCDeviceIDGlobal, prepareForDAQ=1, zeroDAC = 1)
-
+	HW_StopAcq(HARDWARE_ITC_DAC, ITCDeviceIDGlobal, prepareForDAQ=1, zeroDAC = 1, flags=HARDWARE_ABORT_ON_ERROR)
 	SWS_SaveAndScaleITCData(panelTitle, forcedStop = forcedStop)
 
 	if(forcedStop)
@@ -150,9 +143,7 @@ Function DQS_FIFOMonitor(s)
 	NVAR ITCDeviceIDGlobal = $GetITCDeviceIDGlobal(panelTitleG)
 	oscilloscopeSubwindow  = SCOPE_GetGraph(panelTitleG)
 
-	HW_SelectDevice(HARDWARE_ITC_DAC, ITCDeviceIDGlobal, flags=HARDWARE_ABORT_ON_ERROR)
-
-	moreData = HW_ITC_MoreData(ITCDeviceIDGlobal, fifoPos=fifoPos)
+	moreData = HW_ITC_MoreData(ITCDeviceIDGlobal, fifoPos=fifoPos, flags=HARDWARE_ABORT_ON_ERROR)
 
 	SCOPE_UpdateOscilloscopeData(panelTitleG, DATA_ACQUISITION_MODE, fifoPos=fifoPos)
 

Packages/MIES/MIES_PressureControl.ipf
@@ -608,8 +608,7 @@ Function P_SetAndGetPressure(panelTitle, headStage, psi)
 	sprintf msg, "panelTitle=%s, hwtype=%d, deviceID=%d, channel=%d, headstage=%d, psi=%g\r", panelTitle, hwType, deviceID, channel, headStage, psi
 	DEBUGPRINT(msg)
 
-	HW_SelectDevice(hwType, deviceID, flags=HARDWARE_ABORT_ON_ERROR)
-	HW_WriteDAC(hwType, deviceID, channel, psi / scale + PRESSURE_OFFSET)
+	HW_WriteDAC(hwType, deviceID, channel, psi / scale + PRESSURE_OFFSET, flags=HARDWARE_ABORT_ON_ERROR)
 
 	return psi
 End
@@ -741,8 +740,6 @@ Function P_UpdateTTLstate(panelTitle, headStage, ONorOFFA, ONorOFFB)
 	ASSERT(IsFinite(ttlBitA), "TTL A must be finite")
 
 	if(hwType == HARDWARE_ITC_DAC)
-		HW_SelectDevice(hwType, deviceID, flags=HARDWARE_ABORT_ON_ERROR)
-
 		if(IsFinite(ttlBitB))
 			ASSERT(HW_ITC_GetRackForTTLBit(panelTitle, ttlBitA) == HW_ITC_GetRackForTTLBit(panelTitle, ttlBitB), "Both TTLbits have to be on the same rack")
 		endif
@@ -752,7 +749,7 @@ Function P_UpdateTTLstate(panelTitle, headStage, ONorOFFA, ONorOFFB)
 		// sending
 		channel = ttlBitA
 
-		val = HW_ReadDigital(hwType, deviceID, channel)
+		val = HW_ReadDigital(hwType, deviceID, channel, flags=HARDWARE_ABORT_ON_ERROR)
 
 		WAVE binary = P_DecToBinary(val)
 
@@ -783,23 +780,23 @@ Function P_UpdateTTLstate(panelTitle, headStage, ONorOFFA, ONorOFFB)
 				endif
 			endif
 
-			HW_WriteDigital(hwType, deviceID, channel, outputDecimal)
+			HW_WriteDigital(hwType, deviceID, channel, outputDecimal, flags=HARDWARE_ABORT_ON_ERROR)
 		endif
 
 	elseif(hwType == HARDWARE_NI_DAC)
 
 		channel = 0
-		val = HW_ReadDigital(hwType, deviceID, channel, line=ttlBitA)
+		val = HW_ReadDigital(hwType, deviceID, channel, line=ttlBitA, flags=HARDWARE_ABORT_ON_ERROR)
 
 		if(ONorOFFA != val)
-			HW_WriteDigital(hwType, deviceID, channel, ONorOFFA, line=ttlBitA)
+			HW_WriteDigital(hwType, deviceID, channel, ONorOFFA, line=ttlBitA, flags=HARDWARE_ABORT_ON_ERROR)
 		endif
 
 		if(IsFinite(ttlBitB))
-			val = HW_ReadDigital(hwType, deviceID, channel, line=ttlBitB)
+			val = HW_ReadDigital(hwType, deviceID, channel, line=ttlBitB, flags=HARDWARE_ABORT_ON_ERROR)
 
 			if(ONorOFFB != val)
-				HW_WriteDigital(hwType, deviceID, channel, ONorOFFB, line=ttlBitB)
+				HW_WriteDigital(hwType, deviceID, channel, ONorOFFB, line=ttlBitB, flags=HARDWARE_ABORT_ON_ERROR)
 			endif
 		endif
 	endif
@@ -1009,8 +1006,7 @@ static Function P_DataAcq(panelTitle, headStage)
 	DAC      = pressureDataWv[headStage][%DAC]
 	ADC      = pressureDataWv[headStage][%ADC]
 
-	HW_SelectDevice(hwType, deviceID, flags=HARDWARE_ABORT_ON_ERROR)
-	HW_StopAcq(hwType, deviceID)
+	HW_StopAcq(hwType, deviceID, flags=HARDWARE_ABORT_ON_ERROR)
 
 	// record onset of data acquisition
 	pressureDataWv[][%OngoingPessurePulse]          = 0 // ensure that only one headstage is recorded as having an ongoing pressure pulse
@@ -1089,8 +1085,7 @@ Function P_ITC_FIFOMonitorProc(s)
 	hwType   = pressureDataWv[headStage][%HW_DAC_Type]
 	deviceID = pressureDataWv[headStage][%DAC_DevID]
 
-	HW_SelectDevice(hwType, deviceID, flags=HARDWARE_ABORT_ON_ERROR)
-	moreData = HW_ITC_MoreData(deviceID, ADChannelToMonitor=1, stopCollectionPoint=350 / HARDWARE_ITC_MIN_SAMPINT)
+	moreData = HW_ITC_MoreData(deviceID, ADChannelToMonitor=1, stopCollectionPoint=350 / HARDWARE_ITC_MIN_SAMPINT, flags=HARDWARE_ABORT_ON_ERROR)
 
 	if(!moreData)
 		HW_StopAcq(hwType, deviceID)
@@ -1426,9 +1421,6 @@ static Function P_TTLforPpulse(panelTitle, headStage)
 
 	ASSERT(IsFinite(TTL), "TTL A must be finite")
 
-	rack = HW_ITC_GetRackForTTLBit(pressureDevice, TTL)
-	HW_SelectDevice(hwType, deviceID, flags=HARDWARE_ABORT_ON_ERROR)
-
 	ret = ParseDeviceString(panelTitle, deviceType, deviceNumber)
 	if(!cmpstr(deviceType, "ITC1600"))
 		// request TTL bit definitly in rack zero
@@ -1442,6 +1434,7 @@ static Function P_TTLforPpulse(panelTitle, headStage)
 		ASSERT(0, "does currently not work with the ITC18USB as DI/DO channels are different")
 	endif
 
+	rack = HW_ITC_GetRackForTTLBit(pressureDevice, TTL)
 	if(rack == RACK_ZERO)
 		rackZeroState = P_UpdateTTLdecimal(pressureDevice, rackZeroState, TTL, 1)
 		ITCData[PRESSURE_TTL_HIGH_START, PRESSURE_PULSE_ENDpt][%TTL_R0] = rackZeroState
@@ -1841,7 +1834,6 @@ static Function P_DACIsCollectingData(panelTitle, headStage)
 	hwType   = pressureDataWv[headStage][%HW_DAC_Type]
 	deviceID = pressureDataWv[headStage][%DAC_DevID]
 
-	HW_SelectDevice(hwType, deviceID, flags=HARDWARE_ABORT_ON_ERROR)
 	return HW_IsRunning(hwType, deviceID)
 End
 

Packages/MIES/MIES_SamplingInterval.ipf
@@ -282,7 +282,6 @@ Function SI_CreateLookupWave(panelTitle, [ignoreChannelOrder])
 	WAVE ITCChanConfigWave = GetITCChanConfigWave(panelTitle)
 
 	NVAR ITCDeviceIDGlobal = $GetITCDeviceIDGlobal(panelTitle)
-	HW_SelectDevice(HARDWARE_ITC_DAC, ITCDeviceIDGlobal, flags=HARDWARE_ABORT_ON_ERROR)
 
 	ret = ParseDeviceString(panelTitle, deviceType, deviceNumber)
 	ASSERT(ret, "Could not parse panelTitle")

Packages/MIES/MIES_TestPulse.ipf
@@ -543,8 +543,7 @@ Function TP_Setup(panelTitle, runMode)
 	DC_ConfigureDataForITC(panelTitle, TEST_PULSE_MODE, multiDevice=multiDevice)
 
 	NVAR ITCDeviceIDGlobal = $GetITCDeviceIDGlobal(panelTitle)
-	HW_SelectDevice(HARDWARE_ITC_DAC, ITCDeviceIDGlobal, flags=HARDWARE_ABORT_ON_ERROR)
-	HW_ITC_PrepareAcq(ITCDeviceIDGlobal)
+	HW_ITC_PrepareAcq(ITCDeviceIDGlobal, flags=HARDWARE_ABORT_ON_ERROR)
 End
 
 /// @brief Perform common actions after the testpulse

commit b8427404df8cd5ca36e401020b6b772fdc9114c9

MIES_DAC-Hardware.ipf: Fix function signatures used without ITC XOP2
Broken since 54d284f5 (Remove explicit HW_SelectDevice calls, 2018-03-21)
but only found now with the support for CI testing without XOPs.

Original-CommitID: eaa411c3e4d959d9b1f5ba5f454dd2b3c667f202

Packages/MIES/MIES_DAC-Hardware.ipf
@@ -1466,8 +1466,8 @@ Function HW_ITC_CloseAllDevices([flags])
 	DEBUGPRINT("Unimplemented")
 End
 
-Function HW_ITC_CloseDevice([flags])
-	variable flags
+Function HW_ITC_CloseDevice(deviceID, [flags])
+	variable deviceID, flags
 
 	DEBUGPRINT("Unimplemented")
 End
@@ -1478,14 +1478,14 @@ Function HW_ITC_SelectDevice(deviceID, [flags])
 	DEBUGPRINT("Unimplemented")
 End
 
-Function HW_ITC_EnableYoking([flags])
-	variable flags
+Function HW_ITC_EnableYoking(deviceID, [flags])
+	variable deviceID, flags
 
 	DEBUGPRINT("Unimplemented")
 End
 
-Function HW_ITC_DisableYoking([flags])
-	variable flags
+Function HW_ITC_DisableYoking(deviceID, [flags])
+	variable deviceID, flags
 
 	DEBUGPRINT("Unimplemented")
 End
@@ -1539,20 +1539,20 @@ threadsafe Function HW_ITC_StartAcq_TS(deviceID, triggerMode, [flags])
 	DEBUGPRINT_TS("Unimplemented")
 End
 
-Function HW_ITC_StartAcq(triggerMode, [flags])
-	variable triggerMode, flags
+Function HW_ITC_StartAcq(deviceID, triggerMode, [flags])
+	variable deviceID, triggerMode, flags
 
 	DEBUGPRINT("Unimplemented")
 End
 
-Function HW_ITC_IsRunning([flags])
-	variable flags
+Function HW_ITC_IsRunning(deviceID, [flags])
+	variable deviceID, flags
 
 	DEBUGPRINT("Unimplemented")
 End
 
-Function/WAVE HW_ITC_GetState([flags])
-	variable flags
+Function/WAVE HW_ITC_GetState(deviceID, [flags])
+	variable deviceID, flags
 
 	DEBUGPRINT("Unimplemented")
 End