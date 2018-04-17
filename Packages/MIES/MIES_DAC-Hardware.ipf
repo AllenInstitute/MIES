AllenInstitute/MIES

commit 5b454452904c4b79b0a77e89577dbf1dcc62e8d0

ITCChanConfigWave: Adapt to new ITC XOP requirements
We now need to pass in one more row for the data offset.
We take this opportunity to also add wave versioning and documenting the
exact format.

Original-CommitID: f114fda35a2df873274f28fed52c2240a3b22ed3

Packages/MIES/MIES_DAC-Hardware.ipf
@@ -1255,12 +1255,13 @@ End
 /// @param dataFunc    [optional, defaults to GetITCDataWave()] override wave getter for the ITC data wave
 /// @param config      ITC config wave
 /// @param configFunc  [optional, defaults to GetITCChanConfigWave()] override wave getter for the ITC config wave
+/// @param offset      [optional, defaults to zero] offset into the data wave in points
 /// @param flags       [optional, default none] One or multiple flags from @ref HardwareInteractionFlags
-Function HW_ITC_PrepareAcq(deviceID, [data, dataFunc, config, configFunc, flags])
+Function HW_ITC_PrepareAcq(deviceID, [data, dataFunc, config, configFunc, flags, offset])
 	variable deviceID
 	WAVE/Z data, config
 	FUNCREF HW_WAVE_GETTER_PROTOTYPE dataFunc, configFunc
-	variable flags
+	variable flags, offset
 
 	string panelTitle
 
@@ -1282,6 +1283,10 @@ Function HW_ITC_PrepareAcq(deviceID, [data, dataFunc, config, configFunc, flags]
 		else
 			WAVE config = configFunc(panelTitle)
 		endif
+	endif
+
+	if(!ParamIsDefault(offset))
+		config[][%Offset] = offset
 	endif
 
 	WAVE config_t = HW_ITC_TransposeAndToDouble(config)

Packages/MIES/MIES_AnalysisFunctionHelpers.ipf
@@ -167,8 +167,13 @@ Function/WAVE AFH_GetChannelUnits(ITCChanConfigWave)
 
 	string units
 
+	if(IsLatestConfigWaveVersion(ITCChanConfigWave))
+		units = GetStringFromWaveNote(ITCChanConfigWave, CHANNEL_UNIT_KEY, keySep = "=")
+		return ListToTextWave(units, ",")
+	else
 		units = note(ITCChanConfigWave)
 		return ListToTextWave(units, ";")
+	endif
 End
 
 /// @brief Return the channel unit

Packages/MIES/MIES_Constants.ipf
@@ -852,3 +852,5 @@ Constant NUM_ENTRIES_FOR_MULTITHREAD = 16
 StrConstant DAQ_ALLOWED_FUNCTIONS = "DQ_RestartDAQ;DAP_ButtonProc_AcquireData;RA_CounterMD"
 
 StrConstant RESISTANCE_GRAPH = "AnalysisFuncResistanceGraph"
+
+StrConstant CHANNEL_UNIT_KEY = "ChannelUnit"

Packages/MIES/MIES_DataConfiguratorITC.ipf
@@ -368,9 +368,10 @@ static Function DC_MakeITCConfigAllConfigWave(panelTitle, numActiveChannels)
 	string panelTitle
 	variable numActiveChannels
 
-	DFREF dfr = GetDevicePath(panelTitle)
-	Make/I/O/N=(numActiveChannels, 4) dfr:ITCChanConfigWave/Wave=wv
-	wv = 0
+	WAVE config = GetITCChanConfigWave(panelTitle)
+
+	Redimension/N=(numActiveChannels, -1) config
+	FastOp config = 0
 End
 
 /// @brief Creates ITCDataWave; The wave that the ITC device takes DA and TTL data from and passes AD data to for all channels.
@@ -503,9 +504,9 @@ static Function DC_PlaceDataInITCChanConfigWave(panelTitle, dataAcqOrTP)
 			continue
 		endif
 
-		ITCChanConfigWave[j][0] = ITC_XOP_CHANNEL_TYPE_DAC
-		ITCChanConfigWave[j][1] = i
-		unitList = AddListItem(DAG_GetTextualValue(panelTitle, ctrl, index = i), unitList, ";", Inf)
+		ITCChanConfigWave[j][%ChannelType]   = ITC_XOP_CHANNEL_TYPE_DAC
+		ITCChanConfigWave[j][%ChannelNumber] = i
+		unitList = AddListItem(DAG_GetTextualValue(panelTitle, ctrl, index = i), unitList, ",", Inf)
 		j += 1
 	endfor
 
@@ -521,35 +522,36 @@ static Function DC_PlaceDataInITCChanConfigWave(panelTitle, dataAcqOrTP)
 			continue
 		endif
 
-		ITCChanConfigWave[j][0] = ITC_XOP_CHANNEL_TYPE_ADC
-		ITCChanConfigWave[j][1] = i
-		unitList = AddListItem(DAG_GetTextualValue(panelTitle, ctrl, index = i), unitList, ";", Inf)
+		ITCChanConfigWave[j][%ChannelType]   = ITC_XOP_CHANNEL_TYPE_ADC
+		ITCChanConfigWave[j][%ChannelNumber] = i
+		unitList = AddListItem(DAG_GetTextualValue(panelTitle, ctrl, index = i), unitList, ",", Inf)
 		j += 1
 	endfor
 
-	Note ITCChanConfigWave, unitList
+	AddEntryIntoWaveNoteAsList(ITCChanConfigWave, CHANNEL_UNIT_KEY, str = unitList, replaceEntry = 1)
 
-	ITCChanConfigWave[][2] = DAP_GetITCSampInt(panelTitle, dataAcqOrTP)
-	ITCChanConfigWave[][3] = 0
+	ITCChanConfigWave[][%SamplingInterval] = DAP_GetITCSampInt(panelTitle, dataAcqOrTP)
+	ITCChanConfigWave[][%DecimationMode]   = 0
+	ITCChanConfigWave[][%Offset]           = 0
 
 	if(dataAcqOrTP == DATA_ACQUISITION_MODE)
 		WAVE sweepDataLNB = GetSweepSettingsWave(panelTitle)
 
 		if(DC_AreTTLsInRackChecked(RACK_ZERO, panelTitle))
-			ITCChanConfigWave[j][0] = ITC_XOP_CHANNEL_TYPE_TTL
+			ITCChanConfigWave[j][%ChannelType] = ITC_XOP_CHANNEL_TYPE_TTL
 
 			channel = HW_ITC_GetITCXOPChannelForRack(panelTitle, RACK_ZERO)
-			ITCChanConfigWave[j][1] = channel
+			ITCChanConfigWave[j][%ChannelNumber] = channel
 			sweepDataLNB[0][10][INDEP_HEADSTAGE] = channel
 
 			j += 1
 		endif
 
 		if(DC_AreTTLsInRackChecked(RACK_ONE, panelTitle))
-			ITCChanConfigWave[j][0] = ITC_XOP_CHANNEL_TYPE_TTL
+			ITCChanConfigWave[j][%ChannelType] = ITC_XOP_CHANNEL_TYPE_TTL
 
 			channel = HW_ITC_GetITCXOPChannelForRack(panelTitle, RACK_ONE)
-			ITCChanConfigWave[j][1] = channel
+			ITCChanConfigWave[j][%ChannelNumber] = channel
 			sweepDataLNB[0][11][INDEP_HEADSTAGE] = channel
 		endif
 	endif

Packages/MIES/MIES_MiesUtilities.ipf
@@ -1368,6 +1368,19 @@ Function GetSamplingInterval(config)
 	return samplingInterval[0]
 End
 
+/// @brief Returns the data offset of the sweep in points
+Function GetDataOffset(config)
+	Wave config
+
+	ASSERT(IsValidConfigWave(config),"Expected a valid config wave")
+
+	Duplicate/D/R=[][4]/FREE config, offsets
+
+	// The data offset is the same for all channels
+	ASSERT(WaveMax(offsets) == WaveMin(offsets), "Expected constant data offset for all channels")
+	return offsets[0]
+End
+
 /// @brief Write the given property to the config wave
 ///
 /// @note Please add new properties as required

Packages/MIES/MIES_Oscilloscope.ipf
@@ -480,6 +480,8 @@ Function SCOPE_UpdateOscilloscopeData(panelTitle, dataAcqOrTP, [chunk, fifoPos])
 
 		ASSERT(!ParamIsDefault(fifoPos), "optional parameter fifoPos missing")
 
+		fifoPos += GetDataOffset(ITCChanConfigWave)
+
 		if(fifoPos == 0 || !IsFinite(fifoPos))
 			// nothing to do
 			return NaN

Packages/MIES/MIES_WaveDataFolderGetters.ipf
@@ -546,7 +546,37 @@ Function/Wave GetITCDataWave(panelTitle)
 	return wv
 End
 
+static Constant ITC_CONFIG_WAVE_VERSION = 1
+
+/// @brief Check if the given ITC config wave is the latest version
+Function IsLatestConfigWaveVersion(wv)
+	WAVE wv
+
+	return ExistsWithCorrectLayoutVersion(wv, ITC_CONFIG_WAVE_VERSION)
+End
+
 /// @brief Return the ITC channel config wave
+///
+/// Rows:
+/// - One for each channel, the order is DA, AD, TTL (same as in the ITCDataWave)
+///
+/// Columns:
+/// - channel type, one of @ref ITC_XOP_CHANNEL_CONSTANTS
+/// - channel number (0-based)
+/// - sampling interval in microseconds (1e-6)
+/// - decimation mode (always zero)
+/// - data offset
+///
+/// The wave note holds a list of channel units. The order
+/// is the same as the rows. TTL channels don't have units. Querying the
+/// channel unit should always be done via AFH_GetChannelUnit()/AFH_GetChannelUnits().
+///
+/// Version 1 changes:
+/// - Columns now have dimension labels
+/// - One more column with the channel data offset
+/// - Due to the wave versioning the channel unit is now stored with the
+///   #CHANNEL_UNIT_KEY as key and it is now separated not with semicolon
+///   anymore but a comma.
 Function/Wave GetITCChanConfigWave(panelTitle)
 	string panelTitle
 
@@ -554,11 +584,25 @@ Function/Wave GetITCChanConfigWave(panelTitle)
 
 	WAVE/I/Z/SDFR=dfr wv = ITCChanConfigWave
 
-	if(WaveExists(wv))
+	if(ExistsWithCorrectLayoutVersion(wv, ITC_CONFIG_WAVE_VERSION))
 		return wv
+	elseif(WaveExists(wv))
+		Redimension/I/N=(-1, 5) wv
+		// offset
+		wv[][4] = 0
+		Note/K wv
+	else
+		Make/I/N=(2, 5) dfr:ITCChanConfigWave/Wave=wv
 	endif
 
-	Make/I/N=(2, 4) dfr:ITCChanConfigWave/Wave=wv
+	SetDimLabel COLS, 0, ChannelType, wv
+	SetDimLabel COLS, 1, ChannelNumber, wv
+	SetDimLabel COLS, 2, SamplingInterval, wv
+	SetDimLabel COLS, 3, DecimationMode, wv
+	SetDimLabel COLS, 4, Offset, wv
+
+	SetWaveVersion(wv, ITC_CONFIG_WAVE_VERSION)
+	AddEntryIntoWaveNoteAsList(wv, CHANNEL_UNIT_KEY, str = "")
 
 	return wv
 End

commit 2c53fdbb5160f73092d1a450cdb1c606e27417fe

HW_ITC_PrepareAcq: Fix function signature for without hardware XOPs present
Bug introduced in 5b454452 (ITCChanConfigWave: Adapt to new ITC XOP
requirements, 2018-03-08).

Original-CommitID: 0201af0ab74c0c0f24654a5a1ab63f907bddb408

Packages/MIES/MIES_DAC-Hardware.ipf
@@ -1593,11 +1593,11 @@ Function HW_ITC_DebugMode(state, [flags])
 	DEBUGPRINT("Unimplemented")
 End
 
-Function HW_ITC_PrepareAcq(deviceID, [data, dataFunc, config, configFunc, flags])
+Function HW_ITC_PrepareAcq(deviceID, [data, dataFunc, config, configFunc, flags, offset])
 	variable deviceID
 	WAVE/Z data, config
 	FUNCREF HW_WAVE_GETTER_PROTOTYPE dataFunc, configFunc
-	variable flags
+	variable flags, offset
 
 	DEBUGPRINT("Unimplemented")
 End