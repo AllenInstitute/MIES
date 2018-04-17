AllenInstitute/MIES

commit ffd8539ac32431e784b2447fd1515c3d08c33e9d

Labnotebook Getters: Add convenience wrapper for querying settings of the last sweep of a RA cycle
Original-CommitID: 1ff3c05d37b40ebcb4f5d308765cac409b738818

Packages/MIES/MIES_MiesUtilities.ipf
@@ -435,6 +435,54 @@ Function/S GetLastSettingTextIndep(textualValues, sweepNo, setting, entrySourceT
 	endif
 End
 
+/// @brief Return a headstage independent setting from the numerical
+///        labnotebook of the sweeps in the same RA cycle
+///
+/// @return the headstage independent setting or `defValue`
+Function GetLastSettingIndepRAC(numericalValues, sweepNo, setting, entrySourceType, [defValue])
+	Wave numericalValues
+	variable sweepNo
+	string setting
+	variable defValue, entrySourceType
+
+	if(ParamIsDefault(defValue))
+		defValue = NaN
+	endif
+
+	WAVE/Z settings = GetLastSettingRAC(numericalValues, sweepNo, setting, entrySourceType)
+
+	if(WaveExists(settings))
+		return settings[GetIndexForHeadstageIndepData(numericalValues)]
+	else
+		DEBUGPRINT("Missing setting in labnotebook", str=setting)
+		return defValue
+	endif
+End
+
+/// @brief Return a headstage independent setting from the numerical
+///        labnotebook of the sweeps in the same RA cycle
+///
+/// @return the headstage independent setting or `defValue`
+Function/S GetLastSettingTextIndepRAC(textualValues, sweepNo, setting, entrySourceType, [defValue])
+	Wave/T textualValues
+	variable sweepNo
+	string setting, defValue
+	variable entrySourceType
+
+	if(ParamIsDefault(defValue))
+		defValue = ""
+	endif
+
+	WAVE/T/Z settings = GetLastSettingTextRAC(textualValues, sweepNo, setting, entrySourceType)
+
+	if(WaveExists(settings))
+		return settings[GetIndexForHeadstageIndepData(textualValues)]
+	else
+		DEBUGPRINT("Missing setting in labnotebook", str=setting)
+		return defValue
+	endif
+End
+
 /// @brief Returns a wave with the latest value of a setting from the history wave
 /// for a given sweep number.
 ///
@@ -702,6 +750,62 @@ Function/WAVE GetLastSettingText(textualValues, sweepNo, setting, entrySourceTyp
 		// return if we have at least one non-empty entry
 		if(Sum(lengths) > 0)
 			return status
+		endif
+	endfor
+
+	return $""
+End
+
+/// @brief Return the last textual value of the sweeps in the same RA cycle
+///
+/// @see GetLastSettingText
+Function/WAVE GetLastSettingTextRAC(textualValues, sweepNo, setting, entrySourceType)
+	WAVE textualValues
+	variable sweepNo
+	string setting
+	variable entrySourceType
+
+	variable i, numSweeps
+
+	WAVE/Z sweeps = AFH_GetSweepsFromSameRACycle(textualValues, sweepNo)
+	if(!WaveExists(sweeps) || DimSize(sweeps, ROWS) == 0)
+		return $""
+	endif
+
+	numSweeps = DimSize(sweeps, ROWS)
+	for(i = numSweeps - 1; i >= 0; i += 1)
+		WAVE/Z settings = GetLastSettingText(textualValues, sweeps[i], setting, entrySourceType)
+
+		if(WaveExists(settings))
+			return settings
+		endif
+	endfor
+
+	return $""
+End
+
+/// @brief Return the last numerical value of the sweeps in the same RA cycle
+///
+/// @see GetLastSetting
+Function/WAVE GetLastSettingRAC(numericalValues, sweepNo, setting, entrySourceType)
+	WAVE numericalValues
+	variable sweepNo
+	string setting
+	variable entrySourceType
+
+	variable i, numSweeps
+
+	WAVE/Z sweeps = AFH_GetSweepsFromSameRACycle(numericalValues, sweepNo)
+	if(!WaveExists(sweeps) || DimSize(sweeps, ROWS) == 0)
+		return $""
+	endif
+
+	numSweeps = DimSize(sweeps, ROWS)
+	for(i = numSweeps - 1; i >= 0; i -= 1)
+		WAVE/Z settings = GetLastSetting(numericalValues, sweeps[i], setting, entrySourceType)
+
+		if(WaveExists(settings))
+			return settings
 		endif
 	endfor
 

commit 6611bf7bf4b0b9779e5591d226863654e8ea6cfe

GetLastSettingTextRAC: Avoid endless loop
We want to count downwards so we have to remove one on each iteration.

Bug introduced in ffd8539a (Labnotebook Getters: Add convenience wrapper
for querying settings of the last sweep of a RA cycle, 2017-10-24).

Original-CommitID: 3c65954b3155b8cf6cd729507cd98087e39c46dd

Packages/MIES/MIES_MiesUtilities.ipf
@@ -836,7 +836,7 @@ Function/WAVE GetLastSettingTextRAC(textualValues, sweepNo, setting, entrySource
 	endif
 
 	numSweeps = DimSize(sweeps, ROWS)
-	for(i = numSweeps - 1; i >= 0; i += 1)
+	for(i = numSweeps - 1; i >= 0; i -= 1)
 		WAVE/Z settings = GetLastSettingText(textualValues, sweeps[i], setting, entrySourceType)
 
 		if(WaveExists(settings))