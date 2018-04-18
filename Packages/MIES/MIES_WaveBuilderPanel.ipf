AllenInstitute/MIES

commit 77a4233a09cf402602b43e3a99f14ab6ada055bc

Use differnt colors for sweeps in the wavebuilder
This avoids confusion when you have both the wavebuilder and the
databrowser open.

Original-CommitID: b98615672cc8de8ea0001c2691057cbe163dc913

Packages/MIES/MIES_WaveBuilderPanel.ipf
@@ -949,7 +949,7 @@ static Function WBP_DisplaySetInPanel()
 	for(i = 0; i < numSweeps; i += 1)
 		trace = NameOfWave(displayData) + "_S" + num2str(i)
 		AppendToGraph/W=$waveBuilderGraph displayData[][i]/TN=$trace
-		GetTraceColor(i, red, green, blue)
+		GetSweepColor(i, red, green, blue)
 		ModifyGraph/W=$waveBuilderGraph rgb($trace) = (red, green, blue)
 	endfor
 
@@ -2339,7 +2339,7 @@ Function WBP_ShowFFTSpectrumIfReq(segmentWave, sweep)
 	ModifyGraph/W=$graphPhase log(bottom)=1
 	ModifyGraph/W=$graphPhase mode=4
 
-	GetTraceColor(sweep, red, green, blue)
+	GetSweepColor(sweep, red, green, blue)
 	ModifyGraph/W=$graphMag rgb($trace)   = (red, green, blue)
 	ModifyGraph/W=$graphPhase rgb($trace) = (red, green, blue)
 
@@ -2351,4 +2351,13 @@ Function WBP_ShowFFTSpectrumIfReq(segmentWave, sweep)
 
 	RestoreCursor(graphPhase, cursorInfoPhaseA)
 	RestoreCursor(graphPhase, cursorInfoPhaseB)
+End
+
+/// @brief Return distinct colors the sweeps of the wavebuilder
+///
+/// These are backwards compared to the trace colors
+static Function GetSweepColor(sweep, red, green, blue)
+	variable sweep, &red, &green, &blue
+
+	return GetTraceColor(20 - sweep, red, green, blue)
 End

commit b7b4d3d12eaf8a4f06da76e4f1fe90afe6c848c7

WBP_GetSweepColor: Support stimsets with more than 20 sweeps
Broken since 77a4233a (Use differnt colors for sweeps in the
wavebuilder, 2017-01-13).

Original-CommitID: b7d65baa1905c99369d1de08d17b384332f4c485

Packages/MIES/MIES_WaveBuilderPanel.ipf
@@ -2463,7 +2463,7 @@ End
 static Function WBP_GetSweepColor(sweep, red, green, blue)
 	variable sweep, &red, &green, &blue
 
-	return GetTraceColor(20 - sweep, red, green, blue)
+	return GetTraceColor(20 - mod(sweep, 20), red, green, blue)
 End
 
 /// @brief Add an analysis function parameter to the given stimset