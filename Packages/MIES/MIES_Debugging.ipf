AllenInstitute/MIES

commit 4e8c18f80487af5ada17410be7e71d0fa698408a

Remove stale wrapper functions
Not required anymore since 7f7f9643 (Remove module support,
2018-03-21)..

Original-CommitID: bf51a664c3792c0efa4d1c1b704d9e40993ba53a

Packages/MIES/MIES_Debugging.ipf
@@ -155,7 +155,7 @@ Function DEBUGPRINT(msg, [var, str, format])
 
 	FindFirstOutsideCaller(func, line, file)
 
-	if(!IsEmpty(file) && !DebuggingEnabledForFileWrapper(file))
+	if(!IsEmpty(file) && !DP_DebuggingEnabledForFile(file))
 		return NaN
 	endif
 
@@ -248,7 +248,7 @@ Function DEBUGPRINTSTACKINFO()
 
 	FindFirstOutsideCaller(func, line, file)
 
-	if(!IsEmpty(file) && !DebuggingEnabledForFileWrapper(file))
+	if(!IsEmpty(file) && !DP_DebuggingEnabledForFile(file))
 		return NaN
 	endif
 
@@ -423,22 +423,6 @@ End
 Function DisableEvilMode()
 	Execute/P/Q "SetIgorOption poundUnDefine=EVIL_KITTEN_EATING_MODE"
 	Execute/P/Q "COMPILEPROCEDURES "
-End
-
-/// @brief Prototype for DebuggingEnabledForFileWrapper()
-Function DebuggingEnabledForFileSimple(file)
-	string file
-
-	return 1
-End
-
-/// @brief Wrapper for DP_DebuggingEnabledForFile()
-Function DebuggingEnabledForFileWrapper(file)
-	string file
-
-	FUNCREF DebuggingEnabledForFileSimple f = $"DP_DebuggingEnabledForFile"
-
-	return f(file)
 End
 
 /// @brief Complain and ask the user to report the error

Packages/MIES/MIES_IgorHooks.ipf
@@ -62,32 +62,6 @@ Function IH_KillStimSets()
 	CallFunctionForEachListItem(KillOrMoveToTrashPath, list)
 End
 
-/// @brief Prototype function for #IH_UnlockAllDevicesWrapper
-Function IH_UnlockAllDevicesProto()
-
-End
-
-/// @brief Prototype function for #IH_SerAllCommentNBsWrapper
-Function IH_SerAllCommentNBsProto()
-
-End
-
-/// @brief Calls `DAP_UnlockAllDevices` if it can be found,
-/// otherwise calls `IH_UnlockAllDevicesProto` which does nothing.
-static Function IH_UnlockAllDevicesWrapper()
-
-	FUNCREF IH_UnlockAllDevicesProto f = $"DAP_UnlockAllDevices"
-	f()
-End
-
-/// @brief Calls #DAP_SerializeAllCommentNBs if it can be found,
-/// otherwise calls #IH_SerAllCommentNBsProto which does nothing.
-static Function IH_SerAllCommentNBsWrapper()
-
-	FUNCREF IH_SerAllCommentNBsProto f = $"DAP_SerializeAllCommentNBs"
-	f()
-End
-
 static Function BeforeExperimentSaveHook(rN, fileName, path, type, creator, kind)
 	Variable rN, kind
 	String fileName, path, type, creator
@@ -97,7 +71,7 @@ static Function BeforeExperimentSaveHook(rN, fileName, path, type, creator, kind
 		return NaN
 	endif
 
-	IH_SerAllCommentNBsWrapper()
+	DAP_SerializeAllCommentNBs()
 	IH_KillTemporaries()
 #if !defined(IGOR64)
 	IH_KillStimSets()
@@ -121,7 +95,7 @@ static Function IH_Cleanup()
 	debuggerState = DisableDebugger()
 
 	try
-		IH_UnlockAllDevicesWrapper(); AbortOnRTE
+		DAP_UnlockAllDevices(); AbortOnRTE
 		IH_RemoveAmplifierConnWaves(); AbortOnRTE
 		IH_KillTemporaries(); AbortOnRTE
 		IH_KillStimSets(); AbortOnRTE

Packages/MIES/MIES_MiesUtilities.ipf
@@ -2496,10 +2496,10 @@ Function SaveExperimentSpecial(mode)
 			list = GetListOfObjects(dfr, "TPStorage_*", fullPath=1)
 			CallFunctionForEachListItem(killFunc, list)
 
-			RemoveTracesFromGraph(GetScopeGraphWrapper(device))
+			RemoveTracesFromGraph(SCOPE_GetGraph(device))
 		endfor
 
-		ClearDataBrowserGraphsWrapper()
+		DB_ClearAllGraphs()
 	endif
 
 	SaveExperiment
@@ -2522,28 +2522,6 @@ Function/S CleanupExperimentName(expName)
 	expName  = RemoveEnding(expName, SIBLING_FILENAME_SUFFIX)
 
 	return expName
-End
-
-Function ClearDataBrowserGraphsProto()
-
-End
-
-static Function ClearDataBrowserGraphsWrapper()
-
-	FUNCREF ClearDataBrowserGraphsProto f = $"DB_ClearAllGraphs"
-	f()
-End
-
-Function/S GetScopeGraphProto(panelTitle)
-	string panelTitle
-
-End
-
-static Function/S GetScopeGraphWrapper(panelTitle)
-	string panelTitle
-
-	FUNCREF GetScopeGraphProto f = $"SCOPE_GetGraph"
-	return f(panelTitle)
 End
 
 /// @brief Return the maximum count of the given type
@@ -4352,22 +4330,6 @@ Function/S ConvertAmplifierModeToString(mode)
 			return "Unknown mode (" + num2str(mode) + ")"
 			break
 	endswitch
-End
-
-/// @brief Prototype function for DAP_UpdateDAQControls
-Function UpdateDAQControlsProto(panelTitle, updateFlag)
-	string panelTitle
-	variable updateFlag
-End
-
-/// @brief Prototype function for DAP_UpdateDAQControls
-Function UpdateDAQControlsWrapper(panelTitle, updateFlag)
-	string panelTitle
-	variable updateFlag
-
-	FUNCREF UpdateDAQControlsProto f = $"DAP_UpdateDAQControls"
-
-	f(panelTitle, updateFlag)
 End
 
 /// @brief Update the repurposed sweep time global variable

Packages/MIES/MIES_OverlaySweeps.ipf
@@ -49,27 +49,11 @@ Function OVS_IgnoreHeadstageInOverlay()
 	if(!IsEmpty(folder))
 		WAVE traceWave     = TraceNameToWaveRef(graph, trace)
 		DFREF sweepDataDFR = GetWavesDataFolderDFR(traceWave)
-		index = OVS_GetIndexFromSweepDataPathW(graph, sweepDataDFR)
+		index = SB_GetIndexFromSweepDataPath(graph, sweepDataDFR)
 		OVS_AddToIgnoreList(extPanel, headstage, index=index)
 	else
 		OVS_AddToIgnoreList(extPanel, headstage, sweepNo=sweepNo)
 	endif
-End
-
-Function OVS_GetIndexFromSweepDataPathP(graph, dataDFR)
-	string graph
-	DFREF dataDFR
-
-	ASSERT(0, "Can't call prototype function")
-End
-
-Function OVS_GetIndexFromSweepDataPathW(graph, dataDFR)
-	string graph
-	DFREF dataDFR
-
-	FUNCREF OVS_GetIndexFromSweepDataPathP f = $"SB_GetIndexFromSweepDataPath"
-
-	return f(graph, dataDFR)
 End
 
 /// @brief Return a list of choices for the sweep selection popup

Packages/MIES/MIES_WaveBuilderPanel.ipf
@@ -2039,7 +2039,7 @@ Function WBP_UpdateITCPanelPopUps([panelTitle])
 			SetControlUserData(panelTitle, ctrlIndexEnd, "MenuExp", TTLlist)
 		endfor
 
-		UpdateDAQControlsWrapper(panelTitle, REASON_STIMSET_CHANGE)
+		DAP_UpdateDAQControls(panelTitle, REASON_STIMSET_CHANGE)
 	endfor
 End
 

Packages/MIES/MIES_WaveDataFolderGetters.ipf
@@ -4391,12 +4391,12 @@ Function/Wave GetDA_EphysGuiStateNum(panelTitle)
 	elseif(WaveExists(wv)) // handle upgrade
 		// change the required dimensions and leave all others untouched with -1
 		// the extended dimensions are initialized with zero
-		uniqueCtrlList = GetUniqueSpecCtrlTypeListWrp(panelTitle, WAVE_TYPE_NUMERICAL)
+		uniqueCtrlList = DAG_GetUniqueSpecCtrlListNum(panelTitle)
 		uniqueCtrlCount = itemsInList(uniqueCtrlList)
 		Redimension/D/N=(NUM_MAX_CHANNELS, COMMON_CONTROL_GROUP_COUNT_NUM + uniqueCtrlCount, -1, -1) wv
 		wv = Nan
 	else
-		uniqueCtrlList = GetUniqueSpecCtrlTypeListWrp(panelTitle, WAVE_TYPE_NUMERICAL)
+		uniqueCtrlList = DAG_GetUniqueSpecCtrlListNum(panelTitle)
 		uniqueCtrlCount = itemsInList(uniqueCtrlList)
 		Make/N=(NUM_MAX_CHANNELS, COMMON_CONTROL_GROUP_COUNT_NUM + uniqueCtrlCount)/D dfr:DA_EphysGuiStateNum/Wave=wv
 		wv = Nan
@@ -4423,7 +4423,7 @@ Function/Wave GetDA_EphysGuiStateNum(panelTitle)
 	SetWaveDimLabel(wv, uniqueCtrlList, COLS, startPos = COMMON_CONTROL_GROUP_COUNT_NUM)
 	SetWaveVersion(wv, DA_EPHYS_PANEL_VERSION)
 	// needs to be called after setting the wave version in order to avoid infinite recursion
-	RecordGuiStateWrapper(panelTitle, WAVE_TYPE_NUMERICAL, wv)
+	DAG_RecordGuiStateNum(panelTitle, GuiState = wv)
 	return wv
 End
 
@@ -4457,12 +4457,12 @@ Function/Wave GetDA_EphysGuiStateTxT(panelTitle)
 	elseif(WaveExists(wv)) // handle upgrade
 		// change the required dimensions and leave all others untouched with -1
 		// the extended dimensions are initialized with zero
-		uniqueCtrlList = GetUniqueSpecCtrlTypeListWrp(panelTitle, WAVE_TYPE_TEXTUAL)
+		uniqueCtrlList = DAG_GetUniqueSpecCtrlListTxt(panelTitle)
 		uniqueCtrlCount = itemsInList(uniqueCtrlList)
 		Redimension/N=(NUM_MAX_CHANNELS, COMMON_CONTROL_GROUP_COUNT_TXT + uniqueCtrlCount, -1, -1) wv
 		wv = ""
 	else
-		uniqueCtrlList = GetUniqueSpecCtrlTypeListWrp(panelTitle, WAVE_TYPE_TEXTUAL)
+		uniqueCtrlList = DAG_GetUniqueSpecCtrlListTxt(panelTitle)
 		uniqueCtrlCount = itemsInList(uniqueCtrlList)
 		Make/T/N=(NUM_MAX_CHANNELS, COMMON_CONTROL_GROUP_COUNT_TXT + uniqueCtrlCount) dfr:DA_EphysGuiStateTxT/Wave=wv
 		wv = ""
@@ -4482,62 +4482,8 @@ Function/Wave GetDA_EphysGuiStateTxT(panelTitle)
 	SetWaveDimLabel(wv, uniqueCtrlList, COLS, startPos = COMMON_CONTROL_GROUP_COUNT_TXT)
 	SetWaveVersion(wv, DA_EPHYS_PANEL_VERSION)
 	// needs to be called after setting the wave version in order to avoid infinite recursion
-	RecordGuiStateWrapper(panelTitle, WAVE_TYPE_TEXTUAL, wv)
+	DAG_RecordGuiStateTxT(panelTitle, Guistate = wv)
 	return wv
-End
-
-static Function/S GetUniqueSpecCtrlTypeListWrp(panelTitle, type)
-	string panelTitle
-	variable type
-
-	switch(type)
-		case WAVE_TYPE_NUMERICAL:
-			FUNCREF GetUniqueSpecCtrlTypeListProto f = $"DAG_GetUniqueSpecCtrlListNum"
-			break
-		case WAVE_TYPE_TEXTUAL:
-			FUNCREF GetUniqueSpecCtrlTypeListProto f = $"DAG_GetUniqueSpecCtrlListTxT"
-			break
-		default:
-			ASSERT(0, "Unknown type")
-			break
-	endswitch
-
-	return f(panelTitle)
-End
-
-Function/S GetUniqueSpecCtrlTypeListProto(panelTitle)
-	string panelTitle
-
-	ASSERT(0, "Prototype function can not be called")
-End
-
-/// @brief Calls DAG_RecordGuiStateNum()/DAG_RecordGuiStateTxT if it can be found,
-/// otherwise calls RecordGuiStateProto() which aborts.
-static Function RecordGuiStateWrapper(str, type, wv)
-	string str
-	variable type
-	WAVE wv
-
-	switch(type)
-		case WAVE_TYPE_NUMERICAL:
-			FUNCREF RecordGuiStateProto f = $"DAG_RecordGuiStateNum"
-			break
-		case WAVE_TYPE_TEXTUAL:
-			FUNCREF RecordGuiStateProto f = $"DAG_RecordGuiStateTxT"
-			break
-		default:
-			ASSERT(0, "Unknown type")
-			break
-	endswitch
-
-	f(str, GUISTATE = wv)
-End
-
-Function RecordGuiStateProto(str, [GUISTATE])
-	string str
-	WAVE GUISTATE
-
-	ASSERT(0, "Prototype function can not be called")
 End
 
 /// @brief Return the datafolder reference to the NeuroDataWithoutBorders folder,

commit 84f0d38dc118fefafdbb38b271abbbebad6dea7c

Make debug mode compile again
Broken since 4e8c18f8 (Remove stale wrapper functions, 2018-05-10).

The BkgWatcher IM includes MIES_Debugging.ipf but not
MIES_DebugPanel.ipf so that wrapper is really required.

Original-CommitID: 55af6a2fd21c4060904b73fa6848837003086775

Packages/MIES/MIES_Debugging.ipf
@@ -162,7 +162,7 @@ Function DEBUGPRINT(msg, [var, str, format])
 
 	FindFirstOutsideCaller(func, line, file)
 
-	if(!IsEmpty(file) && !DP_DebuggingEnabledForFile(file))
+	if(!IsEmpty(file) && !DebuggingEnabledForFileWrapper(file))
 		return NaN
 	endif
 
@@ -257,7 +257,7 @@ Function DEBUGPRINTSTACKINFO()
 
 	FindFirstOutsideCaller(func, line, file)
 
-	if(!IsEmpty(file) && !DP_DebuggingEnabledForFile(file))
+	if(!IsEmpty(file) && !DebuggingEnabledForFileWrapper(file))
 		return NaN
 	endif
 
@@ -434,6 +434,22 @@ End
 Function DisableEvilMode()
 	Execute/P/Q "SetIgorOption poundUnDefine=EVIL_KITTEN_EATING_MODE"
 	Execute/P/Q "COMPILEPROCEDURES "
+End
+
+/// @brief Prototype for DebuggingEnabledForFileWrapper()
+Function DebuggingEnabledForFileSimple(file)
+	string file
+
+	return 1
+End
+
+/// @brief Wrapper for DP_DebuggingEnabledForFile()
+Function DebuggingEnabledForFileWrapper(file)
+	string file
+
+	FUNCREF DebuggingEnabledForFileSimple f = $"DP_DebuggingEnabledForFile"
+
+	return f(file)
 End
 
 /// @brief Complain and ask the user to report the error