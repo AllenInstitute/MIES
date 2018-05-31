AllenInstitute/MIES

commit 798b9bf4bfd2ed83a316cba08fee62fb89868de6

Analysis functions: Add central storage wave to make calling them faster
Until now we always parsed the stimset wave note to call an analysis
function. This includes a considerable overhead. Use a storage wave for
having quick access to the analysis function names.

Function Dostuff()

	variable refTime = stopmstimer(-2)
	AFM_CallAnalysisFunctions("ITC18USB_DEV_0", POST_SWEEP_EVENT)
	print (stopmstimer(-2) - refTime) / 1e6
End

takes 15ms for DoStuff() when TestAnalysisFunction_V1 is setup for the
post sweep event. With the analysis function storage wave this
goes done to 3ms. For unset analysis functions, especially relevant for
mid sweep event, this goes done from 1.5ms to 0.1ms.

Original-CommitID: 6f38281eb1795b6ed0f7cff35e92d7cf28aabdfa

Packages/MIES/MIES_DataConfiguratorITC.ipf
@@ -37,6 +37,10 @@ static Function DC_UpdateGlobals(panelTitle, dataAcqOrTP)
 
 	DAP_RecordDA_EphysGuiState(panelTitle)
 
+	// we need to update the list of analysis functions here as the stimset
+	// can change due to indexing, etc.
+	AFM_UpdateAnalysisFunctionWave(panelTitle)
+
 	pulseDuration = GetSetVariable(panelTitle, "SetVar_DataAcq_TPDuration")
 	duration = pulseDuration / (DAP_GetITCSampInt(panelTitle, TEST_PULSE_MODE) / 1000)
 	baselineFrac = GetSetVariable(panelTitle, "SetVar_DataAcq_TPBaselinePerc") / 100

Packages/MIES/MIES_AnalysisFunctionManagement.ipf
@@ -16,8 +16,8 @@ Function AFM_CallAnalysisFunctions(panelTitle, eventType)
 	string panelTitle
 	variable eventType
 
-	variable error, i, valid_f1, valid_f2, ret
-	string func, setName
+	variable error, i, valid_f1, valid_f2, ret, DAC
+	string func, setName, ctrl
 
 	WAVE GuiState = GetDA_EphysGuiStateNum(panelTitle)
 
@@ -29,15 +29,17 @@ Function AFM_CallAnalysisFunctions(panelTitle, eventType)
 	NVAR stopCollectionPoint = $GetStopCollectionPoint(panelTitle)
 	WAVE statusHS = DAP_ControlStatusWaveCache(panelTitle, CHANNEL_TYPE_HEADSTAGE)
 
+	WAVE/T analysisFunctions = GetAnalysisFunctionStorage(panelTitle)
+
 	for(i = 0; i < NUM_HEADSTAGES; i += 1)
 
 		if(!statusHS[i])
 			continue
 		endif
 
-		AFM_GetAnalysisFuncFromHS(panelTitle, i, eventType, func, setName)
+		func = analysisFunctions[i][eventType]
 
-		if(isEmpty(func) || isEmpty(setName))
+		if(isEmpty(func))
 			continue
 		endif
 
@@ -49,6 +51,11 @@ Function AFM_CallAnalysisFunctions(panelTitle, eventType)
 				// nothing to do
 				break
 			case POST_SET_EVENT:
+				DAC = AFH_GetDACFromHeadstage(panelTitle, i)
+
+				ctrl    = GetPanelControl(DAC, CHANNEL_TYPE_DAC, CHANNEL_CONTROL_WAVE)
+				setName = GetPopupMenuString(panelTitle, ctrl)
+
 				if(mod(count + 1, IDX_NumberOfTrialsInSet(setName)) != 0)
 					continue
 				endif
@@ -97,54 +104,37 @@ Function AFM_CallAnalysisFunctions(panelTitle, eventType)
 	return 0
 End
 
-/// @brief Get the analysis function and the stimset from the headstage
+/// @brief Update the analysis function storage wave from the stimset waves notes
 ///
 /// We are called earlier than DAP_CheckSettings() so we can not rely on anything setup in a sane way.
-///
-/// @param[in]  panelTitle Device
-/// @param[in]  headStage  Headstage
-/// @param[in]  eventType  One of @ref EVENT_TYPE_ANALYSIS_FUNCTIONS
-/// @param[out] func       Analysis function name
-/// @param[out] setName    Name of the Stim set
-static Function AFM_GetAnalysisFuncFromHS(panelTitle, headStage, eventType, func, setName)
+Function AFM_UpdateAnalysisFunctionWave(panelTitle)
 	string panelTitle
-	variable headStage, eventType
-	string &func, &setName
 
-	string ctrl, dacWave, setNameFromCtrl
-	variable clampMode, DACchannel
+	variable i, j, DAC
+	string ctrl, setName
 
-	func    = ""
-	setName = ""
+	WAVE statusHS            = DAP_ControlStatusWaveCache(panelTitle, CHANNEL_TYPE_HEADSTAGE)
+	WAVE/T analysisFunctions = GetAnalysisFunctionStorage(panelTitle)
 
-	WAVE chanAmpAssign = GetChanAmpAssign(panelTitle)
+	analysisFunctions = ""
 
-	clampMode = DAP_MIESHeadstageMode(panelTitle, headStage)
-	if(clampMode == V_CLAMP_MODE)
-		DACchannel = ChanAmpAssign[%VC_DA][headStage]
-	elseif(clampMode == I_CLAMP_MODE)
-		DACchannel = ChanAmpAssign[%IC_DA][headStage]
-	else
-		return NaN
-	endif
+	for(i = 0; i < NUM_HEADSTAGES; i += 1)
 
-	if(!IsFinite(DACchannel))
-		return NaN
-	endif
+		if(!statusHS[i])
+			continue
+		endif
 
-	ctrl = GetPanelControl(DACchannel, CHANNEL_TYPE_DAC, CHANNEL_CONTROL_WAVE)
-	setNameFromCtrl = GetPopupMenuString(panelTitle, ctrl)
+		DAC = AFH_GetDACFromHeadstage(panelTitle, i)
 
-	if(!cmpstr(setNameFromCtrl, NONE))
-		return NaN
-	endif
+		ctrl    = GetPanelControl(DAC, CHANNEL_TYPE_DAC, CHANNEL_CONTROL_WAVE)
+		setName = GetPopupMenuString(panelTitle, ctrl)
 
-	WAVE/Z stimSet = WB_CreateAndGetStimSet(setNameFromCtrl)
+		WAVE/Z stimSet = WB_CreateAndGetStimSet(setName)
 
-	if(!WaveExists(stimSet))
-		return NaN
-	endif
+		if(!WaveExists(stimSet))
+			continue
+		endif
 
-	func    = ExtractAnalysisFuncFromStimSet(stimSet, eventType)
-	setName = setNameFromCtrl
+		analysisFunctions[i][] = ExtractAnalysisFuncFromStimSet(stimSet, q)
+	endfor
 End

Packages/MIES/MIES_PanelITC.ipf
@@ -8057,6 +8057,7 @@ Function DAP_UpdateDAQControls(panelTitle, updateFlag)
 	if(updateFlag & REASON_STIMSET_CHANGE)
 		DAP_UpdateITIAcrossSets(panelTitle)
 		DAP_UpdateSweepSetVariables(panelTitle)
+		AFM_UpdateAnalysisFunctionWave(panelTitle)
 	endif
 
 	if(updateFlag & REASON_HEADSTAGE_CHANGE)

Packages/MIES/MIES_WaveDataFolderGetters.ipf
@@ -5144,3 +5144,22 @@ Function/WAVE GetAnalysisFuncDAScaleResFit(panelTitle, headstage)
 End
 
 /// @}
+
+/// @brief Return the storage wave for the analysis functions
+Function/WAVE GetAnalysisFunctionStorage(panelTitle)
+	string panelTitle
+
+	variable versionOfWave = 0
+	DFREF dfr = GetDevicePath(panelTitle)
+	WAVE/T/Z/SDFR=dfr wv = analysisFunctions
+
+	if(ExistsWithCorrectLayoutVersion(wv, versionOfWave))
+		return wv
+	elseif(WaveExists(wv))
+		 // handle upgrade
+	else
+		Make/T/N=(NUM_HEADSTAGES, TOTAL_NUM_EVENTS) dfr:analysisFunctions/WAVE=wv
+	endif
+
+	return wv
+End

commit 068b1023e1a400813f2c038b8d0a3c86050f9e99

Indexing: Fix missing call to DAP_UpdateDAQControls
The indexing code still changes the stimsets via calls to
SetPopupMenuIndex.

This means that the procedure for updating all internal waves after a
stimset changed was not called. DAP_UpdateDAQControls with
REASON_STIMSET_CHANGE_DUR_DAQ does, among other things, call
AFM_UpdateAnalysisFunctionWave which extracts the analysis functions
from the current stimset. And this has to be done after every indexing
step.

Found with patchseq PSQ_SquarePulse and PSQ_Rheobase and indexing turned
on.

The code already smelled a bit as we were the only outside caller of
DAP_UpdateITIAcrossSets which already looks odd.

Broken since 798b9bf4 (Analysis functions: Add central storage wave to
make calling them faster, 2017-10-24).

We can not just use REASON_STIMSET_CHANGE as that changes the sweep set
variables.

Original-CommitID: 50650c14c2cb7622f3be8684f6e4e7a03c4b6031

Packages/MIES/MIES_Constants.ipf
@@ -773,8 +773,9 @@ StrConstant STIMSET_ACQ_CYCLE_ID_KEY = "Stimset Acq Cycle ID"
 ///
 /// @anchor UpdateControlsFlags
 /// @{
-Constant REASON_STIMSET_CHANGE   = 0x01
-Constant REASON_HEADSTAGE_CHANGE = 0x02
+Constant REASON_STIMSET_CHANGE         = 0x01
+Constant REASON_HEADSTAGE_CHANGE       = 0x02
+Constant REASON_STIMSET_CHANGE_DUR_DAQ = 0x04
 /// @}
 
 /// Parameters for GetLastSetting()/GetLastSettingText() for using the row caching

Packages/MIES/MIES_DAEphys.ipf
@@ -1264,7 +1264,7 @@ Function DAP_ButtonProc_AllChanOff(ba) : ButtonControl
 	endswitch
 End
 
-Function DAP_UpdateITIAcrossSets(panelTitle)
+static Function DAP_UpdateITIAcrossSets(panelTitle)
 	string panelTitle
 
 	variable numActiveDAChannels, maxITI
@@ -4499,6 +4499,9 @@ Function DAP_UpdateDAQControls(panelTitle, updateFlag)
 	if(updateFlag & REASON_STIMSET_CHANGE)
 		DAP_UpdateITIAcrossSets(panelTitle)
 		DAP_UpdateSweepSetVariables(panelTitle)
+		AFM_UpdateAnalysisFunctionWave(panelTitle)
+	elseif(updateFlag & REASON_STIMSET_CHANGE_DUR_DAQ)
+		DAP_UpdateITIAcrossSets(panelTitle)
 		AFM_UpdateAnalysisFunctionWave(panelTitle)
 	endif
 

Packages/MIES/MIES_DataConfiguratorITC.ipf
@@ -37,6 +37,7 @@ static Function DC_UpdateGlobals(panelTitle, dataAcqOrTP)
 
 	// we need to update the list of analysis functions here as the stimset
 	// can change due to indexing, etc.
+	// @todo investigate if this is really required here
 	AFM_UpdateAnalysisFunctionWave(panelTitle)
 
 	pulseDuration = DAG_GetNumericalValue(panelTitle, "SetVar_DataAcq_TPDuration")

Packages/MIES/MIES_Indexing.ipf
@@ -62,6 +62,8 @@ Function IDX_ResetStartFinishForIndexing(panelTitle)
 		WAVE stimsets = IDX_GetStimsets(panelTitle, i, CHANNEL_TYPE_TTL)
 		DAG_Update(panelTitle, ctrl, val = idx, str = IDX_GetSingleStimset(stimsets, idx, allowNone = 1))
 	endfor
+
+	DAP_UpdateDAQControls(panelTitle, REASON_STIMSET_CHANGE_DUR_DAQ)
 End
 
 /// @brief Locked indexing, indexes all active channels at once
@@ -75,10 +77,12 @@ Function IDX_IndexingDoIt(panelTitle)
 		IDX_IndexSingleChannel(panelTitle, CHANNEL_TYPE_TTL, i)
 	endfor
 
-	DAP_UpdateITIAcrossSets(panelTitle)
+	DAP_UpdateDAQControls(panelTitle, REASON_STIMSET_CHANGE_DUR_DAQ)
 End
 
 /// @brief Indexes a single channel - used when indexing is unlocked
+///
+/// Callers need to call DAP_UpdateDAQControls() with #REASON_STIMSET_CHANGE_DUR_DAQ.
 static Function IDX_IndexSingleChannel(panelTitle, channelType, i)
 	string panelTitle
 	variable channelType, i
@@ -512,7 +516,7 @@ Function IDX_ApplyUnLockedIndexing(panelTitle, count, DAorTTL)
 	endfor
 
 	if(update)
-		DAP_UpdateITIAcrossSets(panelTitle)
+		DAP_UpdateDAQControls(panelTitle, REASON_STIMSET_CHANGE_DUR_DAQ)
 	endif
 End
 