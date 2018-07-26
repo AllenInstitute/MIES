AllenInstitute/MIES

commit 4b420e389a2ad31b19f15b88f8d7543b2ad36e37

DP_OpenDebugPanel: Initialize debug checkbox on panel creation
Original-CommitID: fb3f235352d180125f701e36fdf6228c436f194a

Packages/MIES/MIES_DebugPanel.ipf
@@ -41,6 +41,9 @@ Function DP_OpenDebugPanel()
 	WAVE/T listWave  = GetDebugPanelListWave()
 	WAVE listSelWave = GetDebugPanelListSelWave()
 	ListBox listbox_mies_files win=$PANEL, listWave=listWave, selWave=listSelWave
+
+	SetCheckBoxState(PANEL, "check_debug_mode", QuerySetIgorOption("DEBUGGING_ENABLED") == 1)
+	// we can't readout the ITC XOP debugging state
 End
 
 Window DP_DebugPanel() : Panel

commit 53cb74e429c0832823d60198ad2555942409596e

DP_OpenDebugPanel: Fix debug flag initialization
The DEBUGGING_ENABLED symbol is set via SetIgorOption poundDefine so we
have to query it with a different syntax.

Broken since 4b420e38 (DP_OpenDebugPanel: Initialize debug checkbox on
panel creation, 2017-05-31).

Original-CommitID: 249b7e68128bcb00800a670ad91576695e060081

Packages/MIES/MIES_DebugPanel.ipf
@@ -30,6 +30,8 @@ End
 
 Function DP_OpenDebugPanel()
 
+	variable debugMode
+
 	DoWindow/F $PANEL
 	if(V_Flag)
 		return NaN
@@ -42,7 +44,8 @@ Function DP_OpenDebugPanel()
 	WAVE listSelWave = GetDebugPanelListSelWave()
 	ListBox listbox_mies_files win=$PANEL, listWave=listWave, selWave=listSelWave
 
-	SetCheckBoxState(PANEL, "check_debug_mode", QuerySetIgorOption("DEBUGGING_ENABLED") == 1)
+	debugMode = QuerySetIgorOption("DEBUGGING_ENABLED", globalSymbol = 1)
+	SetCheckBoxState(PANEL, "check_debug_mode",  debugMode == 1)
 	// we can't readout the ITC XOP debugging state
 End
 