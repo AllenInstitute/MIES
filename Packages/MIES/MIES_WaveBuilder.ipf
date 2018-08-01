AllenInstitute/MIES

commit c57e4f1686459650af2602873479194a458b0a15

WB_AddDelta: Avoid asserting out on unknown delta operation modes
Since 05b89934 (Enhance the available delta modes in the wavebuilder
panel, 2015-03-26) we have different operation modes for the delta
values.

And in that commit it was decided that we assert out on unknown
operation modes. This is unwise as it completely breaks creating
stimsets with older MIES versions which not yet know about future
operation modes.

Instead we know complain and don't create the stimset.

Original-CommitID: 02ac54bfab52fb9e0fa3638fc33bbcf351b95f90

Packages/MIES/MIES_WaveBuilder.ipf
@@ -341,7 +341,9 @@ Function/Wave WB_GetStimSet([setName])
 	for(i=0; i < numSweeps; i+=1)
 		data[i] = WB_MakeWaveBuilderWave(WPCopy, WPT, SegWvType, i, numEpochs, channelType, updateEpochIDWave, stimset = setName)
 		lengthOf1DWaves = max(DimSize(data[i], ROWS), lengthOf1DWaves)
-		WB_AddDelta(WPCopy, numEpochs)
+		if(WB_AddDelta(setName, WPCopy, numEpochs))
+			return $""
+		endif
 	endfor
 
 	// copy the random seed value to WP in order to preserve it
@@ -389,9 +391,13 @@ End
 ///
 /// Relies on alternating sequence of parameter and delta's in parameter waves as documented in WB_MakeWaveBuilderWave()
 ///
+/// @param setName    name of the stimset
 /// @param WP         wavebuilder parameter wave (temporary copy)
 /// @param numEpochs  number of epochs
-static Function WB_AddDelta(WP, numEpochs)
+///
+/// @return one on error, zero otherwise
+static Function WB_AddDelta(setName, WP, numEpochs)
+	string setName
 	Wave WP
 	variable numEpochs
 
@@ -446,7 +452,9 @@ static Function WB_AddDelta(WP, numEpochs)
 							WP[i + 1][j][k] *= -1
 							break
 						default:
-							ASSERT(0, "Unkonwn operation")
+							// future proof
+							printf "WB_AddDelta: Stimset %s uses an unknown operation %d and can therefore not be recreated.\r", setName, operation
+							return 1
 							break
 					endswitch
 				endif
@@ -462,6 +470,8 @@ static Function WB_AddDelta(WP, numEpochs)
 			endif
 		endfor
 	endfor
+
+	return 0
 End
 
 static Structure SegmentParameters

commit 53aaa8cf5e73c029e9f830ec526f4b442403b472

WB_AddDelta: Use proper %g for floating point delta operation
Bug introduced in c57e4f16 (WB_AddDelta: Avoid asserting out on unknown
delta operation modes, 2018-05-28).

Original-CommitID: 469ba37fee0f19d092609bb36ba2240d41b24455

Packages/MIES/MIES_WaveBuilder.ipf
@@ -625,7 +625,7 @@ static Function WB_CalculateParameterWithDelta(operation, value, delta, dme, lde
 			break
 		default:
 			// future proof
-			printf "WB_AddDelta: Stimset %s uses an unknown operation %d and can therefore not be recreated.\r", setName, operation
+			printf "WB_AddDelta: Stimset %s uses an unknown operation %g and can therefore not be recreated.\r", setName, operation
 			return 1
 			break
 	endswitch