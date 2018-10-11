AllenInstitute/MIES

commit d1e78ad7eb40f459a61c1eea42d5d5cd7eceb66c

The Pressurecontrol device list is filtered now to not contain DAQ devices.
Original-CommitID: eeb78f72600fb78deb692f9536592510b339e4b7

Packages/MIES/MIES_PressureControl.ipf
@@ -2180,8 +2180,20 @@ Function ButtonProc_Hrdwr_P_UpdtDAClist(ba) : ButtonControl
 
 	switch(ba.eventCode)
 		case 2: // mouse up
+			string filteredList = ""
 			string DeviceList = NONE + ";" + HW_ITC_ListDevices() + HW_NI_ListDevices()
-			SetPopupMenuVal(ba.win, "popup_Settings_Pressure_dev", DeviceList)
+			string lockedList = GetListOfLockedDevices()
+			string dev
+			variable nrDevs = ItemsInList(DeviceList)
+			variable i
+			for(i = 0;i < nrDevs; i += 1)
+				dev = StringFromList(i, DeviceList)
+				if(WhichListItem(dev, lockedList) == -1)
+					filteredList = AddListItem(dev, filteredList)
+				endif
+			endfor
+
+			SetPopupMenuVal(ba.win, "popup_Settings_Pressure_dev", filteredList)
 			break
 	endswitch
 

commit 6efebabd51841b66cb82adf2aaad94a20876657e

ButtonProc_Hrdwr_P_UpdtDAClist: Keep list sorting intact
Broken since d1e78ad7 (The Pressurecontrol device list is filtered now
to not contain DAQ devices., 2018-08-24).

Original-CommitID: 07e6aebfb759abf2c931735ce500681438213e40

Packages/MIES/MIES_PressureControl.ipf
@@ -2189,7 +2189,7 @@ Function ButtonProc_Hrdwr_P_UpdtDAClist(ba) : ButtonControl
 			for(i = 0;i < nrDevs; i += 1)
 				dev = StringFromList(i, DeviceList)
 				if(WhichListItem(dev, lockedList) == -1)
-					filteredList = AddListItem(dev, filteredList)
+					filteredList = AddListItem(dev, filteredList, ";", INF)
 				endif
 			endfor
 