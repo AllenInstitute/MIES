AllenInstitute/MIES

commit 19393ff6bc12065d2a66de57753b71edbc4afcd9

build_documentation.sh: Bail out on sphinx warnings and errors
Original-CommitID: 70b472228371c46425100732bb4e53432afac36d

tools/build-documentation.sh
@@ -56,7 +56,17 @@ fi
 if hash sphinx-build 2>/dev/null; then
   echo "Start sphinx-build"
 
-  sphinx-build . html
+  rm -f sphinx-output.log
+
+  sphinx-build -Q -w sphinx-output.log . html
+
+  if [ -e "sphinx-output.log" ]
+  then
+    echo "Errors building the documentation" 1>&2
+    echo "sphinx-build says: "               1>&2
+    cat sphinx-output.log                    1>&2
+    exit 1
+  fi
 
 else
   echo "Errors building the documentation" 1>&2

commit 2d9bb76ed1d6da74611307e3c990636b28ebad27

Documentation: Really bailout on sphinx warnings and errors
CLI flag documentation is a bit difficult to grasp as it seems.
Broken since 19393ff6 (build_documentation.sh: Bail out on sphinx
warnings and errors, 2017-10-19).

Original-CommitID: 9a175b9b3e9cdfe5deb918c2120f6ba51bb12cb4

tools/build-documentation.sh
@@ -59,7 +59,7 @@ if hash sphinx-build 2>/dev/null; then
 
   rm -f sphinx-output.log
 
-  sphinx-build -Q -w sphinx-output.log . html
+  sphinx-build -q -w sphinx-output.log . html
 
   if [ -e "sphinx-output.log" ]
   then