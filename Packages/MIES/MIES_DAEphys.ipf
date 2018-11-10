AllenInstitute/MIES

commit ff7dd72af5fa92b1d98b740b542ea65210f62ddb

Analysis parameters: Add method to request the types as well
Real world experience showed that being able to request a specific
type for the analysis parameters helps a lot.

A breaking change is that the names now must be separated with commas
(,) as that is more in line who we store the entries in the stimset.

Original-CommitID: df4494c0a742d25c30b24d8761079ef93808b9f2

Packages/MIES/MIES_DAEphys.ipf
@@ -1806,7 +1806,7 @@ Function DAP_CheckSettings(panelTitle, mode)
 
 	variable numDACs, numADCs, numHS, numEntries, i, indexingEnabled, clampMode
 	variable ampSerial, ampChannelID, minValue, maxValue
-	string ctrl, endWave, ttlWave, dacWave, refDacWave
+	string ctrl, endWave, ttlWave, dacWave, refDacWave, reqParams
 	string list
 
 	ASSERT(mode == DATA_ACQUISITION_MODE || mode == TEST_PULSE_MODE, "Invalid mode")
@@ -2130,9 +2130,10 @@ static Function DAP_CheckHeadStage(panelTitle, headStage, mode)
 
 	string dacWave, endWave, unit, func, info, str, ADUnit, DAUnit, listOfAnalysisFunctions
 	variable DACchannel, ADCchannel, DAheadstage, ADheadstage, DAGain, ADGain, realMode
-	variable gain, scale, clampMode, i, ampConnState, needResetting
-	variable DAGainMCC, ADGainMCC
-	string DAUnitMCC, ADUnitMCC, params, names, requestedNames, diff
+	variable gain, scale, clampMode, i, j, ampConnState, needResetting
+	variable DAGainMCC, ADGainMCC, numEntries
+	string DAUnitMCC, ADUnitMCC, suppParams, suppName, suppType, reqParams, reqNames, reqName
+	string diff, name, type, suppNames, reqType
 
 	if(DAP_DeviceIsUnlocked(panelTitle))
 		printf "(%s) Device is unlocked. Please lock the device.\r", panelTitle
@@ -2369,16 +2370,42 @@ static Function DAP_CheckHeadStage(panelTitle, headStage, mode)
 					return 1
 				elseif(i == GENERIC_EVENT)
 					// check that all required user parameters are supplied
-					requestedNames = AFH_GetListOfReqAnalysisParams(func)
-					if(!IsEmpty(requestedNames))
-						params = ExtractAnalysisFunctionParams(stimSet)
-						names = AFH_GetListOfAnalysisParamNames(params)
-						diff = GetListDifference(requestedNames, names)
+					reqParams = AFH_GetListOfReqAnalysisParams(func)
+					if(!IsEmpty(reqParams))
+						reqNames   = AFH_GetListOfAnalysisParamNames(reqParams)
+						suppParams = ExtractAnalysisFunctionParams(stimSet)
+						suppNames  = AFH_GetListOfAnalysisParamNames(suppParams)
+						diff = GetListDifference(reqNames, suppNames)
 						if(!IsEmpty(diff))
 							printf "(%s) The required analysis parameters requested by %s for stim set %s were not all supplied (missing are: %s)\r", panelTitle, func, dacWave, diff
 							ControlWindowToFront()
 							return 1
 						endif
+
+						numEntries = ItemsInList(reqNames, ",")
+						for(j = 0; i < numEntries; j += 1)
+							reqName = StringFromList(i, reqNames, ",")
+							reqType = AFH_GetAnalysisParamType(reqName, reqParams, typeCheck = 0)
+							// no type specification is allowed
+							if(IsEmpty(reqType))
+								continue
+							endif
+
+							// invalid types are not allowed
+							if(WhichListItem(reqType, ANALYSIS_FUNCTION_PARAMS_TYPES) == -1)
+								printf "(%s) The analysis parameter %s for %s in stim set %s has type %s which is unknown.\r", panelTitle, name, func, dacWave, type
+								ControlWindowToFront()
+								return 1
+							endif
+
+							// non matching type
+							suppType = AFH_GetAnalysisParamType(reqName, suppParams, typeCheck = 0)
+							if(cmpstr(reqType, suppType))
+								printf "(%s) The analysis parameter %s for %s in stim set %s has type %s but the required type is %s which is unknown.\r", panelTitle, name, func, dacWave, suppType, reqType
+								ControlWindowToFront()
+								return 1
+							endif
+						endfor
 					endif
 				endif
 			endfor

Packages/MIES/MIES_AnalysisFunctionHelpers.ipf
@@ -378,11 +378,13 @@ Function/S AFH_GetAnalysisFunctions(versionBitMask)
 End
 
 /// @brief Return the list of required analysis function
-/// parameters as specified by the function `$func_GetParams`
+/// parameters, possibly including the type, as specified by the function `$func_GetParams`
 ///
 /// @param func Analysis function `V3` which must be valid and existing
 Function/S AFH_GetListOfReqAnalysisParams(func)
 	string func
+
+	string params
 
 	FUNCREF AF_PROTO_PARAM_GETTER_V3 f = $(func + "_GetParams")
 
@@ -390,7 +392,11 @@ Function/S AFH_GetListOfReqAnalysisParams(func)
 		return ""
 	endif
 
-	return f()
+	params = f()
+
+	ASSERT(strsearch(params, ";", 0) == -1, "Entries must be separated with ,")
+
+	return params
 End
 
 /// @defgroup AnalysisFunctionParameterHelpers Analysis Helper functions for dealing with user parameters

Packages/MIES/MIES_AnalysisFunctions.ipf
@@ -60,9 +60,9 @@
 ///
 /// If you want to enforce a list of parameters which must be present, define
 /// an additional function named like your analysis function but suffixed with
-/// `_GetParams` and return a semicolon separated list of required names. This
-/// is then checked before DAQ, and if not all required parameters are supplied
-/// DAQ is not started.
+/// `_GetParams` and return a comma separated list of required names. Adding the
+/// type is also possible via `$name:$type` syntax. The list of parameter names and types
+/// is then checked before DAQ.
 
 /// @name Initial parameters for stimulation
 ///@{

Packages/MIES/MIES_AnalysisFunctions_PatchSeq.ipf
@@ -936,7 +936,7 @@ End
 /// - OperationMode (String):  Operation mode of the analayis function. Can be
 ///                            either #PSQ_DS_SUB or #PSQ_DS_SUPRA.
 Function/S PSQ_DAScale_GetParams()
-	return "DAScales;OperationMode"
+	return "DAScales,OperationMode"
 End
 
 /// @brief Patch Seq Analysis function to find a suitable DAScale
@@ -1716,7 +1716,7 @@ End
 ///                              the pulse onset in order to label the cell
 ///                              as having "spiked".
 Function/S PSQ_Ramp_GetParams()
-	return "NumberOfSpikes;"
+	return "NumberOfSpikes,"
 End
 
 /// @brief Analysis function for applying a ramp stim set and finding the position were it spikes.

Packages/MIES/MIES_WaveBuilderPanel.ipf
@@ -2586,7 +2586,7 @@ End
 static Function WBP_UpdateParameterWave()
 
 	string params, names, name, type, genericFunc, reqParams
-	string missingParams
+	string missingParams, reqNames
 	variable i, numEntries, offset
 
 	Wave/T listWave = WBP_GetAnalysisParamGUIListWave()
@@ -2596,6 +2596,7 @@ static Function WBP_UpdateParameterWave()
 
 	genericFunc = WPT[%$("Analysis function (generic)")][%Set]
 	reqParams = AFH_GetListOfReqAnalysisParams(genericFunc)
+	reqNames  = AFH_GetListOfAnalysisParamNames(reqParams)
 
 	params = WBP_GetAnalysisParameters()
 	names  = AFH_GetListOfAnalysisParamNames(params)
@@ -2612,7 +2613,7 @@ static Function WBP_UpdateParameterWave()
 
 	offset = DimSize(listWave, ROWS)
 
-	missingParams = GetListDifference(reqParams, names)
+	missingParams = GetListDifference(reqNames, names)
 	numEntries = ItemsInList(missingParams)
 	Redimension/N=(offset + numEntries, -1) listWave, selWave
 

commit 7e51055c04ee2e2c15eb9256c8dd8c55352a3906

DAP_CheckHeadStage: Fix error message for analysis parameters
Broken since ff7dd72a (Analysis parameters: Add method to request the
types as well, 2018-05-13).

Original-CommitID: d55569c0d76be981e23ef2c2d5b0297a6cd99df1

Packages/MIES/MIES_DAEphys.ipf
@@ -2485,7 +2485,7 @@ static Function DAP_CheckHeadStage(panelTitle, headStage, mode)
 
 							// invalid types are not allowed
 							if(WhichListItem(reqType, ANALYSIS_FUNCTION_PARAMS_TYPES) == -1)
-								printf "(%s) The required analysis parameter %s for %s in stim set %s has type %s which is unknown.\r", panelTitle, name, func, dacWave, type
+								printf "(%s) The required analysis parameter %s for %s in stim set %s has type %s which is unknown.\r", panelTitle, reqName, func, dacWave, type
 								ControlWindowToFront()
 								return 1
 							endif
@@ -2493,7 +2493,7 @@ static Function DAP_CheckHeadStage(panelTitle, headStage, mode)
 							// non matching type
 							suppType = AFH_GetAnalysisParamType(reqName, suppParams, typeCheck = 0)
 							if(cmpstr(reqType, suppType))
-								printf "(%s) The analysis parameter %s for %s in stim set %s has type %s but the required type is %s which is unknown.\r", panelTitle, name, func, dacWave, suppType, reqType
+								printf "(%s) The analysis parameter %s for %s in stim set %s has type %s but the required type is %s which is unknown.\r", panelTitle, reqName, func, dacWave, suppType, reqType
 								ControlWindowToFront()
 								return 1
 							endif