1292769f744be3e0b4a5c1258f7306dfe96f949e
Thomas Braun <thomas.braun@byte-physics.de>
2018-11-11 16:34:30 +0100

    Compilation Testing: Reinstate timeout
    Since 4eaa63bf (Compilation Testing: Rework using /CompErrNoDialog found
    in Igor Pro 7, build 30097 or later, 2017-09-07) we don't execute Igor
    with a timeout anymore as we can catch most errors without requiring it.
    
    But some Igor errors like "Name too long" still result in a stuck Igor
    Pro.
    
    We now use the timeout command as before, but with increased duration,
    and also create failed XML test case for proper reporting.
    
    The JUNIT XML file was created via
    
    Function TestRunTimedOut()
    	FAIL()
    End
    
    and the properties entries removed.
    
    Original-CommitID: bbea1855516082884e33a29a7ce94ba890f48155

--- tools/compilation-testing/autorun-test.sh
+++ tools/compilation-testing/autorun-test.sh
@@ -7,7 +7,17 @@ touch $StateFile
 for i in $(ls *.pxp)
 do
 	echo "Running experiment $i"
-	WINEPREFIX=/home/thomasb/.wine-igor wine64 C:/Program\ Files/WaveMetrics/Igor\ Pro\ 7\ Folder/IgorBinaries_x64/Igor64.exe /CompErrNoDialog /N /I "$(pwd)/$i" 2> errorOutput.log
+	timeout --kill-after=10s 5m env WINEPREFIX=/home/thomasb/.wine-igor \
+    wine64 C:/Program\ Files/WaveMetrics/Igor\ Pro\ 7\ Folder/IgorBinaries_x64/Igor64.exe \
+    /CompErrNoDialog /N /I "$(pwd)/$i" 2> errorOutput.log
+
+	ret=$?
+	echo "timeout returned: $ret"
+
+	if [ $ret -eq 124 -o $ret -eq 136 ]
+	then
+		cp ../JU_timeout.template JU_timeout-reached.xml
+	fi
 done
 
 echo "Igor Pro compilation error output:"
