AllenInstitute/MIES

commit 3fa0b189423b13e4a6e7effee5db46eb9cfa52d4

Add analysis function for setting controls in the pre set event
Original-CommitID: ac13debd683e43d967ef81b9dcb432026375afe9

Packages/MIES/MIES_AnalysisFunctions.ipf
@@ -983,3 +983,86 @@ Function ReachTargetVoltage(panelTitle, eventType, ITCDataWave, headStage, realD
 		SetDAScale(panelTitle, i, amps)
 	endfor
 End
+
+/// @brief Analysis function to set GUI controls in the PRE_SET_EVENT
+///
+/// Usage:
+/// - Add analysis parameters named like the control
+/// - Their value, either a variable or a string, determine the target value
+Function SetControlInPreSetEvent(panelTitle, s)
+	string panelTitle
+	STRUCT AnalysisFunction_V3 &s
+
+	string ctrls, ctrl, type, str
+	variable numEntries, i, var, controlType
+
+	switch(s.eventType)
+		case PRE_SET_EVENT:
+
+			if(s.headstage != DAP_GetHighestActiveHeadstage(panelTitle))
+				break
+			endif
+
+			ctrls = AFH_GetListOfAnalysisParamNames(s.params)
+			numEntries = ItemsInList(ctrls)
+
+			// check names and types
+			for(i = 0; i < numEntries; i += 1)
+				ctrl = StringFromList(i, ctrls)
+
+				if(!ControlExists(paneltitle, ctrl))
+					printf "(%s): The analysis parameter %s is not a valid control.\r", panelTitle, ctrl
+					ControlWindowToFront()
+					return 1
+				endif
+
+				type = AFH_GetAnalysisParamType(ctrl, s.params)
+
+				strswitch(type)
+					case "variable":
+						// all controls accept variables
+						break
+					case "string":
+						controlType = GetControlType(panelTitle, ctrl)
+						if(controlType != CONTROL_TYPE_SETVARIABLE && controlType != CONTROL_TYPE_POPUPMENU)
+							printf "(%s): The analysis parameter's %s value is a string but refers to a control which does not accept a string.\r", panelTitle, ctrl
+							ControlWindowToFront()
+							return 1
+						endif
+						break
+					default:
+						printf "(%s): The analysis parameter %s must have the type \"variable\" or \"string\".\r", panelTitle, ctrl
+						ControlWindowToFront()
+						return 1
+						break
+				endswitch
+
+				if(WhichListItem(ctrl, CONTROLS_DISABLE_DURING_DAQ) != -1)
+					printf "(%s): The analysis parameter %s is a control which can not be changed during DAQ.\r", panelTitle, ctrl
+					ControlWindowToFront()
+					return 1
+				elseif(IsControlDisabled(panelTitle, ctrl))
+					printf "(%s): The analysis parameter %s is a control which is hidden. Therefore it can not be set.\r", panelTitle, ctrl
+					ControlWindowToFront()
+					return 1
+				endif
+			endfor
+
+			for(i = 0; i < numEntries; i += 1)
+				ctrl = StringFromList(i, ctrls)
+				type = AFH_GetAnalysisParamType(ctrl, s.params)
+
+				strswitch(type)
+					case "variable":
+						var = AFH_GetAnalysisParamNumerical(ctrl, s.params)
+						PGC_SetAndActivateControl(panelTitle, ctrl, val = var)
+						break
+					case "string":
+						str = AFH_GetAnalysisParamTextual(ctrl, s.params)
+						PGC_SetAndActivateControl(panelTitle, ctrl, str = str)
+						break
+				endswitch
+			endfor
+			break
+	endswitch
+End

Packages/Testing-MIES/UTF_AnalysisFunctionManagement.ipf
@@ -1732,3 +1732,175 @@ static Function AFT_Test20()
 
 	CHECK_EQUAL_WAVES(anaFuncOrderIndex, anaFuncOrderSorted)
 End
+
+static Function AFT_SetControls1_Setter()
+
+	WBP_AddAnalysisParameter("AnaFuncSetCtrl_DA_0", "unknown_ctrl", var=1)
+End
+
+// complains on invalid control
+static Function AFT_SetControls1()
+
+	STRUCT DAQSettings s
+	InitDAQSettingsFromString(s, "DAQ_MD1_RA0_IDX0_LIDX0_BKG_1")
+
+	AcquireData(s, "AnaFuncSetCtrl_DA_0", postInitializeFunc = AFT_SetControls1_Setter)
+End
+
+static Function AFT_SetControlsTest1()
+
+	variable sweepNo
+
+	CHECK_EQUAL_VAR(GetSetVariable(DEVICE, "SetVar_Sweep"), 0)
+
+	sweepNo = AFH_GetLastSweepAcquired(DEVICE)
+	CHECK_EQUAL_VAR(sweepNo, NaN)
+
+	EnsureNoAnaFuncErrors()
+End
+
+static Function AFT_SetControls2_Setter()
+
+	WBP_AddAnalysisParameter("AnaFuncSetCtrl_DA_0", "Check_DataAcq_Indexing", str="my string")
+End
+
+// complains about string param with variable-only control
+static Function AFT_SetControls2()
+
+	STRUCT DAQSettings s
+	InitDAQSettingsFromString(s, "DAQ_MD1_RA0_IDX0_LIDX0_BKG_1")
+
+	AcquireData(s, "AnaFuncSetCtrl_DA_0", postInitializeFunc = AFT_SetControls2_Setter)
+End
+
+static Function AFT_SetControlsTest2()
+
+	variable sweepNo
+
+	CHECK_EQUAL_VAR(GetSetVariable(DEVICE, "SetVar_Sweep"), 0)
+
+	sweepNo = AFH_GetLastSweepAcquired(DEVICE)
+	CHECK_EQUAL_VAR(sweepNo, NaN)
+
+	EnsureNoAnaFuncErrors()
+End
+
+static Function AFT_SetControls3_Setter()
+
+	Make/FREE wv
+	WBP_AddAnalysisParameter("AnaFuncSetCtrl_DA_0", "Check_DataAcq_Indexing", wv=wv)
+End
+
+// invalid parameter type
+static Function AFT_SetControls3()
+
+	STRUCT DAQSettings s
+	InitDAQSettingsFromString(s, "DAQ_MD1_RA0_IDX0_LIDX0_BKG_1")
+
+	AcquireData(s, "AnaFuncSetCtrl_DA_0", postInitializeFunc = AFT_SetControls3_Setter)
+End
+
+static Function AFT_SetControlsTest3()
+
+	variable sweepNo
+
+	CHECK_EQUAL_VAR(GetSetVariable(DEVICE, "SetVar_Sweep"), 0)
+
+	sweepNo = AFH_GetLastSweepAcquired(DEVICE)
+	CHECK_EQUAL_VAR(sweepNo, NaN)
+
+	EnsureNoAnaFuncErrors()
+End
+
+static Function AFT_SetControls4_Setter()
+
+	Make/FREE wv
+	WBP_AddAnalysisParameter("AnaFuncSetCtrl_DA_0", "Check_DataAcq_Indexing", var=1)
+End
+
+// unchangeable control
+static Function AFT_SetControls4()
+
+	STRUCT DAQSettings s
+	InitDAQSettingsFromString(s, "DAQ_MD1_RA0_IDX0_LIDX0_BKG_1")
+
+	AcquireData(s, "AnaFuncSetCtrl_DA_0", postInitializeFunc = AFT_SetControls4_Setter)
+End
+
+static Function AFT_SetControlsTest4()
+
+	variable sweepNo
+
+	CHECK_EQUAL_VAR(GetSetVariable(DEVICE, "SetVar_Sweep"), 0)
+
+	sweepNo = AFH_GetLastSweepAcquired(DEVICE)
+	CHECK_EQUAL_VAR(sweepNo, NaN)
+
+	EnsureNoAnaFuncErrors()
+End
+
+static Function AFT_SetControls5_Setter()
+
+	Make/FREE wv
+	WBP_AddAnalysisParameter("AnaFuncSetCtrl_DA_0", "popup_MoreSettings_DeviceType", var=1)
+End
+
+// hidden control
+static Function AFT_SetControls5()
+
+	STRUCT DAQSettings s
+	InitDAQSettingsFromString(s, "DAQ_MD1_RA0_IDX0_LIDX0_BKG_1")
+
+	AcquireData(s, "AnaFuncSetCtrl_DA_0", postInitializeFunc = AFT_SetControls5_Setter)
+End
+
+static Function AFT_SetControlsTest5()
+
+	variable sweepNo
+
+	CHECK_EQUAL_VAR(GetSetVariable(DEVICE, "SetVar_Sweep"), 0)
+
+	sweepNo = AFH_GetLastSweepAcquired(DEVICE)
+	CHECK_EQUAL_VAR(sweepNo, NaN)
+
+	EnsureNoAnaFuncErrors()
+End
+
+static Function AFT_SetControls6_Setter()
+
+	WBP_AddAnalysisParameter("AnaFuncSetCtrl_DA_0", "Check_Settings_UseDoublePrec", var=1)
+	WBP_AddAnalysisParameter("AnaFuncSetCtrl_DA_0", "check_Settings_ShowScopeWindow", var=0)
+	WBP_AddAnalysisParameter("AnaFuncSetCtrl_DA_0", "SetVar_DataAcq_Comment", str="abcd efgh")
+	WBP_AddAnalysisParameter("AnaFuncSetCtrl_DA_0", "SetVar_DataAcq_TPBaselinePerc", var=47)
+End
+
+// works with different controls
+static Function AFT_SetControls6()
+
+	STRUCT DAQSettings s
+	InitDAQSettingsFromString(s, "DAQ_MD1_RA0_IDX0_LIDX0_BKG_1")
+
+	AcquireData(s, "AnaFuncSetCtrl_DA_0", postInitializeFunc = AFT_SetControls6_Setter)
+End
+
+static Function AFT_SetControlsTest6()
+
+	variable sweepNo
+	string ref, actual
+
+	CHECK_EQUAL_VAR(GetSetVariable(DEVICE, "SetVar_Sweep"), 1)
+
+	sweepNo = AFH_GetLastSweepAcquired(DEVICE)
+	CHECK_EQUAL_VAR(sweepNo, 0)
+
+	CHECK_EQUAL_VAR(GetCheckBoxState(DEVICE, "Check_Settings_UseDoublePrec"), 1)
+	CHECK_EQUAL_VAR(GetCheckBoxState(DEVICE, "check_Settings_ShowScopeWindow"), 0)
+
+	ref = "abcd efgh"
+	actual = GetSetVariableString(DEVICE, "SetVar_DataAcq_Comment")
+	CHECK_EQUAL_STR(ref, actual)
+
+	CHECK_EQUAL_VAR(GetSetVariable(DEVICE, "SetVar_DataAcq_TPBaselinePerc"), 47)
+
+	EnsureNoAnaFuncErrors()
+End

Packages/Testing-MIES/UTF_HardwareMain.ipf
@@ -254,6 +254,19 @@ Function run()
 	list = AddListItem("AFT_DAQ20", list, ";", INF)
 	list = AddListItem("AFT_Test20", list, ";", INF)
 
+	list = AddListItem("AFT_SetControls1", list, ";", INF)
+	list = AddListItem("AFT_SetControlsTest1", list, ";", INF)
+	list = AddListItem("AFT_SetControls2", list, ";", INF)
+	list = AddListItem("AFT_SetControlsTest2", list, ";", INF)
+	list = AddListItem("AFT_SetControls3", list, ";", INF)
+	list = AddListItem("AFT_SetControlsTest3", list, ";", INF)
+	list = AddListItem("AFT_SetControls4", list, ";", INF)
+	list = AddListItem("AFT_SetControlsTest4", list, ";", INF)
+	list = AddListItem("AFT_SetControls5", list, ";", INF)
+	list = AddListItem("AFT_SetControlsTest5", list, ";", INF)
+	list = AddListItem("AFT_SetControls6", list, ";", INF)
+	list = AddListItem("AFT_SetControlsTest6", list, ";", INF)
+
 	list = AddListItem("MSQ_FRE_Run1", list, ";", INF)
 	list = AddListItem("MSQ_FRE_Test1", list, ";", INF)
 	list = AddListItem("MSQ_FRE_Run2", list, ";", INF)

commit 1af7d6c8652b5ecfe47344af489f1f94f656762d

SetControlInPreSetEvent: Fix typo
Introduced in 3fa0b189 (Add analysis function for setting controls in
the pre set event, 2018-11-11).

Original-CommitID: 32533ec34763cef3868143ce26cd01ac317afb84

Packages/MIES/MIES_AnalysisFunctions.ipf
@@ -1042,7 +1042,7 @@ Function SetControlInPreSetEvent(panelTitle, s)
 					ControlWindowToFront()
 					return 1
 				elseif(IsControlDisabled(panelTitle, ctrl))
-					printf "(%s): The analysis parameter %s is a control which is hidden. Therefore it can not be set.\r", panelTitle, ctrl
+					printf "(%s): The analysis parameter %s is a control which is disabled. Therefore it can not be set.\r", panelTitle, ctrl
 					ControlWindowToFront()
 					return 1
 				endif