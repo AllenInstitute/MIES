AllenInstitute/MIES

commit 70cc9b9904b3d47e9161fd269e9afcc38647679a

EnsureLargeEnoughWave: Add support for checking free memory before increasing the wave's size
For some cases we want to avoid the out-of-memory error and handle it
ourselves.

We don't do the free memory check by default as calculating that is
potentially expensive.

Original-CommitID: 9921100831324a43dc3275e789bd3e53ef880c81

Packages/MIES/MIES_Utilities.ipf
@@ -300,12 +300,21 @@ End
 ///                        The actual size of the wave after the function returns might be larger.
 /// @param dimension       [optional, defaults to ROWS] dimension to resize, all other dimensions are left untouched.
 /// @param initialValue    [optional, defaults to zero] initialValue of the new wave points
-Function EnsureLargeEnoughWave(wv, [minimumSize, dimension, initialValue])
+/// @param checkFreeMemory [optional, defaults to false] check if the free memory is enough for increasing the size
+///
+/// @return 0 on success, (only for checkFreeMemory = True) 1 if increasing the wave's size would fail due to little memory
+Function EnsureLargeEnoughWave(wv, [minimumSize, dimension, initialValue, checkFreeMemory])
 	Wave wv
-	variable minimumSize, dimension, initialValue
+	variable minimumSize, dimension, initialValue, checkFreeMemory
 
 	if(ParamIsDefault(dimension))
 		dimension = ROWS
+	endif
+
+	if(ParamIsDefault(checkFreeMemory))
+		checkFreeMemory = 0
+	else
+		checkFreeMemory = !!checkFreeMemory
 	endif
 
 	ASSERT(dimension == ROWS || dimension == COLS || dimension == LAYERS || dimension == CHUNKS, "Invalid dimension")
@@ -319,10 +328,16 @@ Function EnsureLargeEnoughWave(wv, [minimumSize, dimension, initialValue])
 	endif
 
 	if(minimumSize < DimSize(wv, dimension))
-		return NaN
+		return 0
 	endif
 
 	minimumSize *= 2
+
+	if(checkFreeMemory)
+		if(GetWaveSize(wv) * (minimumSize - DimSize(wv, dimension)) / 1024 / 1024 / 1024 >= GetFreeMemory())
+			return 1
+		endif
+	endif
 
 	Make/FREE/L/N=(MAX_DIMENSION_COUNT) targetSizes = -1
 	targetSizes[dimension] = minimumSize
@@ -348,6 +363,8 @@ Function EnsureLargeEnoughWave(wv, [minimumSize, dimension, initialValue])
 			break
 		endswitch
 	endif
+
+	return 0
 End
 
 /// @brief Check that the given value can be stored in the wave

Packages/Testing-MIES/UTF_Utils.ipf
@@ -410,6 +410,26 @@ Function ELE_KeepsMinimumWaveSize3()
 	CHECK(DimSize(wv, ROWS) > MINIMUM_WAVE_SIZE)
 End
 
+Function ELE_Returns1WithCheckMem()
+	Make/FREE/N=(MINIMUM_WAVE_SIZE) wv
+	CHECK_EQUAL_VAR(EnsureLargeEnoughWave(wv, minimumSize = 2^50, checkFreeMemory = 1), 1)
+	CHECK_EQUAL_VAR(DimSize(wv, ROWS), MINIMUM_WAVE_SIZE)
+End
+
+Function ELE_AbortsWithTooLargeValue()
+	Make/FREE/N=(MINIMUM_WAVE_SIZE) wv
+
+	variable err
+
+	try
+		EnsureLargeEnoughWave(wv, minimumSize = 2^50); AbortOnRTE
+		FAIL()
+	catch
+		err = GetRTError(1)
+		PASS()
+	endtry
+End
+
 /// @}
 
 /// DoAbortNow

commit 0fb42e5caacb0a6eed9abaf7f252eb02f308e22a

EnsureLargeEnoughWave: Fix free memory check
Broken since 70cc9b99 (EnsureLargeEnoughWave: Add support for checking
free memory before increasing the wave's size, 2018-11-09).

Original-CommitID: ce9f9ad8c5ce3dd8e7df312bd41a663ea3571ffb

Packages/MIES/MIES_Utilities.ipf
@@ -334,7 +334,7 @@ Function EnsureLargeEnoughWave(wv, [minimumSize, dimension, initialValue, checkF
 	minimumSize *= 2
 
 	if(checkFreeMemory)
-		if(GetWaveSize(wv) * (minimumSize - DimSize(wv, dimension)) / 1024 / 1024 / 1024 >= GetFreeMemory())
+		if(GetWaveSize(wv) * (minimumSize / DimSize(wv, dimension)) / 1024 / 1024 / 1024 >= GetFreeMemory())
 			return 1
 		endif
 	endif