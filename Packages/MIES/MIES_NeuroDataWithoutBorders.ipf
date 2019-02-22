AllenInstitute/MIES

commit 906f8b79c7d2f91f78d4b8c2b6a090c61c398782

NWB_AppendSweepLowLevel: Clarify code and use the ttlBit
The suffix of the splitted TTL names *is* the ttlBit.

Original-CommitID: 75e0059a8ccb443d74579f81a7b630bba5bee899

Packages/MIES/MIES_NeuroDataWithoutBorders.ipf
@@ -651,6 +651,7 @@ static Function NWB_AppendSweepLowLevel(locationID, panelTitle, ITCDataWave, ITC
 	variable sweep, chunkedLayout
 
 	variable groupID, numEntries, i, j, ttlBits, dac, adc, col, refTime
+	variable ttlBit
 	string group, path, list, name
 	string channelSuffix, listOfStimsets, contents
 
@@ -800,11 +801,13 @@ static Function NWB_AppendSweepLowLevel(locationID, panelTitle, ITCDataWave, ITC
 		numEntries = ItemsInList(list)
 		for(j = 0; j < numEntries; j += 1)
 			name = StringFromList(j, list)
+			ttlBit = str2num(name[1,inf])
+			ASSERT((ttlBit & ttlBits) == 1, "Invalid ttlBit")
 			WAVE/SDFR=dfr params.data = $name
 			path                 = "/stimulus/presentation"
-			params.channelSuffix = name[1, inf]
+			params.channelSuffix = num2str(ttlBit)
 			params.channelSuffixDesc = NWB_SOURCE_TTL_BIT
-			params.stimset       = StringFromList(str2num(name[1,inf]), listOfStimsets)
+			params.stimset       = StringFromList(ttlBit, listOfStimsets)
 			NWB_GetTimeSeriesProperties(params, tsp)
 			params.groupIndex    = IsFinite(params.groupIndex) ? params.groupIndex : IPNWB#GetNextFreeGroupIndex(locationID, path)
 			IPNWB#WriteSingleChannel(locationID, path, params, tsp, chunkedLayout=chunkedLayout)

commit a2b9c6c641968770513f0102f637cc72e6570bf0

NWB_AppendSweepLowLevel: Check for correct ttlBit
We need to check that the ttlBits bitmask has an entry of ttlBit, so the
usual pattern is to compare to ttlBit and not 1.

Broken since 906f8b79 (NWB_AppendSweepLowLevel: Clarify code and use the
ttlBit, 2018-09-07).

Original-CommitID: 90405b145078b333174013d4e40659a9d8be8dde

Packages/MIES/MIES_NeuroDataWithoutBorders.ipf
@@ -824,7 +824,7 @@ static Function NWB_AppendSweepLowLevel(locationID, panelTitle, ITCDataWave, ITC
 			for(j = 0; j < numEntries; j += 1)
 				name = StringFromList(j, list)
 				ttlBit = 2^str2num(name[1,inf])
-				ASSERT((ttlBit & ttlBits) == 1, "Invalid ttlBit")
+				ASSERT((ttlBit & ttlBits) == ttlBit, "Invalid ttlBit")
 				WAVE/SDFR=dfr params.data = $name
 				path                 = "/stimulus/presentation"
 				params.channelSuffix = num2str(ttlBit)