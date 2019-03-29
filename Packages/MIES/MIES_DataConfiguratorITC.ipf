AllenInstitute/MIES

commit cb206d3c917a78af4e1d67bf8d8306eecb2cdef4

GetChannelClampMode: Extend to also hold headstage information
It is currently suprisingly difficult to get the headstage from a DA/AD
channel number.

The old approach involved ChanAmpAssign and the ChannelClampMode waves
and was slow. This is ugly especially when called during the mid sweep
event.

As ChannelClampMode is one of the few waves which is indexed by
channel number, the obvious solution is to extend it to hold the
headstage as well.

For wave upgrades we prefill the headstage numbers using the old
algorithm.

Original-CommitID: 134392ccd47ccee801593250561a60e74a842e0c

Packages/MIES/MIES_DataConfiguratorITC.ipf
@@ -757,7 +757,7 @@ static Function DC_PlaceDataInHardwareDataWave(panelTitle, numActiveChannels, da
 
 		maxITI = max(maxITI, WB_GetITI(stimSet[activeColumn], setColumn[activeColumn]))
 
-		channelMode = ChannelClampMode[i][%DAC]
+		channelMode = ChannelClampMode[i][%DAC][%Headstage]
 		if(channelMode == V_CLAMP_MODE)
 			testPulseAmplitude[activeColumn] = TPAmpVClamp
 		elseif(channelMode == I_CLAMP_MODE || channelMode == I_EQUAL_ZERO_MODE)

Packages/MIES/MIES_AnalysisFunctionHelpers.ipf
@@ -33,20 +33,9 @@ Function AFH_GetHeadstageFromADC(panelTitle, AD)
 	string panelTitle
 	variable AD
 
-	variable i, row, entries
+	WAVE channelClampMode = GetChannelClampMode(panelTitle)
 
-	WAVE ChanAmpAssign = GetChanAmpAssign(panelTitle)
-	WAVE ChannelClampMode = GetChannelClampMode(panelTitle)
-
-	entries = DimSize(ChanAmpAssign, COLS)
-	row = ChannelClampMode[AD][%ADC] == V_CLAMP_MODE ? 2 : 2 + 4
-	for(i = 0; i < entries; i += 1)
-		if(chanAmpAssign[row][i] == AD)
-			return i
-		endif
-	endfor
-
-	return NaN
+	return channelClampMode[AD][%ADC][%Headstage]
 End
 
 /// @brief Return the headstage the DA channel is assigned to
@@ -60,21 +49,9 @@ Function AFH_GetHeadstageFromDAC(panelTitle, DA)
 	string 	panelTitle
 	variable DA
 
-	variable i, row, entries
-
-	WAVE ChanAmpAssign = GetChanAmpAssign(panelTitle)
 	WAVE channelClampMode = GetChannelClampMode(panelTitle)
 
-	entries = DimSize(chanAmpAssign, COLS)
-	row = channelClampMode[DA][%DAC] == V_CLAMP_MODE ? 0 : 0 + 4
-
-	for(i = 0; i < entries; i += 1)
-		if(chanAmpAssign[row][i] == DA)
-			return i
-		endif
-	endfor
-
-	return NaN
+	return channelClampMode[DA][%DAC][%Headstage]
 End
 
 /// @brief Return the AD channel assigned to the headstage

Packages/MIES/MIES_DAEphys.ipf
@@ -2341,14 +2341,14 @@ static Function DAP_CheckHeadStage(panelTitle, headStage, mode)
 		return 1
 	endif
 
-	realMode = channelClampMode[DACchannel][%DAC]
+	realMode = channelClampMode[DACchannel][%DAC][%ClampMode]
 	if(realMode != clampMode)
 		printf "(%s) The clamp mode of DA %d is %s and differs from the requested mode %s.\r", panelTitle, DACchannel, ConvertAmplifierModeToString(realMode), ConvertAmplifierModeToString(clampMode)
 		ControlWindowToFront()
 		return 1
 	endif
 
-	realMode = channelClampMode[ADCchannel][%ADC]
+	realMode = channelClampMode[ADCchannel][%ADC][%ClampMode]
 	if(realMode != clampMode)
 		printf "(%s) The clamp mode of AD %d is %s and differs from the requested mode %s.\r", panelTitle, ADCchannel, ConvertAmplifierModeToString(realMode), ConvertAmplifierModeToString(clampMode)
 		ControlWindowToFront()
@@ -2681,8 +2681,9 @@ static Function DAP_ApplyClmpModeSavdSettngs(panelTitle, headStage, clampMode)
 	GuiState[DACchannel][%$GetSpecialControlLabel(CHANNEL_TYPE_DAC, CHANNEL_CONTROL_GAIN)] = DAGain
 	ctrl = GetPanelControl(DACchannel, CHANNEL_TYPE_DAC, CHANNEL_CONTROL_UNIT)
 	SetSetVariableString(panelTitle, ctrl, DaUnit)
-	ChannelClampMode[DACchannel][%DAC] = clampMode
 	GuiStateTxT[DACchannel][%$GetSpecialControlLabel(CHANNEL_TYPE_DAC, CHANNEL_CONTROL_UNIT)] = DAUnit
+	ChannelClampMode[DACchannel][%DAC][%ClampMode] = clampMode
+	ChannelClampMode[DACchannel][%DAC][%Headstage] = headStage
 
 	// ADC channels
 	ctrl = GetPanelControl(ADCchannel, CHANNEL_TYPE_ADC, CHANNEL_CONTROL_CHECK)
@@ -2694,7 +2695,8 @@ static Function DAP_ApplyClmpModeSavdSettngs(panelTitle, headStage, clampMode)
 	ctrl = GetPanelControl(ADCchannel, CHANNEL_TYPE_ADC, CHANNEL_CONTROL_UNIT)
 	SetSetVariableString(panelTitle, ctrl, ADUnit)
 	GuiStateTxT[ADCchannel][%$GetSpecialControlLabel(CHANNEL_TYPE_ADC, CHANNEL_CONTROL_UNIT)] = ADUnit
-	ChannelClampMode[ADCchannel][%ADC] = clampMode
+	ChannelClampMode[ADCchannel][%ADC][%ClampMode] = clampMode
+	ChannelClampMode[ADCchannel][%ADC][%Headstage] = headStage
 End
 
 static Function DAP_RemoveClampModeSettings(panelTitle, headStage, clampMode)
@@ -2722,12 +2724,12 @@ static Function DAP_RemoveClampModeSettings(panelTitle, headStage, clampMode)
 
 	ctrl = GetPanelControl(DACchannel, CHANNEL_TYPE_DAC, CHANNEL_CONTROL_CHECK)
 	SetCheckBoxState(panelTitle, ctrl, CHECKBOX_UNSELECTED)
-	ChannelClampMode[DACchannel][%DAC] = nan
+	ChannelClampMode[DACchannel][%DAC][] = NaN
 	GuiState[DACchannel][%$GetSpecialControlLabel(CHANNEL_TYPE_DAC, CHANNEL_CONTROL_CHECK)] = CHECKBOX_UNSELECTED
 
 	ctrl = GetPanelControl(ADCchannel, CHANNEL_TYPE_ADC, CHANNEL_CONTROL_CHECK)
 	SetCheckBoxState(panelTitle, ctrl, CHECKBOX_UNSELECTED)
-	ChannelClampMode[ADCchannel][%ADC] = nan
+	ChannelClampMode[ADCchannel][%ADC][] = NaN
 	GuiState[ADCchannel][%$GetSpecialControlLabel(CHANNEL_TYPE_ADC, CHANNEL_CONTROL_CHECK)] = CHECKBOX_UNSELECTED
 End
 

Packages/MIES/MIES_WaveDataFolderGetters.ipf
@@ -454,6 +454,35 @@ Function/Wave GetTPResultAsyncBuffer(panelTitle)
 	return wv
 End
 
+static Function GetHeadstageCompat(panelTitle, channelType, channelNumber, clampMode)
+	string panelTitle
+	variable channelType, channelNumber, clampMode
+
+	variable i, row
+
+	if(!AI_IsValidClampMode(clampMode))
+		return NaN
+	endif
+
+	WAVE chanAmpAssign = GetChanAmpAssign(panelTitle)
+
+	if(channelType == ITC_XOP_CHANNEL_TYPE_ADC)
+		row = clampMode == V_CLAMP_MODE ? 2 : 2 + 4
+	elseif(channelType == ITC_XOP_CHANNEL_TYPE_DAC)
+		row = clampMode == V_CLAMP_MODE ? 0 : 0 + 4
+	else
+		ASSERT(0, "Unexpected clamp mode")
+	endif
+
+	for(i = 0; i < NUM_HEADSTAGES; i += 1)
+		if(chanAmpAssign[row][i] == channelNumber)
+			return i
+		endif
+	endfor
+
+	return NaN
+ End
+
 /// @brief Return a wave reference to the channel clamp mode wave
 ///
 /// Only specialized code which does not have a headstage, or needs to know the
@@ -463,28 +492,40 @@ End
 /// - Channel numbers
 ///
 /// Columns:
-/// - 0: DAC channels
-/// - 1: ADC channels
+/// - 0: DAC
+/// - 1: ADC
 ///
-/// Contents:
-/// - Clamp mode: One of V_CLAMP_MODE, I_CLAMP_MODE and I_EQUAL_ZERO_MODE
+/// Layers:
+/// - 0: Clamp Mode
+/// - 1: Headstage
 Function/Wave GetChannelClampMode(panelTitle)
 	string panelTitle
 
 	DFREF dfr = GetDevicePath(panelTitle)
+	variable versionOfNewWave = 1
 
 	Wave/Z/SDFR=dfr wv = ChannelClampMode
 
-	if(WaveExists(wv))
+	if(ExistsWithCorrectLayoutVersion(wv, versionOfNewWave))
 		return wv
+	elseif(WaveExists(wv))
+		Redimension/N=(-1, -1, 2) wv
+
+		// prefill with existing algorithm for easier upgrades
+		wv[][%DAC][1] = GetHeadstageCompat(panelTitle, ITC_XOP_CHANNEL_TYPE_DAC, p, wv[p][%DAC][0])
+		wv[][%ADC][1] = GetHeadstageCompat(panelTitle, ITC_XOP_CHANNEL_TYPE_ADC, p, wv[p][%ADC][0])
+
+		return wv
+	else
+		Make/N=(NUM_AD_CHANNELS, 2, 2) dfr:ChannelClampMode/Wave=wv
+		wv = NaN
 	endif
-
-	Make/N=(NUM_AD_CHANNELS, 2) dfr:ChannelClampMode/Wave=wv
-
-	wv = NaN
 
 	SetDimLabel COLS, 0, DAC, wv
 	SetDimLabel COLS, 1, ADC, wv
+
+	SetDimLabel LAYERS, 0, ClampMode, wv
+	SetDimLabel LAYERS, 1, Headstage, wv
 
 	return wv
 End

commit 823706d7906e063c01957e6d4b4ae30093e3a861

DC_PlaceDataInHardwareDataWave: Use correct layer in channel clamp mode wave
Broken since cb206d3c (GetChannelClampMode: Extend to also hold headstage information, 2019-03-08).

Original-CommitID: 76671f653690f4394e287083cd14a0389c9af9cf

Packages/MIES/MIES_DataConfiguratorITC.ipf
@@ -758,7 +758,7 @@ static Function DC_PlaceDataInHardwareDataWave(panelTitle, numActiveChannels, da
 		maxITI = max(maxITI, WB_GetITI(stimSet[activeColumn], setColumn[activeColumn]))
 
 		if(IsFinite(headstageDAC[activeColumn]))
-			channelMode = ChannelClampMode[i][%DAC][%Headstage]
+			channelMode = ChannelClampMode[i][%DAC][%ClampMode]
 			if(channelMode == V_CLAMP_MODE)
 				testPulseAmplitude[activeColumn] = TPAmpVClamp
 			elseif(channelMode == I_CLAMP_MODE || channelMode == I_EQUAL_ZERO_MODE)