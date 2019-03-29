AllenInstitute/MIES

commit bace06b0083f6f65cd0c6a78dc0e01e5c96dd9ae

Extend CalculateAverage to not use CRC
If the destination wave should get the exact name as the
`averageWaveName` parameter without trailing CRC, use the new optional
`skipCRC` parameter. In `PA_Average` this reduces an additional
`MoveWave`.

Original-CommitID: 02758b0d0f0efa9e3af845188af4d6f589679abb

Packages/MIES/MIES_MiesUtilities.ipf
@@ -3225,15 +3225,19 @@ End
 /// @param listOfWaves       list of 1D waves to average
 /// @param averageDataFolder folder where the data is to be stored
 /// @param averageWaveName   base name of the averaged data
+/// @param skipCRC           [optional, defaults to false] Add the average wave CRC as suffix to its name
 ///
 /// @return wave reference to the average wave
-Function/WAVE CalculateAverage(listOfWaves, averageDataFolder, averageWaveName)
+Function/WAVE CalculateAverage(listOfWaves, averageDataFolder, averageWaveName, [skipCRC])
 	string listOfWaves
 	DFREF averageDataFolder
 	string averageWaveName
+	variable skipCRC
 
 	variable ret, crc
 	string key, wvName
+
+	skipCRC = ParamIsDefault(skipCRC) ? 0 : !!skipCRC
 
 	WAVE waveRefs = ListToWaveRefWave(listOfWaves, 1)
 	key = CA_AveragingKey(waveRefs)
@@ -3241,7 +3245,10 @@ Function/WAVE CalculateAverage(listOfWaves, averageDataFolder, averageWaveName)
 	WAVE/Z freeAverageWave = CA_TryFetchingEntryFromCache(key)
 
 	if(WaveExists(freeAverageWave)) // found in the cache
-		wvName = averageWaveName + "_" + num2istr(GetNumberFromWaveNote(freeAverageWave, "DataCRC"))
+		wvName = averageWaveName
+		if(!skipCRC)
+			wvName += "_" + num2istr(GetNumberFromWaveNote(freeAverageWave, "DataCRC"))
+		endif
 		WAVE/Z/SDFR=averageDataFolder permAverageWave = $wvName
 
 		if(!WaveExists(permAverageWave))
@@ -3257,14 +3264,18 @@ Function/WAVE CalculateAverage(listOfWaves, averageDataFolder, averageWaveName)
 
 	WAVE/SDFR=averageDataFolder averageWave = $averageWaveName
 
-	crc = WaveCRC(0, averageWave)
-	wvName = averageWaveName + "_" + num2istr(crc)
+	wvName = averageWaveName
 
-	WAVE/Z/SDFR=averageDataFolder averageWaveToDelete = $wvName
-	KillOrMoveToTrash(wv=averageWaveToDelete)
-	MoveWave averageWave, averageDataFolder:$wvName
+	if(!skipCRC)
+		crc = WaveCRC(0, averageWave)
+		wvName += "_" + num2istr(crc)
 
-	SetNumberInWaveNote(averageWave, "DataCRC", crc)
+		WAVE/Z/SDFR=averageDataFolder averageWaveToDelete = $wvName
+		KillOrMoveToTrash(wv=averageWaveToDelete)
+		MoveWave averageWave, averageDataFolder:$wvName
+		SetNumberInWaveNote(averageWave, "DataCRC", crc)
+	endif
+
 	AddEntryIntoWaveNoteAsList(averageWave, "SourceWavesForAverage", str=ReplaceString(";", listOfWaves, "|"))
 	CA_StoreEntryIntoCache(key, averageWave)
 

Packages/MIES/MIES_PulseAveraging.ipf
@@ -753,7 +753,7 @@ Function/WAVE PA_Average(listOfWaves, outputDFR, outputWaveName, [averageMode, g
 		averageMode = 0
 	endif
 	if(ParamIsDefault(getterMode))
-		getterMode = 2
+		getterMode = 1
 	endif
 
 	switch(getterMode)
@@ -792,13 +792,7 @@ Function/WAVE PA_Average(listOfWaves, outputDFR, outputWaveName, [averageMode, g
 				CA_StoreEntryIntoCache(key, wv)
 				break
 			case 1:
-				WAVE wv = CalculateAverage(listOfWaves, outputDFR, outputWaveName)
-				WAVE/Z/SDFR=outputDFR average = $outputWaveName
-				if(WaveExists(average))
-					MoveWaveWithOverwrite(average, wv)
-				else
-					Rename wv $outputWaveName
-				endif
+				WAVE wv = CalculateAverage(listOfWaves, outputDFR, outputWaveName, skipCRC = 1)
 				break
 			case 2:
 #if (IgorVersion() >= 8.00)

commit a9380777441cf6a5df74311fa7ace0951b1b9a74

Fix Retrieving Cached Average Waves
Cached Average Waves were not retrieved by CalculateAverage if the
destination wave already existed. This fix overwrites the existing wave
with the one retrieved from the cache.

Bug introduced in bace06b0 (Extend CalculateAverage to not use CRC,
2019-01-18)

Closes https://github.com/AllenInstitute/MIES/issues/128

Original-CommitID: 0d2a00fad40b9096724a7e4900046ec8a606cd0f

Packages/MIES/MIES_MiesUtilities.ipf
@@ -3243,19 +3243,20 @@ Function/WAVE CalculateAverage(listOfWaves, averageDataFolder, averageWaveName, 
 	key = CA_AveragingKey(waveRefs)
 
 	WAVE/Z freeAverageWave = CA_TryFetchingEntryFromCache(key)
-
 	if(WaveExists(freeAverageWave)) // found in the cache
 		wvName = averageWaveName
 		if(!skipCRC)
 			wvName += "_" + num2istr(GetNumberFromWaveNote(freeAverageWave, "DataCRC"))
 		endif
-		WAVE/Z/SDFR=averageDataFolder permAverageWave = $wvName
 
+		WAVE/Z/SDFR=averageDataFolder permAverageWave = $wvName
 		if(!WaveExists(permAverageWave))
 			MoveWave freeAverageWave, averageDataFolder:$wvName
-			WAVE/SDFR=averageDataFolder permAverageWave = $wvName
+		else
+			Duplicate/O freeAverageWave averageDataFolder:$wvName
 		endif
 
+		WAVE/SDFR=averageDataFolder permAverageWave = $wvName
 		return permAverageWave
 	endif
 