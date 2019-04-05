AllenInstitute/MIES

commit a30062d22ec0945bc99eb464ec01673948ac0f52

Add ChangeWaveLock
Original-CommitID: 495fc4f8b88ecbcd1cd32ec5cbb2f45643a8dbb8

Packages/MIES/MIES_Utilities.ipf
@@ -4042,3 +4042,25 @@ threadsafe Function/S TestPulseRunModeToString(runMode)
 			break
 	endswitch
 End
+
+/// @brief Adapt the wave lock status on the wave and its contained waves
+Function ChangeWaveLock(wv, val)
+	WAVE/WAVE wv
+	variable val
+
+	variable numEntries, i
+
+	val = !!val
+
+	if(!IsWaveRefWave(wv))
+		SetWaveLock val, wv
+		return NaN
+	endif
+
+	ASSERT(DimSize(wv, ROWS) != numpnts(wv), "Expected a 1D wave")
+	numEntries = DimSize(wv, ROWS)
+
+	for(i = 0; i < numEntries; i += 1)
+		ChangeWaveLock(wv[i], val)
+	endfor
+End

commit 95ba827b97cf7d92e2780a809815520dfbc6dcc0

ChangeWaveLock: Make it work
Broken since a30062d2 (Add ChangeWaveLock, 2019-03-28).

Original-CommitID: c58ad26e8fc5c2f4c3f300a5dc647f4e65321fa1

Packages/MIES/MIES_Utilities.ipf
@@ -4057,7 +4057,7 @@ Function ChangeWaveLock(wv, val)
 		return NaN
 	endif
 
-	ASSERT(DimSize(wv, ROWS) != numpnts(wv), "Expected a 1D wave")
+	ASSERT(DimSize(wv, ROWS) == numpnts(wv), "Expected a 1D wave")
 	numEntries = DimSize(wv, ROWS)
 
 	for(i = 0; i < numEntries; i += 1)