AllenInstitute/MIES

commit e3eaa511de8d2eb438a0b757ba92b67efce61189

Implement foreground single device RA
This is the DAQ mode with the lowest overhead as we don't have to wait
for the background functions (these are limited to 1/60 seconds
resolution).

In addition foreground DAQ can now also be stopped via ESC.

Original-CommitID: 1c8b4f27bdc50d32ed127a8668902416ef3bafad

Packages/MIES/MIES_RepeatedAcquisition.ipf
@@ -54,34 +54,76 @@ static Function RA_HandleITI_MD(panelTitle)
 	DQM_StartBackgroundTimer(panelTitle, ITI, funcList)
 End
 
+static Function RA_WaitUntiIITIDone(panelTitle, elapsedTime)
+	string panelTitle
+	variable elapsedTime
+
+	variable reftime, timeLeft
+	string oscilloscopeSubwindow
+
+	refTime = RelativeNowHighPrec()
+	oscilloscopeSubwindow = SCOPE_GetGraph(panelTitle)
+
+	do
+		timeLeft = max((refTime + elapsedTime) - RelativeNowHighPrec(), 0)
+		SetValDisplay(panelTitle, "valdisp_DataAcq_ITICountdown", var = timeLeft)
+
+		DoUpdate/W=$oscilloscopeSubwindow
+
+		if(timeLeft == 0)
+			return 0
+		endif
+	while(!(GetKeyState(0) & ESCAPE_KEY))
+
+	return 1
+End
+
 static Function RA_HandleITI(panelTitle)
 	string panelTitle
 
-	variable ITI
+	variable ITI, refTime, background, aborted
 	string funcList
 
 	AFM_CallAnalysisFunctions(panelTitle, POST_SET_EVENT)
 	ITI = RA_RecalculateITI(panelTitle)
+	background = DAG_GetNumericalValue(panelTitle, "Check_Settings_BackgrndDataAcq")
+	funcList = "RA_Counter(\"" + panelTitle + "\")"
 
 	if(!DAG_GetNumericalValue(panelTitle, "check_Settings_ITITP") || ITI <= 0)
 
-		funcList = "RA_Counter(\"" + panelTitle + "\")"
-
 		if(ITI <= 0)
 			ExecuteListOfFunctions(funcList)
-			return NaN
-		endif
+		elseif(background)
+			DQS_StartBackgroundTimer(panelTitle, ITI, funcList)
+		else
+			RA_WaitUntiIITIDone(panelTitle, ITI)
 
-		DQS_StartBackgroundTimer(panelTitle, ITI, funcList)
+			if(aborted)
+				RA_FinishAcquisition(panelTitle)
+			else
+				ExecuteListOfFunctions(funcList)
+			endif
+		endif
 
 		return NaN
 	endif
 
-	TP_Setup(panelTitle, TEST_PULSE_BG_SINGLE_DEVICE | TEST_PULSE_DURING_RA_MOD)
-	TPS_StartBackgroundTestPulse(panelTitle)
+	if(background)
+		TP_Setup(panelTitle, TEST_PULSE_BG_SINGLE_DEVICE | TEST_PULSE_DURING_RA_MOD)
+		TPS_StartBackgroundTestPulse(panelTitle)
+		funcList = "TPS_StopTestPulseSingleDevice(\"" + panelTitle + "\")" + ";" + "RA_Counter(\"" + panelTitle + "\")"
+		DQS_StartBackgroundTimer(panelTitle, ITI, funcList)
+	else
+		TP_Setup(panelTitle, TEST_PULSE_FG_SINGLE_DEVICE | TEST_PULSE_DURING_RA_MOD)
+		aborted = TPS_StartTestPulseForeground(panelTitle, elapsedTime = ITI)
+		TP_Teardown(panelTitle)
 
-	funcList = "TPS_StopTestPulseSingleDevice(\"" + panelTitle + "\")" + ";" + "RA_Counter(\"" + panelTitle + "\")"
-	DQS_StartBackgroundTimer(panelTitle, ITI, funcList)
+		if(aborted)
+			RA_FinishAcquisition(panelTitle)
+		else
+			ExecuteListOfFunctions(funcList)
+		endif
+	endif
 End
 
 /// @brief Return the total number of sets
@@ -228,12 +270,15 @@ Function RA_Counter(panelTitle)
 	if(Count < TotTrials)
 		try
 			DC_ConfigureDataForITC(panelTitle, DATA_ACQUISITION_MODE)
+
+			if(DAG_GetNumericalValue(panelTitle, "Check_Settings_BackgrndDataAcq"))
+				DQS_BkrdDataAcq(panelTitle)
+			else
+				DQS_DataAcq(panelTitle)
+			endif
 		catch
 			RA_FinishAcquisition(panelTitle)
 		endtry
-
-		ASSERT(DAG_GetNumericalValue(panelTitle, "Check_Settings_BackgrndDataAcq"), "Only background DAQ can be used with RA")
-		DQS_BkrdDataAcq(panelTitle)
 	else
 		RA_FinishAcquisition(panelTitle)
 	endif

Packages/MIES/MIES_DAEphys.ipf
@@ -1880,13 +1880,6 @@ Function DAP_CheckSettings(panelTitle, mode)
 				return 1
 			endif
 
-			if(DAG_GetNumericalValue(panelTitle, "Check_DataAcq1_RepeatAcq") && !DAG_GetNumericalValue(panelTitle, "Check_Settings_BackgrndDataAcq"))
-				printf "(%s) Repeated random acquisition with foregound DAQ is currently brocken.\r", panelTitle
-				printf "(%s) If you need this feature please contact the MIES developers.\r", panelTitle
-				ControlWindowToFront()
-				return 1
-			endif
-
 			if(DAG_GetNumericalValue(panelTitle, "Check_DataAcq1_DistribDaq") && DAG_GetNumericalValue(panelTitle, "Check_DataAcq1_dDAQOptOv"))
 				printf "(%s) Only one of distributed DAQ and optimized overlap distributed DAQ can be checked.\r", panelTitle
 				ControlWindowToFront()

Packages/MIES/MIES_DataAcquisition_Single.ipf
@@ -44,22 +44,17 @@ Function DQS_StartDAQSingleDevice(panelTitle, [useBackground])
 			return NaN
 		endtry
 
-		if(!useBackground)
-			DQS_DataAcq(panelTitle)
-			if(DAG_GetNumericalValue(panelTitle, "Check_DataAcq1_RepeatAcq"))
-				RA_Start(panelTitle)
-			else
-				DAP_OneTimeCallAfterDAQ(panelTitle)
-			endif
-		else
+		if(useBackground)
 			DQS_BkrdDataAcq(panelTitle)
+		else
+			DQS_DataAcq(panelTitle)
 		endif
 	else
 		DQ_StopDAQ(panelTitle)
 	endif
 End
 
-static Function DQS_DataAcq(panelTitle)
+Function DQS_DataAcq(panelTitle)
 	string panelTitle
 
 	variable fifoPos
@@ -81,11 +76,14 @@ static Function DQS_DataAcq(panelTitle)
 		DoXOPIdle
 		SCOPE_UpdateOscilloscopeData(panelTitle, DATA_ACQUISITION_MODE, fifoPos=fifoPos)
 		DoUpdate/W=$oscilloscopeSubwindow
+
+		if(GetKeyState(0) & ESCAPE_KEY)
+			DQS_StopDataAcq(panelTitle, forcedStop = 1)
+			return NaN
+		endif
 	while(HW_ITC_MoreData(ITCDeviceIDGlobal, fifoPos=fifoPos))
 
-	HW_StopAcq(HARDWARE_ITC_DAC, ITCDeviceIDGlobal, prepareForDAQ=1)
-
-	SWS_SaveAndScaleITCData(panelTitle)
+	DQS_StopDataAcq(panelTitle)
 End
 
 /// @brief Fifo monitor for DAQ Single Device

Packages/MIES/MIES_TestPulse_Single.ipf
@@ -101,11 +101,12 @@ End
 /// @param panelTitle  device
 /// @param elapsedTime [defaults to infinity] allow to run the testpulse for the given amount
 ///                                           of seconds only.
+/// @return zero if time elapsed, one if the Testpulse was manually stopped
 Function TPS_StartTestPulseForeground(panelTitle, [elapsedTime])
 	string panelTitle
 	variable elapsedTime
 
-	variable i, refTime
+	variable i, refTime, timeLeft
 	string oscilloscopeSubwindow
 
 	if(ParamIsDefault(elapsedTime))
@@ -134,12 +135,21 @@ Function TPS_StartTestPulseForeground(panelTitle, [elapsedTime])
 			SCOPE_UpdateGraph(panelTitle)
 		endif
 
-		DoUpdate/W=$oscilloscopeSubwindow
+		if(IsFinite(refTime))
+			timeLeft = max((refTime + elapsedTime) - RelativeNowHighPrec(), 0)
+			SetValDisplay(panelTitle, "valdisp_DataAcq_ITICountdown", var = timeLeft)
 
-		if(IsFinite(refTime) && abs(RelativeNowHighPrec() - (refTime + elapsedTime)) < 100e-6)
-			break
+			DoUpdate/W=$oscilloscopeSubwindow
+
+			if(timeLeft == 0)
+				return 0
+			endif
+		else
+			DoUpdate/W=$oscilloscopeSubwindow
 		endif
 
 		i += 1
 	while(!(GetKeyState(0) & ESCAPE_KEY))
-END
+
+	return 1
+End

commit 07f61dd282bb5cb5c8e4527c3eb04293e70e41ae

RA_HandleITI: Fix not aborting when ESC is pressed
Broken since e3eaa511 (Implement foreground single device RA,
2017-11-17).

Original-CommitID: 16bdc2dc9aaccdf2f7ee80c658d969c219479f34

Packages/MIES/MIES_RepeatedAcquisition.ipf
@@ -98,7 +98,7 @@ static Function RA_HandleITI(panelTitle)
 		elseif(background)
 			DQS_StartBackgroundTimer(panelTitle, ITI, funcList)
 		else
-			RA_WaitUntiIITIDone(panelTitle, ITI)
+			aborted = RA_WaitUntiIITIDone(panelTitle, ITI)
 
 			if(aborted)
 				RA_FinishAcquisition(panelTitle)