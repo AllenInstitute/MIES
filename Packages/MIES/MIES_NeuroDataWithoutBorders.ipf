AllenInstitute/MIES

commit 7c37bf08084e5294a4f8e87002e3e2cb230cfb33

NWB: Use Labnotebook property electrodeName if available
Original-CommitID: e58f5501c18dcf8df13285821d942d6b674483c9

Packages/MIES/MIES_NeuroDataWithoutBorders.ipf
@@ -457,6 +457,17 @@ static Function NWB_AppendSweepLowLevel(locationID, panelTitle, ITCDataWave, ITC
 	WAVE/T stimSets = GetLastSettingText(textualValues, sweep, STIM_WAVE_NAME_KEY)
 	ASSERT(WaveExists(stimSets), "Labnotebook is too old for NWB export.")
 
+	// 95402da6 (NWB: Allow documenting the physical electrode, 2016-08-05)
+	WAVE/Z/T electrodeNames = GetLastSettingText(textualValues, sweep, "Electrode")
+	if(!WaveExists(electrodeNames))
+		Make/FREE/T/N=(NUM_HEADSTAGES) electrodeNames = GetDefaultElectrodeName(p)
+	else
+		WAVE/Z nonEmptyElectrodes = FindIndizes(wvText=electrodeNames, col=0, prop=PROP_NON_EMPTY)
+		if(!WaveExists(nonEmptyElectrodes)) // all are empty
+			electrodeNames[] = GetDefaultElectrodeName(p)
+		endif
+	endif
+
 	STRUCT IPNWB#WriteChannelParams params
 	IPNWB#InitWriteChannelParams(params)
 
@@ -481,11 +492,11 @@ static Function NWB_AppendSweepLowLevel(locationID, panelTitle, ITCDataWave, ITC
 		STRUCT IPNWB#TimeSeriesProperties tsp
 
 		sprintf contents, "Headstage %d", i
-		IPNWB#AddElectrode(locationID, i, contents, panelTitle)
+		IPNWB#AddElectrode(locationID, electrodeNames[i], contents, panelTitle)
 
 		adc                    = ADCs[i]
 		dac                    = DACs[i]
-		params.electrodeNumber = i
+		params.electrodeName   = electrodeNames[i]
 		params.clampMode       = clampMode[i]
 		params.stimset         = stimSets[i]
 

Packages/IPNWB/IPNWB_HDF5Helpers.ipf
@@ -338,3 +338,13 @@ Function H5_CreateGroupsRecursively(locationID, path, [groupID])
 		groupID = id
 	endif
 End
+
+/// @brief Return true if `name` is a valid hdf5 identifier
+///
+/// This is more restrictive than the actual HDF5 library checks.
+/// See the BNF Grammar [here](https://www.hdfgroup.org/HDF5/doc/UG/HDF5_Users_Guide-Responsive%20HTML5/index.html#t=HDF5_Users_Guide%2FGroups%2FHDF5_Groups.htm%3Frhtocid%3Dtoc4.0_1%23TOC_4_1_Introductionbc-1).
+Function H5_IsValidIdentifier(name)
+	string name
+
+	return GrepString(name, "^[A-Za-z0-9_ -]+$")
+End

Packages/IPNWB/IPNWB_Writer.ipf
@@ -196,15 +196,17 @@ Function AddDevice(locationID, name, data)
 	H5_WriteTextDataset(locationID, path, str=data, skipIfExists=1)
 End
 
-/// @brief Add an entry for the electrode `number` with contents `data`
-Function AddElectrode(locationID, number, data, device)
-	variable locationID, number
-	string data, device
+/// @brief Add an entry for the electrode `name` with contents `data`
+Function AddElectrode(locationID, name, data, device)
+	variable locationID
+	string name, data, device
 
 	string path
 	variable groupID
 
-	sprintf path, "/general/intracellular_ephys/electrode_%d", number
+	ASSERT(H5_IsValidIdentifier(name), "The electrode name must be a valid HDF5 identifier")
+
+	sprintf path, "/general/intracellular_ephys/electrode_%s", name
 	H5_CreateGroupsRecursively(locationID, path, groupID=groupID)
 	H5_WriteTextDataset(groupID, "description", str=data, overwrite=1)
 	H5_WriteTextDataset(groupID, "device", str=device, overwrite=1)
@@ -402,6 +404,7 @@ Structure WriteChannelParams
 	variable channelType     ///< channel type, one of @ref IPNWB_ChannelTypes
 	variable channelNumber   ///< running number of the channel
 	variable electrodeNumber ///< electrode identifier the channel was acquired with
+	string electrodeName     ///< electrode identifier the channel was acquired with (string version)
 	variable clampMode       ///< clamp mode, one of @ref IPNWB_ClampModes
 	variable groupIndex      ///< Should be filled with the result of GetNextFreeGroupIndex(locationID, path) before
 							 ///  the first call and must stay constant for all channels for this measurement.
@@ -473,7 +476,7 @@ Function WriteSingleChannel(locationID, path, p, tsp, [chunkedLayout])
 		sprintf str, "%s", channelTypeStr
 	endif
 
-	sprintf source, "Device=%s;Sweep=%d;%s;ElectrodeNumber=%d", p.device, p.sweep, str, p.electrodeNumber
+	sprintf source, "Device=%s;Sweep=%d;%s;ElectrodeNumber=%d;ElectrodeName=%s", p.device, p.sweep, str, p.electrodeNumber, p.electrodeName
 
 	if(strlen(p.channelSuffixDesc) > 0 && strlen(p.channelSuffix) > 0)
 		ASSERT(strsearch(p.channelSuffix, "=", 0) == -1, "channelSuffix must not contain an equals (=) symbol")
@@ -487,7 +490,7 @@ Function WriteSingleChannel(locationID, path, p, tsp, [chunkedLayout])
 	endif
 
 	if(p.channelType == CHANNEL_TYPE_ADC)
-		sprintf str, "electrode_%d", p.electrodeNumber
+		sprintf str, "electrode_%s", p.electrodeName
 		H5_WriteTextDataset(groupID, "electrode_name", str=str, overwrite=1)
 
 		if(p.clampMode == V_CLAMP_MODE)
@@ -500,7 +503,7 @@ Function WriteSingleChannel(locationID, path, p, tsp, [chunkedLayout])
 			ancestry = "TimeSeries;PatchClampSeries"
 		endif
 	elseif(p.channelType == CHANNEL_TYPE_DAC)
-		sprintf str, "electrode_%d", p.electrodeNumber
+		sprintf str, "electrode_%s", p.electrodeName
 		H5_WriteTextDataset(groupID, "electrode_name", str=str, overwrite=1)
 
 		if(p.clampMode == V_CLAMP_MODE)

commit 01264daf3866b3eaf779e9317638c4d616006c31

WIP NWB_AppendSweepLowLevel: Set correct electrode number for associated DA/AD channels
Bug introduced in 7c37bf08 (NWB: Use Labnotebook property electrodeName
if available, 2016-08-06).

With default electrode names, which are just strings with the electrode
numbers, the buggy source attributes are

ITC1600_Dev_0;Sweep=0;AD=10;ElectrodeNumber=0;ElectrodeName=6

(note the difference between the ElectrodeName and the ElectrodeNumber)

where as it should be

ITC1600_Dev_0;Sweep=0;AD=10;ElectrodeNumber=6;ElectrodeName=6

One side effect of that bug is also that all TimeSeries attributes
written by NWB_GetTimeSeriesProperties are fetched for the first
headstage and not for the correct ones.

Original-CommitID: 6e0b27401223012ec8f101509faf63b25969be19

Packages/MIES/MIES_NeuroDataWithoutBorders.ipf
@@ -764,6 +764,7 @@ static Function NWB_AppendSweepLowLevel(locationID, panelTitle, ITCDataWave, ITC
 
 		adc                    = ADCs[i]
 		dac                    = DACs[i]
+		params.electrodeNumber = i
 		params.electrodeName   = electrodeNames[i]
 		params.clampMode       = clampMode[i]
 		params.stimset         = stimSets[i]