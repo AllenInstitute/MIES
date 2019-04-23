AllenInstitute/MIES

commit 903cd360b109e1573cd5d3836ad97f7e5a5f68f3

DAP_GetNIDeviceList: No checks in evil mode
Original-CommitID: 035d87d9bfafe07177610cf45a7374d34408ecdd

Packages/MIES/MIES_DAEphys.ipf
@@ -50,6 +50,10 @@ Function/S DAP_GetNIDeviceList()
 		if(IsEmpty(DAQmxDevice))
 			break
 		endif
+
+#ifdef EVIL_KITTEN_EATING_MODE
+		devList += DAQmxDevName + ";"
+#else
 		for(j = 0; j < numPattern; j += 1)
 			pattern = StringFromList(j, NI_DAC_PATTERNS, "|")
 			if(!(strsearch(DAQmxDevice, pattern, 0) == -1))
@@ -63,6 +67,7 @@ Function/S DAP_GetNIDeviceList()
 				endif
 			endif
 		endfor
+#endif
 	endfor
 
 	globalNIDevList = devList

commit 31960c0c6b1c66f133bfba1119ce35bbd7058bf7

DAP_GetNIDeviceList(): Fix evil mode code
We need the name of the NI device and not the full configuration string.

Bug introduced in 903cd360 (DAP_GetNIDeviceList: No checks in evil mode,
2019-02-14).

Original-CommitID: 31c918de156ceaea19e490a29d8408bdfab2b9cc

Packages/MIES/MIES_DAEphys.ipf
@@ -53,7 +53,7 @@ Function/S DAP_GetNIDeviceList()
 		endif
 
 #ifdef EVIL_KITTEN_EATING_MODE
-		devList += DAQmxDevName + ";"
+		devList += StringByKey("NAME", DAQmxDevice) + ";"
 #else
 		for(j = 0; j < numPattern; j += 1)
 			pattern = StringFromList(j, NI_DAC_PATTERNS, "|")