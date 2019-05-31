AllenInstitute/MIES

commit cd0f66aae2ca82cf8f20b6778b16a78d49c8b940

upload-github-release-asset.sh: Reset latest tag on master branch
This now makes sense as we work on the public MIES repository.

tools/upload-github-release-asset.sh
@@ -48,6 +48,8 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 case "$branch" in
   master)
     tag=latest
+    git tag --force $tags
+    git push --force origin $tags
     ;;
   release/*)
     tag=$(git tag | tail -1)

commit b6d488625bac81d77449d135bd14a8ee0b367440

upload-github-release-asset.sh: Fix invalid variable reference
Bug introduced in cd0f66aa (upload-github-release-asset.sh: Reset latest
tag on master branch, 2019-05-30).

tools/upload-github-release-asset.sh
@@ -48,8 +48,8 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 case "$branch" in
   master)
     tag=latest
-    git tag --force $tags
-    git push --force origin $tags
+    git tag --force $tag
+    git push --force origin $tag
     ;;
   release/*)
     tag=$(git tag | tail -1)