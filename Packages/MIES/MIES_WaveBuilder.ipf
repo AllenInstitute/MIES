AllenInstitute/MIES

commit 7f929ad2c361fc48f3c17434074e69d93f593b41

Wavebuilder: Upgrade WP to version 10
Introduce per entry delta multiplier/exponentials and per entry delta
operation modes.

We also rework WB_AddDelta to be more general and us the dimension
labels to find the corresponding entries.

Original-CommitID: d5cd30ab2412ddd3ae58222edcd2f6e0f7067ed7

Packages/MIES/MIES_WaveBuilder.ipf
@@ -407,9 +407,39 @@ Function/Wave WB_GetStimSet([setName])
 	return stimSet
 End
 
+/// @brief Return a free wave with indizes referencing the values with delta values
+///
+/// Indizes are into `WP` and reference the entry with the value itself.
+/// @sa AddDimLabelsToWP()
+static Function/WAVE WB_GetControlWithDeltaIdx()
+
+	Make/FREE/B indizes = {0, 2, 4, 6, 10, 12, 14, 16, 20, 22, 24, 26, 28, 30, 45}
+	return indizes
+End
+
+/// @brief Return the `WP`/`WPT` dimension labels for the related delta controls
+/// given the index into `WP` of the value itself.
+Function WB_GetDeltaDimLabel(WP, index, s)
+	WAVE WP
+	variable index
+	STRUCT DeltaControlNames &s
+
+	string name
+
+	name = GetDimLabel(WP, ROWS, index)
+	ASSERT(!IsEmpty(name), "Invalid empty name")
+
+	s.main   = name
+	s.delta  = name + " delta"
+	s.dme    = name + " dme"
+	s.ldelta = name + " ldel"
+	s.op     = name + " op"
+End
+
 /// @brief Add delta to appropriate parameters
 ///
-/// Relies on alternating sequence of parameter and delta's in parameter waves as documented in WB_MakeWaveBuilderWave()
+/// Relies on alternating sequence of parameter and delta's in parameter waves
+/// as documented in WB_MakeWaveBuilderWave().
 ///
 /// @param setName    name of the stimset
 /// @param WP         wavebuilder parameter wave (temporary copy)
@@ -422,72 +452,61 @@ static Function WB_AddDelta(setName, WP, numEpochs)
 	variable numEpochs
 
 	variable i, j, k
-	variable offsetFactor, durationFactor, amplitudeFactor
-	variable operation, factor
-	variable numEpochTypes
+	variable operation, row
+	variable numEpochTypes, numEntries
+	string list, entry
 
 	numEpochTypes = DimSize(WP, LAYERS)
 
-	for(i = 0; i <= 30; i += 2)
+	WAVE indizes = WB_GetControlWithDeltaIdx()
+	numEntries = DimSize(indizes, ROWS)
+
+	for(i = 0; i < numEntries; i += 1)
+		STRUCT DeltaControlNames s
+		WB_GetDeltaDimLabel(WP, indizes[i], s)
+
 		for(j = 0; j < numEpochs; j += 1)
 			for(k = 0; k < numEpochTypes; k += 1)
 
-				WP[i][j][k] += WP[i + 1][j][k]
-
-				operation = WP[40][j][k]
-				if(operation)
-					durationFactor  = WP[52][j][k]
-					amplitudeFactor = WP[50][j][k]
-					offsetFactor    = WP[51][j][k]
-					switch(i)
-						case 0:
-							factor = durationFactor
-							break
-						case 2:
-							factor = amplitudeFactor
-							break
-						case 4:
-							factor = offsetFactor
-							break
-						default:
-							factor = 1
-							break
-					endswitch
-
-					switch(operation)
-						case 1: // Simple factor
-							WP[i + 1][j][k] = WP[i + 1][j][k] * factor
-							break
-						case 2: // Log
-							// ignore a delta value of exactly zero
-							WP[i + 1][j][k] = WP[i + 1][j][k] == 0 ? 0 : log(WP[i + 1][j][k])
-							break
-						case 3: // Squared
-							WP[i + 1][j][k] = (WP[i + 1][j][k])^2
-							break
-						case 4: // Power
-							WP[i + 1][j][k] = (WP[i + 1][j][k])^factor
-							break
-						case 5: // Alternate
-							WP[i + 1][j][k] *= -1
-							break
-						default:
-							// future proof
-							printf "WB_AddDelta: Stimset %s uses an unknown operation %d and can therefore not be recreated.\r", setName, operation
-							return 1
-							break
-					endswitch
+				// special handling for "Number of pulses"
+				// don't do anything if the number of pulses is calculated
+				// and not entered
+				if(indizes[i] == 45 && !WP[46][j][k])
+					continue
 				endif
-			endfor
-		endfor
-	endfor
 
-	// number of pulses has a non-standard delta position
-	for(j = 0; j < numEpochs; j += 1)
-		for(k = 0; k < numEpochTypes; k += 1)
-			if(WP[46][j][k]) // use pulses checkbox
-				WP[45][j][k] += WP[47][j][k]
-			endif
+				operation = WP[%$s.op][j][k]
+				row       = FindDimlabel(WP, ROWS, s.delta)
+
+				WP[%$s.main][j][k] += WP[row][j][k]
+
+				switch(operation)
+					case 0:
+						// delta is constant
+						break
+					case 1: // Simple factor
+						WP[row][j][k] = WP[row][j][k] * WP[%$s.dme][j][k]
+						break
+					case 2: // Log
+						// ignore a delta value of exactly zero
+						WP[row][j][k] = WP[row][j][k] == 0 ? 0 : log(WP[row][j][k])
+						break
+					case 3: // Squared
+						WP[row][j][k] = (WP[row][j][k])^2
+						break
+					case 4: // Power
+						WP[row][j][k] = (WP[row][j][k])^(WP[%$s.dme][j][k])
+						break
+					case 5: // Alternate
+						WP[row][j][k] *= -1
+						break
+					default:
+						// future proof
+						printf "WB_AddDelta: Stimset %s uses an unknown operation %d and can therefore not be recreated.\r", setName, operation
+						return 1
+						break
+				endswitch
+			endfor
 		endfor
 	endfor
 

Packages/MIES/MIES_Structures.ipf
@@ -190,3 +190,8 @@ Structure AnalysisFunction_V3
 	/// wave. Settable via WBP_AddAnalysisParameter().
 	string params
 EndStructure
+
+/// @brief Helper structure for WB_GetDeltaDimLabel()
+Structure DeltaControlNames
+	string main, delta, dme, op, ldelta
+EndStructure

Packages/MIES/MIES_WaveDataFolderGetters.ipf
@@ -2514,7 +2514,7 @@ Function/WAVE GetTestPulse()
 	return wv
 End
 
-static Constant WP_WAVE_LAYOUT_VERSION = 9
+static Constant WP_WAVE_LAYOUT_VERSION = 10
 
 /// @brief Automated testing helper
 static Function GetWPVersion()
@@ -2531,7 +2531,7 @@ Function UpgradeWaveParam(wv)
 		return NaN
 	endif
 
-	Redimension/N=(61, -1, 9) wv
+	Redimension/N=(86, -1, 9) wv
 	AddDimLabelsToWP(wv)
 
 	// custom wave offsets special location removed
@@ -2557,6 +2557,13 @@ Function UpgradeWaveParam(wv)
 	if(WaveVersionIsSmaller(wv, 9))
 		// preselect per epoch RNG
 		wv[39][][] = 1
+	endif
+
+	// upgrade to wave version 10
+	if(WaveVersionIsSmaller(wv, 10))
+		// delta operation mode was global before but now is per entry
+		Multithread wv[70,85][][] = wv[40][q][r]
+		wv[40][][] = NaN
 	endif
 
 	SetWaveVersion(wv, WP_WAVE_LAYOUT_VERSION)
@@ -2588,12 +2595,12 @@ static Function AddDimLabelsToWP(wv)
 
 	SetDimLabel ROWS, -1, $("Property")                       , wv
 	SetDimLabel ROWS, 0 , $("Duration")                       , wv
-	SetDimLabel ROWS, 1 , $("Duration step delta")            , wv
+	SetDimLabel ROWS, 1 , $("Duration delta")                 , wv
 	SetDimLabel ROWS, 2 , $("Amplitude")                      , wv
 	SetDimLabel ROWS, 3 , $("Amplitude delta")                , wv
 	SetDimLabel ROWS, 4 , $("Offset")                         , wv
 	SetDimLabel ROWS, 5 , $("Offset delta")                   , wv
-	SetDimLabel ROWS, 6 , $("Sin/chirp/saw tooth frequency")  , wv
+	SetDimLabel ROWS, 6 , $("Sin/chirp/saw frequency")        , wv
 	SetDimLabel ROWS, 7 , $("Sin/chirp/saw frequency delta")  , wv
 	SetDimLabel ROWS, 8 , $("Train pulse duration")           , wv
 	SetDimLabel ROWS, 9 , $("Train pulse duration delta")     , wv
@@ -2620,7 +2627,7 @@ static Function AddDimLabelsToWP(wv)
 	SetDimLabel ROWS, 31, $("PT: Last Mixed Frequency delta") , wv
 	// unused entries are not labeled
 	SetDimLabel ROWS, 39, $("Reseed RNG for each epoch")      , wv
-	SetDimLabel ROWS, 40, $("Delta type")                     , wv
+	// unused entry, previously the global delta operation
 	SetDimLabel ROWS, 41, $("PT: Mixed Frequency")            , wv
 	SetDimLabel ROWS, 42, $("PT: Shuffle")                    , wv
 	SetDimLabel ROWS, 43, $("Chirp type: Log or sin")         , wv
@@ -2630,13 +2637,43 @@ static Function AddDimLabelsToWP(wv)
 	SetDimLabel ROWS, 47, $("Number of pulses delta")         , wv
 	SetDimLabel ROWS, 48, $("Random Seed")                    , wv
 	SetDimLabel ROWS, 49, $("Reseed RNG for each step")       , wv
-	SetDimLabel ROWS, 50, $("Amplitude delta mult/exp")       , wv
-	SetDimLabel ROWS, 51, $("Offset delta mult/exp")          , wv
-	SetDimLabel ROWS, 52, $("Duration delta mult/exp")        , wv
+	// `dme` means `delta multiplier/exponential`
+	SetDimLabel ROWS, 50, $("Amplitude dme")                  , wv
+	SetDimLabel ROWS, 51, $("Offset dme")                     , wv
+	SetDimLabel ROWS, 52, $("Duration dme")                   , wv
 	SetDimLabel ROWS, 53, $("Trigonometric function Sin/Cos") , wv
 	SetDimLabel ROWS, 54, $("Noise Type: White, Pink, Brown") , wv
 	SetDimLabel ROWS, 55, $("Build resolution (index)")       , wv
 	SetDimLabel ROWS, 56, $("Pulse train type (index)")       , wv
+	SetDimLabel ROWS, 57, $("Sin/chirp/saw frequency dme")    , wv
+	SetDimLabel ROWS, 58, $("Train pulse duration dme")       , wv
+	SetDimLabel ROWS, 59, $("PSC exp rise time dme")          , wv
+	SetDimLabel ROWS, 60, $("PSC exp decay time 1/2 dme")     , wv
+	SetDimLabel ROWS, 61, $("PSC exp decay time 2/2 dme")     , wv
+	SetDimLabel ROWS, 62, $("PSC ratio decay times dme")      , wv
+	SetDimLabel ROWS, 63, $("Low pass filter cut off dme")    , wv
+	SetDimLabel ROWS, 64, $("High pass filter cut off dme")   , wv
+	SetDimLabel ROWS, 65, $("Chirp end frequency dme")        , wv
+	SetDimLabel ROWS, 66, $("Noise filter order dme")         , wv
+	SetDimLabel ROWS, 67, $("PT: First Mixed Frequency dme")  , wv
+	SetDimLabel ROWS, 68, $("PT: Last Mixed Frequency dme")   , wv
+	SetDimLabel ROWS, 69, $("Number of pulses dme")           , wv
+	SetDimLabel ROWS, 70, $("Amplitude op")                   , wv
+	SetDimLabel ROWS, 71, $("Offset op")                      , wv
+	SetDimLabel ROWS, 72, $("Duration op")                    , wv
+	SetDimLabel ROWS, 73, $("Sin/chirp/saw frequency op")     , wv
+	SetDimLabel ROWS, 74, $("Train pulse duration op")        , wv
+	SetDimLabel ROWS, 75, $("PSC exp rise time op")           , wv
+	SetDimLabel ROWS, 76, $("PSC exp decay time 1/2 op")      , wv
+	SetDimLabel ROWS, 77, $("PSC exp decay time 2/2 op")      , wv
+	SetDimLabel ROWS, 78, $("PSC ratio decay times op")       , wv
+	SetDimLabel ROWS, 79, $("Low pass filter cut off op")     , wv
+	SetDimLabel ROWS, 80, $("High pass filter cut off op")    , wv
+	SetDimLabel ROWS, 81, $("Chirp end frequency op")         , wv
+	SetDimLabel ROWS, 82, $("Noise filter order op")          , wv
+	SetDimLabel ROWS, 83, $("PT: First Mixed Frequency op")   , wv
+	SetDimLabel ROWS, 84, $("PT: Last Mixed Frequency op")    , wv
+	SetDimLabel ROWS, 85, $("Number of pulses op")            , wv
 End
 
 /// @brief Return the parameter wave for the wave builder panel
@@ -2665,7 +2702,7 @@ Function/WAVE GetWaveBuilderWaveParam()
 	if(WaveExists(wv))
 		UpgradeWaveParam(wv)
 	else
-		Make/N=(61, 100, 9) dfr:WP/Wave=wv
+		Make/N=(86, 100, 9) dfr:WP/Wave=wv
 
 		// noise low/high pass filter to off
 		wv[20][][EPOCH_TYPE_NOISE] = 0

commit c9119176bad5abba9d49e0ebfc969ea654ec6779

Wavebuilder: Fix forgotten delta mode for pulse duration for pulse train pulse
Broken since 7f929ad2 (Wavebuilder: Upgrade WP to version 10,
2018-06-14).

Packages/MIES/MIES_WaveBuilder.ipf
@@ -454,7 +454,7 @@ End
 /// @sa AddDimLabelsToWP()
 static Function/WAVE WB_GetControlWithDeltaIdx()
 
-	Make/FREE/B indizes = {0, 2, 4, 6, 10, 12, 14, 16, 20, 22, 24, 26, 28, 30, 45, 99}
+	Make/FREE/B indizes = {0, 2, 4, 6, 8, 10, 12, 14, 16, 20, 22, 24, 26, 28, 30, 45, 99}
 	return indizes
 End
 
@@ -464,7 +464,7 @@ End
 static Function/WAVE WB_GetControlWithDeltaWvs(WP, SegWvType)
 	WAVE WP, SegWvType
 
-	Make/FREE/WAVE locations = {WP, WP, WP, WP, WP, WP, WP, WP, WP, WP, WP, WP, WP, WP, WP, SegWvType}
+	Make/FREE/WAVE locations = {WP, WP, WP, WP, WP, WP, WP, WP, WP, WP, WP, WP, WP, WP, WP, WP, SegWvType}
 	return locations
 End
 