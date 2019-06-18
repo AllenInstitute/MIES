AllenInstitute/MIES

commit 8505172f92638b9f946a5f44df382bef5fe9ac5d

Databrowser: Use PGC routines for on-creation locking
This is much more reliable.

Original-CommitID: f2ce769f7d29e100b8d8fa0c9a0ff84e1739ee5d

Packages/MIES/MIES_DataBrowser.ipf
@@ -13,7 +13,7 @@
 static strConstant EXT_PANEL_SETTINGSHISTORY = "SettingsHistoryPanel"
 
 Function/S DB_OpenDataBrowser()
-	string win, device, devicesWithData
+	string win, device, devicesWithData, bsPanel
 
 	Execute "DataBrowser()"
 	win = GetCurrentWindow()
@@ -28,7 +28,8 @@ Function/S DB_OpenDataBrowser()
 	devicesWithData = ListMatch(DB_GetAllDevicesWithData(), "!" + NONE)
 	if(ItemsInList(devicesWithData) == 1)
 		device = StringFromList(0, devicesWithData)
-		DB_LockToDevice(win, device)
+		bsPanel = BSP_GetPanel(win)
+		PGC_SetAndActivateControl(bsPanel, "popup_DB_lockedDevices", val = 0)
 	endif
 
 	return GetMainWindow(GetCurrentWindow())

Packages/MIES/MIES_BrowserSettingsPanel.ipf
@@ -170,9 +170,6 @@ Function BSP_DynamicStartupSettings(mainPanel)
 	if(IsDataBrowser(mainPanel))
 		EnableControls(bsPanel, controlsDB)
 		DisableControls(bsPanel, controlsSB)
-		if(BSP_HasBoundDevice(mainPanel))
-			SetPopupMenuString(bsPanel, "popup_DB_lockedDevices", BSP_GetDevice(mainPanel))
-		endif
 	else
 		EnableControls(bsPanel, controlsSB)
 		DisableControls(bsPanel, controlsDB)

commit 842dae8aecaa8dd66acff042a14ecc4b2099e4f8

DB_OpenDataBrowser: Fix locking if we only have one device
Broken since 8505172f (Databrowser: Use PGC routines for on-creation
locking, 2018-05-07).

Packages/MIES/MIES_DataBrowser.ipf
@@ -28,7 +28,7 @@ Function/S DB_OpenDataBrowser()
 	if(ItemsInList(devicesWithData) == 1)
 		device = StringFromList(0, devicesWithData)
 		bsPanel = BSP_GetPanel(win)
-		PGC_SetAndActivateControl(bsPanel, "popup_DB_lockedDevices", val = 0)
+		PGC_SetAndActivateControl(bsPanel, "popup_DB_lockedDevices", str = device)
 	endif
 
 	return GetMainWindow(GetCurrentWindow())