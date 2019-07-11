AllenInstitute/MIES

commit d25b9d5731314f22f20ba95b5b6421f36af1d178

Refactor GetTTLStimSets to be hardware independent
The current GetTTLStimSets forces the user to know a lot about the
hardware as the channel parameter is either the *hardware channel* for
ITC hardware or ignored for NI hardware.

Instead the user usually wants to just get a list of TTL stimsets for
the DAEphys TTL channels.

Refactor the function to return exactly that and also add some testing.

Packages/MIES/MIES_NeuroDataWithoutBorders.ipf
@@ -629,20 +629,15 @@ static Function/S NWB_GetStimsetFromSweepGeneric(sweep, numericalValues, textual
 		stimsetList = AddListItem(name, stimsetList)
 	endfor
 
+	WAVE/Z/T ttlStimSets = GetTTLstimSets(numericalValues, textualValues, sweep)
+
 	// handle TTL channels
 	for(i = 0; i < NUM_DA_TTL_CHANNELS; i += 1)
-		ttlList = GetTTLstimSets(numericalValues, textualValues, sweep, i)
-		numEntries = ItemsInList(ttlList)
-		for(j = 0; j < numEntries; j += 1)
-			name = StringFromList(j, ttlList)
-
-			// work around empty TTL stim set bug in labnotebook
-			if(isEmpty(name))
-				continue
-			endif
-
-			stimsetList = AddListItem(name, stimsetList)
-		endfor
+		name = ttlStimSets[i]
+		if(isEmpty(name))
+			continue
+		endif
+		stimsetList = AddListItem(name, stimsetList)
 	endfor
 
 	return stimsetList
@@ -814,8 +809,16 @@ static Function NWB_AppendSweepLowLevel(locationID, panelTitle, ITCDataWave, ITC
 	// introduced in db531d20 (DC_PlaceDataInITCDataWave: Document the digitizer hardware type, 2018-07-30)
 	// before that we only had ITC hardware
 	hardwareType = GetLastSettingIndep(numericalValues, sweep, "Digitizer Hardware Type", DATA_ACQUISITION_MODE, defValue = HARDWARE_ITC_DAC)
+	WAVE/Z/T ttlStimsets = GetTTLStimSets(numericalValues, textualValues, sweep)
 
+	// i has the following meaning:
+	// - ITC hardware: hardware channel
+	// - NI hardware: DAEphys TTL channel
 	for(i = 0; i < NUM_DA_TTL_CHANNELS; i += 1)
+
+		if(!WaveExists(ttlStimsets))
+			break
+		endif
 
 		if(hardwareType == HARDWARE_ITC_DAC)
 			ttlBits = GetTTLBits(numericalValues, sweep, i)
@@ -823,12 +826,10 @@ static Function NWB_AppendSweepLowLevel(locationID, panelTitle, ITCDataWave, ITC
 				continue
 			endif
 
-			listOfStimsets = GetTTLstimSets(numericalValues, textualValues, sweep, i)
 		elseif(hardwareType == HARDWARE_NI_DAC)
 			ttlBits = NaN
 
-			listOfStimsets = GetTTLstimSets(numericalValues, textualValues, sweep, NaN)
-			stimset = StringFromList(i, listOfStimsets)
+			stimset = ttlStimsets[i]
 			if(IsEmpty(stimset))
 				continue
 			endif
@@ -860,7 +861,7 @@ static Function NWB_AppendSweepLowLevel(locationID, panelTitle, ITCDataWave, ITC
 				path                 = "/stimulus/presentation"
 				params.channelSuffix = num2str(ttlBit)
 				params.channelSuffixDesc = NWB_SOURCE_TTL_BIT
-				params.stimset       = StringFromList(log(ttlBit)/log(2), listOfStimsets)
+				params.stimset       = ttlStimsets[log(ttlBit)/log(2)]
 				NWB_GetTimeSeriesProperties(params, tsp)
 				params.groupIndex    = IsFinite(params.groupIndex) ? params.groupIndex : IPNWB#GetNextFreeGroupIndex(locationID, path)
 				IPNWB#WriteSingleChannel(locationID, path, params, tsp, compressionMode = compressionMode)

Packages/MIES/MIES_MiesUtilities.ipf
@@ -4096,35 +4096,46 @@ Function GetIndexForHeadstageIndepData(numericalValues)
 	return DimSize(numericalValues, LAYERS) == NUM_HEADSTAGES ? 0 : INDEP_HEADSTAGE
 End
 
-/// @brief Get the TTL stim sets from the labnotebook
+/// @brief Return a list of TTL stimsets which are indexed by DAEphys TTL channels
+///
+/// The indexing here is **hardware independent**.
+/// For ITC hardware the assertion "log(ttlBit)/log(2) == DAEphys TTL channel" holds.
+///
 /// @param numericalValues Numerical labnotebook values
 /// @param textualValues   Text labnotebook values
 /// @param sweep           Sweep number
-/// @param channel         TTL channel
-///
-/// @return list of stim sets, empty entries for non active TTL bits
-Function/S GetTTLStimSets(numericalValues, textualValues, sweep, channel)
-	WAVE numericalValues
-	WAVE/T textualValues
-	variable sweep, channel
+Function/WAVE GetTTLStimSets(numericalValues, textualValues, sweep)
+	WAVE numericalValues, textualValues
+	variable sweep
 
-	variable index = GetIndexForHeadstageIndepData(numericalValues)
+	variable index
 
-	WAVE/Z ttlRackZeroChannel = GetLastSetting(numericalValues, sweep, "TTL rack zero channel", DATA_ACQUISITION_MODE)
-	WAVE/Z ttlRackOneChannel  = GetLastSetting(numericalValues, sweep, "TTL rack one channel", DATA_ACQUISITION_MODE)
-	WAVE/Z ttlChannels        = GetLastSetting(textualValues, sweep, "TTL channels", DATA_ACQUISITION_MODE)
+	index = GetIndexForHeadstageIndepData(numericalValues)
 
-	if(WaveExists(ttlChannels))
+	WAVE/T/Z ttlStimsets = GetLastSetting(textualValues, sweep, "TTL stim sets", DATA_ACQUISITION_MODE)
+	WAVE/T/Z ttlStimsetsRackZero = GetLastSetting(textualValues, sweep, "TTL rack zero stim sets", DATA_ACQUISITION_MODE)
+	WAVE/T/Z ttlStimsetsRackOne = GetLastSetting(textualValues, sweep, "TTL rack one stim sets", DATA_ACQUISITION_MODE)
+
+	if(WaveExists(ttlStimsets))
+		// NI hardware
 		WAVE/T ttlStimsets = GetLastSetting(textualValues, sweep, "TTL stim sets", DATA_ACQUISITION_MODE)
-	elseif(WaveExists(ttlRackZeroChannel) && ttlRackZeroChannel[index] == channel)
-		WAVE/T ttlStimsets = GetLastSetting(textualValues, sweep, "TTL rack zero stim sets", DATA_ACQUISITION_MODE)
-	elseif(WaveExists(ttlRackOneChannel) && ttlRackOneChannel[index] == channel)
-		WAVE/T ttlStimsets = GetLastSetting(textualValues, sweep, "TTL rack one stim sets", DATA_ACQUISITION_MODE)
-	else
-		return ""
+		return ListToTextWave(ttlStimsets[index], ";")
+	elseif(WaveExists(ttlStimsetsRackZero) || WaveExists(ttlStimsetsRackOne))
+		// ITC hardware
+		Make/FREE/T/N=(NUM_DA_TTL_CHANNELS) entries
+		if(WaveExists(ttlStimsetsRackZero))
+			entries += StringFromList(p, ttlStimsetsRackZero[index])
+		endif
+
+		if(WaveExists(ttlStimsetsRackOne))
+			entries += StringFromList(p, ttlStimsetsRackOne[index])
+		endif
+
+		return entries
 	endif
 
-	return ttlStimSets[index]
+	// no TTL entries
+	return $""
 End
 
 /// @brief Return a sorted list of all DA/TTL stim set waves

Packages/Testing-MIES/UTF_BasicHardwareTests.ipf
@@ -2160,6 +2160,9 @@ Function UnassociatedChannels_REENTRY([str])
 
 			WAVE TTLs = GetTTLListFromConfig(config)
 
+			WAVE/Z ttlStimSets = GetTTLstimSets(numericalValues, textualValues, j)
+			CHECK_EQUAL_TEXTWAVES(ttlStimSets, {"", "StimulusSetA_TTL_0", "", "StimulusSetB_TTL_0", "", "", "", ""})
+
 			switch(GetHardwareType(device))
 				case HARDWARE_ITC_DAC:
 					// check TTL LBN keys

Packages/Testing-MIES/UTF_TestNWBExportV1.ipf
@@ -284,7 +284,7 @@ Function TestTimeSeries(fileID, device, groupID, channel, sweep, pxpSweepsDFR)
 	variable channelGroupID, num_samples, starting_time, session_start_time, actual, scale, scale_ref
 	variable clampMode, gain, gain_ref, resolution, conversion
 	string stimulus, stimulus_expected, neurodata_type_ref, neurodata_type, channelName
-	string electrode_name, electrode_name_ref, key, unit_ref, unit, base_unit_ref, TTLStimsets
+	string electrode_name, electrode_name_ref, key, unit_ref, unit, base_unit_ref
 
 	STRUCT IPNWB#ReadChannelParams params
 	IPNWB#InitReadChannelParams(params)
@@ -316,12 +316,13 @@ Function TestTimeSeries(fileID, device, groupID, channel, sweep, pxpSweepsDFR)
 	elseif(params.channelType == ITC_XOP_CHANNEL_TYPE_ADC && IsNaN(params.electrodeNumber)) // unassoc AD
 		stimulus_expected = "PLACEHOLDER"
 	elseif(params.channelType == ITC_XOP_CHANNEL_TYPE_TTL)
-		TTLStimsets = GetTTLStimSets(numericalValues, textualValues, sweep, params.channelNumber)
+		WAVE/T/Z TTLStimsets = GetTTLStimSets(numericalValues, textualValues, sweep)
+		CHECK_WAVE(TTLStimsets, TEXT_WAVE)
 
 		if(IsNaN(params.ttlBit))
-			stimulus_expected = StringFromList(params.channelNumber, TTLStimsets)
+			stimulus_expected = TTLStimsets[params.channelNumber]
 		else
-			stimulus_expected = StringFromList(log(params.ttlBit)/log(2), TTLStimsets)
+			stimulus_expected = TTLStimsets[log(params.ttlBit)/log(2)]
 		endif
 	else
 		WAVE/Z/T wvText = GetLastSetting(textualValues, sweep, "Stim Wave Name", DATA_ACQUISITION_MODE)

commit 74e93d80cc97f6fdb9a9f49dba4e5d9315ca75d7

NWB_GetStimsetFromSweepGeneric: Don't fail without TTL stimsets
Bug introduced in d25b9d57 (Refactor GetTTLStimSets to be hardware
independent, 2019-06-28).

Packages/MIES/MIES_NeuroDataWithoutBorders.ipf
@@ -633,6 +633,11 @@ static Function/S NWB_GetStimsetFromSweepGeneric(sweep, numericalValues, textual
 
 	// handle TTL channels
 	for(i = 0; i < NUM_DA_TTL_CHANNELS; i += 1)
+
+		if(!WaveExists(ttlStimsets))
+			break
+		endif
+
 		name = ttlStimSets[i]
 		if(isEmpty(name))
 			continue