AllenInstitute/MIES

commit f256dc50654c04cd3861988f9c269a9143608f11

Move background tasks initialization into common routine
And also setup always all of them so that they are treated as running
for the new background watchdog mode in the UTF.

Original-CommitID: 4d5a5b6d25700f98062f47ad030341f8d4294ad6

Packages/MIES/MIES_MiesUtilities.ipf
@@ -4967,3 +4967,14 @@ Function/S GetMIESVersionAsString()
 	SVAR miesVersion = $GetMiesVersion()
 	return NormalizeToEOL(miesVersion, "\n")
 End
+
+// @brief Common setup routine for all MIES background tasks for DAQ, TP and pressure control
+Function SetupBackgroundTasks()
+	CtrlNamedBackground $TASKNAME_TIMERMD, period = 6, proc=DQM_Timer
+	CtrlNamedBackground $TASKNAME_FIFOMONMD, period=1, proc=DQM_FIFOMonitor
+	CtrlNamedBackground $TASKNAME_FIFOMON, period = 5, proc=DQS_FIFOMonitor
+	CtrlNamedBackground $TASKNAME_TIMER, period = 5, proc=DQS_Timer, start
+	CtrlNamedBackground $TASKNAME_TPMD, period=5, proc=TPM_BkrdTPFuncMD
+	CtrlNamedBackground $TASKNAME_TP, period = 5, proc=TPS_TestPulseFunc
+	CtrlNamedBackground P_ITC_FIFOMonitor, period = 10, proc=P_ITC_FIFOMonitorProc
+End

Packages/MIES/MIES_DAEphys.ipf
@@ -4509,7 +4509,7 @@ Function DAP_LockDevice(panelTitle)
 	if(ItemsInList(GetListOfLockedDevices()) == 1)
 		DAP_LoadBuiltinStimsets()
 		GetPxPVersion()
-
+		SetupBackgroundTasks()
 #if IgorVersion() >= 8.0
 #if NumberByKey("BUILD", IgorInfo(0)) >= 33453
 		CtrlNamedBackground _all_, noevents=1

Packages/MIES/MIES_DataAcquisition_Multi.ipf
@@ -267,7 +267,7 @@ Function DQM_StartBackgroundTimer(panelTitle, runTime, funcList)
 
 	DQM_MakeOrUpdateTimerParamWave(panelTitle, funcList, startTime, durationTime, endTime, 1)
 	if(!IsBackgroundTaskRunning(TASKNAME_TIMERMD))
-		CtrlNamedBackground $TASKNAME_TIMERMD, period = 6, proc = DQM_Timer, start
+		CtrlNamedBackground $TASKNAME_TIMERMD, start
 	endif
 End
 
@@ -323,7 +323,6 @@ Function DQM_Timer(s)
 End
 
 static Function DQM_StartBckrdFIFOMonitor()
-	CtrlNamedBackground $TASKNAME_FIFOMONMD, period=1, proc=DQM_FIFOMonitor
 	CtrlNamedBackground $TASKNAME_FIFOMONMD, start
 End
 

Packages/MIES/MIES_DataAcquisition_Single.ipf
@@ -118,7 +118,6 @@ static Function DQS_StopDataAcq(panelTitle, [forcedStop])
 End
 
 Function DQS_StartBackgroundFifoMonitor()
-	CtrlNamedBackground $TASKNAME_FIFOMON, period = 5, proc = DQS_FIFOMonitor
 	CtrlNamedBackground $TASKNAME_FIFOMON, start
 End
 
@@ -187,7 +186,7 @@ Function DQS_StartBackgroundTimer(panelTitle, runTime, funcList)
 	repeatedAcqStart    = RelativeNowHighPrec()
 	repeatedAcqDuration = runTime
 
-	CtrlNamedBackground $TASKNAME_TIMER, period = 5, proc = DQS_Timer, start
+	CtrlNamedBackground $TASKNAME_TIMER, start
 End
 
 /// @brief Stop the background timer used for ITI tracking

Packages/MIES/MIES_PressureControl.ipf
@@ -1053,7 +1053,6 @@ static Function P_DataAcq(panelTitle, headStage)
 		HW_ITC_PrepareAcq(deviceID, dataFunc=P_GetITCData, configFunc=P_GetITCChanConfig)
 		HW_StartAcq(hwType, deviceID, flags=HARDWARE_ABORT_ON_ERROR)
 
-		CtrlNamedBackground P_ITC_FIFOMonitor, period = 10, proc = P_ITC_FIFOMonitorProc
 		CtrlNamedBackground P_ITC_FIFOMonitor, start
 	elseif(hwType == HARDWARE_NI_DAC)
 

Packages/MIES/MIES_TestPulse_Multi.ipf
@@ -138,7 +138,6 @@ static Function TPM_BkrdTPMD(panelTitle, [triggerMode])
 	endswitch
 	if(!IsBackgroundTaskRunning(TASKNAME_TPMD))
 		ASYNC_Start(ThreadProcessorCount, disableTask=1)
-		CtrlNamedBackground $TASKNAME_TPMD, period=5, proc=TPM_BkrdTPFuncMD
 		CtrlNamedBackground $TASKNAME_TPMD, start
 	endif
 End

Packages/MIES/MIES_TestPulse_Single.ipf
@@ -13,7 +13,6 @@ Function TPS_StartBackgroundTestPulse(panelTitle)
 	string panelTitle
 
 	ASYNC_Start(ThreadProcessorCount, disableTask=1)
-	CtrlNamedBackground $TASKNAME_TP, period = 5, proc = TPS_TestPulseFunc
 	CtrlNamedBackground $TASKNAME_TP, start
 End
 

commit 65aab3191fe71d43e1bbfb01f35c57e74e9d3f4f

SetupBackgroundTasks: Don't start the timer task for single device DAQ
Bug introduced in f256dc50 (Move background tasks initialization into
common routine, 2019-04-10).

Packages/MIES/MIES_MiesUtilities.ipf
@@ -5404,7 +5404,7 @@ Function SetupBackgroundTasks()
 	CtrlNamedBackground $TASKNAME_TIMERMD, period = 6, proc=DQM_Timer
 	CtrlNamedBackground $TASKNAME_FIFOMONMD, period=1, proc=DQM_FIFOMonitor
 	CtrlNamedBackground $TASKNAME_FIFOMON, period = 5, proc=DQS_FIFOMonitor
-	CtrlNamedBackground $TASKNAME_TIMER, period = 5, proc=DQS_Timer, start
+	CtrlNamedBackground $TASKNAME_TIMER, period = 5, proc=DQS_Timer
 	CtrlNamedBackground $TASKNAME_TPMD, period=5, proc=TPM_BkrdTPFuncMD
 	CtrlNamedBackground $TASKNAME_TP, period = 5, proc=TPS_TestPulseFunc
 	CtrlNamedBackground P_ITC_FIFOMonitor, period = 10, proc=P_ITC_FIFOMonitorProc