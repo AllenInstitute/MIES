AllenInstitute/MIES

commit 41d2c83775c561a4a8c0a618e6290e0e47b0ffa4

TestPulseMarker is now saved in TPStorage as well as in the stored TPWaves
The stored TP waves use the key "marker" to store the attributed unique number

The TP waves also save the headstages as comma separated list with the key
"headstages" in the order of columns.

Packages/MIES/MIES_Oscilloscope.ipf
@@ -467,6 +467,7 @@ Function SCOPE_UpdateOscilloscopeData(panelTitle, dataAcqOrTP, [chunk, fifoPos, 
 	variable tpChannels, numADCs, numDACs, tpLengthPoints, tpStart, tpEnd, tpStartPos
 	variable TPChanIndex, saveTP, sampleInt
 	variable headstage, fifoLatest
+	string hsList
 
 	variable hardwareType = GetHardwareType(panelTitle)
 	switch(hardwareType)
@@ -545,6 +546,7 @@ Function SCOPE_UpdateOscilloscopeData(panelTitle, dataAcqOrTP, [chunk, fifoPos, 
 					Duplicate/FREE/R=[tpStartPos, tpStartPos + tpLengthPoints - 1][numDACs, numDACs + tpChannels - 1] OscilloscopeData, StoreTPWave
 					SetScale/P x, 0, sampleInt, osciUnits, StoreTPWave
 					TPChanIndex = 0
+					hsList = ""
 				endif
 
 				for(j = 0; j < numADCs; j += 1)
@@ -563,16 +565,18 @@ Function SCOPE_UpdateOscilloscopeData(panelTitle, dataAcqOrTP, [chunk, fifoPos, 
 						tpInput.hsIndex = headstage
 						TP_SendToAnalysis(tpInput)
 
-						if(saveTP && TPChanIndex != j)
-							MultiThread StoreTPWave[][TPChanIndex] = channelData[p]
+						if(saveTP)
+							hsList = AddListItem(num2str(headstage), hsList, ",", Inf)
+							if(TPChanIndex != j)
+								MultiThread StoreTPWave[][TPChanIndex] = channelData[p]
+							endif
 						endif
-
 
 					endif
 				endfor
 
 				if(saveTP)
-					TP_StoreTP(panelTitle, StoreTPWave)
+					TP_StoreTP(panelTitle, StoreTPWave, tpInput.measurementMarker, hsList)
 					WaveClear StoreTPWave
 				endif
 

Packages/MIES/MIES_TestPulse.ipf
@@ -98,9 +98,15 @@ End
 /// @param panelTitle panel title
 ///
 /// @param TPWave reference to wave holding the TP data in the same format as OscilloscopeData
-Function TP_StoreTP(panelTitle, TPWave)
+///
+/// @param tpMarker unique number for this set of TPs from all TP channels
+///
+/// @param hsList list of headstage numbers in the same order as the columns of TPWave
+Function TP_StoreTP(panelTitle, TPWave, tpMarker, hsList)
 	string panelTitle
 	WAVE TPWave
+	variable tpMarker
+	string hsList
 
 	variable index
 
@@ -108,6 +114,8 @@ Function TP_StoreTP(panelTitle, TPWave)
 	index = GetNumberFromWaveNote(storedTP, NOTE_INDEX)
 	EnsureLargeEnoughWave(storedTP, minimumSize=index)
 	Note/K TPWave, "TimeStamp: " + GetISO8601TimeStamp(numFracSecondsDigits = 3)
+	SetNumberInWaveNote(TPWave, "TPMarker", tpMarker, format="%d")
+	SetStringInWaveNote(TPWave, "Headstages", hsList)
 	storedTP[index++] = TPWave
 
 	SetNumberInWaveNote(storedTP, NOTE_INDEX, index)
@@ -231,7 +239,7 @@ Function TP_ROAnalysis(dfr, err, errmsg)
 			TP_CalculateAverage(TPSSBuffer, SSResistance)
 		endif
 
-		TP_RecordTP(panelTitle, BaselineSSAvg, InstResistance, SSResistance, now)
+		TP_RecordTP(panelTitle, BaselineSSAvg, InstResistance, SSResistance, now, marker)
 		DQ_ApplyAutoBias(panelTitle, BaselineSSAvg, SSResistance)
 	endif
 
@@ -362,10 +370,10 @@ End
 /// When the TP is initiated by any method, the TP storageWave should be empty
 /// If 200 ms have elapsed, or it is the first TP sweep,
 /// data from the input waves is transferred to the storage waves.
-static Function TP_RecordTP(panelTitle, BaselineSSAvg, InstResistance, SSResistance, now)
+static Function TP_RecordTP(panelTitle, BaselineSSAvg, InstResistance, SSResistance, now, tpMarker)
 	string 	panelTitle
 	wave 	BaselineSSAvg, InstResistance, SSResistance
-	variable now
+	variable now, tpMarker
 
 	variable delta, i, ret, lastPressureCtrl
 	WAVE TPStorage = GetTPStorage(panelTitle)
@@ -429,6 +437,7 @@ static Function TP_RecordTP(panelTitle, BaselineSSAvg, InstResistance, SSResista
 	TPStorage[count][][%Baseline_IC] = hsProp[q][%ClampMode] == I_CLAMP_MODE ? baselineSSAvg[q] : NaN
 
 	TPStorage[count][][%DeltaTimeInSeconds] = count > 0 ? now - TPStorage[0][0][%TimeInSeconds] : 0
+	TPStorage[count][][%TPMarker] = tpMarker
 
 	lastPressureCtrl = GetNumberFromWaveNote(TPStorage, PRESSURE_CTRL_LAST_INVOC)
 	if((now - lastPressureCtrl) > TP_PRESSURE_INTERVAL)

commit 15730af4e1260e57157f1a81ba36319bd85162cf

fix that with TP during DAQ all test pulse channels are stored
Previously there was a bug that stored the last test pulse channel
(from the last headstage in the wave note list) in column 0.
The remaining columns had data chunks from the original ADC channels,
that were not necessarily TP channels.
Bug introduced in 41d2c83775c561a4a8c0a618e6290e0e47b0ffa4

Fix: The column index for StoreTPWave is now increased after one channel
was saved to it.

Packages/MIES/MIES_Oscilloscope.ipf
@@ -592,6 +592,7 @@ Function SCOPE_UpdateOscilloscopeData(panelTitle, dataAcqOrTP, [chunk, fifoPos, 
 							if(TPChanIndex != j)
 								MultiThread StoreTPWave[][TPChanIndex] = channelData[p]
 							endif
+							TPChanIndex += 1
 						endif
 
 					endif