2f71bfafe0249cd586ae229505ff29d92e2701cf
Matthias Kastner <matthias.kastner@byte-physics.de>
2019-09-11 23:48:09 +0200

    Restructure Building Process
    Use a log function during building process to differentiate between
    verbose and non-verbose modes.
    
    Let building process output `--version` information in debug mode.
    Standard to debug mode.
    
    all output to stdout.
    
    Update docker
    * use python3
    * use current debian:stable which is buster.
    * use pip3
    * install breathe, sphinx via pip
    * add latex support

--- tools/build-documentation.sh
+++ tools/build-documentation.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# vim: set ts=2 sw=2 tw=0 et :
 
 top_level=$(git rev-parse --show-toplevel)
 branch=$(git rev-parse --abbrev-ref HEAD)
@@ -9,9 +10,28 @@ function Passed {
   exit 0
 }
 
-function Failed {
-  cp "$top_level/tools/JU_Failed.xml" "$top_level/tools/unit-testing/"
-  exit 1
+# logging function inspired by https://stackoverflow.com/a/33597663/7809404
+# set verbose level to warning
+declare -A LOG_LEVELS LOG_COLORS
+LOG_LEVELS=([0]="emerg" [1]="alert" [2]="crit " [3]="err" [4]="warning" [5]="notice" [6]="info" [7]="debug")
+LOG_COLORS=([0]="91m"   [1]="91m"   [2]="91m"  [3]="91m" [4]="93m"     [5]="96m"    [6]="92m"  [7]="90m"  )
+VERBOSE=7
+
+function .log () {
+  local LEVEL=${1}
+  shift
+  if [ ${VERBOSE} -ge ${LEVEL} ]; then
+    while read line; do
+      python3 -c "print('\033[${LOG_COLORS[$LEVEL]}[${LOG_LEVELS[$LEVEL]}]\033[0m ' + '$line')"
+    done <<< "$@"
+  fi
+
+  if [ 4 -ge ${LEVEL} ]; then
+    .log 6 "Error building the documentation"
+    .log 6 "================================"
+    cp "$top_level/tools/JU_Failed.xml" "$top_level/tools/unit-testing/"
+    exit 1
+  fi
 }
 
 case $(uname) in
@@ -23,47 +43,39 @@ case $(uname) in
       ;;
 esac
 
-echo "Start building the documentation"
+echo
+.log 6 "Building The Documentation"
+.log 6 "=========================="
 
 cd "$top_level/Packages/doc"
 
-output=$( (cat Doxyfile ; echo "HAVE_DOT = NO" ; echo "GENERATE_HTML = NO") | doxygen - 2>&1  >/dev/null | grep -v "warning: ignoring unsupported tag" )
+echo
+.log 6 "Analyzing Doxygen Comments"
+.log 6 "--------------------------"
 
-if [ ! -z  "$output" ]
+if ! command -v doxygen > /dev/null; then .log 2 "[doxygen] doxygen not found."; fi
+.log 7 "[doxygen] $(doxygen --version)"
+if ! (cat Doxyfile; echo "HAVE_DOT = NO"; echo "GENERATE_HTML = NO") | doxygen -
 then
-  echo "Errors building the documentation" 1>&2
-  echo "Doxygen says: "                    1>&2
-  echo "$output"                           1>&2
-  Failed
+  .log 3 "[doxygen] failed."
 fi
 
-if hash dot 2>/dev/null; then
-  echo "Start converting dot files to svg"
+.log 6 "[dot] Converting dot Files To svg"
+if ! command -v dot > /dev/null; then .log 2 "[dot] dot/graphviz not found."; fi
+.log 7 "[dot] $(dot -V)"
 
-  for i in $(ls *.dot)
-  do
-    dot -Tsvg -O "$i"
-  done
-
-else
-  echo "Errors building the documentation" 1>&2
-  echo "dot/graphviz could not be found"   1>&2
-  Failed
-fi
+for i in $(ls *.dot)
+do
+	dot -Tsvg -O "$i"
+done
 
 cp "$top_level/Packages/IPNWB/Readme.rst" "$top_level/Packages/doc/IPNWB.rst"
 cp "$top_level/Packages/ZeroMQ/Readme.rst" "$top_level/Packages/doc/ZeroMQ-XOP-Readme.rst"
 
-if hash breathe-apidoc 2>/dev/null; then
-  echo "Start breathe-apidoc"
-
-  breathe-apidoc -f -o . xml
-
-else
-  echo "Errors building the documentation" 1>&2
-  echo "breathe-apidoc could not be found" 1>&2
-  Failed
-fi
+.log 6 "[breathe] Converting doxygen xml To rst"
+if ! command -v breathe-apidoc > /dev/null; then .log 2 "[breathe] breathe-apidoc not found."; fi
+.log 7 "[breathe] $(breathe-apidoc --version)"
+breathe-apidoc -f -o . xml
 
 # Add labels to each group and each file
 # can be referenced via :ref:`Group LabnotebookQueryFunctions`
@@ -81,30 +93,23 @@ do
   sed -i "1s/^/.. _File ${name}:\n\n/" $i
 done
 
-if hash sphinx-build 2>/dev/null; then
-  echo "Start sphinx-build"
+echo
+.log 6 "Build Sphinx Documentation"
+.log 6 "--------------------------"
 
-  rm -f sphinx-output.log
-
-  sphinx-build -q -w sphinx-output.log . html
-
-  sed -i -e '/WARNING: Duplicate declaration./d' sphinx-output.log
-
-  if [ -s "sphinx-output.log" ]
-  then
-    echo "Errors building the documentation" 1>&2
-    echo "sphinx-build says: "               1>&2
-    cat sphinx-output.log                    1>&2
-    Failed
-  fi
-
-else
-  echo "Errors building the documentation" 1>&2
-  echo "sphinx-build could not be found"   1>&2
-  Failed
+if ! command -v sphinx-build > /dev/null; then .log 2 "[sphinx] sphinx-build not found."; fi
+.log 7 "[sphinx] $(sphinx-build --version)"
+rm -f sphinx-output.log
+sphinx-build -w sphinx-output.log . html
+if [ -s "sphinx-output.log" ]
+then
+  .log 5 "[sphinx] $(python3 --version)"
+  .log 5 "[sphinx] $(pip3 list sphinx)"
 fi
 
-echo "Start zipping the results"
+.log 6 "[sphinx] zipping the results"
+${ZIP_EXE} --version
+
 rm -f mies-docu*.zip
 "$ZIP_EXE" -qr0 mies-docu-$version.zip html
 
