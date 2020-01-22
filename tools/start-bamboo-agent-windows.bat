7188e02adaf78c6ea762141d2e63ba2f593d0d81
Thomas Braun <thomas.braun@byte-physics.de>
2020-01-23 00:11:51 +0100

    Tools: Reorganize bamboo agent starting scripts
    The scripts for windows were missing and the linux ones need to be
    renamed.

---
+++ tools/start-bamboo-agent-windows.bat
@@ -0,0 +1,3 @@
+rem Comment in the matching one depending on if you installed git as user or admin
+rem "C:\Users\svc_multipatch\AppData\Local\Programs\Git\bin\bash.exe"  -c ~/start-bamboo-agent.sh
+"C:\Program Files\Git\bin\bash.exe" -c ~/start-bamboo-agent.sh
---
+++ tools/start-bamboo-agent-windows.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+~/bamboo-agent-home/bin/wrapper.exe -c ~/bamboo-agent-home/conf/wrapper.conf
