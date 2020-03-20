AllenInstitute/MIES

commit 4c36e6f8fc62c2a9af019cf8a2babd7306061c56

SweepFormula: Update sweeps() function
The sweeps() function behaved strange if OVS was off. Switching to
PA_GetTraceInfos is better and works even if OVS_IsActive is not
enabled.

Packages/MIES/MIES_SweepFormula.ipf
@@ -683,6 +683,7 @@ Function/WAVE SF_FormulaExecutor(jsonID, [jsonPath, graph])
 			///           displayed (default): get (selected) sweeps
 			///           al:                  get all possible sweeps
 			ASSERT(JSON_GetArraySize(jsonID, jsonPath) <= 1, "Function requires 1 argument at most.")
+			ASSERT(!ParamIsDefault(graph) && !IsEmpty(graph), "Graph not specified.")
 
 			JSONtype = JSON_GetType(jsonID, jsonPath + "/0")
 			if(JSONtype == JSON_NULL)
@@ -691,20 +692,17 @@ Function/WAVE SF_FormulaExecutor(jsonID, [jsonPath, graph])
 
 			strswitch(wvT[0])
 				case "all":
-					mode = OVS_SWEEP_ALL_SWEEPNO
+					WAVE out = OVS_GetSelectedSweeps(graph, OVS_SWEEP_ALL_SWEEPNO)
 					break
 				case "displayed":
-					mode = OVS_SWEEP_SELECTION_SWEEPNO
+					WAVE/T/Z traces = PA_GetTraceInfos(graph)
+					Make/N=(DimSize(traces, ROWS))/FREE traceSweeps = str2num(traces[p][%sweepNumber])
+					WAVE out = GetUniqueEntries(traceSweeps)
 					break
 				default:
 					ASSERT(0, "Undefined argument")
 			endswitch
 
-			ASSERT(!ParamIsDefault(graph) && !IsEmpty(graph), "Graph not specified.")
-			WAVE/Z out = OVS_GetSelectedSweeps(graph, mode)
-			if(!WaveExists(out) && mode == OVS_SWEEP_SELECTION_SWEEPNO)
-				WAVE/Z out = OVS_GetSelectedSweeps(graph, OVS_SWEEP_ALL_SWEEPNO)
-			endif
 			if(!WaveExists(out))
 				Make/N=1/FREE out = {NaN} // simulates [null]
 			endif

commit 4c27a6b2edd2d1e3d8b518564415f6be1512d405

SF_FormulaExecutor: Handle no sweeps properly
PA_GetTraceInfos can return an invalid wave reference, so we need to
handle that properly.

Bug introduced in 4c36e6f8 (SweepFormula: Update sweeps() function,
2019-11-20).

Packages/MIES/MIES_SweepFormula.ipf
@@ -725,8 +725,10 @@ Function/WAVE SF_FormulaExecutor(jsonID, [jsonPath, graph])
 					break
 				case "displayed":
 					WAVE/T/Z traces = PA_GetTraceInfos(graph)
-					Make/N=(DimSize(traces, ROWS))/FREE traceSweeps = str2num(traces[p][%sweepNumber])
-					WAVE out = GetUniqueEntries(traceSweeps)
+					if(WaveExists(traces))
+						Make/N=(DimSize(traces, ROWS))/FREE traceSweeps = str2num(traces[p][%sweepNumber])
+						WAVE out = GetUniqueEntries(traceSweeps)
+					endif
 					break
 				default:
 					ASSERT(0, "Undefined argument")