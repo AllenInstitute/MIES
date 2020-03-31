AllenInstitute/MIES

commit 8f7c44379542d06750d026d31c462cad01a5b5a5

Add menu option to open the diagnostics directory

Packages/MIES/MIES_Utilities.ipf
@@ -4983,3 +4983,25 @@ static Function JsonSetEmptyArray(jsonID, path)
 	JSON_SetWave(jsonID, path, w)
 	JSON_Remove(jsonID, path + "/0")
 End
+
+/// @brief Return the name of a symbolic path which points to the crash dump
+/// directory on windows
+Function/S GetSymbolicPathForDiagnosticsDirectory()
+
+	string userName, path, symbPath
+
+	userName = GetSystemUserName()
+
+	sprintf path, "C:Users:%s:AppData:Roaming:WaveMetrics:Igor Pro %s:Diagnostics:", userName, GetIgorProVersion()[0]
+	symbPath = "crashInfo"
+
+	NewPath/O $symbPath, path
+
+	return symbPath
+End
+
+Function ShowDiagnosticsDirectory()
+
+	string symbPath = GetSymbolicPathForDiagnosticsDirectory()
+	PathInfo/SHOW $symbPath
+End

Packages/MIES/MIES_Menu.ipf
@@ -52,6 +52,7 @@ Menu "Mies Panels"
 		"Check GUI control procedures of top panel", /Q, SearchForInvalidControlProcs(GetCurrentWindow())
 		"Flush Cache"                              , /Q, CA_FlushCache()
 		"Output Cache statistics"                  , /Q, CA_OutputCacheStatistics()
+		"Show Diagnostics (crash dumps) directory" , /Q, ShowDiagnosticsDirectory()
 	End
 End
 

commit 8ea760ab36bbb2092719a8bb3252a1c4c6f8a8f1

GetSymbolicPathForDiagnosticsDirectory: Handle non existing diagnostics folder
We need to create it, if it does not exist.

Bug introduced in 8f7c4437 (Add menu option to open the diagnostics
directory, 2020-02-11).

Packages/MIES/MIES_Utilities.ipf
@@ -4832,6 +4832,11 @@ Function/S GetSymbolicPathForDiagnosticsDirectory()
 	userName = GetSystemUserName()
 
 	sprintf path, "C:Users:%s:AppData:Roaming:WaveMetrics:Igor Pro %s:Diagnostics:", userName, GetIgorProVersion()[0]
+
+	if(!FolderExists(path))
+		CreateFolderOnDisk(path)
+	endif
+
 	symbPath = "crashInfo"
 
 	NewPath/O/Q $symbPath, path