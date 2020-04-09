AllenInstitute/MIES

commit d13a6b726575194535bdadb0c602567ef5b1c06a

GetListOfObjects: Rewrite and speed it up a lot
The current implementation of GetListOfObjects suffers from N^2
complexity as it uses GetIndexedObjNameDFR.

This can be worked around by using VariableList/StringList/WaveList and DataFolderDir.

We also drop expensive flags like matchList and waveProperty. matchList
can be replaced with ConvertListToRegexpWithAlternations, waveProperty
does not have a replacement.

One drawback of the new approach is that GetAllObjects now fails if the
datafolder has a comma `,` in it. But this limitation is documented and
not a problem for us.

Packages/MIES/MIES_Utilities.ipf
@@ -207,26 +207,22 @@ End
 /// @brief Alternative implementation for WaveList/VariableList/etc. which honours a dfref and thus
 /// does not require SetDataFolder calls.
 ///
-/// @param dfr                                  datafolder reference to search for the waves
-/// @param matchExpr                            expression matching the waves, either a regular (exprType == MATCH_REGEXP)
+/// @param dfr                                  datafolder reference to search for the objects
+/// @param matchExpr                            expression matching the objects, either a regular (exprType == MATCH_REGEXP)
 ///                                             or wildcard (exprType == MATCH_WILDCARD) expression
 /// @param typeFlag [optional, default: COUNTOBJECTS_WAVES] One of @ref TypeFlags
-/// @param matchList [optional, empty]          additional semicolon delimited list of wave names, allows to further
-///                                             qualify the returned wave names.
-/// @param waveProperty [optional, empty]       additional properties of matching waves, inspired by WaveList,
-///                                             currently implemented are `MINCOLS` and `TEXT`
-/// @param fullPath [optional, default: false]  should only the wavename or the absolute path of the wave be returned.
+/// @param fullPath [optional, default: false]  should only the object name or the absolute path of the object be returned
 /// @param recursive [optional, default: false] descent into all subfolders recursively
 /// @param exprType [optional, defaults: MATCH_REGEXP] convention used for matchExpr, one of @ref MatchExpressions
 ///
-/// @returns list of wave names matching regExpStr located in dfr
-Function/S GetListOfObjects(dfr, matchExpr, [typeFlag, matchList, waveProperty, fullPath, recursive, exprType])
+/// @returns list of object names matching matchExpr
+Function/S GetListOfObjects(dfr, matchExpr, [typeFlag, fullPath, recursive, exprType])
 	dfref dfr
-	string matchExpr, matchList, waveProperty
+	string matchExpr
 	variable fullPath, recursive, typeFlag, exprType
 
-	variable i, j, numWaveProperties, numWaves, matches, val, numFolders
-	string name, str, prop, subList, basePath
+	variable i, numFolders
+	string name, folders, basePath, subList
 	string list = ""
 
 	ASSERT(DataFolderExistsDFR(dfr),"Non-existing datafolder")
@@ -248,14 +244,6 @@ Function/S GetListOfObjects(dfr, matchExpr, [typeFlag, matchList, waveProperty, 
 		typeFlag = COUNTOBJECTS_WAVES
 	endif
 
-	if(ParamIsDefault(waveProperty))
-		waveProperty = ""
-	endif
-
-	if(ParamIsDefault(matchList))
-		matchList = ""
-	endif
-
 	if(ParamIsDefault(exprType))
 		exprType = MATCH_REGEXP
 	else
@@ -264,84 +252,77 @@ Function/S GetListOfObjects(dfr, matchExpr, [typeFlag, matchList, waveProperty, 
 
 	basePath = GetDataFolder(1, dfr)
 
+	list = ListMatchesExpr(GetAllObjects(dfr, typeFlag), matchExpr, exprType)
+
+	if(fullPath)
+		list = AddPrefixToEachListItem(basePath, list)
+	endif
+
 	if(recursive)
-		numFolders = CountObjectsDFR(dfr, COUNTOBJECTS_DATAFOLDER)
+		folders = GetAllObjects(dfr, COUNTOBJECTS_DATAFOLDER)
+		numFolders = ItemsInList(folders)
 		for(i = 0; i < numFolders; i+=1)
-			name = basePath + GetIndexedObjNameDFR(dfr, COUNTOBJECTS_DATAFOLDER, i)
+			name = basePath + StringFromList(i, folders)
 			DFREF subFolder = $name
-			subList = GetListOfObjects(subFolder, matchExpr, matchList=matchList, waveProperty=waveProperty, \
-						               fullPath=fullPath, recursive=recursive, exprType=exprType)
+			subList = GetListOfObjects(subFolder, matchExpr, typeFlag = typeFlag, fullPath=fullPath, recursive=recursive, exprType=exprType)
 			if(!IsEmpty(subList))
 				list = AddListItem(RemoveEnding(subList, ";"), list)
 			endif
 		endfor
 	endif
 
-	numWaves = CountObjectsDFR(dfr, typeFlag)
-	for(i=0; i<numWaves; i+=1)
-		name = GetIndexedObjNameDFR(dfr, typeFlag, i)
+	return list
+End
 
-		if(!StringMatchesExpr(name, matchExpr, exprType))
-			continue
-		endif
+/// @brief Return a list of all objects of the given type from dfr
+///
+/// Does not work for datafolders which have a comma (`,`) in them.
+static Function/S GetAllObjects(dfr, typeFlag)
+	DFREF dfr
+	variable typeFlag
 
-		if(!IsEmpty(matchList) && WhichListItem(name, matchList, ";", 0, 0) == -1)
-			continue
-		endif
+	string list
 
-		matches = 1
-		if(!isEmpty(waveProperty))
-			ASSERT(typeFlag == COUNTOBJECTS_WAVES, "waveProperty does not make sense for type flags other than COUNTOBJECTS_WAVES")
-			WAVE/SDFR=dfr wv = $name
-			numWaveProperties = ItemsInList(waveProperty)
-			for(j = 0; j < numWaveProperties; j += 1)
-				str  = StringFromList(j, waveProperty)
-				prop = StringFromList(0, str, ":")
-				val  = str2num(StringFromList(1, str, ":"))
-				ASSERT(IsFinite(val), "non finite value")
-				ASSERT(!IsEmpty(prop), "empty option")
+	DFREF oldDFR = GetDataFolderDFR()
 
-				strswitch(prop)
-					case "MINCOLS":
-						matches = matches & DimSize(wv, COLS) >= val
-						break
-					case "TEXT":
-						matches = matches & IsTextWave(wv) == !!val
-						break
-					default:
-						ASSERT(0, "property not implemented")
-						break
-				endswitch
+	SetDataFolder dfr
 
-				if(!matches) // no need to check the other properties
-					break
-				endif
-			endfor
-		endif
+	switch(typeFlag)
+		case COUNTOBJECTS_WAVES:
+			list = WaveList("*", ";", "")
+			break
+		case COUNTOBJECTS_VAR:
+			list = VariableList("*", ";", 11)
+			break
+		case COUNTOBJECTS_STR:
+			list = StringList("*", ";")
+			break
+		case COUNTOBJECTS_DATAFOLDER:
+			list = DataFolderDir(2^0)
+			list = StringByKey("FOLDERS", list)
+			list = ReplaceString(",", list, ";")
+			break
+		default:
+			SetDataFolder oldDFR
+			ASSERT(0, "Invalid type flag")
+	endswitch
 
-		if(matches)
-			if(fullPath)
-				list = AddListItem(basePath + name, list, ";", Inf)
-			else
-				list = AddListItem(name, list, ";", Inf)
-			endif
-		endif
-	endfor
+	SetDataFolder oldDFR
 
 	return list
 End
 
-/// @brief Matches `name` against the expression `matchExpr` using the given
+/// @brief Matches `list` against the expression `matchExpr` using the given
 ///        convention in `exprType`
-Function StringMatchesExpr(name, matchExpr, exprType)
-	string name, matchExpr
+Function/S ListMatchesExpr(list, matchExpr, exprType)
+	string list, matchExpr
 	variable exprType
 
 	switch(exprType)
 		case MATCH_REGEXP:
-			return GrepString(name, matchExpr)
+			return GrepList(list, matchExpr)
 		case MATCH_WILDCARD:
-			return StringMatch(name, matchExpr)
+			return ListMatch(list, matchExpr)
 		default:
 			ASSERT(0, "invalid exprType")
 	endswitch
@@ -5029,4 +5010,22 @@ Function/WAVE HexToBinary(str)
 	Make/N=(length / 2)/FREE/B/U bin = HexToNumber(str[p * 2]) | (HexToNumber(str[p * 2 + 1]) << 4)
 
 	return bin
+End
+
+/// @brief Turn a list of entries into a regular expression with alternations.
+///
+/// Can be used for GetListOfObjects() if you know in advance which entries to filter out.
+Function/S ConvertListToRegexpWithAlternations(list)
+	string list
+
+	variable i, numEntries
+	string entry
+	string regexpList = ""
+
+	numEntries = ItemsInList(list)
+	for(i = 0; i < numEntries; i += 1)
+		regexpList = AddListItem("\\Q" + StringFromList(i, list) + "\\E", regexpList, "|", inf)
+	endfor
+
+	return regexpList
 End

Packages/MIES/MIES_AnalysisBrowser.ipf
@@ -490,7 +490,7 @@ static Function AB_LoadDataWrapper(tmpDFR, expFilePath, datafolderPath, listOfNa
 
 	RemoveAllEmptyDataFolders(tmpDFR)
 
-	list = GetListOfObjects(tmpDFR, ".*", matchList=listOfNames, recursive=1, typeFlag=typeFlags)
+	list = GetListOfObjects(tmpDFR, ConvertListToRegexpWithAlternations(listOfNames), recursive=1, typeFlag=typeFlags)
 
 	return ItemsInList(list)
 End

Packages/MIES/MIES_AnalysisFunctionHelpers.ipf
@@ -185,7 +185,7 @@ Function AFH_GetLastSweepAcquired(panelTitle)
 	string list, name
 	variable numItems, i, sweep
 
-	list = GetListOfObjects(GetDeviceDataPath(panelTitle), DATA_SWEEP_REGEXP, waveProperty="MINCOLS:2")
+	list = GetListOfObjects(GetDeviceDataPath(panelTitle), DATA_SWEEP_REGEXP)
 	list = SortList(list, ";", 1 + 16) // descending and case-insensitive alphanumeric
 
 	numItems = ItemsInList(list)

Packages/MIES/MIES_DataBrowser.ipf
@@ -313,7 +313,7 @@ Function/S DB_GetPlainSweepList(win)
 
 	device = BSP_GetDevice(win)
 	dfr = GetDeviceDataPath(device)
-	return GetListOfObjects(dfr, DATA_SWEEP_REGEXP, waveProperty="MINCOLS:2")
+	return GetListOfObjects(dfr, DATA_SWEEP_REGEXP)
 End
 
 static Function [variable first, variable last] DB_FirstAndLastSweepAcquired(string win)

Packages/MIES/MIES_SweepSaving.ipf
@@ -190,10 +190,10 @@ Function SWS_DeleteDataWaves(panelTitle)
 	sweepNo   = DAG_GetNumericalValue(panelTitle, "SetVar_Sweep")
 	path      = GetDeviceDataPathAsString(panelTitle)
 	DFREF dfr = GetDeviceDataPath(panelTitle)
-	list      = GetListOfObjects(dfr, DATA_SWEEP_REGEXP, waveProperty="MINCOLS:2")
-	list     += GetListOfObjects(dfr, DATA_SWEEP_REGEXP_BAK, waveProperty="MINCOLS:2")
-	list     += GetListOfObjects(dfr, DATA_CONFIG_REGEXP, waveProperty="MINCOLS:2")
-	list     += GetListOfObjects(dfr, DATA_CONFIG_REGEXP_BAK, waveProperty="MINCOLS:2")
+	list      = GetListOfObjects(dfr, DATA_SWEEP_REGEXP)
+	list     += GetListOfObjects(dfr, DATA_SWEEP_REGEXP_BAK)
+	list     += GetListOfObjects(dfr, DATA_CONFIG_REGEXP)
+	list     += GetListOfObjects(dfr, DATA_CONFIG_REGEXP_BAK)
 	list     += GetListOfObjects(dfr, ".*", typeFlag=COUNTOBJECTS_DATAFOLDER)
 
 	numItems = ItemsInList(list)

Packages/Testing-MIES/UTF_Utils.ipf
@@ -2132,7 +2132,41 @@ Function GetListOfObjectsWorks2()
 	CHECK_EQUAL_STR(result, expected)
 End
 
-// Not checked: typeFlag, matchList and waveProperty
+Function GetListOfObjectsWorksWithFolder()
+
+	string result, expected
+
+	NewDataFolder/O test1
+	NewDataFolder/O :test1:test2
+	NewDataFolder/O :test1:test2:test3
+
+	DFREF dfr = $":test1"
+	CHECK(DataFolderExistsDFR(dfr))
+
+	DFREF dfrDeep = $":test1:test2"
+	CHECK(DataFolderExistsDFR(dfrDeep))
+
+	result = GetListOfObjects(dfr, ".*", recursive = 0, fullpath = 0, typeFlag = COUNTOBJECTS_DATAFOLDER)
+	expected = "test2"
+	result = SortList(result)
+	expected = SortList(expected)
+	CHECK_EQUAL_STR(result, expected)
+
+	result = GetListOfObjects(dfr, ".*", recursive = 1, fullpath = 0, typeFlag = COUNTOBJECTS_DATAFOLDER)
+	expected = "test2;test3"
+	result = SortList(result)
+	expected = SortList(expected)
+	CHECK_EQUAL_STR(result, expected)
+
+	result = GetListOfObjects(dfr, ".*", recursive = 1, fullpath = 1, typeFlag = COUNTOBJECTS_DATAFOLDER)
+	result = TrimVolatileFolderName_IGNORE(result)
+	expected = ":test1:test2;:test1:test2:test3"
+	result = SortList(result)
+	expected = SortList(expected)
+	CHECK_EQUAL_STR(result, expected)
+End
+
+// Not checked: typeFlag
 /// @}
 
 /// @{

commit 1049c22cc585f1cc35922dfcfe442f5a085a1d04

ConvertListToRegexpWithAlternations: Fix returned regexp
We need to remove the trailing "|" so that the returned regexp does not
match the empty string.

Broken since d13a6b72 (GetListOfObjects: Rewrite and speed it up a lot,
2020-03-26).

And while we are at, we also encapsulate the whole in non-matching
brackets to avoid any problems when the result value is used in another
regular expression.

Packages/MIES/MIES_Utilities.ipf
@@ -5035,6 +5035,8 @@ Function/S ConvertListToRegexpWithAlternations(list)
 		regexpList = AddListItem("\\Q" + StringFromList(i, list) + "\\E", regexpList, "|", inf)
 	endfor
 
+	regexpList = "(?:" + RemoveEnding(regexpList, "|") + ")"
+
 	return regexpList
 End
 