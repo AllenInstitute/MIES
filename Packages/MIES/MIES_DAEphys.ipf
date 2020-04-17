AllenInstitute/MIES

commit ff7dd72af5fa92b1d98b740b542ea65210f62ddb

Analysis parameters: Add method to request the types as well
Real world experience showed that being able to request a specific
type for the analysis parameters helps a lot.

A breaking change is that the names now must be separated with commas
(,) as that is more in line who we store the entries in the stimset.

Original-CommitID: df4494c0a742d25c30b24d8761079ef93808b9f2

Packages/MIES/MIES_DAEphys.ipf
@@ -1806,7 +1806,7 @@ Function DAP_CheckSettings(panelTitle, mode)
 
 	variable numDACs, numADCs, numHS, numEntries, i, indexingEnabled, clampMode
 	variable ampSerial, ampChannelID, minValue, maxValue
-	string ctrl, endWave, ttlWave, dacWave, refDacWave
+	string ctrl, endWave, ttlWave, dacWave, refDacWave, reqParams
 	string list
 
 	ASSERT(mode == DATA_ACQUISITION_MODE || mode == TEST_PULSE_MODE, "Invalid mode")
@@ -2130,9 +2130,10 @@ static Function DAP_CheckHeadStage(panelTitle, headStage, mode)
 
 	string dacWave, endWave, unit, func, info, str, ADUnit, DAUnit, listOfAnalysisFunctions
 	variable DACchannel, ADCchannel, DAheadstage, ADheadstage, DAGain, ADGain, realMode
-	variable gain, scale, clampMode, i, ampConnState, needResetting
-	variable DAGainMCC, ADGainMCC
-	string DAUnitMCC, ADUnitMCC, params, names, requestedNames, diff
+	variable gain, scale, clampMode, i, j, ampConnState, needResetting
+	variable DAGainMCC, ADGainMCC, numEntries
+	string DAUnitMCC, ADUnitMCC, suppParams, suppName, suppType, reqParams, reqNames, reqName
+	string diff, name, type, suppNames, reqType
 
 	if(DAP_DeviceIsUnlocked(panelTitle))
 		printf "(%s) Device is unlocked. Please lock the device.\r", panelTitle
@@ -2369,16 +2370,42 @@ static Function DAP_CheckHeadStage(panelTitle, headStage, mode)
 					return 1
 				elseif(i == GENERIC_EVENT)
 					// check that all required user parameters are supplied
-					requestedNames = AFH_GetListOfReqAnalysisParams(func)
-					if(!IsEmpty(requestedNames))
-						params = ExtractAnalysisFunctionParams(stimSet)
-						names = AFH_GetListOfAnalysisParamNames(params)
-						diff = GetListDifference(requestedNames, names)
+					reqParams = AFH_GetListOfReqAnalysisParams(func)
+					if(!IsEmpty(reqParams))
+						reqNames   = AFH_GetListOfAnalysisParamNames(reqParams)
+						suppParams = ExtractAnalysisFunctionParams(stimSet)
+						suppNames  = AFH_GetListOfAnalysisParamNames(suppParams)
+						diff = GetListDifference(reqNames, suppNames)
 						if(!IsEmpty(diff))
 							printf "(%s) The required analysis parameters requested by %s for stim set %s were not all supplied (missing are: %s)\r", panelTitle, func, dacWave, diff
 							ControlWindowToFront()
 							return 1
 						endif
+
+						numEntries = ItemsInList(reqNames, ",")
+						for(j = 0; i < numEntries; j += 1)
+							reqName = StringFromList(i, reqNames, ",")
+							reqType = AFH_GetAnalysisParamType(reqName, reqParams, typeCheck = 0)
+							// no type specification is allowed
+							if(IsEmpty(reqType))
+								continue
+							endif
+
+							// invalid types are not allowed
+							if(WhichListItem(reqType, ANALYSIS_FUNCTION_PARAMS_TYPES) == -1)
+								printf "(%s) The analysis parameter %s for %s in stim set %s has type %s which is unknown.\r", panelTitle, name, func, dacWave, type
+								ControlWindowToFront()
+								return 1
+							endif
+
+							// non matching type
+							suppType = AFH_GetAnalysisParamType(reqName, suppParams, typeCheck = 0)
+							if(cmpstr(reqType, suppType))
+								printf "(%s) The analysis parameter %s for %s in stim set %s has type %s but the required type is %s which is unknown.\r", panelTitle, name, func, dacWave, suppType, reqType
+								ControlWindowToFront()
+								return 1
+							endif
+						endfor
 					endif
 				endif
 			endfor

Packages/MIES/MIES_AnalysisFunctionHelpers.ipf
@@ -378,11 +378,13 @@ Function/S AFH_GetAnalysisFunctions(versionBitMask)
 End
 
 /// @brief Return the list of required analysis function
-/// parameters as specified by the function `$func_GetParams`
+/// parameters, possibly including the type, as specified by the function `$func_GetParams`
 ///
 /// @param func Analysis function `V3` which must be valid and existing
 Function/S AFH_GetListOfReqAnalysisParams(func)
 	string func
+
+	string params
 
 	FUNCREF AF_PROTO_PARAM_GETTER_V3 f = $(func + "_GetParams")
 
@@ -390,7 +392,11 @@ Function/S AFH_GetListOfReqAnalysisParams(func)
 		return ""
 	endif
 
-	return f()
+	params = f()
+
+	ASSERT(strsearch(params, ";", 0) == -1, "Entries must be separated with ,")
+
+	return params
 End
 
 /// @defgroup AnalysisFunctionParameterHelpers Analysis Helper functions for dealing with user parameters

Packages/MIES/MIES_AnalysisFunctions.ipf
@@ -60,9 +60,9 @@
 ///
 /// If you want to enforce a list of parameters which must be present, define
 /// an additional function named like your analysis function but suffixed with
-/// `_GetParams` and return a semicolon separated list of required names. This
-/// is then checked before DAQ, and if not all required parameters are supplied
-/// DAQ is not started.
+/// `_GetParams` and return a comma separated list of required names. Adding the
+/// type is also possible via `$name:$type` syntax. The list of parameter names and types
+/// is then checked before DAQ.
 
 /// @name Initial parameters for stimulation
 ///@{

Packages/MIES/MIES_AnalysisFunctions_PatchSeq.ipf
@@ -936,7 +936,7 @@ End
 /// - OperationMode (String):  Operation mode of the analayis function. Can be
 ///                            either #PSQ_DS_SUB or #PSQ_DS_SUPRA.
 Function/S PSQ_DAScale_GetParams()
-	return "DAScales;OperationMode"
+	return "DAScales,OperationMode"
 End
 
 /// @brief Patch Seq Analysis function to find a suitable DAScale
@@ -1716,7 +1716,7 @@ End
 ///                              the pulse onset in order to label the cell
 ///                              as having "spiked".
 Function/S PSQ_Ramp_GetParams()
-	return "NumberOfSpikes;"
+	return "NumberOfSpikes,"
 End
 
 /// @brief Analysis function for applying a ramp stim set and finding the position were it spikes.

Packages/MIES/MIES_WaveBuilderPanel.ipf
@@ -2586,7 +2586,7 @@ End
 static Function WBP_UpdateParameterWave()
 
 	string params, names, name, type, genericFunc, reqParams
-	string missingParams
+	string missingParams, reqNames
 	variable i, numEntries, offset
 
 	Wave/T listWave = WBP_GetAnalysisParamGUIListWave()
@@ -2596,6 +2596,7 @@ static Function WBP_UpdateParameterWave()
 
 	genericFunc = WPT[%$("Analysis function (generic)")][%Set]
 	reqParams = AFH_GetListOfReqAnalysisParams(genericFunc)
+	reqNames  = AFH_GetListOfAnalysisParamNames(reqParams)
 
 	params = WBP_GetAnalysisParameters()
 	names  = AFH_GetListOfAnalysisParamNames(params)
@@ -2612,7 +2613,7 @@ static Function WBP_UpdateParameterWave()
 
 	offset = DimSize(listWave, ROWS)
 
-	missingParams = GetListDifference(reqParams, names)
+	missingParams = GetListDifference(reqNames, names)
 	numEntries = ItemsInList(missingParams)
 	Redimension/N=(offset + numEntries, -1) listWave, selWave
 

commit 5cebfd5c1ff7d6825c82f19150bb3ed5238429ad

DAP_CheckStimset: Fix checking required analysis parameters
We have a semicolon separated list in reqNames and also the indizes in
the loop header were wrong.

Bug introduced in ff7dd72a (Analysis parameters: Add method to request
the types as well, 2018-05-13).

The bug manifested itself through only checking the 6th required
parameter and beyond.

Embarissingly the tests expected the broken behaviour. This is now fixed
as well.

Packages/MIES/MIES_DAEphys.ipf
@@ -2766,9 +2766,9 @@ static Function DAP_CheckStimset(panelTitle, channelType, channel, headstage)
 						return 1
 					endif
 
-					numEntries = ItemsInList(reqNames, ",")
-					for(k = 0; j < numEntries; k += 1)
-						reqName = StringFromList(j, reqNames, ",")
+					numEntries = ItemsInList(reqNames)
+					for(k = 0; k < numEntries; k += 1)
+						reqName = StringFromList(k, reqNames)
 
 						if(!AFH_IsValidAnalysisParameter(reqName))
 							printf "(%s) The required analysis parameter %s for %s in stim set %s has the invalid name %s.\r", panelTitle, name, func, setName, reqName

Packages/Testing-MIES/UTF_AnalysisFunctionManagement.ipf
@@ -1487,7 +1487,15 @@ static Function AFT14b([str])
 	InitDAQSettingsFromString(s, "MD1_RA0_I0_L0_BKG_1")
 
 	FUNCREF CALLABLE_PROTO f = SetParams3_IGNORE
-	AcquireData(s, "AnaFuncParams3_DA_0", str, postInitializeFunc = f)
+
+	try
+		ClearRTError()
+		AcquireData(s, "AnaFuncParams3_DA_0", str, postInitializeFunc = f); AbortOnRTE
+		FAIL()
+	catch
+		ClearRTError()
+		PASS()
+	endtry
 End
 
 static Function AFT14b_REENTRY([str])
@@ -1496,20 +1504,20 @@ static Function AFT14b_REENTRY([str])
 	variable sweepNo
 	string key
 
-	CHECK_EQUAL_VAR(GetSetVariable(str, "SetVar_Sweep"), 1)
+	CHECK_EQUAL_VAR(GetSetVariable(str, "SetVar_Sweep"), 0)
 
 	sweepNo = AFH_GetLastSweepAcquired(str)
-	CHECK_EQUAL_VAR(sweepNo, 0)
+	CHECK_EQUAL_VAR(sweepNo, NaN)
 
 	WAVE anaFuncTracker = TrackAnalysisFunctionCalls()
 
 	CHECK_EQUAL_VAR(anaFuncTracker[PRE_DAQ_EVENT], 1)
-	CHECK_EQUAL_VAR(anaFuncTracker[PRE_SET_EVENT], 1)
-	CHECK_EQUAL_VAR(anaFuncTracker[PRE_SWEEP_EVENT], 1)
-	CHECK(anaFuncTracker[MID_SWEEP_EVENT] >= 1)
-	CHECK_EQUAL_VAR(anaFuncTracker[POST_SWEEP_EVENT], 1)
+	CHECK_EQUAL_VAR(anaFuncTracker[PRE_SET_EVENT], 0)
+	CHECK_EQUAL_VAR(anaFuncTracker[PRE_SWEEP_EVENT], 0)
+	CHECK(anaFuncTracker[MID_SWEEP_EVENT] >= 0)
+	CHECK_EQUAL_VAR(anaFuncTracker[POST_SWEEP_EVENT], 0)
 	CHECK_EQUAL_VAR(anaFuncTracker[POST_SET_EVENT], 0)
-	CHECK_EQUAL_VAR(anaFuncTracker[POST_DAQ_EVENT], 1)
+	CHECK_EQUAL_VAR(anaFuncTracker[POST_DAQ_EVENT], 0)
 	CHECK_EQUAL_VAR(anaFuncTracker[GENERIC_EVENT], 0)
 End
 
@@ -1529,7 +1537,15 @@ static Function AFT14c([str])
 	InitDAQSettingsFromString(s, "MD1_RA0_I0_L0_BKG_1")
 
 	FUNCREF CALLABLE_PROTO f = SetParams4_IGNORE
-	AcquireData(s, "AnaFuncParams4_DA_0", str, postInitializeFunc = f)
+
+	try
+		ClearRTError()
+		AcquireData(s, "AnaFuncParams4_DA_0", str, postInitializeFunc = f); AbortOnRTE
+		FAIL()
+	catch
+		ClearRTError()
+		PASS()
+	endtry
 End
 
 static Function AFT14c_REENTRY([str])
@@ -1538,20 +1554,20 @@ static Function AFT14c_REENTRY([str])
 	variable sweepNo
 	string key
 
-	CHECK_EQUAL_VAR(GetSetVariable(str, "SetVar_Sweep"), 1)
+	CHECK_EQUAL_VAR(GetSetVariable(str, "SetVar_Sweep"), 0)
 
 	sweepNo = AFH_GetLastSweepAcquired(str)
-	CHECK_EQUAL_VAR(sweepNo, 0)
+	CHECK_EQUAL_VAR(sweepNo, NaN)
 
 	WAVE anaFuncTracker = TrackAnalysisFunctionCalls()
 
 	CHECK_EQUAL_VAR(anaFuncTracker[PRE_DAQ_EVENT], 1)
-	CHECK_EQUAL_VAR(anaFuncTracker[PRE_SET_EVENT], 1)
-	CHECK_EQUAL_VAR(anaFuncTracker[PRE_SWEEP_EVENT], 1)
-	CHECK(anaFuncTracker[MID_SWEEP_EVENT] >= 1)
-	CHECK_EQUAL_VAR(anaFuncTracker[POST_SWEEP_EVENT], 1)
+	CHECK_EQUAL_VAR(anaFuncTracker[PRE_SET_EVENT], 0)
+	CHECK_EQUAL_VAR(anaFuncTracker[PRE_SWEEP_EVENT], 0)
+	CHECK(anaFuncTracker[MID_SWEEP_EVENT] >= 0)
+	CHECK_EQUAL_VAR(anaFuncTracker[POST_SWEEP_EVENT], 0)
 	CHECK_EQUAL_VAR(anaFuncTracker[POST_SET_EVENT], 0)
-	CHECK_EQUAL_VAR(anaFuncTracker[POST_DAQ_EVENT], 1)
+	CHECK_EQUAL_VAR(anaFuncTracker[POST_DAQ_EVENT], 0)
 	CHECK_EQUAL_VAR(anaFuncTracker[GENERIC_EVENT], 0)
 End
 