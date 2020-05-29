AllenInstitute/MIES

commit 0cb2be532d51a082c37dcc4ec01b908b34fe8a81

CreateTiledChannelGraph: Show the headstage as well
We now add an additional free axis which will be used for labeling the
DA and AD channels with their headstage. And also move
the current axis labels closer to the axis.

If both are shown we use the axis range of both (including the distance
between them) otherwise we only use the respective axis lengths.

This also requires us to remove any remaining free axis before we start,
this is done by RemoveFreeAxisFromGraph.

Packages/MIES/MIES_MiesUtilities.ipf
@@ -2085,12 +2085,12 @@ Function CreateTiledChannelGraph(graph, config, sweepNo, numericalValues,  textu
 	variable moreData, low, high, step, spacePerSlot, chan, numSlots, numHorizWaves, numVertWaves, idx
 	variable numTTLBits, colorIndex, totalVertBlocks, headstage
 	variable delayOnsetUser, delayOnsetAuto, delayTermination, delaydDAQ, dDAQEnabled, oodDAQEnabled
-	variable stimSetLength, samplingInt, xRangeStart, xRangeEnd, first, last, count
+	variable stimSetLength, samplingInt, xRangeStart, xRangeEnd, first, last, count, freeAxisHigh
 	variable numDACsOriginal, numADCsOriginal, numTTLsOriginal, numRegions, numEntries, numRangesPerEntry
 	variable totalXRange = NaN
 
 	string trace, traceType, channelID, axisLabel, entry, range
-	string unit, name, str, vertAxis, oodDAQRegionsAll, dDAQActiveHeadstageAll, horizAxis
+	string unit, name, str, vertAxis, oodDAQRegionsAll, dDAQActiveHeadstageAll, horizAxis, freeAxis
 
 	ASSERT(!isEmpty(graph), "Empty graph")
 	ASSERT(IsFinite(sweepNo), "Non-finite sweepNo")
@@ -2486,6 +2486,21 @@ Function CreateTiledChannelGraph(graph, config, sweepNo, numericalValues,  textu
 						sprintf str, "vert axis=[%g, %g]", low, high
 						DEBUGPRINT(str)
 						ModifyGraph/W=$graph axisEnab($vertAxis) = {low, high}
+
+						if(channelTypes[i] == ITC_XOP_CHANNEL_TYPE_ADC || (!tgs.displayADC && channelTypes[i] == ITC_XOP_CHANNEL_TYPE_DAC) && IsFinite(headstage))
+							if(tgs.displayDAC && tgs.displayADC)
+								freeAxisHigh = high * (1 + 1 / ADC_SLOT_MULTIPLIER) + GRAPH_DIV_SPACING
+							else
+								freeAxisHigh = high
+							endif
+
+							freeAxisHigh = min(1, freeAxisHigh)
+
+							freeAxis = "freeaxis_hs" + num2str(headstage)
+							NewFreeAxis/O $freeAxis
+							ModifyGraph/W=$graph axisEnab($freeAxis)={low,freeAxisHigh}, standoff($freeAxis)=0, lblPosMode($freeAxis)=2, axRGB($freeAxis)=(65535,65535,65535,0), tlblRGB($freeAxis)=(65535,65535,65535,0), alblRGB($freeAxis)=(0,0,0), lblMargin($freeAxis)=0, lblLatPos($freeAxis)=0
+							Label/W=$graph $freeAxis "HS" + num2str(headstage)
+						endif
 					endif
 
 					if(k == 0) // first column, add labels
@@ -2495,7 +2510,7 @@ Function CreateTiledChannelGraph(graph, config, sweepNo, numericalValues,  textu
 							unit = "a.u."
 						endif
 
-						axisLabel = traceType + "\r(" + unit + ")"
+						axisLabel = "\Z08"+ traceType + "\r(" + unit + ")"
 
 						FindValue/TXOP=4/TEXT=(vertAxis) axisLabelCache
 						axisIndex = V_Value
@@ -2514,7 +2529,7 @@ Function CreateTiledChannelGraph(graph, config, sweepNo, numericalValues,  textu
 							SetNumberInWaveNote(axisLabelCache, NOTE_INDEX, count + 1)
 						endif
 
-						ModifyGraph/W=$graph lblPosMode($vertAxis) = 1, standoff($vertAxis) = 0, freePos($vertAxis) = 0
+						ModifyGraph/W=$graph lblPosMode($vertAxis) = 2, standoff($vertAxis) = 0, freePos($vertAxis) = 0, lblLatPos($vertAxis) = 3, lblMargin($vertAxis) = 15
 
 						if(channelTypes[i] == ITC_XOP_CHANNEL_TYPE_TTL && tgs.splitTTLBits)
 							ModifyGraph/W=$graph nticks($vertAxis)=2,manTick($vertAxis)={0,1,0,0},manMinor($vertAxis)={0,50}
@@ -4637,6 +4652,28 @@ Function CheckIfPathsRefIdenticalFiles(list)
 	endfor
 
 	return 1
+End
+
+/// @brief Remove all free axis from the given graph
+Function RemoveFreeAxisFromGraph(graph)
+	string graph
+
+	string list, name, info
+	variable i, numEntries
+
+	list = AxisList(graph)
+	numEntries = ItemsInList(list)
+
+	for(i = 0; i < numEntries; i += 1)
+		name = StringFromList(i, list)
+		info = AxisInfo(graph, name)
+
+		if(!NumberByKey("ISTFREE", info))
+			continue
+		endif
+
+		KillFreeAxis/W=$graph $name
+	endfor
 End
 
 /// @brief Remove traces from a graph and optionally try to kill their waves

Packages/MIES/MIES_AnalysisBrowser_SweepBrowser.ipf
@@ -358,6 +358,7 @@ Function SB_UpdateSweepPlot(win, [newSweep])
 
 	WAVE/T cursorInfos = GetCursorInfos(graph)
 	RemoveTracesFromGraph(graph)
+	RemoveFreeAxisFromGraph(graph)
 
 	WAVE/T sweepMap = SB_GetSweepBrowserMap(sweepBrowserDFR)
 	WAVE channelSel = GetChannelSelectionWave(sweepBrowserDFR)

Packages/MIES/MIES_DataBrowser.ipf
@@ -238,6 +238,7 @@ Function/S DB_ClearAllGraphs()
 
 		if(WindowExists(graph))
 			RemoveTracesFromGraph(graph)
+			RemoveFreeAxisFromGraph(graph)
 		endif
 	endfor
 End
@@ -384,6 +385,7 @@ Function DB_UpdateSweepPlot(win)
 
 	WAVE/T cursorInfos = GetCursorInfos(graph)
 	RemoveTracesFromGraph(graph)
+	RemoveFreeAxisFromGraph(graph)
 
 	if(!BSP_HasBoundDevice(win))
 		return NaN
@@ -525,6 +527,7 @@ static Function DB_ClearGraph(win)
 	endif
 
 	RemoveTracesFromGraph(graph)
+	RemoveFreeAxisFromGraph(graph)
 	UpdateLBGraphLegend(graph)
 End
 

commit c5eda158de635f45876093c6adf132093a70db3b

CreateTiledChannelGraph: Add forgotten window specification
Broken since 0cb2be53 (CreateTiledChannelGraph: Show the headstage as
well, 2020-03-30).

Packages/MIES/MIES_MiesUtilities.ipf
@@ -2497,7 +2497,7 @@ Function CreateTiledChannelGraph(graph, config, sweepNo, numericalValues,  textu
 							freeAxisHigh = min(1, freeAxisHigh)
 
 							freeAxis = "freeaxis_hs" + num2str(headstage)
-							NewFreeAxis/O $freeAxis
+							NewFreeAxis/O/W=$graph $freeAxis
 							ModifyGraph/W=$graph axisEnab($freeAxis)={low,freeAxisHigh}, standoff($freeAxis)=0, lblPosMode($freeAxis)=2, axRGB($freeAxis)=(65535,65535,65535,0), tlblRGB($freeAxis)=(65535,65535,65535,0), alblRGB($freeAxis)=(0,0,0), lblMargin($freeAxis)=0, lblLatPos($freeAxis)=0
 							Label/W=$graph $freeAxis "HS" + num2str(headstage)
 						endif