AllenInstitute/MIES

commit e029423bfb6bb5a7d84e6719686c7a1d2ca0045a

Make DataBrowser and PulseAverage graphs *much* faster
We now don't use the Igor builtin user data for traces anymore, but our own.

This means all calls to TraceNameList, GetUserData, TraceNameToWaveRef and TraceInfo are gone,
and replaced by calls to our new TUD* functions or new convenience wrappers.

This required a large number of changes:
- Combine GetAllSweepTraces and GetAllSweepTracesFromGraphs and let it return a text wave
- Generalize PA_GetTraceInfos to GetTraceInfos
- Unify the trace naming with space for up to 10e6 traces
- Artefact removal traces can now be found with the traceType and not with special trace naming
- SweepFormula now also uses the new functionality. This also makes it faster, as it does not need to query the
  traces for their user data.
- CreateTiledChannelGraph now also stores the XAXIS/YAXIS and the traceRange to avoid later calls to TraceInfo
- ReplaceAllWavesWithBackup does now accept a text wave instead of a list
- PulseAverage:
  * Use per trace user data instead of SPECIAL_TRACES graph user data
  * Optimize further and only call the post-processing block (average, multiple graphs, deconvolution) if we really need to do something
  * Less complicated code by introducing isDiagonalElement

Packages/MIES/MIES_AnalysisBrowser_SweepBrowser.ipf
@@ -359,6 +359,7 @@ Function SB_UpdateSweepPlot(win, [newSweep])
 	WAVE/T/Z cursorInfos = GetCursorInfos(graph)
 	RemoveTracesFromGraph(graph)
 	RemoveFreeAxisFromGraph(graph)
+	TUD_Clear(graph)
 
 	WAVE/T sweepMap = SB_GetSweepBrowserMap(sweepBrowserDFR)
 	WAVE channelSel = GetChannelSelectionWave(sweepBrowserDFR)
@@ -783,7 +784,12 @@ Function SB_ButtonProc_FindMinis(ba) : ButtonControl
 
 	switch(ba.eventCode)
 		case 2: // mouse up
-			list = GetAllSweepTraces(graph)
+
+			WAVE/T/Z tracePaths = GetAllSweepTraces(graph)
+
+			if(!WaveExists(tracePaths))
+				break
+			endif
 
 			first = NumberByKey("POINT", CsrInfo(A, graph))
 			last  = NumberByKey("POINT", CsrInfo(B, graph))
@@ -791,10 +797,9 @@ Function SB_ButtonProc_FindMinis(ba) : ButtonControl
 
 			DFREF workDFR = UniqueDataFolder(SB_GetSweepBrowserFolder(graph), "findminis")
 
-			numTraces = ItemsInList(list)
+			numTraces = DimSize(tracePaths, ROWS)
 			for(i = 0; i < numTraces; i += 1)
-				trace = StringFromList(i, list)
-				WAVE full = TraceNameToWaveRef(graph, trace)
+				WAVE full = $tracePaths[i]
 				if(IsFinite(first) && isFinite(last))
 					Duplicate/R=[first, last] full, workDFR:$(NameOfWave(full) + "_res")/Wave=wv
 				else
@@ -852,7 +857,7 @@ End
 Function SB_ButtonProc_RestoreData(ba) : ButtonControl
 	STRUCT WMButtonAction &ba
 
-	string graph, bsPanel, traceList
+	string graph, bsPanel
 	variable autoRemoveOldState
 
 	graph   = GetMainWindow(ba.win)
@@ -860,8 +865,9 @@ Function SB_ButtonProc_RestoreData(ba) : ButtonControl
 
 	switch(ba.eventCode)
 		case 2: // mouse up
-			traceList = GetAllSweepTraces(graph)
-			ReplaceAllWavesWithBackup(graph, traceList)
+
+			WAVE/T/Z tracePaths = GetAllSweepTraces(graph)
+			ReplaceAllWavesWithBackup(graph, tracePaths)
 
 			if(!AR_IsActive(graph))
 				SB_UpdateSweepPlot(graph)

Packages/MIES/MIES_AnalysisBrowser_SweepBrowser_Export.ipf
@@ -191,7 +191,7 @@ static Function/WAVE SBE_GetPulseStartTimesForSel()
 
 	ADC = str2num(GetPopupMenuString(SBE_EXPORT_PANEL, "popup_sweep_export_pulse_AD"))
 
-	WAVE/Z/T traceData = PA_GetTraceInfos(graph)
+	WAVE/Z/T traceData = GetGraphUserData(graph)
 	if(!WaveExists(traceData))
 		return $""
 	endif
@@ -257,7 +257,13 @@ Function/S SBE_GetSourceGraphADTraces()
 		return ""
 	endif
 
-	return GetAllSweepTraces(sourceGraph, channelType = ITC_XOP_CHANNEL_TYPE_ADC)
+	WAVE/Z/T result = GetAllSweepTraces(sourceGraph, channelType = ITC_XOP_CHANNEL_TYPE_ADC)
+
+	if(!WaveExists(result))
+		return ""
+	endif
+
+	return TextWaveToList(result, ";")
 End
 
 /// @brief Export the sweep browser traces to a user given folder
@@ -436,20 +442,20 @@ static Function SBE_ExportSweepBrowser(sett)
 
 		for(j = 0; j < numTraces; j += 1)
 			trace = StringFromList(j, traceList)
-			traceAxis = StringByKey("YAXIS", TraceInfo(sett.sourceGraph, trace, 0))
+			traceAxis = TUD_GetUserData(sett.sourceGraph, trace, "YAXIS")
 
 			if(cmpstr(traceAxis, axis))
 				continue
 			endif
 
-			WAVE/Z textualValues = $GetUserData(sett.sourceGraph, trace, "textualValues")
+			WAVE/Z textualValues = $TUD_GetUserData(sett.sourceGraph, trace, "textualValues")
 
 			if(!WaveExists(textualValues)) // non-sweep waves
 				continue
 			endif
 
-			headstage = str2num(GetUserData(sett.sourceGraph, trace, "headstage"))
-			sweep = str2num(GetUserData(sett.sourceGraph, trace, "sweepNumber"))
+			headstage = str2num(TUD_GetUserData(sett.sourceGraph, trace, "headstage"))
+			sweep = str2num(TUD_GetUserData(sett.sourceGraph, trace, "sweepNumber"))
 
 			WAVE/T stimSets = GetLastSetting(textualValues, sweep, STIM_WAVE_NAME_KEY, DATA_ACQUISITION_MODE)
 

Packages/MIES/MIES_ArtefactRemoval.ipf
@@ -146,6 +146,7 @@ Function AR_RemoveTraces(graph)
 	for(i = 0; i < numEntries; i += 1)
 		trace = StringFromList(i, traces)
 		RemoveFromGraph/W=$graph $trace
+		TUD_RemoveUserData(graph, trace)
 	endfor
 End
 
@@ -153,7 +154,13 @@ End
 Function/S AR_GetHighlightTraces(graph)
 	string graph
 
-	return ListMatch(TraceNameList(graph, ";", 1), "AR_*")
+	WAVE/Z/T traces = TUD_GetUserDataAsWave(graph, "traceName", keys = {"traceType"}, values = {"ArtefactRemoval"})
+
+	if(!WaveExists(traces))
+		return ""
+	endif
+
+	return TextWaveToList(traces, ";")
 End
 
 Function AR_HighlightArtefactsEntry(graph)
@@ -183,7 +190,7 @@ Function AR_HighlightArtefactsEntry(graph)
 	numEntries = ItemsInList(traces)
 	for(i = 0; i < numEntries; i += 1)
 		trace = StringFromList(i, traces)
-		index = str2num(GetUserData(graph, trace, "AR_INDEX"))
+		index = str2num(TUD_GetUserData(graph, trace, "AR_INDEX"))
 
 		if(row == index)
 			ModifyGraph/W=$graph rgb($trace)=(1,39321,19939,32768)
@@ -199,8 +206,8 @@ Function AR_HandleRanges(graph, [removeRange])
 	variable removeRange
 
 	variable first, last, substituteValue
-	variable i, j, k, l, numEntries
-	string traceName, leftAxis, bottomAxis, extPanel, yRangeStr
+	variable i, j, k, traceIndex, numEntries
+	string traceName, leftAxis, bottomAxis, extPanel
 
 	extPanel = BSP_GetPanel(graph)
 
@@ -223,14 +230,6 @@ Function AR_HandleRanges(graph, [removeRange])
 	DFREF sweepDFR = AR_GetSweepFolder(graph)
 	WAVE/WAVE ADCs = GetITCDataSingleColumnWaves(sweepDFR, ITC_XOP_CHANNEL_TYPE_ADC)
 
-	WAVE/T/Z traceData = PA_GetTraceInfos(graph, channelType = ITC_XOP_CHANNEL_TYPE_ADC)
-
-	if(!WaveExists(traceData))
-		return NaN
-	endif
-
-	Make/FREE/T/N=(DimSize(traceData, ROWS)) leftAxisData = StringByKey("YAXIS", TraceInfo(graph, traceData[p][%traceName], 0))
-
 	ASSERT(DimSize(listBoxWave, ROWS) == DimSize(artefactWave, ROWS), "Unexpected dimension sizes")
 
 	numEntries = DimSize(listBoxWave, ROWS)
@@ -248,10 +247,10 @@ Function AR_HandleRanges(graph, [removeRange])
 				continue
 			endif
 
-			WAVE/Z indizes = FindIndizes(traceData, colLabel="channelNumber",var = j)
-			ASSERT(WaveExists(indizes) && DimSize(indizes, ROWS) >= 1, "Expected one hit")
-
-			leftAxis = leftAxisData[indizes[0]]
+			WAVE/T/Z leftAxisMatches = TUD_GetUserDataAsWave(graph, "YAXIS", keys = {"channelType", "channelNumber"}, \
+						                                     values = {"AD", num2str(j)})
+			ASSERT(WaveExists(leftAxisMatches) && DimSize(leftAxisMatches, ROWS) >= 1, "Expected one hit")
+			leftAxis = leftAxisMatches[0]
 
 			// skip that AD as the range originated from the DA of the same headstage
 			if(j == artefactWave[i][%ADC])
@@ -272,12 +271,13 @@ Function AR_HandleRanges(graph, [removeRange])
 				substituteValue = AD[first]
 				AD[first, last] = substituteValue
 			else
-				sprintf traceName, "AR_%d_AD_%d_HS_%d", l, j, i
+				sprintf traceName, "T%0*d_HS_%d", TRACE_NAME_NUM_DIGITS, traceIndex, j, i
+				traceIndex += 1
 				AppendToGraph/W=$graph/L=$leftAxis/B=$bottomAxis AD[first, last]/TN=$traceName
 				ModifyGraph/W=$graph mode($traceName)=3, marker($traceName)=8
 				ModifyGraph/W=$graph msize($traceName)=0.5,rgb($traceName)=(65535,0,0,32768)
-				ModifyGraph/W=$graph userData($traceName)={AR_INDEX, USERDATA_MODIFYGRAPH_REPLACE, num2str(i)}
-				l += 1
+				TUD_SetUserData(graph, traceName, "AR_INDEX", num2str(i))
+				TUD_SetUserData(graph, traceName, "traceType", "ArtefactRemoval")
 			endif
 		endfor
 	endfor

Packages/MIES/MIES_Constants.ipf
@@ -1299,3 +1299,6 @@ Constant LNB_TYPE_TEXTUAL = 2
 
 /// @brief Wave note key for the indexing helper JSON document
 StrConstant TUD_INDEX_JSON = "INDEX_JSON"
+
+/// @brief sprintf field width for trace names
+Constant TRACE_NAME_NUM_DIGITS = 6

Packages/MIES/MIES_DataBrowser.ipf
@@ -386,6 +386,7 @@ Function DB_UpdateSweepPlot(win)
 	WAVE/T/Z cursorInfos = GetCursorInfos(graph)
 	RemoveTracesFromGraph(graph)
 	RemoveFreeAxisFromGraph(graph)
+	TUD_Clear(graph)
 
 	if(!BSP_HasBoundDevice(win))
 		return NaN
@@ -1112,8 +1113,9 @@ Function DB_ButtonProc_RestoreData(ba) : ButtonControl
 
 	switch(ba.eventCode)
 		case 2: // mouse up
-			traceList = GetAllSweepTraces(graph)
-			ReplaceAllWavesWithBackup(graph, traceList)
+
+			WAVE/T/Z tracePaths = GetSweepUserData(graph, "fullPath")
+			ReplaceAllWavesWithBackup(graph, tracePaths)
 
 			zeroTracesOldState = GetCheckBoxState(bsPanel, "check_Calculation_ZeroTraces")
 			SetCheckBoxState(bsPanel, "check_Calculation_ZeroTraces", CHECKBOX_UNSELECTED)

Packages/MIES/MIES_OverlaySweeps.ipf
@@ -24,7 +24,7 @@ Function OVS_IgnoreHeadstageInOverlay()
 		return NaN
 	endif
 
-	sweepNo = str2num(GetUserData(graph, trace, "sweepNumber"))
+	sweepNo = str2num(TUD_GetUserData(graph, trace, "sweepNumber"))
 
 	if(!IsValidSweepNumber(sweepNo))
 		printf "Could not extract sweep number information from trace \"%s\".\r", trace
@@ -32,7 +32,7 @@ Function OVS_IgnoreHeadstageInOverlay()
 		return NaN
 	endif
 
-	headstage = str2num(GetUserData(graph, trace, "headstage"))
+	headstage = str2num(TUD_GetUserData(graph, trace, "headstage"))
 
 	if(!IsFinite(headstage))
 		printf "Ignoring trace \"%s\" as it is not associated with a headstage.\r", trace
@@ -47,7 +47,7 @@ Function OVS_IgnoreHeadstageInOverlay()
 	folder = GetUserData(graph, "", "folder")
 
 	if(!IsEmpty(folder))
-		WAVE traceWave     = TraceNameToWaveRef(graph, trace)
+		WAVE traceWave = $TUD_GetUserData(graph, trace, "fullPath")
 		DFREF sweepDataDFR = GetWavesDataFolderDFR(traceWave)
 		index = SB_GetIndexFromSweepDataPath(graph, sweepDataDFR)
 		OVS_AddToIgnoreList(extPanel, headstage, index=index)

commit b09d8888143018ece52f9d489f8359818848b90b

SB_ButtonProc_FindMinis: Fix trace gathering
Broken since e029423b (Make DataBrowser and PulseAverage graphs *much*
faster, 2020-06-15).

Packages/MIES/MIES_AnalysisBrowser_SweepBrowser.ipf
@@ -572,7 +572,7 @@ Function SB_ButtonProc_FindMinis(ba) : ButtonControl
 		case 2: // mouse up
 			graph = GetMainWindow(ba.win)
 
-			WAVE/T/Z tracePaths = GetAllSweepTraces(graph)
+			WAVE/T/Z tracePaths = GetSweepUserData(graph, "fullPath")
 
 			if(!WaveExists(tracePaths))
 				break