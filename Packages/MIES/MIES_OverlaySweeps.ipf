AllenInstitute/MIES

commit 663db034f3e6220d990fd2e19bc8dc7785ca55ad

OVS_UpdatePanel: Make it run faster
OVS_UpdatePanel is always called when we have acquired new sweeps and a
databrowser is open.

This function currently iterates over all sweeps and queries various
labnotebook entries for populating the sweep selection choices for the
popupmenu in the overlay sweeps tab.

This is a rather expensive task which only needs to be done if the user
chooses the popup menu the first time after changes.

This is now implemented.

Packages/MIES/MIES_OverlaySweeps.ipf
@@ -69,7 +69,7 @@ Function/S OVS_GetSweepSelectionChoices(win)
 	endif
 	DFREF dfr = OVS_GetFolder(win)
 
-	WAVE/T sweepSelChoices = GetOverlaySweepSelectionChoices(dfr)
+	WAVE/T sweepSelChoices = GetOverlaySweepSelectionChoices(win, dfr)
 
 	Duplicate/FREE/R=[][][0]/T sweepSelChoices, sweepSelecChoicesDAStimSets
 
@@ -132,23 +132,25 @@ Function OVS_UpdatePanel(win, [fullUpdate])
 
 	DFREF dfr = BSP_GetFolder(win, MIES_BSP_PANEL_FOLDER)
 
+	WAVE/T sweepSelectionChoices = GetOverlaySweepSelectionChoices(win, dfr, skipUpdate = 1)
+	SetNumberInWaveNote(sweepSelectionChoices, NOTE_NEEDS_UPDATE, 1)
+	WaveClear sweepSelectionChoices
+
 	WAVE/T listBoxWave           = GetOverlaySweepsListWave(dfr)
 	WAVE listBoxSelWave          = GetOverlaySweepsListSelWave(dfr)
-	WAVE/T sweepSelectionChoices = GetOverlaySweepSelectionChoices(dfr)
 
 	WAVE/WAVE allNumericalValues = BSP_GetNumericalValues(win)
-	WAVE/WAVE allTextualValues   = BSP_GetTextualValues(win)
 
 	WAVE updateHandle = OVS_BeginIncrementalUpdate(win, fullUpdate = fullUpdate)
 
 	if(!WaveExists(sweeps))
-		Redimension/N=(0, -1, -1) listBoxWave, listBoxSelWave, sweepSelectionChoices
+		Redimension/N=(0, -1, -1) listBoxWave, listBoxSelWave
 		OVS_EndIncrementalUpdate(win, updateHandle)
 		return NaN
 	endif
 
 	numEntries = DimSize(sweeps, ROWS)
-	Redimension/N=(numEntries, -1, -1) listBoxWave, listBoxSelWave, sweepSelectionChoices
+	Redimension/N=(numEntries, -1, -1) listBoxWave, listBoxSelWave
 
 	MultiThread listBoxWave[][%Sweep] = num2str(sweeps[p])
 
@@ -157,26 +159,6 @@ Function OVS_UpdatePanel(win, [fullUpdate])
 	else
 		listBoxSelWave[][%Headstages] = ClearBit(listBoxSelWave[p][%Headstages], LISTBOX_CELL_EDITABLE)
 	endif
-
-	for(i = 0; i < numEntries; i += 1)
-		WAVE/T stimsets = GetLastSetting(allTextualValues[i], sweeps[i], STIM_WAVE_NAME_KEY, DATA_ACQUISITION_MODE)
-		sweepSelectionChoices[i][][%Stimset] = stimsets[q]
-		WAVE/T/Z TTLStimSets = GetTTLstimSets(allNumericalValues[i], allTextualValues[i], sweeps[i])
-		if(WaveExists(TTLStimSets))
-			sweepSelectionChoices[i][][%TTLStimSet] = TTLStimSets[q]
-		else
-			sweepSelectionChoices[i][][%TTLStimSet] = ""
-		endif
-
-		WAVE/Z clampMode = GetLastSetting(allNumericalValues[i], sweeps[i], "Clamp Mode", DATA_ACQUISITION_MODE)
-
-		if(!WaveExists(clampMode))
-			WAVE/Z clampMode = GetLastSetting(allNumericalValues[i], sweeps[i], "Operating Mode", DATA_ACQUISITION_MODE)
-			ASSERT(WaveExists(clampMode), "Labnotebook is too old for NWB export.")
-		endif
-
-		sweepSelectionChoices[i][][%StimsetAndClampMode] = SelectString(IsFinite(clampMode[q]), "", stimsets[q] + " (" + ConvertAmplifierModeShortStr(clampMode[q]) + ")")
-	endfor
 
 	lastEntry = numEntries - 1
 
@@ -198,6 +180,51 @@ Function OVS_UpdatePanel(win, [fullUpdate])
 	endif
 
 	OVS_EndIncrementalUpdate(win, updateHandle)
+End
+
+/// @brief Update the sweep selection choices for the popup menu
+///
+/// This function is expensive as it iterates over all sweeps.
+Function OVS_UpdateSweepSelectionChoices(string win, WAVE/T sweepSelectionChoices)
+
+	variable numEntries, i, needsUpdate
+
+	needsUpdate = GetNumberFromWaveNote(sweepSelectionChoices, NOTE_NEEDS_UPDATE)
+
+	if(!needsUpdate)
+		return NaN
+	endif
+
+	WAVE/WAVE allNumericalValues = BSP_GetNumericalValues(win)
+	WAVE/WAVE allTextualValues   = BSP_GetTextualValues(win)
+
+	WAVE/Z sweeps = GetPlainSweepList(win)
+
+	numEntries = WaveExists(sweeps) ? DimSize(sweeps, ROWS) : 0
+
+	Redimension/N=(numEntries, -1, -1, -1) sweepSelectionChoices
+
+	for(i = 0; i < numEntries; i += 1)
+		WAVE/T stimsets = GetLastSetting(allTextualValues[i], sweeps[i], STIM_WAVE_NAME_KEY, DATA_ACQUISITION_MODE)
+		sweepSelectionChoices[i][][%Stimset] = stimsets[q]
+		WAVE/T/Z TTLStimSets = GetTTLstimSets(allNumericalValues[i], allTextualValues[i], sweeps[i])
+		if(WaveExists(TTLStimSets))
+			sweepSelectionChoices[i][][%TTLStimSet] = TTLStimSets[q]
+		else
+			sweepSelectionChoices[i][][%TTLStimSet] = ""
+		endif
+
+		WAVE/Z clampMode = GetLastSetting(allNumericalValues[i], sweeps[i], "Clamp Mode", DATA_ACQUISITION_MODE)
+
+		if(!WaveExists(clampMode))
+			WAVE/Z clampMode = GetLastSetting(allNumericalValues[i], sweeps[i], "Operating Mode", DATA_ACQUISITION_MODE)
+			ASSERT(WaveExists(clampMode), "Labnotebook is too old for NWB export.")
+		endif
+
+		sweepSelectionChoices[i][][%StimsetAndClampMode] = SelectString(IsFinite(clampMode[q]), "", stimsets[q] + " (" + ConvertAmplifierModeShortStr(clampMode[q]) + ")")
+	endfor
+
+	SetNumberInWaveNote(sweepSelectionChoices, NOTE_NEEDS_UPDATE, 0)
 End
 
 /// @brief Return the selected sweeps (either indizes or the real sweep numbers)
@@ -545,8 +572,6 @@ static Function OVS_ChangeSweepSelection(win, choiceString)
 
 	WAVE updateHandle = OVS_BeginIncrementalUpdate(win)
 
-	WAVE/T sweepSelectionChoices = GetOverlaySweepSelectionChoices(dfr)
-
 	offset = GetSetVariable(extPanel, "setvar_overlaySweeps_offset")
 	step   = GetSetVariable(extPanel, "setvar_overlaySweeps_step")
 
@@ -558,6 +583,8 @@ static Function OVS_ChangeSweepSelection(win, choiceString)
 	elseif(!cmpstr(choiceString, "All"))
 		listboxSelWave[offset, inf;step][%Sweep] = listboxSelWave[p][q] | LISTBOX_CHECKBOX_SELECTED
 	else
+		WAVE/T sweepSelectionChoices = GetOverlaySweepSelectionChoices(win, dfr)
+
 		numLayers = DimSize(sweepSelectionChoices, LAYERS)
 		for(i = 0; i < NUM_HEADSTAGES; i += 1)
 			for(j = 0; j < numLayers; j += 1)

Packages/MIES/MIES_Constants.ipf
@@ -1255,3 +1255,5 @@ Constant POST_PLOT_FULL_UPDATE = 0x8     ///< Forces a complete update from scra
 /// @}
 
 Constant PA_SETTINGS_STRUCT_VERSION = 2
+
+StrConstant NOTE_NEEDS_UPDATE = "NeedsUpdate"

Packages/MIES/MIES_WaveDataFolderGetters.ipf
@@ -5758,10 +5758,18 @@ End
 
 /// @brief Return the overlay sweeps wave with all sweep selection choices
 /// for the databrowser or the sweepbrowser
-Function/WAVE GetOverlaySweepSelectionChoices(dfr)
+Function/WAVE GetOverlaySweepSelectionChoices(win, dfr, [skipUpdate])
+	string win
 	DFREF dfr
+	variable skipUpdate
 
 	variable versionOfNewWave = 3
+
+	if(ParamIsDefault(skipUpdate))
+		skipUpdate = 0
+	else
+		skipUpdate = !!skipUpdate
+	endif
 
 	ASSERT(DataFolderExistsDFR(dfr), "Invalid dfr")
 	string newName = "overlaySweepSelectionChoices"
@@ -5774,6 +5782,9 @@ Function/WAVE GetOverlaySweepSelectionChoices(dfr)
 	WAVE/T/Z wv = UpgradeWaveLocationAndGetIt(p)
 
 	if(ExistsWithCorrectLayoutVersion(wv, versionOfNewWave))
+		if(!skipUpdate)
+			OVS_UpdateSweepSelectionChoices(win, wv)
+		endif
 		return wv
 	elseif(WaveExists(wv))
 		Redimension/N=(-1, -1, 3) wv
@@ -5783,8 +5794,9 @@ Function/WAVE GetOverlaySweepSelectionChoices(dfr)
 	endif
 
 	SetWaveDimLabel(wv, "Stimset;TTLStimset;StimsetAndClampMode", LAYERS)
-
+	SetNumberInWaveNote(wv, "NeedsUpdate", 1)
 	SetWaveVersion(wv, versionOfNewWave)
+
 	return wv
 End
 

commit d40b484b70c0e51a7cdf04742511bcce6053071d

OVS_UpdateSweepSelectionChoices: Handle no sweeps better
We can only call the labnotebook gather functions if we have sweeps. Bug
introduced in 663db034 (OVS_UpdatePanel: Make it run faster,
2020-08-26).

Packages/MIES/MIES_OverlaySweeps.ipf
@@ -195,12 +195,14 @@ Function OVS_UpdateSweepSelectionChoices(string win, WAVE/T sweepSelectionChoice
 		return NaN
 	endif
 
-	WAVE/WAVE allNumericalValues = BSP_GetNumericalValues(win)
-	WAVE/WAVE allTextualValues   = BSP_GetTextualValues(win)
-
 	WAVE/Z sweeps = GetPlainSweepList(win)
 
 	numEntries = WaveExists(sweeps) ? DimSize(sweeps, ROWS) : 0
+
+	if(numEntries > 0)
+		WAVE/WAVE allNumericalValues = BSP_GetNumericalValues(win)
+		WAVE/WAVE allTextualValues   = BSP_GetTextualValues(win)
+	endif
 
 	Redimension/N=(numEntries, -1, -1, -1) sweepSelectionChoices
 