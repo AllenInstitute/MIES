AllenInstitute/MIES

commit a3cb9399156f856803bd58b4ebc7d47ad9539470

Documentation: Undust docker images for documentation building
We now have the possibility to use docker images on our linux bamboo
agent.

Therefore let's use it!

tools/docker/build-documentation.sh
@@ -1,0 +1,21 @@
+#/bin/bash
+
+# checks for correct installation
+if [ ! $(docker -v | grep -c -w version) -eq 1 ]; then
+	echo "docker not found."
+	exit 1
+fi
+if [ ! $(groups | grep -c -w docker) -eq 1 ]; then
+	echo "add current user $(whoami) to docker group!"
+	exit 1
+fi
+
+top_level=$(git rev-parse --show-toplevel)
+
+# build containter
+echo "Start building Docker container 'mies-documentation'"
+docker build --build-arg USERID=$(id -u) --build-arg GROUPID=$(id -g) -t mies-documentation $top_level/tools/docker
+
+# execute build script
+# use 'docker run -it ..' for interactive debugging
+docker run --rm -v $HOME/bamboo-agent-home:/home/ci/bamboo-agent-home -v $top_level:/home/ci mies-documentation tools/build-documentation.sh

tools/docker/Dockerfile
@@ -1,0 +1,53 @@
+FROM debian:buster
+MAINTAINER Thomas Braun thomas.braun@byte-physics.de
+
+# MIES documentation script
+#
+# tools/build-documentation.sh
+
+RUN DEBIAN_FRONTEND=noninteractive \
+    apt-get update &&              \
+    apt-get install -y             \
+      doxygen                      \
+      gawk                         \
+      git                          \
+      graphviz                     \
+      pandoc                       \
+      python2                      \
+      python-pip                   \
+      zip &&                       \
+    apt-get clean
+
+RUN DEBIAN_FRONTEND=noninteractive \
+  pip2 install                     \
+    breathe==4.11.0                \
+    sphinx==1.8.3                  \
+    sphinxcontrib-fulltoc==1.1     \
+    sphinxcontrib-images==0.8.0
+
+# [optional] IPNWB submodule script
+#
+# Packages/IPNWB/update_doc.sh
+
+RUN DEBIAN_FRONTEND=noninteractive \
+  apt-get update &&                \
+  apt-get install -y               \
+    git                            \
+    jq &&                          \
+  apt-get clean
+
+RUN DEBIAN_FRONTEND=noninteractive \
+  pip2 install                     \
+    json2yaml==1.1.1               \
+    yq==2.10.0
+
+ARG USERID
+ARG GROUPID
+
+# add normal user
+RUN groupadd -g $USERID ci
+RUN useradd -u $USERID -g $GROUPID -ms /bin/bash ci
+
+USER ci
+
+WORKDIR /home/ci

tools/docker/docker-build-documentation.sh
@@ -1,22 +1,0 @@
-#/bin/bash
-# script checks for correct installation and executes build-documentation afterwards
-# output is verbose
-#
-# check installation
-if [ ! $(docker -v | grep -c -w version) -eq 1 ]; then
-	echo "docker not found. installing."
-	sudo apt-get -y -q install docker.io
-fi
-if [ ! $(groups | grep -c -w docker) -eq 1 ]; then
-	echo "adding current user to docker group."
-	sudo usermod -aG docker $(whoami)
-	echo "please log off for changes to take affect."
-	exit 0
-fi
-# build containter
-echo "start building Docker container 'docker-build-documentation'"
-docker build -qt docker-build-documentation -f docker-build-documentation/Dockerfile .
-# run doxygen to see if correct version was installed
-echo "Doxygen version" $(docker run -it --rm -v $(pwd)/../..:/opt/mies docker-build-documentation doxygen -v)
-# execute build script (has to be executed from the docker-dir in ./tools/docker)
-docker run -it --rm -v $(pwd)/../..:/opt/mies docker-build-documentation /bin/sh /opt/mies/tools/build-documentation.sh

tools/docker/docker-build-documentation/Dockerfile
@@ -1,28 +1,0 @@
-FROM debian:experimental
-MAINTAINER Matthias J. Kastner matthias.kastner@byte-physics.de
-
-# download appropriate packages
-# @todo utf8x.def is missing in texlive-latex-base and *-full. skip latex
-RUN DEBIAN_FRONTEND=noninteractive \
-	apt-get update && \
-	apt-get install -y \
-		doxygen \
-		gawk \
-		git \
-		graphviz \
-		pandoc \
-		python \
-		python-breathe \
-		python-pip \
-		python-sphinx \
-		zip
-
-# clear cache with downloaded packages
-RUN DEBIAN_FRONTEND=noninteractive \
-	apt-get clean
-
-# install packages via pip
-RUN DEBIAN_FRONTEND=noninteractive \
-	pip install -U sphinxcontrib-fulltoc
-
-WORKDIR /opt/mies/tools/

commit a916fcc0bb0f60f0e7af6ba0a58c33cb4170ef0c

tools/docker/build-documentation.sh: Fix shebang
Fix shebang, bug introduced in a3cb9399 (Documentation: Unddocker
images for documentation building, 2020-02-17).

tools/docker/build-documentation.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 set -e
 