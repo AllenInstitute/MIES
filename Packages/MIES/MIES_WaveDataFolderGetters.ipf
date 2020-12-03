AllenInstitute/MIES

commit 4f2b81825bc012b01c052cfc98293f19158b7dbd

Created global wave getter for DQM ActiveDevicesList, added COL for activeChunk
it is used by TPwhileDAQ to save the last processed TP chunk for ITC

Changed ActiveDeviceList wave for DQM to store double precision

Removed StopCollectionPoint column from DQM ActiveDeviceList
as it is no longer used.

current wave version is now 3

replaced DQM_MakeOrUpdateActivDevLstWave with
DQM_AddDevice and DQM_RemoveDevice

Packages/MIES/MIES_WaveDataFolderGetters.ipf
@@ -800,6 +800,60 @@ Function/Wave GetITCChanConfigWave(panelTitle)
 	return wv
 End
 
+static Constant DQM_ACTIVE_DEV_WAVE_VERSION = 3
+
+/// @brief Returns a reference to the wave of active devices for data acquisition in multi device mode
+///
+/// The wave is used in data acquisition in multiple device mode to keep track of active devices.
+///
+/// Columns:
+/// DeviceID id of an active device
+/// ADChannelToMonitor index of first active AD channel in HardwareDataWave
+/// StopCollectionPoint number of samples to acquire
+/// hardwareType type of hardware of the device
+/// activeChunk if a channel of the device is used for TP while DAQ this column saves the number of the last evaluated test pulse
+///
+/// Changes in version 1:
+/// added column activeChunk
+/// Changes in version 2:
+/// changed precision to double
+/// Changes in version 3:
+/// removed column 2 with StopCollectionPoint as it is no longer used
+Function/Wave GetDQMActiveDeviceList()
+
+	DFREF dfr = GetActiveITCDevicesFolder()
+
+	WAVE/Z/D/SDFR=dfr wv = ActiveDeviceList
+
+	if(ExistsWithCorrectLayoutVersion(wv, DQM_ACTIVE_DEV_WAVE_VERSION))
+		return wv
+	elseif(WaveExists(wv))
+		// do sequential version upgrade
+		if(WaveVersionIsSmaller(wv, 1))
+			Redimension/N=(-1, 5) wv
+			wv[][4] = NaN
+			Note/K wv
+		endif
+		if(WaveVersionIsSmaller(wv, 2))
+			Redimension/D/N=(-1, -1) wv
+		endif
+		if(WaveVersionIsSmaller(wv, 3))
+			DeleteWavePoint(wv, COLS, 2)
+		endif
+	else
+		Make/D/N=(0, 4) dfr:ActiveDeviceList/WAVE=wv
+	endif
+
+	SetDimLabel COLS, 0, DeviceID,    wv
+	SetDimLabel COLS, 1, ADChannelToMonitor, wv
+	SetDimLabel COLS, 2, HardwareType, wv
+	SetDimLabel COLS, 3, ActiveChunk, wv
+
+	SetWaveVersion(wv, DQM_ACTIVE_DEV_WAVE_VERSION)
+
+	return wv
+End
+
 /// @brief Return the intermediate storage wave for the TTL data
 Function/Wave GetTTLWave(panelTitle)
 	string panelTitle

Packages/MIES/MIES_DataAcquisition_Multi.ipf
@@ -19,16 +19,15 @@
 Function DQM_FIFOMonitor(s)
 	STRUCT WMBackgroundStruct &s
 
-	DFREF activeDevices = GetActiveITCDevicesFolder()
-	WAVE/SDFR=activeDevices ActiveDeviceList
 	variable deviceID, isFinished, hardwareType
 	variable i, j, fifoPos, fifoEnd, result, channel, stopCollectionPoint
 	string panelTitle, fifoChannelName, fifoName, errMsg
+	WAVE ActiveDeviceList = GetDQMActiveDeviceList()
 	Make/FREE/N=(0) wNIReadOut
 
 	for(i = 0; i < DimSize(ActiveDeviceList, ROWS); i += 1)
-		deviceID   = ActiveDeviceList[i][0] // deviceID
-		hardwareType = ActiveDeviceList[i][3] // hardwareType
+		deviceID   = ActiveDeviceList[i][%DeviceID]
+		hardwareType = ActiveDeviceList[i][%HardwareType]
 		panelTitle = HW_GetMainDeviceName(hardwareType, deviceID)
 		switch(hardwareType)
 			case HARDWARE_NI_DAC:
@@ -81,7 +80,7 @@ Function DQM_FIFOMonitor(s)
 		endif
 
 		if(isFinished)
-			DQM_MakeOrUpdateActivDevLstWave(panelTitle, deviceID, 0, 0, -1)
+			DQM_RemoveDevice(panelTitle, deviceID)
 			DQM_StopDataAcq(panelTitle, deviceID)
 			i = 0
 			continue
@@ -107,7 +106,7 @@ Function DQM_TerminateOngoingDAQHelper(panelTitle)
 	String panelTitle
 
 	NVAR ITCDeviceIDGlobal = $GetITCDeviceIDGlobal(panelTitle)
-	WAVE/T/SDFR=GetActiveITCDevicesFolder() ActiveDeviceList
+	WAVE ActiveDeviceList = GetDQMActiveDeviceList()
 
 	variable hardwareType = GetHardwareType(panelTitle)
 	if(hardwareType == HARDWARE_ITC_DAC)
@@ -116,7 +115,7 @@ Function DQM_TerminateOngoingDAQHelper(panelTitle)
 	HW_StopAcq(hardwareType, ITCDeviceIDGlobal, zeroDAC = 1, flags=HARDWARE_ABORT_ON_ERROR)
 
 	// remove device passed in from active device lists
-	DQM_MakeOrUpdateActivDevLstWave(panelTitle, ITCDeviceIDGlobal, 0, 0, -1)
+	DQM_RemoveDevice(panelTitle, ITCDeviceIDGlobal)
 
 	// determine if device removed was the last device on the list, if yes stop the background function
 	if(DimSize(ActiveDeviceList, ROWS) == 0)
@@ -363,46 +362,50 @@ static Function DQM_BkrdDataAcq(panelTitle, [triggerMode])
 		TFH_StartFIFOStopDaemon(hardwareType, ITCDeviceIDGlobal)
 	endif
 
-	DQM_MakeOrUpdateActivDevLstWave(panelTitle, ITCDeviceIDGlobal, ADChannelToMonitor, StopCollectionPoint, 1) // adds a device
+	DQM_AddDevice(panelTitle)
 
 	if(!IsBackgroundTaskRunning(TASKNAME_FIFOMONMD))
 		DQM_StartBckrdFIFOMonitor()
 	endif
 End
 
-static Function DQM_MakeOrUpdateActivDevLstWave(panelTitle, ITCDeviceIDGlobal, ADChannelToMonitor, StopCollectionPoint, addOrRemoveDevice)
+/// @brief Removes a device from the ActiveDeviceList
+///
+/// @param panelTitle panel title
+///
+/// @param ITCDeviceIDGlobal device id of the device to be removed
+static Function DQM_RemoveDevice(panelTitle, ITCDeviceIDGlobal)
 	string panelTitle
-	Variable ITCDeviceIDGlobal, ADChannelToMonitor, StopCollectionPoint, addOrRemoveDevice // when removing a device only the ITCDeviceIDGlobal is needed
+	variable ITCDeviceIDGlobal
+
+	variable idCol
+	WAVE ActiveDeviceList = GetDQMActiveDeviceList()
+
+	idCol = FindDimLabel(ActiveDeviceList, 1, "DeviceID")
+	Duplicate/FREE/R=[][idCol] ActiveDeviceList ListOfITCDeviceIDGlobal // duplicates the column that contains the global device ID's
+	FindValue/V=(ITCDeviceIDGlobal) ListOfITCDeviceIDGlobal
+	DeleteWavePoint(ActiveDeviceList, ROWS, V_Value)
+End
+
+/// @brief Adds a device to the ActiveDeviceList
+///
+/// @param panelTitle panel title
+static Function DQM_AddDevice(panelTitle)
+	string panelTitle
 
 	variable numberOfRows
 
-	DFREF dfr = GetActiveITCDevicesFolder()
-	WAVE/Z/D/SDFR=dfr ActiveDeviceList
-	variable hardwareType = GetHardwareType(panelTitle)
+	NVAR ADChannelToMonitor  = $GetADChannelToMonitor(panelTitle)
+	NVAR ITCDeviceIDGlobal   = $GetITCDeviceIDGlobal(panelTitle)
+	WAVE ActiveDeviceList    = GetDQMActiveDeviceList()
 
-	if(addOrRemoveDevice == 1) // add a ITC device
-		if(!WaveExists(ActiveDeviceList))
-			Make/D/N=(1, 4) dfr:ActiveDeviceList/WAVE=ActiveDeviceList
-			ActiveDeviceList[0][0] = ITCDeviceIDGlobal
-			ActiveDeviceList[0][1] = ADChannelToMonitor
-			ActiveDeviceList[0][2] = StopCollectionPoint
-			ActiveDeviceList[0][3] = hardwareType
-		else
-			numberOfRows = DimSize(ActiveDeviceList, ROWS)
-			Redimension/D/N=(numberOfRows + 1, 4) ActiveDeviceList
-			ActiveDeviceList[numberOfRows][0] = ITCDeviceIDGlobal
-			ActiveDeviceList[numberOfRows][1] = ADChannelToMonitor
-			ActiveDeviceList[numberOfRows][2] = StopCollectionPoint
-			ActiveDeviceList[numberOfRows][3] = hardwareType
-		endif
-	elseif(addOrRemoveDevice == -1) // remove a ITC device
-		Duplicate /FREE /r = [][0] ActiveDeviceList ListOfITCDeviceIDGlobal // duplicates the column that contains the global device ID's
-		FindValue/V=(ITCDeviceIDGlobal) ListOfITCDeviceIDGlobal
-		ASSERT(V_Value >= 0, "Trying to remove a non existing device")
-		DeletePoints/M=(ROWS) V_Value, 1, ActiveDeviceList
-	else
-		ASSERT(0, "Invalid addOrRemoveDevice value")
-	endif
+	numberOfRows = DimSize(ActiveDeviceList, ROWS)
+	Redimension/N=(numberOfRows + 1, 4) ActiveDeviceList
+
+	ActiveDeviceList[numberOfRows][%DeviceID] = ITCDeviceIDGlobal
+	ActiveDeviceList[numberOfRows][%ADChannelToMonitor] = ADChannelToMonitor
+	ActiveDeviceList[numberOfRows][%HardwareType] = GetHardwareType(panelTitle)
+	ActiveDeviceList[numberOfRows][%ActiveChunk] = NaN
 End
 
 static Function DQM_MakeOrUpdateTimerParamWave(panelTitle, listOfFunctions, startTime, RunTime, EndTime, addOrRemoveDevice)

Packages/MIES/MIES_MiesUtilities.ipf
@@ -4084,7 +4084,7 @@ Function IsDeviceActiveWithBGTask(panelTitle, task)
 			WAVE/Z/SDFR=GetActiveITCDevicesTimerFolder() deviceIDList = ActiveDevTimeParam
 			break
 		case "ITC_FIFOMonitorMD":
-			WAVE/Z/SDFR=GetActiveITCDevicesFolder() deviceIDList = ActiveDeviceList
+			WAVE deviceIDList = GetDQMActiveDeviceList()
 			break
 		case "TestPulse":
 		case "ITC_Timer":

Packages/MIES/MIES_RepeatedAcquisition.ipf
@@ -424,8 +424,7 @@ static Function RA_AreLeaderAndFollowerFinished()
 	variable numCandidates, i
 	string listOfCandidates, candidate
 
-	WAVE/SDFR=GetActiveITCDevicesFolder() ActiveDeviceList
-
+	WAVE ActiveDeviceList = GetDQMActiveDeviceList()
 	Duplicate/FREE/R=[][0] ActiveDeviceList, activeIDs
 
 	if(DimSize(activeIDs, ROWS) == 0)

commit b4f21a9df666a778e485faac98d6ee86812c6e07

GetDQMActiveDeviceList: Nicify and fix comment
StopCollectionPoint is not a valid column anymore for the latest wave
version. Bug introduced in 4f2b8182 (Created global wave getter for DQM ActiveDevicesList, added COL for activeChunk, 2019-01-09).

Packages/MIES/MIES_WaveDataFolderGetters.ipf
@@ -876,18 +876,15 @@ static Constant DQM_ACTIVE_DEV_WAVE_VERSION = 3
 /// The wave is used in data acquisition in multiple device mode to keep track of active devices.
 ///
 /// Columns:
-/// DeviceID id of an active device
-/// ADChannelToMonitor index of first active AD channel in HardwareDataWave
-/// StopCollectionPoint number of samples to acquire
-/// hardwareType type of hardware of the device
-/// activeChunk if a channel of the device is used for TP while DAQ this column saves the number of the last evaluated test pulse
+/// - DeviceID id of an active device
+/// - ADChannelToMonitor index of first active AD channel in HardwareDataWave
+/// - HardwareType type of hardware of the device
+/// - ActiveChunk if a channel of the device is used for TP while DAQ this column saves the number of the last evaluated test pulse
 ///
-/// Changes in version 1:
-/// added column activeChunk
-/// Changes in version 2:
-/// changed precision to double
-/// Changes in version 3:
-/// removed column 2 with StopCollectionPoint as it is no longer used
+/// Version changes:
+/// - 1: Added column activeChunk
+/// - 2: Changed precision to double
+/// - 3: Removed column 2 with StopCollectionPoint as it is no longer used
 Function/Wave GetDQMActiveDeviceList()
 
 	DFREF dfr = GetActiveITCDevicesFolder()