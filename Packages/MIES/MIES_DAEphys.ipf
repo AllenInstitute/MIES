AllenInstitute/MIES

commit 3fdbb60c0dfabe3209bb1ace84b1072e4586c7ec

DAEphys: Add a wave with device information
In a future commit we want to check if our devices match things like
expected channel numbers.

Therefore it is beneficial to have a wave storing this information. It
is only written once on device locking.

Packages/MIES/MIES_DAEphys.ipf
@@ -4595,6 +4595,9 @@ Function DAP_LockDevice(panelTitle)
 		CtrlNamedBackground _all_, noevents=1
 #endif
 	endif
+
+	WAVE deviceInfo = GetDeviceInfoWave(panelTitle)
+	HW_WriteDeviceInfo(hardwareType, ITCDeviceIDGlobal, deviceInfo)
 End
 
 static Function DAP_LoadBuiltinStimsets()

Packages/MIES/MIES_DAC-Hardware.ipf
@@ -441,6 +441,35 @@ Function/WAVE HW_GetDeviceInfo(hardwareType, deviceID, [flags])
 	endswitch
 End
 
+/// @brief Update the device info wave
+///
+/// Query the data via GetDeviceInfoWave().
+Function HW_WriteDeviceInfo(hardwareType, deviceID, deviceInfo)
+	variable hardwareType, deviceID
+	WAVE deviceInfo
+
+	HW_AssertOnInvalid(hardwareType, deviceID)
+
+	WAVE devInfoHW = HW_GetDeviceInfo(hardwareType, deviceID, flags = HARDWARE_ABORT_ON_ERROR)
+	deviceInfo[%HardwareType] = hardwareType
+
+	switch(hardwareType)
+		case HARDWARE_ITC_DAC:
+			deviceInfo[%AD]   = devInfoHW[%ADCCount]
+			deviceInfo[%DA]   = devInfoHW[%DACCount]
+			deviceInfo[%TTL]  = min(devInfoHW[%DOCount], devInfoHW[%DICount])
+			deviceInfo[%Rack] = ceil(deviceInfo[%TTL] / 3)
+			break
+		case HARDWARE_NI_DAC:
+			WAVE/T devInfoHWText = devInfoHW
+			deviceInfo[%AD]   = str2num(devInfoHWText[%AI])
+			deviceInfo[%DA]   = str2num(devInfoHWText[%AO])
+			deviceInfo[%TTL]  = str2num(devInfoHWText[%DIOPortWidth])
+			deviceInfo[%Rack] = NaN
+			break
+	endswitch
+End
+
 /// @brief Start data acquisition
 ///
 /// @param hardwareType One of @ref HardwareDACTypeConstants

Packages/MIES/MIES_WaveDataFolderGetters.ipf
@@ -6431,3 +6431,33 @@ Function/WAVE GetAnaFuncDashboardColorWave(dfr)
 
 	return wv
 End
+
+/// @brief Return a wave with device information
+Function/WAVE GetDeviceInfoWave(panelTitle)
+	string panelTitle
+
+	variable versionOfNewWave = 1
+
+	DFREF dfr = GetDevicePath(panelTitle)
+	WAVE/D/Z/SDFR=dfr wv = deviceInfo
+
+	if(ExistsWithCorrectLayoutVersion(wv, versionOfNewWave))
+		return wv
+	elseif(WaveExists(wv))
+		// handle upgrade
+	else
+		Make/D/N=(5) dfr:deviceInfo/Wave=wv
+	endif
+
+	SetDimLabel ROWS, 0, AD, wv
+	SetDimLabel ROWS, 1, DA, wv
+	SetDimLabel ROWS, 2, TTL, wv
+	SetDimLabel ROWS, 3, Rack, wv
+	SetDimLabel ROWS, 4, HardwareType, wv
+
+	wv = NaN
+
+	SetWaveVersion(wv, versionOfNewWave)
+
+	return wv
+End

commit 42218878f606a5c58dc76e9e205355571a115fce

DAP_LockDevice: Pass the locked device name to GetDeviceInfoWave
Bug introduced in 3fdbb60c (DAEphys: Add a wave with device information,
2019-09-06).

Packages/MIES/MIES_DAEphys.ipf
@@ -4742,7 +4742,7 @@ Function DAP_LockDevice(panelTitle)
 		KillOrMoveToTrash(wv = GetDQMActiveDeviceList())
 	endif
 
-	WAVE deviceInfo = GetDeviceInfoWave(panelTitle)
+	WAVE deviceInfo = GetDeviceInfoWave(panelTitleLocked)
 	HW_WriteDeviceInfo(hardwareType, ITCDeviceIDGlobal, deviceInfo)
 End
 