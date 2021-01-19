AllenInstitute/MIES

commit 58e89869c2c8c0f977a9179ed61802da425b12e7

tools/clean_mies_installation.sh: Only install HDF5 routines when required
These are shipped with IP9 and activated be default.

tools/clean_mies_installation.sh
@@ -134,8 +134,12 @@ do
     continue
   fi
 
+  if [ $version -le 8 ]
+  then
+    cp -r  "$base_folder"/Packages/HDF-IP${i}  "$user_proc"
+  fi
+
   cp -r  "$base_folder"/Packages/Arduino  "$user_proc"
-  cp -r  "$base_folder"/Packages/HDF-IP${i}  "$user_proc"
   cp -r  "$base_folder"/Packages/IPNWB  "$user_proc"
   cp -r  "$base_folder"/Packages/MIES_include.ipf  "$user_proc"
   cp -r  "$base_folder"/Packages/MIES  "$user_proc"
@@ -155,8 +159,12 @@ do
     cp -r  "$base_folder"/XOPs-IP${i}/*  "$xops32"
     cp -r  "$base_folder"/XOPs-IP${i}-64bit/*  "$xops64"
   else
-    cp -r  "$base_folder"/XOPs-IP${i}/HDF5*  "$xops32"
-    cp -r  "$base_folder"/XOPs-IP${i}-64bit/HDF5*  "$xops64"
+    if [ $version -le 8 ]
+    then
+      cp -r  "$base_folder"/XOPs-IP${i}/HDF5*  "$xops32"
+      cp -r  "$base_folder"/XOPs-IP${i}-64bit/HDF5*  "$xops64"
+    fi
+
     cp -r  "$base_folder"/XOPs-IP${i}/MIESUtils*  "$xops32"
     cp -r  "$base_folder"/XOPs-IP${i}-64bit/MIESUtils*  "$xops64"
   fi

commit f8c15daac36d89b0702696e5b676bbcef55db2fd

tools/clean_mies_installation.sh: Fix version branches
The version is stored in $i. Bug introduced in 58e89869
(tools/clean_mies_installation.sh: Only install HDF5 routines when
required, 2021-01-10).

tools/clean_mies_installation.sh
@@ -134,7 +134,7 @@ do
     continue
   fi
 
-  if [ $version -le 8 ]
+  if [ $i -le 8 ]
   then
     cp -r  "$base_folder"/Packages/HDF-IP${i}  "$user_proc"
   fi
@@ -159,7 +159,7 @@ do
     cp -r  "$base_folder"/XOPs-IP${i}/*  "$xops32"
     cp -r  "$base_folder"/XOPs-IP${i}-64bit/*  "$xops64"
   else
-    if [ $version -le 8 ]
+    if [ $i -le 8 ]
     then
       cp -r  "$base_folder"/XOPs-IP${i}/HDF5*  "$xops32"
       cp -r  "$base_folder"/XOPs-IP${i}-64bit/HDF5*  "$xops64"