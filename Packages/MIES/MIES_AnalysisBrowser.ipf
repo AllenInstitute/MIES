AllenInstitute/MIES

commit b5bf561f0139e194e69376aa4bc1a908bc0ac0e7

Add new trace popup for convenient stimulus set opening
This allows to right-click and select in the databrowser/sweepbrowser to
open the stimulus set of the AD/DA/TTL trace in the wavebuilder.

Packages/MIES/MIES_WaveBuilderPanel.ipf
@@ -9,6 +9,10 @@
 /// @file MIES_WaveBuilderPanel.ipf
 /// @brief __WBP__ Panel for creating stimulus sets
 
+Menu "TracePopup"
+	"Open stimulus set in wavebuilder", /Q, WB_OpenStimulusSetInWaveBuilder()
+End
+
 static StrConstant panel              = "WaveBuilder"
 static StrConstant WaveBuilderGraph   = "WaveBuilder#WaveBuilderGraph"
 static StrConstant AnalysisParamGUI   = "WaveBuilder#AnalysisParamGUI"
@@ -31,6 +35,39 @@ static Constant  STIMULUS_TYPE_TLL           = 2
 static Constant FROM_PANEL_TO_WAVE = 0x1
 static Constant FROM_WAVE_TO_PANEL = 0x2
 /// @}
+
+Function WB_OpenStimulusSetInWaveBuilder()
+
+	string graph, trace, extPanel, waveBuilder
+	variable sweepNo, headstage
+
+	GetLastUserMenuInfo
+	graph = S_graphName
+	trace = S_traceName
+
+	extPanel = BSP_GetPanel(graph)
+
+	if(!WindowExists(extPanel))
+		printf "Context menu option \"%s\" is only useable for the databrowser/sweepbrowser.\r", S_Value
+		ControlWindowToFront()
+		return NaN
+	endif
+
+	sweepNo = str2num(TUD_GetUserData(graph, trace, "sweepNumber"))
+	headstage = str2num(TUD_GetUserData(graph, trace, "headstage"))
+	WAVE/T textualValues = $TUD_GetUserData(graph, trace, "textualValues")
+
+	WAVE/T/Z stimset = GetLastSetting(textualValues, sweepNo, STIM_WAVE_NAME_KEY, DATA_ACQUISITION_MODE)
+
+	if(!WaveExists(stimset))
+		printf "Context menu option \"%s\" could not find the stimulus set of the trace %s.\r", S_Value, trace
+		ControlWindowToFront()
+		return NaN
+	endif
+
+	waveBuilder = WBP_CreateWaveBuilderPanel()
+	PGC_SetAndActivateControl(waveBuilder, "popup_WaveBuilder_SetList", str = stimset[headstage])
+End
 
 Function/S WBP_CreateWaveBuilderPanel()
 

commit bdd985a721312cafcd70d4f3ee1d0efbfbd538b8

WB_OpenStimulusSetInWaveBuilder: Handle non existing stimsets better
When we try showing a stimulus set we might realize that it does not
exist. Bug introduced in b5bf561f (Add new trace popup for convenient
stimulus set opening, 2021-02-18).

We can not do anything when we are in the databrowser. But we can try
loading the stimulus set when we are in the sweepbrowser.

Close #932

Packages/MIES/MIES_AnalysisBrowser.ipf
@@ -1452,6 +1452,19 @@ static Function AB_LoadFromFile(AB_LoadType, [sweepBrowserDFR])
 	return oneValidLoad
 End
 
+Function AB_LoadStimsetForSweep(string device, variable index, variable sweep)
+
+	string dataFolder, discLocation, fileType
+
+	WAVE/T map = GetAnalysisBrowserMap()
+
+	dataFolder   = map[index][%DataFolder]
+	discLocation = map[index][%DiscLocation]
+	fileType     = map[index][%FileType]
+
+	return AB_LoadStimsetFromFile(discLocation, dataFolder, fileType, device, sweep, overwrite = 0)
+End
+
 // @brief common ASSERT statements for AB_LoadSweepFromFile and AB_LoadStimsetFromFile
 static Function AB_LoadFromFileASSERT(discLocation, dataFolder, fileType, device, sweep, overwrite)
 	string discLocation, dataFolder, fileType, device

Packages/MIES/MIES_AnalysisBrowser_SweepBrowser.ipf
@@ -21,6 +21,18 @@ Function/DF SB_GetSweepBrowserFolder(win)
 	return BSP_GetFolder(win, MIES_BSP_PANEL_FOLDER)
 End
 
+Function SB_TranslateSBMapIndexToABMapIndex(string win, variable sbIndex)
+
+	WAVE/T sweepMap = SB_GetSweepBrowserMapFromGraph(win)
+	WAVE/T analysisMap = GetAnalysisBrowserMap()
+
+	// now search the DataFolder from sweepMap in the analyisMap
+	FindValue/TXOP=4/RMD=[][FindDimLabel(analysisMap, COLS, "DataFolder")]/TEXT=(sweepMap[sbIndex][%DataFolder]) analysisMap
+	ASSERT(V_Value >= 0, "Inconsistent AnalysisBrowserMap and SweepBrowserMap")
+
+	return V_row
+End
+
 static Function/DF SB_GetSweepDataPathFromIndex(sweepBrowserDFR, mapIndex)
 	DFREF sweepBrowserDFR
 	variable mapIndex

Packages/MIES/MIES_WaveBuilderPanel.ipf
@@ -38,8 +38,8 @@ static Constant FROM_WAVE_TO_PANEL = 0x2
 
 Function WB_OpenStimulusSetInWaveBuilder()
 
-	string graph, trace, extPanel, waveBuilder
-	variable sweepNo, headstage
+	string graph, trace, extPanel, waveBuilder, stimset, device
+	variable sweepNo, headstage, abIndex, sbIndex
 
 	GetLastUserMenuInfo
 	graph = S_graphName
@@ -57,16 +57,44 @@ Function WB_OpenStimulusSetInWaveBuilder()
 	headstage = str2num(TUD_GetUserData(graph, trace, "headstage"))
 	WAVE/T textualValues = $TUD_GetUserData(graph, trace, "textualValues")
 
-	WAVE/T/Z stimset = GetLastSetting(textualValues, sweepNo, STIM_WAVE_NAME_KEY, DATA_ACQUISITION_MODE)
+	WAVE/T/Z stimsetLBN = GetLastSetting(textualValues, sweepNo, STIM_WAVE_NAME_KEY, DATA_ACQUISITION_MODE)
 
-	if(!WaveExists(stimset))
+	if(!WaveExists(stimsetLBN) || IsNaN(headstage))
 		printf "Context menu option \"%s\" could not find the stimulus set of the trace %s.\r", S_Value, trace
 		ControlWindowToFront()
 		return NaN
 	endif
 
+	stimset = stimsetLBN[headstage]
+
+	WAVE/Z stimsetWave = WB_CreateAndGetStimSet(stimset)
+
+	if(!WaveExists(stimsetWave))
+		if(BSP_IsDataBrowser(graph))
+			printf "Context menu option \"%s\" could not be find the stimulus set %s.", S_Value, stimset
+			ControlWindowToFront()
+			return NaN
+		else
+			// we might need to load the stimset
+			WAVE traceWave = $TUD_GetUserData(graph, trace, "fullPath")
+			DFREF sweepDataDFR = GetWavesDataFolderDFR(traceWave)
+			sbIndex = SB_GetIndexFromSweepDataPath(graph, sweepDataDFR)
+
+			DFREF sweepBrowserDFR = SB_GetSweepBrowserFolder(graph)
+			WAVE/T sweepMap = GetSweepBrowserMap(sweepBrowserDFR)
+
+			abIndex = SB_TranslateSBMapIndexToABMapIndex(graph, sbIndex)
+			device  = sweepMap[sbIndex][%Device]
+			if(AB_LoadStimsetForSweep(device, abIndex, sweepNo))
+				printf "Context menu option \"%s\" could not load the stimulus set %s.", S_Value, stimset
+				ControlWindowToFront()
+				return NaN
+			endif
+		endif
+	endif
+
 	waveBuilder = WBP_CreateWaveBuilderPanel()
-	PGC_SetAndActivateControl(waveBuilder, "popup_WaveBuilder_SetList", str = stimset[headstage])
+	PGC_SetAndActivateControl(waveBuilder, "popup_WaveBuilder_SetList", str = stimset)
 End
 
 Function/S WBP_CreateWaveBuilderPanel()