AllenInstitute/MIES

commit 6a432cb7dc1350d5904ef580dae031b7a4e3e387

Add code to allow recovering deleted sweeps
Sweeps can be deleted manually or due to 9c27dcd (SWS_DeleteDataWaves:
Make it faster, 2020-03-26) when sweep rollback is used.

In case the databrowser was open when that happened we still have the 1D
waves lying around. So we can try to reconstruct the sweep and config
waves from the 1D waves and the LBN entries.

Packages/MIES/MIES_NeuroDataWithoutBorders.ipf
@@ -456,6 +456,8 @@ Function NWB_ExportAllData(nwbVersion, [overrideFilePath, writeStoredTestPulses,
 		list = GetListOfObjects(dfr, DATA_SWEEP_REGEXP)
 		numWaves = ItemsInList(list)
 
+		NWB_CheckForMissingSweeps(panelTitle, ListToTextWave(list, ";"))
+
 		LOG_AddEntry(PACKAGE_MIES, "export", keys = {"device", "#sweeps"}, values = {panelTitle, num2str(numWaves)})
 
 		for(j = 0; j < numWaves; j += 1)
@@ -471,8 +473,6 @@ Function NWB_ExportAllData(nwbVersion, [overrideFilePath, writeStoredTestPulses,
 			sweep = ExtractSweepNumber(name)
 
 			if(!WaveExists(configWave))
-				printf "Sweep %d can not be exported as the config wave is missing.", sweep
-				ControlWindowToFront()
 				continue
 			endif
 
@@ -488,6 +488,30 @@ Function NWB_ExportAllData(nwbVersion, [overrideFilePath, writeStoredTestPulses,
 	endif
 
 	LOG_AddEntry(PACKAGE_MIES, "end", keys = {"size [MiB]"}, values = {num2str(NWB_GetExportedFileSize())})
+End
+
+Function NWB_CheckForMissingSweeps(string panelTitle, WAVE/T sweepNames)
+	WAVE numericalValues = GetLBNumericalValues(panelTitle)
+
+	WAVE sweepsFromLBN = GetSweepsWithSetting(numericalValues, "SweepNum")
+
+	Make/FREE/N=(DimSize(sweepNames, ROWS)) sweepsFromDFR = ExtractSweepNumber(sweepNames[p])
+
+	if(EqualWaves(sweepsfromLBN, sweepsFromDFR, 1) == 1)
+		return 0
+	endif
+
+	printf "The labnotebook and the device data folder from device %s differ in the number of recorded sweeps.\r", panelTitle
+	printf "If you have used sweep rollback, this can be totally normal and expected.\r"
+	printf "In other cases, or if you think that data is unexpectedly missing please see RecreateSweepWaveFromBackupAndLBN().\r"
+	printf "Labnotebook: \r"
+	print sweepsFromLBN
+	printf "Device data folder: \r"
+	print sweepsFromDFR
+
+	ControlWindowToFront()
+
+	return 1
 End
 
 /// @brief Return the file size in MiB of the currently written into NWB file

Packages/MIES/MIES_MiesUtilities.ipf
@@ -7335,3 +7335,366 @@ Function UploadLogFile()
 
 	printf "Successfully uploaded the MIES logfile. Please mention your ticket \"%s\" if you are contacting support.\r", ticket
 End
+
+/// @brief Tries to find deleted sweep and config waves which are still present due to 1D waves from databrowser backups or
+///        labnotebook entries
+///
+/// The result is placed in `root:reconstructed` which is overwritten.
+///
+/// For reconstructing single sweep or config waves, see RecreateSweepWaveFromBackupAndLBN() and
+/// RecreateConfigWaveFromLBN().
+///
+/// Example invocation:
+/// - RecreateMissingSweepAndConfigWaves("ITC18USB_DEV_0", root:MIES:HardwareDevices:ITC18USB:Device0:Data:)
+/// - Inspect the results in `root:reconstructed`
+/// - If you are satisfied, move the waves from root:reconstructed into e.g. `root:MIES:HardwareDevices:ITC18USB:Device0:Data:`
+/// - Proceed with the data as usual
+///
+/// In case the routine throws an assertion, please open an issue so that we can investigate.
+Function RecreateMissingSweepAndConfigWaves(string panelTitle, DFREF deviceDataDFR)
+
+	printf "Trying to resurrect missing sweeps from device %s\r", panelTitle
+
+	variable i, numEntries, sweepNo, samplingInterval
+	string path
+
+	WAVE numericalKeys = GetLBTextualKeys(panelTitle)
+	WAVE textualKeys = GetLBNumericalKeys(panelTitle)
+
+	// now the labnotebooks are upgraded to the latest version
+	WAVE numericalValues = GetLBNumericalValues(panelTitle)
+	WAVE textualValues = GetLBTextualValues(panelTitle)
+
+	WAVE/Z sweepsFromNum = GetSweepsWithSetting(numericalValues, "SweepNum")
+	WAVE/Z sweepsFromText = GetSweepsWithSetting(textualValues, "SweepNum")
+
+	// consistency check
+	ASSERT(WaveExists(sweepsFromNum) && WaveExists(sweepsFromText) && EqualWaves(sweepsFromNum, sweepsFromText, 1), "Unexpected numerical/textual LBN entries")
+
+	WAVE sweeps = sweepsFromNum
+
+	Duplicate/FREE sweeps, missingSweep, missingConfig
+
+	missingSweep[]  = 1
+	missingConfig[] = 1
+
+	numEntries = DimSize(sweeps, ROWS)
+	for(i = 0; i < numEntries; i += 1)
+		sweepNo = sweepsFromNum[i]
+
+		WAVE/Z/SDFR=deviceDataDFR sweepWave = $GetSweepWaveName(sweepNo)
+		Wave/Z/SDFR=deviceDataDFR configWave = $GetConfigWaveName(sweepNo)
+
+		if(WaveExists(sweepWave) && WaveExists(configWave))
+			missingSweep[i]  = 0
+			missingConfig[i] = 0
+			continue
+		endif
+
+		if(WaveExists(sweepWave))
+			missingSweep[i] = 0
+		else
+			printf "Sweep %d is missing its sweep wave\r", sweepNo
+		endif
+
+		if(WaveExists(configWave))
+			missingConfig[i] = 0
+		else
+			printf "Sweep %d is missing its config wave\r", sweepNo
+		endif
+	endfor
+
+	DFREF dest = createDFWithAllParents("root:reconstructed")
+	MoveToTrash(dfr = dest)
+	DFREF dest = createDFWithAllParents("root:reconstructed")
+
+	// now try to reconstruct the sweep and config waves from the X_XXXX folders
+	for(i = 0; i < numEntries; i += 1)
+		if(!missingSweep[i] && !missingConfig[i])
+			continue
+		endif
+
+		sweepNo = sweepsFromNum[i]
+
+		printf "Trying to recontruct sweep %d\r", sweepNo
+
+		if(missingSweep[i])
+			WAVE/Z sweepWave = RecreateSweepWaveFromBackupAndLBN(numericalValues, textualValues, sweepNo, deviceDataDFR)
+
+			if(!WaveExists(sweepWave))
+				printf "Could not reconstruct sweep\r"
+				continue
+			endif
+		else
+			WAVE/Z sweepWave
+		endif
+
+		if(missingConfig[i])
+			WAVE configWave = RecreateConfigWaveFromLBN(panelTitle, numericalValues, textualValues, sweepNo)
+		else
+			WAVE/Z configWave
+		endif
+
+		if(WaveExists(sweepWave) && WaveExists(configWave))
+			ASSERT(IsValidSweepAndConfig(sweepWave, configWave), "Recreation created incompatible sweep and config waves")
+		endif
+
+		if(WaveExists(configWave))
+			MoveWave configWave, dest:$GetConfigWaveName(sweepNo)
+		endif
+
+		if(WaveExists(sweepWave))
+			MoveWave sweepWave, dest:$GetSweepWaveName(sweepNo)
+		endif
+
+		printf "Reconstructed successfully.\r"
+	endfor
+End
+
+/// @brief Try recreating a 2D DAQ sweep wave from its databrowser backup
+///
+/// @return $"" if reconstruction failed or a free wave with the sweep data and correct metadata
+Function/WAVE RecreateSweepWaveFromBackupAndLBN(WAVE numericalValues, WAVE/T textualValues, variable sweepNo, DFREF deviceDataDFR)
+	string path
+	variable samplingInterval
+
+	path = GetSingleSweepFolderAsString(deviceDataDFR, sweepNo)
+
+	if(!DataFolderExists(path))
+		return $""
+	endif
+
+	DFREF singleSweepFolder = $path
+
+	WAVE/WAVE/Z DAWaves, ADWaves, TTLWaves
+	WAVE/Z DAChans, ADChans, TTLChans
+
+	[DAChans, DAWaves] = GetSingleSweepWaves(singleSweepFolder, XOP_CHANNEL_TYPE_DAC)
+	[ADChans, ADWaves] = GetSingleSweepWaves(singleSweepFolder, XOP_CHANNEL_TYPE_ADC)
+	[TTLChans, TTLWaves] = GetSingleSweepWaves(singleSweepFolder, XOP_CHANNEL_TYPE_TTL)
+
+	// check that we have found all 1D waves of one sweep
+	WAVE DAFromLBN = GetActiveChannels(numericalValues, textualValues, sweepNo, XOP_CHANNEL_TYPE_DAC)
+	WAVE DAFromLBNWoNaN = ZapNaNs(DAFromLBN)
+
+	WAVE ADFromLBN = GetActiveChannels(numericalValues, textualValues, sweepNo, XOP_CHANNEL_TYPE_ADC)
+	WAVE ADFromLBNWoNaN = ZapNaNs(ADFromLBN)
+
+	WAVE/Z TTLFromLBN = GetActiveChannels(numericalValues, textualValues, sweepNo, XOP_CHANNEL_TYPE_TTL, TTLmode = TTL_HARDWARE_CHANNEL)
+
+	if(WaveExists(TTLFromLBN))
+		WAVE/Z TTLFromLBNWoNaN = ZapNaNs(TTLFromLBN)
+	endif
+
+	if(!WaveExists(TTLFromLBNWoNaN))
+		Make/FREE/N=0 TTLFromLBNWoNaN
+	endif
+
+	if(DimSize(DAFromLBNWoNaN, ROWS) != DimSize(DAChans, ROWS)       \
+	   || DimSize(ADFromLBNWoNaN, ROWS) != DimSize(ADChans, ROWS)    \
+	   || DimSize(TTLFromLBNWoNaN, ROWS) != DimSize(TTLChans, ROWS))
+	   return $""
+	endif
+
+	Make/FREE/N=(0, 0) sweepWave
+
+	// Add DA/AD data with increasing channel numbers
+	AddToSweepWave(DAWaves, DAChans, sweepWave)
+	AddToSweepWave(ADWaves, ADChans, sweepWave)
+	AddToSweepWave(TTLWaves, TTLChans, sweepWave)
+
+	// Attach the wave note
+	SVAR txt = singleSweepFolder:note
+	Note/K sweepWave, txt
+
+	// add ms scale
+	samplingInterval = GetSamplingIntervalFromLBN(numericalValues, sweepNo)
+	SetScale/P x, 0, samplingInterval, "ms", sweepWave
+
+	ASSERT(IsValidSweepWave(sweepWave), "Recreation created an invalid wave")
+
+	return sweepWave
+End
+
+/// @brief Try recreating the DAQ config wave from labnotebook entries
+///
+/// @return `$""` if recreation failed or a free wave on success.
+Function/WAVE RecreateConfigWaveFromLBN(string panelTitle, WAVE numericalValues, WAVE textualValues, variable sweepNo)
+
+	variable samplingInterval
+
+	// ensure we start with a fresh config wave
+	WAVE configWave = GetDAQConfigWave(panelTitle)
+	MoveToTrash(wv=configWave)
+
+	WAVE configWave = GetDAQConfigWave(panelTitle)
+	Redimension/N=(0, -1) configWave
+
+	ASSERT(GetWaveVersion(configWave) == 2, "Reconstruction might need adaptation for new config wave version")
+
+	AddChannelPropertiesFromLBN(numericalValues, textualValues, sweepNo, configWave, XOP_CHANNEL_TYPE_DAC)
+	AddChannelPropertiesFromLBN(numericalValues, textualValues, sweepNo, configWave, XOP_CHANNEL_TYPE_ADC)
+	AddChannelPropertiesFromLBN(numericalValues, textualValues, sweepNo, configWave, XOP_CHANNEL_TYPE_TTL)
+
+	samplingInterval = GetSamplingIntervalFromLBN(numericalValues, sweepNo)
+
+	configWave[][%SamplingInterval] = samplingInterval * 1000
+
+	// always 0, see DC_PlaceDataInDAQConfigWave
+	configWave[][%DecimationMode] = 0
+
+	// nearly always 0, except for PSQ_Ramp, use -1 to be really honest
+	// as we don't really know the used offset anymore
+	configWave[][%Offset] = -1
+
+	AddDAQChannelTypeFromLBN(numericalValues, textualValues, sweepNo, configWave)
+	AddChannelUnitFromLBN(numericalValues, textualValues, sweepNo, configWave)
+
+	ASSERT(IsValidSweepWave(configWave), "Recreation created an invalid wave")
+
+	return configWave
+End
+
+static Function GetSamplingIntervalFromLBN(WAVE numericalValues, variable sweepNo)
+	variable samplingInterval
+
+	samplingInterval = GetLastSettingIndep(numericalValues, sweepNo, "Sampling interval", DATA_ACQUISITION_MODE)
+
+	// convert to integer as that is stored in the config wave
+	return round(samplingInterval * 1000) / 1000
+End
+
+/// @brief Set `ChannelNumber` and `ChannelType` in configWave
+static Function AddChannelPropertiesFromLBN(WAVE numericalValues, WAVE textualValues, variable sweepNo, WAVE configWave, variable channelType)
+	variable i, numEntries, offset
+
+	WAVE/Z channelLBN = GetActiveChannels(numericalValues, textualValues, sweepNo, channelType, TTLmode = TTL_HARDWARE_CHANNEL)
+
+	if(!WaveExists(channelLBN))
+		// nothing to do
+		return NaN
+	endif
+
+	WAVE/Z channelLBNWoNaN = ZapNaNs(channelLBN)
+
+	if(!WaveExists(channelLBNWoNaN))
+		// nothing to do
+		return NaN
+	endif
+
+	// sort channelNumbers ascending so that we start with the smallest channel numbers first
+	Sort channelLBNWoNaN, channelLBNWoNaN
+
+	numEntries = DimSize(channelLBNWoNaN, ROWS)
+
+	offset = DimSize(configWave, ROWS)
+	Redimension/N=(offset + numEntries, -1) configWave
+
+	for(i = 0; i < numEntries; i += 1)
+		configWave[offset + i][%ChannelNumber] = channelLBNWoNaN[i]
+		configWave[offset + i][%ChannelType]   = channelType
+	endfor
+End
+
+/// @brief Set `DAQChannelType` in configWave
+static Function AddDAQChannelTypeFromLBN(WAVE numericalValues, WAVE textualValues, variable sweepNo, WAVE configWave)
+	variable i, numEntries, headstage, channelType, index
+
+	Make/FREE validChannelTypes = {DAQ_CHANNEL_TYPE_DAQ, DAQ_CHANNEL_TYPE_TP, DAQ_CHANNEL_TYPE_UNKOWN}
+
+	numEntries = DimSize(configWave, ROWS)
+	for(i = 0; i < numEntries; i += 1)
+		WAVE/Z setting
+
+		switch(configWave[i][%ChannelType])
+			case XOP_CHANNEL_TYPE_DAC:
+				[setting, index] = GetLastSettingChannel(numericalValues, $"", sweepNo, "DA ChannelType", configWave[i][%ChannelNumber], configWave[i][%ChannelType], DATA_ACQUISITION_MODE)
+				channelType = setting[index]
+				break
+			case XOP_CHANNEL_TYPE_ADC:
+				[setting, index] = GetLastSettingChannel(numericalValues, $"", sweepNo, "AD ChannelType", configWave[i][%ChannelNumber], configWave[i][%ChannelType], DATA_ACQUISITION_MODE)
+				channelType = setting[index]
+				break
+			case XOP_CHANNEL_TYPE_TTL:
+				channelType = DAQ_CHANNEL_TYPE_DAQ
+				break
+		endswitch
+
+		ASSERT(IsFinite(GetRowIndex(validChannelTypes, val = channelType)), "Invalid channel type")
+
+		configWave[i][%DAQChannelType] = channelType
+	endfor
+End
+
+/// @brief Add wave note entry #CHANNEL_UNIT_KEY for the used channel units
+static Function AddChannelUnitFromLBN(WAVE numericalValues, WAVE/T textualValues, variable sweepNo, WAVE configWave)
+	variable i, numEntries, index
+	string key
+	string unitList = ""
+
+	numEntries = DimSize(configWave, ROWS)
+	for(i = 0; i < numEntries; i += 1)
+
+		if(configWave[i][%ChannelType] == XOP_CHANNEL_TYPE_TTL)
+			// done
+			break
+		endif
+
+		key = StringFromList(configWave[i][%ChannelType], XOP_CHANNEL_NAMES) + " Unit"
+		WAVE/Z setting
+		[setting, index] = GetLastSettingChannel(numericalValues, textualValues, sweepNo, key, configWave[i][%ChannelNumber], configWave[i][%ChannelType], DATA_ACQUISITION_MODE)
+
+		WAVE/T settingText = setting
+		unitList = AddListItem(settingText[index], unitList, ",", inf)
+	endfor
+
+	AddEntryIntoWaveNoteAsList(configWave, CHANNEL_UNIT_KEY, str = unitList, replaceEntry = 1)
+End
+
+/// @brief Return waves with the channel numbers and references to 1D waves from the `X_XXXX` folders
+static Function [WAVE channelNumbers, WAVE/WAVE existingWaves] GetSingleSweepWaves(DFREF singleSweepFolder, variable channelType)
+
+	variable numEntries, i, channelNumber, size
+
+	WAVE/WAVE waves = GetDAQDataSingleColumnWaves(singleSweepFolder, channelType)
+
+	Make/FREE/N=0 channelNumbers
+	Make/FREE/N=0/WAVE existingWaves
+
+	numEntries = DimSize(waves, ROWS)
+	for(i = 0; i < numEntries; i += 1)
+		if(!WaveExists(waves[i]))
+			continue
+		endif
+
+		channelNumber = str2num(StringFromList(1, NameOfWave(waves[i]), "_"))
+
+		size = DimSize(channelNumbers, ROWS)
+		Redimension/N=(size + 1) channelNumbers, existingWaves
+
+		channelNumbers[size] = channelNumber
+		existingWaves[size]  = waves[i]
+	endfor
+
+	return [channelNumbers, existingWaves]
+End
+
+/// @brief Add the returned 1D waves from GetSingleSweepWaves() into sweepWave
+static Function AddToSweepWave(WAVE/WAVE channelWaves, Wave channelNumbers, WAVE sweepWave)
+	variable i, numEntries
+
+	// sort channelNumbers and channelWaves ascending so that we start with the smallest channel numbers first
+	Sort channelNumbers, channelNumbers, channelWaves
+
+	numEntries = DimSize(channelWaves, ROWS)
+	for(i = 0; i < numEntries; i += 1)
+		WAVE wv = channelWaves[i]
+
+		if(i == 0 && DimSize(sweepWave, ROWS) == 0)
+			// adapt sweep data type
+			Redimension/Y=(WaveType(wv)) sweepWave
+		endif
+
+		Concatenate {wv}, sweepWave
+	endfor
+End

commit fd908e4d2666259240b78a75c60022c7042ee2d3

NWB_CheckForMissingSweeps: Handle no sweeps gracefully
Calling NWB_ExportAllData where at least one device has content but no
sweeps currently leads to NWB_CheckForMissingSweeps bugging out as
GetSweepsWithSetting returns a null wave.

Bug introduced in 6a432cb7 (Add code to allow recovering deleted sweeps,
2021-04-19).

The test ExportIntoNWB does trigger that in the following way:
- Acquire TP, this means we then have one device with content
- Enable "Export into NWB" after each sweep
- Acquire TP again, now NWB_PrepareExport is called for a device with content

Packages/MIES/MIES_NeuroDataWithoutBorders.ipf
@@ -501,9 +501,17 @@ End
 Function NWB_CheckForMissingSweeps(string panelTitle, WAVE/T sweepNames)
 	WAVE numericalValues = GetLBNumericalValues(panelTitle)
 
-	WAVE sweepsFromLBN = GetSweepsWithSetting(numericalValues, "SweepNum")
+	WAVE/Z sweepsFromLBN = GetSweepsWithSetting(numericalValues, "SweepNum")
 
-	Make/FREE/N=(DimSize(sweepNames, ROWS)) sweepsFromDFR = ExtractSweepNumber(sweepNames[p])
+	if(!WaveExists(sweepsFromLBN))
+		Make/FREE/N=(0) sweepsFromLBN
+	endif
+
+	if(DimSize(sweepNames, ROWS) == 0)
+		Make/FREE/N=(0) sweepsFromDFR
+	else
+		Make/FREE/N=(DimSize(sweepNames, ROWS)) sweepsFromDFR = ExtractSweepNumber(sweepNames[p])
+	endif
 
 	if(EqualWaves(sweepsfromLBN, sweepsFromDFR, 1) == 1)
 		return 0

Packages/Testing-MIES/UTF_BasicHardwareTests.ipf
@@ -4676,3 +4676,53 @@ Function ExportStimsetsAndRoundtripThem([variable var])
 		CHECK_EQUAL_WAVES(oldWave, newWave)
 	endfor
 End
+
+/// UTF_TD_GENERATOR HardwareMain#DeviceNameGeneratorMD1
+Function ExportIntoNWB([str])
+	string str
+
+	string filePathExport, experimentName
+
+	STRUCT DAQSettings s
+	InitDAQSettingsFromString(s, "MD1_RA0_I0_L0_BKG_1")
+
+	CloseNwBFile()
+	PathInfo home
+	CHECK(V_Flag)
+
+	experimentName = GetExperimentName()
+	CHECK(cmpstr(experimentName, UNTITLED_EXPERIMENT))
+
+	DeleteFile/Z S_path + experimentName + ".nwb"
+
+	AcquireData(s, str, startTPInstead = 1)
+
+	CtrlNamedBackGround StopTPAfterFiveSeconds, start=(ticks + TP_DURATION_S * 60), period=1, proc=StopTPAfterFiveSeconds_IGNORE
+End
+
+Function ExportIntoNWB_REENTRY([str])
+	string str
+	variable sweepNo
+
+	CHECK_EQUAL_VAR(GetSetVariable(str, "SetVar_Sweep"), 0)
+
+	sweepNo = AFH_GetLastSweepAcquired(str)
+	CHECK_EQUAL_VAR(sweepNo, NaN)
+
+	RegisterReentryFunction(GetRTStackInfo(1))
+
+	PGC_SetAndActivateControl(str, "Check_Settings_NwbExport", val = CHECKBOX_SELECTED)
+	PGC_SetAndActivateControl(str, "StartTestPulseButton")
+
+	CtrlNamedBackGround StopTPAfterFiveSeconds, start=(ticks + TP_DURATION_S * 60), period=1, proc=StopTPAfterFiveSeconds_IGNORE
+End
+
+Function ExportIntoNWB_REENTRY_REENTRY([str])
+	string str
+	variable sweepNo
+
+	CHECK_EQUAL_VAR(GetSetVariable(str, "SetVar_Sweep"), 0)
+
+	sweepNo = AFH_GetLastSweepAcquired(str)
+	CHECK_EQUAL_VAR(sweepNo, NaN)
+End