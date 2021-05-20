AllenInstitute/MIES

commit e2cfdeb558c6de69d4cd7f3114f5eac0db0091d5

Neurodata Without Borders 2.0.1.
Update to Neurodata Without Borders (NWB) Version 2.0.1.

- The NWBv2 specifications are read from json files in the IPNWB
  repository and written to the nwb file during save.
- The `IVS PatchClampAutomation` module defaults to NWBv1 for export.
- History is added to /general/data_collection
- Use SweepTable for AnalysisBrowser loading of nwb v2 sweep_number and
  TimeSeries paths.

Packages/MIES/MIES_DAEphys.ipf
@@ -450,6 +450,7 @@ Function DAP_EphysPanelStartUpSettings()
 	CheckBox check_Settings_TPAfterDAQ WIN = $panelTitle, value= 0
 
 	CheckBox Check_Settings_NwbExport WIN = $panelTitle,value= 0
+	PopupMenu Popup_Settings_NwbVersion WIN = $panelTitle, mode=2, popvalue="2"
 
 	PopupMenu Popup_Settings_DecMethod, mode=2, popvalue="MinMax"
 	SetVariable setvar_Settings_DecMethodFac, WIN = $panelTitle, value = _NUM:-1
@@ -2464,7 +2465,7 @@ Function DAP_CheckSettings(panelTitle, mode)
 	endfor
 
 	if(DAG_GetNumericalValue(panelTitle, "Check_Settings_NwbExport"))
-		NWB_PrepareExport()
+		NWB_PrepareExport(DAG_GetNumericalValue(panelTitle, "Popup_Settings_NwbVersion"))
 	endif
 
 	return 0

Packages/MIES/MIES_SweepSaving.ipf
@@ -45,7 +45,7 @@ Function SWS_SaveAcquiredData(panelTitle, [forcedStop])
 	ED_createWaveNoteTags(panelTitle, sweepNo)
 
 	if(DAG_GetNumericalValue(panelTitle, "Check_Settings_NwbExport"))
-		NWB_AppendSweep(panelTitle, sweepWave, configWave, sweepNo)
+		NWB_AppendSweep(panelTitle, sweepWave, configWave, sweepNo, DAG_GetNumericalValue(panelTitle, "Popup_Settings_NwbVersion"))
 	endif
 
 	if(!forcedStop)

.gitignore
@@ -1,4 +1,9 @@
+# Documentation files
 Packages/doc/html
+Packages/doc/IPNWB
+Packages/doc/doctrees
+Packages/doc/sphinx-output.log
+
 Packages/MIES/backup
 *.orig
 *.rej

Packages/MIES/MIES_AnalysisBrowser.ipf
@@ -570,8 +570,7 @@ End
 
 /// @brief Analyse data in NWB file and sort as sweeps.
 ///
-/// Function uses source attribute of /acquisition/timeseries
-///                               and /stimulus/presentation
+/// @todo: Update this function for the use with SweepTable
 ///
 /// @param discLocation  location of NWB File on Disc.
 ///                      ID in AnalysisBrowserMap
@@ -580,7 +579,7 @@ End
 static Function AB_LoadSweepsFromNWB(discLocation, dataFolder, device)
 	string discLocation, dataFolder, device
 
-	variable h5_fileID, h5_groupID
+	variable h5_fileID, nwbVersion
 	string channelList
 
 	Wave/I sweeps = GetAnalysisChannelSweepWave(dataFolder, device)
@@ -588,34 +587,32 @@ static Function AB_LoadSweepsFromNWB(discLocation, dataFolder, device)
 	// open hdf5 file
 	h5_fileID = IPNWB#H5_OpenFile(discLocation)
 
-	// load from /acquisition/timeseries
-	channelList = IPNWB#ReadAcquisition(h5_fileID)
-	h5_groupID  = IPNWB#OpenAcquisition(h5_fileID)
+	// load from /acquisition
+	nwbVersion = IPNWB#GetNWBMajorVersion(IPNWB#ReadNWBVersion(h5_fileID))
+	channelList = IPNWB#ReadAcquisition(h5_fileID, nwbVersion)
 	Wave/T acquisition = GetAnalysisChannelAcqWave(dataFolder, device)
-	AB_StoreChannelsBySweep(h5_groupID, channelList, sweeps, acquisition)
-	HDF5CloseGroup/Z h5_groupID
+	AB_StoreChannelsBySweep(h5_fileID, nwbVersion, channelList, sweeps, acquisition)
 
 	// load from /stimulus/presentation
 	channelList = IPNWB#ReadStimulus(h5_fileID)
-	h5_groupID  = IPNWB#OpenStimulus(h5_fileID)
 	Wave/T stimulus = GetAnalysisChannelStimWave(dataFolder, device)
-	AB_StoreChannelsBySweep(h5_groupID, channelList, sweeps, stimulus)
-	HDF5CloseGroup/Z h5_groupID
+	AB_StoreChannelsBySweep(h5_fileID, nwbVersion, channelList, sweeps, stimulus)
 
 	// close hdf5 file
 	IPNWB#H5_CloseFile(h5_fileID)
 End
 
 /// @brief Store channelList in storage wave according to index in sweeps wave
-static Function AB_StoreChannelsBySweep(groupID, channelList, sweeps, storage)
-	variable groupID
+///
+/// @todo Update this function for the use with SweepTable
+static Function AB_StoreChannelsBySweep(groupID, nwbVersion, channelList, sweeps, storage)
+	variable groupID, nwbVersion
 	string channelList
 	Wave/I sweeps
 	Wave/T storage
 
-	variable numChannels, numSweeps, i
+	variable numChannels, numSweeps, i, sweepNo, sweep_table_id
 	string channelString
-	STRUCT IPNWB#ReadChannelParams channel
 
 	numChannels = ItemsInList(channelList)
 	numSweeps = GetNumberFromWaveNote(sweeps, NOTE_INDEX)
@@ -623,15 +620,28 @@ static Function AB_StoreChannelsBySweep(groupID, channelList, sweeps, storage)
 	EnsureLargeEnoughWave(storage, minimumSize = numSweeps, dimension = ROWS)
 	storage = ""
 
+	WAVE/Z SweepTableNumber
+	WAVE/Z/T SweepTableSeries
+	if(nwbVersion == 2)
+		[SweepTableNumber, SweepTableSeries] = IPNWB#LoadSweepTable(groupID, nwbVersion)
+	endif
+
 	for(i = 0; i < numChannels; i += 1)
 		channelString = StringFromList(i, channelList)
-		IPNWB#LoadSourceAttribute(groupID, channelString, channel)
-		FindValue/I=(channel.sweep)/S=0 sweeps
+		if(nwbVersion == 2)
+			WAVE indices = FindIndizes(SweepTableSeries, col = 0, str = channelString)
+			ASSERT(DimSize(indices, ROWS) == 1, "Invalid Amount of Sweep Number Associated in " + channelString)
+			sweepNo = SweepTableNumber[indices[0]]
+		else
+			sweepNo = IPNWB#LoadSweepNumber(groupID, channelString, nwbVersion)
+		endif
+		FindValue/I=(sweepNo)/S=0 sweeps
+		ASSERT(isFinite(sweepNo), "Invalid Sweep Number Associated in " + channelString)
 		if(V_Value == -1)
 			numSweeps += 1
 			EnsureLargeEnoughWave(sweeps, minimumSize = numSweeps, dimension = ROWS, initialValue = -1)
 			EnsureLargeEnoughWave(storage, minimumSize = numSweeps, dimension = ROWS)
-			sweeps[numSweeps - 1] = channel.sweep
+			sweeps[numSweeps - 1] = sweepNo
 			storage[numSweeps - 1] = AddListItem(channelString, "")
 		else
 			storage[V_Value] = AddListItem(channelString, storage[V_Value])
@@ -932,11 +942,6 @@ static Function/S AB_LoadLabNotebookFromNWB(discLocation)
 		IPNWB#H5_CloseFile(h5_fileID)
 		return ""
 	endtry
-
-	if(!IPNWB#CheckIntegrity(h5_fileID))
-		IPNWB#H5_CloseFile(h5_fileID)
-		return ""
-	endif
 
 	notebookList = IPNWB#ReadLabNoteBooks(h5_fileID)
 	h5_notebooksID = IPNWB#H5_OpenGroup(h5_fileID, "/general/labnotebook")
@@ -1567,7 +1572,7 @@ static Function AB_LoadSweepFromNWB(discLocation, sweepDFR, device, sweep)
 	variable sweep
 
 	string channelList
-	variable h5_fileID, h5_groupID, numSweeps
+	variable h5_fileID, h5_groupID, numSweeps, version
 
 	Wave/T nwb = AB_GetMap(discLocation)
 
@@ -1593,12 +1598,13 @@ static Function AB_LoadSweepFromNWB(discLocation, sweepDFR, device, sweep)
 
 	// open NWB file
 	h5_fileID = IPNWB#H5_OpenFile(discLocation)
+	version = IPNWB#GetNWBMajorVersion(IPNWB#ReadNWBVersion(h5_fileID))
 
 	// load acquisition
 	Wave/T acquisition = GetAnalysisChannelAcqWave(nwb[%DataFolder], device)
 	channelList = acquisition[V_Value]
-	h5_groupID = IPNWB#OpenAcquisition(h5_fileID)
-	if(AB_LoadSweepFromNWBgeneric(h5_groupID, channelList, sweepDFR, configSweep))
+	h5_groupID = IPNWB#OpenAcquisition(h5_fileID, version)
+	if(AB_LoadSweepFromNWBgeneric(h5_groupID, version, channelList, sweepDFR, configSweep))
 		return 1
 	endif
 
@@ -1606,7 +1612,7 @@ static Function AB_LoadSweepFromNWB(discLocation, sweepDFR, device, sweep)
 	Wave/T stimulus = GetAnalysisChannelStimWave(nwb[%DataFolder], device)
 	channelList = stimulus[V_Value]
 	h5_groupID = IPNWB#OpenStimulus(h5_fileID)
-	if(AB_LoadSweepFromNWBgeneric(h5_groupID, channelList, sweepDFR, configSweep))
+	if(AB_LoadSweepFromNWBgeneric(h5_groupID, version, channelList, sweepDFR, configSweep))
 		return 1
 	endif
 
@@ -1616,8 +1622,8 @@ static Function AB_LoadSweepFromNWB(discLocation, sweepDFR, device, sweep)
 	return 0
 End
 
-static Function AB_LoadSweepFromNWBgeneric(h5_groupID, channelList, sweepDFR, configSweep)
-	variable h5_groupID
+static Function AB_LoadSweepFromNWBgeneric(h5_groupID, nwbVersion, channelList, sweepDFR, configSweep)
+	variable h5_groupID, nwbVersion
 	string channelList
 	DFREF sweepDFR
 	Wave/I configSweep
@@ -1631,10 +1637,7 @@ static Function AB_LoadSweepFromNWBgeneric(h5_groupID, channelList, sweepDFR, co
 
 	for(i = 0; i < numChannels; i += 1)
 		channel = StringFromList(i, channelList)
-
-		// use AnalyseChannelName as a fallback if properties from the source attribute are missing
 		IPNWB#AnalyseChannelName(channel, p)
-		IPNWB#LoadSourceAttribute(h5_groupID, channel, p)
 
 		switch(p.channelType)
 			case ITC_XOP_CHANNEL_TYPE_DAC:
@@ -1645,7 +1648,7 @@ static Function AB_LoadSweepFromNWBgeneric(h5_groupID, channelList, sweepDFR, co
 				break
 			case ITC_XOP_CHANNEL_TYPE_ADC:
 				channelName = "AD"
-				wave loaded = IPNWB#LoadTimeseries(h5_groupID, channel)
+				wave loaded = IPNWB#LoadTimeseries(h5_groupID, channel, nwbVersion)
 				channelName += "_" + num2str(p.channelNumber)
 				fakeConfigWave = 1
 				break
@@ -1681,6 +1684,7 @@ static Function AB_LoadSweepFromNWBgeneric(h5_groupID, channelList, sweepDFR, co
 			default:
 				ASSERT(0, "unknown channel type " + num2str(p.channelType))
 		endswitch
+		ASSERT(WaveExists(loaded), "No Wave loaded")
 
 		if(waveNoteLoaded == 0)
 			SVAR/Z test = sweepDFR:note

Packages/MIES/MIES_Constants.ipf
@@ -666,6 +666,7 @@ Constant AMPLIFIER_CONNECTION_MCC_FAILED = 2 ///< calling MCC_SelectMultiClamp70
 
 /// Additional entry in the NWB source attribute for TTL data
 StrConstant NWB_SOURCE_TTL_BIT = "TTLBit"
+StrConstant IPNWB_PLACEHOLDER = "PLACEHOLDER"
 
 /// @name Convenience constants for DAP_UpdateClampmodeTabs() and DAP_ChangeHeadStageMode()
 /// @anchor MCCSyncOverrides
@@ -793,6 +794,7 @@ StrConstant PRESSURE_STARTSEAL      = "Initial Seal Pressure (psi)"
 StrConstant PRESSURE_MAXSEAL        = "Maximum Seal Pressure (psi)"
 StrConstant TP_AFTER_DAQ            = "Activate TP after DAQ"
 StrConstant EXPORT_NWB              = "Export to NWB"
+StrConstant NWB_VERSION             = "NWB Version"
 StrConstant APPEND_ASYNC            = "Append Asynchronous reading to wave note"
 StrConstant SYNC_MIES_MCC           = "Sync MIES to MCC"
 StrConstant ENABLE_I_EQUAL_ZERO     = "Switch clamp modes via I equal zero"

Packages/MIES/MIES_DAEphys_Macro.ipf
@@ -3683,6 +3683,11 @@ Window DA_Ephys() : Panel
 	CheckBox Check_Settings_NwbExport,userdata(ResizeControlsInfo) += A"zzzzzzzzzzzz!!#u:Du]k<zzzzzzzzzzz"
 	CheckBox Check_Settings_NwbExport,userdata(ResizeControlsInfo) += A"zzz!!#u:Du]k<zzzzzzzzzzzzzz!!!"
 	CheckBox Check_Settings_NwbExport,value= 0
+	PopupMenu Popup_Settings_NwbVersion,pos={139.00,231.00},size={69.00,19.00},proc=DAP_PopMenuProc_UpdateGuiState,title="version"
+	PopupMenu Popup_Settings_NwbVersion,help={"Set the NWB Version of the export when using triggered export from DA_Ephys"}
+	PopupMenu Popup_Settings_NwbVersion,userdata(tabcontrol)=  "ADC"
+	PopupMenu Popup_Settings_NwbVersion,userdata(tabnum)=  "5"
+	PopupMenu Popup_Settings_NwbVersion,mode=2,popvalue="2",value= #"\"1;2\""
 	SetVariable setvar_DataAcq_OnsetDelayUser,pos={296.00,656.00},size={167.00,18.00},bodyWidth=50,disable=1,proc=DAP_SetVar_UpdateGuiState,title="User onset delay (ms)"
 	SetVariable setvar_DataAcq_OnsetDelayUser,help={"A global parameter that delays the onset time of a set after the initiation of data acquistion. Data acquisition start time is NOT delayed. Useful when set(s) have insufficient baseline epoch."}
 	SetVariable setvar_DataAcq_OnsetDelayUser,userdata(tabnum)=  "0"

Packages/MIES/MIES_ExperimentConfig.ipf
@@ -407,6 +407,8 @@ static Function ExpConfig_DAEphysSettings(panelTitle, UserSettings)
 	PGC_SetAndActivateControl(panelTitle,"check_Settings_TP_SaveTP", val = str2numSafe(UserSettings[V_value][%SettingValue]))
 	FindValue /TXOP = 4 /TEXT = EXPORT_NWB UserSettings
 	PGC_SetAndActivateControl(panelTitle,"Check_Settings_NwbExport", val = str2numSafe(UserSettings[V_value][%SettingValue]))
+	FindValue /TXOP = 4 /TEXT = NWB_VERSION UserSettings
+	PGC_SetAndActivateControl(panelTitle,"Popup_Settings_NwbVersion", val = str2numSafe(UserSettings[V_value][%SettingValue]))
 	FindValue /TXOP = 4 /TEXT = APPEND_ASYNC UserSettings
 	PGC_SetAndActivateControl(panelTitle,"Check_Settings_Append", val = str2numSafe(UserSettings[V_value][%SettingValue]))
 	FindValue /TXOP = 4 /TEXT = SYNC_MIES_MCC UserSettings

Packages/MIES/MIES_IVSCC.ipf
@@ -9,6 +9,7 @@
 /// @file MIES_IVSCC.ipf
 /// @brief __IVS__ Routines for IVSCC/PatchSeq automation
 
+static Constant    IVS_DEFAULT_NWBVERSION = 1
 static Constant    IVS_DEFAULT_HEADSTAGE  = 0
 static StrConstant IVS_DEFAULT_PANELTITLE = "ITC18USB_Dev_0"
 
@@ -436,7 +437,7 @@ Function IVS_ExportAllData(filePath)
 
 	printf "Saving experiment data in NWB format to %s\r", filePath
 
-	NWB_ExportAllData(overrideFilePath = filePath)
+	NWB_ExportAllData(IVS_DEFAULT_NWBVERSION, overrideFilePath = filePath)
 	CloseNWBFile()
 End
 

commit d7e4c3c3ad458104f61472ced5276f0df46d6099

DAEphys: Read NWB version correctly for per-sweep NWB export
We need to read the popup menu value and not it's index. Bug introduced
in e2cfdeb5 (Neurodata Without Borders 2.0.1., 2019-06-04).

The fallout from this bug is that when you select NWB v1 it does not
work, and for NWB v2 you export into NWB v1 instead.

Packages/MIES/MIES_DAEphys.ipf
@@ -2548,7 +2548,7 @@ Function DAP_CheckSettings(panelTitle, mode)
 	endif
 
 	if(DAG_GetNumericalValue(panelTitle, "Check_Settings_NwbExport"))
-		NWB_PrepareExport(DAG_GetNumericalValue(panelTitle, "Popup_Settings_NwbVersion"))
+		NWB_PrepareExport(str2num(DAG_GetTextualValue(panelTitle, "Popup_Settings_NwbVersion")))
 	endif
 
 	return 0

Packages/MIES/MIES_DAEphys_GuiState.ipf
@@ -167,6 +167,8 @@ End
 
 /// @brief Query a control value from the numerical gui state wave
 ///
+/// This does return the zero-based *index* for PopupMenues.
+///
 /// Convienience wrapper to make the call sites nicer.
 ///
 /// @param panelTitle device

Packages/MIES/MIES_SweepSaving.ipf
@@ -55,7 +55,7 @@ Function SWS_SaveAcquiredData(panelTitle, [forcedStop])
 	ED_createWaveNoteTags(panelTitle, sweepNo)
 
 	if(DAG_GetNumericalValue(panelTitle, "Check_Settings_NwbExport"))
-		NWB_AppendSweep(panelTitle, sweepWave, configWave, sweepNo, DAG_GetNumericalValue(panelTitle, "Popup_Settings_NwbVersion"))
+		NWB_AppendSweep(panelTitle, sweepWave, configWave, sweepNo, str2num(DAG_GetTextualValue(panelTitle, "Popup_Settings_NwbVersion")))
 	endif
 
 	SWS_AfterSweepDataChangeHook(panelTitle)