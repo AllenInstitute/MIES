81bd0e26ef5b37d531e5ad1f25a0f06531a47298
Thomas Braun <thomas.braun@byte-physics.de>
2021-05-25 21:26:58 +0200

    tools/nwb-read-tests/run.sh: Raise pynwb version

--- tools/nwb-read-tests/run.sh
+++ tools/nwb-read-tests/run.sh
@@ -25,7 +25,7 @@ echo "Start building Docker container \"$tag\""
 
 docker build --build-arg USERID=$(id -u)                     \
              --build-arg GROUPID=$(id -g)                    \
-             --build-arg PACKAGE_WITH_VERSION="pynwb==1.4.0" \
+             --build-arg PACKAGE_WITH_VERSION="pynwb==1.5.1" \
              -t $tag $top_level/tools/nwb-read-tests
 
 # use 'docker run -it ..' for interactive debugging
