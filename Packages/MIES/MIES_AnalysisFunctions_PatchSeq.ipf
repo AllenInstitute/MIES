AllenInstitute/MIES

commit 0ef300da641169aaf6a4d38812c3feb9860f5b5e

PSQ_DaScale: Add new operation mode
Add two variants for the PSQ_DaScale analysis function.

Sub:
- Uses DAScale from analysis parameters directly

Supra:
- Uses DAScale from analysis parameters as offset to the DAScale value
  PSQ_Rheobase found for long pulses (PSQ_GetLastPassingLongRHSweep).

Original-CommitID: 6a664ec2cb6df7f82b1ce1c9792d972931c91904

Packages/MIES/MIES_AnalysisFunctions_PatchSeq.ipf
@@ -739,9 +739,85 @@ static Function PSQ_TestOverrideActive()
 	return 0
 End
 
-/// @brief Require DAScales parameter from stimset
+/// @brief Return a sweep number of an existing sweep matching the following conditions
+///
+/// - Acquired with Rheobase analysis function
+/// - Sweep set was passing
+/// - Pulse duration was longer than 500ms
+///
+/// And as usual we want the *last* matching sweep.
+///
+/// @return existing sweep number or -1 in case no such sweep could be found
+Function PSQ_GetLastPassingLongRHSweep(panelTitle, headstage)
+	string panelTitle
+	variable headstage
+
+	string key
+	variable i, numEntries, sweepNo, sweepCol
+
+	WAVE numericalValues = GetLBNumericalValues(panelTitle)
+
+	// rheobase sweeps passing
+	key = PSQ_CreateLBNKey(PSQ_RHEOBASE, PSQ_FMT_LBN_SET_PASS, query = 1)
+	WAVE/Z sweeps = GetSweepsWithSetting(numericalValues, key)
+
+	if(!WaveExists(sweeps))
+		return -1
+	endif
+
+	// pulse duration
+	key = PSQ_CreateLBNKey(PSQ_RHEOBASE, PSQ_FMT_LBN_PULSE_DUR, query = 1)
+
+	numEntries = DimSize(sweeps, ROWS)
+	for(i = numEntries - 1; i >= 0; i -= 1)
+		sweepNo = sweeps[i]
+		WAVE/Z setting = GetLastSettingRAC(numericalValues, sweepNo, key, UNKNOWN_MODE)
+
+		if(WaveExists(setting) && setting[headstage] > 500)
+			return sweepNo
+		endif
+	endfor
+
+	return -1
+End
+
+/// @brief Return the DAScale offset for PSQ_DaScale()
+///
+/// @return DAScale value in pA or NaN on error
+Function PSQ_DS_GetDAScaleOffset(panelTitle, headstage, opMode)
+	string panelTitle, opMode
+	variable headstage
+
+	variable sweepNo
+
+	if(!cmpstr(opMode, PSQ_DS_SUPRA))
+		if(PSQ_TestOverrideActive())
+			return PSQ_DS_OFFSETSCALE_FAKE
+		endif
+
+		sweepNo = PSQ_GetLastPassingLongRHSweep(panelTitle, sweepNo)
+		if(!IsValidSweepNumber(sweepNo))
+			return NaN
+		endif
+
+		WAVE numericalValues = GetLBNumericalValues(panelTitle)
+		WAVE/Z setting = GetLastSetting(numericalValues, sweepNo, STIMSET_SCALE_FACTOR_KEY, DATA_ACQUISITION_MODE)
+		ASSERT(WaveExists(setting), "Could not find DAScale value of matching rheobase sweep")
+		return setting[headstage]
+	elseif(!cmpstr(opMode, PSQ_DS_SUB))
+		return 0
+	else
+		ASSERT(0, "unknown opMode")
+	endif
+End
+
+/// @brief Require parameters from stimset
+///
+/// - DAScales (Numeric wave): DA Scale Factors in pA
+/// - OperationMode (String):  Operation mode of the analayis function. Can be
+///                            either #PSQ_DS_SUB or #PSQ_DS_SUPRA.
 Function/S PSQ_SubThreshold_GetParams()
-	return "DAScales"
+	return "DAScales;OperationMode"
 End
 
 /// @brief Patch Seq Analysis function to find a suitable DAScale
@@ -832,10 +908,14 @@ Function PSQ_DAScale(panelTitle, s)
 	variable index, skipToEnd, ret
 	variable sweepPassed, setPassed, numSweepsPass, length, minLength
 	variable sweepsInSet, passesInSet, acquiredSweepsInSet, numBaselineChunks
-	string msg, stimset, key
+	string msg, stimset, key, opMode
+	variable daScaleOffset = NaN
 
 	WAVE/D/Z DAScales = AFH_GetAnalysisParamWave("DAScales", s.params)
 	ASSERT(WaveExists(DAScales), "Missing DAScale parameter")
+
+	opMode = AFH_GetAnalysisParamTextual("OperationMode", s.params)
+	ASSERT(!cmpstr(opMode, PSQ_DS_SUB) || !cmpstr(opMode, PSQ_DS_SUPRA), "Invalid opMode")
 
 	numSweepsPass = DimSize(DAScales, ROWS)
 	ASSERT(numSweepsPass > 0, "Invalid number of entries in DAScales")
@@ -875,6 +955,13 @@ Function PSQ_DAScale(panelTitle, s)
 				return 1
 			endif
 
+			daScaleOffset = PSQ_DS_GetDAScaleOffset(panelTitle, s.headstage, opMode)
+			if(!IsFinite(daScaleOffset))
+				printf "(%s): Could not find a valid DAScale threshold value from previous rheobase runs with long pulses.\r", panelTitle
+				ControlWindowToFront()
+				return 1
+			endif
+
 			KillOrMoveToTrash(wv = GetAnalysisFuncDAScaleDeltaI(panelTitle))
 			KillOrMoveToTrash(wv = GetAnalysisFuncDAScaleDeltaV(panelTitle))
 			KillOrMoveToTrash(wv = GetAnalysisFuncDAScaleRes(panelTitle))
@@ -884,6 +971,8 @@ Function PSQ_DAScale(panelTitle, s)
 		case POST_SWEEP_EVENT:
 			WAVE numericalValues = GetLBNumericalValues(panelTitle)
 			WAVE textualValues   = GetLBTextualValues(panelTitle)
+
+			daScaleOffset = PSQ_DS_GetDAScaleOffset(panelTitle, s.headstage, opMode)
 
 			key = PSQ_CreateLBNKey(PSQ_DA_SCALE, PSQ_FMT_LBN_SWEEP_PASS, query = 1)
 			sweepPassed = GetLastSettingIndep(numericalValues, s.sweepNo, key, UNKNOWN_MODE)
@@ -963,7 +1052,8 @@ Function PSQ_DAScale(panelTitle, s)
 				printf "(%s): The stimset has too many sweeps, increase the size of DAScales.\r", GetRTStackInfo(1)
 				continue
 			elseif(index < DimSize(DAScales, ROWS))
-				SetDAScale(panelTitle, i, DAScales[index] * 1e-12)
+				ASSERT(isFinite(daScaleOffset), "DAScale offset is non-finite")
+				SetDAScale(panelTitle, i, (DAScales[index] + daScaleOffset) * 1e-12)
 			endif
 		endfor
 	endif

Packages/MIES/MIES_Constants.ipf
@@ -843,6 +843,13 @@ Constant PSQ_RB_DASCALE_STEP     = 10e-12
 Constant PSQ_RB_FINALSCALE_FAKE  = 42e-12
 /// @}
 
+/// PatchSeq DAScale
+/// @{
+Constant PSQ_DS_OFFSETSCALE_FAKE = 23 // pA
+StrConstant PSQ_DS_SUB           = "Sub"
+StrConstant PSQ_DS_SUPRA         = "Supra"
+/// @}
+
 Constant TP_MD_THREAD_DEAD_MAX_RETRIES = 10
 
 /// @todo: IP8 convert all call sites to use MultiThread/T=

Packages/Testing-MIES/UTF_HardwareMain.ipf
@@ -127,22 +127,22 @@ Function run()
 
 #ifndef TESTS_WITH_YOKING
 
-	list = AddListItem("PS_DS_Run1", list, ";", INF)
-	list = AddListItem("PS_DS_Test1", list, ";", INF)
-	list = AddListItem("PS_DS_Run2", list, ";", INF)
-	list = AddListItem("PS_DS_Test2", list, ";", INF)
-	list = AddListItem("PS_DS_Run3", list, ";", INF)
-	list = AddListItem("PS_DS_Test3", list, ";", INF)
-	list = AddListItem("PS_DS_Run4", list, ";", INF)
-	list = AddListItem("PS_DS_Test4", list, ";", INF)
-	list = AddListItem("PS_DS_Run5", list, ";", INF)
-	list = AddListItem("PS_DS_Test5", list, ";", INF)
-	list = AddListItem("PS_DS_Run6", list, ";", INF)
-	list = AddListItem("PS_DS_Test6", list, ";", INF)
-	list = AddListItem("PS_DS_Run7", list, ";", INF)
-	list = AddListItem("PS_DS_Test7", list, ";", INF)
-	list = AddListItem("PS_DS_Run8", list, ";", INF)
-	list = AddListItem("PS_DS_Test8", list, ";", INF)
+	list = AddListItem("PS_DS_Sub_Run1", list, ";", INF)
+	list = AddListItem("PS_DS_Sub_Test1", list, ";", INF)
+	list = AddListItem("PS_DS_Sub_Run2", list, ";", INF)
+	list = AddListItem("PS_DS_Sub_Test2", list, ";", INF)
+	list = AddListItem("PS_DS_Sub_Run3", list, ";", INF)
+	list = AddListItem("PS_DS_Sub_Test3", list, ";", INF)
+	list = AddListItem("PS_DS_Sub_Run4", list, ";", INF)
+	list = AddListItem("PS_DS_Sub_Test4", list, ";", INF)
+	list = AddListItem("PS_DS_Sub_Run5", list, ";", INF)
+	list = AddListItem("PS_DS_Sub_Test5", list, ";", INF)
+	list = AddListItem("PS_DS_Sub_Run6", list, ";", INF)
+	list = AddListItem("PS_DS_Sub_Test6", list, ";", INF)
+	list = AddListItem("PS_DS_Sub_Run7", list, ";", INF)
+	list = AddListItem("PS_DS_Sub_Test7", list, ";", INF)
+	list = AddListItem("PS_DS_Sub_Run8", list, ";", INF)
+	list = AddListItem("PS_DS_Sub_Test8", list, ";", INF)
 
 	list = AddListItem("PS_SP_Run1", list, ";", INF)
 	list = AddListItem("PS_SP_Test1", list, ";", INF)

Packages/Testing-MIES/UTF_PatchSeqDAScale.ipf
@@ -5,8 +5,9 @@
 static Constant HEADSTAGE = 0
 
 /// @brief Acquire data with the given DAQSettings
-static Function AcquireData(s)
+static Function AcquireData(s, stimset)
 	STRUCT DAQSettings& s
+	string stimset
 
 	Initialize_IGNORE()
 
@@ -23,7 +24,7 @@ static Function AcquireData(s)
 	PGC_SetAndActivateControl(DEVICE, "check_DataAcq_AutoBias", val = 1)
 	PGC_SetAndActivateControl(DEVICE, "setvar_DataAcq_AutoBiasV", val = 70)
 	PGC_SetAndActivateControl(DEVICE, GetPanelControl(0, CHANNEL_TYPE_HEADSTAGE, CHANNEL_CONTROL_CHECK), val=1)
-	PGC_SetAndActivateControl(DEVICE, GetPanelControl(0, CHANNEL_TYPE_DAC, CHANNEL_CONTROL_WAVE), val = GetStimSet("PatchSeqDAScale_DA_0") + 1)
+	PGC_SetAndActivateControl(DEVICE, GetPanelControl(0, CHANNEL_TYPE_DAC, CHANNEL_CONTROL_WAVE), str = stimset)
 
 	WAVE ampMCC = GetAmplifierMultiClamps()
 	WAVE ampTel = GetAmplifierTelegraphServers()
@@ -66,18 +67,18 @@ Function/WAVE GetSweepResults_IGNORE(sweepNo)
 	return sweepPassed
 End
 
-Function PS_DS_Run1()
+Function PS_DS_Sub_Run1()
 
 	STRUCT DAQSettings s
 	InitDAQSettingsFromString(s, "DAQ_MD1_RA1_IDX0_LIDX0_BKG_1")
-	AcquireData(s)
+	AcquireData(s, "PSQ_DaScale_Sub_DA_0")
 
 	WAVE wv = PSQ_CreateOverrideResults(DEVICE, HEADSTAGE, PSQ_DA_SCALE)
 	// all tests fail
 	wv = 0
 End
 
-Function PS_DS_Test1()
+Function PS_DS_Sub_Test1()
 
 	variable sweepNo, setPassed
 
@@ -95,11 +96,11 @@ Function PS_DS_Test1()
 	CHECK_EQUAL_WAVES(sweepPassed, {0, 0, 0, 0, 0})
 End
 
-Function PS_DS_Run2()
+Function PS_DS_Sub_Run2()
 
 	STRUCT DAQSettings s
 	InitDAQSettingsFromString(s, "DAQ_MD1_RA1_IDX0_LIDX0_BKG_1")
-	AcquireData(s)
+	AcquireData(s, "PSQ_DaScale_Sub_DA_0")
 
 	WAVE wv = PSQ_CreateOverrideResults(DEVICE, HEADSTAGE, PSQ_DA_SCALE)
 	// only pre pulse chunk pass, others fail
@@ -107,7 +108,7 @@ Function PS_DS_Run2()
 	wv[0][] = 1
 End
 
-Function PS_DS_Test2()
+Function PS_DS_Sub_Test2()
 
 	variable sweepNo, setPassed
 
@@ -125,11 +126,11 @@ Function PS_DS_Test2()
 	CHECK_EQUAL_WAVES(sweepPassed, {0, 0, 0, 0, 0})
 End
 
-Function PS_DS_Run3()
+Function PS_DS_Sub_Run3()
 
 	STRUCT DAQSettings s
 	InitDAQSettingsFromString(s, "DAQ_MD1_RA1_IDX0_LIDX0_BKG_1")
-	AcquireData(s)
+	AcquireData(s, "PSQ_DaScale_Sub_DA_0")
 
 	WAVE wv = PSQ_CreateOverrideResults(DEVICE, HEADSTAGE, PSQ_DA_SCALE)
 	// pre pulse chunk pass
@@ -138,7 +139,7 @@ Function PS_DS_Run3()
 	wv[0,1][] = 1
 End
 
-Function PS_DS_Test3()
+Function PS_DS_Sub_Test3()
 
 	variable sweepNo, setPassed
 
@@ -156,11 +157,11 @@ Function PS_DS_Test3()
 	CHECK_EQUAL_WAVES(sweepPassed, {1, 1, 1, 1, 1})
 End
 
-Function PS_DS_Run4()
+Function PS_DS_Sub_Run4()
 
 	STRUCT DAQSettings s
 	InitDAQSettingsFromString(s, "DAQ_MD1_RA1_IDX0_LIDX0_BKG_1")
-	AcquireData(s)
+	AcquireData(s, "PSQ_DaScale_Sub_DA_0")
 
 	WAVE wv = PSQ_CreateOverrideResults(DEVICE, HEADSTAGE, PSQ_DA_SCALE)
 	// pre pulse chunk pass
@@ -170,7 +171,7 @@ Function PS_DS_Run4()
 	wv[DimSize(wv, ROWS) - 1][] = 1
 End
 
-Function PS_DS_Test4()
+Function PS_DS_Sub_Test4()
 
 	variable sweepNo, setPassed
 
@@ -188,11 +189,11 @@ Function PS_DS_Test4()
 	CHECK_EQUAL_WAVES(sweepPassed, {1, 1, 1, 1, 1})
 End
 
-Function PS_DS_Run5()
+Function PS_DS_Sub_Run5()
 
 	STRUCT DAQSettings s
 	InitDAQSettingsFromString(s, "DAQ_MD1_RA1_IDX0_LIDX0_BKG_1")
-	AcquireData(s)
+	AcquireData(s, "PSQ_DaScale_Sub_DA_0")
 
 	WAVE wv = PSQ_CreateOverrideResults(DEVICE, HEADSTAGE, PSQ_DA_SCALE)
 	// pre pulse chunk fails
@@ -201,7 +202,7 @@ Function PS_DS_Run5()
 	wv[0][] = 0
 End
 
-Function PS_DS_Test5()
+Function PS_DS_Sub_Test5()
 
 	variable sweepNo, setPassed
 
@@ -219,11 +220,11 @@ Function PS_DS_Test5()
 	CHECK_EQUAL_WAVES(sweepPassed, {0, 0, 0, 0, 0})
 End
 
-Function PS_DS_Run6()
+Function PS_DS_Sub_Run6()
 
 	STRUCT DAQSettings s
 	InitDAQSettingsFromString(s, "DAQ_MD1_RA1_IDX0_LIDX0_BKG_1")
-	AcquireData(s)
+	AcquireData(s, "PSQ_DaScale_Sub_DA_0")
 
 	WAVE wv = PSQ_CreateOverrideResults(DEVICE, HEADSTAGE, PSQ_DA_SCALE)
 	// pre pulse chunk pass
@@ -233,7 +234,7 @@ Function PS_DS_Run6()
 	wv[2][] = 1
 End
 
-Function PS_DS_Test6()
+Function PS_DS_Sub_Test6()
 
 	variable sweepNo, setPassed
 
@@ -251,11 +252,11 @@ Function PS_DS_Test6()
 	CHECK_EQUAL_WAVES(sweepPassed, {1, 1, 1, 1, 1})
 End
 
-Function PS_DS_Run7()
+Function PS_DS_Sub_Run7()
 
 	STRUCT DAQSettings s
 	InitDAQSettingsFromString(s, "DAQ_MD1_RA1_IDX0_LIDX0_BKG_1")
-	AcquireData(s)
+	AcquireData(s, "PSQ_DaScale_Sub_DA_0")
 
 	WAVE wv = PSQ_CreateOverrideResults(DEVICE, HEADSTAGE, PSQ_DA_SCALE)
 	// pre pulse chunk pass
@@ -265,7 +266,7 @@ Function PS_DS_Run7()
 	wv[0, 1][2,6] = 1
 End
 
-Function PS_DS_Test7()
+Function PS_DS_Sub_Test7()
 
 	variable sweepNo, setPassed
 
@@ -283,11 +284,11 @@ Function PS_DS_Test7()
 	CHECK_EQUAL_WAVES(sweepPassed, {0, 0, 1, 1, 1, 1, 1})
 End
 
-Function PS_DS_Run8()
+Function PS_DS_Sub_Run8()
 
 	STRUCT DAQSettings s
 	InitDAQSettingsFromString(s, "DAQ_MD1_RA1_IDX0_LIDX0_BKG_1")
-	AcquireData(s)
+	AcquireData(s, "PSQ_DaScale_Sub_DA_0")
 
 	WAVE wv = PSQ_CreateOverrideResults(DEVICE, HEADSTAGE, PSQ_DA_SCALE)
 	// pre pulse chunk pass
@@ -301,7 +302,7 @@ Function PS_DS_Run8()
 	wv[0, 1][8] = 1
 End
 
-Function PS_DS_Test8()
+Function PS_DS_Sub_Test8()
 
 	variable sweepNo, setPassed
 

Packages/doc/patch-seq-dascale.dot
@@ -90,4 +90,16 @@ digraph G {
     "Stimset is large enough";
     "Prepare DAQ Event" -> "Stimset is large enough";
     "Stimset is large enough" -> "All checks passed";
+    "All required analysis parameters present?\n DAScale : numeric wave\n OperationMode: string";
+    "Prepare DAQ Event" -> "All required analysis parameters present?\n DAScale : numeric wave\n OperationMode: string";
+    "All required analysis parameters present?\n DAScale : numeric wave\n OperationMode: string" -> "All checks passed";
+    "Get OperationMode";
+    "All required analysis parameters present?\n DAScale : numeric wave\n OperationMode: string" -> "Get OperationMode";
+    "Look for an existing and passing rheobase sweep\n within a passing set and\n a pulse duration of more than 500ms";
+    "Get OperationMode" -> "Look for an existing and passing rheobase sweep\n within a passing set and\n a pulse duration of more than 500ms"  [label="Supra"];
+    "Look for an existing and passing rheobase sweep\n within a passing set and\n a pulse duration of more than 500ms" -> "Abort"  [label="Not found"];
+    "Use the DAScale value of that sweep\n as offset to the DAScale values from the analysis parameters";
+    "Look for an existing and passing rheobase sweep\n within a passing set and\n a pulse duration of more than 500ms" -> "Use the DAScale value of that sweep\n as offset to the DAScale values from the analysis parameters"  [label="Found"];
+    "Use the DAScale values from the analysis parameters";
+    "Get OperationMode" -> "Use the DAScale values from the analysis parameters"  [label="Sub"];
 }

commit d314f5a3bcc48af34ed0ffbee392e00b563f003b

PSQ_DS_GetDAScaleOffset: Pass correct headstage parameter to PSQ_GetLastPassingLongRHSweep
The function expects the headstage number and not the sweep number. As
sweepNo is initialized to zero, this bug is asymptomatic when headstage
zero is used.

Bug introduced in 0ef300da (PSQ_DaScale: Add new operation mode, 2018-02-15).

Reported-by: Tim <t.s.heistek@vu.nl>

Packages/MIES/MIES_AnalysisFunctions_PatchSeq.ipf
@@ -1066,7 +1066,7 @@ Function PSQ_DS_GetDAScaleOffset(panelTitle, headstage, opMode)
 			return PSQ_DS_OFFSETSCALE_FAKE
 		endif
 
-		sweepNo = PSQ_GetLastPassingLongRHSweep(panelTitle, sweepNo)
+		sweepNo = PSQ_GetLastPassingLongRHSweep(panelTitle, headstage)
 		if(!IsValidSweepNumber(sweepNo))
 			return NaN
 		endif