AllenInstitute/MIES

commit c9b753378da9fb5f795f862af772eec1cef8282a

NWB_Flush: Adapt and make it threadsafe
This now uses the new IPNWB API of H5_FlushFile.

Packages/MIES/MIES_NeuroDataWithoutBorders.ipf
@@ -1674,15 +1674,9 @@ Function NWB_LoadLabNoteBook(locationID, notebook, dfr)
 End
 
 /// @brief Flushes the contents of the NWB file to disc
-Function NWB_Flush()
-	SVAR filePathExport = $GetNWBFilePathExport()
-	NVAR fileIDExport   = $GetNWBFileIDExport()
+threadsafe Function NWB_Flush(variable locationID)
 
-	if(!IsFinite(fileIDExport) || !H5_IsFileOpen(fileIDExport))
-		return NaN
-	endif
-
-	fileIDExport = H5_FlushFile(fileIDExport, filePathExport, write = 1)
+	H5_FlushFile(locationID)
 End
 
 static Function NWB_AppendIgorHistoryAndLogFile(nwbVersion, locationID)

Packages/MIES/MIES_IgorHooks.ipf
@@ -137,7 +137,9 @@ static Function BeforeExperimentSaveHook(rN, fileName, path, type, creator, kind
 #if !defined(IGOR64)
 	IH_KillStimSets()
 #endif
-	NWB_Flush()
+
+	NVAR fileIDExport = $GetNWBFileIDExport()
+	NWB_Flush(fileIDExport)
 
 	LOG_AddEntry(PACKAGE_MIES, "end")
 End

commit 920d955ca3178d9fd8cdd42fc7f107c899c1f6d7

NWB_Flush: Only flush with a valid HDF5 file ID
Broken since c9b75337 (NWB_Flush: Adapt and make it threadsafe, 2021-05-11).

Packages/MIES/MIES_NeuroDataWithoutBorders.ipf
@@ -1768,6 +1768,10 @@ End
 /// @brief Flushes the contents of the NWB file to disc
 threadsafe Function NWB_Flush(variable locationID)
 
+	if(IsNaN(locationID))
+		return NaN
+	endif
+
 	H5_FlushFile(locationID)
 End
 