AllenInstitute/MIES

commit f6720e1cbc444a2f074a7054e8581cf24eb73f94

RemovePrefix/RemovePrefixFromListItem: Teach them to remove regular expressions as well
And also rename RemovePrefix's optional argument from startStr to start
as it can be a regular expression now as well.

The tests are also enhanced.

Packages/MIES/MIES_Utilities.ipf
@@ -1517,19 +1517,28 @@ Function/S UniqueWaveName(dfr, baseName)
 	return ""
 End
 
-/// @brief Remove str with the first character removed, or
-/// if given with startStr removed
+/// @brief Remove a prefix from a string
 ///
-/// Same semantics as the RemoveEnding builtin
-Function/S RemovePrefix(str, [startStr])
-	string str, startStr
+/// Same semantics as the RemoveEnding builtin for regExp == 0.
+///
+/// @param str    string to potentially remove something from its beginning
+/// @param start  [optional, defaults to the first character] Remove this from
+///               the begin pf str
+/// @param regExp [optional, defaults to false] If start is a simple string (false)
+///               or a regular expression (true)
+Function/S RemovePrefix(string str, [string start, variable regExp])
+	variable length, pos, skipLength, err
+	string regExpResult
 
-	variable length, pos
+	if(ParamIsDefault(regExp))
+		regExp = 0
+	else
+		regExp = !!regExp
+	endif
 
 	length = strlen(str)
 
-	if(ParamIsDefault(startStr))
-
+	if(ParamIsDefault(start))
 		if(length <= 0)
 			return str
 		endif
@@ -1537,13 +1546,25 @@ Function/S RemovePrefix(str, [startStr])
 		return str[1, length - 1]
 	endif
 
-	pos = strsearch(str, startStr, 0)
+	if(regExp)
+		SplitString/E="^(" + start + ")" str, regExpResult; err = GetRTError(1)
 
-	if(pos != 0)
-		return str
+		if(V_flag == 1 && err == 0)
+			skipLength = strlen(regExpResult)
+		else
+			return str
+		endif
+	else
+		pos = strsearch(str, start, 0)
+
+		if(pos != 0)
+			return str
+		endif
+
+		skipLength = strlen(start)
 	endif
 
-	return 	str[strlen(startStr), length - 1]
+	return str[skipLength, length - 1]
 End
 
 /// @brief Returns a unique and non-existing file or folder name
@@ -2390,22 +2411,25 @@ End
 
 /// @brief Remove a string prefix from each list item and
 ///        return the new list
-Function/S RemovePrefixFromListItem(prefix, list, [listSep])
-	string prefix, list
-	string listSep
+Function/S RemovePrefixFromListItem(string prefix, string list, [string listSep, variable regExp])
+	string result, entry
+	variable numEntries, i
 
 	if(ParamIsDefault(listSep))
 		listSep = ";"
 	endif
 
-	string result, entry
-	variable numEntries, i
+	if(ParamIsDefault(regExp))
+		regExp = 0
+	else
+		regExp = !!regExp
+	endif
 
 	result = ""
 	numEntries = ItemsInList(list, listSep)
 	for(i = 0; i < numEntries; i += 1)
 		entry = StringFromList(i, list, listSep)
-		result = AddListItem(RemovePrefix(entry, startStr = prefix), result, listSep, inf)
+		result = AddListItem(RemovePrefix(entry, start = prefix, regExp = regExp), result, listSep, inf)
 	endfor
 
 	return result

Packages/MIES/MIES_AnalysisBrowser.ipf
@@ -86,7 +86,7 @@ static Function AB_AddMapEntry(baseFolder, discLocation)
 	map[index][%DiscLocation] = discLocation
 
 	// %FileName = filename + extension
-	relativePath = RemovePrefix(discLocation, startStr=baseFolder)
+	relativePath = RemovePrefix(discLocation, start = baseFolder)
 	map[index][%FileName] = relativePath
 
 	extension = "." + GetFileSuffix(discLocation)
@@ -107,7 +107,7 @@ static Function AB_AddMapEntry(baseFolder, discLocation)
 	// %DataFolder = igor friendly DF name
 	DFREF dfr = GetAnalysisFolder()
 	DFREF expFolder = UniqueDataFolder(dfr, RemoveEnding(relativePath, extension))
-	dataFolder = RemovePrefix(GetDataFolder(1, expFolder), startStr=GetDataFolder(1, dfr))
+	dataFolder = RemovePrefix(GetDataFolder(1, expFolder), start = GetDataFolder(1, dfr))
 	map[index][%DataFolder] = RemoveEnding(dataFolder, ":")
 
 	index += 1
@@ -2106,7 +2106,7 @@ static Function AB_LoadWave(expFilePath, fullPath, overwrite)
 	DFREF newDFR = UniqueDataFolder(GetAnalysisFolder(), "temp")
 
 	dataFolder = GetFolder(fullPath)
-	loadList = AddListItem(RemovePrefix(fullPath, startStr = dataFolder), loadList)
+	loadList = AddListItem(RemovePrefix(fullPath, start = dataFolder), loadList)
 	if(isEmpty(dataFolder))
 		dataFolder = "root:"
 	endif

Packages/MIES/MIES_AnalysisBrowser_SweepBrowser.ipf
@@ -216,7 +216,7 @@ Function/WAVE SB_GetChannelInfoFromGraph(graph, channel, [experiment])
 		numWaves = ItemsInList(list)
 		for(j = 0; j < numWaves; j += 1)
 			path = StringFromList(j, list)
-			channelNumber = str2num(RemovePrefix(GetBaseName(path), startstr=channel + "_"))
+			channelNumber = str2num(RemovePrefix(GetBaseName(path), start = channel + "_"))
 			ASSERT(IsFinite(channelNumber), "Extracted non finite channel number")
 
 			strswitch(channel)

Packages/MIES/MIES_AnalysisBrowser_SweepBrowser_Export.ipf
@@ -356,7 +356,7 @@ static Function SBE_ExportSweepBrowser(sett)
 	// replace relative reference to sweepBrowserDFR
 	// with absolut ones to newPrefix
 	folder = GetDataFolder(1, sweepBrowserDFR)
-	folder = RemovePrefix(folder, startStr="root:")
+	folder = RemovePrefix(folder, start = "root:")
 	folder = ":::::::" + folder
 	graphMacro = ReplaceString(folder, graphMacro, newPrefix + ":")
 
@@ -373,7 +373,7 @@ static Function SBE_ExportSweepBrowser(sett)
 		if(DataFolderRefsEqual(loc, sweepBrowserDFR))
 			DFREF dfr = createDFWithAllParents(newPrefix)
 		else
-			relativeDest = RemovePrefix(GetDataFolder(1, loc), startStr=analysisPrefix)
+			relativeDest = RemovePrefix(GetDataFolder(1, loc), start = analysisPrefix)
 			DFREF dfr = createDFWithAllParents(newPrefix + relativeDest)
 		endif
 
@@ -524,7 +524,7 @@ static Function SBE_ExportSweepBrowser(sett)
 	endfor
 
 	if(doCreateNewGraph)
-		graphName = RemovePrefix(newPrefix, startStr="root:")
+		graphName = RemovePrefix(newPrefix, start = "root:")
 		if(WindowExists(graphName))
 			graphName = UniqueName(graphName, 6, 0)
 		endif

Packages/MIES/MIES_Menu.ipf
@@ -109,7 +109,7 @@ Function OpenAboutDialog()
 
 	version = ROStr(GetMiesVersion())
 	version = StringFromList(0, version, "\r")
-	version = RemovePrefix(version, startStr = "Release_")
+	version = RemovePrefix(version, start = "Release_")
 	Notebook $nb text="Version: " + version  + "\r"
 	Notebook $nb text="\r"
 	NotebookAction/W=$nb name=Action1, title="Report an Issue/Enhancement proposal", ignoreErrors=1

Packages/MIES/MIES_MiesUtilities.ipf
@@ -1721,7 +1721,7 @@ Function/S GetAllDevices()
 			numDevices = ItemsInList(subFolders)
 			for(j = 0; j < numDevices ; j += 1)
 				subFolder = StringFromList(j, subFolders)
-				number = RemovePrefix(subFolder, startStr = "Device")
+				number = RemovePrefix(subFolder, start = "Device")
 				device = BuildDeviceString(folder, number)
 				path   = GetDevicePathAsString(device)
 
@@ -4177,7 +4177,7 @@ static Function AverageWavesFromSameYAxisIfReq(graph, averagingEnabled, averageD
 				k += 1
 			endif
 		elseif(StringMatch(axis, VERT_AXIS_BASE_NAME + "*"))
-			averageWaveName = "average" + RemovePrefix(axis, startStr=VERT_AXIS_BASE_NAME)
+			averageWaveName = "average" + RemovePrefix(axis, start = VERT_AXIS_BASE_NAME)
 		else
 			sprintf averageWaveName, "average_%d", k
 			k += 1

Packages/MIES/MIES_NeuroDataWithoutBorders.ipf
@@ -1114,7 +1114,7 @@ static Function NWB_WriteStimsetCustomWave(nwbVersion, locationID, custom_wave, 
 	// build path for NWB file
 	pathInNWB = GetWavesDataFolder(custom_wave, 1)
 	pathInNWB = RemoveEnding(pathInNWB, ":")
-	pathInNWB = RemovePrefix(pathInNWB, startStr = "root")
+	pathInNWB = RemovePrefix(pathInNWB, start = "root")
 	pathInNWB = ReplaceString(":", pathInNWB, "/")
 	pathInNWB = "/general/stimsets/referenced" + pathInNWB
 
@@ -1316,7 +1316,7 @@ Function NWB_LoadCustomWave(locationID, fullPath, overwrite)
 		endif
 	endif
 
-	pathInNWB = RemovePrefix(fullPath, startStr = "root:")
+	pathInNWB = RemovePrefix(fullPath, start = "root:")
 	pathInNWB = ReplaceString(":", pathInNWB, "/")
 	pathInNWB = "/general/stimsets/referenced/" + pathInNWB
 

Packages/MIES/MIES_ProgrammaticGUIControl.ipf
@@ -83,8 +83,8 @@ static Function [string value, variable type] PGC_ParsePopupMenuValue(string rec
 	listOrFunc = ReplaceString("\\\"", listOrFunc, "\"")
 
 	// misc cleanup
-	listOrFunc = RemovePrefix(listOrFunc, startStr="#")
-	listOrFunc = RemovePrefix(listOrFunc, startStr="\"")
+	listOrFunc = RemovePrefix(listOrFunc, start = "#")
+	listOrFunc = RemovePrefix(listOrFunc, start = "\"")
 	listOrFunc = RemoveEnding(listOrFunc, "\"")
 
 	SplitString/E="^\"\*([A-Z]{1,})\*\"$" listOrFunc, builtinPopupMenu

commit 13769e8f6f6ad0d5abf8e5983bc8b682dda6d21d

RemovePrefix: Clear error before querying it
Otherwise RemovePrefix trips over lingering RTEs.

Bug introduced in f6720e1c (RemovePrefix/RemovePrefixFromListItem: Teach
them to remove regular expressions as well, 2021-02-11).

Packages/MIES/MIES_Utilities.ipf
@@ -1680,6 +1680,7 @@ threadsafe Function/S RemovePrefix(string str, [string start, variable regExp])
 	endif
 
 	if(regExp)
+		ClearRTError()
 		SplitString/E="^(" + start + ")" str, regExpResult; err = GetRTError(1)
 
 		if(V_flag == 1 && err == 0)