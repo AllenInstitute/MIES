AllenInstitute/MIES

commit 36f581146bb2a7c78305524cf3a499e88cdff4f5

Add support for downloading stimsets from DANDI
MIES users can now download stimulus sets from DANDI from within Igor
Pro.

Interactive menu entries are in "Mies Panels"->NWB/DANDI->"Download
stimsets" or programmatically via DND_FetchAssetFromSet.

Packages/MIES/MIES_Menu.ipf
@@ -31,13 +31,14 @@ Menu "Mies Panels"
 		"IVSCC control panel"                  , /Q, IVS_CreatePanel()
 	End
 	"-"
-	SubMenu "Neurodata Without Borders (NWB)"
+	SubMenu "Neurodata Without Borders (NWB)/DANDI"
 		"Export all data into NWB"             , /Q, NWB_ExportWithDialog(NWB_EXPORT_DATA)
 		"-"
 		"Export all data into NWBv1 (legacy)"  , /Q, NWB_ExportWithDialog(NWB_EXPORT_DATA, nwbVersion = 1)
 		"-"
 		"Export all stimsets into NWB"         , /Q, NWB_ExportWithDialog(NWB_EXPORT_STIMSETS)
 		"Load Stimsets from NWB"               , /Q, NWB_LoadAllStimsets()
+		"Download Stimsets"                    , /Q, MEN_DownloadStimsets()
 	End
 	SubMenu "View Files"
 		"Configuration"                        , /Q, CONF_OpenConfigInNotebook()
@@ -255,4 +256,12 @@ Function MEN_OpenZeroMQXOPLogFile()
 	endif
 
 	NotebookSelectionAtEnd(name)
+End
+
+Function MEN_DownloadStimsets()
+	string path
+
+	path = DND_FetchAssetFromSet(DND_STIMSET_DANDI_SET)
+
+	NWB_LoadAllStimsets(filename = path, overwrite = 0)
 End

Packages/MIES/MIES_Constants.ipf
@@ -1518,3 +1518,5 @@ StrConstant NWB_WORKLOAD_CLASS = "nwb_writing"
 Constant ID_HEADSTAGE_SETTINGS = 0x1
 Constant ID_POPUPMENU_SETTINGS = 0x2
 /// @}
+
+Constant DND_STIMSET_DANDI_SET = 107

Packages/MIES/MIES_DANDI.ipf
@@ -1,0 +1,131 @@
+﻿#pragma TextEncoding = "UTF-8"
+#pragma rtGlobals=3 // Use modern global access method and strict wave access.
+#pragma rtFunctionErrors=1
+
+#ifdef AUTOMATED_TESTING
+#pragma ModuleName=MIES_DANDI
+#endif
+
+static Function/S DND_GetDestinationIgorPath()
+
+	string path = "tmpPath"
+	NewPath/C/O/Q $path, SpecialDirPath("Temporary", 0, 0, 1) + "DandisetDownloads"
+
+	return path
+End
+
+static Function/S DND_ChooseAsset(WAVE/T props)
+	variable idx, ret
+
+	WAVE data = GetDandiDialogWave(props)
+	Duplicate/FREE data, mock
+
+	// select the first asset when mocking
+	mock[]  = 0
+	mock[0] = 1
+
+	ret = ID_AskUserForSettings(ID_POPUPMENU_SETTINGS, "Stimulus set to load", data, mock)
+
+	if(ret)
+		return ""
+	endif
+
+	idx = GetRowIndex(data, val = 1)
+	ASSERT(IsFinite(idx), "Invalid index")
+
+	return props[idx][%asset_id]
+End
+
+/// @brief Download the given asset from the set
+///
+/// The asset hash is checked before returning.
+///
+/// @return absolute path to the downloaded asset
+static Function/S DND_FetchAsset(variable setNumber, string assetID, string assetPath)
+	string url, refHash, downloadUrl, path, name, assetFilePath, data, fname
+	variable jsonID
+
+	sprintf url, "https://api.dandiarchive.org/api/dandisets/%06d/versions/draft/assets/%s", setNumber, assetID
+	UrlRequest/Z url=url
+	ASSERT(!V_flag, "Could fetch asset properties")
+
+	jsonID = JSON_Parse(S_serverResponse)
+
+	refHash = JSON_GetString(jsonID, "/digest/dandi:sha2-256")
+	downloadUrl = JSON_GetString(jsonID, "/contentUrl/0")
+
+	JSON_Release(jsonID)
+
+	path = DND_GetDestinationIgorPath()
+	name = GetFile(assetPath, sep = "/")
+
+	UrlRequest/FILE=name/O/P=$path/Z url=downloadURL
+	ASSERT(!V_flag, "Could not download dandiset asset.")
+
+	PathInfo/S $path
+	assetFilePath = S_path + name
+	[data, fname] = LoadTextFile(assetFilePath)
+
+	ASSERT(!cmpstr(refHash, Hash(data, 1)), "Invalid checksum")
+
+	KillPath $path
+
+	return assetFilePath
+End
+
+/// @brief Parse the REST API response and fill the DANDI set properties wave
+///
+/// The returned wave will have the format as described at GetDandiSetProperties()
+/// and row dimension label with the asset ID.
+///
+/// Example: https://api.dandiarchive.org/api/dandisets/000068/versions/draft/assets/
+static Function/WAVE DND_ParseSetReponse(string response)
+	string path
+	variable jsonID, numEntries, i
+
+	jsonID = JSON_Parse(response)
+
+	numEntries = JSON_GetArraySize(jsonID, "/results")
+
+	if(!numEntries)
+		JSON_Release(jsonID)
+		return $""
+	endif
+
+	WAVE/T props = GetDandiSetProperties()
+
+	Redimension/N=(numEntries, -1) props
+
+	props[][] = JSON_GetString(jsonID, "/results/" + num2str(p) + "/" + GetDimLabel(props, COLS, q))
+
+	JSON_Release(jsonID)
+
+	for(i = 0; i < numEntries; i += 1)
+		SetDimLabel ROWS, i, $props[i][%asset_id], props
+	endfor
+
+	return props
+End
+
+Function/S DND_FetchAssetFromSet(variable setNumber)
+	string url, assetID , name, path, data, fname, assetFilePath
+	variable numAssets, idx, i
+
+	sprintf url, "https://api.dandiarchive.org/api/dandisets/%06d/versions/draft/assets/", setNumber
+	UrlRequest/Z url=url
+	ASSERT(!V_flag, "Could not download dandiset file list")
+
+	WAVE/T props = DND_ParseSetReponse(S_serverResponse)
+
+	if(!WaveExists(props))
+		return ""
+	endif
+
+	assetID = DND_ChooseAsset(props)
+
+	if(IsEmpty(assetID))
+		return ""
+	endif
+
+	return DND_FetchAsset(setNumber, assetID, props[%$assetID][%path])
+End

Packages/MIES/MIES_WaveDataFolderGetters.ipf
@@ -6672,3 +6672,46 @@ Function/WAVE GetValidAcqStateTransitions()
 
 	return wv
 End
+
+Function/S GetDANDIFolderAsString()
+	return "root:MIES:DANDI"
+End
+
+/// @brief Return the data folder reference to the DANDI folder
+Function/DF GetDANDIFolder()
+
+	return createDFWithAllParents(GetDANDIFolderAsString())
+End
+
+/// @brief Return a free wave with the DANDI set properties
+///
+/// Rows:
+/// - one row for each asset
+///
+/// Columns:
+/// - ID
+/// - created timestamp (ISO8601)
+/// - modified timestamp (ISO8601)
+/// - file path of the asset inside the DANDI set
+Function/WAVE GetDandiSetProperties()
+
+	Make/FREE/N=(0, 4)/T wv
+
+	// we don't care about size
+	SetDimensionLabels(wv, "asset_id;created;modified;path", COLS)
+
+	return wv
+End
+
+Function/WAVE GetDandiDialogWave(WAVE props)
+
+	DFREF dfr = GetDANDIFolder()
+	Make/O/N=(DimSize(props, ROWS)) dfr:data/WAVE=data
+
+	Duplicate/FREE/RMD=[][FindDimLabel(props, COLS, "path")] props, paths
+	Redimension/N=(-1) paths
+
+	SetDimensionLabels(data, TextWaveToList(paths, ";"), ROWS)
+
+	return data
+End

Packages/MIES_Include.ipf
@@ -175,6 +175,7 @@ End
 #include "MIES_DAEphys"
 #include "MIES_DAEphys_Macro"
 #include "MIES_DAEphys_GuiState"
+#include "MIES_DANDI"
 #include "MIES_DataBrowser"
 #include "MIES_DataBrowser_Macro"
 #include "MIES_DataAcquisition"

commit c3aaa3363f3ba740f2c6542e54a9e48d446d9eec

Menu: Fix display of NWB/DANDI entry
"/" is a special character, so we need to turn off special character
expansion for it to display.

Bug introduced in 36f58114 (Add support for downloading stimsets from DANDI, 2021-06-11).

Packages/MIES/MIES_Menu.ipf
@@ -31,7 +31,7 @@ Menu "Mies Panels"
 		"IVSCC control panel"                  , /Q, IVS_CreatePanel()
 	End
 	"-"
-	SubMenu "Neurodata Without Borders (NWB)/DANDI"
+	SubMenu "\\M0Neurodata Without Borders (NWB)/DANDI"
 		"Export all data into NWB"             , /Q, NWB_ExportWithDialog(NWB_EXPORT_DATA)
 		"-"
 		"Export all data into NWBv1 (legacy)"  , /Q, NWB_ExportWithDialog(NWB_EXPORT_DATA, nwbVersion = 1)