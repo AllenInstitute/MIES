AllenInstitute/MIES

commit 55ee1ccfc50edda4d1156d0f4447556e0635e44c

Introduce Analysis functions V3
The new analysis functions have the following features:
- Supports pre sweep event; called immediately before DAQ starts
- Uses the structure AnalysisFunction_V3 to pass data into analysis
  functions with additional members
- Allows to pass in arbitrary data via analysis function user
  parameters. Currently not with a GUI but only via
  WBP_AddAnalysisParameter. The user can enforce a list of parameters
  which must be present.
- One analysis function for all events.

Compatiblity notes:
- V1 and V2 analysis functions are still executed for existing stimsets.
  Changed and new stimsets must use V3 functions. See
  TestAnalysisFunction_V3() for an example V3 function.

New Textual Labnotebook Entries:
- "Pre Sweep function"
- "Generic function"
- "Function params"

Original-CommitID: 529219e6921ab3efad348a7b98e32d74b4bb37dc

Packages/MIES/MIES_AnalysisFunctions.ipf
@@ -12,19 +12,20 @@
 /// @sa MIES_AnalysisFunctionPrototypes.ipf
 ///
 /// Users can implement functions which are called at certain events for each
-/// data acquisition cycle. These functions should *never* abort, error out with a runtime error, or open dialogs!
+/// data acquisition cycle. See TestAnalysisFunction_V3() for an example.
 ///
-/// Useful helper functions are defined in MIES_AnalysisFunctionHelpers.ipf.
-///
-/// @anchor AnalysisFunctionEventDescriptionTable
+/// @anchor AnalysisFunction_V3DescriptionTable
 ///
 /// Event      | Description                                  | Specialities
 /// -----------|----------------------------------------------|---------------------------------------------------------------
 /// Pre DAQ    | Before any DAQ occurs                        | Called before the settings are validated
 /// Mid Sweep  | Each time when new data is polled            | Available for background DAQ only
+/// Pre Sweep  | Immediately before the sweep starts          | None
 /// Post Sweep | After each sweep (before possible ITI pause) | None
 /// Post Set   | After a *full* set has been acquired         | This event is not always reached as the user might not acquire all steps of a set
 /// Post DAQ   | After all DAQ has been finished              | None
+///
+/// Useful helper functions are defined in MIES_AnalysisFunctionHelpers.ipf.
 ///
 /// The Post Sweep/Set/DAQ functions are *not* executed if a currently running sweep is aborted.
 ///
@@ -44,6 +45,22 @@
 /// 1                                 | Pre DAQ     | DAQ is prevented to start
 /// #ANALYSIS_FUNC_RET_REPURP_TIME    | Mid Sweep   | Current sweep is immediately stopped. Left over time is repurposed for ITI.
 /// #ANALYSIS_FUNC_RET_EARLY_STOP     | Mid Sweep   | Current sweep is immediately stopped without honouring the left over time in a special way.
+///
+/// @anchor AnalysisFunctionParameters Analayis function user parameters (V3 only)
+///
+/// For some analysis functions it is beneficial to send in additional data
+/// depending on the stimset. This is supported by adding parameters and their
+/// values via WBP_AddAnalysisParameter() to the stimset and then querying them
+/// with the help of @ref AnalysisFunctionParameterHelpers. The parameters are
+/// stored serialized in the `WPT` wave, see GetWaveBuilderWaveTextParam() for
+/// the exact format. See TestAnalysisFunction_V3() for an example
+/// implementation.
+///
+/// If you want to enforce a list of parameters which must be present, define
+/// an additional function named like your analysis function but suffixed with
+/// `_GetParams` and return a semicolon separated list of required names. This
+/// is then checked before DAQ, and if not all required parameters are supplied
+/// DAQ is not started.
 
 /// @name Initial parameters for stimulation
 ///@{
@@ -81,6 +98,76 @@ Function TestAnalysisFunction_V2(panelTitle, eventType, ITCDataWave, headStage, 
 	variable headstage, realDataLength
 
 	printf "Analysis function version 2 called: device %s, eventType \"%s\", headstage %d\r", panelTitle, StringFromList(eventType, EVENT_NAME_LIST), headStage
+
+	return 0
+End
+
+Function TestAnalysisFunction_V3(panelTitle, s)
+	string panelTitle
+	STRUCT AnalysisFunction_V3& s
+
+	string names, name, type
+	variable numEntries, i
+
+	/// Query parameters
+	printf "User parameters: "
+	print s.params
+
+	names = AFH_GetListOfAnalysisParamNames(s.params)
+
+	numEntries = ItemsInList(names)
+	for(i = 0; i < numEntries; i += 1)
+		name = Stringfromlist(i, names)
+		type = AFH_GetAnalysisParamType(name, s.params)
+		strswitch(type)
+			case "string":
+				print AFH_GetAnalysisParamTextual(name, s.params)
+				break
+			case "variable":
+				print AFH_GetAnalysisParamTextual(name, s.params)
+				break
+			case "wave":
+				WAVE/Z wv = AFH_GetAnalysisParamWave(name, s.params)
+				print wv
+			case "wave":
+				WAVE/T/Z wvText = AFH_GetAnalysisParamTextWave(name, s.params)
+				print wvText
+				break
+			default:
+				printf "Unsupported parameter type %s\r", type
+				break
+		endswitch
+	endfor
+
+	switch(s.eventType)
+		case PRE_DAQ_EVENT:
+			// code
+			// can also return with != 0, see @ref AnalysisFunction_V3DescriptionTable
+			break
+		case PRE_SWEEP_EVENT:
+			// code
+			break
+		case MID_SWEEP_EVENT:
+			// code
+			// can also return with != 0, see @ref AnalysisFunction_V3DescriptionTable
+			break
+		case POST_SWEEP_EVENT:
+			// code
+			break
+		case POST_SET_EVENT:
+			// code
+			break
+		case POST_DAQ_EVENT:
+			// code
+			break
+		default:
+			printf "Unsupported event type %d\r", s.eventType
+			break
+	endswitch
+
+	printf "Analysis function version 3 called: device %s, eventType \"%s\", headstage %d\r", panelTitle, StringFromList(s.eventType, EVENT_NAME_LIST), s.headStage
+
+	return 0
 End
 
 Function Enforce_VC(panelTitle, eventType, ITCDataWave, headStage, realDataLength)

Packages/MIES/MIES_AnalysisFunctionHelpers.ipf
@@ -286,7 +286,7 @@ Function/S AFH_GetAnalysisFunctions(versionBitMask)
 
 	string funcList, func
 	string funcListClean = ""
-	variable numEntries, i, valid_f1, valid_f2
+	variable numEntries, i, valid_f1, valid_f2, valid_f3
 
 	funcList  = FunctionList("*", ";", "KIND:2,WIN:MIES_AnalysisFunctions.ipf")
 	funcList += FunctionList("*", ";", "KIND:2,WIN:MIES_AnalysisFunctions_PatchSeq.ipf")
@@ -300,15 +300,126 @@ Function/S AFH_GetAnalysisFunctions(versionBitMask)
 		// this allows to check if the signature of func is the same as the one of AF_PROTO_ANALYSIS_FUNC_V*
 		FUNCREF AF_PROTO_ANALYSIS_FUNC_V1 f1 = $func
 		FUNCREF AF_PROTO_ANALYSIS_FUNC_V2 f2 = $func
+		FUNCREF AF_PROTO_ANALYSIS_FUNC_V3 f3 = $func
 
 		valid_f1 = FuncRefIsAssigned(FuncRefInfo(f1))
 		valid_f2 = FuncRefIsAssigned(FuncRefInfo(f2))
+		valid_f3 = FuncRefIsAssigned(FuncRefInfo(f3))
 
 		if((valid_f1 && (versionBitMask & ANALYSIS_FUNCTION_VERSION_V1))    \
-		   || (valid_f2 && (versionBitMask & ANALYSIS_FUNCTION_VERSION_V2)))
+		   || (valid_f2 && (versionBitMask & ANALYSIS_FUNCTION_VERSION_V2)) \
+		   || (valid_f3 && (versionBitMask & ANALYSIS_FUNCTION_VERSION_V3)))
 			funcListClean = AddListItem(func, funcListClean, ";", Inf)
 		endif
 	endfor
 
 	return funcListClean
+End
+
+/// @brief Return the list of required analysis function
+/// parameters as specified by the function `$func_GetParams`
+///
+/// @param func Analysis function `V3` which must be valid and existing
+Function/S AFH_GetListOfReqAnalysisParams(func)
+	string func
+
+	FUNCREF AF_PROTO_PARAM_GETTER_V3 f = $(func + "_GetParams")
+
+	if(!FuncRefIsAssigned(FuncRefInfo(f))) // no such getter functions
+		return ""
+	endif
+
+	return f()
+End
+
+/// @defgroup AnalysisFunctionParameterHelpers Analysis Helper functions for dealing with user parameters
+
+/// @brief Return a semicolon separated list of user parameters
+///
+/// @ingroup AnalysisFunctionParameterHelpers
+/// @param params serialized parameters, usually just #AnalysisFunction_V3.params
+Function/S AFH_GetListOfAnalysisParamNames(params)
+	string params
+
+	string entry, name
+	string list = ""
+	variable i, numEntries, pos
+
+	numEntries = ItemsInList(params, ",")
+	for(i = 0; i < numEntries; i += 1)
+		entry = StringFromList(i, params, ",")
+		pos  = strsearch(entry, ":", 0)
+		ASSERT(pos != -1, "Invalid params format")
+		name = entry[0, pos - 1]
+		list = AddListItem(name, list, ";", Inf)
+	endfor
+
+	return list
+End
+
+/// @brief Return the type of the user parameter
+///
+/// @param name   parameter name
+/// @param params serialized parameters, usually just #AnalysisFunction_V3.params
+///
+/// @ingroup AnalysisFunctionParameterHelpers
+/// @return one of @ref AnalysisFunctionParameterTypes
+Function/S AFH_GetAnalysisParamType(name, params)
+	string name, params
+
+	string typeAndValue, type
+	variable pos
+
+	typeAndValue = StringByKey(name , params, ":", ",", 0)
+
+	pos = strsearch(typeAndValue, "=", 0)
+	ASSERT(pos != -1, "Invalid params format")
+	type = typeAndValue[0, pos - 1]
+	ASSERT(!IsEmpty(type) && WhichListItem(type, ANALYSIS_FUNCTION_PARAMS_TYPES) != -1, "Invalid type")
+
+	return typeAndValue[0, pos - 1]
+End
+
+/// @brief Return a numerical user parameter
+///
+/// @param name   parameter name
+/// @param params serialized parameters, usually just #AnalysisFunction_V3.params
+/// @ingroup AnalysisFunctionParameterHelpers
+Function AFH_GetAnalysisParamNumerical(name, params)
+	string name, params
+
+	return NumberByKey(name + ":variable", params, "=", ",", 0)
+End
+
+/// @brief Return a textual user parameter
+///
+/// @param name   parameter name
+/// @param params serialized parameters, usually just #AnalysisFunction_V3.params
+/// @ingroup AnalysisFunctionParameterHelpers
+Function/S AFH_GetAnalysisParamTextual(name, params)
+	string name, params
+
+	return StringByKey(name + ":string", params, "=", ",", 0)
+End
+
+/// @brief Return a numerical wave user parameter
+///
+/// @param name   parameter name
+/// @param params serialized parameters, usually just #AnalysisFunction_V3.params
+/// @ingroup AnalysisFunctionParameterHelpers
+Function/WAVE AFH_GetAnalysisParamWave(name, params)
+	string name, params
+
+	return ListToNumericWave(StringByKey(name + ":wave", params, "=", ",", 0), "|")
+End
+
+/// @brief Return a textual wave user parameter
+///
+/// @param name   parameter name
+/// @param params serialized parameters, usually just #AnalysisFunction_V3.params
+/// @ingroup AnalysisFunctionParameterHelpers
+Function/WAVE AFH_GetAnalysisParamTextWave(name, params)
+	string name, params
+
+	return ListToTextWave(StringByKey(name + ":textwave", params, "=", ",", 0), "|")
 End

Packages/MIES/MIES_AnalysisFunctionManagement.ipf
@@ -16,9 +16,10 @@ Function AFM_CallAnalysisFunctions(panelTitle, eventType)
 	string panelTitle
 	variable eventType
 
-	variable error, i, valid_f1, valid_f2, ret, DAC
-	variable realDataLength
+	variable error, i, valid_f1, valid_f2, valid_f3, ret, DAC, sweepsInSet
+	variable realDataLength, sweepNo
 	string func, setName, ctrl
+	struct AnalysisFunction_V3 s
 
 	WAVE GuiState = GetDA_EphysGuiStateNum(panelTitle)
 
@@ -28,6 +29,7 @@ Function AFM_CallAnalysisFunctions(panelTitle, eventType)
 
 	NVAR count = $GetCount(panelTitle)
 	NVAR stopCollectionPoint = $GetStopCollectionPoint(panelTitle)
+	NVAR fifoPosition = $GetFifoPosition(panelTitle)
 	WAVE statusHS = DAG_GetChannelState(panelTitle, CHANNEL_TYPE_HEADSTAGE)
 
 	WAVE/T analysisFunctions = GetAnalysisFunctionStorage(panelTitle)
@@ -44,29 +46,39 @@ Function AFM_CallAnalysisFunctions(panelTitle, eventType)
 			continue
 		endif
 
-		func = analysisFunctions[i][eventType]
+		// always prefer the generic event over the specialized ones
+		func = analysisFunctions[i][GENERIC_EVENT]
+
+		if(isEmpty(func))
+			func = analysisFunctions[i][eventType]
+		endif
 
 		if(isEmpty(func))
 			continue
 		endif
 
+		DAC = AFH_GetDACFromHeadstage(panelTitle, i)
+
+		ctrl = GetPanelControl(DAC, CHANNEL_TYPE_DAC, CHANNEL_CONTROL_WAVE)
+		// deliberately not using the GUI state wave
+		setName = GetPopupMenuString(panelTitle, ctrl)
+		sweepsInSet = IDX_NumberOfSweepsInSet(setName)
+
 		switch(eventType)
 			case PRE_DAQ_EVENT:
-			case MID_SWEEP_EVENT:
+			case PRE_SWEEP_EVENT:
+			case MID_SWEEP_EVENT: // fallthrough-by-design
+				sweepNo = DAG_GetNumericalValue(panelTitle, "SetVar_Sweep")
+				break
 			case POST_SWEEP_EVENT:
-			case POST_DAQ_EVENT:
-				// nothing to do
+			case POST_DAQ_EVENT: // fallthrough-by-design
+				sweepNo = DAG_GetNumericalValue(panelTitle, "SetVar_Sweep") - 1
 				break
 			case POST_SET_EVENT:
-				DAC = AFH_GetDACFromHeadstage(panelTitle, i)
-
-				ctrl    = GetPanelControl(DAC, CHANNEL_TYPE_DAC, CHANNEL_CONTROL_WAVE)
-				// deliberately not using the GUI state wave
-				setName = GetPopupMenuString(panelTitle, ctrl)
-
-				if(mod(count + 1, IDX_NumberOfSweepsInSet(setName)) != 0)
+				if(mod(count + 1, sweepsInSet) != 0)
 					continue
 				endif
+				sweepNo = DAG_GetNumericalValue(panelTitle, "SetVar_Sweep") - 1
 				break
 			default:
 				ASSERT(0, "Invalid eventType")
@@ -75,12 +87,13 @@ Function AFM_CallAnalysisFunctions(panelTitle, eventType)
 
 		FUNCREF AF_PROTO_ANALYSIS_FUNC_V1 f1 = $func
 		FUNCREF AF_PROTO_ANALYSIS_FUNC_V2 f2 = $func
+		FUNCREF AF_PROTO_ANALYSIS_FUNC_V3 f3 = $func
 
 		valid_f1 = FuncRefIsAssigned(FuncRefInfo(f1))
 		valid_f2 = FuncRefIsAssigned(FuncRefInfo(f2))
+		valid_f3 = FuncRefIsAssigned(FuncRefInfo(f3))
 
 		// all functions are valid
-
 		WAVE ITCDataWave = GetITCDataWave(panelTitle)
 		SetWaveLock 1, ITCDataWave
 
@@ -90,6 +103,17 @@ Function AFM_CallAnalysisFunctions(panelTitle, eventType)
 				ret = f1(panelTitle, eventType, ITCDataWave, i); AbortOnRTE
 			elseif(valid_f2)
 				ret = f2(panelTitle, eventType, ITCDataWave, i, realDataLength); AbortOnRTE
+			elseif(valid_f3)
+				s.eventType         = eventType
+				WAVE s.rawDACWave   = ITCDataWave
+				s.headstage         = i
+				s.lastValidRowIndex = realDataLength
+				s.lastKnownRowIndex = fifoPosition
+				s.sweepNo           = sweepNo
+				s.sweepsInSet       = sweepsInSet
+				s.params            = analysisFunctions[i][ANALYSIS_FUNCTION_PARAMS]
+
+				ret = f3(panelTitle, s); AbortOnRTE
 			else
 				ASSERT(0, "impossible case")
 			endif
@@ -157,5 +181,7 @@ Function AFM_UpdateAnalysisFunctionWave(panelTitle)
 
 			analysisFunctions[i][j] = func
 		endfor
+
+		analysisFunctions[i][ANALYSIS_FUNCTION_PARAMS] = ExtractAnalysisFunctionParams(stimSet)
 	endfor
 End

Packages/MIES/MIES_AnalysisFunctionPrototypes.ipf
@@ -11,7 +11,7 @@
 ///
 /// @sa MIES_AnalysisFunctions.ipf
 
-/// @deprecated Use AF_PROTO_ANALYSIS_FUNC_V2() instead
+/// @deprecated Use AF_PROTO_ANALYSIS_FUNC_V3() instead
 ///
 /// @param panelTitle  device
 /// @param eventType   eventType, one of @ref EVENT_TYPE_ANALYSIS_FUNCTIONS,
@@ -27,6 +27,8 @@ Function AF_PROTO_ANALYSIS_FUNC_V1(panelTitle, eventType, ITCDataWave, headStage
 	variable headstage
 End
 
+/// @deprecated Use AF_PROTO_ANALYSIS_FUNC_V3() instead
+///
 /// @param panelTitle     device
 /// @param eventType      eventType, one of @ref EVENT_TYPE_ANALYSIS_FUNCTIONS,
 ///                       always compare `eventType` with the constants, never use the current numerical value directly
@@ -35,7 +37,7 @@ End
 /// @param realDataLength number of rows in `ITCDataWave` with data, the total number of rows in `ITCDataWave` might be
 ///                       higher due to alignment requirements of the data acquisition hardware. `NaN` for #PRE_DAQ_EVENT events.
 ///
-/// @return see @ref AnalysisFunctionEventDescriptionTable
+/// @return see @ref AnalysisFunction_V3DescriptionTable
 Function AF_PROTO_ANALYSIS_FUNC_V2(panelTitle, eventType, ITCDataWave, headStage, realDataLength)
 	string panelTitle
 	variable eventType
@@ -43,4 +45,21 @@ Function AF_PROTO_ANALYSIS_FUNC_V2(panelTitle, eventType, ITCDataWave, headStage
 	variable headstage, realDataLength
 
 	return 0
+End
+
+/// @param panelTitle device
+/// @param s          analysis event structure
+///
+/// @return see @ref AnalysisFunction_V3DescriptionTable
+Function AF_PROTO_ANALYSIS_FUNC_V3(panelTitle, s)
+	string panelTitle
+	STRUCT AnalysisFunction_V3 &s
+
+	return 0
+End
+
+/// @brief Prototype function for the user supplied parameter getter functions
+///
+Function/S AF_PROTO_PARAM_GETTER_V3()
+
 End

Packages/MIES/MIES_Constants.ipf
@@ -426,16 +426,30 @@ Constant MID_SWEEP_EVENT  = 1
 Constant POST_SWEEP_EVENT = 2
 Constant POST_SET_EVENT   = 3
 Constant POST_DAQ_EVENT   = 4
+Constant PRE_SWEEP_EVENT  = 5
 /// @}
 
+Constant GENERIC_EVENT = 6 ///< Only used for internal bookkeeping. Never
+                           ///  send to analysis functions.
+
 /// Number of @ref EVENT_TYPE_ANALYSIS_FUNCTIONS
-Constant TOTAL_NUM_EVENTS   = 5
+Constant TOTAL_NUM_EVENTS   = 7
+
+/// Column for GetAnalysisFunctionStorage(). Same value as #TOTAL_NUM_EVENTS
+/// but more readable.
+Constant ANALYSIS_FUNCTION_PARAMS = 7
+
+StrConstant ANALYSIS_FUNCTION_PARAMS_LBN = "Function params"
 
 /// Human readable names for @ref EVENT_TYPE_ANALYSIS_FUNCTIONS
-StrConstant EVENT_NAME_LIST = "Pre DAQ;Mid Sweep;Post Sweep;Post Set;Post DAQ"
+StrConstant EVENT_NAME_LIST = "Pre DAQ;Mid Sweep;Post Sweep;Post Set;Post DAQ;Pre Sweep;Generic"
 
 /// Labnotebook entries
-StrConstant EVENT_NAME_LIST_LBN = "Pre DAQ function;Mid Sweep function;Post Sweep function;Post Set function;Post DAQ function"
+StrConstant EVENT_NAME_LIST_LBN = "Pre DAQ function;Mid Sweep function;Post Sweep function;Post Set function;Post DAQ function;Pre Sweep function;Generic function"
+
+/// List of valid analysis function types
+/// @anchor AnalysisFunctionParameterTypes
+StrConstant ANALYSIS_FUNCTION_PARAMS_TYPES = "variable;string;wave;textwave"
 
 /// Special return values for analysis functions. See also @ref
 /// AnalysisFunctionReturnTypes.
@@ -451,6 +465,7 @@ Constant ANALYSIS_FUNC_RET_EARLY_STOP  = -101
 /// @{
 Constant ANALYSIS_FUNCTION_VERSION_V1  = 0x0001
 Constant ANALYSIS_FUNCTION_VERSION_V2  = 0x0002
+Constant ANALYSIS_FUNCTION_VERSION_V3  = 0x0004
 Constant ANALYSIS_FUNCTION_VERSION_ALL = 0xFFFF
 /// @}
 
@@ -488,7 +503,7 @@ Constant HARDWARE_DAC_EXTERNAL_TRIGGER = 0x1
 Constant DA_EPHYS_PANEL_VERSION     = 27
 Constant DATABROWSER_PANEL_VERSION  = 7
 Constant SWEEPBROWSER_PANEL_VERSION = 3
-Constant WAVEBUILDER_PANEL_VERSION  = 3
+Constant WAVEBUILDER_PANEL_VERSION  = 4
 
 /// Version of the labnotebooks (numerical and textual)
 ///
@@ -497,7 +512,7 @@ Constant WAVEBUILDER_PANEL_VERSION  = 3
 /// - Changed names of entries
 /// - Changed units or meaning of entries
 /// - New/Changed layers of entries
-Constant LABNOTEBOOK_VERSION = 14
+Constant LABNOTEBOOK_VERSION = 15
 
 /// @name The channel numbers for the different ITC devices used for accesssing
 ///       the TTLs

Packages/MIES/MIES_DAEphys.ipf
@@ -2050,7 +2050,7 @@ static Function DAP_CheckHeadStage(panelTitle, headStage, mode)
 	variable DACchannel, ADCchannel, DAheadstage, ADheadstage, DAGain, ADGain, realMode
 	variable gain, scale, clampMode, i, ampConnState, needResetting
 	variable DAGainMCC, ADGainMCC
-	string DAUnitMCC, ADUnitMCC
+	string DAUnitMCC, ADUnitMCC, params, names, requestedNames, diff
 
 	if(DAP_DeviceIsUnlocked(panelTitle))
 		printf "(%s) Device is unlocked. Please lock the device.\r", panelTitle
@@ -2285,6 +2285,19 @@ static Function DAP_CheckHeadStage(panelTitle, headStage, mode)
 					printf "(%s) The event type \"%s\" for stim set %s can not be used together with foreground DAQ\r", panelTitle, StringFromList(i, EVENT_NAME_LIST), dacWave
 					ControlWindowToFront()
 					return 1
+				elseif(i == GENERIC_EVENT)
+					// check that all required user parameters are supplied
+					requestedNames = AFH_GetListOfReqAnalysisParams(func)
+					if(!IsEmpty(requestedNames))
+						params = ExtractAnalysisFunctionParams(stimSet)
+						names = AFH_GetListOfAnalysisParamNames(params)
+						diff = GetListDifference(requestedNames, names)
+						if(!IsEmpty(diff))
+							printf "(%s) The required analysis parameters requested by %s for stim set %s were not all supplied (missing are: %s)\r", panelTitle, func, dacWave, diff
+							ControlWindowToFront()
+							return 1
+						endif
+					endif
 				endif
 			endfor
 		endif

Packages/MIES/MIES_DataAcquisition_Multi.ipf
@@ -332,6 +332,7 @@ static Function DQM_BkrdDataAcq(panelTitle, [triggerMode])
 		DQ_StartITCDeviceTimer(panelTitle)
 	endif
 
+	AFM_CallAnalysisFunctions(panelTitle, PRE_SWEEP_EVENT)
 	HW_StartAcq(HARDWARE_ITC_DAC, ITCDeviceIDGlobal, triggerMode=triggerMode, flags=HARDWARE_ABORT_ON_ERROR)
 	ED_MarkSweepStart(panelTitle)
 	TFH_StartFIFOStopDaemon(HARDWARE_ITC_DAC, ITCDeviceIDGlobal)

Packages/MIES/MIES_DataAcquisition_Single.ipf
@@ -69,6 +69,7 @@ Function DQS_DataAcq(panelTitle)
 		DQ_StartITCDeviceTimer(panelTitle) // starts a timer for each ITC device. Timer is used to do real time ITI timing.
 	endif
 
+	AFM_CallAnalysisFunctions(panelTitle, PRE_SWEEP_EVENT)
 	HW_StartAcq(HARDWARE_ITC_DAC, ITCDeviceIDGlobal, flags=HARDWARE_ABORT_ON_ERROR)
 	ED_MarkSweepStart(panelTitle)
 
@@ -101,6 +102,7 @@ Function DQS_BkrdDataAcq(panelTitle)
 		DQ_StartITCDeviceTimer(panelTitle) // starts a timer for each ITC device. Timer is used to do real time ITI timing.
 	endif
 
+	AFM_CallAnalysisFunctions(panelTitle, PRE_SWEEP_EVENT)
 	HW_StartAcq(HARDWARE_ITC_DAC, ITCDeviceIDGlobal, flags=HARDWARE_ABORT_ON_ERROR)
 	ED_MarkSweepStart(panelTitle)
 

commit 55aabda43409d183039f00f0d5ea7cad529b3800

TestAnalysisFunction_V3: Fix duplicated case label
We want to call AFH_GetAnalysisParamTextWave for "textwave". Bug
introduced in 55ee1ccf (Introduce Analysis functions V3, 2018-01-26).

Packages/MIES/MIES_AnalysisFunctions.ipf
@@ -222,7 +222,7 @@ Function TestAnalysisFunction_V3(panelTitle, s)
 			case "wave":
 				WAVE/Z wv = AFH_GetAnalysisParamWave(name, s.params)
 				print wv
-			case "wave":
+			case "textwave":
 				WAVE/T/Z wvText = AFH_GetAnalysisParamTextWave(name, s.params)
 				print wvText
 				break