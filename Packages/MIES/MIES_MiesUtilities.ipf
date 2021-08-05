AllenInstitute/MIES

commit 7fd21c6bf0ef17541239d7d34853f489ff562d47

Set a custom logging template for the ZeroMQ-XOP
We can reuse what we already have in LOG_GenerateEntryTemplate.

The log entries then look like

{"direction":"Incoming","exp":"Untitled","id":"0b6da49","source":"XOP","str":"abcd","ts":"2021-04-12T14:55:19.163+02:00"}

Packages/MIES/MIES_MiesUtilities.ipf
@@ -7161,3 +7161,25 @@ Function UploadLogFile()
 
 	printf "Successfully uploaded the MIES logfile. Please mention your ticket \"%s\" if you are contacting support.\r", ticket
 End
+
+/// @brief Update the logging template used by the ZeroMQ-XOP
+Function/S UpdateZeroMQXOPLoggingTemplate()
+	variable JSONid
+	string str
+
+#if exists("zeromq_set_logging_template")
+
+	JSONid = LOG_GenerateEntryTemplate("XOP")
+
+	str = JSON_Dump(JSONid)
+	JSON_Release(JSONid)
+
+	zeromq_set_logging_template(str)
+#else
+
+	DEBUGPRINT("ZeroMQ XOP is not present")
+
+#endif
+
+	return str
+End

Packages/MIES/MIES_IgorHooks.ipf
@@ -248,6 +248,7 @@ static Function IgorStartOrNewHook(igorApplicationNameStr)
 	PS_FixPackageLocation(PACKAGE_MIES)
 
 	LOG_MarkSessionStart(PACKAGE_MIES)
+	UpdateZeroMQXOPLoggingTemplate()
 
 	miesVersion = ROStr(GetMiesVersion())
 	LOG_AddEntry(PACKAGE_MIES, "start", keys = {"version", "computername", "username", "igorinfo"}, \

Packages/MIES/MIES_Logging.ipf
@@ -45,7 +45,7 @@ End
 /// @brief Generate a JSON text with all required entries
 ///
 /// Caller is responsible for the JSON text memory.
-threadsafe static Function LOG_GenerateEntryTemplate(string source)
+threadsafe Function LOG_GenerateEntryTemplate(string source)
 	variable JSONid
 
 	JSONid = JSON_New()

commit 41ddb6d68caafcb4b78f14acf54ba7a41ec9b6d4

UpdateZeroMQXOPLoggingTemplate: Remove bogus returned string
The returned string is null in case the ZeroMQ XOP is not present.

As we don't use the return value anywhere we can just remove it. Bug
introduced in 7fd21c6b (Set a custom logging template for the ZeroMQ-XOP, 2021-04-12).

Packages/MIES/MIES_MiesUtilities.ipf
@@ -6989,7 +6989,7 @@ Function UploadLogFiles()
 End
 
 /// @brief Update the logging template used by the ZeroMQ-XOP
-Function/S UpdateZeroMQXOPLoggingTemplate()
+Function UpdateZeroMQXOPLoggingTemplate()
 	variable JSONid
 	string str
 
@@ -7006,8 +7006,6 @@ Function/S UpdateZeroMQXOPLoggingTemplate()
 	DEBUGPRINT("ZeroMQ XOP is not present")
 
 #endif
-
-	return str
 End
 
 /// @brief Return the disc location of the (possibly non-existing) ZeroMQ-XOP logfile