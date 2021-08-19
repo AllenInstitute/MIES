AllenInstitute/MIES

commit 6a43fbcd5c4a7a7745c6fc7e4a97646c65b03565

PatchSeqSubThreshold: Calculate the resistance of a passed sweep
Original-CommitID: 009c980a7dd84ce74c002d31da7d5fbcb894561c

Packages/MIES/MIES_AnalysisFunctions.ipf
@@ -861,6 +861,14 @@ End
 ///    rmsLongPassed  = GetLastSettingIndep(numericalValues, sweepNo, LABNOTEBOOK_USER_PREFIX + PATCHSEQ_LBN_RMS_LONG_PASSED, UNKNOWN_MODE)
 ///    targetVPassed  = GetLastSettingIndep(numericalValues, sweepNo, LABNOTEBOOK_USER_PREFIX + PATCHSEQ_LBN_TARGETV_PASSED, UNKNOWN_MODE)
 ///
+///    // get fitted resistance from last passing sweep
+///    variable lastSweepNo
+///    WAVE/Z sweeps = AFH_GetSweepsFromSameRACycle(numericalValues, sweepNo)
+///    ASSERT(WaveExists(sweeps), "Missing RA cycle information, maybe the sweep is too old?")
+///    lastSweepNo = sweeps[DimSize(sweeps, ROWS) - 1]
+///    WAVE/Z resistanceFitted = GetLastSetting(numericalValues, lastSweepNo, LABNOTEBOOK_USER_PREFIX + "ResistanceFromFit", UNKNOWN_MODE)
+///	   ASSERT(WaveExists(resistanceFitted), "Expected fitted resistance data")
+///	   // resistance for the first headstage can be found in resistanceFitted[0]
 /// \endrst
 ///
 /// @verbatim
@@ -958,6 +966,22 @@ Function PatchSeqSubThreshold(panelTitle, eventType, ITCDataWave, headStage, rea
 				// not enough sweeps left to pass the set
 				skipToEnd = (sweepsInSet - acquiredSweepsInSet) < (PATCHSEQ_NUM_SWEEPS_PASSED - passesInSet)
 			else
+				// sweep passed
+
+				WAVE/Z sweep = GetSweepWave(panelTitle, sweepNo)
+				ASSERT(WaveExists(sweep), "Expected a sweep for evaluation")
+
+				Make/D/FREE/N=(LABNOTEBOOK_LAYER_COUNT) deltaV     = NaN
+				Make/D/FREE/N=(LABNOTEBOOK_LAYER_COUNT) deltaI     = NaN
+				Make/D/FREE/N=(LABNOTEBOOK_LAYER_COUNT) resistance = NaN
+
+				CalculateTPLikePropsFromSweep(numericalValues, textualValues, sweep, deltaI, deltaV, resistance)
+
+				ED_AddEntryToLabnotebook(panelTitle, "Delta I", deltaI, unit = "I")
+				ED_AddEntryToLabnotebook(panelTitle, "Delta V", deltaV, unit = "V")
+
+				PlotResistanceGraph(panelTitle)
+
 				if(passesInSet >= PATCHSEQ_NUM_SWEEPS_PASSED)
 					skipToEnd = 1
 				else

commit a7a5a75ba5956f4ca4697e7487b6263764ed1a28

AdjustDAScale/PSQ_DAScale: Use correct unit for "Delta I" labnotebook entry
The SI unit for current is of course Amperes (A) and not I. Bug
introduced in 6a43fbcd (PatchSeqSubThreshold: Calculate the resistance
of a passed sweep, 2017-09-27).

Packages/MIES/MIES_AnalysisFunctions.ipf
@@ -754,7 +754,7 @@ Function AdjustDAScale(panelTitle, eventType, DAQDataWave, headStage, realDataLe
 
 	CalculateTPLikePropsFromSweep(numericalValues, textualValues, sweep, deltaI, deltaV, resistance)
 
-	ED_AddEntryToLabnotebook(panelTitle, "Delta I", deltaI, unit = "I")
+	ED_AddEntryToLabnotebook(panelTitle, "Delta I", deltaI, unit = "A")
 	ED_AddEntryToLabnotebook(panelTitle, "Delta V", deltaV, unit = "V")
 
 	FitResistance(panelTitle)
@@ -1162,7 +1162,7 @@ Function ReachTargetVoltage(string panelTitle, STRUCT AnalysisFunction_V3& s)
 
 			CalculateTPLikePropsFromSweep(numericalValues, textualValues, sweep, deltaI, deltaV, resistance)
 
-			ED_AddEntryToLabnotebook(panelTitle, "Delta I", deltaI, unit = "I")
+			ED_AddEntryToLabnotebook(panelTitle, "Delta I", deltaI, unit = "A")
 			ED_AddEntryToLabnotebook(panelTitle, "Delta V", deltaV, unit = "V")
 
 			FitResistance(panelTitle, showPlot = 1)

Packages/MIES/MIES_AnalysisFunctions_PatchSeq.ipf
@@ -1574,7 +1574,7 @@ Function PSQ_DAScale(panelTitle, s)
 
 					CalculateTPLikePropsFromSweep(numericalValues, textualValues, sweep, deltaI, deltaV, resistance)
 
-					ED_AddEntryToLabnotebook(panelTitle, "Delta I", deltaI, unit = "I")
+					ED_AddEntryToLabnotebook(panelTitle, "Delta I", deltaI, unit = "A")
 					ED_AddEntryToLabnotebook(panelTitle, "Delta V", deltaV, unit = "V")
 
 					FitResistance(panelTitle, showPlot = showPlot)

Packages/MIES/MIES_Constants.ipf
@@ -633,7 +633,7 @@ Constant ANALYSISBROWSER_PANEL_VERSION    =  1
 /// - Changed names of entries
 /// - Changed units or meaning of entries
 /// - New/Changed layers of entries
-Constant LABNOTEBOOK_VERSION = 46
+Constant LABNOTEBOOK_VERSION = 47
 
 /// Version of the stimset wave note
 Constant STIMSET_NOTE_VERSION = 7