AllenInstitute/MIES

commit 857e9fe6dc0929f590966dd3935d56299f62f4a1

DAEphys: Add controls for tweaking the decimation for DAQ

Packages/MIES/MIES_Constants.ipf
@@ -588,7 +588,7 @@ Constant HARDWARE_DAC_EXTERNAL_TRIGGER = 0x1
 /// @}
 
 /// Used to upgrade the GuiStateWave as well as the DA Ephys panel
-Constant DA_EPHYS_PANEL_VERSION     = 42
+Constant DA_EPHYS_PANEL_VERSION     = 43
 Constant DATABROWSER_PANEL_VERSION  = 10
 Constant SWEEPBROWSER_PANEL_VERSION = 3
 Constant WAVEBUILDER_PANEL_VERSION  = 8

Packages/MIES/MIES_DAEphys.ipf
@@ -404,6 +404,9 @@ Function DAP_EphysPanelStartUpSettings()
 
 	CheckBox Check_Settings_NwbExport WIN = $panelTitle,value= 0
 
+	PopupMenu Popup_Settings_DecMethod, mode=2, popvalue="MinMax"
+	SetVariable setvar_Settings_DecMethodFac, WIN = $panelTitle, value = _NUM:-1
+
 	SetVariable min_AsyncAD_00 WIN = $panelTitle,value= _NUM:0
 	SetVariable max_AsyncAD_00 WIN = $panelTitle,value= _NUM:0
 	CheckBox check_AsyncAlarm_00  WIN = $panelTitle,value= 0
@@ -1992,7 +1995,7 @@ Function DAP_CheckSettings(panelTitle, mode)
 	string panelTitle
 	variable mode
 
-	variable numDACs, numADCs, numHS, numEntries, i, clampMode, headstage
+	variable numDACs, numADCs, numHS, numEntries, i, clampMode, headstage, decFactor
 	variable ampSerial, ampChannelID, minValue, maxValue, leftOverBytes, hardwareType
 	variable lastStartSeconds, lastITI, nextStart, leftTime, sweepNo, validSampInt
 	string ctrl, endWave, ttlWave, dacWave, refDacWave, reqParams
@@ -2368,6 +2371,13 @@ Function DAP_CheckSettings(panelTitle, mode)
 					ControlWindowToFront()
 					return 1
 				endif
+			endif
+
+			decFactor = DAG_GetNumericalValue(panelTitle, "setvar_Settings_DecMethodFac")
+			if(!(decFactor == -1 || decFactor > 1))
+				printf "(%s) The decimation factor %d is not valid.\r", panelTitle, decFactor
+				ControlWindowToFront()
+				return 1
 			endif
 		endif
 

commit 602e1453ef9617216673374234ca08c121ec56f2

DA_Ephys: Use the correct GUI procedure for Popup_Settings_DecMethod
Broken since 857e9fe6 (DAEphys: Add controls for tweaking the decimation
for DAQ, 2019-08-23).

Packages/MIES/MIES_Constants.ipf
@@ -621,7 +621,7 @@ Constant HARDWARE_DAC_EXTERNAL_TRIGGER = 0x1
 /// @}
 
 /// Used to upgrade the GuiStateWave as well as the DA Ephys panel
-Constant DA_EPHYS_PANEL_VERSION           = 53
+Constant DA_EPHYS_PANEL_VERSION           = 54
 Constant DATA_SWEEP_BROWSER_PANEL_VERSION = 30
 Constant WAVEBUILDER_PANEL_VERSION        = 11
 Constant ANALYSISBROWSER_PANEL_VERSION    =  1

Packages/MIES/MIES_DAEphys_Macro.ipf
@@ -4318,7 +4318,7 @@ Window DA_Ephys() : Panel
 	SetVariable setvar_Settings_OsciUpdExt,userdata(tabnum)="5"
 	SetVariable setvar_Settings_OsciUpdExt,userdata(tabcontrol)="ADC"
 	SetVariable setvar_Settings_OsciUpdExt,limits={0,1000,1},value=_NUM:10
-	PopupMenu Popup_Settings_DecMethod,pos={40.00,582.00},size={189.00,19.00},bodyWidth=80,disable=1,proc=DAP_PopMenuProc_FixedSampInt
+	PopupMenu Popup_Settings_DecMethod,pos={40.00,582.00},size={189.00,19.00},bodyWidth=80,disable=1,proc=DAP_PopMenuProc_UpdateGuiState
 	PopupMenu Popup_Settings_DecMethod,title="Decimation Method"
 	PopupMenu Popup_Settings_DecMethod,help={"Decimation methods for speeding up the display"}
 	PopupMenu Popup_Settings_DecMethod,userdata(tabnum)="5"