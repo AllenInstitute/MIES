c8ef9817945ae9656d45dcc8d8943514c0a62ba5
Thomas Braun <thomas.braun@byte-physics.de>
2021-11-09 15:48:36 +0100

    tools/cleanup-dotfiles.sh: Add script for cleaning up dot files
    Unfortunately the output of dot -T canon is not stable, so calling it
    multiple times always gives a slightly different output.

---
+++ tools/cleanup-dotfiles.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+git --version > /dev/null
+if [ $? -ne 0 ]
+then
+  echo "Could not find git executable"
+  exit 1
+fi
+
+top_level=$(git rev-parse --show-toplevel)
+
+if [ -z "$top_level" ]
+then
+  echo "This is not a git repository"
+  exit 1
+fi
+
+cd $top_level/Packages/doc/dot
+
+for i in $(ls *patch-seq*.dot)
+do
+  dot -T canon $i -o $i.opt
+  dos2unix -q $i.opt
+  mv $i.opt $i
+done
