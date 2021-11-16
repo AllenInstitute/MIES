AllenInstitute/MIES

commit e5082ec29f08e4bd77aaba0f81632c327acd6284

LayoutGraph/BSP_AddTracesForEpochs: Use a separate axis for epoch info
This makes the visualisation clearer.

Packages/MIES/MIES_BrowserSettingsPanel.ipf
@@ -1493,6 +1493,9 @@ Function BSP_AddTracesForEpochs(string win)
 		yaxis = traceInfos[j][%YAXIS]
 		xaxis = traceInfos[j][%XAXIS]
 
+		// use our own y axis
+		yaxis = ReplaceString("_DA", yaxis, "_EP_DA")
+
 		headstage   = str2num(traceInfos[j][%headstage])
 		sweepNumber = str2num(traceInfos[j][%sweepNumber])
 
@@ -1555,10 +1558,19 @@ Function BSP_AddTracesForEpochs(string win)
 		sprintf level_4_trace, "level%d_x_sweep%d_HS%d", 4, sweepNumber, headstage
 
 		AppendToGraph/W=$win/L=$yAxis levels_y[][0]/TN=$level_0_trace vs levels_x[][0]
+		TUD_SetUserData(win, level_0_trace, "YAXIS", yaxis)
+
 		AppendToGraph/W=$win/L=$yAxis levels_y[][1]/TN=$level_1_trace vs levels_x[][1]
+		TUD_SetUserData(win, level_1_trace, "YAXIS", yaxis)
+
 		AppendToGraph/W=$win/L=$yAxis levels_y[][2]/TN=$level_2_trace vs levels_x[][2]
+		TUD_SetUserData(win, level_2_trace, "YAXIS", yaxis)
+
 		AppendToGraph/W=$win/L=$yAxis levels_y[][3]/TN=$level_3_trace vs levels_x[][3]
+		TUD_SetUserData(win, level_3_trace, "YAXIS", yaxis)
+
 		AppendToGraph/W=$win/L=$yAxis levels_y[][4]/TN=$level_4_trace vs levels_x[][4]
+		TUD_SetUserData(win, level_4_trace, "YAXIS", yaxis)
 
 		[c] = GetTraceColor(0)
 		ModifyGraph/W=$win marker($level_0_trace)=10, mode($level_0_trace)=4, rgb($level_0_trace)=(c.red, c.green, c.blue)

Packages/MIES/MIES_MiesUtilities.ipf
@@ -11,7 +11,9 @@
 /// @file MIES_MiesUtilities.ipf
 /// @brief This file holds utility functions which need to know about MIES internals.
 
-static Constant ADC_SLOT_MULTIPLIER = 4
+static Constant ADC_SLOT_MULTIPLIER   = 4
+static Constant EPOCH_SLOT_MULTIPLIER = 3
+
 static Constant NUM_CHANNEL_TYPES   = 3
 
 static Constant GET_LB_MODE_NONE  = 0
@@ -2205,7 +2207,7 @@ End
 /// For overlayed channels we have up to three blocks (DA, AD, TTL) in that order.
 Function LayoutGraph(string win, STRUCT TiledGraphSettings &tgs)
 
-	variable i, numSlots, headstage,  numBlocksTTL, numBlocks, spacePerSlot
+	variable i, numSlots, headstage,  numBlocksTTL, numBlocks, numBlocksEpoch, spacePerSlot
 	variable numBlocksDA, numBlocksAD, first, firstFreeAxis, lastFreeAxis, orientation
 	variable numBlocksUnassocDA, numBlocksUnassocAD, numBlocksHS
 	string graph, regex, freeAxis, axis
@@ -2229,9 +2231,14 @@ Function LayoutGraph(string win, STRUCT TiledGraphSettings &tgs)
 	if(tgs.overLayChannels)
 		// up to three blocks
 
-		regex = ".*DA$"
+		// (?<! is a negative look behind assertion
+		regex = ".*(?<!EP_)DA$"
 		WAVE/T/Z DAaxes = GrepWave(allVerticalAxes, regex)
 		numBlocksDA = WaveExists(DAaxes) ? DimSize(DAaxes, ROWS) : 0
+
+		regex = ".*EP_DA$"
+		WAVE/T/Z Epochaxes = GrepWave(allVerticalAxes, regex)
+		numBlocksEpoch = WaveExists(Epochaxes) ? DimSize(Epochaxes, ROWS) : 0
 
 		regex = ".*AD$"
 		WAVE/T/Z ADaxes = GrepWave(allVerticalAxes, regex)
@@ -2241,13 +2248,17 @@ Function LayoutGraph(string win, STRUCT TiledGraphSettings &tgs)
 		WAVE/T/Z TTLaxes = GrepWave(allVerticalAxes, regex)
 		numBlocksTTL = WaveExists(TTLaxes) ? DimSize(TTLaxes, ROWS) : 0
 
-		numBlocks = numBlocksAD + numBlocksDA + numBlocksTTL
-		numSlots = ADC_SLOT_MULTIPLIER * numBlocksAD + numBlocksDA + numBlocksTTL
+		numBlocks = numBlocksAD + numBlocksDA + numBlocksTTL + numBlocksEpoch
+		numSlots = ADC_SLOT_MULTIPLIER * numBlocksAD + numBlocksDA + numBlocksTTL + EPOCH_SLOT_MULTIPLIER * numBlocksEpoch
 
 		spacePerSlot = (1.0 - (numBlocks - 1) * GRAPH_DIV_SPACING) / numSlots
 
 		if(WaveExists(DAaxes))
 			EnableAxis(graph, DAaxes, spacePerSlot, first, last)
+		endif
+
+		if(WaveExists(Epochaxes))
+			EnableAxis(graph, Epochaxes, EPOCH_SLOT_MULTIPLIER * spacePerSlot, first, last)
 		endif
 
 		if(WaveExists(ADaxes))
@@ -2293,6 +2304,11 @@ Function LayoutGraph(string win, STRUCT TiledGraphSettings &tgs)
 	WAVE/T/Z axes = GrepWave(allVerticalAxes, regex)
 	numBlocksDA = WaveExists(axes) ? DimSize(axes, ROWS) : 0
 
+	// epoch info for associated DA channels
+	regex = ".*col0_EP_DA_(?:[[:digit:]]{1,2})_HS_(?:[[:digit:]]{1,2})$"
+	WAVE/T/Z axes = GrepWave(allVerticalAxes, regex)
+	numBlocksEpoch = WaveExists(axes) ? DimSize(axes, ROWS) : 0
+
 	// associated AD channels
 	regex = ".*col0_AD_(?:[[:digit:]]{1,2})_HS_(?:[[:digit:]]{1,2})$"
 	WAVE/T/Z axes = GrepWave(allVerticalAxes, regex)
@@ -2315,8 +2331,8 @@ Function LayoutGraph(string win, STRUCT TiledGraphSettings &tgs)
 	// Unassoc DA: 4 slots
 	// TTL: 1 slot per ttlsWithBits
 
-	numBlocks = numBlocksAD + numBlocksDA + numBlocksUnassocDA + numBlocksUnassocAD + numBlocksTTL
-	numSlots = ADC_SLOT_MULTIPLIER * numBlocksAD + numBlocksDA + numBlocksUnassocDA + ADC_SLOT_MULTIPLIER * numBlocksUnassocAD + numBlocksTTL
+	numBlocks = numBlocksAD + numBlocksDA + numBlocksUnassocDA + numBlocksUnassocAD + numBlocksTTL + numBlocksEpoch
+	numSlots = ADC_SLOT_MULTIPLIER * numBlocksAD + numBlocksDA + numBlocksUnassocDA + ADC_SLOT_MULTIPLIER * numBlocksUnassocAD + numBlocksTTL + EPOCH_SLOT_MULTIPLIER * numBlocksEpoch
 
 	spacePerSlot = (1.0 - (numBlocks - 1) * GRAPH_DIV_SPACING) / numSlots
 
@@ -2324,13 +2340,21 @@ Function LayoutGraph(string win, STRUCT TiledGraphSettings &tgs)
 	// headstages with associated channels
 	for(i = 0; i < numBlocksHS; i += 1)
 		headstage = headstages[i]
-		regex = ".*DA_(?:[[:digit:]]{1,2})_HS_" + num2str(headstage)
+		// (?<! is a negative look behind assertion
+		regex = ".*(?<!EP_)DA_(?:[[:digit:]]{1,2})_HS_" + num2str(headstage)
 		WAVE/T/Z axes = GrepWave(allVerticalAxes, regex)
 
 		lastFreeAxis = last
 
 		if(WaveExists(axes))
 			EnableAxis(graph, axes, spacePerSlot, first, last)
+		endif
+
+		regex = ".*EP_DA_(?:[[:digit:]]{1,2})_HS_" + num2str(headstage)
+		WAVE/T/Z axes = GrepWave(allVerticalAxes, regex)
+
+		if(WaveExists(axes))
+			EnableAxis(graph, axes, EPOCH_SLOT_MULTIPLIER * spacePerSlot, first, last)
 		endif
 
 		regex = ".*AD_(?:[[:digit:]]{1,2})_HS_" + num2str(headstage)
@@ -2363,6 +2387,9 @@ Function LayoutGraph(string win, STRUCT TiledGraphSettings &tgs)
 		ASSERT(WaveExists(axes), "Unexpected number of matches")
 		EnableAxis(graph, axes, ADC_SLOT_MULTIPLIER * spacePerSlot, first, last)
 	endfor
+
+	// BSP_AddTracesForEpochs ignores unassociated DA channels
+	// so we don't have epoch axes here
 
 	// TTLs
 	for(i = 0; i < numBlocksTTL; i += 1)

commit 5ff3ca92563665e17e6917fbe87d52c37f035869

BSP_AddTracesForEpochs: Properly remove traces and trace user data
When we use our own trace user data (TUD_XXX) we always need to remove
that user data as well.

Otherwise functions querying the user data get wrong answers. Bug
introduced in e5082ec2 (LayoutGraph/BSP_AddTracesForEpochs: Use a
separate axis for epoch info, 2021-11-04).

We need to set four user data keys as TUD_SetUserDataFromWaves requires
us to set continuous ranges.

Packages/MIES/MIES_BrowserSettingsPanel.ipf
@@ -1500,6 +1500,24 @@ End
 
 #if IgorVersion() >= 9.0
 
+static Function BSP_RemoveTraces(string graph)
+	variable i, numEntries
+	string trace
+
+	WAVE/Z/T traces = TUD_GetUserDataAsWave(graph, "traceName", keys = {"traceType"}, values = {"EpochVis"})
+
+	if(!WaveExists(traces))
+		return NaN
+	endif
+
+	numEntries = DimSize(traces, ROWS)
+	for(i = 0; i < numEntries; i += 1)
+		trace = traces[i]
+		RemoveFromGraph/W=$graph $trace
+		TUD_RemoveUserData(graph, trace)
+	endfor
+End
+
 /// @brief Debug function to add traces with epoch information
 Function BSP_AddTracesForEpochs(string win)
 
@@ -1517,7 +1535,7 @@ Function BSP_AddTracesForEpochs(string win)
 	endif
 
 	DFREF dfr = GetEpochsVisualizationFolder(BSP_GetFolder(win, MIES_BSP_PANEL_FOLDER))
-	RemoveTracesFromGraph(win, dfr = dfr)
+	BSP_RemoveTraces(win)
 
 	WAVE/T/Z traceInfos = GetTraceInfos(win, addFilterKeys = {"channelType", "AssociatedHeadstage"}, addFilterValues = {"DA", "1"})
 
@@ -1599,19 +1617,19 @@ Function BSP_AddTracesForEpochs(string win)
 		sprintf level_4_trace, "%s_level%d_x_sweep%d_HS%d", GetTraceNamePrefix(traceIndex++), 4, sweepNumber, headstage
 
 		AppendToGraph/W=$win/L=$yAxis levels_y[][0]/TN=$level_0_trace vs levels_x[][0]
-		TUD_SetUserData(win, level_0_trace, "YAXIS", yaxis)
+		TUD_SetUserDataFromWaves(win, level_0_trace, {"traceType", "occurence", "XAXIS", "YAXIS"}, {"EpochVis", "", "bottom", yaxis})
 
 		AppendToGraph/W=$win/L=$yAxis levels_y[][1]/TN=$level_1_trace vs levels_x[][1]
-		TUD_SetUserData(win, level_1_trace, "YAXIS", yaxis)
+		TUD_SetUserDataFromWaves(win, level_1_trace, {"traceType", "occurence", "XAXIS", "YAXIS"}, {"EpochVis", "", "bottom", yaxis})
 
 		AppendToGraph/W=$win/L=$yAxis levels_y[][2]/TN=$level_2_trace vs levels_x[][2]
-		TUD_SetUserData(win, level_2_trace, "YAXIS", yaxis)
+		TUD_SetUserDataFromWaves(win, level_2_trace, {"traceType", "occurence", "XAXIS", "YAXIS"}, {"EpochVis", "", "bottom", yaxis})
 
 		AppendToGraph/W=$win/L=$yAxis levels_y[][3]/TN=$level_3_trace vs levels_x[][3]
-		TUD_SetUserData(win, level_3_trace, "YAXIS", yaxis)
+		TUD_SetUserDataFromWaves(win, level_3_trace, {"traceType", "occurence", "XAXIS", "YAXIS"}, {"EpochVis", "", "bottom", yaxis})
 
 		AppendToGraph/W=$win/L=$yAxis levels_y[][4]/TN=$level_4_trace vs levels_x[][4]
-		TUD_SetUserData(win, level_4_trace, "YAXIS", yaxis)
+		TUD_SetUserDataFromWaves(win, level_4_trace, {"traceType", "occurence", "XAXIS", "YAXIS"}, {"EpochVis", "", "bottom", yaxis})
 
 		[c] = GetTraceColor(0)
 		ModifyGraph/W=$win marker($level_0_trace)=10, mode($level_0_trace)=4, rgb($level_0_trace)=(c.red, c.green, c.blue)