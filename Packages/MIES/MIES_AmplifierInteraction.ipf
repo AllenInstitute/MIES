AllenInstitute/MIES

commit f1774338705bfb4ae0d05b7ab40e4fe031192ba2

MIES: Add ifdef'ed ModuleName pragma
This allows the unit testing framework to access static functions and
constants.

Original-CommitID: 5c041ae0c7f4df5785d196cbec6bcce372fe29c4

Packages/MIES/MIES_AmplifierInteraction.ipf
@@ -2,6 +2,10 @@
 #pragma rtGlobals=3		// Use modern global access method and strict wave access.
 #pragma rtFunctionErrors=1
 
+#ifdef AUTOMATED_TESTING
+#pragma ModuleName=MiesTesting
+#endif
+
 /// @file MIES_AmplifierInteraction.ipf
 /// @brief __AI__ Interface with the Axon/MCC amplifiers
 

Packages/MIES/MIES_AnalysisBrowser.ipf
@@ -3,6 +3,10 @@
 #pragma rtFunctionErrors=1
 #pragma igorVersion=7.0
 
+#ifdef AUTOMATED_TESTING
+#pragma ModuleName=MIES_AB
+#endif
+
 /// @file MIES_AnalysisBrowser.ipf
 /// @brief __AB__ Analysis browser
 ///

Packages/MIES/MIES_AnalysisBrowser_LabNotebookTPStorageBrowser.ipf
@@ -2,6 +2,10 @@
 #pragma rtGlobals=3		// Use modern global access method and strict wave access.
 #pragma rtFunctionErrors=1
 
+#ifdef AUTOMATED_TESTING
+#pragma ModuleName=MIES_LBN
+#endif
+
 /// @file MIES_AnalysisBrowser_LabNotebookTPStorageBrowser.ipf
 /// @brief __LBN__ Panels for browsing the labnotebook and the TPStorage waves
 /// in the analysis browser

Packages/MIES/MIES_AnalysisBrowser_SweepBrowser.ipf
@@ -2,6 +2,10 @@
 #pragma rtGlobals=3		// Use modern global access method and strict wave access.
 #pragma rtFunctionErrors=1
 
+#ifdef AUTOMATED_TESTING
+#pragma ModuleName=MIES_SB
+#endif
+
 /// @file MIES_AnalysisBrowser_SweepBrowser.ipf
 /// @brief __SB__  Visualization of sweep data in the analysis browser
 

Packages/MIES/MIES_AnalysisFunctionHelpers.ipf
@@ -2,6 +2,10 @@
 #pragma rtGlobals=3		// Use modern global access method and strict wave access.
 #pragma rtFunctionErrors=1
 
+#ifdef AUTOMATED_TESTING
+#pragma ModuleName=MIES_AFH
+#endif
+
 /// @file MIES_AnalysisFunctionHelpers.ipf
 /// @brief __AFH__ Helper functions for analysis function writers
 ///

Packages/MIES/MIES_AnalysisFunctionPrototypes.ipf
@@ -2,6 +2,10 @@
 #pragma rtGlobals=3		// Use modern global access method and strict wave access.
 #pragma rtFunctionErrors=1
 
+#ifdef AUTOMATED_TESTING
+#pragma ModuleName=MIES_AFP
+#endif
+
 /// @file MIES_AnalysisFunctionPrototypes.ipf
 /// @brief __AF__ Analysis functions prototypes to be called during data acquisition
 ///

Packages/MIES/MIES_AnalysisFunctions.ipf
@@ -2,6 +2,10 @@
 #pragma rtGlobals=3		// Use modern global access method and strict wave access.
 #pragma rtFunctionErrors=1
 
+#ifdef AUTOMATED_TESTING
+#pragma ModuleName=MIES_AF
+#endif
+
 /// @file MIES_AnalysisFunctions.ipf
 /// @brief __AF__ Analysis functions to be called during data acquisition
 ///

Packages/MIES/MIES_AnalysisMaster.ipf
@@ -2,6 +2,10 @@
 #pragma rtGlobals=3		// Use modern global access method and strict wave access.
 #pragma rtFunctionErrors=1
 
+#ifdef AUTOMATED_TESTING
+#pragma ModuleName=MIES_AM
+#endif
+
 /// @file MIES_AnalysisMaster.ipf
 /// @brief __AM__ Waveform analysis framework
 

commit 2a678d75d906abed1d4cd2fc43becd1a740c0ce9

MIES_AmplifierInteraction.ipf: Use unique module name
Bug introduced in f1774338 (MIES: Add ifdef'ed ModuleName pragma, 2017-02-14).

Packages/MIES/MIES_AmplifierInteraction.ipf
@@ -3,7 +3,7 @@
 #pragma rtFunctionErrors=1
 
 #ifdef AUTOMATED_TESTING
-#pragma ModuleName=MiesTesting
+#pragma ModuleName=MIES_AI
 #endif
 
 /// @file MIES_AmplifierInteraction.ipf