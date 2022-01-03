AllenInstitute/MIES

commit 5688983acb5aa8a45833592a574e498f24daaf99

run/RunWithOpts: Add instrumentation support
By setting the environment variable BAMBOO_INSTRUMENT_TESTS we can now
do instrumented tests in CI. We always do them on the main branch in CI,
as we can't set per-job variables with bamboo conveniently.

Locally RunWithOpts allows to do instrumentation on all MIES files or
only on single files as well.

Packages/Testing-MIES/UTF_HelperFunctions.ipf
@@ -93,3 +93,15 @@ Function AdjustAnalysisParamsForPSQ(string device, string stimset)
 	AFH_AddAnalysisParameter(stimset, "SamplingMultiplier", var = 4)
 	AFH_AddAnalysisParameter(stimset, "SamplingFrequency", var = samplingFrequency)
 End
+
+Function DoInstrumentation()
+#if IgorVersion() >= 9.0
+	variable instru = str2numSafe(GetEnvironmentVariable("BAMBOO_INSTRUMENT_TESTS")) == 1           \
+	                  || !cmpstr(GetEnvironmentVariable("bamboo.planRepository.branchName"), "main")
+
+	return instru
+#else
+	// no support in IP8
+	return 0
+#endif
+End

Packages/Testing-MIES/UTF_HardwareMain.ipf
@@ -42,16 +42,18 @@ Constant PSQ_TEST_HEADSTAGE = 2
 
 // Entry point for UTF
 Function run()
-	return RunWithOpts()
+	return RunWithOpts(instru = DoInstrumentation())
 End
 
 // Examples:
 // - RunWithOpts()
 // - RunWithOpts(testsuite = "UTF_Epochs.ipf")
 // - RunWithOpts(testcase = "EP_EpochTest7")
-Function RunWithOpts([string testcase, string testsuite, variable allowdebug])
+// - RunWithOpts(testcase = "EP_EpochTest7", instru = 1, traceWinList = "MIES_Epochs.ipf")
+Function RunWithOpts([string testcase, string testsuite, variable allowdebug, variable instru, string traceWinList])
 
 	variable debugMode
+	string traceOptions = ""
 	string list = ""
 	string name = "MIES with Hardware"
 
@@ -68,6 +70,22 @@ Function RunWithOpts([string testcase, string testsuite, variable allowdebug])
 	if(ParamIsDefault(testcase))
 		testcase = ""
 	endif
+
+	if(ParamIsDefault(instru))
+		instru = 0
+	else
+		instru = !!instru
+	endif
+
+	if(ParamIsDefault(traceWinList))
+		traceWinList = "MIES_.*\.ipf"
+	endif
+
+	if(!instru)
+		traceWinList = ""
+	endif
+
+	traceOptions = ReplaceNumberByKey(UTF_KEY_REGEXP, traceOptions, 1)
 
 	list = AddListItem("UTF_VeryBasicHardwareTests.ipf", list, ";", inf)
 	list = AddListItem("UTF_BasicHardwareTests.ipf", list, ";", inf)
@@ -98,9 +116,9 @@ Function RunWithOpts([string testcase, string testsuite, variable allowdebug])
 	endif
 
 	if(IsEmpty(testcase))
-		RunTest(testsuite, name = name, enableJU = 1, debugMode= debugMode)
+		RunTest(testsuite, name = name, enableJU = 1, debugMode= debugMode, traceOptions=traceOptions, traceWinList=traceWinList)
 	else
-		RunTest(testsuite, name = name, enableJU = 1, debugMode= debugMode, testcase = testcase)
+		RunTest(testsuite, name = name, enableJU = 1, debugMode= debugMode, testcase = testcase, traceOptions=traceOptions, traceWinList=traceWinList)
 	endif
 End
 

Packages/Testing-MIES/UTF_Main.ipf
@@ -31,15 +31,17 @@
 
 // Entry point for UTF
 Function run()
-	return RunWithOpts()
+	return RunWithOpts(instru = DoInstrumentation())
 End
 
 // Examples:
 // - RunWithOpts()
 // - RunWithOpts(testsuite = "UTF_Configuration.ipf")
 // - RunWithOpts(testcase = "TestFindLevel")
-Function RunWithOpts([string testcase, string testsuite, variable allowdebug])
+Function RunWithOpts([string testcase, string testsuite, variable allowdebug, variable instru, string traceWinList])
+
 	variable debugMode
+	string traceOptions = ""
 	string list = ""
 	string name = "MIES"
 
@@ -56,6 +58,22 @@ Function RunWithOpts([string testcase, string testsuite, variable allowdebug])
 	if(ParamIsDefault(testcase))
 		testcase = ""
 	endif
+
+	if(ParamIsDefault(instru))
+		instru = 0
+	else
+		instru = !!instru
+	endif
+
+	if(ParamIsDefault(traceWinList))
+		traceWinList = "MIES_.*\.ipf"
+	endif
+
+	if(!instru)
+		traceWinList = ""
+	endif
+
+	traceOptions = ReplaceNumberByKey(UTF_KEY_REGEXP, traceOptions, 1)
 
 	// sorted list
 	list = AddListItem("UTF_AnalysisFunctionHelpers.ipf", list, ";", inf)
@@ -84,9 +102,9 @@ Function RunWithOpts([string testcase, string testsuite, variable allowdebug])
 	endif
 
 	if(IsEmpty(testcase))
-		RunTest(testsuite, name = name, enableJU = 1, debugMode= debugMode)
+		RunTest(testsuite, name = name, enableJU = 1, debugMode= debugMode, traceOptions=traceOptions, traceWinList=traceWinList)
 	else
-		RunTest(testsuite, name = name, enableJU = 1, debugMode= debugMode, testcase = testcase)
+		RunTest(testsuite, name = name, enableJU = 1, debugMode= debugMode, testcase = testcase, traceOptions=traceOptions, traceWinList=traceWinList)
 	endif
 End
 

Packages/Testing-MIES/UTF_PA_Main.ipf
@@ -10,16 +10,17 @@
 
 // Entry point for UTF
 Function run()
-	return RunWithOpts()
+	return RunWithOpts(instru = DoInstrumentation())
 End
 
 // Examples:
 // - RunWithOpts()
 // - RunWithOpts(testsuite = "UTF_PA_Tests.ipf")
 // - RunWithOpts(testcase = "PAT_ZeroPulses")
-Function RunWithOpts([string testcase, string testsuite, variable allowdebug])
+Function RunWithOpts([string testcase, string testsuite, variable allowdebug, variable instru, string traceWinList])
 
 	variable debugMode
+	string traceOptions = ""
 	string list = ""
 	string name = "MIES pulse average tests"
 
@@ -37,6 +38,22 @@ Function RunWithOpts([string testcase, string testsuite, variable allowdebug])
 		testcase = ""
 	endif
 
+	if(ParamIsDefault(instru))
+		instru = 0
+	else
+		instru = !!instru
+	endif
+
+	if(ParamIsDefault(traceWinList))
+		traceWinList = "MIES_.*\.ipf"
+	endif
+
+	if(!instru)
+		traceWinList = ""
+	endif
+
+	traceOptions = ReplaceNumberByKey(UTF_KEY_REGEXP, traceOptions, 1)
+
 	list = AddListItem("UTF_PA_Tests.ipf", list, ";", inf)
 
 	if(ParamIsDefault(testsuite))
@@ -46,8 +63,8 @@ Function RunWithOpts([string testcase, string testsuite, variable allowdebug])
 	endif
 
 	if(IsEmpty(testcase))
-		RunTest(testsuite, name = name, enableJU = 1, debugMode= debugMode)
+		RunTest(testsuite, name = name, enableJU = 1, debugMode= debugMode, traceOptions=traceOptions, traceWinList=traceWinList)
 	else
-		RunTest(testsuite, name = name, enableJU = 1, debugMode= debugMode, testcase = testcase)
+		RunTest(testsuite, name = name, enableJU = 1, debugMode= debugMode, testcase = testcase, traceOptions=traceOptions, traceWinList=traceWinList)
 	endif
 End

Packages/Testing-MIES/UTF_PatchSeqRamp.ipf
@@ -412,12 +412,16 @@ static Function PS_RA3_REENTRY([str])
 	CHECK_EQUAL_VAR(numEntries, 3)
 
 	WAVE/Z durations = GetPulseDurations_IGNORE(sweepNo, str)
-	CHECK_GT_VAR(durations[0], SPIKE_POSITION_MS - PSQ_BL_EVAL_RANGE)
-	CHECK_LT_VAR(durations[0], SPIKE_POSITION_TEST_DELAY_MS)
-	CHECK_GT_VAR(durations[1], SPIKE_POSITION_MS - PSQ_BL_EVAL_RANGE)
-	CHECK_LT_VAR(durations[1], SPIKE_POSITION_TEST_DELAY_MS)
-	CHECK_GT_VAR(durations[2], SPIKE_POSITION_MS - PSQ_BL_EVAL_RANGE)
-	CHECK_LT_VAR(durations[2], SPIKE_POSITION_TEST_DELAY_MS)
+	if(TestHelperFunctions#DoInstrumentation())
+		CHECK_WAVE(durations, NUMERIC_WAVE)
+	else
+		CHECK_GT_VAR(durations[0], SPIKE_POSITION_MS - PSQ_BL_EVAL_RANGE)
+		CHECK_LT_VAR(durations[0], SPIKE_POSITION_TEST_DELAY_MS)
+		CHECK_GT_VAR(durations[1], SPIKE_POSITION_MS - PSQ_BL_EVAL_RANGE)
+		CHECK_LT_VAR(durations[1], SPIKE_POSITION_TEST_DELAY_MS)
+		CHECK_GT_VAR(durations[2], SPIKE_POSITION_MS - PSQ_BL_EVAL_RANGE)
+		CHECK_LT_VAR(durations[2], SPIKE_POSITION_TEST_DELAY_MS)
+	endif
 
 	CommonAnalysisFunctionChecks(str, sweepNo, setPassed)
 	[chunkStart, chunkEnd] = GetPostBaseLineInterval(str, 0, 1)
@@ -484,10 +488,14 @@ static Function PS_RA4_REENTRY([str])
 	CHECK_EQUAL_VAR(numEntries, 3)
 
 	WAVE/Z durations = GetPulseDurations_IGNORE(sweepNo, str)
-	CHECK_GT_VAR(durations[0], SPIKE_POSITION_MS - PSQ_BL_EVAL_RANGE)
-	CHECK_LT_VAR(durations[0], SPIKE_POSITION_TEST_DELAY_MS)
-	CHECK_CLOSE_VAR(durations[1], 15000, tol = 1)
-	CHECK_CLOSE_VAR(durations[2], 15000, tol = 1)
+	if(TestHelperFunctions#DoInstrumentation())
+		CHECK_WAVE(durations, NUMERIC_WAVE)
+	else
+		CHECK_GT_VAR(durations[0], SPIKE_POSITION_MS - PSQ_BL_EVAL_RANGE)
+		CHECK_LT_VAR(durations[0], SPIKE_POSITION_TEST_DELAY_MS)
+		CHECK_CLOSE_VAR(durations[1], 15000, tol = 1)
+		CHECK_CLOSE_VAR(durations[2], 15000, tol = 1)
+	endif
 
 	CommonAnalysisFunctionChecks(str, sweepNo, setPassed)
 	[chunkStart, chunkEnd] = GetPostBaseLineInterval(str, 0, 1)
@@ -552,10 +560,14 @@ static Function PS_RA5_REENTRY([str])
 	CHECK_EQUAL_VAR(numEntries, 3)
 
 	WAVE/Z durations = GetPulseDurations_IGNORE(sweepNo, str)
-	CHECK_GT_VAR(durations[0], 15000 - PSQ_BL_EVAL_RANGE)
-	CHECK_GT_VAR(durations[1], SPIKE_POSITION_MS - PSQ_BL_EVAL_RANGE)
-	CHECK_GT_VAR(durations[2], SPIKE_POSITION_MS - PSQ_BL_EVAL_RANGE)
-	CHECK_LT_VAR(durations[2], SPIKE_POSITION_TEST_DELAY_MS)
+	if(TestHelperFunctions#DoInstrumentation())
+		CHECK_WAVE(durations, NUMERIC_WAVE)
+	else
+		CHECK_GT_VAR(durations[0], 15000 - PSQ_BL_EVAL_RANGE)
+		CHECK_GT_VAR(durations[1], SPIKE_POSITION_MS - PSQ_BL_EVAL_RANGE)
+		CHECK_GT_VAR(durations[2], SPIKE_POSITION_MS - PSQ_BL_EVAL_RANGE)
+		CHECK_LT_VAR(durations[2], SPIKE_POSITION_TEST_DELAY_MS)
+	endif
 
 	CommonAnalysisFunctionChecks(str, sweepNo, setPassed)
 	CheckPSQChunkTimes(str, {20, 520, 16020, 16520}, sweep = 0)
@@ -621,10 +633,14 @@ static Function PS_RA6_REENTRY([str])
 	CHECK_EQUAL_VAR(numEntries, 3)
 
 	WAVE/Z durations = GetPulseDurations_IGNORE(sweepNo, str)
-	CHECK_GT_VAR(durations[0], 15000 - PSQ_BL_EVAL_RANGE)
-	CHECK_GT_VAR(durations[1], 15000 - PSQ_BL_EVAL_RANGE)
-	CHECK_GT_VAR(durations[2], SPIKE_POSITION_MS - PSQ_BL_EVAL_RANGE)
-	CHECK_LT_VAR(durations[2], SPIKE_POSITION_TEST_DELAY_MS)
+	if(TestHelperFunctions#DoInstrumentation())
+		CHECK_WAVE(durations, NUMERIC_WAVE)
+	else
+		CHECK_GT_VAR(durations[0], 15000 - PSQ_BL_EVAL_RANGE)
+		CHECK_GT_VAR(durations[1], 15000 - PSQ_BL_EVAL_RANGE)
+		CHECK_GT_VAR(durations[2], SPIKE_POSITION_MS - PSQ_BL_EVAL_RANGE)
+		CHECK_LT_VAR(durations[2], SPIKE_POSITION_TEST_DELAY_MS)
+	endif
 
 	CommonAnalysisFunctionChecks(str, sweepNo, setPassed)
 	CheckPSQChunkTimes(str, {20, 520, 16020, 16520}, sweep = 0)

commit 93b6b7fffc86d2f59fe00bfa4c43c7bf46ba95e7

DoInstrumentation: Use correct environment variable
Bug introduced in 5688983a (run/RunWithOpts: Add instrumentation support,
2021-12-22).

Packages/Testing-MIES/UTF_HelperFunctions.ipf
@@ -97,7 +97,7 @@ End
 Function DoInstrumentation()
 #if IgorVersion() >= 9.0
 	variable instru = str2numSafe(GetEnvironmentVariable("BAMBOO_INSTRUMENT_TESTS")) == 1           \
-	                  || !cmpstr(GetEnvironmentVariable("bamboo.planRepository.branchName"), "main")
+	                  || !cmpstr(GetEnvironmentVariable("bamboo_repository_git_branch"), "main")
 
 	return instru
 #else