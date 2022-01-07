AllenInstitute/MIES

commit 0a86df51dbf655480c1bd685d1fe7b6937193c58

PSQ Analysis functions: Store the baseline voltage when evaluated

Packages/MIES/MIES_AnalysisFunctions_PatchSeq.ipf
@@ -41,6 +41,7 @@
 /// PSQ_FMT_LBN_INITIAL_SCALE       Initial DAScale                                           Numerical                RB                       No                     No
 /// PSQ_FMT_LBN_RMS_SHORT_PASS      Short RMS baseline QC result                              Numerical                DA, RB, RA               Yes                    Yes
 /// PSQ_FMT_LBN_RMS_LONG_PASS       Long RMS baseline QC result                               Numerical                DA, RB, RA               Yes                    Yes
+/// PSQ_FMT_LBN_TARGETV             Target voltage baseline                                   Numerical                DA, RB, RA           Yes                    Yes
 /// PSQ_FMT_LBN_TARGETV_PASS        Target voltage baseline QC result                         Numerical                DA, RB, RA               Yes                    Yes
 /// PSQ_FMT_LBN_CHUNK_PASS          Which chunk passed/failed baseline QC                     Numerical                DA, RB, RA               Yes                    Yes
 /// PSQ_FMT_LBN_BL_QC_PASS          Pass/fail state of the complete baseline                  Numerical                DA, RB, RA               No                     Yes
@@ -368,6 +369,13 @@ static Function PSQ_EvaluateBaselineProperties(panelTitle, scaledDACWave, type, 
 
 		// more tests can be added here
 	endfor
+
+	if(HasOneValidEntry(avgVoltage))
+		// mV -> V
+		avgVoltage[] *= 1000
+		key = CreateAnaFuncLBNKey(type, PSQ_FMT_LBN_TARGETV, chunk = chunk)
+		ED_AddEntryToLabnotebook(panelTitle, key, avgVoltage, unit = "Volt", overrideSweepNo = sweepNo)
+	endif
 
 	// document results per headstage and chunk
 	key = CreateAnaFuncLBNKey(type, PSQ_FMT_LBN_RMS_SHORT_PASS, chunk = chunk)

Packages/MIES/MIES_Constants.ipf
@@ -962,6 +962,7 @@ StrConstant PSQ_FMT_LBN_FINAL_SCALE        = "%s final DAScale"
 StrConstant PSQ_FMT_LBN_INITIAL_SCALE      = "%s initial DAScale"
 StrConstant PSQ_FMT_LBN_RMS_SHORT_PASS     = "%s Chk%d S-RMS QC"
 StrConstant PSQ_FMT_LBN_RMS_LONG_PASS      = "%s Chk%d L-RMS QC"
+StrConstant PSQ_FMT_LBN_TARGETV            = "%s Chk%d T-V BL"
 StrConstant PSQ_FMT_LBN_TARGETV_PASS       = "%s Chk%d T-V BL QC"
 StrConstant PSQ_FMT_LBN_CHUNK_PASS         = "%s Chk%d BL QC"
 StrConstant PSQ_FMT_LBN_BL_QC_PASS         = "%s BL QC"

commit 846059d7c1eec8f35450c1fa68166190a475b33c

PSQ_EvaluateBaselineProperties: Fix scale of average voltage
When going from milli to unity the factor is 1e-3. Bug introduced in
0a86df51 (PSQ Analysis functions: Store the baseline voltage when
evaluated, 2021-01-15).

Packages/MIES/MIES_AnalysisFunctions_PatchSeq.ipf
@@ -545,7 +545,7 @@ static Function PSQ_EvaluateBaselineProperties(string device, STRUCT AnalysisFun
 
 	if(HasOneValidEntry(avgVoltage))
 		// mV -> V
-		avgVoltage[] *= 1000
+		avgVoltage[] *= 1e-3
 		key = CreateAnaFuncLBNKey(type, PSQ_FMT_LBN_TARGETV, chunk = chunk)
 		ED_AddEntryToLabnotebook(device, key, avgVoltage, unit = "Volt", overrideSweepNo = s.sweepNo)
 	endif

Packages/MIES/MIES_Constants.ipf
@@ -1026,10 +1026,10 @@ StrConstant FMT_LBN_ANA_FUNC_VERSION = "%s version"
 
 /// @name Analysis function versions
 /// @{
-Constant PSQ_CHIRP_VERSION         = 5
-Constant PSQ_DA_SCALE_VERSION      = 2
-Constant PSQ_RAMP_VERSION          = 3
-Constant PSQ_RHEOBASE_VERSION      = 2
+Constant PSQ_CHIRP_VERSION         = 6
+Constant PSQ_DA_SCALE_VERSION      = 3
+Constant PSQ_RAMP_VERSION          = 4
+Constant PSQ_RHEOBASE_VERSION      = 3
 Constant PSQ_SQUARE_PULSE_VERSION  = 1
 Constant MSQ_FAST_RHEO_EST_VERSION = 0
 Constant MSQ_DA_SCALE_VERSION      = 0