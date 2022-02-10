AllenInstitute/MIES

commit d0dfc2e8c245cad0748918f160b17acd8e1be451

Labnotebook: Enforce that the INDEP getters are correctly used
As the last commit showed, we don't have any safety precautions when an
dependent setting is queried as independent.

But we can add a check for CI to enforce that. We don't do that check
outside of CI for speed reasons. The IsConstant check is required for
very old entries which have the same value in all LABNOTEBOOK_LAYER_COUNT
rows.

We can not enforce the other direction, a dependent setting queried as
independent, as the user uses GetLastSetting for that and we don't know
inside GetLastSetting what she wants to do.

Packages/MIES/MIES_MiesUtilities.ipf
@@ -484,6 +484,19 @@ Function FindRange(wv, col, val, forwardORBackward, entrySourceType, first, last
 	endif
 End
 
+/// @brief Test helper to enforce that every query done for an INDEP_HEADSTAGE setting
+/// does not search an entry which is HEADSTAGE dependent. The additional check that not all
+/// entries are the same is for really old legacy entries which are INDEP but set for all headstages.
+///
+/// Does nothing outside of CI.
+static Function EnforceIndependentSetting(WAVE settings)
+
+#ifdef AUTOMATED_TESTING
+	Duplicate/FREE/RMD=[0, NUM_HEADSTAGES - 1] settings, settingsHS
+	ASSERT(!HasOneValidEntry(settingsHS) || IsConstant(settings, settings[0]), "The labnotebook query asked for independent headstage setting, but the entry has headstage dependent settings.")
+#endif
+End
+
 /// @brief Returns the numerical index for the sweep number column
 /// in the settings history waves (numeric and text)
 Function GetSweepColumn(labnotebookValues)
@@ -581,6 +594,7 @@ Function GetLastSettingIndep(numericalValues, sweepNo, setting, entrySourceType,
 	WAVE/Z settings = GetLastSetting(numericalValues, sweepNo, setting, entrySourceType)
 
 	if(WaveExists(settings))
+		EnforceIndependentSetting(settings)
 		return settings[GetIndexForHeadstageIndepData(numericalValues)]
 	else
 		DEBUGPRINT("Missing setting in labnotebook", str=setting)
@@ -607,6 +621,7 @@ Function/S GetLastSettingTextIndep(textualValues, sweepNo, setting, entrySourceT
 	WAVE/T/Z settings = GetLastSetting(textualValues, sweepNo, setting, entrySourceType)
 
 	if(WaveExists(settings))
+		EnforceIndependentSetting(settings)
 		return settings[GetIndexForHeadstageIndepData(textualValues)]
 	else
 		DEBUGPRINT("Missing setting in labnotebook", str=setting)
@@ -634,6 +649,7 @@ Function GetLastSettingIndepRAC(numericalValues, sweepNo, setting, entrySourceTy
 	WAVE/Z settings = GetLastSettingRAC(numericalValues, sweepNo, setting, entrySourceType)
 
 	if(WaveExists(settings))
+		EnforceIndependentSetting(settings)
 		return settings[GetIndexForHeadstageIndepData(numericalValues)]
 	else
 		DEBUGPRINT("Missing setting in labnotebook", str=setting)
@@ -661,6 +677,7 @@ Function GetLastSettingIndepSCI(numericalValues, sweepNo, setting, headstage, en
 	WAVE/Z settings = GetLastSettingSCI(numericalValues, sweepNo, setting, headstage, entrySourceType)
 
 	if(WaveExists(settings))
+		EnforceIndependentSetting(settings)
 		return settings[GetIndexForHeadstageIndepData(numericalValues)]
 	else
 		DEBUGPRINT("Missing setting in labnotebook", str=setting)
@@ -688,6 +705,7 @@ Function/S GetLastSettingTextIndepRAC(numericalValues, textualValues, sweepNo, s
 	WAVE/T/Z settings = GetLastSettingTextRAC(numericalValues, textualValues, sweepNo, setting, entrySourceType)
 
 	if(WaveExists(settings))
+		EnforceIndependentSetting(settings)
 		return settings[GetIndexForHeadstageIndepData(textualValues)]
 	else
 		DEBUGPRINT("Missing setting in labnotebook", str=setting)
@@ -1635,6 +1653,7 @@ Function GetLastSweepWithSettingIndep(numericalValues, setting, sweepNo, [defVal
 	WAVE/Z settings = GetLastSweepWithSetting(numericalValues, setting, sweepNo)
 
 	if(WaveExists(settings))
+		EnforceIndependentSetting(settings)
 		return settings[GetIndexForHeadstageIndepData(numericalValues)]
 	else
 		DEBUGPRINT("Missing setting in labnotebook", str=setting)
@@ -1698,6 +1717,7 @@ Function/S GetLastSweepWithSettingTextI(numericalValues, setting, sweepNo, [defV
 	WAVE/T/Z settings = GetLastSweepWithSettingText(numericalValues, setting, sweepNo)
 
 	if(WaveExists(settings))
+		EnforceIndependentSetting(settings)
 		return settings[GetIndexForHeadstageIndepData(numericalValues)]
 	else
 		DEBUGPRINT("Missing setting in labnotebook", str=setting)

commit d675afb7d43bc782c02af4855b25609bac850f11

CreateTiledChannelGraph: Fix assertion with dDAQ slider
When acquiring oodDAQ data and when trying to view the data with dDAQ
checkbox enabled and AUTOMATED_TESTING being defined, we get an assertion from
EnforceIndependentSetting introduced in d0dfc2e8 (Labnotebook: Enforce
that the INDEP getters are correctly used, 2021-04-22).

We can avoid that by querying the INDEP_HEADSTAGE entry of "Stim set
length" only when dDAQ is enabled, as it is in the headstage dependent
entries for all other cases.

Packages/MIES/MIES_MiesUtilities.ipf
@@ -2677,9 +2677,6 @@ Function CreateTiledChannelGraph(string graph, WAVE config, variable sweepNo, WA
 	endif
 
 	if(tgs.dDAQDisplayMode)
-		stimSetLength = GetLastSettingIndep(numericalValues, sweepNo, "Stim set length", DATA_ACQUISITION_MODE)
-		DEBUGPRINT("Stim set length (labnotebook, NaN for oodDAQ)", var=stimSetLength)
-
 		samplingInt = GetSamplingInterval(config) * 1e-3
 
 		// dDAQ data taken with versions prior to
@@ -2733,6 +2730,9 @@ Function CreateTiledChannelGraph(string graph, WAVE config, variable sweepNo, WA
 			sprintf str, "oodDAQRegions (%d) concatenated: _%s_, totalRange=%g", numRegions, oodDAQRegionsAll, totalXRange
 			DEBUGPRINT(str)
 		else
+			stimSetLength = GetLastSettingIndep(numericalValues, sweepNo, "Stim set length", DATA_ACQUISITION_MODE)
+			DEBUGPRINT("Stim set length (labnotebook, NaN for oodDAQ)", var=stimSetLength)
+
 			dDAQActiveHeadstageAll = ""
 
 			for(i = 0; i < NUM_HEADSTAGES; i += 1)