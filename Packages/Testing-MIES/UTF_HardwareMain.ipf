AllenInstitute/MIES

commit 7eb14a9fa1bb8910b2972f304a76b976ef0a2736

BUG_TS: Add it
As we want to track the global number of bugs, we need to use the new TSDS
helper functions.

Packages/Testing-MIES/UTF_HardwareMain.ipf
@@ -297,7 +297,7 @@ Function TEST_CASE_END_OVERRIDE(name)
 	string name
 
 	string dev, experimentNWBFile, baseFolder, nwbFile
-	variable numEntries, i, fileID, nwbVersion, expensiveChecks
+	variable numEntries, i, fileID, nwbVersion, expensiveChecks, bugCount_ts
 
 	expensiveChecks = DoExpensiveChecks()
 
@@ -361,6 +361,13 @@ Function TEST_CASE_END_OVERRIDE(name)
 
 	NVAR bugCount = $GetBugCount()
 	CHECK_EQUAL_VAR(bugCount, 0)
+
+#if IgorVersion() >= 9.0
+	TUFXOP_AcquireMutex/N=(TSDS_BUGCOUNT)
+	bugCount_ts = TSDS_ReadVar(TSDS_BUGCOUNT, defValue = 0)
+	TUFXOP_ReleaseMutex/N=(TSDS_BUGCOUNT)
+	CHECK_EQUAL_VAR(bugCount_ts, 0)
+#endif
 
 	if(expensiveChecks)
 		// store experiment NWB file for later validation

Packages/MIES/MIES_Constants.ipf
@@ -1660,3 +1660,9 @@ Constant TP_BASELINE_FIT_RESULT_TOO_NOISY = 32
 Constant TP_GETVALUES_DEFAULT            = 0x0
 Constant TP_GETVALUES_LATEST_AUTOTPCYCLE = 0x1
 /// @}
+
+/// Possible names for TSDS_Read*/TSDS_Write
+/// @anchor ThreadsafeDataExchangeNames
+/// @{
+StrConstant TSDS_BUGCOUNT = "BugCount"
+/// @}

Packages/MIES/MIES_Debugging.ipf
@@ -526,6 +526,41 @@ Function BUG(msg)
 #endif
 End
 
+/// @brief Threadsafe variant of BUG()
+///
+/// @todo IP9: Unify with BUG
+threadsafe Function BUG_TS(string msg)
+	variable bugCount
+	string stacktrace
+
+	msg = RemoveEnding(msg, "\r")
+
+#if IgorVersion() >= 9.0
+	stacktrace = GetStackTrace()
+#else
+	stacktrace = "stacktrace not available"
+#endif
+
+	LOG_AddEntry_TS(PACKAGE_MIES, "report", "BUG_TS",  \
+	                keys = {"msg", "stacktrace"},      \
+	                values = {msg, stacktrace})
+
+	printf "BUG_TS: %s\r", msg
+
+#ifdef AUTOMATED_TESTING
+
+#if IgorVersion() >= 9.0
+	TUFXOP_AcquireMutex/N=(TSDS_BUGCOUNT)
+	bugCount = TSDS_ReadVar(TSDS_BUGCOUNT, defValue = 0, create = 1)
+	bugCount += 1
+	TSDS_Write(TSDS_BUGCOUNT, var = bugCount)
+	TUFXOP_ReleaseMutex/N=(TSDS_BUGCOUNT)
+#endif
+
+	ASSERT_TS(0, "BUG_TS: Should never be called during automated testing.")
+#endif
+End
+
 /// @brief Debug helper which creates a textwave which will hold
 ///        the names of all waves and their size in MiB
 Function GetSizeOfAllWavesInExperiment()

Packages/Testing-MIES/UTF_Utils.ipf
@@ -5666,3 +5666,56 @@ Function BUGWorks()
 	bugCount = ROVar(GetBugCount())
 	CHECK_EQUAL_VAR(bugCount, 1)
 End
+
+#if IgorVersion() >= 9.0
+
+Function BUG_TSWorks1()
+	variable bugCount
+
+	TUFXOP_Clear/N=(TSDS_BUGCOUNT)/Q/Z
+
+	bugCount = TSDS_ReadVar(TSDS_BUGCOUNT)
+	CHECK_EQUAL_VAR(bugCount, NaN)
+
+	try
+		BUG_TS("abcd")
+		FAIL()
+	catch
+		PASS()
+	endtry
+
+	bugCount = TSDS_ReadVar(TSDS_BUGCOUNT)
+	CHECK_EQUAL_VAR(bugCount, 1)
+End
+
+threadsafe static Function BugHelper(variable idx)
+
+	try
+		BUG_TS(num2str(idx))
+		return 1
+	catch
+		return 0
+	endtry
+End
+
+Function BUG_TSWorks2()
+	variable bugCount, numThreads
+
+	TUFXOP_Clear/N=(TSDS_BUGCOUNT)/Q/Z
+
+	bugCount = TSDS_ReadVar(TSDS_BUGCOUNT)
+	CHECK_EQUAL_VAR(bugCount, NaN)
+
+	numThreads = 10
+
+	Make/FREE/N=(numThreads) junk = NaN
+
+	MultiThread/NT=(numThreads) junk = BugHelper(p)
+
+	CHECK_EQUAL_VAR(Sum(junk), 0)
+
+	bugCount = TSDS_ReadVar(TSDS_BUGCOUNT)
+	CHECK_EQUAL_VAR(bugCount, numThreads)
+End
+
+#endif

commit 04900d734eaff33eaf6408c2ed6c207641d5fa72

Tests: Initialize TSDS_BUGCOUNT on each test case begin
This helps recovering from BUG_TS errors on developer machines.

We do already clear the main thread global counter by removing
the root:MIES datafolder.

Broken since 7eb14a9f (BUG_TS: Add it, 2022-01-25).

Packages/Testing-MIES/UTF_HardwareMain.ipf
@@ -297,6 +297,12 @@ Function TEST_CASE_BEGIN_OVERRIDE(name)
 	DuplicateDataFolder/O=1/Z source, dest
 	CHECK_EQUAL_VAR(V_flag, 0)
 
+#if IgorVersion() >= 9.0
+	TUFXOP_AcquireMutex/N=(TSDS_BUGCOUNT)
+	TSDS_Write(TSDS_BUGCOUNT, var = 0)
+	TUFXOP_ReleaseMutex/N=(TSDS_BUGCOUNT)
+#endif
+
 #ifndef TESTS_WITH_NI_HARDWARE
 	HW_ITC_CloseAllDevices()
 #endif