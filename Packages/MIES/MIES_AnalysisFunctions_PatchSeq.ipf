AllenInstitute/MIES

commit c371535c5f30904c2f4122162ec40d2b98d7dcc5

Add PSQ_Chirp

Packages/MIES/MIES_Constants.ipf
@@ -938,6 +938,8 @@ Constant PSQ_DS_PULSE_DUR        = 1000
 
 Constant PSQ_RA_BL_EVAL_RANGE = 500
 
+Constant PSQ_CR_BL_EVAL_RANGE = 500
+
 Constant PSQ_SPIKE_LEVEL         = 0.01 // mV
 Constant PSQ_RMS_SHORT_THRESHOLD = 0.07 // mV
 Constant PSQ_RMS_LONG_THRESHOLD  = 0.5  // mV
@@ -974,6 +976,11 @@ StrConstant PSQ_FMT_LBN_RB_LIMITED_RES     = "%s limited resolut."
 StrConstant PSQ_FMT_LBN_DA_fI_SLOPE        = "%s f-I slope"
 StrConstant PSQ_FMT_LBN_DA_fI_SLOPE_REACHED= "%s f-I slope QC"
 StrConstant PSQ_FMT_LBN_DA_OPMODE          = "%s operation mode"
+StrConstant PSQ_FMT_LBN_CR_RESISTANCE      = "%s input resistance"
+StrConstant PSQ_FMT_LBN_CR_INSIDE_BOUNDS   = "%s inside bounds"
+StrConstant PSQ_FMT_LBN_CR_BOUNDS_ACTION   = "%s bounds action"
+StrConstant PSQ_FMT_LBN_CR_CYCLES          = "%s cycle x values"
+StrConstant PSQ_FMT_LBN_CR_BOUNDS_STATE    = "%s bounds state"
 /// @}
 
 /// @name PatchSeq and MultiPatchSeq types of analysis functions
@@ -988,6 +995,7 @@ Constant PSQ_DA_SCALE      = 0x01
 Constant PSQ_SQUARE_PULSE  = 0x02
 Constant PSQ_RHEOBASE      = 0x04
 Constant PSQ_RAMP          = 0x08
+Constant PSQ_CHIRP         = 0x80
 /// @}
 
 /// @anchor MultiPatchSeqAnalysisFunctionTypes
@@ -1021,6 +1029,25 @@ StrConstant PSQ_DS_SUPRA         = "Supra"
 /// @{
 Constant PSQ_RA_DASCALE_DEFAULT = 1 // pA
 Constant PSQ_RA_NUM_SWEEPS_PASS = 3
+/// @}
+
+/// @name PatchSeq Chirp
+/// @{
+Constant PSQ_CR_NUM_SWEEPS_PASS = 3
+Constant PSQ_CR_NUM_SWEEPS_FAIL = 3
+Constant PSQ_CR_RESISTANCE_FAKE = 1 // GOhm
+Constant PSQ_CR_BASELINE_V_FAKE = 1 // mV
+Constant PSQ_CR_LIMIT_BAND_LOW  = 1 // mV
+Constant PSQ_CR_LIMIT_BAND_HIGH = 100 // mV
+/// @}
+
+/// @name Bounds action values, see also PSQ_CR_BoundsActionToString()
+/// @anchor ChirpBoundsAction
+/// @{
+Constant PSQ_CR_PASS     = 0x1
+Constant PSQ_CR_DECREASE = 0x2
+Constant PSQ_CR_INCREASE = 0x4
+Constant PSQ_CR_RERUN    = 0x8
 /// @}
 
 /// @name MultiPatchSeq various constants

Packages/MIES/MIES_AnalysisFunctions_Dashboard.ipf
@@ -223,13 +223,14 @@ static Function AD_FillWaves(panelTitle, list, info)
 			list[index][3] = msg
 
 			// get the passing/failing sweeps
-			// PSQ_DA PSQ_RA, PSQ_SP, MSQ_DA, MSQ_FRE, MSQ_SC: use PSQ_FMT_LBN_SWEEP_PASS
+			// PSQ_CR, PSQ_DA, PSQ_RA, PSQ_SP, MSQ_DA, MSQ_FRE, MSQ_SC: use PSQ_FMT_LBN_SWEEP_PASS
 			// PSQ_RB: If passed use last spiking/non-spiking duo
 			//     If not passed, all are failing
 
 			WAVE sweeps = AFH_GetSweepsFromSameSCI(numericalValues, sweepNo, headstage)
 
 			switch(anaFuncType)
+				case PSQ_CHIRP:
 				case PSQ_DA_SCALE:
 				case PSQ_RAMP:
 				case PSQ_SQUARE_PULSE:

Packages/MIES/MIES_MiesUtilities.ipf
@@ -22,6 +22,7 @@ static Constant GET_LB_MODE_READ  = 1
 
 static Constant GET_LB_MODE_WRITE = 2
 
+static StrConstant PSQ_CR_LBN_PREFIX = "Chirp"
 static StrConstant PSQ_SP_LBN_PREFIX = "Squ. Pul."
 static StrConstant PSQ_DS_LBN_PREFIX = "DA Scale"
 static StrConstant PSQ_RB_LBN_PREFIX = "Rheobase"
@@ -7186,6 +7187,8 @@ Function MapAnaFuncToConstant(anaFunc)
 	strswitch(anaFunc)
 		case "PSQ_Ramp":
 			return PSQ_RAMP
+		case "PSQ_Chirp":
+			return PSQ_CHIRP
 		case "PSQ_DaScale":
 			return PSQ_DA_SCALE
 		case "PSQ_Rheobase":
@@ -7224,6 +7227,9 @@ Function/S CreateAnaFuncLBNKey(type, formatString, [chunk, query])
 			break
 		case SC_SPIKE_CONTROL:
 			prefix = MSQ_SC_LBN_PREFIX
+			break
+		case PSQ_CHIRP:
+			prefix = PSQ_CR_LBN_PREFIX
 			break
 		case PSQ_DA_SCALE:
 			prefix = PSQ_DS_LBN_PREFIX

Packages/MIES/MIES_Structures.ipf
@@ -351,3 +351,9 @@ Structure BufferedDrawInfo
 	variable jsonID
 	WAVE/WAVE traceWaves
 EndStructure
+
+/// @brief Helper strucuture for PSQ_CR_DetermineBoundsState()
+Structure ChirpBoundsInfo
+	variable minimumFac, centerFac, maximumFac
+	string state
+EndStructure

commit a8435226639c7d83b5197b328584a8515646491e

PSQ_Chirp: Remove initial DAScale labnotebook unit
This is just a scaling factor, broken since c371535c (Add PSQ_Chirp,
2020-06-02).

Packages/MIES/MIES_AnalysisFunctions_PatchSeq.ipf
@@ -3993,7 +3993,7 @@ Function PSQ_Chirp(device, s)
 				Make/FREE/N=(LABNOTEBOOK_LAYER_COUNT)/D result = NaN
 				key = CreateAnaFuncLBNKey(PSQ_CHIRP, PSQ_FMT_LBN_INITIAL_SCALE)
 				result[INDEP_HEADSTAGE] = initialDAScale
-				ED_AddEntryToLabnotebook(device, key, result, overrideSweepNo = s.sweepNo, unit = "Amperes")
+				ED_AddEntryToLabnotebook(device, key, result, overrideSweepNo = s.sweepNo)
 
 				SetDAScale(device, s.headstage, absolute=initialDAScale, roundTopA = 1)
 			endif

Packages/MIES/MIES_Constants.ipf
@@ -1042,7 +1042,7 @@ StrConstant FMT_LBN_ANA_FUNC_VERSION = "%s version"
 /// @name Analysis function versions
 /// @{
 Constant PSQ_PIPETTE_BATH_VERSION    = 0
-Constant PSQ_CHIRP_VERSION           = 6
+Constant PSQ_CHIRP_VERSION           = 7
 Constant PSQ_DA_SCALE_VERSION        = 3
 Constant PSQ_RAMP_VERSION            = 4
 Constant PSQ_RHEOBASE_VERSION        = 3