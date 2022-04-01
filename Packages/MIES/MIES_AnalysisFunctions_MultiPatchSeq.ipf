AllenInstitute/MIES

commit 1e7ede2342fc101dcda047bb3d0acc88fac47b3c

Add FastRheoEstimate analysis function
Original-CommitID: 14fa5b8c2b9dd90c8432644c416657de6026e40c

Packages/MIES/MIES_Constants.ipf
@@ -927,6 +927,55 @@ Constant PSQ_RA_DASCALE_DEFAULT = 1 // pA
 Constant PSQ_RA_NUM_SWEEPS_PASS = 3
 /// @}
 
+/// @name MultiPatchSeq various constants
+/// @{
+Constant MSQ_FRE_INIT_AMP_m50    = -50e-12
+Constant MSQ_FRE_INIT_AMP_p100   = +100e-12
+Constant MSQ_FRE_INIT_AMP_p10    = +10e-12
+
+Constant MSQ_RB_PRE_BL_EVAL_RANGE  = 500
+Constant MSQ_RB_POST_BL_EVAL_RANGE = 500
+
+Constant MSQ_DS_BL_EVAL_RANGE_MS = 500
+Constant MSQ_DS_PULSE_DUR        = 1000
+
+Constant MSQ_RA_BL_EVAL_RANGE = 500
+
+Constant MSQ_SPIKE_LEVEL         = 0.01 // mV
+Constant MSQ_RMS_SHORT_THRESHOLD = 0.07 // mV
+Constant MSQ_RMS_LONG_THRESHOLD  = 0.5  // mV
+Constant MSQ_TARGETV_THRESHOLD   = 1    // mV
+/// @}
+
+/// @name MultiPatchSeq types of analysis functions
+/// @anchor MultiPatchSeqAnalysisFunctionTypes
+/// @{
+Constant MSQ_FAST_RHEO_EST = 0x1
+
+/// List of analysis function types
+StrConstant MSQ_LIST_OF_TYPES = "0x1"
+/// @}
+
+/// @anchor MultiPatchSeqLabnotebookFormatStrings
+/// @{
+StrConstant MSQ_FMT_LBN_DASCALE_EXC    = "%s DAScale exceeded"
+StrConstant MSQ_FMT_LBN_STEPSIZE          = "%s step size"
+StrConstant MSQ_FMT_LBN_SPIKE_DETECT      = "%s spike detected"
+StrConstant MSQ_FMT_LBN_SPIKE_POSITIONS   = "%s spike positions"
+StrConstant MSQ_FMT_LBN_FINAL_SCALE       = "%s final DAScale"
+StrConstant MSQ_FMT_LBN_INITIAL_SCALE     = "%s initial DAScale"
+StrConstant MSQ_FMT_LBN_RMS_SHORT_PASS    = "%s Chk%d S-RMS QC"
+StrConstant MSQ_FMT_LBN_RMS_LONG_PASS     = "%s Chk%d L-RMS QC"
+StrConstant MSQ_FMT_LBN_TARGETV_PASS      = "%s Chk%d T-V BL QC"
+StrConstant MSQ_FMT_LBN_CHUNK_PASS        = "%s Chk%d BL QC"
+StrConstant MSQ_FMT_LBN_BL_QC_PASS        = "%s Baseline QC"
+StrConstant MSQ_FMT_LBN_HEADSTAGE_PASS    = "%s Headstage QC"
+StrConstant MSQ_FMT_LBN_SWEEP_PASS        = "%s Sweep QC"
+StrConstant MSQ_FMT_LBN_SET_PASS          = "%s Set QC"
+StrConstant MSQ_FMT_LBN_PULSE_DUR         = "%s Pulse duration"
+StrConstant MSQ_FMT_LBN_ACTIVE_HS         = "%s Active Headstage"
+/// @}
+
 Constant TP_MD_THREAD_DEAD_MAX_RETRIES = 10
 
 /// @todo: IP8 convert all call sites to use MultiThread/T=

Packages/MIES/MIES_MiesUtilities.ipf
@@ -4876,3 +4876,10 @@ Function GetTotalOnsetDelay(numericalValues, sweepNo)
 	return GetLastSettingIndep(numericalValues, sweepNo, "Delay onset auto", DATA_ACQUISITION_MODE) + \
 			GetLastSettingIndep(numericalValues, sweepNo, "Delay onset user", DATA_ACQUISITION_MODE)
 End
+
+/// @brief Check if the given multiplier is a valid sampling interval multiplier
+Function IsValidSamplingMultiplier(multiplier)
+	variable multiplier
+
+	return IsFinite(multiplier) && WhichListItem(num2str(multiplier), DAP_GetSamplingMultiplier()) != -1
+End

Packages/MIES/MIES_Utilities.ipf
@@ -3700,6 +3700,13 @@ Function WaveModCountWrapper(wv)
 
 End
 
+// @brief Convert a number to the strings `Passed` (!= 0) or `Failed` (0).
+Function/S ToPassFail(passedOrFailed)
+	variable passedOrFailed
+
+	return SelectString(passedOrFailed, "failed", "passed")
+End
+
 // @brief Convert a number to the strings `True` (!= 0) or `False` (0).
 Function/S ToTrueFalse(var)
 	variable var

Packages/MIES_Include.ipf
@@ -39,6 +39,7 @@
 #include "MIES_AnalysisFunctionManagement"
 #include "MIES_AnalysisFunctions_PatchSeq"
 #include "MIES_AnalysisFunctions_Dashboard"
+#include "MIES_AnalysisFunctions_MultiPatchSeq"
 #include "MIES_AnalysisFunctionPrototypes"
 #include "MIES_AnalysisFunctions"
 #include "MIES_AnalysisMaster"

Packages/doc/multi-patch-seq-fast-rheo-estimate.dot
@@ -1,0 +1,140 @@
+digraph G {
+    fontname="sans-serif";
+    penwidth="0.1";
+    ratio="compress";
+    center="true";
+    compound="true";
+    edge [comment="Wildcard edge",
+          fontname="sans-serif",
+          fontsize=10,
+          colorscheme="blues3",
+          color=2,
+          fontcolor=3];
+    node [fontname="serif",
+          fontsize=13,
+          fillcolor="1",
+          colorscheme="blues4",
+          color="2",
+          fontcolor="4",
+          style="filled"];
+    "Start DAQ/RA (only call for last active headstage)";
+    "Start DAQ/RA (only call for last active headstage)" -> "Prepare DAQ/Pre Set Event";
+    "Start DAQ/RA (only call for last active headstage)" -> "Post Sweep Event";
+    "Post DAQ Event";
+    "Start DAQ/RA (only call for last active headstage)" -> "Post DAQ Event";
+    subgraph cluster_preDAQ {
+        color="invis";
+        edge [comment="Wildcard node added automatic in EG."];
+        node [comment="Wildcard node added automatic in EG."];
+        "Enable \"Multidevice DAQ\"";
+        "Disable \"dDAQ\"/\"oodDAQ\"";
+        "Enable \"Repeated Acquisition\"";
+        "Check if no TTL channels are active";
+        "Set \"TP during ITI\" to disabled";
+        "Prepare DAQ/Pre Set Event";
+        "All checks passed";
+        "Check if there is at least\none active IC headstage";
+        "All required analysis parameters present?";
+        "Enable \"Multidevice DAQ\"" -> "Disable \"dDAQ\"/\"oodDAQ\"";
+        "Disable \"dDAQ\"/\"oodDAQ\"" -> "Set \"TP inserting\" to disabled";
+        "Set \"TP inserting\" to disabled" -> "Set \"TP during ITI\" to disabled";
+        "Set \"TP during ITI\" to disabled" -> "Set \"ITI\" to 100ms";
+        "Set \"ITI\" to 100ms" -> "Enable \"Repeated Acquisition\"";
+        "Check if there is at least\none active IC headstage" -> "Check if no TTL channels are active";
+        "Check if no TTL channels are active" -> "All required analysis parameters present?";
+        "All checks passed" -> "Set DAScale to 100pA\nfor all active IC headstages"  [label="Yes"];
+        "Write initial stepsize of 100pA into\nlabnotebook for all active IC headstages";
+        "Set DAScale to 100pA\nfor all active IC headstages" -> "Write initial stepsize of 100pA into\nlabnotebook for all active IC headstages";
+        "Abort";
+        "All checks passed" -> "Abort"  [label="No"];
+        "Check if all active IC\n headstages have the same stimset" -> "All checks passed";
+        "Is Prepare DAQ Event?";
+        "All checks passed" -> "Is Prepare DAQ Event?"  [label="Yes"];
+        "Is Prepare DAQ Event?" -> "Store active headstages in labnotebook"  [label="Yes"];
+        "Store active headstages in labnotebook" -> "Turn off active non-IC headstages";
+        "Read active headstages from labnotebook\nand turn them all on again";
+        "Check if all active IC\n headstages have the same stimset";
+        "Store active headstages in labnotebook";
+        "Turn off active non-IC headstages";
+        "All required analysis parameters present?" -> "Check if all active IC\n headstages have the same stimset";
+        "Set \"DAScale exceeded\" labnotebook entry\nto false for all active IC headstages";
+        "Write initial stepsize of 100pA into\nlabnotebook for all active IC headstages" -> "Set \"DAScale exceeded\" labnotebook entry\nto false for all active IC headstages";
+    }
+
+    subgraph cluster_postSweep {
+        color="invis";
+        edge [comment="Wildcard node added automatic in EG."];
+        node [comment="Wildcard node added automatic in EG."];
+        "Post Sweep Event";
+        "Mark sweep as failed";
+        "Post Sweep Event" -> "Iterate over all active headstages";
+        "Iterate over all active headstages";
+        "Active headstages left?";
+        "Active headstages left?" -> "Have all headstages passed in one sweep\nof their stimset cycle?"  [label="No"];
+        "Active headstages left?" -> "Headstage passed?"  [label="Yes"];
+        "Headstage exceeded DAScale\n according to labnotebook entry?";
+        "Headstage passed?" -> "Headstage exceeded DAScale\n according to labnotebook entry?"  [label="No"];
+        "Headstage exceeded DAScale\n according to labnotebook entry?" -> "Spike detected?\n(x range: pulse onset to end of sweep\ny condition: max above 0.01mV)\nStore result in labnotebook"  [label="No"];
+        "Have all headstages passed in one sweep\nof their stimset cycle?" -> "Mark sweep as passed"  [label="Yes"];
+        "Prepare DAQ/Pre Set Event" -> "Enable \"Multidevice DAQ\"";
+        "Prepare DAQ/Pre Set Event" -> "Check if there is at least\none active IC headstage";
+        "Iterate over all active headstages" -> "Active headstages left?";
+        "Spike detected?\n(x range: pulse onset to end of sweep\ny condition: max above 0.01mV)\nStore result in labnotebook";
+        "Spike detected?\n(x range: pulse onset to end of sweep\ny condition: max above 0.01mV)\nStore result in labnotebook" -> "Step size from labnotebook? (1)"  [label="Yes"];
+        "Step size from labnotebook? (1)" -> "Store DAScale in labnotebook"  [label="+10pA"];
+        "Offset DAScale by -50pA";
+        "Step size from labnotebook? (1)";
+        "Step size from labnotebook? (2)";
+        "Step size from labnotebook? (1)" -> "Offset DAScale by -50pA"  [label="-50pA"];
+        "Write step size of -50pA to labnotebook";
+        "Step size from labnotebook? (1)" -> "Write step size of -50pA to labnotebook"  [label="+100pA"];
+        "Write step size of -50pA to labnotebook" -> "Offset DAScale by -50pA";
+        "Spike detected?\n(x range: pulse onset to end of sweep\ny condition: max above 0.01mV)\nStore result in labnotebook" -> "Step size from labnotebook? (2)"  [label="No"];
+        "Write step size of +10pA to labnotebook";
+        "Offset DAScale by +10pA";
+        "Offset DAScale by +100pA";
+        "Step size from labnotebook? (2)" -> "Offset DAScale by +10pA"  [label="+10pA"];
+        "Step size from labnotebook? (2)" -> "Write step size of +10pA to labnotebook"  [label="-50pA"];
+        "Step size from labnotebook? (2)" -> "Offset DAScale by +100pA"  [label="+100pA"];
+        "Write step size of +10pA to labnotebook" -> "Offset DAScale by +10pA";
+        "Mark headstage as passed";
+        "Mark headstage as passed" -> "Set DAScale to zero";
+        "Store DAScale in labnotebook" -> "Mark headstage as passed";
+        "Mark headstage as failed";
+        "Offset DAScale by -50pA" -> "Mark headstage as failed";
+        "Offset DAScale by +10pA" -> "Mark headstage as failed";
+        "Offset DAScale by +100pA" -> "Mark headstage as failed";
+        "Skip to end of active set";
+        "Mark sweep as passed" -> "Skip to end of active set";
+        "\"MaximumDAScale\" analysis parameter is not NaN?";
+        "Mark headstage as failed" -> "\"MaximumDAScale\" analysis parameter is not NaN?";
+        "DAScale is larger than \"MaximumDAScale\"?";
+        "\"MaximumDAScale\" analysis parameter is not NaN?" -> "DAScale is larger than \"MaximumDAScale\"?"  [label="Yes"];
+        "Mark headstage as \"DAScale exceeded\"\nin labnotebook";
+        "DAScale is larger than \"MaximumDAScale\"?" -> "Mark headstage as \"DAScale exceeded\"\nin labnotebook"  [label="Yes"];
+        "Mark headstage as \"DAScale exceeded\"\nin labnotebook" -> "Set DAScale to zero";
+        "Have all headstages passed in one sweep\nof their stimset cycle?" -> "Mark sweep as failed"  [label="No"];
+        "Have all active headstages an DAScale exceeded\n labnotebook entry?";
+        "Mark sweep as failed" -> "Have all active headstages an DAScale exceeded\n labnotebook entry?";
+        "Have all active headstages an DAScale exceeded\n labnotebook entry?" -> "Skip to end of active set"  [label="Yes"];
+        "Store pulse duration in labnotebook";
+        "Post Sweep Event" -> "Store pulse duration in labnotebook";
+    }
+
+    "Post DAQ Event" -> "Read active headstages from labnotebook\nand turn them all on again";
+    "Update Dashboard";
+    "Read active headstages from labnotebook\nand turn them all on again" -> "Update Dashboard";
+    "Post Set Event" -> "Mark set as passed if 1 sweep passed,\nas failure otherwise";
+    "Start DAQ/RA (only call for last active headstage)" -> "Post Set Event";
+    "Analysis parameter \"PostDAQSetDAScale\" is true" -> "Set DAScale to the stored LBN value taking into account\nthe analysis parameter \"PostDAQDAScaleFactor\"\nand \"PostDAQDAScaleMinOffset\".\nSet it to \"PostDAQDAScaleForFailedHS\" for failed headstages.";
+    "Mark set as passed if 1 sweep passed,\nas failure otherwise";
+    "Set \"TP during ITI\" to enabled";
+    "Post Set Event" -> "Set \"TP during ITI\" to enabled";
+    "Set \"TP inserting\" to enabled";
+    "Set \"TP during ITI\" to enabled" -> "Set \"TP inserting\" to enabled";
+    "Mark set as passed if 1 sweep passed,\nas failure otherwise" -> "Analysis parameter \"PostDAQSetDAScale\" is true";
+    "Set \"User onset delay\" to 0ms";
+    "Set \"Termination delay\" to 0 ms";
+    "Set \"TP inserting\" to enabled" -> "Set \"User onset delay\" to 0ms";
+    "Set \"User onset delay\" to 0ms" -> "Set \"Termination delay\" to 0 ms";
+}

commit b455f27952034ba908a7db40093bd5b8e9983719

MSQ_FastRheoEst: Use binary unit for active headstage
It makes sense to just use the same unit as the statusHS wave, which is
from "Headstage Active" labnotebook entry.

Broken since 1e7ede23 (Add FastRheoEstimate analysis function,
2018-09-27).

Packages/MIES/MIES_AnalysisFunctions_MultiPatchSeq.ipf
@@ -735,7 +735,7 @@ Function MSQ_FastRheoEst(device, s)
 			key = CreateAnaFuncLBNKey(MSQ_FAST_RHEO_EST, MSQ_FMT_LBN_ACTIVE_HS)
 			Make/FREE/D/N=(LABNOTEBOOK_LAYER_COUNT) values = NaN
 			values[0, NUM_HEADSTAGES - 1] = statusHS[p]
-			ED_AddEntryToLabnotebook(device, key, values, overrideSweepNo = s.sweepNo)
+			ED_AddEntryToLabnotebook(device, key, values, unit = LABNOTEBOOK_BINARY_UNIT, overrideSweepNo = s.sweepNo)
 
 			for(i = 0; i < NUM_HEADSTAGES; i += 1)
 				if(statusHS[i] && !statusHSIC[i]) // active non-IC headstage

Packages/MIES/MIES_Constants.ipf
@@ -1048,7 +1048,7 @@ Constant PSQ_RAMP_VERSION            = 4
 Constant PSQ_RHEOBASE_VERSION        = 3
 Constant PSQ_SQUARE_PULSE_VERSION    = 1
 Constant PSQ_SEAL_EVALUATION_VERSION = 1
-Constant MSQ_FAST_RHEO_EST_VERSION   = 0
+Constant MSQ_FAST_RHEO_EST_VERSION   = 1
 Constant MSQ_DA_SCALE_VERSION        = 0
 Constant SC_SPIKE_CONTROL_VERSION    = 0
 /// @}