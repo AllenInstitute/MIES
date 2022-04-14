AllenInstitute/MIES

commit 6e32699f05566bd34b192616094dc9e78be6b093

PSQ_PipetteInBath: Add it
With tests and flowchart.

Packages/MIES/MIES_Constants.ipf
@@ -1028,12 +1028,15 @@ StrConstant PSQ_FMT_LBN_CR_CYCLES          = "%s cycle x values"
 StrConstant PSQ_FMT_LBN_CR_BOUNDS_STATE    = "%s bounds state"
 StrConstant PSQ_FMT_LBN_CR_SPIKE_CHECK     = "%s spike check"
 StrConstant PSQ_FMT_LBN_CR_SPIKE_PASS      = "%s spike QC"
+StrConstant PSQ_FMT_LBN_PB_RESISTANCE      = "%s pipette resistance"
+StrConstant PSQ_FMT_LBN_PB_RESISTANCE_PASS = "%s pipette resistance QC"
 /// @}
 
 StrConstant FMT_LBN_ANA_FUNC_VERSION = "%s version"
 
 /// @name Analysis function versions
 /// @{
+Constant PSQ_PIPETTE_BATH_VERSION  = 0
 Constant PSQ_CHIRP_VERSION         = 6
 Constant PSQ_DA_SCALE_VERSION      = 3
 Constant PSQ_RAMP_VERSION          = 4
@@ -1054,18 +1057,19 @@ Constant INVALID_ANALYSIS_FUNCTION = 0xFFFF
 
 /// @anchor PatchSeqAnalysisFunctionTypes
 /// @{
-Constant PSQ_DA_SCALE      = 0x01
-Constant PSQ_SQUARE_PULSE  = 0x02
-Constant PSQ_RHEOBASE      = 0x04
-Constant PSQ_RAMP          = 0x08
-Constant PSQ_CHIRP         = 0x80
+Constant PSQ_DA_SCALE      = 0x001
+Constant PSQ_SQUARE_PULSE  = 0x002
+Constant PSQ_RHEOBASE      = 0x004
+Constant PSQ_RAMP          = 0x008
+Constant PSQ_CHIRP         = 0x080
+Constant PSQ_PIPETTE_BATH  = 0x100
 /// @}
 
 /// @anchor MultiPatchSeqAnalysisFunctionTypes
 /// @{
-Constant MSQ_FAST_RHEO_EST = 0x10
-Constant MSQ_DA_SCALE      = 0x20
-Constant SC_SPIKE_CONTROL  = 0x40
+Constant MSQ_FAST_RHEO_EST = 0x010
+Constant MSQ_DA_SCALE      = 0x020
+Constant SC_SPIKE_CONTROL  = 0x040
 /// @}
 
 /// @}
@@ -1101,6 +1105,11 @@ Constant PSQ_CR_RESISTANCE_FAKE = 1 // GOhm
 Constant PSQ_CR_BASELINE_V_FAKE = 1 // mV
 Constant PSQ_CR_LIMIT_BAND_LOW  = 1 // mV
 Constant PSQ_CR_LIMIT_BAND_HIGH = 100 // mV
+/// @}
+
+/// @name PatchSeq Pipette
+/// @{
+Constant PSQ_PB_NUM_SWEEPS_PASS = 1
 /// @}
 
 /// @name Bounds action values, see also PSQ_CR_BoundsActionToString()

Packages/MIES/MIES_AnalysisFunctions_Dashboard.ipf
@@ -114,6 +114,10 @@ static Function/S AD_GetResultMessage(variable anaFuncType, variable passed, WAV
 	// - needs at least $NUM_DA_SCALES passing sweeps
 	//   and for supra mode if the FinalSlopePercent parameter is present this has to be reached as well
 
+	// PSQ_PB
+	// - baseline QC
+	// - pipette resistance QC
+
 	// PSQ_RA
 	// - baseline QC
 	// - needs at least PSQ_RA_NUM_SWEEPS_PASS passing sweeps
@@ -138,6 +142,8 @@ static Function/S AD_GetResultMessage(variable anaFuncType, variable passed, WAV
 			return AD_GetDaScaleFailMsg(numericalValues, textualValues, sweepNo, headstage)
 		case PSQ_RAMP:
 			return AD_GetRampFailMsg(numericalValues, textualValues, sweepNo, headstage)
+		case PSQ_PIPETTE_BATH:
+			return AD_GetPipetteInBathFailMsg(numericalValues, textualValues, sweepNo, headstage)
 		case PSQ_RHEOBASE:
 			return AD_GetRheobaseFailMsg(numericalValues, textualValues, sweepNo, headstage)
 		case PSQ_SQUARE_PULSE:
@@ -273,6 +279,7 @@ static Function AD_FillWaves(win, list, info)
 			switch(anaFuncType)
 				case PSQ_CHIRP:
 				case PSQ_DA_SCALE:
+				case PSQ_PIPETTE_BATH:
 				case PSQ_RAMP:
 				case PSQ_SQUARE_PULSE:
 				case MSQ_DA_SCALE:
@@ -476,6 +483,10 @@ static Function/S AD_GetRheobaseFailMsg(WAVE numericalValues, WAVE/T textualValu
 	return msg
 End
 
+static Function/S AD_GetPipetteInBathFailMsg(WAVE numericalValues, WAVE/T textualValues, variable sweepNo, variable headstage)
+	return AD_GetPerSweepFailMessage(PSQ_PIPETTE_BATH, numericalValues, textualValues, sweepNo, headstage, numRequiredPasses = PSQ_PB_NUM_SWEEPS_PASS)
+End
+
 static Function/S AD_GetFastRheoEstFailMsg(WAVE numericalValues, variable sweepNo, variable headstage)
 
 	string key
@@ -565,6 +576,7 @@ static Function/S AD_GetBaselineFailMsg(anaFuncType, numericalValues, sweepNo, h
 
 	switch(anaFuncType)
 		case PSQ_DA_SCALE:
+		case PSQ_PIPETTE_BATH:
 		case PSQ_RHEOBASE:
 		case PSQ_RAMP:
 		case PSQ_CHIRP:
@@ -675,7 +687,7 @@ End
 static Function/S AD_GetPerSweepFailMessage(variable anaFuncType, WAVE numericalValues, WAVE/T textualValues, variable refSweepNo, variable headstage, [variable numRequiredPasses])
 	string key, msg, str
 	string text = ""
-	variable numPasses, i, numSweeps, sweepNo, boundsAction, spikeCheck
+	variable numPasses, i, numSweeps, sweepNo, boundsAction, spikeCheck, resistancePass
 	string perSweepFailedMessage = ""
 
 	if(!ParamIsDefault(numRequiredPasses))
@@ -754,6 +766,22 @@ static Function/S AD_GetPerSweepFailMessage(variable anaFuncType, WAVE numerical
 
 				if(WaveExists(limitedResolution) && limitedResolution[headstage])
 					sprintf text, "Sweep %d failed: Limited resolution", sweepNo
+					break
+				endif
+				break
+			case PSQ_PIPETTE_BATH:
+				msg = AD_GetBaselineFailMsg(anaFuncType, numericalValues, sweepNo, headstage)
+
+				if(!IsEmpty(msg))
+					sprintf text, "Sweep %d failed: %s", sweepNo, msg
+					break
+				endif
+
+				key = CreateAnaFuncLBNKey(anaFuncType, PSQ_FMT_LBN_PB_RESISTANCE_PASS, query = 1)
+				resistancePass = GetLastSettingIndep(numericalValues, sweepNo, key, UNKNOWN_MODE)
+
+				if(IsFinite(resistancePass) && !resistancePass)
+					sprintf text, "Sweep %d failed: Pipette resistance is out of range", sweepNo
 					break
 				endif
 				break

Packages/MIES/MIES_MiesUtilities.ipf
@@ -21,6 +21,7 @@ static Constant GET_LB_MODE_READ  = 1
 
 static Constant GET_LB_MODE_WRITE = 2
 
+static StrConstant PSQ_PB_LBN_PREFIX = "Pipette in Bath"
 static StrConstant PSQ_CR_LBN_PREFIX = "Chirp"
 static StrConstant PSQ_SP_LBN_PREFIX = "Squ. Pul."
 static StrConstant PSQ_DS_LBN_PREFIX = "DA Scale"
@@ -6995,6 +6996,8 @@ Function MapAnaFuncToConstant(anaFunc)
 			return PSQ_CHIRP
 		case "PSQ_DaScale":
 			return PSQ_DA_SCALE
+		case "PSQ_PipetteInBath":
+			return PSQ_PIPETTE_BATH
 		case "PSQ_Rheobase":
 			return PSQ_RHEOBASE
 		case "PSQ_SquarePulse":
@@ -7047,6 +7050,9 @@ Function/S CreateAnaFuncLBNKey(type, formatString, [chunk, query])
 		case PSQ_SQUARE_PULSE:
 			prefix = PSQ_SP_LBN_PREFIX
 			break
+		case PSQ_PIPETTE_BATH:
+			prefix = PSQ_PB_LBN_PREFIX
+			break
 		default:
 			return ""
 			break
@@ -7084,6 +7090,8 @@ Function GetAnalysisFunctionVersion(variable type)
 			return PSQ_CHIRP_VERSION
 		case PSQ_DA_SCALE:
 			return PSQ_DA_SCALE_VERSION
+		case PSQ_PIPETTE_BATH:
+			return PSQ_PIPETTE_BATH_VERSION
 		case PSQ_RAMP:
 			return PSQ_RAMP_VERSION
 		case PSQ_RHEOBASE:

Packages/Testing-MIES/UTF_HardwareMain.ipf
@@ -25,6 +25,7 @@
 #include "UTF_MultiPatchSeqSpikeControl"
 #include "UTF_PatchSeqChirp"
 #include "UTF_PatchSeqDAScale"
+#include "UTF_PatchSeqPipetteInBath"
 #include "UTF_PatchSeqRamp"
 #include "UTF_PatchSeqRheobase"
 #include "UTF_PatchSeqSquarePulse"
@@ -102,6 +103,7 @@ Function RunWithOpts([string testcase, string testsuite, variable allowdebug, va
 	list = AddListItem("UTF_PatchSeqSquarePulse.ipf", list, ";", inf)
 	list = AddListItem("UTF_PatchSeqRheobase.ipf", list, ";", inf)
 	list = AddListItem("UTF_PatchSeqRamp.ipf", list, ";", inf)
+	list = AddListItem("UTF_PatchSeqPipetteInBath.ipf", list, ";", inf)
 	list = AddListItem("UTF_ReachTargetVoltage.ipf", list, ";", inf)
 	list = AddListItem("UTF_MultiPatchSeqFastRheoEstimate.ipf", list, ";", inf)
 	list = AddListItem("UTF_MultiPatchSeqDAScale.ipf", list, ";", inf)

Packages/doc/dot/patch-seq-pipette-bath.dot
@@ -1,0 +1,122 @@
+digraph G {
+	graph [center=true,
+		color=white,
+		compound=true,
+		fontcolor=black,
+		fontname=Helvetica,
+		fontsize=14,
+		penwidth=0.1,
+		ratio="0,01"
+	];
+	node [color="#bdd7e7",
+		fillcolor="#eff3ff",
+		fontcolor="#2171b5",
+		fontname=Helvetica,
+		fontsize=13,
+		label="\N",
+		shape=ellipse,
+		style=filled
+	];
+	edge [color="#9ecae1",
+		fontcolor="#3182bd",
+		fontname=Helvetica,
+		fontsize=10,
+		style=solid
+	];
+	"Start DAQ/RA" -> "Mid Sweep Event";
+	"Start DAQ/RA" -> "Post DAQ Event";
+	"Start DAQ/RA" -> "Post Set Event";
+	"Start DAQ/RA" -> "Post Sweep Event";
+	"Prepare DAQ/Pre Set Event"	 [label="Pre DAQ Event"];
+	"Start DAQ/RA" -> "Prepare DAQ/Pre Set Event";
+	n83	 [label="Pre Sweep Config Event"];
+	"Start DAQ/RA" -> n83;
+	n85	 [label="Pre Set Event"];
+	"Start DAQ/RA" -> n85;
+	"Mid Sweep Event" -> "BL QC passed?";
+	"Post DAQ Event" -> "Update Dashboard";
+	n81	 [label="Has passing set QC\n labnotebook entry"];
+	"Post DAQ Event" -> n81;
+	"Post Set Event" -> "Update Dashboard";
+	"Has three passing sweeps in set\n with the same DAScale value? [1]"	 [label="Has one passing sweep?"];
+	"Post Set Event" -> "Has three passing sweeps in set\n with the same DAScale value? [1]";
+	n55	 [label="Publish results via ZeroMQ message"];
+	"Post Set Event" -> n55;
+	n87	 [label="Check sampling frequency\n and store the QC state in the labnotebook"];
+	"Post Sweep Event" -> n87;
+	"Enable \"TP during ITI\""	 [label="Disable \"Indexing\""];
+	"Prepare DAQ/Pre Set Event" -> "Enable \"TP during ITI\"";
+	"Prepare DAQ/Pre Set Event" -> "Check if no TTL channels are active";
+	n84	 [label="Check that the number of testpulses\n from the analysis parameter \"NumberOfTestpulses\"\n matches the number of actually present \
+test pulses"];
+	n83 -> n84;
+	n86	 [label="Set analysis function version\n in labnotebook"];
+	n85 -> n86;
+	"BL QC passed?" -> "Grab next completely acquired chunk\n(500ms) of stimset baseline"	 [label=No];
+	n82	 [label="Set stimset from analysis parameter\n \"NextStimSetName\""];
+	n81 -> n82	 [label=Yes];
+	"Has three passing sweeps in set\n with the same DAScale value? [1]" -> "Mark set as failed\n in labnotebook"	 [label=No];
+	"Has three passing sweeps in set\n with the same DAScale value? [1]" -> "Mark set as passed\n in labnotebook"	 [label=Yes];
+	n93	 [label="Read the \"Steady State Resistance\"\n from the SweepFormula results\n labnotebook entry"];
+	n87 -> n93;
+	"Enable \"TP during ITI\"" -> "Enable \"Multidevice DAQ\"";
+	"Check if mode is IC"	 [label="Check if mode is VC"];
+	"Check if no TTL channels are active" -> "Check if mode is IC";
+	"Grab next completely acquired chunk\n(500ms) of stimset baseline" -> "Chunk already evaluated?"	 [label="Have one"];
+	"Chunk already evaluated?" -> "Grab next completely acquired chunk\n(500ms) of stimset baseline"	 [label=Yes];
+	"Chunk already evaluated?" -> "Baseline chunk type"	 [label=No];
+	"Enable \"Multidevice DAQ\"" -> "Disable \"dDAQ\"/\"oodDAQ\"";
+	"Check if mode is IC" -> "Check if only one headstage is active";
+	"Baseline chunk type" -> "Check RMS short"	 [label="pre pulse"];
+	"Disable \"dDAQ\"/\"oodDAQ\"" -> "Enable \"Repeated Acquisition\"";
+	"Check RMS short" -> "Check RMS long"	 [label=Passed];
+	"Add BL QC failed labnotebook entry"	 [label="Add BL chunk QC failed\n labnotebook entry"];
+	"Check RMS short" -> "Add BL QC failed labnotebook entry"	 [label=Failed];
+	n88	 [label="Open Databrowser\n and attach to the current device"];
+	"Enable \"Repeated Acquisition\"" -> n88;
+	n91	 [label="Apply auto pipette offset"];
+	"Enable \"Repeated Acquisition\"" -> n91;
+	"Check if only one headstage is active" -> "All checks passed";
+	"Check RMS long" -> "Add BL QC failed labnotebook entry"	 [label=Failed];
+	"Check target voltage [1]"	 [label="Check leak current"];
+	"Check RMS long" -> "Check target voltage [1]"	 [label=Passed];
+	n79	 [label="Add BL QC failed\n labnotebook entry"];
+	"Add BL QC failed labnotebook entry" -> n79;
+	n90	 [label="Enable \"Sweep Formula\" in the databrowser"];
+	n88 -> n90;
+	"All checks passed" -> Abort	 [label=No];
+	"Mark chunk as passed/failed [1]"	 [label="Add BL chunk QC passed\n labnotebook entry"];
+	"Check target voltage [1]" -> "Mark chunk as passed/failed [1]"	 [label=Passed];
+	n78	 [label="Apply auto pipette offset"];
+	"Check target voltage [1]" -> n78	 [label=Failed];
+	n79 -> "Early Sweep stop";
+	n80	 [label="Add BL QC passed\n labnotebook entry"];
+	"Mark chunk as passed/failed [1]" -> n80;
+	n78 -> "Add BL QC failed labnotebook entry";
+	n89	 [label="Read \"SamplingMultiplier\" analysis parameter and apply it"];
+	n84 -> Abort	 [label=Yes];
+	n86 -> n89;
+	n94	 [label="Is the value between the\n \"MinPipetteResistance\" and \"MaxPipetteResitance\"\n analysis parameters?"];
+	n93 -> n94;
+	n95	 [label="Store it in the labnotebook"];
+	n93 -> n95;
+	n92	 [label="Set SweepFormula code for calculating\n testpulse properties from testpulse like epochs in the sweep"];
+	n90 -> n92;
+	n96	 [label="Store it in the labnotebook"];
+	n94 -> n96;
+	n98	 [label="Does the labnotebook has passing\n baseline QC\npipette resistance QC\n and sampling frequency QC entries?"];
+	n96 -> n98;
+	n99	 [label="Mark sweep as passed"];
+	n98 -> n99	 [label=Yes];
+	n100	 [label="Mark sweep as failed"];
+	n98 -> n100;
+	n101	 [label="Skip to end of Set"];
+	n99 -> n101;
+	n102	 [label="Is the number of failed sweeps\n larger than the \"NumberofFailedSweeps\" analysis parameter?"];
+	n100 -> n102;
+	n104	 [label="Has the sampling frequency QC check failed?"];
+	n100 -> n104;
+	n103	 [label="Skip to end of DAQ"];
+	n102 -> n103;
+	n104 -> n103	 [label=Yes];
+}

commit f2e1a9016e533dbe9abecb5cb1a04afc1711a4c0

PSQ_PipetteInBath: Store resistance values with correct decimal multiplier
We get from the SweepFromula MegaOhm and want to store Ohm in the
labnotebook. We therefore need to scale by 1e6 and not 1e9.

And while at it, we also prefer MEGA_TO_ONE.

Broken since 6e32699f (PSQ_PipetteInBath: Add it, 2021-12-08).

Packages/MIES/MIES_AnalysisFunctions_PatchSeq.ipf
@@ -4529,7 +4529,7 @@ Function PSQ_PipetteInBath(string device, struct AnalysisFunction_V3& s)
 			// END TEST
 
 			Make/D/FREE/N=(LABNOTEBOOK_LAYER_COUNT) resistance = NaN
-			resistance[INDEP_HEADSTAGE] = pipetteResistance * 1e9
+			resistance[INDEP_HEADSTAGE] = pipetteResistance * MEGA_TO_ONE
 			key = CreateAnaFuncLBNKey(PSQ_PIPETTE_BATH, PSQ_FMT_LBN_PB_RESISTANCE)
 			ED_AddEntryToLabnotebook(device, key, resistance, unit = "Ω", overrideSweepNo = s.sweepNo)
 

Packages/MIES/MIES_Constants.ipf
@@ -1050,7 +1050,7 @@ StrConstant FMT_LBN_ANA_FUNC_VERSION = "%s version"
 
 /// @name Analysis function versions
 /// @{
-Constant PSQ_PIPETTE_BATH_VERSION    = 0
+Constant PSQ_PIPETTE_BATH_VERSION    = 1
 Constant PSQ_CHIRP_VERSION           = 7
 Constant PSQ_DA_SCALE_VERSION        = 3
 Constant PSQ_RAMP_VERSION            = 4

Packages/Testing-MIES/UTF_PatchSeqPipetteInBath.ipf
@@ -308,7 +308,7 @@ static Function PS_PB1_REENTRY([str])
 	CHECK_WAVE(entries[%leakCurPass], NULL_WAVE)
 	CHECK_WAVE(entries[%leakCur], NULL_WAVE)
 
-	Make/D resistanceRef = {5e9, 5e9, 5e9}
+	Make/D resistanceRef = {5e6, 5e6, 5e6}
 	CHECK_EQUAL_WAVES(entries[%resistance], resistanceRef, mode = WAVE_DATA)
 	CHECK_EQUAL_WAVES(entries[%resistancePass], {0, 0, 0}, mode = WAVE_DATA)
 
@@ -373,7 +373,7 @@ static Function PS_PB2_REENTRY([str])
 	CHECK_EQUAL_WAVES(entries[%leakCurPass], {1}, mode = WAVE_DATA)
 	CHECK_WAVE(entries[%leakCur], NUMERIC_WAVE)
 
-	Make/FREE/D resistanceRef = {12.5e9}
+	Make/FREE/D resistanceRef = {12.5e6}
 	CHECK_EQUAL_WAVES(entries[%resistance], resistanceRef, mode = WAVE_DATA)
 	CHECK_EQUAL_WAVES(entries[%resistancePass], {1}, mode = WAVE_DATA)
 
@@ -450,7 +450,7 @@ static Function PS_PB3_REENTRY([str])
 	CHECK_EQUAL_WAVES(entries[%leakCurPass], {1, NaN, 1}, mode = WAVE_DATA)
 	CHECK_WAVE(entries[%leakCur], NUMERIC_WAVE)
 
-	Make/FREE/D resistanceRef = {5e9, 12.5e9, 5e9}
+	Make/FREE/D resistanceRef = {5e6, 12.5e6, 5e6}
 	CHECK_EQUAL_WAVES(entries[%resistance], resistanceRef, mode = WAVE_DATA)
 	CHECK_EQUAL_WAVES(entries[%resistancePass], {0, 1, 0}, mode = WAVE_DATA)
 
@@ -517,7 +517,7 @@ static Function PS_PB4_REENTRY([str])
 	CHECK_WAVE(entries[%leakCurPass], NULL_WAVE)
 	CHECK_WAVE(entries[%leakCur], NULL_WAVE)
 
-	Make/FREE/D resistanceRef = {5e9, 5e9}
+	Make/FREE/D resistanceRef = {5e6, 5e6}
 	CHECK_EQUAL_WAVES(entries[%resistance], resistanceRef, mode = WAVE_DATA)
 	CHECK_EQUAL_WAVES(entries[%resistancePass], {0, 0}, mode = WAVE_DATA)
 
@@ -586,7 +586,7 @@ static Function PS_PB5_REENTRY([str])
 	CHECK_EQUAL_WAVES(entries[%leakCurPass], {0, 0, 0}, mode = WAVE_DATA)
 	CHECK_WAVE(entries[%leakCur], NUMERIC_WAVE)
 
-	Make/FREE/D resistanceRef = {12.5e9, 12.5e9, 12.5e9}
+	Make/FREE/D resistanceRef = {12.5e6, 12.5e6, 12.5e6}
 	CHECK_EQUAL_WAVES(entries[%resistance], resistanceRef, mode = WAVE_DATA)
 	CHECK_EQUAL_WAVES(entries[%resistancePass], {1, 1, 1}, mode = WAVE_DATA)
 
@@ -655,7 +655,7 @@ static Function PS_PB6_REENTRY([str])
 	CHECK_WAVE(entries[%leakCur], NULL_WAVE)
 	CHECK_WAVE(entries[%leakCur], NULL_WAVE)
 
-	Make/FREE/D resistanceRef = {12.5e9, 12.5e9, 12.5e9}
+	Make/FREE/D resistanceRef = {12.5e6, 12.5e6, 12.5e6}
 	CHECK_EQUAL_WAVES(entries[%resistance], resistanceRef, mode = WAVE_DATA)
 	CHECK_EQUAL_WAVES(entries[%resistancePass], {1, 1, 1}, mode = WAVE_DATA)
 
@@ -724,7 +724,7 @@ static Function PS_PB7_REENTRY([str])
 	CHECK_EQUAL_WAVES(entries[%leakCurPass], {1}, mode = WAVE_DATA)
 	CHECK_WAVE(entries[%leakCur], NUMERIC_WAVE)
 
-	Make/FREE/D resistanceRef = {12.5e9}
+	Make/FREE/D resistanceRef = {12.5e6}
 	CHECK_EQUAL_WAVES(entries[%resistance], resistanceRef, mode = WAVE_DATA)
 	CHECK_EQUAL_WAVES(entries[%resistancePass], {1}, mode = WAVE_DATA)
 