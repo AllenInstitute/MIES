AllenInstitute/MIES

commit 1a64ca17d2278e0329ef93e4a42a9fdb182e5cd2

PSQ_SealEvaluation: Add it

Packages/MIES/MIES_Constants.ipf
@@ -642,7 +642,7 @@ Constant ANALYSISBROWSER_PANEL_VERSION    =  1
 /// - New/Changed layers of entries
 ///
 /// @{
-Constant LABNOTEBOOK_VERSION = 55
+Constant LABNOTEBOOK_VERSION = 56
 Constant RESULTS_VERSION     = 2
 /// @}
 
@@ -1030,21 +1030,27 @@ StrConstant PSQ_FMT_LBN_CR_SPIKE_CHECK     = "%s spike check"
 StrConstant PSQ_FMT_LBN_CR_SPIKE_PASS      = "%s spike QC"
 StrConstant PSQ_FMT_LBN_PB_RESISTANCE      = "%s pipette resistance"
 StrConstant PSQ_FMT_LBN_PB_RESISTANCE_PASS = "%s pipette resistance QC"
+StrConstant PSQ_FMT_LBN_SE_RESISTANCE_A    = "%s seal resistance A"
+StrConstant PSQ_FMT_LBN_SE_RESISTANCE_B    = "%s seal resistance B"
+StrConstant PSQ_FMT_LBN_SE_RESISTANCE_MAX  = "%s seal resistance max"
+StrConstant PSQ_FMT_LBN_SE_RESISTANCE_PASS = "%s seal resistance QC"
+StrConstant PSQ_FMT_LBN_SE_TESTPULSE_GROUP = "%s test pulse group"
 /// @}
 
 StrConstant FMT_LBN_ANA_FUNC_VERSION = "%s version"
 
 /// @name Analysis function versions
 /// @{
-Constant PSQ_PIPETTE_BATH_VERSION  = 0
-Constant PSQ_CHIRP_VERSION         = 6
-Constant PSQ_DA_SCALE_VERSION      = 3
-Constant PSQ_RAMP_VERSION          = 4
-Constant PSQ_RHEOBASE_VERSION      = 3
-Constant PSQ_SQUARE_PULSE_VERSION  = 1
-Constant MSQ_FAST_RHEO_EST_VERSION = 0
-Constant MSQ_DA_SCALE_VERSION      = 0
-Constant SC_SPIKE_CONTROL_VERSION  = 0
+Constant PSQ_PIPETTE_BATH_VERSION    = 0
+Constant PSQ_CHIRP_VERSION           = 6
+Constant PSQ_DA_SCALE_VERSION        = 3
+Constant PSQ_RAMP_VERSION            = 4
+Constant PSQ_RHEOBASE_VERSION        = 3
+Constant PSQ_SQUARE_PULSE_VERSION    = 1
+Constant PSQ_SEAL_EVALUATION_VERSION = 0
+Constant MSQ_FAST_RHEO_EST_VERSION   = 0
+Constant MSQ_DA_SCALE_VERSION        = 0
+Constant SC_SPIKE_CONTROL_VERSION    = 0
 /// @}
 
 /// @name PatchSeq and MultiPatchSeq types of analysis functions
@@ -1057,12 +1063,13 @@ Constant INVALID_ANALYSIS_FUNCTION = 0xFFFF
 
 /// @anchor PatchSeqAnalysisFunctionTypes
 /// @{
-Constant PSQ_DA_SCALE      = 0x001
-Constant PSQ_SQUARE_PULSE  = 0x002
-Constant PSQ_RHEOBASE      = 0x004
-Constant PSQ_RAMP          = 0x008
-Constant PSQ_CHIRP         = 0x080
-Constant PSQ_PIPETTE_BATH  = 0x100
+Constant PSQ_DA_SCALE        = 0x001
+Constant PSQ_SQUARE_PULSE    = 0x002
+Constant PSQ_RHEOBASE        = 0x004
+Constant PSQ_RAMP            = 0x008
+Constant PSQ_CHIRP           = 0x080
+Constant PSQ_PIPETTE_BATH    = 0x100
+Constant PSQ_SEAL_EVALUATION = 0x200
 /// @}
 
 /// @anchor MultiPatchSeqAnalysisFunctionTypes
@@ -1110,6 +1117,19 @@ Constant PSQ_CR_LIMIT_BAND_HIGH = 100 // mV
 /// @name PatchSeq Pipette
 /// @{
 Constant PSQ_PB_NUM_SWEEPS_PASS = 1
+/// @}
+
+/// @name PatchSeq SealCheck
+/// @{
+Constant PSQ_SE_NUM_SWEEPS_PASS = 1
+Constant PSQ_SE_REQUIRED_EPOCHS = 22
+/// @}
+
+/// @name Testpulse Group Selector values, see also PSQ_SE_ParseTestpulseGroupSelection()
+/// @{
+Constant PSQ_SE_TGS_FIRST  = 0x1
+Constant PSQ_SE_TGS_SECOND = 0x2
+Constant PSQ_SE_TGS_BOTH   = 0x3
 /// @}
 
 /// @name Bounds action values, see also PSQ_CR_BoundsActionToString()
@@ -1594,6 +1614,7 @@ StrConstant AUTO_TP_FILTER                = "testpulse:autotune result"
 StrConstant AMPLIFIER_CLAMP_MODE_FILTER   = "amplifier:clamp mode"
 StrConstant AMPLIFIER_AUTO_BRIDGE_BALANCE = "amplifier:auto bridge balance"
 StrConstant ANALYSIS_FUNCTION_PB          = "analysis function:pipette in bath"
+StrConstant ANALYSIS_FUNCTION_SE          = "analysis function:seal evaluation"
 /// @}
 
 /// which is sufficient to represent each sample point time with a distinctive number up to rates of 10 MHz.

Packages/MIES/MIES_MiesUtilities.ipf
@@ -27,6 +27,7 @@ static StrConstant PSQ_SP_LBN_PREFIX = "Squ. Pul."
 static StrConstant PSQ_DS_LBN_PREFIX = "DA Scale"
 static StrConstant PSQ_RB_LBN_PREFIX = "Rheobase"
 static StrConstant PSQ_RA_LBN_PREFIX = "Ramp"
+static StrConstant PSQ_SE_LBN_PREFIX = "Seal evaluation"
 
 static StrConstant MSQ_FRE_LBN_PREFIX = "F Rheo E"
 static StrConstant MSQ_DS_LBN_PREFIX  = "Da Scale"
@@ -6991,6 +6992,8 @@ Function MapAnaFuncToConstant(anaFunc)
 			return PSQ_DA_SCALE
 		case "PSQ_PipetteInBath":
 			return PSQ_PIPETTE_BATH
+		case "PSQ_SealEvaluation":
+			return PSQ_SEAL_EVALUATION
 		case "PSQ_Rheobase":
 			return PSQ_RHEOBASE
 		case "PSQ_SquarePulse":
@@ -7042,6 +7045,8 @@ Function/S CreateAnaFuncLBNKey(type, formatString, [chunk, query])
 			break
 		case PSQ_SQUARE_PULSE:
 			prefix = PSQ_SP_LBN_PREFIX
+		case PSQ_SEAL_EVALUATION:
+			prefix = PSQ_SE_LBN_PREFIX
 			break
 		case PSQ_PIPETTE_BATH:
 			prefix = PSQ_PB_LBN_PREFIX
@@ -7091,6 +7096,8 @@ Function GetAnalysisFunctionVersion(variable type)
 			return PSQ_RHEOBASE_VERSION
 		case PSQ_SQUARE_PULSE:
 			return PSQ_SQUARE_PULSE_VERSION
+		case PSQ_SEAL_EVALUATION:
+			return PSQ_SEAL_EVALUATION_VERSION
 		// MSQ
 		case MSQ_FAST_RHEO_EST:
 			return MSQ_FAST_RHEO_EST_VERSION

Packages/MIES/MIES_ForeignFunctionInterface.ipf
@@ -81,7 +81,7 @@ Function/WAVE FFI_GetAvailableMessageFilters()
 
 	Make/FREE/T wv = {ZeroMQ_HEARTBEAT, IVS_PUB_FILTER, PRESSURE_STATE_FILTER, PRESSURE_SEALED_FILTER, \
 					  PRESSURE_BREAKIN_FILTER, AUTO_TP_FILTER, AMPLIFIER_CLAMP_MODE_FILTER,            \
-					  AMPLIFIER_AUTO_BRIDGE_BALANCE, ANALYSIS_FUNCTION_PB}
+					  AMPLIFIER_AUTO_BRIDGE_BALANCE, ANALYSIS_FUNCTION_PB, ANALYSIS_FUNCTION_SE}
 
 	Note/K wv "Heartbeat is sent every 5 seconds."
 

Packages/Testing-MIES/UTF_HardwareMain.ipf
@@ -28,6 +28,7 @@
 #include "UTF_PatchSeqPipetteInBath"
 #include "UTF_PatchSeqRamp"
 #include "UTF_PatchSeqRheobase"
+#include "UTF_PatchSeqSealEvaluation"
 #include "UTF_PatchSeqSquarePulse"
 #include "UTF_ReachTargetVoltage"
 #include "UTF_SetControls"
@@ -100,6 +101,7 @@ Function RunWithOpts([string testcase, string testsuite, variable allowdebug, va
 	list = AddListItem("UTF_SetControls.ipf", list, ";", inf)
 	list = AddListItem("UTF_PatchSeqChirp.ipf", list)
 	list = AddListItem("UTF_PatchSeqDAScale.ipf", list, ";", inf)
+	list = AddListItem("UTF_PatchSeqSealEvaluation.ipf", list, ";", inf)
 	list = AddListItem("UTF_PatchSeqSquarePulse.ipf", list, ";", inf)
 	list = AddListItem("UTF_PatchSeqRheobase.ipf", list, ";", inf)
 	list = AddListItem("UTF_PatchSeqRamp.ipf", list, ";", inf)

Packages/Testing-MIES/UTF_ZeroMQPublishing.ipf
@@ -272,3 +272,50 @@ static Function CheckPipetteInBathPublishing()
 
 	JSON_Release(jsonID)
 End
+
+static Function CheckSealEvaluationPublishing()
+	string device, msg, expected, actual
+	variable headstage, i, jsonID, value, sweepNo
+
+	WaitForPubSubHeartbeat()
+
+	device = "my_device"
+	headstage = 0
+	sweepNo = 0
+
+	// BEGIN required entries
+	ED_AddEntryToLabnotebook(device, "Seal evaluation Set QC", {NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, 1}, unit = LABNOTEBOOK_BINARY_UNIT, overrideSweepNo = sweepNo)
+	ED_AddEntryToLabnotebook(device, "Seal evaluation seal resistance max", {NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, 123}, unit = "Ω", overrideSweepNo = sweepNo)
+
+	Make/FREE/N=(1, 1, LABNOTEBOOK_LAYER_COUNT) values = NaN
+	Make/T/FREE/N=(3, 1, 1) keys
+
+	values[headstage] = GetUniqueInteger()
+	keys[0][0][0] = STIMSET_ACQ_CYCLE_ID_KEY
+	keys[2][0][0] = "1"
+
+	ED_AddEntriesToLabnotebook(values, keys, sweepNo, device, DATA_ACQUISITION_MODE)
+	// END required entries
+
+	MIES_PSQ#PSQ_SE_Publish(device, sweepNo, headstage)
+
+	msg = FetchPublishedMessage(ANALYSIS_FUNCTION_SE)
+
+	jsonID = JSON_Parse(msg)
+
+	expected = LABNOTEBOOK_BINARY_UNIT
+	actual   = JSON_GetString(jsonID, "/results/USER_Seal evaluation Set QC/unit")
+	CHECK_EQUAL_STR(actual, expected)
+
+	WAVE/Z entries = JSON_GetWave(jsonID, "/results/USER_Seal evaluation Set QC/value")
+	CHECK_EQUAL_WAVES(entries, {1}, mode = WAVE_DATA)
+
+	expected = "Ω"
+	actual = JSON_GetString(jsonID, "/results/USER_Seal evaluation seal resistance max/unit")
+	CHECK_EQUAL_STR(actual, expected)
+
+	WAVE/Z entries = JSON_GetWave(jsonID, "/results/USER_Seal evaluation seal resistance max/value")
+	CHECK_EQUAL_WAVES(entries, {123}, mode = WAVE_DATA)
+
+	JSON_Release(jsonID)
+End

commit be830309c45918e83aa4a6dcb5a563ed58022b9e

CreateAnaFuncLBNKey: Add missing break after PSQ_SQUARE_PULSE
Bug introduced in 1a64ca17 (PSQ_SealEvaluation: Add it, 2022-02-22).

The fallout is that the analysis functions PSQ_SquarePulse also uses the
PSQ_SealEvaluation labnotebook prefix.

Affected users need to translate the entries from "Seal evaluation" to
"Squ. Pul." for PSQ_SquarePulse and disambiguate them for
PSQ_SealEvaluation.

Packages/MIES/MIES_Constants.ipf
@@ -1108,7 +1108,7 @@ Constant PSQ_CHIRP_VERSION           = 8
 Constant PSQ_DA_SCALE_VERSION        = 3
 Constant PSQ_RAMP_VERSION            = 4
 Constant PSQ_RHEOBASE_VERSION        = 3
-Constant PSQ_SQUARE_PULSE_VERSION    = 1
+Constant PSQ_SQUARE_PULSE_VERSION    = 2
 Constant PSQ_SEAL_EVALUATION_VERSION = 1
 Constant PSQ_TRUE_REST_VM_VERSION    = 0
 Constant MSQ_FAST_RHEO_EST_VERSION   = 1

Packages/MIES/MIES_MiesUtilities.ipf
@@ -7071,6 +7071,7 @@ Function/S CreateAnaFuncLBNKey(type, formatString, [chunk, query])
 			break
 		case PSQ_SQUARE_PULSE:
 			prefix = PSQ_SP_LBN_PREFIX
+			break
 		case PSQ_SEAL_EVALUATION:
 			prefix = PSQ_SE_LBN_PREFIX
 			break