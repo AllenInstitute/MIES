9d34248e59bad1015fcd1c1a3b92d6ada9ee0937
Thomas Braun <thomas.braun@byte-physics.de>
2022-07-20 16:58:42 +0200

    tools/clean_mies_installation.sh: Install TUF XOP for release package
    When skipping hardware XOPs we have a list of files to install. This list
    was not updated in 65994377 (Add TUFXOP for IP9-64bit, 2021-06-26).

--- tools/clean_mies_installation.sh
+++ tools/clean_mies_installation.sh
@@ -150,6 +150,7 @@ do
     cp -r  "$base_folder"/XOPs-IP${i}-64bit/MIESUtils*  "$xops64"
     cp -r  "$base_folder"/XOPs-IP${i}-64bit/JSON*  "$xops64"
     cp -r  "$base_folder"/XOPs-IP${i}-64bit/ZeroMQ*  "$xops64"
+    cp -r  "$base_folder"/XOPs-IP${i}-64bit/TUF*  "$xops64"
     cp -r  "$base_folder"/XOPs-IP${i}-64bit/libzmq*  "$xops64"
   fi
 
