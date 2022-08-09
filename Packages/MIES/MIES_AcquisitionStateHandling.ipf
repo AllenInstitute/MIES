AllenInstitute/MIES

commit 5cacbd0ffffabcfa9a07b491ac393efaf93926e6

AD_FillWaves: Handle prematurely stopped sweeps better
Sometimes users just stop a sweep with analysis functions attached. In
this case we can't rely on all labnotebook entries being present.

We now don't ignore these sweeps anymore. And also show a message for
ongoing stimsets.

But ongoing stimsets have the feature that they might be finished at some
point, therefore we need to revisit their last sweep the next time.

Packages/MIES/MIES_AnalysisFunctions_Dashboard.ipf
@@ -78,10 +78,12 @@ Function AD_Update(win)
 	DEBUGPRINT_ELAPSED(refTime)
 End
 
-static Function/S AD_GetResultMessage(variable anaFuncType, variable passed, WAVE numericalValues, WAVE/T textualValues, variable sweepNo, variable headstage)
+static Function/S AD_GetResultMessage(variable anaFuncType, variable passed, WAVE numericalValues, WAVE/T textualValues, variable sweepNo, variable headstage, variable ongoingDAQ)
 
 	if(passed)
 		return "Pass"
+	elseif(ongoingDAQ)
+		return "Sweep not yet finished"
 	endif
 
 	// MSQ_DA
@@ -149,7 +151,7 @@ static Function AD_FillWaves(win, list, info)
 	string win
 	WAVE/T list, info
 
-	variable i, j, headstage, passed, sweepNo, numEntries
+	variable i, j, headstage, passed, sweepNo, numEntries, ongoingDAQ
 	variable index, anaFuncType, stimsetCycleID, firstValid, lastValid
 	string key, anaFunc, stimset, msg
 
@@ -209,8 +211,15 @@ static Function AD_FillWaves(win, list, info)
 			stimsetCycleID = stimsetCycleIDs[headstage]
 
 			FindValue/RMD=[][0]/TXOP=4/TEXT=AD_FormatListKey(stimsetCycleID, headstage) info
-			if(V_Value >= 0) // already included
-				continue
+			if(V_Value >= 0)
+				if(!cmpstr(info[V_Value][%$"Ongoing DAQ"], "1"))
+					// if DAQ was ongoing we want to overwrite this entry and all later entries
+					index = V_Value
+					info[index, inf][] = ""
+				else
+					// otherwise we want to keep it
+					continue
+				endif
 			endif
 
 			WAVE/Z/T stimsets = GetLastSetting(textualValues, sweepNo, STIM_WAVE_NAME_KEY, DATA_ACQUISITION_MODE)
@@ -220,17 +229,14 @@ static Function AD_FillWaves(win, list, info)
 
 			if(anaFuncType == INVALID_ANALYSIS_FUNCTION)
 				passed = NaN
+				ongoingDAQ = 0
 			else
 				key = CreateAnaFuncLBNKey(anaFuncType, PSQ_FMT_LBN_SET_PASS, query = 1)
 				passed = GetLastSettingIndepSCI(numericalValues, sweepNo, key, headstage, UNKNOWN_MODE)
-
-				if(isNaN(passed))
-					// the set is not yet finished
-					continue
-				endif
+				ongoingDAQ = IsNaN(passed)
 			endif
 
-			msg = AD_GetResultMessage(anaFuncType, passed, numericalValues, textualValues, sweepNo, headstage)
+			msg = AD_GetResultMessage(anaFuncType, passed, numericalValues, textualValues, sweepNo, headstage, ongoingDAQ)
 
 			EnsureLargeEnoughWave(list, dimension = ROWS, minimumSize = index)
 			EnsureLargeEnoughWave(info, dimension = ROWS, minimumSize = index)
@@ -256,8 +262,14 @@ static Function AD_FillWaves(win, list, info)
 				case MSQ_FAST_RHEO_EST:
 				case SC_SPIKE_CONTROL:
 					key = CreateAnaFuncLBNKey(anaFuncType, PSQ_FMT_LBN_SWEEP_PASS, query = 1)
-					WAVE sweepPass = GetLastSettingIndepEachSCI(numericalValues, sweepNo, key, headstage, UNKNOWN_MODE)
-					ASSERT(DimSize(sweeps, ROWS) == DimSize(sweepPass, ROWS), "Unexpected wave sizes")
+					WAVE/Z sweepPass = GetLastSettingIndepEachSCI(numericalValues, sweepNo, key, headstage, UNKNOWN_MODE, defValue = 0)
+
+					if(!WaveExists(sweepPass))
+						Duplicate/FREE sweeps, sweepPass
+						sweepPass = 0
+					else
+						ASSERT(DimSize(sweeps, ROWS) == DimSize(sweepPass, ROWS), "Unexpected wave sizes")
+					endif
 
 					Duplicate/FREE sweeps, passingSweepsAll, failingSweepsAll
 					passingSweepsAll[] = sweepPass[p]  ? sweeps[p] : NaN
@@ -296,6 +308,7 @@ static Function AD_FillWaves(win, list, info)
 			info[index][%$STIMSET_ACQ_CYCLE_ID_KEY] = AD_FormatListKey(stimsetCycleID, headstage)
 			info[index][%$"Passing Sweeps"] = NumericWaveToList(passingSweeps, ";")
 			info[index][%$"Failing Sweeps"] = NumericWaveToList(failingSweeps, ";")
+			info[index][%$"Ongoing DAQ"] = num2str(ongoingDAQ)
 
 			SetNumberInWaveNote(list, NOTE_INDEX, ++index)
 		endfor

Packages/MIES/MIES_WaveDataFolderGetters.ipf
@@ -6282,7 +6282,7 @@ End
 Function/WAVE GetAnaFuncDashboardInfoWave(dfr)
 	DFREF dfr
 
-	variable versionOfNewWave = 2
+	variable versionOfNewWave = 3
 
 	ASSERT(DataFolderExistsDFR(dfr), "Invalid dfr")
 	WAVE/T/Z/SDFR=dfr wv = dashboardInfoWave
@@ -6291,13 +6291,15 @@ Function/WAVE GetAnaFuncDashboardInfoWave(dfr)
 		return wv
 	elseif(WaveExists(wv))
 		// handle upgrade
+		Redimension/N=(-1, 4) wv
 	else
-		Make/T/N=(MINIMUM_WAVE_SIZE, 3) dfr:dashboardInfoWave/Wave=wv
+		Make/T/N=(MINIMUM_WAVE_SIZE, 4) dfr:dashboardInfoWave/Wave=wv
 	endif
 
 	SetDimLabel COLS, 0, $STIMSET_ACQ_CYCLE_ID_KEY, wv
 	SetDimLabel COLS, 1, $"Passing Sweeps", wv
 	SetDimLabel COLS, 2, $"Failing Sweeps", wv
+	SetDimLabel COLS, 3, $"Ongoing DAQ", wv
 
 	SetWaveVersion(wv, versionOfNewWave)
 

commit c71562451fbef9149a5b66b6582acf61ca9250f5

AD_FillWaves: Handle sweeps without analysis functions better
If the stimset does not have an analysis function attached, we assumed
until now that it is never ongoing. But that is wrong as these stimsets
can also have multiple sweeps.

We now check if the SCI is different than the SCI of the last acquired
sweep and that data acquisition is ongoing. This works, but needs a final
update after DAQ.

Bug introduced in 5cacbd0f (AD_FillWaves: Handle prematurely stopped
sweeps better, 2021-10-22).

Packages/MIES/MIES_AcquisitionStateHandling.ipf
@@ -87,6 +87,8 @@ Function AS_HandlePossibleTransition(string device, variable newAcqState, [varia
 
 	switch(newAcqState)
 		case AS_INACTIVE:
+			AD_UpdateAllDatabrowser()
+			break
 		case AS_EARLY_CHECK:
 			// do nothing
 			break

Packages/MIES/MIES_AnalysisFunctions_Dashboard.ipf
@@ -225,6 +225,8 @@ static Function AD_FillWaves(win, list, info)
 			continue
 		endif
 
+		WAVE/Z lastSweepStimsetCycleIDs = GetLastSetting(numericalValues, WaveMax(totalSweepsPresent), STIMSET_ACQ_CYCLE_ID_KEY, DATA_ACQUISITION_MODE)
+
 		key = StringFromList(GENERIC_EVENT, EVENT_NAME_LIST_LBN)
 		WAVE/Z/T anaFuncs = GetLastSetting(textualValues, sweepNo, key, DATA_ACQUISITION_MODE)
 
@@ -271,7 +273,9 @@ static Function AD_FillWaves(win, list, info)
 
 			if(anaFuncType == INVALID_ANALYSIS_FUNCTION)
 				passed = NaN
-				ongoingDAQ = 0
+				// current sweep is from the same SCI than the last acquired sweep and DAQ is not inactive
+				ASSERT(WaveExists(lastSweepStimsetCycleIDs), "Missing last sweep SCIs")
+				ongoingDAQ = (lastSweepStimsetCycleIDs[headstage] == stimsetCycleID) && (acqState != AS_INACTIVE)
 			else
 				key = CreateAnaFuncLBNKey(anaFuncType, PSQ_FMT_LBN_SET_PASS, query = 1)
 				passed = GetLastSettingIndepSCI(numericalValues, sweepNo, key, headstage, UNKNOWN_MODE)