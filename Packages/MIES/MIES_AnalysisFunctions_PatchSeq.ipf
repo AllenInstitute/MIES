AllenInstitute/MIES

commit 0b29312896434a9d89ddd93c6b3ea2374ae8ede9

Patch Seq analysis functions: Make the baseline RMS short/long thresholds configurable

Packages/MIES/MIES_AnalysisFunctions_PatchSeq.ipf
@@ -40,7 +40,9 @@
 /// PSQ_FMT_LBN_SPIKE_DASCALE_ZERO  Sweep spiked with DAScale of 0                            Numerical                SP                       No                     No
 /// PSQ_FMT_LBN_INITIAL_SCALE       Initial DAScale                                           Numerical                RB, CR                   No                     No
 /// PSQ_FMT_LBN_RMS_SHORT_PASS      Short RMS baseline QC result                              Numerical                DA, RB, RA, CR           Yes                    Yes
+/// PSQ_FMT_LBN_RMS_SHORT_THRESHOLD Short RMS baseline threshold [V]                          Numerical                DA, RB, RA, CR           No                     Yes
 /// PSQ_FMT_LBN_RMS_LONG_PASS       Long RMS baseline QC result                               Numerical                DA, RB, RA, CR           Yes                    Yes
+/// PSQ_FMT_LBN_RMS_LONG_THRESHOLD  Long RMS baseline threshold [V]                           Numerical                DA, RB, RA, CR           No                     Yes
 /// PSQ_FMT_LBN_TARGETV             Target voltage baseline                                   Numerical                DA, RB, RA, CR           Yes                    Yes
 /// PSQ_FMT_LBN_TARGETV_PASS        Target voltage baseline QC result                         Numerical                DA, RB, RA, CR           Yes                    Yes
 /// PSQ_FMT_LBN_CHUNK_PASS          Which chunk passed/failed baseline QC                     Numerical                DA, RB, RA, CR           Yes                    Yes
@@ -217,6 +219,31 @@ static Function/WAVE PSQ_DeterminePulseDuration(panelTitle, sweepNo, type, total
 	return durations
 End
 
+/// @brief Return the baseline RMS short/long thresholds in mV
+static Function [variable rmsShortThreshold, variable rmsLongThreshold] PSQ_GetBaselineRMSThresholds(string params)
+
+	rmsShortThreshold = AFH_GetAnalysisParamNumerical("BaselineRMSShortThreshold", params, defValue = PSQ_RMS_SHORT_THRESHOLD)
+	rmsLongThreshold  = AFH_GetAnalysisParamNumerical("BaselineRMSLongThreshold", params, defValue = PSQ_RMS_LONG_THRESHOLD)
+
+	return [rmsShortThreshold, rmsLongThreshold]
+End
+
+static Function PSQ_StoreRMSThresholdsInLabnotebook(string paneltitle, variable type, variable sweepNo, variable headstage, variable rmsShortThreshold, variable rmsLongThreshold)
+	string key
+
+	Make/FREE/D/N=(LABNOTEBOOK_LAYER_COUNT) values = NaN
+
+	key = CreateAnaFuncLBNKey(type, PSQ_FMT_LBN_RMS_SHORT_THRESHOLD)
+	// mV -> V
+	values[headstage] = rmsShortThreshold * 1e-3
+	ED_AddEntryToLabnotebook(panelTitle, key, values, unit = "V", overrideSweepNo = sweepNo)
+
+	key = CreateAnaFuncLBNKey(type, PSQ_FMT_LBN_RMS_LONG_THRESHOLD)
+	// mV -> V
+	values[headstage] = rmsLongThreshold * 1e-3
+	ED_AddEntryToLabnotebook(panelTitle, key, values, unit = "V", overrideSweepNo = sweepNo)
+End
+
 static Function PSQ_EvaluateBaselinePassed(string panelTitle, variable type, variable sweepNo, variable headstage, variable chunk, variable ret)
 	variable baselineQCPassed
 	string key, msg
@@ -312,6 +339,7 @@ static Function PSQ_EvaluateBaselineProperties(string panelTitle, STRUCT Analysi
 	variable targetV, index
 	variable rmsShortPassedAll, rmsLongPassedAll, chunkPassed
 	variable targetVPassedAll, baselineType, chunkLengthTime
+	variable rmsShortThreshold, rmsLongThreshold
 	string msg, adUnit, ctrl, key
 
 	struct PSQ_PulseSettings ps
@@ -348,6 +376,8 @@ static Function PSQ_EvaluateBaselineProperties(string panelTitle, STRUCT Analysi
 	if(IsFinite(chunkPassed)) // already evaluated
 		return !chunkPassed
 	endif
+
+	[rmsShortThreshold, rmsLongThreshold] = PSQ_GetBaselineRMSThresholds(s.params)
 
 	// Rows: baseline types
 	// - 0: pre pulse
@@ -390,6 +420,9 @@ static Function PSQ_EvaluateBaselineProperties(string panelTitle, STRUCT Analysi
 
 		if(chunk == 0) // pre pulse baseline
 			chunkStartTime = totalOnsetDelay
+
+			// store baseline RMS short/long analysis parameters in labnotebook on first use
+			PSQ_StoreRMSThresholdsInLabnotebook(panelTitle, type, s.sweepNo, i, rmsShortThreshold, rmsLongThreshold)
 		else
 			ASSERT(durations[i] != 0, "Invalid calculated durations")
 			chunkStartTime = (totalOnsetDelay + ps.prePulseChunkLength + durations[i]) + chunk * ps.postPulseChunkLength
@@ -411,7 +444,7 @@ static Function PSQ_EvaluateBaselineProperties(string panelTitle, STRUCT Analysi
 
 			// check 1: RMS of the last 1.5ms of the baseline should be below 0.07mV
 			rmsShort[i]       = PSQ_Calculate(s.scaledDACWave, ADCol, evalStartTime, evalRangeTime, PSQ_CALC_METHOD_RMS)
-			rmsShortPassed[i] = rmsShort[i] < PSQ_RMS_SHORT_THRESHOLD
+			rmsShortPassed[i] = rmsShort[i] < rmsShortThreshold
 
 			sprintf msg, "RMS noise short: %g (%s)\r", rmsShort[i], ToPassFail(rmsShortPassed[i])
 			DEBUGPRINT(msg)
@@ -432,7 +465,7 @@ static Function PSQ_EvaluateBaselineProperties(string panelTitle, STRUCT Analysi
 
 			// check 2: RMS of the last 500ms of the baseline should be below 0.50mV
 			rmsLong[i]       = PSQ_Calculate(s.scaledDACWave, ADCol, evalStartTime, evalRangeTime, PSQ_CALC_METHOD_RMS)
-			rmsLongPassed[i] = rmsLong[i] < PSQ_RMS_LONG_THRESHOLD
+			rmsLongPassed[i] = rmsLong[i] < rmsLongThreshold
 
 			sprintf msg, "RMS noise long: %g (%s)", rmsLong[i], ToPassFail(rmsLongPassed[i])
 			DEBUGPRINT(msg)
@@ -1237,7 +1270,7 @@ End
 Function/S PSQ_DAScale_GetParams()
 	return "DAScales:wave,OperationMode:string,SamplingMultiplier:variable,[ShowPlot:variable],[OffsetOperator:string]," +        \
 		   "[FinalSlopePercent:variable],[MinimumSpikeCount:variable],[MaximumSpikeCount:variable],[DAScaleModifier:variable]," + \
-		   "[SamplingFrequency:variable]"
+		   "[SamplingFrequency:variable],[BaselineRMSShortThreshold:variable],[BaselineRMSLongThreshold:variable]"
 End
 
 Function/S PSQ_DAScale_GetHelp(string name)
@@ -1254,6 +1287,10 @@ Function/S PSQ_DAScale_GetHelp(string name)
 		case "SamplingMultiplier":
 			 return "Sampling multiplier, use 1 for no multiplier"
 			 break
+		case "BaselineRMSShortThreshold":
+			 return "Threshold value in mV for the short RMS baseline QC check (defaults to " + num2str(PSQ_RMS_SHORT_THRESHOLD) + ")"
+		case "BaselineRMSLongThreshold":
+			 return "Threshold value in mV for the long RMS baseline QC check (defaults to " + num2str(PSQ_RMS_LONG_THRESHOLD) + ")"
 		case "OffsetOperator":
 			 return "[Optional, defaults to \"+\"] Set the math operator to use for "      \
 					+ "combining the rheobase DAScale value from the previous run and "    \
@@ -1286,6 +1323,13 @@ Function/S PSQ_DAScale_CheckParam(string name, string params)
 	string str
 
 	strswitch(name)
+		case "BaselineRMSShortThreshold":
+		case "BaselineRMSLongThreshold":
+			val = AFH_GetAnalysisParamNumerical(name, params)
+			if(!(val > 0 && val <= 20))
+				return "Invalid value " + num2str(val)
+			endif
+			break
 		case "DAScales":
 			WAVE/D/Z wv = AFH_GetAnalysisParamWave(name, params)
 			if(!WaveExists(wv))
@@ -1868,7 +1912,7 @@ End
 
 /// @brief Return a list of required parameters
 Function/S PSQ_SquarePulse_GetParams()
-	return "SamplingMultiplier:variable,[SamplingFrequency:variable]"
+	return "SamplingMultiplier:variable,[SamplingFrequency:variable],[BaselineRMSShortThreshold:variable],[BaselineRMSLongThreshold:variable]"
 End
 
 Function/S PSQ_SquarePulse_GetHelp(string name)
@@ -1879,6 +1923,10 @@ Function/S PSQ_SquarePulse_GetHelp(string name)
 			 break
 		 case "SamplingFrequency":
 			 return "Required sampling frequency for the acquired data [kHz]. Defaults to 50."
+		 case "BaselineRMSShortThreshold":
+			 return "Threshold value in mV for the short RMS baseline QC check (defaults to " + num2str(PSQ_RMS_SHORT_THRESHOLD) + ")"
+		 case "BaselineRMSLongThreshold":
+			 return "Threshold value in mV for the long RMS baseline QC check (defaults to " + num2str(PSQ_RMS_LONG_THRESHOLD) + ")"
 		default:
 			 ASSERT(0, "Unimplemented for parameter " + name)
 			 break
@@ -1890,6 +1938,13 @@ Function/S PSQ_SquarePulse_CheckParam(string name, string params)
 	variable val
 
 	strswitch(name)
+		case "BaselineRMSShortThreshold":
+		case "BaselineRMSLongThreshold":
+			val = AFH_GetAnalysisParamNumerical(name, params)
+			if(!(val > 0 && val <= 20))
+				return "Invalid value " + num2str(val)
+			endif
+			break
 		case "SamplingMultiplier":
 			val = AFH_GetAnalysisParamNumerical(name, params)
 			if(!IsValidSamplingMultiplier(val))
@@ -2121,7 +2176,7 @@ End
 
 /// @brief Return a list of required parameters
 Function/S PSQ_Rheobase_GetParams()
-	return "SamplingMultiplier:variable,[SamplingFrequency:variable]"
+	return "SamplingMultiplier:variable,[SamplingFrequency:variable],[BaselineRMSShortThreshold:variable],[BaselineRMSLongThreshold:variable]"
 End
 
 Function/S PSQ_Rheobase_GetHelp(string name)
@@ -2132,6 +2187,10 @@ Function/S PSQ_Rheobase_GetHelp(string name)
 			 break
 		 case "SamplingFrequency":
 			 return "Required sampling frequency for the acquired data [kHz]. Defaults to 50."
+		 case "BaselineRMSShortThreshold":
+			 return "Threshold value in mV for the short RMS baseline QC check (defaults to " + num2str(PSQ_RMS_SHORT_THRESHOLD) + ")"
+		 case "BaselineRMSLongThreshold":
+			 return "Threshold value in mV for the long RMS baseline QC check (defaults to " + num2str(PSQ_RMS_LONG_THRESHOLD) + ")"
 		default:
 			 ASSERT(0, "Unimplemented for parameter " + name)
 			 break
@@ -2143,6 +2202,13 @@ Function/S PSQ_Rheobase_CheckParam(string name, string params)
 	variable val
 
 	strswitch(name)
+		case "BaselineRMSShortThreshold":
+		case "BaselineRMSLongThreshold":
+			val = AFH_GetAnalysisParamNumerical(name, params)
+			if(!(val > 0 && val <= 20))
+				return "Invalid value " + num2str(val)
+			endif
+			break
 		case "SamplingMultiplier":
 			val = AFH_GetAnalysisParamNumerical(name, params)
 			if(!IsValidSamplingMultiplier(val))
@@ -2545,7 +2611,7 @@ End
 
 /// @brief Return a list of required parameters
 Function/S PSQ_Ramp_GetParams()
-	return "NumberOfSpikes:variable,SamplingMultiplier:variable,[SamplingFrequency:variable]"
+	return "NumberOfSpikes:variable,SamplingMultiplier:variable,[SamplingFrequency:variable],[BaselineRMSShortThreshold:variable],[BaselineRMSLongThreshold:variable]"
 End
 
 Function/S PSQ_Ramp_GetHelp(string name)
@@ -2556,6 +2622,10 @@ Function/S PSQ_Ramp_GetHelp(string name)
 		case "SamplingMultiplier":
 			 return "Use 1 for no multiplier"
 			 break
+		 case "BaselineRMSShortThreshold":
+			 return "Threshold value in mV for the short RMS baseline QC check (defaults to " + num2str(PSQ_RMS_SHORT_THRESHOLD) + ")"
+		 case "BaselineRMSLongThreshold":
+			 return "Threshold value in mV for the long RMS baseline QC check (defaults to " + num2str(PSQ_RMS_LONG_THRESHOLD) + ")"
 		 case "NumberOfSpikes":
 			return "Number of spikes required to be found after the pulse onset " \
 			 + "in order to label the cell as having \"spiked\"."
@@ -2570,6 +2640,13 @@ Function/S PSQ_Ramp_CheckParam(string name, string params)
 	variable val
 
 	strswitch(name)
+		case "BaselineRMSShortThreshold":
+		case "BaselineRMSLongThreshold":
+			val = AFH_GetAnalysisParamNumerical(name, params)
+			if(!(val > 0 && val <= 20))
+				return "Invalid value " + num2str(val)
+			endif
+			break
 		case "SamplingMultiplier":
 			val = AFH_GetAnalysisParamNumerical(name, params)
 			if(!IsValidSamplingMultiplier(val))
@@ -3301,6 +3378,10 @@ End
 Function/S PSQ_Chirp_GetHelp(string name)
 
 	strswitch(name)
+		 case "BaselineRMSShortThreshold":
+			 return "Threshold value in mV for the short RMS baseline QC check (defaults to " + num2str(PSQ_RMS_SHORT_THRESHOLD) + ")"
+		 case "BaselineRMSLongThreshold":
+			 return "Threshold value in mV for the long RMS baseline QC check (defaults to " + num2str(PSQ_RMS_LONG_THRESHOLD) + ")"
 		case "LowerRelativeBound":
 			return "Lower bound of a confidence band for the acquired data relative to the pre pulse baseline in mV."
 		case "UpperRelativeBound":
@@ -3333,6 +3414,13 @@ Function/S PSQ_Chirp_CheckParam(string name, string params)
 	string str
 
 	strswitch(name)
+		case "BaselineRMSShortThreshold":
+		case "BaselineRMSLongThreshold":
+			val = AFH_GetAnalysisParamNumerical(name, params)
+			if(!(val > 0 && val <= 20))
+				return "Invalid value " + num2str(val)
+			endif
+			break
 		case "LowerRelativeBound":
 			if(AFH_GetAnalysisParamNumerical("LowerRelativeBound", params) >= AFH_GetAnalysisParamNumerical("UpperRelativeBound", params))
 				return "LowerRelativeBound must be smaller than UpperRelativeBound"
@@ -3397,7 +3485,8 @@ Function/S PSQ_Chirp_GetParams()
 	return "LowerRelativeBound:variable,UpperRelativeBound:variable," +                             \
 		   "[NumberOfChirpCycles:variable],[SpikeCheck:variable],[FailedLevel:variable]," +         \
 		   "[DAScaleOperator:string],[DAScaleModifier:variable],[NumberOfFailedSweeps:variable]," + \
-		   "[SamplingMultiplier:variable],[SamplingFrequency:variable]"
+		   "[SamplingMultiplier:variable],[SamplingFrequency:variable]," +                          \
+		   "[BaselineRMSShortThreshold:variable],[BaselineRMSLongThreshold:variable]"
 End
 
 /// @brief Analysis function for determining the impedance of the cell using a sine chirp stim set

Packages/MIES/MIES_Constants.ipf
@@ -637,7 +637,7 @@ Constant ANALYSISBROWSER_PANEL_VERSION    =  1
 /// - Changed names of entries
 /// - Changed units or meaning of entries
 /// - New/Changed layers of entries
-Constant LABNOTEBOOK_VERSION = 51
+Constant LABNOTEBOOK_VERSION = 52
 
 /// Version of the stimset wave note
 Constant STIMSET_NOTE_VERSION = 7
@@ -987,7 +987,9 @@ StrConstant PSQ_FMT_LBN_SPIKE_COUNT        = "%s spike count"
 StrConstant PSQ_FMT_LBN_FINAL_SCALE        = "%s final DAScale"
 StrConstant PSQ_FMT_LBN_INITIAL_SCALE      = "%s initial DAScale"
 StrConstant PSQ_FMT_LBN_RMS_SHORT_PASS     = "%s Chk%d S-RMS QC"
+StrConstant PSQ_FMT_LBN_RMS_SHORT_THRESHOLD= "%s S-RMS Threshold"
 StrConstant PSQ_FMT_LBN_RMS_LONG_PASS      = "%s Chk%d L-RMS QC"
+StrConstant PSQ_FMT_LBN_RMS_LONG_THRESHOLD = "%s L-RMS Threshold"
 StrConstant PSQ_FMT_LBN_TARGETV            = "%s Chk%d T-V BL"
 StrConstant PSQ_FMT_LBN_TARGETV_PASS       = "%s Chk%d T-V BL QC"
 StrConstant PSQ_FMT_LBN_CHUNK_PASS         = "%s Chk%d BL QC"

Packages/Testing-MIES/UTF_PatchSeqDAScale.ipf
@@ -122,6 +122,17 @@ Function/WAVE GetLBNEntries_IGNORE(device, sweepNo, name, [chunk])
 		case PSQ_FMT_LBN_TARGETV:
 			return GetLastSettingEachSCI(numericalValues, sweepNo, key, PSQ_TEST_HEADSTAGE, UNKNOWN_MODE)
 			break
+		case PSQ_FMT_LBN_RMS_SHORT_THRESHOLD:
+		case PSQ_FMT_LBN_RMS_LONG_THRESHOLD:
+			WAVE/Z values = GetLastSettingSCI(numericalValues, sweepNo, key, PSQ_TEST_HEADSTAGE, UNKNOWN_MODE)
+			if(!WaveExists(values))
+				return $""
+			endif
+
+			Make/D/N=1/FREE val = {values[PSQ_TEST_HEADSTAGE]}
+
+			return val
+			break
 		case "Delta I":
 		case "Delta V":
 		case "ResistanceFromFit":
@@ -165,6 +176,12 @@ Function PS_DS_Sub1_REENTRY([str])
 
 	WAVE/Z baselineQCPassed = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_BL_QC_PASS)
 	CHECK_EQUAL_WAVES(sweepPassed, baselineQCPassed)
+
+	WAVE/Z baselineShortThreshold = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_RMS_SHORT_THRESHOLD)
+	CHECK_EQUAL_WAVES(baselineShortThreshold, {PSQ_RMS_SHORT_THRESHOLD * 1e-3}, mode = WAVE_DATA, tol = 1e-6)
+
+	WAVE/Z baselineLongThreshold = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_RMS_LONG_THRESHOLD)
+	CHECK_EQUAL_WAVES(baselineLongThreshold, {PSQ_RMS_LONG_THRESHOLD * 1e-3}, mode = WAVE_DATA, tol = 1e-6)
 
 	// we only test-override chunk passed, so for the others we can just check if they exist or not
 
@@ -281,6 +298,12 @@ Function PS_DS_Sub2_REENTRY([str])
 
 	WAVE/Z baselineQCPassed = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_BL_QC_PASS)
 	CHECK_EQUAL_WAVES(sweepPassed, baselineQCPassed)
+
+	WAVE/Z baselineShortThreshold = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_RMS_SHORT_THRESHOLD)
+	CHECK_EQUAL_WAVES(baselineShortThreshold, {PSQ_RMS_SHORT_THRESHOLD * 1e-3}, mode = WAVE_DATA, tol = 1e-6)
+
+	WAVE/Z baselineLongThreshold = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_RMS_LONG_THRESHOLD)
+	CHECK_EQUAL_WAVES(baselineLongThreshold, {PSQ_RMS_LONG_THRESHOLD * 1e-3}, mode = WAVE_DATA, tol = 1e-6)
 
 	// we only test-override chunk passed, so for the others we can just check if they exist or not
 
@@ -435,6 +458,12 @@ Function PS_DS_Sub3_REENTRY([str])
 	WAVE/Z baselineQCPassed = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_BL_QC_PASS)
 	CHECK_EQUAL_WAVES(sweepPassed, baselineQCPassed)
 
+	WAVE/Z baselineShortThreshold = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_RMS_SHORT_THRESHOLD)
+	CHECK_EQUAL_WAVES(baselineShortThreshold, {PSQ_RMS_SHORT_THRESHOLD * 1e-3}, mode = WAVE_DATA, tol = 1e-6)
+
+	WAVE/Z baselineLongThreshold = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_RMS_LONG_THRESHOLD)
+	CHECK_EQUAL_WAVES(baselineLongThreshold, {PSQ_RMS_LONG_THRESHOLD * 1e-3}, mode = WAVE_DATA, tol = 1e-6)
+
 	// we only test-override chunk passed, so for the others we can just check if they exist or not
 
 	// chunk 0
@@ -568,6 +597,12 @@ Function PS_DS_Sub4_REENTRY([str])
 
 	WAVE/Z baselineQCPassed = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_BL_QC_PASS)
 	CHECK_EQUAL_WAVES(sweepPassed, baselineQCPassed)
+
+	WAVE/Z baselineShortThreshold = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_RMS_SHORT_THRESHOLD)
+	CHECK_EQUAL_WAVES(baselineShortThreshold, {PSQ_RMS_SHORT_THRESHOLD * 1e-3}, mode = WAVE_DATA, tol = 1e-6)
+
+	WAVE/Z baselineLongThreshold = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_RMS_LONG_THRESHOLD)
+	CHECK_EQUAL_WAVES(baselineLongThreshold, {PSQ_RMS_LONG_THRESHOLD * 1e-3}, mode = WAVE_DATA, tol = 1e-6)
 
 	// we only test-override chunk passed, so for the others we can just check if they exist or not
 
@@ -734,6 +769,12 @@ Function PS_DS_Sub5_REENTRY([str])
 	WAVE/Z baselineQCPassed = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_BL_QC_PASS)
 	CHECK_EQUAL_WAVES(sweepPassed, baselineQCPassed)
 
+	WAVE/Z baselineShortThreshold = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_RMS_SHORT_THRESHOLD)
+	CHECK_EQUAL_WAVES(baselineShortThreshold, {PSQ_RMS_SHORT_THRESHOLD * 1e-3}, mode = WAVE_DATA, tol = 1e-6)
+
+	WAVE/Z baselineLongThreshold = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_RMS_LONG_THRESHOLD)
+	CHECK_EQUAL_WAVES(baselineLongThreshold, {PSQ_RMS_LONG_THRESHOLD * 1e-3}, mode = WAVE_DATA, tol = 1e-6)
+
 	// we only test-override chunk passed, so for the others we can just check if they exist or not
 
 	// chunk 0
@@ -852,6 +893,12 @@ Function PS_DS_Sub6_REENTRY([str])
 
 	WAVE/Z baselineQCPassed = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_BL_QC_PASS)
 	CHECK_EQUAL_WAVES(sweepPassed, baselineQCPassed)
+
+	WAVE/Z baselineShortThreshold = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_RMS_SHORT_THRESHOLD)
+	CHECK_EQUAL_WAVES(baselineShortThreshold, {PSQ_RMS_SHORT_THRESHOLD * 1e-3}, mode = WAVE_DATA, tol = 1e-6)
+
+	WAVE/Z baselineLongThreshold = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_RMS_LONG_THRESHOLD)
+	CHECK_EQUAL_WAVES(baselineLongThreshold, {PSQ_RMS_LONG_THRESHOLD * 1e-3}, mode = WAVE_DATA, tol = 1e-6)
 
 	// we only test-override chunk passed, so for the others we can just check if they exist or not
 
@@ -1020,6 +1067,12 @@ Function PS_DS_Sub7_REENTRY([str])
 	WAVE/Z baselineQCPassed = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_BL_QC_PASS)
 	CHECK_EQUAL_WAVES(sweepPassed, baselineQCPassed)
 
+	WAVE/Z baselineShortThreshold = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_RMS_SHORT_THRESHOLD)
+	CHECK_EQUAL_WAVES(baselineShortThreshold, {PSQ_RMS_SHORT_THRESHOLD * 1e-3}, mode = WAVE_DATA, tol = 1e-6)
+
+	WAVE/Z baselineLongThreshold = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_RMS_LONG_THRESHOLD)
+	CHECK_EQUAL_WAVES(baselineLongThreshold, {PSQ_RMS_LONG_THRESHOLD * 1e-3}, mode = WAVE_DATA, tol = 1e-6)
+
 	// we only test-override chunk passed, so for the others we can just check if they exist or not
 
 	// chunk 0
@@ -1162,6 +1215,12 @@ Function PS_DS_Sub8_REENTRY([str])
 	WAVE/Z baselineQCPassed = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_BL_QC_PASS)
 	CHECK_EQUAL_WAVES(sweepPassed, baselineQCPassed)
 
+	WAVE/Z baselineShortThreshold = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_RMS_SHORT_THRESHOLD)
+	CHECK_EQUAL_WAVES(baselineShortThreshold, {PSQ_RMS_SHORT_THRESHOLD * 1e-3}, mode = WAVE_DATA, tol = 1e-6)
+
+	WAVE/Z baselineLongThreshold = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_RMS_LONG_THRESHOLD)
+	CHECK_EQUAL_WAVES(baselineLongThreshold, {PSQ_RMS_LONG_THRESHOLD * 1e-3}, mode = WAVE_DATA, tol = 1e-6)
+
 	// we only test-override chunk passed, so for the others we can just check if they exist or not
 
 	// chunk 0
@@ -1256,6 +1315,138 @@ Function PS_DS_Sub8_REENTRY([str])
 	CHECK_EQUAL_WAVES(stimScale, stimScaleRef, mode = WAVE_DATA, tol = 1e-14)
 
 	CHECK_EQUAL_VAR(MIES_PSQ#PSQ_GetLastPassingDAScaleSub(str, PSQ_TEST_HEADSTAGE), 8)
+
+	CommonAnalysisFunctionChecks(str, sweepNo, setPassed)
+End
+
+Function PS_DS_Sub9_Ignore(device)
+	string device
+
+	AFH_AddAnalysisParameter("PSQ_DaScale_Sub_DA_0", "BaselineRMSShortThreshold", var = 0.150)
+	AFH_AddAnalysisParameter("PSQ_DaScale_Sub_DA_0", "BaselineRMSLongThreshold", var = 0.250)
+End
+
+// Same as PS_DS_Sub1 but with custom RMS short/long thresholds
+// UTF_TD_GENERATOR HardwareMain#DeviceNameGeneratorMD1
+Function PS_DS_Sub9([str])
+	string str
+
+	STRUCT DAQSettings s
+	InitDAQSettingsFromString(s, "MD1_RA1_I0_L0_BKG_1")
+	AcquireData(s, "PSQ_DaScale_Sub_DA_0", str, preAcquireFunc = PS_DS_Sub9_Ignore)
+
+	WAVE wv = PSQ_CreateOverrideResults(str, PSQ_TEST_HEADSTAGE, PSQ_DA_SCALE)
+	// all tests fail
+	wv = 0
+End
+
+Function PS_DS_Sub9_REENTRY([str])
+	string str
+
+	variable sweepNo, numEntries
+
+	sweepNo = 4
+
+	WAVE numericalValues = GetLBNumericalValues(str)
+
+	WAVE/Z setPassed = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_SET_PASS)
+	CHECK_EQUAL_WAVES(setPassed, {0}, mode = WAVE_DATA)
+
+	WAVE/Z sweepPassed = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_SWEEP_PASS)
+	CHECK_EQUAL_WAVES(sweepPassed, {0, 0, 0, 0, 0}, mode = WAVE_DATA)
+
+	WAVE/Z samplingPassed = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_SAMPLING_PASS)
+	CHECK_EQUAL_WAVES(samplingPassed, {1, 1, 1, 1, 1}, mode = WAVE_DATA)
+
+	// BEGIN baseline QC
+
+	WAVE/Z baselineQCPassed = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_BL_QC_PASS)
+	CHECK_EQUAL_WAVES(sweepPassed, baselineQCPassed)
+
+	WAVE/Z baselineShortThreshold = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_RMS_SHORT_THRESHOLD)
+	CHECK_EQUAL_WAVES(baselineShortThreshold, {0.150 * 1e-3}, mode = WAVE_DATA, tol = 1e-6)
+
+	WAVE/Z baselineLongThreshold = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_RMS_LONG_THRESHOLD)
+	CHECK_EQUAL_WAVES(baselineLongThreshold, {0.250 * 1e-3}, mode = WAVE_DATA, tol = 1e-6)
+
+	// we only test-override chunk passed, so for the others we can just check if they exist or not
+
+	// chunk 0
+	WAVE/Z baselineChunkPassed = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_CHUNK_PASS, chunk = 0)
+	CHECK_EQUAL_WAVES(baselineChunkPassed, {0, 0, 0, 0, 0}, mode = WAVE_DATA)
+
+	WAVE/Z baselineRMSShortPassed = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_RMS_SHORT_PASS, chunk = 0)
+	CHECK_WAVE(baselineRMSShortPassed, NUMERIC_WAVE)
+
+	WAVE/Z baselineRMSLongPassed = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_RMS_LONG_PASS, chunk = 0)
+	CHECK_WAVE(baselineRMSLongPassed, NUMERIC_WAVE)
+
+	WAVE/Z baselineTargetVPassed = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_TARGETV_PASS, chunk = 0)
+	CHECK_WAVE(baselineTargetVPassed, NUMERIC_WAVE)
+
+	WAVE/Z targetV = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_TARGETV, chunk = 0)
+	CHECK_WAVE(targetV, NUMERIC_WAVE)
+
+	// chunk 1 does not exist
+	WAVE/Z baselineChunkPassed = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_CHUNK_PASS, chunk = 1)
+	CHECK_WAVE(baselineChunkPassed, NULL_WAVE)
+
+	WAVE/Z baselineRMSShortPassed = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_RMS_SHORT_PASS, chunk = 1)
+	CHECK_WAVE(baselineRMSShortPassed, NULL_WAVE)
+
+	WAVE/Z baselineRMSLongPassed = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_RMS_LONG_PASS, chunk = 1)
+	CHECK_WAVE(baselineRMSLongPassed, NULL_WAVE)
+
+	WAVE/Z baselineTargetVPassed = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_TARGETV_PASS, chunk = 1)
+	CHECK_WAVE(baselineTargetVPassed, NULL_WAVE)
+
+	WAVE/Z targetV = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_TARGETV, chunk = 1)
+	CHECK_WAVE(targetV, NULL_WAVE)
+
+	// END baseline QC
+
+	WAVE/Z spikeDetection = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_SPIKE_DETECT)
+	CHECK_WAVE(spikeDetection, NULL_WAVE)
+
+	WAVE/Z spikeCount = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_SPIKE_COUNT)
+	CHECK_WAVE(spikeCount, NULL_WAVE)
+
+	WAVE/Z pulseDuration = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_PULSE_DUR)
+	CHECK_WAVE(pulseDuration, NULL_WAVE)
+
+	WAVE/Z fISlope = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_DA_fI_SLOPE)
+	CHECK_WAVE(fISlope, NULL_WAVE)
+
+	WAVE/Z fISlopeReached = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_DA_fI_SLOPE_REACHED)
+	CHECK_EQUAL_WAVES(fISlopeReached, {0, 0, 0, 0, 0}, mode = WAVE_DATA)
+
+	WAVE/T/Z opMode = GetLBNEntries_IGNORE(str, sweepNo, PSQ_FMT_LBN_DA_OPMODE)
+	CHECK_EQUAL_TEXTWAVES(opMode, {PSQ_DS_SUB, PSQ_DS_SUB, PSQ_DS_SUB, PSQ_DS_SUB, PSQ_DS_SUB}, mode = WAVE_DATA)
+
+	WAVE/Z deltaI = GetLBNEntries_IGNORE(str, sweepNo, "Delta I")
+	CHECK_WAVE(deltaI, NULL_WAVE)
+
+	WAVE/Z deltaV = GetLBNEntries_IGNORE(str, sweepNo, "Delta V")
+	CHECK_WAVE(deltaV, NULL_WAVE)
+
+	WAVE/Z resistance = GetLBNEntries_IGNORE(str, sweepNo, "ResistanceFromFit")
+	CHECK_WAVE(resistance, NULL_WAVE)
+
+	WAVE/Z resistanceErr = GetLBNEntries_IGNORE(str, sweepNo, "ResistanceFromFit_Err")
+	CHECK_WAVE(resistanceErr, NULL_WAVE)
+
+	WAVE/Z sweeps = AFH_GetSweepsFromSameRACycle(numericalValues, sweepNo)
+	CHECK_WAVE(sweeps, NUMERIC_WAVE)
+	numEntries = DimSize(sweeps, ROWS)
+	CHECK_EQUAL_VAR(numEntries, 5)
+
+	numEntries = DimSize(sweepPassed, ROWS)
+	Make/FREE/D/N=(numEntries) stimScale = GetLastSetting(numericalValues, sweeps[p], STIMSET_SCALE_FACTOR_KEY, DATA_ACQUISITION_MODE)[PSQ_TEST_HEADSTAGE]
+	Make/FREE/D/N=(numEntries) stimScaleRef = -30
+
+	CHECK_EQUAL_WAVES(stimScale, stimScaleRef, mode = WAVE_DATA, tol = 1e-14)
+
+	CHECK_EQUAL_VAR(MIES_PSQ#PSQ_GetLastPassingDAScaleSub(str, PSQ_TEST_HEADSTAGE), -1)
 
 	CommonAnalysisFunctionChecks(str, sweepNo, setPassed)
 End

commit 58d4c2bdd14b7083a60533188b39037b7d83491b

PSQ_SquarePulse: Remove unused analysis parameters
It does not use baseline evaluation so we don't need the RMS threshold
parameters. Bug introduced in 0b293128 (Patch Seq analysis functions: Make
the baseline RMS short/long thresholds configurable, 2021-11-08).

Packages/MIES/MIES_AnalysisFunctions_PatchSeq.ipf
@@ -2453,10 +2453,8 @@ End
 
 /// @brief Return a list of required parameters
 Function/S PSQ_SquarePulse_GetParams()
-	return "AsyncQCChannels:wave,"                 + \
-	       "[BaselineRMSLongThreshold:variable],"  + \
-	       "[BaselineRMSShortThreshold:variable]," + \
-	       "[SamplingFrequency:variable],"         + \
+	return "AsyncQCChannels:wave,"         + \
+	       "[SamplingFrequency:variable]," + \
 	       "SamplingMultiplier:variable"
 End
 
@@ -2464,8 +2462,6 @@ Function/S PSQ_SquarePulse_GetHelp(string name)
 
 	strswitch(name)
 		case "AsyncQCChannels":
-		case "BaselineRMSLongThreshold":
-		case "BaselineRMSShortThreshold":
 		case "SamplingFrequency":
 		case "SamplingMultiplier":
 			return PSQ_GetHelpCommon(PSQ_SQUARE_PULSE, name)
@@ -2480,8 +2476,6 @@ Function/S PSQ_SquarePulse_CheckParam(string name, struct CheckParametersStruct 
 
 	strswitch(name)
 		case "AsyncQCChannels":
-		case "BaselineRMSLongThreshold":
-		case "BaselineRMSShortThreshold":
 		case "SamplingFrequency":
 		case "SamplingMultiplier":
 			return PSQ_CheckParamCommon(name, s)

Packages/MIES/MIES_Constants.ipf
@@ -50,7 +50,7 @@ Constant PSQ_CHIRP_VERSION           = 13
 Constant PSQ_DA_SCALE_VERSION        = 5
 Constant PSQ_RAMP_VERSION            = 6
 Constant PSQ_RHEOBASE_VERSION        = 5
-Constant PSQ_SQUARE_PULSE_VERSION    = 3
+Constant PSQ_SQUARE_PULSE_VERSION    = 4
 Constant PSQ_SEAL_EVALUATION_VERSION = 3
 Constant PSQ_TRUE_REST_VM_VERSION    = 1
 Constant MSQ_FAST_RHEO_EST_VERSION   = 1