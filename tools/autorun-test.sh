788e8e3763011202b417307c085f1ed79e66e4f2
Thomas Braun <thomas.braun@byte-physics.de>
2022-10-31 18:46:51 +0100

    tools/autorun-test.sh: Revise it
    We now output proper error message of the given igor version string is
    empty or if the experiment could not be found.

--- tools/autorun-test.sh
+++ tools/autorun-test.sh
@@ -27,31 +27,29 @@ while getopts ":p:t:v:" o; do
 done
 shift $((OPTIND-1))
 
-if [ -z "${experiment}" ]
+if [ ! -f "${experiment}" ]
 then
+  echo "The given experiment ${experiment} is not an existing file." 1>&2
+  exit 1
+fi
+
+if [ -z "${igorProVersion}" ]
+then
+  echo "The Igor Pro Version is empty." 1>&2
   exit 1
 fi
 
 StateFile=$(dirname ${experiment})/DO_AUTORUN.txt
 touch $StateFile
 
-if [ -z "${igorProVersion}" ]
-then
-  exit 1
-fi
-
 igorProPath=$(${ScriptDir}/get-igor-path.sh ${igorProVersion})
 
 echo "Running experiment $experiment"
 
-case $MSYSTEM in
-  MINGW*)
-    # we don't want MSYS path conversion, as that would break the /X options,
-    # see https://github.com/git-for-windows/build-extra/blob/master/ReleaseNotes.md
-    MSYS_NO_PATHCONV=1 "${igorProPath}" /CompErrNoDialog /N /I "$experiment"
-    ret=$?
-    ;;
-esac
+# we don't want MSYS path conversion, as that would break the /X options,
+# see https://github.com/git-for-windows/build-extra/blob/master/ReleaseNotes.md
+MSYS_NO_PATHCONV=1 "${igorProPath}" /UNATTENDED /N /I "$experiment"
+ret=$?
 
 rm -f $StateFile
 
