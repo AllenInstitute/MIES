AllenInstitute/MIES

commit 70a3710b3eb57663fb59912849d0b9087620304c

tools/check-code.sh: Check that the list of function prefixes has no duplicates

tools/check-code.sh
@@ -170,4 +170,12 @@ then
   ret=1
 fi
 
+matches=$(rg --no-filename '.*@brief[[:space:]]+__([A-Z]{2,})__.*' --replace '$1' ${files} | sort | uniq -d)
+
+if [[ -n "$matches" ]]
+then
+  echo "The list of function prefixes is not unique"
+  echo "$matches"
+fi
+
 exit $ret

Packages/MIES/MIES_DataBrowser_Macro.ipf
@@ -7,7 +7,7 @@
 #endif
 
 /// @file MIES_DataBrowser_Macro.ipf
-/// @brief __DBM__ Macro for DataBrowser
+/// @brief __DB__ Macro for DataBrowser
 
 Window DataBrowser() : Graph
 	PauseUpdate; Silent 1		// building window...

commit ef1147a9c174d040791c2186b6c70a2837e527d7

tools/check-code.sh: Make failing test really fail
Broken since 70a3710b (tools/check-code.sh: Check that the list of function prefixes has no duplicates, 2022-11-22).

tools/check-code.sh
@@ -176,6 +176,7 @@ if [[ -n "$matches" ]]
 then
   echo "The list of function prefixes is not unique"
   echo "$matches"
+  ret=1
 fi
 
 exit $ret