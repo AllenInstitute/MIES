AllenInstitute/MIES

commit 61c5c21aa8d4d0673752aba818795012fc686248

MIES_DAEphys.ipf: Prefer USER_DATA_MENU_EXP over open-coding it
Forgotten in 54c1f4ce (Define a constant for the stimset user data, 2019-10-16).

Packages/MIES/MIES_DAEphys.ipf
@@ -924,7 +924,7 @@ Function DAP_SetVarProc_Channel_Search(sva) : SetVariableControl
 			popupValue = DAP_FormatStimSetPopupValue(channelType, searchString = searchString)
 
 			ctrl = GetPanelControl(channelIndex, channelType, CHANNEL_CONTROL_WAVE)
-			PopupMenu $ctrl win=$panelTitle, value=#popupValue, userdata(MenuExp)=listOfWaves
+			PopupMenu $ctrl win=$panelTitle, value=#popupValue, userdata(USER_DATA_MENU_EXP)=listOfWaves
 			sel = GetPopupMenuString(panelTitle, ctrl)
 			PopupMenu $ctrl win=$panelTitle, popmatch=sel
 
@@ -946,7 +946,7 @@ Function DAP_SetVarProc_Channel_Search(sva) : SetVariableControl
 					DAG_Update(panelTitle, ctrl, str = str)
 
 					ctrl = GetPanelControl(i, channelType, CHANNEL_CONTROL_WAVE)
-					PopupMenu $ctrl win=$panelTitle, value=#popupValue, userdata(MenuExp)=listOfWaves
+					PopupMenu $ctrl win=$panelTitle, value=#popupValue, userdata(USER_DATA_MENU_EXP)=listOfWaves
 					sel = GetPopupMenuString(panelTitle, ctrl)
 					PopupMenu $ctrl win=$panelTitle, popmatch=sel
 
@@ -5606,7 +5606,7 @@ Function DAP_CheckProc_PowerSpectrum(cba) : CheckBoxControl
 	return 0
 End
 
-/// @brief Update the popup menus and its `MenuExp` user data after stim set changes
+/// @brief Update the popup menus and its #USER_DATA_MENU_EXP user data after stim set changes
 ///
 /// @param panelTitle [optional, defaults to all locked devices] device
 Function DAP_UpdateDaEphysStimulusSetPopups([panelTitle])
@@ -5641,15 +5641,15 @@ Function DAP_UpdateDaEphysStimulusSetPopups([panelTitle])
 		for(j = CHANNEL_INDEX_ALL_I_CLAMP; j < NUM_DA_TTL_CHANNELS; j += 1)
 			ctrlWave     = GetPanelControl(j, CHANNEL_TYPE_DAC, CHANNEL_CONTROL_WAVE)
 			ctrlIndexEnd = GetPanelControl(j, CHANNEL_TYPE_DAC, CHANNEL_CONTROL_INDEX_END)
-			SetControlUserData(panelTitle, ctrlWave, "MenuExp", DAlist)
-			SetControlUserData(panelTitle, ctrlIndexEnd, "MenuExp", DAlist)
+			SetControlUserData(panelTitle, ctrlWave, USER_DATA_MENU_EXP, DAlist)
+			SetControlUserData(panelTitle, ctrlIndexEnd, USER_DATA_MENU_EXP, DAlist)
 		endfor
 
 		for(j = CHANNEL_INDEX_ALL; j < NUM_DA_TTL_CHANNELS; j += 1)
 			ctrlWave     = GetPanelControl(j, CHANNEL_TYPE_TTL, CHANNEL_CONTROL_WAVE)
 			ctrlIndexEnd = GetPanelControl(j, CHANNEL_TYPE_TTL, CHANNEL_CONTROL_INDEX_END)
-			SetControlUserData(panelTitle, ctrlWave, "MenuExp", TTLlist)
-			SetControlUserData(panelTitle, ctrlIndexEnd, "MenuExp", TTLlist)
+			SetControlUserData(panelTitle, ctrlWave, USER_DATA_MENU_EXP, TTLlist)
+			SetControlUserData(panelTitle, ctrlIndexEnd, USER_DATA_MENU_EXP, TTLlist)
 		endfor
 
 		DAP_UpdateDAQControls(panelTitle, REASON_STIMSET_CHANGE)

commit ea44102e51da0bb14ca31504b0323458fbc9523e

DAP_SetVarProc_Channel_Search: Use correct userdata name
This requires a literal, so we have to dollar the constant.

Bug introduced in 61c5c21a (MIES_DAEphys.ipf: Prefer USER_DATA_MENU_EXP
over open-coding it, 2021-09-12).

Packages/MIES/MIES_DAEphys.ipf
@@ -979,7 +979,7 @@ Function DAP_SetVarProc_Channel_Search(sva) : SetVariableControl
 			popupValue = DAP_FormatStimSetPopupValue(channelType, searchString = searchString)
 
 			ctrl = GetPanelControl(channelIndex, channelType, CHANNEL_CONTROL_WAVE)
-			PopupMenu $ctrl win=$device, value=#popupValue, userdata(USER_DATA_MENU_EXP)=listOfWaves
+			PopupMenu $ctrl win=$device, value=#popupValue, userdata($USER_DATA_MENU_EXP)=listOfWaves
 			sel = GetPopupMenuString(device, ctrl)
 			PopupMenu $ctrl win=$device, popmatch=sel
 
@@ -1001,7 +1001,7 @@ Function DAP_SetVarProc_Channel_Search(sva) : SetVariableControl
 					DAG_Update(device, ctrl, str = str)
 
 					ctrl = GetPanelControl(i, channelType, CHANNEL_CONTROL_WAVE)
-					PopupMenu $ctrl win=$device, value=#popupValue, userdata(USER_DATA_MENU_EXP)=listOfWaves
+					PopupMenu $ctrl win=$device, value=#popupValue, userdata($USER_DATA_MENU_EXP)=listOfWaves
 					sel = GetPopupMenuString(device, ctrl)
 					PopupMenu $ctrl win=$device, popmatch=sel
 