AllenInstitute/MIES

commit 59cc1c735b73461cd71610cdedaf25cab27dc4f7

GetDeviceInfo: Rework device info gathering
In 3fdbb60c (DAEphys: Add a wave with device information, 2019-09-06) we
added support for gathering information about devices into a permanent
wave. This was reworked in 151c3035 (Rework device info handling,
2021-03-17) to handle also unregistered devices.

In that commit we also adopted the strategy to write the device info waves
once on gathering the device lists via DAP_UpdateDeviceInfoWaves.

This worked until bb2f92b1 (CONF_RestoreDAEphys: Avoid scanning device
lists, 2022-12-13) where we skipped querying the list of devices when
using a JSON configuration file. And thus we also did not query the device
info.

We now switch to query the device info on demand in GetDeviceInfoWave().
As that works for all cases.

Packages/MIES/MIES_DAC-Hardware.ipf
@@ -474,20 +474,34 @@ Function/WAVE HW_GetDeviceInfoUnregistered(variable hardwareType, string device,
 	return devInfo
 End
 
-/// @brief Update the device info wave
-///
-/// Query the data via GetDeviceInfoWave().
-Function HW_WriteDeviceInfo(variable hardwareType, WAVE deviceInfo, WAVE/Z devInfoHW)
+/// @brief Fill the device info wave
+Function HW_WriteDeviceInfo(variable hardwareType, string device, WAVE deviceInfo)
 
-	deviceInfo[%HardwareType] = hardwareType
+	variable deviceID
 
 #ifdef EVIL_KITTEN_EATING_MODE
-	deviceInfo[%AD]   = 1024
-	deviceInfo[%DA]   = 1024
-	deviceInfo[%TTL]  = 1024
-	deviceInfo[%Rack] = NaN
-#else
-	ASSERT(WaveExists(devInfoHW), "Missing device info hardware wave")
+	deviceInfo[%HardwareType] = hardwareType
+	deviceInfo[%AD]           = 1024
+	deviceInfo[%DA]           = 1024
+	deviceInfo[%TTL]          = 1024
+	deviceInfo[%Rack]         = NaN
+
+	return NaN
+#endif // EVIL_KITTEN_EATING_MODE
+
+	deviceID = ROVar(GetDAQDeviceID(device))
+
+	if(HW_IsValidDeviceID(deviceID) && !HW_SelectDevice(hardwareType, deviceID, flags = HARDWARE_PREVENT_ERROR_MESSAGE))
+		WAVE/Z devInfoHW = HW_GetDeviceInfo(hardwareType, deviceID)
+	else
+		WAVE/Z devInfoHW = HW_GetDeviceInfoUnregistered(hardwareType, device, flags = HARDWARE_PREVENT_ERROR_MESSAGE)
+	endif
+
+	if(!WaveExists(devInfoHW))
+		return NaN
+	endif
+
+	deviceInfo[%HardwareType] = hardwareType
 
 	switch(hardwareType)
 		case HARDWARE_ITC_DAC:
@@ -504,7 +518,6 @@ Function HW_WriteDeviceInfo(variable hardwareType, WAVE deviceInfo, WAVE/Z devIn
 			deviceInfo[%Rack] = NaN
 			break
 	endswitch
-#endif // EVIL_KITTEN_EATING_MODE
 End
 
 /// @brief Start data acquisition

Packages/MIES/MIES_DAEphys.ipf
@@ -89,11 +89,6 @@ Function/S DAP_GetNIDeviceList()
 #endif
 	endfor
 
-	// we want to have device infos for all NI devices
-	// devList holds only the ones suitable for DAQ but
-	// skips the ones used for pressure
-	DAP_UpdateDeviceInfoWaves(allDevices, HARDWARE_NI_DAC)
-
 	if(!IsEmpty(devList))
 		globalNIDevList = devList
 	else
@@ -116,8 +111,6 @@ Function/S DAP_GetITCDeviceList()
 	endif
 
 	devList = HW_ITC_ListDevices()
-
-	DAP_UpdateDeviceInfoWaves(devList, HARDWARE_ITC_DAC)
 
 	if(!IsEmpty(devList))
 		globalITCDevList = devList
@@ -5696,26 +5689,6 @@ Function ButtonProc_Hardware_rescan(ba) : ButtonControl
 	endswitch
 
 	return 0
-End
-
-/// @brief Update the device info waves for all passed devices
-///
-/// Usually only called once during startup
-///
-/// @param deviceList   list of devices usable for DAQ and pressure
-/// @param hardwareType One of @ref HardwareDACTypeConstants
-Function DAP_UpdateDeviceInfoWaves(string deviceList, variable hardwareType)
-	string device
-	variable numEntries, i
-
-	numEntries = ItemsInList(deviceList)
-	for(i = 0; i < numEntries; i += 1)
-		device = StringFromList(i, deviceList)
-		WAVE deviceInfo = GetDeviceInfoWave(device)
-		WAVE/Z devInfoHW = HW_GetDeviceInfoUnregistered(hardwareType, device)
-		hardwareType = GetHardwareType(device)
-		HW_WriteDeviceInfo(hardwareType, deviceInfo, devInfoHW)
-	endfor
 End
 
 Function DAP_CheckProc_PowerSpectrum(cba) : CheckBoxControl

Packages/MIES/MIES_WaveDataFolderGetters.ipf
@@ -6800,6 +6800,7 @@ Function/WAVE GetDeviceInfoWave(device)
 	string device
 
 	variable versionOfNewWave = 1
+	variable hardwareType
 
 	DFREF dfr = GetDeviceInfoPath()
 	WAVE/D/Z/SDFR=dfr wv = $device
@@ -6821,6 +6822,9 @@ Function/WAVE GetDeviceInfoWave(device)
 	wv = NaN
 
 	SetWaveVersion(wv, versionOfNewWave)
+
+	hardwareType = GetHardwareType(device)
+	HW_WriteDeviceInfo(hardwareType, device, wv)
 
 	return wv
 End

Packages/tests/HardwareBasic/UTF_VeryBasicHardwareTests.ipf
@@ -21,6 +21,9 @@ End
 static Function TestLocking([str])
 	string str
 
+	// check that we can gather the device config wave
+	KillOrMoveToTrash(dfr = GetDeviceInfoPath())
+
 	try
 		CreateLockedDAEphys(str)
 		PASS()
@@ -89,4 +92,30 @@ static Function CheckDeviceLists()
 	FAIL()
 #endif
 
+End
+
+// UTF_TD_GENERATOR DeviceNameGeneratorMD1
+static Function CheckGetDeviceInfoValid([string str])
+
+	WAVE/Z wv = GetDeviceInfoWave(str)
+	CHECK_WAVE(wv, NORMAL_WAVE | NUMERIC_WAVE)
+	CHECK_GT_VAR(wv[%AD], 0)
+	CHECK_GT_VAR(wv[%AD], 0)
+	CHECK_GT_VAR(wv[%TTL], 0)
+
+#ifdef TESTS_WITH_NI_HARDWARE
+	CHECK_EQUAL_VAR(wv[%Rack], NaN)
+#else
+	CHECK_GE_VAR(wv[%Rack], 0)
+#endif
+
+	CHECK_EQUAL_VAR(wv[%HardwareType], GetHardwareType(str))
+End
+
+// UTF_TD_GENERATOR NonExistingDevices
+static Function CheckGetDeviceInfoWithInvalid([string str])
+
+	WAVE/Z wv = GetDeviceInfoWave(str)
+	CHECK_WAVE(wv, NORMAL_WAVE | NUMERIC_WAVE)
+	CHECK(!HasOneValidEntry(wv))
 End

Packages/tests/UTF_DataGenerators.ipf
@@ -209,3 +209,12 @@ Function/WAVE GetChannelTypes()
 
 	return channelTypes
 End
+
+Function/WAVE NonExistingDevices()
+
+	Make/FREE/T wv = {"Dev0815", "ITC16_DEV_15"}
+
+	SetDimensionLabels(wv, TextWaveToList(wv, ";"), ROWS)
+
+	return wv
+End

commit 3e1541e1de22d6d4ba5eac09f06eb2d95510bd1a

HW_WriteDeviceInfo: Handle non-present Hardware XOPs
When the asked-for XOP is not present, we can just return early.

This also prevents a test failure on the ITC CI machine which does not
have the NIDAQmx XOP.

Bug introduced with 59cc1c73 (GetDeviceInfo: Rework device info gathering,
2023-01-11) via the CheckGetDeviceInfoWithInvalid test.

Packages/MIES/MIES_DAC-Hardware.ipf
@@ -498,6 +498,18 @@ Function HW_WriteDeviceInfo(variable hardwareType, string device, WAVE deviceInf
 	return NaN
 #endif // EVIL_KITTEN_EATING_MODE
 
+#ifndef ITC_XOP_PRESENT
+	if(hardwareType == HARDWARE_ITC_DAC)
+		return NaN
+	endif
+#endif
+
+#ifndef NIDAQMX_XOP_PRESENT
+	if(hardwareType == HARDWARE_NI_DAC)
+		return NaN
+	endif
+#endif
+
 	deviceID = ROVar(GetDAQDeviceID(device))
 
 	if(HW_IsValidDeviceID(deviceID) && !HW_SelectDevice(hardwareType, deviceID, flags = HARDWARE_PREVENT_ERROR_MESSAGE))