AllenInstitute/MIES

commit b8c8b2dcc536829424c51d3844b041511867e7e1

WaveToJSON/JSONToWave: Add them with tests

Packages/MIES/MIES_Utilities.ipf
@@ -6165,3 +6165,87 @@ Function ChangeFreeWaveName(WAVE wv, string name)
 
 	MoveWave wv, dfr:$name
 End
+
+/// @brief Serialize a wave as JSON and return it as string
+///
+/// The format is documented [here](https://github.com/AllenInstitute/ZeroMQ-XOP/#wave-serialization-format).
+Function/S WaveToJSON(WAVE/Z wv)
+
+	return zeromq_test_serializeWave(wv)
+End
+
+/// @brief Deserialize a JSON document generated by WaveToJSON()
+///
+/// Supports only a currently used subset.
+///
+/// @sa WaveToJSON
+Function/WAVE JSONToWave(string str)
+
+	variable jsonID, dim, i, j, k
+	string unit, type, dataUnit, waveNote
+
+	jsonID = JSON_Parse(str, ignoreErr = 1)
+
+	if(IsNaN(jsonID))
+		return $""
+	endif
+
+	type = JSON_GetString(jsonID, "/type", ignoreErr = 1)
+
+	// we only support FP64 for now
+	ASSERT(!cmpstr(type, "NT_FP64"), "Type is not supported")
+	WAVE/D/Z data = JSON_GetWave(jsonID, "/data/raw")
+	ASSERT(WaveExists(data), "Missing data")
+
+	WAVE/D/Z dimSizes = JSON_GetWave(jsonID, "/dimension/size", ignoreErr = 1)
+	ASSERT(WaveExists(dimSizes), "dimension sizes are missing")
+
+	Make/D/FREE/N=(MAX_DIMENSION_COUNT) newSizes = -1
+	newSizes[0, DimSize(dimSizes, ROWS) - 1] = dimSizes[p]
+	Redimension/N=(newSizes[0], newSizes[1], newSizes[2], newSizes[3]) data
+
+	WAVE/D/Z dimDeltas = JSON_GetWave(jsonID, "/dimension/delta", ignoreErr = 1)
+	ASSERT(!WaveExists(dimDeltas), "dimension deltas are not supported")
+
+	WAVE/D/Z dimOffsets = JSON_GetWave(jsonID, "/dimension/offset", ignoreErr = 1)
+	ASSERT(!WaveExists(dimOffsets), "dimension offsets are not supported")
+
+	WAVE/T/Z dimLabelsFull = JSON_GetTextWave(jsonID, "/dimension/label/full", ignoreErr = 1)
+
+	if(WaveExists(dimLabelsFull))
+		for(lbl : dimLabelsFull)
+			SetDimLabel dim, -1, $lbl, data
+			dim++
+		endfor
+	endif
+
+	WAVE/T/Z dimLabelsEach = JSON_GetTextWave(jsonID, "/dimension/label/each", ignoreErr = 1)
+
+	Redimension/N=(newSizes[0], newSizes[1], newSizes[2], newSizes[3]) dimLabelsEach
+
+	for(i = 0; i < MAX_DIMENSION_COUNT; i += 1)
+		for(j = 0; j < newSizes[i]; j += 1)
+			SetDimLabel i, j, $dimLabelsEach[k++], data
+		endfor
+	endfor
+
+	WAVE/T/Z dimUnits = JSON_GetWave(jsonID, "/dimension/unit", ignoreErr = 1)
+	ASSERT(!WaveExists(dimUnits), "dimension units are not supported")
+
+	// no way to restore the modification date
+
+	WAVE/D/Z dataFullScale = JSON_GetWave(jsonID, "/data/fullScale", ignoreErr = 1)
+
+	if(!WaveExists(dataFullScale))
+		Make/FREE/D dataFullScale = {0, 0}
+	endif
+
+	dataUnit = JSON_GetString(jsonID, "/data/unit", ignoreErr = 1)
+
+	SetScale d, dataFullScale[0], dataFullScale[1], dataUnit, data
+
+	waveNote = JSON_GetString(jsonID, "/note", ignoreErr = 1)
+	Note/K data, waveNote
+
+	return data
+End

Packages/tests/Basic/UTF_Utils.ipf
@@ -6463,3 +6463,31 @@ static Function TestDeepCopyWaveRefWave()
 End
 
 /// @}
+
+Function JSONWaveSerializationWorks()
+
+	string str
+
+	Make/FREE/D wv = {{1, 2, 3}, {4, 5, 6}}
+	CHECK_EQUAL_VAR(DimSize(wv, ROWS), 3)
+	CHECK_EQUAL_VAR(DimSize(wv, COLS), 2)
+
+	Note wv, "abcd"
+
+	SetScale d, 7, 8, "efgh", wv
+
+	SetDimLabel ROWS, -1, $"ijkl", wv
+	SetDimLabel COLS, -1, $"mnop", wv
+
+	SetDimLabel ROWS, 0, $"qrst", wv
+	SetDimLabel COLS, 1, $"uvwx", wv
+	SetDimLabel ROWS, 2, $"yz", wv
+
+	str = WaveToJSON(wv)
+	CHECK_PROPER_STR(str)
+
+	WAVE/Z serialized = JSONToWave(str)
+	CHECK_WAVE(serialized, NUMERIC_WAVE | FREE_WAVE, minorType = DOUBLE_WAVE)
+
+	CHECK_EQUAL_WAVES(wv, serialized)
+End

commit 22878a538fc6ff4d497de2f2c588a08c2cc4f0bc

JSONToWave: Fix it without "each dimension labels"
Bug introduced in b8c8b2dc (WaveToJSON/JSONToWave: Add them with tests,
2023-01-02).

Packages/MIES/MIES_Utilities.ipf
@@ -6229,11 +6229,15 @@ Function/WAVE JSONToWave(string str, [string path])
 
 	WAVE/T/Z dimLabelsEach = JSON_GetTextWave(jsonID, path + "/dimension/label/each", ignoreErr = 1)
 
-	for(i = 0; i < MAX_DIMENSION_COUNT; i += 1)
-		for(j = 0; j < newSizes[i]; j += 1)
-			SetDimLabel i, j, $dimLabelsEach[k++], data
+	if(WaveExists(dimLabelsEach))
+		ASSERT(DimSize(dimLabelsEach, ROWS) == Sum(dimSizes), "Mismatched dimension label each wave")
+
+		for(i = 0; i < MAX_DIMENSION_COUNT; i += 1)
+			for(j = 0; j < newSizes[i]; j += 1)
+				SetDimLabel i, j, $dimLabelsEach[k++], data
+			endfor
 		endfor
-	endfor
+	endif
 
 	WAVE/T/Z dimUnits = JSON_GetWave(jsonID, path + "/dimension/unit", ignoreErr = 1)
 	ASSERT(!WaveExists(dimUnits), "dimension units are not supported")

Packages/tests/Basic/UTF_Utils.ipf
@@ -6515,6 +6515,21 @@ Function JSONWaveSerializationWorksOnlyOneRow()
 	CHECK_EQUAL_WAVES(wv, serialized)
 End
 
+Function JSONWaveSerializationWorksNoDimLabels()
+
+	string str
+
+	Make/D/N=1 wv
+
+	str = WaveToJSON(wv)
+	CHECK_PROPER_STR(str)
+
+	WAVE/Z serialized = JSONToWave(str)
+	CHECK_WAVE(serialized, NUMERIC_WAVE | FREE_WAVE, minorType = DOUBLE_WAVE)
+
+	CHECK_EQUAL_WAVES(wv, serialized)
+End
+
 Function JSONWaveSerializationWorksWithPath()
 
 	string str, path