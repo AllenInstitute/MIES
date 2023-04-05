AllenInstitute/MIES

commit 09bc1e9d32258230de6365fc8eea5ba35e3245d2

SF: Add different markers or line styles if formulas are different in same plot
- The marker codes and line codes are defined in a helper function.

Packages/MIES/MIES_SweepFormula.ipf
@@ -1467,7 +1467,7 @@ static Function SF_FormulaPlotter(string graph, string formula, [DFREF dfr, vari
 	variable i, j, k, l, numTraces, splitTraces, splitY, splitX, numGraphs, numWins, numData, dataCnt, traceCnt
 	variable dim1Y, dim2Y, dim1X, dim2X, winDisplayMode, showLegend
 	variable xMxN, yMxN, xPoints, yPoints, keepUserSelection, numAnnotations, formulasAreDifferent
-	variable formulaCounter, gdIndex
+	variable formulaCounter, gdIndex, markerCode, lineCode
 	string win, wList, winNameTemplate, exWList, wName, annotation, yAxisLabel, wvName
 	string yFormula, yFormulasRemain
 	STRUCT SF_PlotMetaData plotMetaData
@@ -1691,27 +1691,6 @@ static Function SF_FormulaPlotter(string graph, string formula, [DFREF dfr, vari
 					endfor
 				endif
 
-				if(DimSize(wvY, ROWS) < SF_MAX_NUMPOINTS_FOR_MARKERS \
-					&& (!WaveExists(wvX) \
-					|| DimSize(wvx, ROWS) <  SF_MAX_NUMPOINTS_FOR_MARKERS))
-					ModifyGraph/W=$win mode=3,marker=19
-
-					WAVE/Z customMarkerAsFree = JWN_GetNumericWaveFromWaveNote(wvY, SF_META_MOD_MARKER)
-
-					if(WaveExists(customMarkerAsFree))
-						DFREF dfrWork = SFH_GetWorkingDF(graph)
-						for(i = 0; i < numTraces; i += 1)
-
-							wvName = UniqueWaveName(dfr, "customMarker_" + NameOfWave(wvY))
-							MoveWave customMarkerAsFree, dfrWork:$wvName
-							WAVE/SDFR=dfrWork customMarker = $wvName
-							ASSERT(DimSize(wvY, ROWS) == DimSize(customMarker, ROWS), "Marker size mismatch")
-
-							ModifyGraph/W=$win zmrkNum($traces[i])={customMarker}
-						endfor
-					endif
-				endif
-
 				showLegend = showLegend && SF_GetShowLegend(wvY)
 
 				dataCnt += 1
@@ -1752,16 +1731,35 @@ static Function SF_FormulaPlotter(string graph, string formula, [DFREF dfr, vari
 			WAVE/T tracesInGraph = plotFormData[0]
 			WAVE/WAVE dataInGraph = plotFormData[1]
 			numTraces = DimSize(tracesInGraph, ROWS)
+			markerCode = formulasAreDifferent ? k : 0
+			markerCode = SFH_GetPlotMarkerCodeSelection(markerCode)
+			lineCode = formulasAreDifferent ? k : 0
+			lineCode = SFH_GetPlotLineCodeSelection(lineCode)
 			for(l = 0; l < numTraces; l += 1)
 
 				WAVE/Z wvX = dataInGraph[l][%WAVEX]
 				WAVE wvY = dataInGraph[l][%WAVEY]
+				trace = tracesInGraph[l]
 
 				if(DimSize(wvY, ROWS) < SF_MAX_NUMPOINTS_FOR_MARKERS \
 					&& (!WaveExists(wvX) \
 					|| DimSize(wvx, ROWS) <  SF_MAX_NUMPOINTS_FOR_MARKERS))
-					trace = tracesInGraph[l]
-					ModifyGraph/W=$win mode($trace)=3,marker($trace)=19
+
+					WAVE/Z customMarkerAsFree = JWN_GetNumericWaveFromWaveNote(wvY, SF_META_MOD_MARKER)
+					if(!WaveExists(customMarkerAsFree))
+						ModifyGraph/W=$win mode($trace)=3,marker($trace)=markerCode
+						continue
+					endif
+
+					DFREF dfrWork = SFH_GetWorkingDF(graph)
+					wvName = UniqueWaveName(dfr, "customMarker_" + NameOfWave(wvY))
+					MoveWave customMarkerAsFree, dfrWork:$wvName
+					WAVE/SDFR=dfrWork customMarker = $wvName
+					ASSERT(DimSize(wvY, ROWS) == DimSize(customMarker, ROWS), "Marker size mismatch")
+					ModifyGraph/W=$win zmrkNum($trace)={customMarker}
+
+				elseif(formulasAreDifferent)
+					ModifyGraph/W=$win lStyle($trace)=lineCode
 				endif
 			endfor
 		endfor

Packages/MIES/MIES_SweepFormula_Helpers.ipf
@@ -1164,3 +1164,17 @@ static Function SFH_EnrichAnnotationsRelease(WAVE formulaIDs)
 		JSON_Release(ids)
 	endfor
 End
+
+Function SFH_GetPlotMarkerCodeSelection(variable count)
+
+	Make/FREE wv = {19, 5, 16, 8, 17, 7, 18, 6}
+
+	return wv[mod(count, DimSize(wv, ROWS))]
+End
+
+Function SFH_GetPlotLineCodeSelection(variable count)
+
+	Make/FREE wv = {0, 3, 7, 2, 1, 8}
+
+	return wv[mod(count, DimSize(wv, ROWS))]
+End

commit 937ecb75c979551228860b4943e9699b05605c47

SF_FormulaPlotter: Use marker style for custom markers as well
Broken since 09bc1e9d (SF: Add different markers or line styles if
formulas are different in same plot, 2023-02-03).

Packages/MIES/MIES_SweepFormula.ipf
@@ -1771,7 +1771,7 @@ static Function SF_FormulaPlotter(string graph, string formula, [DFREF dfr, vari
 					MoveWave customMarkerAsFree, dfrWork:$wvName
 					WAVE/SDFR=dfrWork customMarker = $wvName
 					ASSERT(DimSize(wvY, ROWS) == DimSize(customMarker, ROWS), "Marker size mismatch")
-					ModifyGraph/W=$win zmrkNum($trace)={customMarker}
+					ModifyGraph/W=$win mode($trace)=3,zmrkNum($trace)={customMarker}
 
 				elseif(formulasAreDifferent)
 					ModifyGraph/W=$win lStyle($trace)=lineCode