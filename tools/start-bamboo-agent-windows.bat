03c6670ae50d92fc5233f4a9b446f008b339bd95
Max Brauer <max.brauer@byte-physics.de>
2023-05-12 18:59:47 +0200

    CI: Remove deprecated bamboo tools
    These tools are no longer needed after the migration to Github Actions.
    
    See: #1563

--- tools/start-bamboo-agent-windows.bat
+++
@@ -1,3 +0,0 @@
-rem Comment in the matching one depending on if you installed git as user or admin
-rem "C:\Users\svc_multipatch\AppData\Local\Programs\Git\bin\bash.exe"  -c ~/start-bamboo-agent.sh
-"C:\Program Files\Git\bin\bash.exe" -c ~/start-bamboo-agent.sh
--- tools/start-bamboo-agent-windows.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-~/bamboo-agent-home/bin/wrapper.exe -c ~/bamboo-agent-home/conf/wrapper.conf
