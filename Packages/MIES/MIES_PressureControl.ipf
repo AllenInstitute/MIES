AllenInstitute/MIES

commit d329baaf1d6c1f157b606e0ee0a52919e5e73450

Add entries to DA_Ephys to record User Pressure during TP
Storing the user pressure values in the TPStorage wave allows to later
understand what the user does much better.

Original-CommitID: e7a43f3c87867b46a0a546dc47f06be7b1e9a565

Packages/MIES/MIES_PressureControl.ipf
@@ -107,6 +107,40 @@ Function P_PressureControl(panelTitle)
 
 		P_UpdateTPStorage(panelTitle, headStage)
 	endfor
+
+	P_RecordUserPressure(panelTitle)
+End
+
+static Function P_RecordUserPressure(panelTitle)
+	string panelTitle
+
+	string device
+	variable ADC, i, deviceID, hwType
+
+	WAVE TPStorage = GetTPStorage(panelTitle)
+	variable count = GetNumberFromWaveNote(TPStorage, NOTE_INDEX)
+
+	WAVE pressureType = GetPressureTypeWv(panelTitle)
+	WAVE pressureDataWv = P_GetPressureDataWaveRef(panelTitle)
+
+	TPStorage[count][][%UserPressureType] = pressureType[q]
+
+	for(i = 0; i < NUM_HEADSTAGES; i += 1)
+		if(pressureType[i] != PRESSURE_TYPE_USER)
+			continue
+		endif
+
+		hwType   = pressureDataWv[i][%UserPressureDeviceHWType]
+		deviceID = pressureDataWv[i][%UserPressureDeviceID]
+		ADC      = pressureDataWv[i][%UserPressureDeviceADC]
+
+		if(IsNaN(hwType) || IsNaN(deviceID) || IsNaN(ADC))
+			continue
+		endif
+
+		TPStorage[count][i][%UserPressure]             = HW_ReadADC(hwType, deviceID, ADC)
+		TPStorage[count][i][%UserPressureTimeStampUTC] = DateTimeInUTC()
+	endfor
 End
 
 /// @brief Record pressure in TPStorage wave
@@ -969,6 +1003,28 @@ static Function P_ITC_SetChannels(panelTitle, headstage)
 
 	ITCConfig[%DA][%Chan_num]  = pressureDataWv[headStage][%DAC]
 	ITCConfig[%AD][%Chan_num]  = pressureDataWv[headStage][%ADC]
+End
+
+/// @brief Check wether the given device is used as pressure device already
+Function P_DeviceIsUsedForPressureCtrl(panelTitle, pressureDevice)
+	string panelTitle, pressureDevice
+
+	variable i, hwType, deviceID
+
+	WAVE pressureDataWv = P_GetPressureDataWaveRef(panelTitle)
+
+	for(i = 0; i < NUM_HEADSTAGES; i += 1)
+		deviceID = pressureDataWv[i][%DAC_DevID]
+		hwType   = pressureDataWv[i][%HW_DAC_Type]
+
+		if(isFinite(deviceID) && isFinite(hwType))
+			if(!cmpstr(HW_GetDeviceName(hwType, deviceID), pressureDevice))
+				return 1
+			endif
+		endif
+	endfor
+
+	return 0
 End
 
 /// @brief Perform an acquisition cycle on the pressure device for pressure control
@@ -2175,6 +2231,7 @@ Function ButtonProc_Hrdwr_P_UpdtDAClist(ba) : ButtonControl
 			endfor
 
 			SetPopupMenuVal(ba.win, "popup_Settings_Pressure_dev", filteredList)
+			SetPopupMenuVal(ba.win, "popup_Settings_UserPressure", filteredList)
 			break
 	endswitch
 
@@ -2262,6 +2319,65 @@ Function P_Check_SealAtm(cba) : CheckBoxControl
 			DAP_AbortIfUnlocked(cba.win)
 			DAG_Update(cba.win, cba.ctrlName, val = cba.checked)
 			P_UpdatePressureDataStorageWv(cba.win)
+			break
+	endswitch
+
+	return 0
+End
+
+Function P_ButtonProc_UserPressure(ba) : ButtonControl
+	STRUCT WMButtonAction &ba
+
+	string userPressureDevice, panelTitle
+	variable hardwareType, deviceID, ADC, flags
+
+	switch(ba.eventCode)
+		case 2: // mouse up
+			panelTitle = ba.win
+
+			DAP_AbortIfUnlocked(panelTitle)
+
+			WAVE pressureDataWv = P_GetPressureDataWaveRef(panelTitle)
+			userPressureDevice = DAG_GetTextualValue(panelTitle, "popup_Settings_UserPressure")
+
+			if(!cmpstr(ba.ctrlName, "button_Hardware_PUser_Enable"))
+
+				if(!cmpstr(userPressureDevice, NONE))
+					break
+				endif
+
+				ADC = str2num(DAG_GetTextualValue(panelTitle, "Popup_Settings_UserPressure_ADC"))
+
+				deviceID = HW_OpenDevice(userPressureDevice, hardwareType)
+				HW_RegisterDevice(panelTitle, hardwareType, deviceID, pressureDevice=userPressureDevice)
+
+				pressureDataWv[][%UserPressureDeviceID]     = deviceID
+				pressureDataWv[][%UserPressureDeviceHWType] = hardwareType
+				pressureDataWv[][%UserPressureDeviceADC]    = ADC
+
+				DisableControls(panelTitle, "popup_Settings_UserPressure;Popup_Settings_UserPressure_ADC;button_Hardware_PUser_Enable")
+				EnableControls(panelTitle, "button_Hardware_PUser_Disable")
+
+			elseif(!cmpstr(ba.ctrlName, "button_Hardware_PUser_Disable"))
+
+				// the same device can be used for pressure control and user pressure control
+				if(!P_DeviceIsUsedForPressureCtrl(panelTitle, userPressureDevice))
+					// the device is the same for all headstages
+					deviceID = pressureDataWv[0][%UserPressureDeviceID]
+					hardwareType = pressureDataWv[0][%UserPressureDeviceHWType]
+					HW_CloseDevice(deviceID, hardwareType, flags = HARDWARE_PREVENT_ERROR_POPUP | HARDWARE_PREVENT_ERROR_MESSAGE)
+					HW_DeRegisterDevice(deviceID, hardwareType)
+				endif
+
+				pressureDataWv[][%UserPressureDeviceID]     = NaN
+				pressureDataWv[][%UserPressureDeviceHWType] = NaN
+				pressureDataWv[][%UserPressureDeviceADC]    = NaN
+
+				DisableControls(panelTitle, "button_Hardware_PUser_Disable")
+				EnableControls(panelTitle, "popup_Settings_UserPressure;Popup_Settings_UserPressure_ADC;button_Hardware_PUser_Enable")
+			else
+				ASSERT(0, "Invalid ctrl")
+			endif
 			break
 	endswitch
 

Packages/MIES/MIES_Constants.ipf
@@ -535,7 +535,7 @@ Constant HARDWARE_DAC_EXTERNAL_TRIGGER = 0x1
 /// @}
 
 /// Used to upgrade the GuiStateWave as well as the DA Ephys panel
-Constant DA_EPHYS_PANEL_VERSION     = 37
+Constant DA_EPHYS_PANEL_VERSION     = 38
 Constant DATABROWSER_PANEL_VERSION  = 8
 Constant SWEEPBROWSER_PANEL_VERSION = 3
 Constant WAVEBUILDER_PANEL_VERSION  = 7

Packages/MIES/MIES_DAEphys.ipf
@@ -583,6 +583,14 @@ Function DAP_EphysPanelStartUpSettings()
 	SetVariable setvar_DataAcq_PPPressure, win=$panelTitle, value= _NUM:0
 	SetVariable setvar_DataAcq_SSPressure, win=$panelTitle, value= _NUM:0
 
+	// user pressure
+	PopupMenu popup_Settings_UserPressure WIN = $panelTitle, mode=1
+	EnableControl(panelTitle, "popup_Settings_UserPressure")
+	PopupMenu Popup_Settings_UserPressure_ADC  WIN = $panelTitle, mode=1
+	EnableControl(panelTitle, "Popup_Settings_UserPressure_ADC")
+	EnableControl(panelTitle, "button_Hardware_PUser_Enable")
+	DisableControl(panelTitle, "button_Hardware_PUser_Disable")
+
    ValDisplay valdisp_DataAcq_P_LED_0 WIN = $panelTitle, value= _NUM:-1
    ValDisplay valdisp_DataAcq_P_LED_1 WIN = $panelTitle, value= _NUM:-1
    ValDisplay valdisp_DataAcq_P_LED_2 WIN = $panelTitle, value= _NUM:-1
@@ -4490,6 +4498,7 @@ static Function DAP_UnlockDevice(panelTitle)
 	DAP_SerializeCommentNotebook(panelTitle)
 	DAP_LockCommentNotebook(panelTitle)
 	PGC_SetAndActivateControl(panelTitle, "button_Hardware_P_Disable")
+	PGC_SetAndActivateControl(panelTitle, "button_Hardware_PUser_Disable")
 
 	if(DeviceHasFollower(panelTitle))
 		DAP_RemoveALLYokedDACs(panelTitle)

Packages/MIES/MIES_DAEphys_Macro.ipf
@@ -11,7 +11,7 @@
 
 Window DA_Ephys() : Panel
 	PauseUpdate; Silent 1		// building window...
-	NewPanel /K=1 /W=(128,484,632,1364)
+	NewPanel /K=1 /W=(120,750,624,1636)
 	ValDisplay valdisp_DataAcq_P_LED_Clear,pos={366.00,298.00},size={86.00,29.00},disable=1
 	ValDisplay valdisp_DataAcq_P_LED_Clear,help={"red:user"},userdata(tabnum)=  "0"
 	ValDisplay valdisp_DataAcq_P_LED_Clear,userdata(tabcontrol)=  "tab_DataAcq_Pressure"
@@ -3575,6 +3575,26 @@ Window DA_Ephys() : Panel
 	PopupMenu Popup_Settings_FixedFreq,userdata(tabnum)=  "5"
 	PopupMenu Popup_Settings_FixedFreq,userdata(tabcontrol)=  "ADC"
 	PopupMenu Popup_Settings_FixedFreq,mode=1,popvalue="Maximum",value= #"DAP_GetSamplingFrequencies()"
+	TitleBox Title_settings_Hardware_UPress,pos={43.00,598.00},size={70.00,15.00},title="User Pressure"
+	TitleBox Title_settings_Hardware_UPress,userdata(tabnum)=  "6"
+	TitleBox Title_settings_Hardware_UPress,userdata(tabcontrol)=  "ADC",frame=0
+	PopupMenu popup_Settings_UserPressure,pos={41.00,619.00},size={219.00,19.00},bodyWidth=150,proc=DAP_PopMenuProc_UpdateGuiState,title="DAC devices"
+	PopupMenu popup_Settings_UserPressure,help={"List of available DAC devices for pressure control"}
+	PopupMenu popup_Settings_UserPressure,userdata(tabnum)=  "6"
+	PopupMenu popup_Settings_UserPressure,userdata(tabcontrol)=  "ADC"
+	PopupMenu popup_Settings_UserPressure,mode=1,popvalue="- none -",value= #"\"- none -\""
+	PopupMenu Popup_Settings_UserPressure_ADC,pos={269.00,619.00},size={47.00,19.00},proc=DAP_PopMenuProc_UpdateGuiState,title="AD"
+	PopupMenu Popup_Settings_UserPressure_ADC,userdata(tabnum)=  "6"
+	PopupMenu Popup_Settings_UserPressure_ADC,userdata(tabcontrol)=  "ADC"
+	PopupMenu Popup_Settings_UserPressure_ADC,mode=1,popvalue="0",value= #"\"0;1;2;3;4;5;6;7;8;9;10;11;12;13;14;15\""
+	Button button_Hardware_PUser_Enable,pos={336.00,598.00},size={60.00,45.00},proc=P_ButtonProc_UserPressure,title="Enable"
+	Button button_Hardware_PUser_Enable,help={"Enable device for user pressure acquisition"}
+	Button button_Hardware_PUser_Enable,userdata(tabnum)=  "6"
+	Button button_Hardware_PUser_Enable,userdata(tabcontrol)=  "ADC",fSize=14
+	Button button_Hardware_PUser_Disable,pos={400.00,598.00},size={60.00,45.00},disable=2,proc=P_ButtonProc_UserPressure,title="Disable"
+	Button button_Hardware_PUser_Disable,help={"Disable device for user pressure acquisition"}
+	Button button_Hardware_PUser_Disable,userdata(tabnum)=  "6"
+	Button button_Hardware_PUser_Disable,userdata(tabcontrol)=  "ADC",fSize=14
 	DefineGuide UGV0={FR,-25},UGH0={FB,-27},UGV1={FL,481}
 	SetWindow kwTopWin,hook(cleanup)=DAP_WindowHook
 	SetWindow kwTopWin,userdata(ResizeControlsInfo)= A"!!*'\"z!!#CW!!#Dl5QCcbzzzzzzzzzzzzzzzzzzzz"

commit 7416c2da60a74ad26e046c84b18fe364846569cd

P_ButtonProc_UserPressure: Use correct parameter order
First hardware type and then the device ID.

Bug introduced in d329baaf (Add entries to DA_Ephys to record User Pressure during TP, 2018-12-16).

Packages/MIES/MIES_PressureControl.ipf
@@ -2514,8 +2514,8 @@ Function P_ButtonProc_UserPressure(ba) : ButtonControl
 					// the device is the same for all headstages
 					deviceID = pressureDataWv[0][%UserPressureDeviceID]
 					hardwareType = pressureDataWv[0][%UserPressureDeviceHWType]
-					HW_CloseDevice(deviceID, hardwareType, flags = HARDWARE_PREVENT_ERROR_MESSAGE)
-					HW_DeRegisterDevice(deviceID, hardwareType)
+					HW_CloseDevice(hardwareType, deviceID, flags = HARDWARE_PREVENT_ERROR_MESSAGE)
+					HW_DeRegisterDevice(hardwareType, deviceID)
 				endif
 
 				pressureDataWv[][%UserPressureDeviceID]     = NaN