AllenInstitute/MIES

commit c7ff0792c4953ff95f99f440d1dc2af7c06489c3

SF_FormulaPlotter: Add support for custom markers
Operations can now use custom markers and this allows to have a different
marker for each data point in a formula. In case the graph is plotted with markers
these markers are picked up and applied to all traces.

Packages/MIES/MIES_SweepFormula.ipf
@@ -1451,7 +1451,7 @@ static Function SF_FormulaPlotter(string graph, string formula, [DFREF dfr, vari
 	variable i, j, k, numTraces, splitTraces, splitY, splitX, numGraphs, numWins, numData, dataCnt, traceCnt
 	variable dim1Y, dim2Y, dim1X, dim2X, winDisplayMode
 	variable xMxN, yMxN, xPoints, yPoints, keepUserSelection, numAnnotations
-	string win, wList, winNameTemplate, exWList, wName, annotation, yAxisLabel
+	string win, wList, winNameTemplate, exWList, wName, annotation, yAxisLabel, wvName
 	string yFormula, yFormulasRemain
 	STRUCT SF_PlotMetaData plotMetaData
 	STRUCT RGBColor color
@@ -1666,6 +1666,21 @@ static Function SF_FormulaPlotter(string graph, string formula, [DFREF dfr, vari
 					&& (!WaveExists(wvX) \
 					|| DimSize(wvx, ROWS) <  SF_MAX_NUMPOINTS_FOR_MARKERS))
 					ModifyGraph/W=$win mode=3,marker=19
+
+					WAVE/Z customMarkerAsFree = JWN_GetNumericWaveFromWaveNote(wvY, SF_META_MOD_MARKER)
+
+					if(WaveExists(customMarkerAsFree))
+						DFREF dfrWork = SFH_GetWorkingDF(graph)
+						for(i = 0; i < numTraces; i += 1)
+
+							wvName = UniqueWaveName(dfr, "customMarker_" + NameOfWave(wvY))
+							MoveWave customMarkerAsFree, dfrWork:$wvName
+							WAVE/SDFR=dfrWork customMarker = $wvName
+							ASSERT(DimSize(wvY, ROWS) == DimSize(customMarker, ROWS), "Marker size mismatch")
+
+							ModifyGraph/W=$win zmrkNum($traces[i])={customMarker}
+						endfor
+					endif
 				endif
 
 				dataCnt += 1

Packages/MIES/MIES_Constants.ipf
@@ -1908,6 +1908,7 @@ StrConstant SF_META_XVALUES = "/XValues" // numeric wave
 StrConstant SF_META_XAXISLABEL = "/XAxisLabel" // string
 StrConstant SF_META_YAXISLABEL = "/YAxisLabel" // string
 StrConstant SF_META_OPSTACK = "/OperationStack" // string
+StrConstant SF_META_MOD_MARKER = "/Marker" // numeric wave
 
 StrConstant SF_DATATYPE_SWEEP = "SweepData"
 StrConstant SF_DATATYPE_FINDLEVEL = "FindLevel"

commit 3d1b7296efc39c8e67d434244e5b892be941b209

SF_FormulaPlotter: Fix derived unique wave name for custom markers
Broken since c7ff0792 (SF_FormulaPlotter: Add support for custom markers, 2023-02-27).

Packages/MIES/MIES_SweepFormula.ipf
@@ -1891,7 +1891,7 @@ static Function SF_FormulaPlotter(string graph, string formula, [DFREF dfr, vari
 				WAVE/Z customMarkerAsFree = JWN_GetNumericWaveFromWaveNote(wvY, SF_META_MOD_MARKER)
 				if(WaveExists(customMarkerAsFree))
 					DFREF dfrWork = SFH_GetWorkingDF(graph)
-					wvName = UniqueWaveName(dfr, "customMarker_" + NameOfWave(wvY))
+					wvName = UniqueWaveName(dfrWork, "customMarker_" + NameOfWave(wvY))
 					MoveWave customMarkerAsFree, dfrWork:$wvName
 					WAVE/SDFR=dfrWork customMarker = $wvName
 					ASSERT(DimSize(wvY, ROWS) == DimSize(customMarker, ROWS), "Marker size mismatch")