AllenInstitute/MIES

commit c20443d578e598eff9bea481c19884232c1b3a9b

JSONToWave: Support more wave types and dimension offset/delta/units

Packages/MIES/MIES_Utilities.ipf
@@ -6208,7 +6208,7 @@ End
 /// @sa WaveToJSON
 Function/WAVE JSONToWave(string str, [string path])
 
-	variable jsonID, dim, i, j, k
+	variable jsonID, dim, i, j, k, numEntries, size
 	string unit, type, dataUnit, waveNote
 
 	if(ParamIsDefault(path))
@@ -6231,9 +6231,27 @@ Function/WAVE JSONToWave(string str, [string path])
 
 	type = JSON_GetString(jsonID, path + "/type", ignoreErr = 1)
 
-	// we only support FP64 for now
-	ASSERT(!cmpstr(type, "NT_FP64"), "Type is not supported")
-	WAVE/D/Z data = JSON_GetWave(jsonID, path + "/data/raw")
+	strswitch(type)
+		case "NT_FP64":
+		case "NT_FP32":
+		case "NT_I32":
+		case "NT_I16":
+		case "NT_I8":
+			WAVE/Z data = JSON_GetWave(jsonID, path + "/data/raw")
+			Redimension/Y=(WaveTypeStringToNumber(type)) data
+			break
+		case "TEXT_WAVE_TYPE":
+			WAVE/Z data = JSON_GetTextWave(jsonID, path + "/data/raw")
+			break
+		case "WAVE_TYPE":
+			size = JSON_GetArraySize(jsonID, path + "/data/raw")
+			Make/N=(size)/FREE/WAVE container = JSONToWave(str, path = path + "/data/raw/" + num2str(p))
+			WAVE data = container
+			break
+		default:
+			ASSERT(0, "Type is not supported: " + type)
+	endswitch
+
 	ASSERT(WaveExists(data), "Missing data")
 
 	WAVE/D/Z dimSizes = JSON_GetWave(jsonID, path + "/dimension/size", ignoreErr = 1)
@@ -6244,10 +6262,52 @@ Function/WAVE JSONToWave(string str, [string path])
 	Redimension/N=(newSizes[0], newSizes[1], newSizes[2], newSizes[3]) data
 
 	WAVE/D/Z dimDeltas = JSON_GetWave(jsonID, path + "/dimension/delta", ignoreErr = 1)
-	ASSERT(!WaveExists(dimDeltas), "dimension deltas are not supported")
-
 	WAVE/D/Z dimOffsets = JSON_GetWave(jsonID, path + "/dimension/offset", ignoreErr = 1)
-	ASSERT(!WaveExists(dimOffsets), "dimension offsets are not supported")
+	WAVE/T/Z dimUnits = JSON_GetTextWave(jsonID, path + "/dimension/unit", ignoreErr = 1)
+
+	if(WaveExists(dimDeltas) || WaveExists(dimOffsets) || WaveExists(dimUnits))
+
+		if(WaveExists(dimDeltas))
+			numEntries = DimSize(dimDeltas, ROWS)
+		elseif(WaveExists(dimOffsets))
+			numEntries = DimSize(dimOffsets, ROWS)
+		elseif(WaveExists(dimUnits))
+			numEntries = DimSize(dimUnits, ROWS)
+		endif
+
+		if(!WaveExists(dimDeltas))
+			Make/D/FREE/N=(numEntries) dimDeltas = 1
+		endif
+
+		if(!WaveExists(dimOffsets))
+			Make/D/FREE/N=(numEntries) dimOffsets = 0
+		endif
+
+		if(!WaveExists(dimUnits))
+			Make/T/FREE/N=(numEntries) dimUnits
+		endif
+
+		for(i = 0; i < numEntries; i += 1)
+
+			// @todo avoid switch once SetScale supports strings for the dimension
+			switch(i)
+				case 0:
+					SetScale/P x, dimOffsets[i], dimDeltas[i], dimUnits[i], data
+					break
+				case 1:
+					SetScale/P y, dimOffsets[i], dimDeltas[i], dimUnits[i], data
+					break
+				case 2:
+					SetScale/P z, dimOffsets[i], dimDeltas[i], dimUnits[i], data
+					break
+				case 3:
+					SetScale/P t, dimOffsets[i], dimDeltas[i], dimUnits[i], data
+					break
+				default:
+					ASSERT(0, "Unsupported dimension")
+			endswitch
+		endfor
+	endif
 
 	WAVE/T/Z dimLabelsFull = JSON_GetTextWave(jsonID, path + "/dimension/label/full", ignoreErr = 1)
 
@@ -6269,9 +6329,6 @@ Function/WAVE JSONToWave(string str, [string path])
 			endfor
 		endfor
 	endif
-
-	WAVE/T/Z dimUnits = JSON_GetWave(jsonID, path + "/dimension/unit", ignoreErr = 1)
-	ASSERT(!WaveExists(dimUnits), "dimension units are not supported")
 
 	// no way to restore the modification date
 

Packages/tests/Basic/UTF_Utils.ipf
@@ -6471,6 +6471,9 @@ Function/WAVE GetTestWaveForSerialization()
 
 	Note wv, "abcd"
 
+	SetScale/P x, 0, 1, "1122", wv
+	SetScale/P y, 2, 3, "3344", wv
+
 	SetScale d, 7, 8, "efgh", wv
 
 	SetDimLabel ROWS, -1, $"ijkl", wv
@@ -6494,6 +6497,25 @@ Function JSONWaveSerializationWorks()
 
 	WAVE/Z serialized = JSONToWave(str)
 	CHECK_WAVE(serialized, NUMERIC_WAVE | FREE_WAVE, minorType = DOUBLE_WAVE)
+
+	CHECK_EQUAL_WAVES(wv, serialized)
+End
+
+Function JSONWaveSerializationWorksText()
+
+	string str
+
+	Make/FREE/T wv = {{"1", "2", "3"}, {"4", "5", "6"}}
+	CHECK_EQUAL_VAR(DimSize(wv, ROWS), 3)
+	CHECK_EQUAL_VAR(DimSize(wv, COLS), 2)
+
+	SetScale/P x, 0, 1, "1122", wv
+
+	str = WaveToJSON(wv)
+	CHECK_PROPER_STR(str)
+
+	WAVE/Z serialized = JSONToWave(str)
+	CHECK_WAVE(serialized, TEXT_WAVE | FREE_WAVE)
 
 	CHECK_EQUAL_WAVES(wv, serialized)
 End
@@ -6566,6 +6588,41 @@ Function JSONWaveSerializationWorksWithPath()
 	CHECK_WAVE(serialized, NUMERIC_WAVE | FREE_WAVE, minorType = DOUBLE_WAVE)
 
 	CHECK_EQUAL_WAVES(wv, serialized)
+End
+
+static Function/WAVE GetSupportedWaveTypes()
+
+	Make/FREE/T input = {"NT_FP64", "NT_FP32", "NT_I32", "NT_I16", "NT_I8", "TEXT_WAVE", "WAVE_WAVE"}
+	SetDimensionLabels(input, TextWaveToList(input, ";"), ROWS)
+
+	return input
+End
+
+/// UTF_TD_GENERATOR s0:GetSupportedWaveTypes
+Function JSONWaveCreatesCorrectWaveTypes([STRUCT IUTF_mData &m])
+	string str, typeStr
+	variable type
+
+	typeStr = m.s0
+
+	strswitch(typeStr)
+		case "TEXT_WAVE":
+			Make/FREE/T dataText
+			WAVE data = dataText
+			break
+		case "WAVE_WAVE":
+			Make/FREE/WAVE dataWave
+			WAVE data = dataWave
+			break
+		default:
+			type = WaveTypeStringToNumber(typeStr)
+			Make/FREE/Y=(type) data
+			break
+	endswitch
+
+	str = WaveToJSON(data)
+	WAVE/Z result = JSONToWave(str)
+	CHECK_EQUAL_WAVES(data, result, mode = WAVE_DATA | WAVE_DATA_TYPE)
 End
 
 Function GetMarqueeHelperWorks()

commit 05f7493dda5a45629b7e10f2b837ff182686839e

JSONToWave: Add assertion at correct place
Bug introduced in c20443d5 (JSONToWave: Support more wave types and
dimension offset/delta/units, 2023-05-19).

Packages/MIES/MIES_Utilities.ipf
@@ -6222,10 +6222,12 @@ Function/WAVE JSONToWave(string str, [string path])
 		case "NT_I16":
 		case "NT_I8":
 			WAVE/Z data = JSON_GetWave(jsonID, path + "/data/raw")
+			ASSERT(WaveExists(data), "Missing data")
 			Redimension/Y=(WaveTypeStringToNumber(type)) data
 			break
 		case "TEXT_WAVE_TYPE":
 			WAVE/Z data = JSON_GetTextWave(jsonID, path + "/data/raw")
+			ASSERT(WaveExists(data), "Missing data")
 			break
 		case "WAVE_TYPE":
 			size = JSON_GetArraySize(jsonID, path + "/data/raw")
@@ -6235,8 +6237,6 @@ Function/WAVE JSONToWave(string str, [string path])
 		default:
 			ASSERT(0, "Type is not supported: " + type)
 	endswitch
-
-	ASSERT(WaveExists(data), "Missing data")
 
 	WAVE/D/Z dimSizes = JSON_GetWave(jsonID, path + "/dimension/size", ignoreErr = 1)
 	ASSERT(WaveExists(dimSizes), "dimension sizes are missing")