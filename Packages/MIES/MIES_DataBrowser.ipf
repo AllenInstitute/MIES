AllenInstitute/MIES

commit 7ef8625c878f59fd7a872324b36f8c11886f96a9

Databrowser/SweepBrowser: Add mode property
Until now we treated all databrowser/sweepbrowsers equally. For a future
application we want to use a databrowser for automation purposes.

For this we introduce a new mode which is stored in the window user data,
can be choosen when opened, and also found.

The settings history and the sweep controls subwindows are disabled in
the automation mode.

The call to DB_FindAllDataBrowser in SWS_AfterSweepDataChangeHook needs
the ALL variant as we want to update all databrowsers.

Packages/MIES/MIES_DataBrowser.ipf
@@ -9,8 +9,14 @@
 /// @file MIES_DataBrowser.ipf
 /// @brief __DB__ Panel for browsing acquired data during acquisition
 
-Function/S DB_OpenDataBrowser()
+Function/S DB_OpenDataBrowser([variable mode])
 	string win, winBSP, device, devicesWithData, bsPanel
+
+	if(ParamIsDefault(mode))
+		mode = BROWSER_MODE_USER
+	else
+		ASSERT(mode == BROWSER_MODE_USER || mode == BROWSER_MODE_AUTOMATION || mode == BROWSER_MODE_ALL, "Invalid mode")
+	endif
 
 	Execute "DataBrowser()"
 	win = GetCurrentWindow()
@@ -21,7 +27,7 @@ Function/S DB_OpenDataBrowser()
 	SetWindow $winBSP, tooltipHook(nbinteract)=BSP_TTHookSFFormulaNB
 
 	AddVersionToPanel(win, DATA_SWEEP_BROWSER_PANEL_VERSION)
-	BSP_SetDataBrowser(win)
+	BSP_SetDataBrowser(win, mode)
 	BSP_InitPanel(win)
 
 	// immediately lock if we have only data from one device
@@ -723,10 +729,15 @@ Function DB_SplitSweepsIfReq(string win, variable sweepNo)
 End
 
 /// @brief Find a Databrowser which is locked to the given DAEphys panel
-Function/S DB_FindDataBrowser(device)
-	string device
+Function/S DB_FindDataBrowser(string device, [variable mode])
 
-	WAVE/T/Z matches = DB_FindAllDataBrowser(device)
+	if(ParamIsDefault(mode))
+		mode = BROWSER_MODE_USER
+	else
+		ASSERT(mode == BROWSER_MODE_USER || mode == BROWSER_MODE_AUTOMATION || mode == BROWSER_MODE_ALL, "Invalid mode")
+	endif
+
+	WAVE/T/Z matches = DB_FindAllDataBrowser(device, mode = mode)
 
 	if(!WaveExists(matches))
 		return ""
@@ -736,11 +747,17 @@ Function/S DB_FindDataBrowser(device)
 End
 
 /// @brief Find all Databrowser which are locked to the given DAEphys panel
-Function/WAVE DB_FindAllDataBrowser(string device)
+Function/WAVE DB_FindAllDataBrowser(string device, [variable mode])
 
 	string panelList
 	string panel
 	variable numPanels, i, idx
+
+	if(ParamIsDefault(mode))
+		mode = BROWSER_MODE_USER
+	else
+		ASSERT(mode == BROWSER_MODE_USER || mode == BROWSER_MODE_AUTOMATION || mode == BROWSER_MODE_ALL, "Invalid mode")
+	endif
 
 	panelList = WinList("DB_*", ";", "WIN:1")
 	numPanels = ItemsInList(panelList)
@@ -751,6 +768,10 @@ Function/WAVE DB_FindAllDataBrowser(string device)
 		panel = StringFromList(i, panelList)
 
 		if(!BSP_IsDataBrowser(panel))
+			continue
+		endif
+
+		if(!BSP_HasMode(panel, mode))
 			continue
 		endif
 
@@ -772,13 +793,22 @@ End
 
 /// @brief Returns a databrowser bound to the given `device`
 ///
-/// Creates a new one, if none is found or bound.
-Function/S DB_GetBoundDataBrowser(string device)
+/// @param device locked device
+/// @param mode   [defaults to #BROWSER_MODE_USER] mode of the databrowser to search. One of @ref BrowserModes.
+///
+/// Creates a new one, if none is found nor bound.
+Function/S DB_GetBoundDataBrowser(string device, [variable mode])
 	string databrowser, bsPanel
 
-	databrowser = DB_FindDataBrowser(device)
+	if(ParamIsDefault(mode))
+		mode = BROWSER_MODE_USER
+	else
+		ASSERT(mode == BROWSER_MODE_USER || mode == BROWSER_MODE_AUTOMATION || mode == BROWSER_MODE_ALL, "Invalid mode")
+	endif
+
+	databrowser = DB_FindDataBrowser(device, mode = mode)
 	if(IsEmpty(databrowser)) // not yet open
-		databrowser = DB_OpenDataBrowser()
+		databrowser = DB_OpenDataBrowser(mode = mode)
 	endif
 
 	if(BSP_HasBoundDevice(databrowser))

Packages/MIES/MIES_AnalysisBrowser_SweepBrowser.ipf
@@ -387,16 +387,22 @@ Function SB_SweepBrowserWindowHook(s)
 	return 0
 End
 
-Function/DF SB_OpenSweepBrowser()
+Function/DF SB_OpenSweepBrowser([variable mode])
 
 	string mainWin, renameWin
+
+	if(ParamIsDefault(mode))
+		mode = BROWSER_MODE_USER
+	else
+		ASSERT(mode == BROWSER_MODE_USER || mode == BROWSER_MODE_AUTOMATION || mode == BROWSER_MODE_ALL, "Invalid mode")
+	endif
 
 	Execute "DataBrowser()"
 
 	mainWin = GetMainWindow(GetCurrentWindow())
 
 	AddVersionToPanel(mainWin, DATA_SWEEP_BROWSER_PANEL_VERSION)
-	BSP_SetSweepBrowser(mainWin)
+	BSP_SetSweepBrowser(mainWin, mode)
 
 	SetWindow $mainWin, hook(cleanup)=SB_SweepBrowserWindowHook
 	SB_SetUserData(mainWin)

Packages/MIES/MIES_BrowserSettingsPanel.ipf
@@ -431,35 +431,87 @@ End
 
 /// @brief set DEVICE property to the userdata of the main panel
 ///
-/// @param win 	name of external panel or main window
-/// @param type One of #BROWSERTYPE_DATABROWSER or #BROWSERTYPE_SWEEPBROWSER
-static Function/S BSP_SetBrowserType(string win, string type)
+/// @param win 	   name of external panel or main window
+/// @param type    One of #BROWSERTYPE_DATABROWSER or #BROWSERTYPE_SWEEPBROWSER
+/// @param mode    One of @ref BrowserModes
+static Function/S BSP_SetBrowserType(string win, string type, variable mode)
 	string mainPanel
+
+	string title, suffix
 
 	mainPanel = GetMainWindow(win)
 	ASSERT(WindowExists(mainPanel), "specified panel does not exist.")
 
 	SetWindow $mainPanel, userdata($MIES_BSP_BROWSER) = type
 
-	if(!CmpStr(type, BROWSERTYPE_SWEEPBROWSER))
-		DoWindow/T $mainPanel, SWEEPBROWSER_WINDOW_TITLE
-	elseif(!CmpStr(type, BROWSERTYPE_DATABROWSER))
-		DoWindow/T $mainPanel, DATABROWSER_WINDOW_TITLE
+	if(mode == BROWSER_MODE_USER)
+		suffix = ""
+	elseif(mode == BROWSER_MODE_AUTOMATION)
+		suffix = " (A*U*T*O*M*A*T*I*O*N)"
 	endif
+
+	SetWindow $mainPanel, userdata($MIES_BSP_BROWSER_MODE) = BSP_SerializeBrowserMode(mode)
+
+	if(!CmpStr(type, BROWSERTYPE_SWEEPBROWSER))
+		title = SWEEPBROWSER_WINDOW_TITLE
+	elseif(!CmpStr(type, BROWSERTYPE_DATABROWSER))
+		title = DATABROWSER_WINDOW_TITLE
+	else
+		ASSERT(0, "Invalid type")
+	endif
+
+	title += suffix
+
+	DoWindow/T $mainPanel, title
+End
+
+static Function BSP_ParseBrowserMode(string mode)
+	strswitch(mode)
+		case "User":
+			return BROWSER_MODE_USER
+		case "Automation":
+			return BROWSER_MODE_AUTOMATION
+		case "All":
+			return BROWSER_MODE_ALL
+		default:
+			ASSERT(0, "Invalid mode")
+	endswitch
+End
+
+static Function/S BSP_SerializeBrowserMode(variable mode)
+	switch(mode)
+		case BROWSER_MODE_USER:
+			return "User"
+		case BROWSER_MODE_AUTOMATION:
+			return "Automation"
+		case BROWSER_MODE_ALL:
+			return "All"
+		default:
+			ASSERT(0, "Invalid mode")
+	endswitch
+End
+
+Function BSP_HasMode(string win, variable mode)
+
+	string mainPanel
+	variable foundMode
+
+	mainPanel = GetMainWindow(win)
+	foundMode = BSP_ParseBrowserMode(GetUserData(mainPanel, "", MIES_BSP_BROWSER_MODE))
+
+	return foundMode & mode
 End
 
 /// @brief wrapper function for external calls
-Function BSP_SetDataBrowser(win)
-	string win
+Function BSP_SetDataBrowser(string win, variable mode)
 
-	BSP_SetBrowserType(win, BROWSERTYPE_DATABROWSER)
+	BSP_SetBrowserType(win, BROWSERTYPE_DATABROWSER, mode)
 End
 
 /// @brief wrapper function for external calls
-Function BSP_SetSweepBrowser(win)
-	string win
+Function BSP_SetSweepBrowser(string win, variable mode)
 
-	BSP_SetBrowserType(win, BROWSERTYPE_SWEEPBROWSER)
+	BSP_SetBrowserType(win, BROWSERTYPE_SWEEPBROWSER, mode)
 End
 
 /// @brief wrapper function for external calls
@@ -579,9 +631,11 @@ static Function BSP_MainPanelButtonToggle(mainPanel, visible)
 	visible = !!visible ? 1 : 0
 
 	panelButton = "button_BSP_open"
-	if(!ControlExists(mainPanel, panelButton))
-		return 0
+
+	if(!ControlExists(mainPanel, panelButton) || IsControlDisabled(mainPanel, panelButton))
+		return NaN
 	endif
+
 	if(visible)
 		ShowControl(mainPanel, panelButton)
 	else

Packages/MIES/MIES_Constants.ipf
@@ -19,7 +19,7 @@ Constant DAQ_CONFIG_WAVE_VERSION = 2
 
 /// Used to upgrade the GuiStateWave as well as the DA Ephys panel
 Constant DA_EPHYS_PANEL_VERSION           = 57
-Constant DATA_SWEEP_BROWSER_PANEL_VERSION = 43
+Constant DATA_SWEEP_BROWSER_PANEL_VERSION = 44
 Constant WAVEBUILDER_PANEL_VERSION        = 13
 Constant ANALYSISBROWSER_PANEL_VERSION    =  1
 
@@ -488,6 +488,7 @@ Constant MIES_BSP_DS  = 7
 /// @name values for  UserData in BrowserSettings and derived windows
 /// @{
 strConstant MIES_BSP_BROWSER = "BROWSER"
+strConstant MIES_BSP_BROWSER_MODE = "BROWSERMODE"
 strConstant MIES_BSP_DEVICE = "DEVICE"
 strConstant MIES_BSP_PANEL_FOLDER = "PANEL_FOLDER"
 strConstant MIES_BSP_AR_SWEEPFOLDER = "AR_SWEEPFOLDER"
@@ -1943,4 +1944,12 @@ StrConstant EXPCONFIG_UDATA_EXCLUDE_RESTORE = "Config_DontRestore"
 /// @anchor FFTWinFunctions
 /// @{
 StrConstant FFT_WINF = "Bartlet;Bartlett;Blackman367;Blackman361;Blackman492;Blackman474;Cos1;Cos2;Cos3;Cos4;Hamming;Hanning;KaiserBessel20;KaiserBessel25;KaiserBessel30;Parzen;Poisson2;Poisson3;Poisson4;Riemann;SFT3F;SFT3M;FTNI;SFT4F;SFT5F;SFT4M;FTHP;HFT70;FTSRS;SFT5M;HFT90D;HFT95;HFT116D;HFT144D;HFT169D;HFT196D;HFT223D;HFT248D;"
+/// @}
+
+/// @name Types for DB_GetBoundDataBrowser
+/// @anchor BrowserModes
+/// @{
+Constant BROWSER_MODE_USER       = 0x01
+Constant BROWSER_MODE_AUTOMATION = 0x02
+Constant BROWSER_MODE_ALL        = 0xFF
 /// @}

Packages/MIES/MIES_SweepSaving.ipf
@@ -98,7 +98,7 @@ End
 /// @param device device name
 static Function SWS_AfterSweepDataChangeHook(string device)
 
-	WAVE/T/Z allDBs = DB_FindAllDataBrowser(device)
+	WAVE/T/Z allDBs = DB_FindAllDataBrowser(device, mode = BROWSER_MODE_ALL)
 
 	if(!WaveExists(allDBs))
 		return NaN

Packages/tests/Basic/UTF_SweepFormula.ipf
@@ -12,7 +12,7 @@ Function/S CreateFakeSweepBrowser_IGNORE()
 	DFREF dfr = GetDataFolderDFR()
 	AddVersionToPanel(win, DATA_SWEEP_BROWSER_PANEL_VERSION)
 	BSP_SetFolder(win, dfr, MIES_BSP_PANEL_FOLDER)
-	BSP_SetSweepBrowser(win)
+	BSP_SetSweepBrowser(win, BROWSER_MODE_USER)
 
 	return win
 End
@@ -42,7 +42,7 @@ static Function [string win, string device] CreateFakeDataBrowserWindow()
 
 	Display/N=$win as device
 	AddVersionToPanel(win, DATA_SWEEP_BROWSER_PANEL_VERSION)
-	BSP_SetDataBrowser(win)
+	BSP_SetDataBrowser(win, BROWSER_MODE_USER)
 	BSP_SetDevice(win, device)
 	MIES_DB#DB_SetUserData(win, device)
 End

commit 7880b652750128eca8c52da770d143d63cd3531d

DB_GetBoundDataBrowser: Find databrowser of the correct mode
When we create a new databrowser with a non-default mode we also need to
look for exactly that mode later on.

Bug introduced in 7ef8625c (Databrowser/SweepBrowser: Add mode property,
2022-10-12).

Packages/MIES/MIES_DataBrowser.ipf
@@ -793,7 +793,7 @@ Function/S DB_GetBoundDataBrowser(string device, [variable mode])
 	bsPanel = BSP_GetPanel(databrowser)
 	PGC_SetAndActivateControl(bsPanel, "popup_DB_lockedDevices", str = device)
 
-	return DB_FindDataBrowser(device)
+	return DB_FindDataBrowser(device, mode = mode)
 End
 
 /// @brief Jumps in the SweepFormula help notebook of the current data/sweepbrowser to the first location