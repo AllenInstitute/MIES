AllenInstitute/MIES

commit cc9e52522b6c13404b93fdfcccd8bdf459ab8792

GetLastSettingTextIndepSCI: Add it

Packages/MIES/MIES_MiesUtilities.ipf
@@ -804,6 +804,34 @@ threadsafe Function GetLastSettingIndepSCI(numericalValues, sweepNo, setting, he
 	endif
 End
 
+/// @brief Return a headstage independent setting from the textual
+///        labnotebook of the sweeps in the same SCI cycle
+///
+/// @return the headstage independent setting or `defValue`
+///
+/// @ingroup LabnotebookQueryFunctions
+/// @sa GetLastSetting()
+threadsafe Function/S GetLastSettingTextIndepSCI(textualValues, sweepNo, setting, headstage, entrySourceType, [defValue])
+	WAVE/T textualValues
+	variable sweepNo
+	string setting
+	variable headstage, entrySourceType
+	string defValue
+
+	if(ParamIsDefault(defValue))
+		defValue = ""
+	endif
+
+	WAVE/T/Z settings = GetLastSettingSCI(textualValues, sweepNo, setting, headstage, entrySourceType)
+
+	if(WaveExists(settings))
+		EnforceIndependentSetting(settings)
+		return settings[GetIndexForHeadstageIndepData(textualValues)]
+	else
+		return defValue
+	endif
+End
+
 /// @brief Return a headstage independent setting from the numerical
 ///        labnotebook of the sweeps in the same RA cycle
 ///

commit 4814be08d24cbd65cb4c743e02af0071daae91a6

GetLastSettingTextIndepSCI: Fix it
We always need both labnotebook waves for the labnotebook getter functions
which search the whole SCI and return text entries.

Bug introduced in cc9e5252 (GetLastSettingTextIndepSCI: Add it, 2023-11-13).

Packages/MIES/MIES_MiesUtilities.ipf
@@ -811,7 +811,8 @@ End
 ///
 /// @ingroup LabnotebookQueryFunctions
 /// @sa GetLastSetting()
-threadsafe Function/S GetLastSettingTextIndepSCI(textualValues, sweepNo, setting, headstage, entrySourceType, [defValue])
+threadsafe Function/S GetLastSettingTextIndepSCI(numericalValues, textualValues, sweepNo, setting, headstage, entrySourceType, [defValue])
+	WAVE numericalValues
 	WAVE/T textualValues
 	variable sweepNo
 	string setting
@@ -822,7 +823,7 @@ threadsafe Function/S GetLastSettingTextIndepSCI(textualValues, sweepNo, setting
 		defValue = ""
 	endif
 
-	WAVE/T/Z settings = GetLastSettingSCI(textualValues, sweepNo, setting, headstage, entrySourceType)
+	WAVE/T/Z settings = GetLastSettingTextSCI(numericalValues, textualValues, sweepNo, setting, headstage, entrySourceType)
 
 	if(WaveExists(settings))
 		EnforceIndependentSetting(settings)