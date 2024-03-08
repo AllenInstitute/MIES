AllenInstitute/MIES

commit 595daef00ef31daa66250e430baa3b60f771109d

MIES_SweepFormula_PSX.ipf: Add support for multiple ranges

Packages/MIES/MIES_SweepFormula_PSX.ipf
@@ -1102,7 +1102,7 @@ static Function/WAVE PSX_GenerateSweepEquiv(WAVE selectData)
 End
 
 /// @brief Helper function of the `psxStats` operation
-static Function/WAVE PSX_OperationStatsImpl(string graph, string id, WAVE rangeParam, WAVE selectData, string prop, string stateAsStr, string postProc)
+static Function/WAVE PSX_OperationStatsImpl(string graph, string id, WAVE/WAVE rangeParam, WAVE selectData, string prop, string stateAsStr, string postProc)
 
 	string propLabelAxis, comboKey
 	variable numRows, numCols, i, j, k, index, sweepNo, chanNr, chanType, state, numRanges, lowerBoundary, upperBoundary, temp, err
@@ -1116,16 +1116,8 @@ static Function/WAVE PSX_OperationStatsImpl(string graph, string id, WAVE rangeP
 	numRows = DimSize(selectDataEquiv, ROWS)
 	numCols = DimSize(selectDataEquiv, COLS)
 
-	// see also SFH_EvaluateRange()
-	if(IsNumericWave(rangeParam))
-		numRanges = 1
-		Make/FREE/WAVE allRanges = {rangeParam}
-	elseif(IsTextWave(rangeParam))
-		numRanges = DimSize(rangeParam, ROWS)
-		WAVE/T rangeParamText = rangeParam
-		Make/FREE/WAVE/N=(numRanges) allRanges = ListToTextWave(rangeParamText[p], ";")
-		WaveClear rangeParamText
-	endif
+	WAVE/WAVE allRanges = SplitWavesToDimension(rangeParam)
+	numRanges = DimSize(allRanges, ROWS)
 	WaveClear rangeParam
 
 	WAVE/Z eventContainerFromResults = PSX_GetEventContainerFromResults(id)
@@ -3572,9 +3564,9 @@ End
 /// @brief Read the user JWN from results and create a legend from all operation parameters
 static Function PSX_AddLegend(string win, WAVE/WAVE results)
 
-	variable jsonID, value, type, i, j, numOperations, numParameters
+	variable jsonID, value, type, i, j, numOperations, numParameters, numRows
 	string line, op, param, prefix, opNice, mainWindow, jsonPathOp, jsonPathParam
-	string str
+	string str, containerSep
 	string sep = ", "
 
 	jsonID = JWN_GetWaveNoteAsJSON(results)
@@ -3623,13 +3615,27 @@ static Function PSX_AddLegend(string win, WAVE/WAVE results)
 						str = TextWaveToList(wvText, sep)
 						WaveClear wvText
 					else
-						WAVE wv = JSON_GetWave(jsonID, jsonPathParam, waveMode = 1)
-						ASSERT(IsNumericWave(wv), "Expected numeric wave")
-						str = NumericWaveToList(wv, sep)
-						WaveClear wv
+						if(!cmpstr(param, "range"))
+							WAVE/WAVE container = JWN_GetWaveRefNumericFromWaveNote(results, jsonPathParam)
+							containerSep = "; "
+						else
+							WAVE wv = JSON_GetWave(jsonID, jsonPathParam, waveMode = 1)
+							Make/FREE/WAVE container = {wv}
+							containerSep = ""
+						endif
+
+						str = ""
+						for(WAVE wv : container)
+							ASSERT(IsNumericWave(wv), "Expected numeric wave")
+							// NumericWaveToList outputs in column-major order but we want row-major
+							MatrixOp/FREE dest = wv^t
+							str += RemoveEnding(NumericWaveToList(dest, sep), sep) + containerSep
+						endfor
+
+						str = RemoveEnding(str, containerSep)
 					endif
 
-					sprintf line, "%s: %s", param, RemoveEnding(str, ", ")
+					sprintf line, "%s: %s", param, RemoveEnding(str, sep)
 					break
 				default:
 					ASSERT(0, "Unsupported type")
@@ -4257,7 +4263,7 @@ Function/WAVE PSX_OperationKernel(variable jsonId, string jsonPath, string graph
 	variable riseTau, decayTau, amp, dt, numPoints, numCombos, i, offset
 	string parameterPath, key
 
-	WAVE range = SFH_EvaluateRange(jsonId, jsonPath, graph, SF_OP_PSX_KERNEL, 0)
+	WAVE/WAVE range = SFH_EvaluateRange(jsonId, jsonPath, graph, SF_OP_PSX_KERNEL, 0)
 
 	WAVE/Z selectData = SFH_GetArgumentSelect(jsonID, jsonPath, graph, SF_OP_PSX_KERNEL, 1)
 
@@ -4358,7 +4364,7 @@ Function/WAVE PSX_OperationStats(variable jsonId, string jsonPath, string graph)
 
 	id = SFH_GetArgumentAsText(jsonID, jsonPath, graph, SF_OP_PSX, 0, checkFunc = IsValidObjectName)
 
-	WAVE range = SFH_EvaluateRange(jsonId, jsonPath, graph, SF_OP_PSX_STATS, 1)
+	WAVE/WAVE range = SFH_EvaluateRange(jsonId, jsonPath, graph, SF_OP_PSX_STATS, 1)
 
 	WAVE/Z selectData = SFH_GetArgumentSelect(jsonID, jsonPath, graph, SF_OP_PSX_STATS, 2)
 	SFH_Assert(WaveExists(selectData), "Missing select data")

Packages/tests/Basic/UTF_SweepFormula_PSX.ipf
@@ -421,7 +421,7 @@ static Function StatsComplainsWithoutEvents()
 
 	// matching id but no events
 	try
-		MIES_PSX#PSX_OperationStatsImpl(browser, id, range, selectData, prop, stateAsStr, postProc)
+		MIES_PSX#PSX_OperationStatsImpl(browser, id, {range}, selectData, prop, stateAsStr, postProc)
 		FAIL()
 	catch
 		error = ROStr(GetSweepFormulaParseErrorMessage())
@@ -432,7 +432,7 @@ static Function StatsComplainsWithoutEvents()
 
 	// mismatched id
 	try
-		MIES_PSX#PSX_OperationStatsImpl(browser, id, range, selectData, prop, stateAsStr, postProc)
+		MIES_PSX#PSX_OperationStatsImpl(browser, id, {range}, selectData, prop, stateAsStr, postProc)
 		FAIL()
 	catch
 		error = ROStr(GetSweepFormulaParseErrorMessage())
@@ -669,7 +669,7 @@ static Function StatsWorksWithResults([STRUCT IUTF_mData &m])
 
 	MIES_PSX#PSX_StoreIntoResultsWave(browser, SFH_RESULT_TYPE_PSX_EVENTS, psxEvent, id)
 
-	WAVE/WAVE output = MIES_PSX#PSX_OperationStatsImpl(browser, id, range, selectData, prop, stateAsStr, postProc)
+	WAVE/WAVE output = MIES_PSX#PSX_OperationStatsImpl(browser, id, {range}, selectData, prop, stateAsStr, postProc)
 	CHECK_WAVE(output, WAVE_WAVE)
 
 	Make/FREE/N=4 dims = DimSize(output, p)
@@ -950,7 +950,7 @@ static Function StatsWorksWithResultsSpecialCases([STRUCT IUTF_mData &m])
 		refNum = NaN
 	endif
 
-	WAVE/WAVE output = MIES_PSX#PSX_OperationStatsImpl(browser, id, range, allSelectData, prop, stateAsStr, postProc)
+	WAVE/WAVE output = MIES_PSX#PSX_OperationStatsImpl(browser, id, {range}, allSelectData, prop, stateAsStr, postProc)
 	CHECK_WAVE(output, WAVE_WAVE)
 
 	if(outOfRange)

commit d4e6d2027e883dc6ebd0704026259c868ca570b3

PSX_OperationKernel: Avoid assertion with invalid ranges
We need to zap all null waves before storing the wave ref wave in the JSON
wavenote. The order does matter but SFH_GetSweepsForFormula also ignores
null ranges so we end up being ordered again in the end.

Bug introduced in 595daef0 (MIES_SweepFormula_PSX.ipf: Add support for
multiple ranges, 2024-02-02).

Packages/MIES/MIES_SweepFormula_PSX.ipf
@@ -4377,8 +4377,11 @@ Function/WAVE PSX_OperationKernel(variable jsonId, string jsonPath, string graph
 	Redimension/N=(PSX_KERNEL_OUTPUTWAVES_PER_ENTRY * numCombos) output
 
 	parameterPath = SF_META_USER_GROUP + PSX_JWN_PARAMETERS + "/" + SF_OP_PSX_KERNEL
+
+	WAVE rangeClean = ZapNullRefs(range)
+
 	JWN_CreatePath(output, parameterPath)
-	JWN_SetWaveInWaveNote(output, parameterPath + "/range", range) // not the same as SF_META_RANGE
+	JWN_SetWaveInWaveNote(output, parameterPath + "/range", rangeClean) // not the same as SF_META_RANGE
 	JWN_SetNumberInWaveNote(output, parameterPath + "/riseTau", riseTau)
 	JWN_SetNumberInWaveNote(output, parameterPath + "/decayTau", decayTau)
 	JWN_SetNumberInWaveNote(output, parameterPath + "/amp", amp)

Packages/tests/Basic/UTF_SweepFormula_PSX.ipf
@@ -1072,6 +1072,14 @@ static Function TestOperationPSXKernel()
 	CheckDimensionScaleHelper(dataWref[4], 0, 0.01)
 	CheckDimensionScaleHelper(dataWref[5], 50, 0.2)
 
+	// three waves from first range, none from second
+	str = "psxKernel(dataset([50, 150], wave(\"\")), select(channels(AD6), [0, 2], all), 1, 15, -5)"
+	WAVE/WAVE dataWref = SF_ExecuteFormula(str, win, useVariables = 0)
+	CHECK_WAVE(dataWref, WAVE_WAVE)
+	CHECK_EQUAL_VAR(DimSize(dataWref, ROWS), 3)
+	WAVE/Z range = JWN_GetNumericWaveFromWaveNote(dataWref[2], "/Range")
+	CHECK_EQUAL_WAVES(range, {50, 150}, mode = WAVE_DATA)
+
 	// no data from select statement
 	str = "psxKernel([50, 150], select(channels(AD15), [0]), 1, 15, -5)"
 	try