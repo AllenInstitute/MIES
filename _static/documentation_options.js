db3347507e38b43172b047100d7e77b962f2a681
MIES Deployment <mies-deploy@linux-mint-box.seattle>
2024-03-26 15:16:00 +0000

    Updating documentation to Release_2.7_20230809-544-g00c92fe57b

---
+++ _static/documentation_options.js
@@ -0,0 +1,14 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '(main) Release_2.7_20230809-544-g00c92fe57b',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: false,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
