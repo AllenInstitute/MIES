AllenInstitute/MIES

commit fe7b00b34c75f040bd56d6f1e00127395898def7

CI: Add ipt format to check formatting for PRs

tools/ipt-format.sh
@@ -1,0 +1,30 @@
+#!/bin/bash
+
+# Get top level of git repo
+git --version > /dev/null
+if [ $? -ne 0 ]
+then
+  echo "Could not find git executable"
+  exit 1
+fi
+
+top_level=$(git rev-parse --show-toplevel)
+
+if [ -z "$top_level" ]
+then
+  echo "This is not a git repository"
+  exit 1
+fi
+
+if [ -n "$CI" ]
+then
+  ipt="./ipt.exe"
+else
+  ipt="ipt"
+fi
+
+echo "[format]" > config.toml
+while read -r line; do
+    echo "files = \"$line\"" >> config.toml
+done < <(git ls-files ':(attr:ipt)')
+$ipt --arg-file config.toml format -i

.gitattributes
@@ -63,3 +63,9 @@ Packages/doc/** linguist-documentation
 tools/installer/nsis/** linguist-vendored
 tools/installer/Shelllink/** linguist-vendored
 tools/installer/AccessControl/** linguist-vendored
+
+# input files for IPT
+Packages/Conversion/**/*.ipf ipt
+Packages/MIES/*MIES*.ipf     ipt
+Packages/doc/**/*.ipf        ipt
+Packages/tests/**/*.ipf      ipt

.github/workflows/build-pr.yml
@@ -36,6 +36,33 @@ concurrency:
   cancel-in-progress: true
 jobs:
 
+  Ipt:
+    name: 🔎 IPT Format check
+    runs-on: [ self-hosted, Windows ]
+    steps:
+      - name: Checkout repository
+        uses: actions/checkout@v4
+        # no need to checkout submodules or the full history
+      - name: Download ipt binary from server
+        run: curl -o ipt.exe "https://byte-physics.de/public-downloads/aistorage/transfer/ipt/0.4.0/ipt.exe"
+      - name: IPT version
+        run: ./ipt.exe --version
+      - name: Format code
+        run: tools/ipt-format.sh
+      - name: Check for changed files
+        run: git diff --name-only; git diff --quiet
+      - name: Create patch
+        if: ${{ failure() }}
+        run: git diff > format-changes.patch
+      - name: upload artifacts
+        uses: actions/upload-artifact@v4
+        if: ${{ failure() }}
+        with:
+          name: IPT-assets
+          path: |
+            format-changes.patch
+          if-no-files-found: warn
+
   BuildInstaller:
     name: 🏗 Build ${{ matrix.kind }} installer
     strategy:
@@ -277,6 +304,7 @@ jobs:
     if: ${{ !cancelled() && !inputs.is_called_workflow}}
     needs:
       # All jobs that create new artifacts
+      - Ipt
       - BuildInstaller
       - CompilationEachCommitTest
       - CompilationTest

.gitignore
@@ -56,6 +56,7 @@ Packages/tests/**/*.nwb
 # intermediate test files
 rewritten_config.json
 CheckIfConfigurationRestoresMCCFilterGain.json
+config.toml
 
 # Renamed experiments during tests
 Packages/tests/**/20*.pxp

commit 003f0c332475b5d6d5c444911cb377957f2e4117

tools/ipt-format.sh: Allow to run it from a subdirectory
Bug introduced in fe7b00b34c (CI: Add ipt format to check formatting for
PRs, 2024-03-28).

tools/ipt-format.sh
@@ -27,4 +27,5 @@ echo "[format]" > config.toml
 while read -r line; do
     echo "files = \"$line\"" >> config.toml
 done < <(git ls-files ':(attr:ipt)')
-$ipt --arg-file config.toml format -i
+
+(cd $top_level && $ipt --arg-file config.toml format -i)