AllenInstitute/MIES

commit 6bdae268911062487617f99a7d9cd222dfe87414

BSP_RenameAndSetTitle: Make the title more informative
We now create a unique title for SweepBrowser and DataBrowser. For the
databrowser this also includes the locked-to device.

The numbering in the title takes both SweepBrowser and DataBrowser into
account.

Packages/MIES/MIES_BrowserSettingsPanel.ipf
@@ -1948,3 +1948,35 @@ Function BSP_WindowHook(s)
 	// return zero so that other hooks are called as well
 	return 0
 End
+
+/// @brief Renames the browser window and sets an informative title
+///
+/// @param win     name of the existing window
+/// @param newName suggested new name, will be adapted to be unique
+Function/S BSP_RenameAndSetTitle(string win, string newName)
+
+	variable numOtherBrowser
+	string newTitle
+	string suffix = ""
+
+	if(BSP_IsDataBrowser(win) && BSP_HasBoundDevice(win))
+		suffix = " with \"" + BSP_GetDevice(win) + "\""
+	endif
+
+	if(WindowExists(newName))
+		newName = UniqueName(newName, 9, 1)
+	endif
+
+	DoWindow/W=$win/C $newName
+	win = newName
+
+	numOtherBrowser += ItemsInList(WinList(SWEEPBROWSER_WINDOW_NAME + "*", ";", "WIN:1"))
+	numOtherBrowser += ItemsInList(WinList(DATABROWSER_WINDOW_NAME + "*", ";", "WIN:1"))
+	numOtherBrowser += ItemsInList(WinList("DB_*", ";", "WIN:1"))
+	numOtherBrowser  = max(0, numOtherBrowser - 1)
+
+	sprintf newTitle, "Browser %s%s", SelectString(numOtherBrowser, "", " [" + num2str(numOtherBrowser) + "]"), suffix
+	DoWindow/T $win, newTitle
+
+	return win
+End

Packages/MIES/MIES_AnalysisBrowser_SweepBrowser.ipf
@@ -370,7 +370,7 @@ End
 
 Function/DF SB_OpenSweepBrowser([variable mode])
 
-	string mainWin, renameWin
+	string mainWin
 
 	if(ParamIsDefault(mode))
 		mode = BROWSER_MODE_USER
@@ -390,14 +390,13 @@ Function/DF SB_OpenSweepBrowser([variable mode])
 	DFREF sweepBrowserDFR = BSP_GetFolder(mainWin, MIES_BSP_PANEL_FOLDER)
 	GetSweepBrowserMap(sweepBrowserDFR)
 
-	renameWin = UniqueName(SWEEPBROWSER_WINDOW_NAME, 9, 1)
-	DoWindow/W=$mainWin/C $renameWin
-	mainWin = renameWin
+	mainWin = BSP_RenameAndSetTitle(mainWin, SWEEPBROWSER_WINDOW_NAME)
 
 	string/G sweepBrowserDFR:graph = mainWin
 
 	BSP_InitPanel(mainWin)
 	BSP_ScaleAxes(mainWin)
+
 	return sweepBrowserDFR
 End
 

Packages/MIES/MIES_DataBrowser.ipf
@@ -299,24 +299,18 @@ static Function/S DB_LockToDevice(win, device)
 		newWindow = "DB_" + device
 	endif
 
-	if(CmpStr(win, newWindow))
-		if(windowExists(newWindow))
-			newWindow = UniqueName(newWindow, 9, 1)
-		endif
-		DoWindow/W=$win/C $newWindow
-		win = newWindow
+	win = BSP_RenameAndSetTitle(win, newWindow)
+
+	DB_SetUserData(win, device)
+	if(windowExists(BSP_GetPanel(win)) && BSP_HasBoundDevice(win))
+		BSP_DynamicStartupSettings(win)
+		[first, last] = BSP_FirstAndLastSweepAcquired(win)
+		DB_UpdateLastSweepControls(win, first, last)
 	endif
 
-	DB_SetUserData(newWindow, device)
-	if(windowExists(BSP_GetPanel(newWindow)) && BSP_HasBoundDevice(newWindow))
-		BSP_DynamicStartupSettings(newWindow)
-		[first, last] = BSP_FirstAndLastSweepAcquired(newWindow)
-		DB_UpdateLastSweepControls(newWindow, first, last)
-	endif
+	UpdateSweepPlot(win)
 
-	UpdateSweepPlot(newWindow)
-
-	return newWindow
+	return win
 End
 
 static Function DB_SetUserData(win, device)

commit 5c69adfe9bb823b5e45e42584624a860c62a2417

BSP_RenameAndSetTitle: Keep automation suffix
In 7ef8625c8 (Databrowser/SweepBrowser: Add mode property, 2022-10-12) we
introduced the user and automation browser types. For the automation
browser we changed the title via BSP_SetBrowserType.

But 6bdae2689 (BSP_RenameAndSetTitle: Make the title more informative,
2022-11-06) broke that as it was rewriting the title again after browser
locking to a device.

So we now set the title only in BSP_RenameAndSetTitle.

Packages/MIES/MIES_BrowserSettingsPanel.ipf
@@ -476,26 +476,7 @@ static Function/S BSP_SetBrowserType(string win, string type, variable mode)
 	ASSERT(WindowExists(mainPanel), "specified panel does not exist.")
 
 	SetWindow $mainPanel, userdata($MIES_BSP_BROWSER)=type
-
-	if(mode == BROWSER_MODE_USER)
-		suffix = ""
-	elseif(mode == BROWSER_MODE_AUTOMATION)
-		suffix = " (A*U*T*O*M*A*T*I*O*N)"
-	endif
-
 	SetWindow $mainPanel, userdata($MIES_BSP_BROWSER_MODE)=BSP_SerializeBrowserMode(mode)
-
-	if(!CmpStr(type, BROWSERTYPE_SWEEPBROWSER))
-		title = SWEEPBROWSER_WINDOW_NAME
-	elseif(!CmpStr(type, BROWSERTYPE_DATABROWSER))
-		title = DATABROWSER_WINDOW_NAME
-	else
-		ASSERT(0, "Invalid type")
-	endif
-
-	title += suffix
-
-	DoWindow/T $mainPanel, title
 End
 
 static Function BSP_ParseBrowserMode(string mode)
@@ -1977,11 +1958,13 @@ End
 Function/S BSP_RenameAndSetTitle(string win, string newName)
 
 	variable numOtherBrowser
-	string   newTitle
-	string suffix = ""
+	string newTitle, suffix
+	string modeSuffix = ""
 
 	if(BSP_IsDataBrowser(win) && BSP_HasBoundDevice(win))
 		suffix = " with \"" + BSP_GetDevice(win) + "\""
+	else
+		suffix = " "
 	endif
 
 	if(WindowExists(newName) && cmpstr(win, newName))
@@ -1991,13 +1974,18 @@ Function/S BSP_RenameAndSetTitle(string win, string newName)
 	DoWindow/W=$win/C $newName
 	win = newName
 
+	switch(BSP_GetBrowserMode(win))
+		case BROWSER_MODE_AUTOMATION:
+			modeSuffix = " (A*U*T*O*M*A*T*I*O*N)"
+			break
+	endswitch
+
 	numOtherBrowser += ItemsInList(WinList(SWEEPBROWSER_WINDOW_NAME + "*", ";", "WIN:1"))
 	numOtherBrowser += ItemsInList(WinList(DATABROWSER_WINDOW_NAME + "*", ";", "WIN:1"))
 	numOtherBrowser += ItemsInList(WinList("DB_*", ";", "WIN:1"))
 	numOtherBrowser  = max(0, numOtherBrowser - 1)
 
-	sprintf newTitle, "Browser %s%s", SelectString(numOtherBrowser, "", " [" + num2str(numOtherBrowser) + "]"), suffix
-	newTitle = RemoveEnding(newTitle, " ")
+	sprintf newTitle, "Browser%s%s%s", SelectString(numOtherBrowser, "", " [" + num2str(numOtherBrowser) + "]"), suffix, modeSuffix
 	DoWindow/T $win, newTitle
 
 	return win