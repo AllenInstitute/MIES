AllenInstitute/MIES

commit d3a696ef6c804113e79d98152d167946368461c7

psxStats: Convert avg post processing to stats
Instead of only returning the average we now return multiple values.

Packages/MIES/MIES_SweepFormula_PSX.ipf
@@ -949,7 +949,8 @@ End
 static Function/WAVE PSX_OperationStatsImpl(string graph, string id, WAVE rangeParam, WAVE selectData, string prop, string stateAsStr, string postProc)
 
 	string propLabel, propLabelAxis, comboKey
-	variable numRows, numCols, i, j, k, index, sweepNo, chanNr, chanType, state, numRanges, lowerBoundary, upperBoundary, temp
+	variable numRows, numCols, i, j, k, index, sweepNo, chanNr, chanType, state, numRanges, lowerBoundary, upperBoundary, temp, err
+	variable refMarker
 
 	WAVE/WAVE output = SFH_CreateSFRefWave(graph, SF_OP_PSX_STATS, MINIMUM_WAVE_SIZE)
 
@@ -1080,14 +1081,32 @@ static Function/WAVE PSX_OperationStatsImpl(string graph, string id, WAVE rangeP
 
 						JWN_SetWaveInWaveNote(results, SF_META_XVALUES, eventIndex)
 						break
-					case "avg":
+					case "stats":
 						WAVE/Z resultsRawClean = ZapNaNs(resultsRaw)
 
 						if(!WaveExists(resultsRawClean))
 							continue
 						endif
 
-						MatrixOp/FREE results = mean(resultsRawClean)
+						WaveStats/Q/M=2 resultsRawClean
+
+						Make/FREE/D results = {V_avg, NaN, V_adev, V_sdev, V_skew, V_kurt}
+
+						AssertonAndClearRTError()
+						StatsQuantiles/Q/Z resultsRawClean; err = GetRTError(1)
+
+						if(!err)
+							results[1] = V_Median
+						endif
+
+						WAVE/T statsLabels = ListToTextWave(PSX_STATS_LABELS, ";")
+						JWN_SetWaveInWaveNote(results, SF_META_XVALUES, statsLabels)
+						SetDimensionLabels(results, PSX_STATS_LABELS, ROWS)
+
+						// resize markers
+						Redimension/N=(DimSize(results, ROWS)) marker
+						refMarker= marker[0]
+						marker[] = refMarker
 
 						break
 					case "count":
@@ -1151,7 +1170,6 @@ static Function/WAVE PSX_OperationStatsImpl(string graph, string id, WAVE rangeP
 				Redimension/N=(DimSize(results, ROWS)) marker, comboKeys
 
 				JWN_SetWaveInWaveNote(results, SF_META_MOD_MARKER, marker)
-
 				JWN_CreatePath(results, SF_META_USER_GROUP + PSX_JWN_COMBO_KEYS_NAME)
 				JWN_SetWaveInWaveNote(results, SF_META_USER_GROUP + PSX_JWN_COMBO_KEYS_NAME, comboKeys)
 
@@ -1177,9 +1195,8 @@ static Function/WAVE PSX_OperationStatsImpl(string graph, string id, WAVE rangeP
 			JWN_SetStringInWaveNote(output, SF_META_XAXISLABEL, "Event")
 			JWN_SetStringInWaveNote(output, SF_META_YAXISLABEL, propLabelAxis)
 			break
-		case "avg":
-			JWN_SetStringInWaveNote(output, SF_META_XAXISLABEL, "NA")
-			JWN_SetStringInWaveNote(output, SF_META_YAXISLABEL, "Averaged " + LowerStr(propLabelAxis))
+		case "stats":
+			JWN_SetStringInWaveNote(output, SF_META_XAXISLABEL, "Statistical properties of " + LowerStr(propLabelAxis))
 			break
 		case "count":
 			JWN_SetStringInWaveNote(output, SF_META_XAXISLABEL, "NA")
@@ -3812,7 +3829,7 @@ Function/WAVE PSX_OperationStats(variable jsonId, string jsonPath, string graph)
 
 	prop       = SFH_GetArgumentAsText(jsonID, jsonPath, graph, SF_OP_PSX_STATS, 3, allowedValues = {"amp", "xpos", "xinterval", "tau", "estate", "fstate", "fitresult"})
 	stateAsStr = SFH_GetArgumentAsText(jsonID, jsonPath, graph, SF_OP_PSX_STATS, 4, allowedValues = {"accept", "reject", "undetermined", "all", "every"})
-	postProc   = SFH_GetArgumentAsText(jsonID, jsonPath, graph, SF_OP_PSX_STATS, 5, defValue = "nothing", allowedValues = {"nothing", "avg", "count", "hist", "log10"})
+	postProc   = SFH_GetArgumentAsText(jsonID, jsonPath, graph, SF_OP_PSX_STATS, 5, defValue = "nothing", allowedValues = {"nothing", "stats", "count", "hist", "log10"})
 
 	WAVE/WAVE output = PSX_OperationStatsImpl(graph, id, range, selectData, prop, stateAsStr, postProc)
 

Packages/tests/Basic/UTF_SweepFormula_PSX.ipf
@@ -530,11 +530,12 @@ Function/WAVE StatsTest_GetInput()
 
 	input[%prop]     = "amp"
 	input[%state]    = "accept"
-	input[%postProc] = "avg"
+	input[%postProc] = "stats"
 
-	JWN_SetWaveInWaveNote(input, "/results", {(10 + 30 + 50 + 70) / 4})
-	// no xValues
-	JWN_SetWaveInWaveNote(input, "/marker", {PSX_MARKER_ACCEPT})
+	JWN_SetWaveInWaveNote(input, "/results", {40, 40, 20, 25.81988897471611, 0, -2.0775})
+	JWN_SetWaveInWaveNote(input, "/xValues", ListToTextWave(PSX_STATS_LABELS, ";"))
+	JWN_SetWaveInWaveNote(input, "/marker", {PSX_MARKER_ACCEPT, PSX_MARKER_ACCEPT, PSX_MARKER_ACCEPT, \
+	                                         PSX_MARKER_ACCEPT, PSX_MARKER_ACCEPT, PSX_MARKER_ACCEPT})
 
 	// wv5
 	Duplicate/FREE/T template, wv5
@@ -605,6 +606,12 @@ static Function StatsWorksWithResults([STRUCT IUTF_mData &m])
 	WAVE/Z xValues = JWN_GetNumericWaveFromWaveNote(input, "/xValues")
 	WAVE/Z marker  = JWN_GetNumericWaveFromWaveNote(input, "/marker")
 
+	if(WaveExists(xValues) && !HasOneValidEntry(xValues))
+		WaveClear xValues
+
+		WAVE/Z xValues = JWN_GetTextWaveFromWaveNote(input, "/xValues")
+	endif
+
 	[browser, device, formulaGraph] = CreateFakeDataBrowserWithSweepFormulaGraph()
 
 	[WAVE range, WAVE selectData] = GetFakeRangeAndSelectData()
@@ -637,12 +644,25 @@ static Function StatsWorksWithResults([STRUCT IUTF_mData &m])
 
 	CHECK_EQUAL_WAVES(resultsRead, results, mode = WAVE_DATA, tol = 1e-5)
 
-	WAVE/Z xValuesRead = JWN_GetNumericWaveFromWaveNote(resultsRead, SF_META_XVALUES)
+	WAVE/Z xValuesReadNumeric = JWN_GetNumericWaveFromWaveNote(resultsRead, SF_META_XVALUES)
+
+	if(WaveExists(xValuesReadNumeric) && !HasOneValidEntry(xValuesReadNumeric))
+		WaveClear xValuesReadNumeric
+
+		WAVE/Z xValuesReadText = JWN_GetTextWaveFromWaveNote(resultsRead, SF_META_XVALUES)
+	endif
+
 	if(WaveExists(xValues))
-		CHECK_WAVE(xValuesRead, NUMERIC_WAVE, minorType = DOUBLE_WAVE)
-		CHECK_EQUAL_WAVES(xValuesRead, xValues, mode = WAVE_DATA)
+		if(IsNumericWave(xValuesReadNumeric))
+			CHECK_WAVE(xValuesReadNumeric, NUMERIC_WAVE, minorType = DOUBLE_WAVE)
+			CHECK_EQUAL_WAVES(xValuesReadNumeric, xValues, mode = WAVE_DATA)
+		elseif(IsTextWave(xValuesReadText))
+			CHECK_WAVE(xValuesReadText, TEXT_WAVE)
+			CHECK_EQUAL_WAVES(xValuesReadText, xValues, mode = WAVE_DATA)
+		endif
 	else
-		CHECK_WAVE(xValuesRead, NULL_WAVE)
+		CHECK_WAVE(xValuesReadNumeric, NULL_WAVE)
+		CHECK_WAVE(xValuesReadText, NULL_WAVE)
 		CHECK_WAVE(xValues, NULL_WAVE)
 	endif
 
@@ -754,31 +774,32 @@ Function/WAVE StatsTestSpecialCases_GetInput()
 	JWN_SetWaveInWaveNote(input, "/0/marker", {PSX_MARKER_REJECT})
 
 	// wv5
-	// avg ignores NaN
+	// stats ignores NaN
 	Duplicate/FREE/T template, wv5
 	WAVE/T input = wv5
 
 	input[%prop]            = "amp"
 	input[%state]           = "all"
-	input[%postProc]        = "avg"
+	input[%postProc]        = "stats"
 	input[%refNumOutputRows]= "1"
 	input[%numEventsCombo0] = "2"
 	input[%numEventsCombo1] = "2"
 	input[%outOfRange]      = "0"
 
 	JWN_CreatePath(input, "/0")
-	JWN_SetWaveInWaveNote(input, "/0/results", {10})
-	// no xValues
-	JWN_SetWaveInWaveNote(input, "/0/marker", {PSX_MARKER_REJECT})
+	JWN_SetWaveInWaveNote(input, "/0/results", {10, 0, 0, 0, NaN, NaN})
+	JWN_SetWaveInWaveNote(input, "/0/xValues", ListToTextWave(PSX_STATS_LABELS, ";"))
+	JWN_SetWaveInWaveNote(input, "/0/marker", {PSX_MARKER_REJECT, PSX_MARKER_REJECT, PSX_MARKER_REJECT, \
+	                                           PSX_MARKER_REJECT, PSX_MARKER_REJECT, PSX_MARKER_REJECT})
 
 	// wv6
-	// avg ignores NaN and with all data NaN we don't get anything
+	// stats ignores NaN and with all data NaN we don't get anything
 	Duplicate/FREE/T template, wv6
 	WAVE/T input = wv6
 
 	input[%prop]            = "amp"
 	input[%state]           = "all"
-	input[%postProc]        = "avg"
+	input[%postProc]        = "stats"
 	input[%refNumOutputRows]= "0"
 	input[%numEventsCombo0] = "1"
 	input[%numEventsCombo1] = "1"

Packages/MIES/MIES_Constants.ipf
@@ -2158,3 +2158,5 @@ Constant PSX_MARKER_UNDET  = 18
 /// @{
 Constant PSX_DECAY_FIT_ERROR = -10000
 /// @}
+
+StrConstant PSX_STATS_LABELS = "Average;Median;Average Deviation;Standard deviation;Skewness;Kurtosis"

commit 1e58c207f9a835adfc68292e3a85981c9fb1886c

PSX_OperationStatsImpl: Fix median calculation for post processing stats
It does not make sense to combine /Z with GetRTError(1) as we will never
get an RTE.

The test was also incorrect, so this is now tested as well (again).

Bug introduced in d3a696ef6 (psxStats: Convert avg post processing to
stats, 2023-06-16).

Packages/MIES/MIES_SweepFormula_PSX.ipf
@@ -1343,10 +1343,9 @@ static Function/WAVE PSX_OperationStatsImpl(string graph, string id, WAVE/WAVE r
 
 					Make/FREE/D results = {V_avg, NaN, V_adev, V_sdev, V_skew, V_kurt}
 
-					AssertonAndClearRTError()
-					StatsQuantiles/Q/Z resultsRawClean; err = GetRTError(1)
+					StatsQuantiles/Q/Z resultsRawClean
 
-					if(!err)
+					if(!V_Flag)
 						results[1] = V_Median
 					endif
 

Packages/tests/Basic/UTF_SweepFormula_PSX.ipf
@@ -1029,7 +1029,7 @@ Function/WAVE StatsTestSpecialCases_GetInput()
 	input[%outOfRange]       = "0"
 
 	JWN_CreatePath(input, "/0")
-	JWN_SetWaveInWaveNote(input, "/0/results", {10, 0, 0, 0, NaN, NaN})
+	JWN_SetWaveInWaveNote(input, "/0/results", {10, NaN, 0, 0, NaN, NaN})
 	JWN_SetWaveInWaveNote(input, "/0/xValues", ListToTextWave(PSX_STATS_LABELS, ";"))
 	JWN_SetWaveInWaveNote(input, "/0/marker", {PSX_MARKER_REJECT, PSX_MARKER_REJECT, PSX_MARKER_REJECT, \
 	                                           PSX_MARKER_REJECT, PSX_MARKER_REJECT, PSX_MARKER_REJECT})