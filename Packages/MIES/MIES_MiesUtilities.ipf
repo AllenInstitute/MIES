AllenInstitute/MIES

commit e6d6f19f48ef4b900fcc43d3982ce9bd5853c163

MiesUtil: Add axes for TTL epochs to LayoutGraph function
- add new field visualizeEpochs to TiledGraphSettings structure
  to prevent showing TTL epoch axes if the unsplitted TTL trace
  is shown

- epochs are only shown for TTL if splitTTLBits checkbox is activated as well

Packages/MIES/MIES_MiesUtilities.ipf
@@ -2310,7 +2310,7 @@ End
 /// For overlayed channels we have up to three blocks (DA, AD, TTL) in that order.
 Function LayoutGraph(string win, STRUCT TiledGraphSettings &tgs)
 
-	variable i, numSlots, headstage,  numBlocksTTL, numBlocks, numBlocksEpoch, spacePerSlot
+	variable i, numSlots, headstage,  numBlocksTTL, numBlocks, numBlocksEpochDA, numBlocksEpochTTL, spacePerSlot
 	variable numBlocksDA, numBlocksAD, first, firstFreeAxis, lastFreeAxis, orientation
 	variable numBlocksUnassocDA, numBlocksUnassocAD, numBlocksHS
 	string graph, regex, freeAxis, axis
@@ -2341,18 +2341,22 @@ Function LayoutGraph(string win, STRUCT TiledGraphSettings &tgs)
 
 		sprintf regex, ".*%s_DA$", DB_AXIS_PART_EPOCHS
 		WAVE/T/Z Epochaxes = GrepWave(allVerticalAxes, regex)
-		numBlocksEpoch = WaveExists(Epochaxes) ? DimSize(Epochaxes, ROWS) : 0
+		numBlocksEpochDA = WaveExists(Epochaxes) ? DimSize(Epochaxes, ROWS) : 0
 
 		regex = ".*AD$"
 		WAVE/T/Z ADaxes = GrepWave(allVerticalAxes, regex)
 		numBlocksAD = WaveExists(ADaxes) ? DimSize(ADaxes, ROWS) : 0
 
-		regex = ".*TTL$"
+		sprintf regex, ".*(?<!%s)_TTL$", DB_AXIS_PART_EPOCHS
 		WAVE/T/Z TTLaxes = GrepWave(allVerticalAxes, regex)
 		numBlocksTTL = WaveExists(TTLaxes) ? DimSize(TTLaxes, ROWS) : 0
 
-		numBlocks = numBlocksAD + numBlocksDA + numBlocksTTL + numBlocksEpoch
-		numSlots = ADC_SLOT_MULTIPLIER * numBlocksAD + numBlocksDA + numBlocksTTL + EPOCH_SLOT_MULTIPLIER * numBlocksEpoch
+		sprintf regex, ".*%s_TTL$", DB_AXIS_PART_EPOCHS
+		WAVE/T/Z Epochaxes = GrepWave(allVerticalAxes, regex)
+		numBlocksEpochTTL = WaveExists(Epochaxes) ? DimSize(Epochaxes, ROWS) : 0
+
+		numBlocks = numBlocksAD + numBlocksDA + numBlocksTTL + numBlocksEpochDA + numBlocksEpochTTL
+		numSlots = ADC_SLOT_MULTIPLIER * numBlocksAD + numBlocksDA + numBlocksTTL + EPOCH_SLOT_MULTIPLIER * (numBlocksEpochDA + numBlocksEpochTTL)
 
 		spacePerSlot = (1.0 - (numBlocks - 1) * GRAPH_DIV_SPACING) / numSlots
 
@@ -2410,7 +2414,20 @@ Function LayoutGraph(string win, STRUCT TiledGraphSettings &tgs)
 	// epoch info slots for associated and unassociated DA channels
 	sprintf regex, ".*col0%s_DA_(?:[[:digit:]]{1,2})_HS_(?:([[:digit:]]{1,2}|NaN))$", DB_AXIS_PART_EPOCHS
 	WAVE/T/Z axes = GrepWave(allVerticalAxes, regex)
-	numBlocksEpoch = WaveExists(axes) ? DimSize(axes, ROWS) : 0
+	numBlocksEpochDA = WaveExists(axes) ? DimSize(axes, ROWS) : 0
+
+	// epoch info for TTL channels
+	if(tgs.visualizeEpochs && tgs.splitTTLbits)
+		sprintf regex, ".*col0%s_TTL_(?:[[:digit:]]{1,2})_(?:[[:digit:]])_HS_NaN$", DB_AXIS_PART_EPOCHS
+		WAVE/T/Z axes = GrepWave(allVerticalAxes, regex)
+		numBlocksEpochTTL = WaveExists(axes) ? DimSize(axes, ROWS) : 0
+		if(!numBlocksEpochTTL)
+			// NI Hardware
+			sprintf regex, ".*col0%s_TTL_(?:[[:digit:]]{1,2})_NaN_HS_NaN$", DB_AXIS_PART_EPOCHS
+			WAVE/T/Z axes = GrepWave(allVerticalAxes, regex)
+			numBlocksEpochTTL = WaveExists(axes) ? DimSize(axes, ROWS) : 0
+		endif
+	endif
 
 	// associated AD channels
 	regex = ".*col0_AD_(?:[[:digit:]]{1,2})_HS_(?:[[:digit:]]{1,2})$"
@@ -2434,8 +2451,8 @@ Function LayoutGraph(string win, STRUCT TiledGraphSettings &tgs)
 	// Unassoc DA: 4 slots
 	// TTL: 1 slot per ttlsWithBits
 
-	numBlocks = numBlocksAD + numBlocksDA + numBlocksUnassocDA + numBlocksUnassocAD + numBlocksTTL + numBlocksEpoch
-	numSlots = ADC_SLOT_MULTIPLIER * numBlocksAD + numBlocksDA + numBlocksUnassocDA + ADC_SLOT_MULTIPLIER * numBlocksUnassocAD + numBlocksTTL + EPOCH_SLOT_MULTIPLIER * numBlocksEpoch
+	numBlocks = numBlocksAD + numBlocksDA + numBlocksUnassocDA + numBlocksUnassocAD + numBlocksTTL + numBlocksEpochDA + numBlocksEpochTTL
+	numSlots = ADC_SLOT_MULTIPLIER * numBlocksAD + numBlocksDA + numBlocksUnassocDA + ADC_SLOT_MULTIPLIER * numBlocksUnassocAD + numBlocksTTL + EPOCH_SLOT_MULTIPLIER * (numBlocksEpochDA + numBlocksEpochTTL)
 
 	spacePerSlot = (1.0 - (numBlocks - 1) * GRAPH_DIV_SPACING) / numSlots
 
@@ -2500,7 +2517,15 @@ Function LayoutGraph(string win, STRUCT TiledGraphSettings &tgs)
 
 	// TTLs
 	for(i = 0; i < numBlocksTTL; i += 1)
-		regex = ttlsWithBits[i]
+
+		if(tgs.visualizeEpochs && tgs.splitTTLBits)
+			regex = DB_AXIS_PART_EPOCHS + "_" + ttlsWithBits[i]
+			WAVE/T/Z axes = GrepWave(allVerticalAxes, regex)
+			ASSERT(WaveExists(axes), "Unexpected number of matches")
+			EnableAxis(graph, axes, EPOCH_SLOT_MULTIPLIER * spacePerSlot, first, last)
+		endif
+
+		sprintf regex, ".*(?<!%s)_%s", DB_AXIS_PART_EPOCHS, ttlsWithBits[i]
 		WAVE/T/Z axes = GrepWave(allVerticalAxes, regex)
 		ASSERT(WaveExists(axes), "Unexpected number of matches")
 		EnableAxis(graph, axes, spacePerSlot, first, last)

Packages/MIES/MIES_BrowserSettingsPanel.ipf
@@ -1234,6 +1234,7 @@ Function [STRUCT TiledGraphSettings tgs] BSP_GatherTiledGraphSettings(string win
 	tgs.dDAQDisplayMode      = GetCheckBoxState(bsPanel, "check_BrowserSettings_dDAQ")
 	tgs.dDAQHeadstageRegions = GetSliderPositionIndex(bsPanel, "slider_BrowserSettings_dDAQ")
 	tgs.hideSweep            = GetCheckBoxState(bsPanel, "check_SweepControl_HideSweep")
+	tgs.visualizeEpochs       = GetCheckBoxState(bsPanel, "check_BrowserSettings_VisEpochs")
 
 	if(tgs.overlayChannels)
 		tgs.splitTTLBits = 0

Packages/MIES/MIES_Structures.ipf
@@ -165,6 +165,7 @@ Structure TiledGraphSettings
 	int16 dDAQDisplayMode
 	int16 dDAQHeadstageRegions
 	int16 hideSweep
+	int16 visualizeEpochs
 EndStructure
 
 /// @brief Helper structure for formula parsing of the Wavebuilder combine epoch

commit 9d9cd3ba983e3bd9e363c700326e797c4e46a2ba

LayoutGraph: Fix TTL epoch display without epoch info
When we want to display epoch info for TTL data we also need to have epoch info.

Broken since e6d6f19f4 (MiesUtil: Add axes for TTL epochs to LayoutGraph
function, 2023-06-22).

Packages/MIES/MIES_MiesUtilities.ipf
@@ -2684,7 +2684,7 @@ Function LayoutGraph(string win, STRUCT TiledGraphSettings &tgs)
 	// TTLs
 	for(i = 0; i < numBlocksTTL; i += 1)
 
-		if(tgs.visualizeEpochs && tgs.splitTTLBits)
+		if(tgs.visualizeEpochs && tgs.splitTTLBits && numBlocksEpochTTL > 0)
 			regex = DB_AXIS_PART_EPOCHS + "_" + ttlsWithBits[i]
 			WAVE/T/Z axes = GrepWave(allVerticalAxes, regex)
 			ASSERT(WaveExists(axes), "Unexpected number of matches")