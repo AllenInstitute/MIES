AllenInstitute/MIES

commit 701c9a0ab5ec1dd3eb6f2972c6922f959a360be5

RemoveEndingRegExp: Add tests and handle invalid regex better

Packages/MIES/MIES_Utilities.ipf
@@ -2116,13 +2116,14 @@ threadsafe Function/S RemoveEndingRegExp(str, endingRegExp)
 	string str, endingRegExp
 
 	string endStr
+	variable err
 
 	if(isEmpty(str) || isEmpty(endingRegExp))
 		return str
 	endif
 
-	SplitString/E=("(" + endingRegExp + ")$") str, endStr
-	ASSERT_TS(V_flag == 0 || V_flag == 1, "Unexpected number of matches")
+	SplitString/E=("(" + endingRegExp + ")$") str, endStr; err = GetRTError(1)
+	ASSERT_TS((V_flag == 0 || V_flag == 1) && err == 0, "Unexpected number of matches or invalid regex")
 
 	return RemoveEnding(str, endStr)
 End

Packages/tests/Basic/UTF_Utils.ipf
@@ -7193,3 +7193,40 @@ static Function TestErrorCodeConversion()
 	// is idempotent
 	CHECK_EQUAL_VAR(ConvertXOPErrorCode(convErr), 10009)
 End
+
+static Function TestRemoveEndingRegex()
+
+	string result
+
+	// does nothing with empty string
+	result = RemoveEndingRegExp("", ".*")
+	CHECK_EMPTY_STR(result)
+
+	// does nothing with empty regex
+	result = RemoveEndingRegExp("abcd", "")
+	CHECK_EQUAL_STR(result, "abcd")
+
+	// complains with invalid regex
+	try
+		RemoveEndingRegExp("abcd", "*")
+		FAIL()
+	catch
+		CHECK_NO_RTE()
+	endtry
+
+	// matches
+	result = RemoveEndingRegExp("abcdd", "d*")
+	CHECK_EQUAL_STR(result, "abc")
+
+	// no match
+	result = RemoveEndingRegExp("abcd", "efgh")
+	CHECK_EQUAL_STR(result, "abcd")
+
+	// too many matches
+	try
+		RemoveEndingRegExp("abcd", "ab)(cd")
+		FAIL()
+	catch
+		CHECK_NO_RTE()
+	endtry
+End

commit e9c4c09b3698dd2cd491278f21b5cdec7d1d2f2d

RemoveEndingRegExp: Add missing RTE clearing
Whenever we check the RTE state, we need to clear the RTEs first so that
we can distinguish any lingering RTEs from current ones.

Broken since 701c9a0ab (RemoveEndingRegExp: Add tests and handle invalid
regex better, 2023-10-03).

Packages/MIES/MIES_Utilities.ipf
@@ -2123,6 +2123,7 @@ threadsafe Function/S RemoveEndingRegExp(str, endingRegExp)
 		return str
 	endif
 
+	AssertOnAndClearRTError()
 	SplitString/E=("(" + endingRegExp + ")$") str, endStr; err = GetRTError(1)
 	ASSERT_TS((V_flag == 0 || V_flag == 1) && err == 0, "Unexpected number of matches or invalid regex")
 