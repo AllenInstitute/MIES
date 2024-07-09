AllenInstitute/MIES

commit 2ce9231b81f56722cae84b4180d65f452db5b578

AFH_GetAnalysisParam*: Tighten logic and add tests
We now assert out if the requested parameter name exists but does not
have the expected type. The behaviour that an non-existing parameter
returns successfully is not changed as we want to return a default value
in the future.

Original-CommitID: 3d9137b29f5b0f2de899fc0f32823dba49dd4934

Packages/MIES/MIES_AnalysisFunctionHelpers.ipf
@@ -585,8 +585,17 @@ End
 Function AFH_GetAnalysisParamNumerical(name, params)
 	string name, params
 
+	string type
+
 	ASSERT(AFH_IsValidAnalysisParameter(name), "Name is not a legal non-liberal igor object name")
-	return NumberByKey(name + ":variable", params, "=", ",", 0)
+
+	if(WhichListItem(name, AFH_GetListOfAnalysisParamNames(params)) == -1)
+		return NaN
+	endif
+
+	type = AFH_GetAnalysisParamType(name, params, expectedType = "variable")
+
+	return NumberByKey(name + ":" + type, params, "=", ",", 0)
 End
 
 /// @brief Return a textual user parameter
@@ -597,8 +606,17 @@ End
 Function/S AFH_GetAnalysisParamTextual(name, params)
 	string name, params
 
+	string type
+
 	ASSERT(AFH_IsValidAnalysisParameter(name), "Name is not a legal non-liberal igor object name")
-	return StringByKey(name + ":string", params, "=", ",", 0)
+
+	if(WhichListItem(name, AFH_GetListOfAnalysisParamNames(params)) == -1)
+		return ""
+	endif
+
+	type = AFH_GetAnalysisParamType(name, params, expectedType = "string")
+
+	return StringByKey(name + ":" + type, params, "=", ",", 0)
 End
 
 /// @brief Return a numerical wave user parameter
@@ -612,12 +630,17 @@ End
 Function/WAVE AFH_GetAnalysisParamWave(name, params)
 	string name, params
 
-	ASSERT(AFH_IsValidAnalysisParameter(name), "Name is not a legal non-liberal igor object name")
-	string contents = StringByKey(name + ":wave", params, "=", ",", 0)
+	string type, contents
 
-	if(IsEmpty(contents))
+	ASSERT(AFH_IsValidAnalysisParameter(name), "Name is not a legal non-liberal igor object name")
+
+	if(WhichListItem(name, AFH_GetListOfAnalysisParamNames(params)) == -1)
 		return $""
 	endif
+
+	type = AFH_GetAnalysisParamType(name, params, expectedType = "wave")
+
+	contents = StringByKey(name + ":" + type, params, "=", ",", 0)
 
 	return ListToNumericWave(contents, "|")
 End
@@ -634,12 +657,17 @@ End
 Function/WAVE AFH_GetAnalysisParamTextWave(name, params)
 	string name, params
 
-	ASSERT(AFH_IsValidAnalysisParameter(name), "Name is not a legal non-liberal igor object name")
-	string contents = StringByKey(name + ":textwave", params, "=", ",", 0)
+	string type, contents
 
-	if(IsEmpty(contents))
+	ASSERT(AFH_IsValidAnalysisParameter(name), "Name is not a legal non-liberal igor object name")
+
+	if(WhichListItem(name, AFH_GetListOfAnalysisParamNames(params)) == -1)
 		return $""
 	endif
+
+	type = AFH_GetAnalysisParamType(name, params, expectedType = "textwave")
+
+	contents = StringByKey(name + ":" + type, params, "=", ",", 0)
 
 	return ListToTextWave(contents, "|")
 End

Packages/Testing-MIES/UTF_AnalysisFunctionParameters.ipf
@@ -450,3 +450,229 @@ static Function ReturnsInvalidWaveRef()
 	CHECK_WAVE(AFH_GetAnalysisParamWave("I_DONT_EXIST", params), NULL_WAVE)
 	CHECK_WAVE(AFH_GetAnalysisParamTextWave("I_DONT_EXIST", params), NULL_WAVE)
 End
+
+/// @name AFH_GetAnalysisParamNumerical
+/// @{
+Function GAPN_AbortsWithEmptyName()
+
+	try
+		AFH_GetAnalysisParamNumerical("", "name:variable=0")
+		FAIL()
+	catch
+		PASS()
+	endtry
+End
+
+Function GAPN_AbortsWithIllegalName()
+
+	try
+		AFH_GetAnalysisParamNumerical("123", "name:variable=0")
+		FAIL()
+	catch
+		PASS()
+	endtry
+End
+
+Function GAPN_AbortsWithIllegalType()
+
+	try
+		AFH_GetAnalysisParamNumerical("name", "name:invalidType=0")
+		FAIL()
+	catch
+		PASS()
+	endtry
+End
+
+Function GAPN_AbortsWithWrongType()
+
+	try
+		AFH_GetAnalysisParamNumerical("name", "name:string=0")
+		FAIL()
+	catch
+		PASS()
+	endtry
+End
+
+Function GAPN_ReturnsNanForNonExisting()
+
+	variable result = AFH_GetAnalysisParamNumerical("name", "otherName:variable=0")
+	CHECK_EQUAL_VAR(result, NaN)
+End
+
+Function GAPN_Works()
+
+	variable result = AFH_GetAnalysisParamNumerical("name", "name:variable=123")
+	CHECK_EQUAL_VAR(result, 123)
+End
+
+/// @}
+
+/// @name AFH_GetAnalysisParamTextual
+/// @{
+Function GAPT_AbortsWithEmptyName()
+
+	try
+		AFH_GetAnalysisparamTextual("", "name:string=0")
+		FAIL()
+	catch
+		PASS()
+	endtry
+End
+
+Function GAPT_AbortsWithIllegalName()
+
+	try
+		AFH_GetAnalysisparamTextual("123", "name:string=0")
+		FAIL()
+	catch
+		PASS()
+	endtry
+End
+
+Function GAPT_AbortsWithIllegalType()
+
+	try
+		AFH_GetAnalysisparamTextual("name", "name:invalidType=0")
+		FAIL()
+	catch
+		PASS()
+	endtry
+End
+
+Function GAPT_AbortsWithWrongType()
+
+	try
+		AFH_GetAnalysisparamTextual("name", "name:variable=0")
+		FAIL()
+	catch
+		PASS()
+	endtry
+End
+
+Function GAPT_ReturnsEmptyForNonExisting()
+
+	string result = AFH_GetAnalysisparamTextual("name", "otherName:string=0")
+	string expected = ""
+	CHECK_EQUAL_STR(result, expected)
+End
+
+Function GAPT_Works()
+
+	string result = AFH_GetAnalysisparamTextual("name", "name:string=abcd")
+	string expected = "abcd"
+	CHECK_EQUAL_STR(result, expected)
+End
+
+/// @}
+
+/// @name AFH_GetAnalysisParamWave
+/// @{
+Function GAPW_AbortsWithEmptyName()
+
+	try
+		AFH_GetAnalysisParamWave("", "name:wave=0")
+		FAIL()
+	catch
+		PASS()
+	endtry
+End
+
+Function GAPW_AbortsWithIllegalName()
+
+	try
+		AFH_GetAnalysisParamWave("123", "name:wave=0")
+		FAIL()
+	catch
+		PASS()
+	endtry
+End
+
+Function GAPW_AbortsWithIllegalType()
+
+	try
+		AFH_GetAnalysisParamWave("name", "name:invalidType=0")
+		FAIL()
+	catch
+		PASS()
+	endtry
+End
+
+Function GAPW_AbortsWithWrongType()
+
+	try
+		AFH_GetAnalysisParamWave("name", "name:variable=0")
+		FAIL()
+	catch
+		PASS()
+	endtry
+End
+
+Function GAPW_ReturnsNonExisting()
+
+	WAVE/Z result = AFH_GetAnalysisParamWave("name", "otherName:wave=1|2|3")
+	CHECK_WAVE(result, NULL_WAVE)
+End
+
+Function GAPW_Works()
+
+	WAVE/Z result = AFH_GetAnalysisParamWave("name", "name:wave=1|2|3")
+	CHECK_EQUAL_WAVES(result, {1, 2, 3}, mode = WAVE_DATA)
+End
+
+/// @}
+
+/// @name AFH_GetAnalysisParamTextWave
+/// @{
+Function GAPTW_AbortsWithEmptyName()
+
+	try
+		AFH_GetAnalysisParamTextWave("", "name:textwave=0")
+		FAIL()
+	catch
+		PASS()
+	endtry
+End
+
+Function GAPTW_AbortsWithIllegalName()
+
+	try
+		AFH_GetAnalysisParamTextWave("123", "name:textwave=0")
+		FAIL()
+	catch
+		PASS()
+	endtry
+End
+
+Function GAPTW_AbortsWithIllegalType()
+
+	try
+		AFH_GetAnalysisParamTextWave("name", "name:invalidType=0")
+		FAIL()
+	catch
+		PASS()
+	endtry
+End
+
+Function GAPTW_AbortsWithWrongType()
+
+	try
+		AFH_GetAnalysisParamTextWave("name", "name:variable=0")
+		FAIL()
+	catch
+		PASS()
+	endtry
+End
+
+Function GAPTW_ReturnsNonExisting()
+
+	WAVE/Z result = AFH_GetAnalysisParamTextWave("name", "otherName:textwave=1|2|3")
+	CHECK_WAVE(result, NULL_WAVE)
+End
+
+Function GAPTW_Works()
+
+	WAVE/Z result = AFH_GetAnalysisParamTextWave("name", "name:textwave=a|b|c|d")
+	CHECK_EQUAL_TEXTWAVES(result, {"a", "b", "c", "d"}, mode = WAVE_DATA)
+End
+
+/// @}

commit 4ad95bba1d94e25051cbc17c4dbb010830176709

Analysis Parameters: Ignore case
We don't want to respect case for the analysis parameter names. Broken
since 2ce9231b (AFH_GetAnalysisParam*: Tighten logic and add tests, 2019-02-24).

Packages/MIES/MIES_AnalysisFunctionHelpers.ipf
@@ -581,7 +581,7 @@ End
 /// @return one if present, zero otherwise
 Function AFH_IsParameterPresent(string name, string params)
 
-	return WhichListItem(name, AFH_GetListOfAnalysisParamNames(params)) >= 0
+	return WhichListItem(name, AFH_GetListOfAnalysisParamNames(params), ";", 0, 0) >= 0
 End
 
 /// @brief Return the type of the user parameter
@@ -796,7 +796,7 @@ End
 Function AFH_IsValidAnalysisParamType(type)
 	string type
 
-	return !IsEmpty(type) && WhichListItem(type, ANALYSIS_FUNCTION_PARAMS_TYPES) != -1
+	return !IsEmpty(type) && WhichListItem(type, ANALYSIS_FUNCTION_PARAMS_TYPES, ";", 0, 0) != -1
 End
 
 /// @brief Return an user parameter's value as string
@@ -876,14 +876,14 @@ Function/S AFH_CheckAnalysisParameter(string genericFunc, STRUCT CheckParameters
 	for(i = 0; i < numParams; i += 1)
 		name = StringFromList(i, allNames)
 
-		if(WhichListItem(name, presentNames) == -1)
-			if(WhichListItem(name, optNames) != -1)
+		if(WhichListItem(name, presentNames, ";", 0, 0) == -1)
+			if(WhichListItem(name, optNames, ";", 0, 0) != -1)
 				// non present optional parameters should not be checked
 				continue
 			endif
 
 			// non present required parameters are an error
-			if(WhichListItem(name, reqNames) != -1)
+			if(WhichListItem(name, reqNames, ";", 0, 0) != -1)
 				errorMessages[index++] = name + ": is required but missing"
 				continue
 			endif

Packages/MIES/MIES_AnalysisFunctions.ipf
@@ -86,13 +86,14 @@
 /// the `WPT` wave, see GetWaveBuilderWaveTextParam() for the exact format. See
 /// TestAnalysisFunction_V3() for an example implementation.
 ///
-/// If you want to propose a list of parameters which should/must be present, define
-/// an additional function named like your analysis function but suffixed with
-/// `_GetParams` and return a comma separated list of names. Adding the
-/// type is also possible via `$name:$type` syntax. The list of parameter names and types
-/// is then checked before DAQ. The supplied names are taken to be required by
-/// default, optional parameters, and their types, must be enclosed with `[]`.
-/// The list at #ANALYSIS_FUNCTION_PARAMS_TYPES holds all valid types.
+/// If you want to propose a list of parameters which should/must be present,
+/// define an additional function named like your analysis function but
+/// suffixed with `_GetParams` and return a comma separated list of names.
+/// Adding the type is also possible via `$name:$type` syntax. The list of
+/// parameter names and types is then checked before DAQ. The supplied names
+/// are taken to be required by default, optional parameters, and their types,
+/// must be enclosed with `[]`. The list at #ANALYSIS_FUNCTION_PARAMS_TYPES
+/// holds all valid types. Case does not matter for the parameter names.
 ///
 /// The optional function `_CheckParam` allows you to validate passed
 /// parameters. In case of a valid parameter it must return an emtpy string and

Packages/MIES/MIES_DAEphys.ipf
@@ -2853,7 +2853,7 @@ static Function DAP_CheckAnalysisFunctionAndParameter(device, setName)
 		reqNames   = AFH_GetListOfAnalysisParamNames(reqNamesAndTypesFromFunc)
 		suppParams = ExtractAnalysisFunctionParams(stimSet)
 		suppNames  = AFH_GetListOfAnalysisParamNames(suppParams)
-		diff       = GetListDifference(reqNames, suppNames)
+		diff       = GetListDifference(reqNames, suppNames, caseSensitive = 0)
 		if(!IsEmpty(diff))
 			printf "(%s) The required analysis parameters requested by %s for stim set %s were not all supplied (missing are: %s)\r", device, func, setName, diff
 			ControlWindowToFront()
@@ -2877,7 +2877,7 @@ static Function DAP_CheckAnalysisFunctionAndParameter(device, setName)
 			endif
 
 			// invalid types are not allowed
-			if(WhichListItem(reqType, ANALYSIS_FUNCTION_PARAMS_TYPES) == -1)
+			if(WhichListItem(reqType, ANALYSIS_FUNCTION_PARAMS_TYPES, ";", 0, 0) == -1)
 				printf "(%s) The required analysis parameter %s for %s in stim set %s has type %s which is unknown.\r", device, reqName, func, setName, reqType
 				ControlWindowToFront()
 				return 1

Packages/tests/Basic/UTF_AnalysisFunctionHelpers.ipf
@@ -778,6 +778,7 @@ Function AIPP_Works()
 
 	CHECK(AFH_IsParameterPresent("param1", params))
 	CHECK(AFH_IsParameterPresent("param2", params))
+	CHECK(AFH_IsParameterPresent("Param2", params))
 	CHECK(!AFH_IsParameterPresent("param3", params))
 End
 

Packages/tests/HardwareBasic/UTF_AnalysisFunctionManagement.ipf
@@ -1137,7 +1137,8 @@ static Function AFT14_PreInit(device)
 	string device
 
 	string stimSet = "AnaFuncParams1_DA_0"
-	AFH_AddAnalysisParameter(stimSet, "MyVar", str = "abcd")
+	// use all lower case name for MyVar
+	AFH_AddAnalysisParameter(stimSet, "myvar", str = "abcd")
 	AFH_AddAnalysisParameter(stimSet, "MyStr", str = "abcd")
 	AFH_AddAnalysisParameter(stimSet, "MyWave", wv = {1, 2, 3})
 	Make/FREE/T textData = {"a", "b", "c"}