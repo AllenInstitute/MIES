AllenInstitute/MIES

commit 4cc6f2e94835618370c8645873b5ab3931121b44

GetSymbolicPathForDiagnosticsDirectory: Fix invalid IP version handling
Starting with IP10 the major version number is not a single character
anymore. Avoid future issues like this and use the newly added function
GetIgorProMajorVersion for that.

Packages/MIES/MIES_Utilities_File.ipf
@@ -580,7 +580,7 @@ Function/S GetSymbolicPathForDiagnosticsDirectory()
 
 	userName = GetSystemUserName()
 
-	sprintf path, "C:Users:%s:AppData:Roaming:WaveMetrics:Igor Pro %s:Diagnostics:", userName, GetIgorProVersion()[0]
+	sprintf path, "C:Users:%s:AppData:Roaming:WaveMetrics:Igor Pro %s:Diagnostics:", userName, GetIgorProMajorVersion()
 
 	if(!FolderExists(path))
 		CreateFolderOnDisk(path)

Packages/MIES/MIES_Utilities_System.ipf
@@ -158,6 +158,12 @@ threadsafe Function/S GetIgorProVersion()
 	return StringByKey("IGORFILEVERSION", GetIgorInfo(3))
 End
 
+/// @brief Return the major Igor Pro version
+threadsafe Function GetIgorProMajorVersion()
+
+	return floor(IgorVersion())
+End
+
 /// @brief Return the Igor Pro build version string
 ///
 /// This allows to distinguish different builds from the same major/minor

commit 259c9983e1b2e67534458553c1929bb559ec6578

GetSymbolicPathForDiagnosticsDirectory: Fix RTE in sprintf invocation
Broken since 4cc6f2e9 (GetSymbolicPathForDiagnosticsDirectory: Fix invalid IP version handling, 2024-07-11).

Packages/MIES/MIES_Utilities_File.ipf
@@ -580,7 +580,7 @@ Function/S GetSymbolicPathForDiagnosticsDirectory()
 
 	userName = GetSystemUserName()
 
-	sprintf path, "C:Users:%s:AppData:Roaming:WaveMetrics:Igor Pro %s:Diagnostics:", userName, GetIgorProMajorVersion()
+	sprintf path, "C:Users:%s:AppData:Roaming:WaveMetrics:Igor Pro %d:Diagnostics:", userName, GetIgorProMajorVersion()
 
 	if(!FolderExists(path))
 		CreateFolderOnDisk(path)

Packages/tests/Basic/UTF_Utils_File.ipf
@@ -23,7 +23,6 @@
 // HFSPathToPosix
 // HFSPathToWindows
 // HFSPathToNative
-// GetSymbolicPathForDiagnosticsDirectory
 // ShowDiagnosticsDirectory
 // SanitizeFilename
 // LoadWaveFromDisk
@@ -218,3 +217,14 @@ static Function GetDayOfWeekTest()
 End
 
 /// @}
+
+static Function TestGetSymbolicPathForDiagnosticsDirectory()
+
+	string symbPath
+
+	symbPath = GetSymbolicPathForDiagnosticsDirectory()
+	CHECK_PROPER_STR(symbPath)
+
+	PathInfo $symbPath
+	CHECK(V_flag)
+End