3198e57f0406d221c81db826ee74068ecbe7c709
MIES Deployment <mies-deploy@linux-mint-box.seattle>
2024-10-17 08:18:34 +0000

    Updating documentation to Release_2.8_20240925-59-ga60c890bc8

---
+++ _static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '(main) Release_2.8_20240925-59-ga60c890bc8',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: false,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
