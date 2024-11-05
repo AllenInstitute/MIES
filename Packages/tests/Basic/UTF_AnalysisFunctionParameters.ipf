AllenInstitute/MIES

commit d0961b52d65af43d0d5f1c259e7b23bf80d5f384

AFH_GetAnalysisParameter: Skip type assertion
This is not required anymore since 71322c09 (AFH_GetAnalysisParamType:
Make value and type check optional, 2018-05-13).

Packages/MIES/MIES_AnalysisFunctionHelpers.ipf
@@ -726,8 +726,9 @@ End
 Function/S AFH_GetAnalysisParameter(name, params)
 	string name, params
 
-	string type = AFH_GetAnalysisParamType(name, params)
-	ASSERT(AFH_IsValidAnalysisParamType(type), "Invalid type")
+	string type
+
+	type = AFH_GetAnalysisParamType(name, params)
 
 	return StringByKey(name + ":" + type, params, "=", ",", 0)
 End

commit d95b7db39fb31f3ea07f951c9e112651bc179c66

AFH_GetAnalysisParameter: Fix querying non-existent parameter
If the expected type is not given we need to allow querying non-existent
parameters, otherwise functions like PSQ_CR_FindDependentAnalysisParameter
always bug out.

Broken since d0961b52d (AFH_GetAnalysisParameter: Skip type assertion, 2019-12-12).

Also add some tests.

Packages/MIES/MIES_AnalysisFunctionHelpers.ipf
@@ -822,7 +822,7 @@ Function/S AFH_GetAnalysisParameter(name, params, [expectedType])
 	string type, value
 
 	if(ParamIsDefault(expectedType))
-		type = AFH_GetAnalysisParamType(name, params)
+		type = AFH_GetAnalysisParamType(name, params, typeCheck = 0)
 	else
 		type = AFH_GetAnalysisParamType(name, params, expectedType = expectedType)
 	endif

Packages/tests/Basic/UTF_AnalysisFunctionParameters.ipf
@@ -1023,3 +1023,47 @@ Function GAPasT_Works([WAVE/T wv])
 	CHECK_EQUAL_STR(result, wv[1])
 End
 /// @}
+
+/// @name AFH_GetAnalysisParameter
+/// @{
+static Function GAP_Works()
+
+	string result
+	string params = "var:variable=123,str:string=456"
+
+	result = AFH_GetAnalysisParameter("var", params)
+	CHECK_EQUAL_STR(result, "123")
+
+	result = AFH_GetAnalysisParameter("str", params, expectedType = "string")
+	CHECK_EQUAL_STR(result, "456")
+
+	// wrong type
+	try
+		AFH_GetAnalysisParameter("var1", params, expectedType = "string")
+		FAIL()
+	catch
+		CHECK_NO_RTE()
+	endtry
+
+	// invalid type
+	try
+		AFH_GetAnalysisParameter("var1", params, expectedType = "abcd")
+		FAIL()
+	catch
+		CHECK_NO_RTE()
+	endtry
+
+	// non-existent parameter but type was given
+	try
+		AFH_GetAnalysisParameter("I_DONT_EXIST", params, expectedType = "variable")
+		FAIL()
+	catch
+		CHECK_NO_RTE()
+	endtry
+
+	// empty return without type
+	result = AFH_GetAnalysisParameter("I_DONT_EXIST", params)
+	CHECK_EMPTY_STR(result)
+End
+
+/// @}