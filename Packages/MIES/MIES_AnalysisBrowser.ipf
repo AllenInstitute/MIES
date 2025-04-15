AllenInstitute/MIES

commit d310915ab0e6f72f33894c619bd93a54e9b5c09c

treewide: Fix ReadabilityMissingParenthesis for linter

Packages/MIES/MIES_AnalysisBrowser.ipf
@@ -174,7 +174,7 @@ static Function AB_RemoveMapEntry(variable index)
 	endif
 	map[index][] = ""
 
-	if(index + 1 == GetNumberFromWaveNote(map, NOTE_INDEX))
+	if((index + 1) == GetNumberFromWaveNote(map, NOTE_INDEX))
 		SetNumberInWaveNote(map, NOTE_INDEX, index)
 	endif
 End
@@ -1370,7 +1370,7 @@ static Function AB_CollapseListEntry(variable row, variable col)
 	WAVE   expBrowserSelBak  = CreateBackupWave(expBrowserSel)
 
 	mask = expBrowserSel[row][col]
-	ASSERT(mask & LISTBOX_TREEVIEW && !(mask & LISTBOX_TREEVIEW_EXPANDED), "listbox entry is not a treeview expansion node or is already collapsed")
+	ASSERT((mask & LISTBOX_TREEVIEW) && !(mask & LISTBOX_TREEVIEW_EXPANDED), "listbox entry is not a treeview expansion node or is already collapsed")
 
 	last    = AB_GetRowWithNextTreeView(expBrowserSel, row, col)
 	colSize = DimSize(expBrowserSel, COLS)
@@ -1403,14 +1403,14 @@ static Function AB_ExpandListEntry(variable row, variable col)
 	WAVE   expBrowserSelBak  = CreateBackupWave(expBrowserSel)
 
 	mask = expBrowserSel[row][col]
-	ASSERT(mask & LISTBOX_TREEVIEW && mask & LISTBOX_TREEVIEW_EXPANDED, "listbox entry is not a treeview expansion node or already expanded")
+	ASSERT((mask & LISTBOX_TREEVIEW) && (mask & LISTBOX_TREEVIEW_EXPANDED), "listbox entry is not a treeview expansion node or already expanded")
 
 	lastExpandedRow = NaN
 	last            = AB_GetRowWithNextTreeView(expBrowserSel, row, col)
 	colSize         = DimSize(expBrowserSel, COLS)
 	for(i = last - 1; i >= row; i -= 1)
 		for(j = colSize - 1; j >= col; j -= 1)
-			val = i == row && j == col ? ClearBit(mask, LISTBOX_TREEVIEW_EXPANDED) : expBrowserSel[i][j]
+			val = (i == row && j == col) ? ClearBit(mask, LISTBOX_TREEVIEW_EXPANDED) : expBrowserSel[i][j]
 			if(!(val & LISTBOX_TREEVIEW))
 				continue
 			endif
@@ -1537,7 +1537,7 @@ static Function AB_LoadFromExpandedRange(variable row, variable subSectionColumn
 			endif
 			device = ""
 		else
-			if(expBrowserSel[j][EXPERIMENT_TREEVIEW_COLUMN] & LISTBOX_TREEVIEW || expBrowserSel[j][DEVICE_TREEVIEW_COLUMN] & LISTBOX_TREEVIEW)
+			if((expBrowserSel[j][EXPERIMENT_TREEVIEW_COLUMN] & LISTBOX_TREEVIEW) || (expBrowserSel[j][DEVICE_TREEVIEW_COLUMN] & LISTBOX_TREEVIEW))
 				// ignore rows with tree view icons, we have them already in our list
 				continue
 			endif
@@ -1598,7 +1598,7 @@ static Function AB_GetRowWithNextTreeView(WAVE selWave, variable startRow, varia
 
 	numRows = DimSize(selWave, ROWS)
 	for(i = startRow + 1; i < numRows; i += 1)
-		status[] = (selWave[i][p] & LISTBOX_TREEVIEW ? 1 : 0)
+		status[] = ((selWave[i][p] & LISTBOX_TREEVIEW) ? 1 : 0)
 
 		if(Sum(status, 0, col) > 0)
 			return i
@@ -2033,7 +2033,7 @@ static Function AB_SortConfigSweeps(WAVE/I config)
 	Make/FREE/N=(numRows)/I/U valindex = p
 
 	//sort order: XOP_CHANNEL_TYPE_DAC = 1, XOP_CHANNEL_TYPE_ADC = 0, XOP_CHANNEL_TYPE_TTL = 3
-	MultiThread keyPrimary[] = config[p][%type] == XOP_CHANNEL_TYPE_ADC ? 2 : config[p][%type]
+	MultiThread keyPrimary[] = (config[p][%type] == XOP_CHANNEL_TYPE_ADC) ? 2 : config[p][%type]
 	MultiThread keySecondary[] = config[p][%number]
 	Sort/A {keyPrimary, keySecondary}, valindex
 
@@ -2523,7 +2523,7 @@ static Function AB_AddExperimentEntries(string win, WAVE/T entries)
 				ControlWindowToFront()
 				continue
 			endif
-			if(sTime < stopMSTimer(-2) * MILLI_TO_ONE)
+			if(sTime < (stopMSTimer(-2) * MILLI_TO_ONE))
 				sprintf title, "%s, Reading %s", panel, GetFile(fName)
 				DoWindow/T $panel, title
 				DoUpdate/W=$panel

commit 15eb41b55420f34991cb5dc6776180180278af1f

Bugfix: Select same stim sets sweeps not working in AB
FindIndizes asserted out because a col and a colLabel was specified at the same time.

AB broke with the check introduced in
d310915a (treewide: Fix ReadabilityMissingParenthesis for linter, 2025-02-04)

Ambiguity in AB since:
1785a7b6 (MIES_AnalysisBrowser.ipf: Only search the first column for the selection bits, 2025-01-29)

Packages/MIES/MIES_AnalysisBrowser.ipf
@@ -3245,7 +3245,7 @@ Function AB_ButtonProc_SelectStimSets(STRUCT WMButtonAction &ba) : ButtonControl
 				break
 			endif
 
-			WAVE indizes = FindIndizes(expBrowserList, col = 0, colLabel = "stim sets", str = selectedStimSet)
+			WAVE indizes = FindIndizes(expBrowserList, colLabel = "stim sets", str = selectedStimSet)
 			expBrowserSel[][] = expBrowserSel[p][q] & ~(LISTBOX_SELECT_OR_SHIFT_SELECTION)
 
 			numEntries = DimSize(indizes, ROWS)