1cdcbc80d0efdb683c37fb7a901629a130f4d947
Thomas Braun <thomas.braun@byte-physics.de>
2025-07-16 15:52:25 +0200

    changes

--- Packages/Python/ipfx_helpers.py
+++ Packages/Python/ipfx_helpers.py
@@ -6,6 +6,7 @@ import ipfx
 
 from ipfx.feature_extractor import SpikeFeatureExtractor
 
+
 def extract_spikes(t_path: str, v_path: str, out_folder: str):
 
     t_ip = igorpro.wave(t_path)
@@ -20,12 +21,14 @@ def extract_spikes(t_path: str, v_path: str, out_folder: str):
     spikes = ext.process(t, v, None)
 
     # empty list are not handled correctly, reported as #7234
-    if(spikes.empty):
+    if spikes.empty:
         return ""
 
     # creating a 2D text wave throws, reported as #7225
     resultName = "spikes_output"
-    spikes_text = igorpro.wave.create(resultName, 0, "", igorpro.WaveType.text, folder, True)
+    spikes_text = igorpro.wave.create(
+        resultName, 0, "", igorpro.WaveType.text, folder, True
+    )
 
     # written with wrong shape, already reported to WaveMetrics as #7227, use transpose as workaround
     spikes_text.set_data(spikes.to_numpy().transpose().tolist())
@@ -33,6 +36,6 @@ def extract_spikes(t_path: str, v_path: str, out_folder: str):
     # spikes.columns.array hangs in IP, reported as #7230
     col_labels = spikes.columns.tolist()
     for i in range(len(col_labels)):
-        spikes_text.set_label('y', i, col_labels[i])
+        spikes_text.set_label("y", i, col_labels[i])
 
     return resultName
