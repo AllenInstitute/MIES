AllenInstitute/MIES

commit 91e7dcda5ceb557027f825d8dca761a30f6ecb21

MIES_PackageSettings.ipf: Enhance it
The current code does not support multiple windows of the same name nor
does it save and restore external subwindows.

The first we can achieve with introducing a group for each panel and store
the name, before locking, as part of the json path.

The external subwindow position restoration is a bit more involved as we
need to get its orientation first. This is due to the fact that subwindow
coordinates are relative to [top, left] of the main window. So we can't
just apply the stored coordinates, but instead must leave some zero and
other relative.

Packages/MIES/MIES_PackageSettings.ipf
@@ -11,6 +11,7 @@
 
 static StrConstant PS_STORE_COORDINATES      = "JSONSettings_StoreCoordinates"
 static StrConstant PS_WINDOW_NAME            = "JSONSettings_WindowName"
+static StrConstant PS_WINDOW_GROUP           = "JSONSettings_WindowGroup"
 static StrConstant PS_COORDINATE_SAVING_HOOK = "windowCoordinateSaving"
 
 /// @brief Initialize the `PackageFolder` symbolic path
@@ -135,18 +136,31 @@ static Function/WAVE PS_GetAllWindowsExt(string win)
 	return result
 End
 
-/// @brief Move the window to the stored location
-static Function PS_ApplyStoredWindowCoordinate(variable JSONid, string win)
+static Function/S PS_BuildWindowPath(string win)
 
-	string path, name
-	variable left, top, right, bottom, err
+	string name, group
 
 	name = GetUserData(win, "", PS_WINDOW_NAME)
 	ASSERT(!IsEmpty(name), "Invalid empty name")
 
-	path = "/" + name + "/coordinates"
+	// only the main window has the group info
+	group = GetUserData(GetMainWindow(win), "", PS_WINDOW_GROUP)
+	if(IsEmpty(group))
+		return ""
+	endif
 
-	if(JSON_GetType(JSONid, path, ignoreErr = 1) != JSON_OBJECT)
+	return "/" + group + "/" + name + "/coordinates"
+End
+
+/// @brief Move the window to the stored location
+static Function PS_ApplyStoredWindowCoordinate(variable JSONid, string win, variable orientation)
+
+	string path
+	variable left, top, right, bottom, err
+
+	path = PS_BuildWindowPath(win)
+
+	if(IsEmpty(path) || JSON_GetType(JSONid, path, ignoreErr = 1) != JSON_OBJECT)
 		return NaN
 	endif
 
@@ -158,8 +172,25 @@ static Function PS_ApplyStoredWindowCoordinate(variable JSONid, string win)
 	AssertOnAndClearRTError()
 	try
 		if(IsSubWindow(win))
-			// correct for pixel/points confusion
-			MoveSubWindow/W=$win fnum=(0, PointsToPixel(bottom - top), PointsToPixel(right - left), 0); AbortOnRTE
+			switch(orientation)
+				// correct for pixel/points confusion
+				// left, top, right, bottom
+				// coordinates are relative to [top, left] of the main window
+				case EXT_SUBWINDOW_ORIENTATION_BOTTOM:
+					MoveSubWindow/W=$win fnum=(0, 0, PointsToPixel(right - left), PointsToPixel(bottom - top)); AbortOnRTE
+					break
+				case EXT_SUBWINDOW_ORIENTATION_TOP:
+					MoveSubWindow/W=$win fnum=(0, PointsToPixel(bottom - top), PointsToPixel(right - left), 0); AbortOnRTE
+					break
+				case EXT_SUBWINDOW_ORIENTATION_LEFT:
+					MoveSubWindow/W=$win fnum=(PointsToPixel(right - left), 0, 0, PointsToPixel(bottom - top)); AbortOnRTE
+					break
+				case EXT_SUBWINDOW_ORIENTATION_RIGHT:
+					MoveSubWindow/W=$win fnum=(0, 0, PointsToPixel(right - left), PointsToPixel(bottom - top)); AbortOnRTE
+					break
+				default:
+					FATAL_ERROR("Unknown orientation")
+			endswitch
 		else
 			MoveWindow/W=$win left, top, right, bottom; AbortOnRTE
 		endif
@@ -171,10 +202,10 @@ static Function PS_ApplyStoredWindowCoordinate(variable JSONid, string win)
 End
 
 /// @brief Add user data to mark the window as using coordinate saving
-static Function PS_RegisterForCoordinateSaving(string win, string name)
+static Function PS_RegisterForCoordinateSaving(string win)
 
 	SetWindow $win, userdata($PS_STORE_COORDINATES)="1"
-	SetWindow $win, userdata($PS_WINDOW_NAME)=name
+	SetWindow $win, userdata($PS_WINDOW_NAME)=win
 End
 
 /// @brief Remove user data related to coordinate saving
@@ -182,6 +213,7 @@ Function PS_RemoveCoordinateSaving(string win)
 
 	SetWindow $win, userdata($PS_STORE_COORDINATES)=""
 	SetWindow $win, userdata($PS_WINDOW_NAME)=""
+	SetWindow $win, userdata($PS_WINDOW_GROUP)=""
 
 	SetWindow $win, hook($PS_COORDINATE_SAVING_HOOK)=$""
 End
@@ -220,7 +252,7 @@ End
 /// The window must have been registered beforehand with PS_InitCoordinates().
 Function PS_StoreWindowCoordinate(variable JSONid, string win)
 
-	string path, name
+	string   path
 	variable store
 
 	store = str2numSafe(GetUserData(win, "", PS_STORE_COORDINATES))
@@ -233,10 +265,12 @@ Function PS_StoreWindowCoordinate(variable JSONid, string win)
 		return NaN
 	endif
 
-	name = GetUserData(win, "", PS_WINDOW_NAME)
-	ASSERT(!IsEmpty(name), "Invalid empty name")
+	path = PS_BuildWindowPath(win)
 
-	path = "/" + name + "/coordinates"
+	if(IsEmpty(path))
+		// too few userdata
+		return NaN
+	endif
 
 	if(!JSON_Exists(JSONid, path))
 		JSON_AddTreeObject(JSONid, path)
@@ -258,14 +292,16 @@ End
 /// - Read the current coordinates from the JSON settings file and applying
 ///   them
 ///
-/// @param JSONid  JSON document with settings
-/// @param win     window name
-/// @param name    name to store the window under (especially useful for
-///                windows which are renamed to their locked device)
-/// @param addHook [optional, defaults to true] Add a window hook to store the
-///                coordinates on window killing. Users with their own kill event
-///                handling must pass `addHook=0`.
-Function PS_InitCoordinates(variable JSONid, string win, string name, [variable addHook])
+/// @param JSONid    JSON document with settings
+/// @param win       window name, must be the one before locking
+/// @param recursive [optional, defaults to false] Add hook also to all exterior subwindows
+/// @param addHook   [optional, defaults to true] Add a window hook to store the
+///                  coordinates on window killing. Users with their own kill event
+///                  handling must pass `addHook=0`.
+Function PS_InitCoordinates(variable JSONid, string win, [variable recursive, variable addHook])
+
+	string subWin, mainWin, leaf
+	variable idx, orient
 
 	if(ParamIsDefault(addHook))
 		addHook = 1
@@ -273,12 +309,39 @@ Function PS_InitCoordinates(variable JSONid, string win, string name, [variable 
 		addHook = !!addHook
 	endif
 
-	PS_RegisterForCoordinateSaving(win, name)
-	PS_ApplyStoredWindowCoordinate(JSONid, win)
-
-	if(addHook)
-		SetWindow $win, hook($PS_COORDINATE_SAVING_HOOK)=StoreWindowCoordinatesHook
+	if(ParamIsDefault(recursive))
+		recursive = 0
+	else
+		recursive = !!recursive
 	endif
+
+	WAVE/T allExtSubWins = PS_GetAllWindowsExt(win)
+
+	mainWin                          = allExtSubWins[0]
+	[WAVE/T names, WAVE orientation] = GetExteriorSubWindowOrientations(WinRecreation(mainWin, 0))
+
+	for(subWin : allExtSubWins)
+
+		if(!cmpstr(subWin, mainWin))
+			orient = NaN
+		else
+			leaf = LastStringFromList(subWin, sep = "#")
+			idx  = GetRowIndex(names, str = leaf)
+			ASSERT(IsFinite(idx), "Could not find subwindow")
+			orient = orientation[idx]
+		endif
+
+		PS_RegisterForCoordinateSaving(subWin)
+		PS_ApplyStoredWindowCoordinate(JSONid, subWin, orient)
+
+		if(addHook)
+			SetWindow $subWin, hook($PS_COORDINATE_SAVING_HOOK)=StoreWindowCoordinatesHook
+		endif
+
+		if(!recursive)
+			break
+		endif
+	endfor
 End
 
 /// @brief Write the current JSON settings to disc

Packages/MIES/MIES_AnalysisBrowser.ipf
@@ -2812,7 +2812,7 @@ Function/S AB_OpenAnalysisBrowser([variable restoreSettings])
 	WAVE   sel  = GetExperimentBrowserGUISel()
 	ListBox list_experiment_contents, win=$panel, listWave=list, selWave=sel
 
-	PS_InitCoordinates(JSONid, panel, "analysisbrowser")
+	PS_InitCoordinates(JSONid, panel)
 	SetWindow $panel, hook(cleanup)=AB_WindowHook
 
 	if(restoreSettings)

Packages/MIES/MIES_AnalysisBrowser_Macro.ipf
@@ -170,6 +170,7 @@ Window AnalysisBrowser() : Panel
 	SetWindow kwTopWin, hook(ResizeControls)=ResizeControlsSafe
 	SetWindow kwTopWin, hook(cleanup)=AB_WindowHook
 	SetWindow kwTopWin, userdata(Config_PanelType)="AnalysisBrowser"
+	SetWindow kwTopWin, userdata(JSONSettings_WindowGroup)="analysisbrowser"
 	SetWindow kwTopWin, userdata(ResizeControlsGuides)="splitGuide;UGVL;"
 	SetWindow kwTopWin, userdata(ResizeControlsInfosplitGuide)="NAME:splitGuide;WIN:AnalysisBrowser;TYPE:User;HORIZONTAL:1;POSITION:10.00;GUIDE1:FT;GUIDE2:;RELPOSITION:10;"
 	SetWindow kwTopWin, userdata(ResizeControlsInfoUGVL)="NAME:UGVL;WIN:AnalysisBrowser;TYPE:User;HORIZONTAL:0;POSITION:15.00;GUIDE1:FL;GUIDE2:;RELPOSITION:15;"

Packages/MIES/MIES_BrowserSettingsPanel.ipf
@@ -220,7 +220,7 @@ Function BSP_DynamicStartupSettings(string mainPanel)
 	bsPanel = BSP_GetPanel(mainPanel)
 
 	NVAR JSONid = $GetSettingsJSONid()
-	PS_InitCoordinates(JSONid, mainPanel, "datasweepbrowser")
+	PS_InitCoordinates(JSONid, mainPanel)
 
 	PopupMenu popup_overlaySweeps_select, win=$bsPanel, value=#("OVS_GetSweepSelectionChoices(\"" + bsPanel + "\")")
 

Packages/MIES/MIES_DAEphys.ipf
@@ -4172,7 +4172,7 @@ Function/S DAP_CreateDAEphysPanel()
 	AddVersionToPanel(panel, DA_EPHYS_PANEL_VERSION)
 
 	NVAR JSONid = $GetSettingsJSONid()
-	PS_InitCoordinates(JSONid, panel, "daephys")
+	PS_InitCoordinates(JSONid, panel)
 
 	return panel
 End

Packages/MIES/MIES_DAEphys_Macro.ipf
@@ -4344,4 +4344,5 @@ Window DA_Ephys() : Panel
 	SetWindow kwTopWin, userdata(ResizeControlsInfoUGV1)="NAME:UGV1;WIN:DA_Ephys;TYPE:User;HORIZONTAL:0;POSITION:481.00;GUIDE1:FL;GUIDE2:;RELPOSITION:481;"
 	SetWindow kwTopWin, userdata(Config_PanelType)="DA_Ephys"
 	SetWindow kwTopWin, userdata(Config_RadioCouplingFunc)="DAP_GetRadioButtonCoupling"
+	SetWindow kwTopWin, userdata(JSONSettings_WindowGroup)="daephys"
 EndMacro

Packages/MIES/MIES_DataBrowser_Macro.ipf
@@ -29,6 +29,7 @@ Window DataBrowser() : Graph
 	SetWindow kwTopWin, userdata(ResizeControlsInfo)+=A"zzzzzzzzzzzzzzzzzzz!!!"
 	SetWindow kwTopWin, userdata(BROWSERMODE)="User"
 	SetWindow kwTopWin, userdata(SweepFormulaContentCRC)="1827768143"
+	SetWindow kwTopWin, userdata(JSONSettings_WindowGroup)="databrowser"
 	Execute/Q/Z "SetWindow kwTopWin sizeLimit={366,279,inf,inf}" // sizeLimit requires Igor 7 or later
 	NewPanel/HOST=#/EXT=2/W=(0, 0, 580, 70) as "Sweep Control"
 	Button button_SweepControl_NextSweep, pos={330.00, 3.00}, size={150.00, 36.00}, proc=BSP_ButtonProc_ChangeSweep

Packages/MIES/MIES_Downsample.ipf
@@ -362,6 +362,7 @@ Function CreateDownsamplePanel()
 	NewPanel/N=$panel/W=(283, 389, 847, 643)/K=1
 	ASSERT(CmpStr(panel, S_name) == 0, "window already exists")
 	SetWindow $panel, hook(cleanup)=DownsampleWindowHook
+	SetWindow kwTopWin, userdata(JSONSettings_WindowGroup)="downsample"
 
 	PopupMenu popup_deviceselection_id, pos={28, 13}, size={214, 21}, bodyWidth=130, proc=PopupMenuDeviceSelection, title="Device Selection"
 	PopupMenu popup_deviceselection_id, mode=1, value=#"GetPopupMenuDeviceListWithData()"
@@ -423,7 +424,7 @@ Function CreateDownsamplePanel()
 	GroupBox group2, pos={11, 190}, size={253, 32}
 
 	NVAR JSONid = $GetSettingsJSONid()
-	PS_InitCoordinates(JSONid, panel, "downsample", addHook = 0)
+	PS_InitCoordinates(JSONid, panel, addHook = 0)
 
 	UpdatePanel(panel)
 	UpdatePopupMenuWindowFunction(panel)

commit 343604aafba886e6ed1c96ab267ad74789966e99

PS_RemoveCoordinateSaving: Don't remove the window group
We need that to be set. Bug introduced in 91e7dcda5c
(MIES_PackageSettings.ipf: Enhance it, 2025-07-23).

Packages/MIES/MIES_PackageSettings.ipf
@@ -213,7 +213,6 @@ Function PS_RemoveCoordinateSaving(string win)
 
 	SetWindow $win, userdata($PS_STORE_COORDINATES)=""
 	SetWindow $win, userdata($PS_WINDOW_NAME)=""
-	SetWindow $win, userdata($PS_WINDOW_GROUP)=""
 
 	SetWindow $win, hook($PS_COORDINATE_SAVING_HOOK)=$""
 End