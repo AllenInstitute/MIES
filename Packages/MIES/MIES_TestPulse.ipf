AllenInstitute/MIES

commit abe6323227b47fda60751071cc89b52fd4dda88e

Auto TP: Publish results via zeromq

Packages/MIES/MIES_TestPulse.ipf
@@ -239,6 +239,8 @@ Function TP_ROAnalysis(dfr, err, errmsg)
 		TPResults[%AutoTPBaselineRangeExceeded][] = NaN
 		TPResults[%AutoTPBaselineFitResult][]     = NaN
 
+		MultiThread TPResults[%AutoTPDeltaV][] = TPResults[%ElevatedSteadyState][q] - TPResults[%BaselineSteadyState][q]
+
 		TP_AutoAmplitudeAndBaseline(panelTitle, TPResults, marker)
 		DQ_ApplyAutoBias(panelTitle, TPResults)
 		TP_RecordTP(panelTitle, TPResults, now, marker)
@@ -569,7 +571,7 @@ static Function TP_AutoAmplitudeAndBaseline(string panelTitle, WAVE TPResults, v
 			indizes[i] += 1
 			SetStringInWaveNote(overrideResults, "Next unread index [amplitude]", NumericWaveToList(indizes, ","))
 		else
-			voltage = (TPResults[%ElevatedSteadyState][i] - TPResults[%BaselineSteadyState][i]) * 1e-3
+			voltage = TPResults[%AutoTPDeltaV][i] * 1e-3
 		endif
 
 		skipAutoBaseline = 0
@@ -662,6 +664,8 @@ static Function TP_AutoTPTurnOff(string panelTitle, WAVE autoTPEnable, variable 
 
 	QC = !!QC
 
+	TP_PublishAutoTPResult(panelTitle, headstage, QC)
+
 	TP_AutoTPGenerateNewCycleID(panelTitle, headstage = headstage)
 
 	WAVE TPSettingsLBN = GetTPSettingsLabnotebook(panelTitle)
@@ -727,6 +731,64 @@ static Function TP_AutoDisableIfFinished(string panelTitle, WAVE TPStorage)
 
 		TP_RestartTestPulse(panelTitle, TPState)
 	endif
+
+	if(needsUpdate)
+		DAP_TPSettingsToGUI(panelTitle, entry = "autoTPEnable")
+	endif
+End
+
+static Function TP_PublishAutoTPResult(string panelTitle, variable headstage, variable result)
+
+	variable jsonID, err
+	string payload, path
+
+	WAVE TPSettings = GetTPSettings(panelTitle)
+	WAVE TPStorage = GetTPStorage(panelTitle)
+
+	WAVE/Z autoTPDeltaV = TP_GetValuesFromTPStorage(TPStorage, headstage, "AutoTPDeltaV", 1)
+	ASSERT(WaveExists(autoTPDeltaV), "Missing auto TP delta V")
+
+	jsonID = FFI_GetJSONTemplate(panelTitle, headstage)
+	JSON_AddTreeObject(jsonID, "results")
+	JSON_AddBoolean(jsonID, "results/QC", result)
+
+	path = "results/amplitude"
+	JSON_AddTreeObject(jsonID, path)
+	JSON_AddVariable(jsonID, path + "/value", TPSettings[%amplitudeIC][headstage])
+	JSON_AddString(jsonID, path + "/unit", "mV")
+
+	path = "results/baseline"
+	JSON_AddTreeObject(jsonID, path)
+	JSON_AddVariable(jsonID, path + "/value", TPSettings[%baselinePerc][INDEP_HEADSTAGE])
+	JSON_AddString(jsonID, path + "/unit", "%")
+
+	path = "results/amplitude IC"
+	JSON_AddTreeObject(jsonID, path)
+	JSON_AddVariable(jsonID, path + "/value", TPSettings[%amplitudeIC][headstage])
+	JSON_AddString(jsonID, path + "/unit", "pA")
+
+	path = "results/amplitude VC"
+	JSON_AddTreeObject(jsonID, path)
+	JSON_AddVariable(jsonID, path + "/value", TPSettings[%amplitudeVC][headstage])
+	JSON_AddString(jsonID, path + "/unit", "mV")
+
+	path = "results/delta V"
+	JSON_AddTreeObject(jsonID, path)
+	JSON_AddVariable(jsonID, path + "/value", autoTPDeltaV[0])
+	JSON_AddString(jsonID, path + "/unit", "mV")
+
+	payload = JSON_Dump(jsonID, indent = 2)
+	JSON_Release(jsonID)
+
+	DEBUGPRINT(payload)
+
+	AssertOnAndClearRTError()
+	try
+		zeromq_pub_send(AUTO_TP_FILTER, payload); AbortOnRTE
+	catch
+		err = ClearRTError()
+		BUG("Could not publish auto TP results " + num2str(err))
+	endtry
 End
 
 
@@ -1060,6 +1122,7 @@ static Function TP_RecordTP(panelTitle, TPResults, now, tpMarker)
 	TPStorage[count][][%AutoTPBaseline]              = TPResults[%AutoTPBaseline][q]
 	TPStorage[count][][%AutoTPBaselineRangeExceeded] = TPResults[%AutoTPBaselineRangeExceeded][q]
 	TPStorage[count][][%AutoTPBaselineFitResult]     = TPResults[%AutoTPBaselineFitResult][q]
+	TPStorage[count][][%AutoTPDeltaV]                = TPResults[%AutoTPDeltaV][q]
 
 	WAVE TPSettings = GetTPSettings(panelTitle)
 	TPStorage[count][][%AutoTPCycleID] = hsProp[q][%Enabled] ? TPSettings[%autoTPCycleID][q] : NaN

Packages/MIES/MIES_Constants.ipf
@@ -1570,6 +1570,8 @@ Constant LBN_TEXTUAL_VALUES   = 0x8
 StrConstant PRESSURE_STATE_FILTER  = "pressure:state"
 StrConstant PRESSURE_SEALED_FILTER = "pressure:sealed"
 
+StrConstant AUTO_TP_FILTER = "testpulse:autotune result"
+
 /// which is sufficient to represent each sample point time with a distinctive number up to rates of 10 MHz.
 Constant EPOCHTIME_PRECISION = 7
 

Packages/MIES/MIES_ForeignFunctionInterface.ipf
@@ -76,7 +76,7 @@ End
 /// @sa FFI_GetJSONTemplate
 Function/WAVE FFI_GetAvailableMessageFilters()
 
-	Make/FREE/T wv = {ZeroMQ_HEARTBEAT, IVS_PUB_FILTER, PRESSURE_STATE_FILTER, PRESSURE_SEALED_FILTER}
+	Make/FREE/T wv = {ZeroMQ_HEARTBEAT, IVS_PUB_FILTER, PRESSURE_STATE_FILTER, PRESSURE_SEALED_FILTER, AUTO_TP_FILTER}
 
 	Note/K wv "Heartbeat is sent every 5 seconds."
 

Packages/MIES/MIES_WaveDataFolderGetters.ipf
@@ -2376,6 +2376,7 @@ End
 /// - 28: Auto TP Cycle ID: Unique number which is constant until the "Auto TP"
 ///       state is switched (aka on->off or off->on)
 /// - 29: Auto TP Baseline Fit result: One of @ref TPBaselineFitResults
+/// - 30: Auto TP Delta V [mV]
 Function/Wave GetTPStorage(panelTitle)
 	string panelTitle
 
@@ -2387,7 +2388,7 @@ Function/Wave GetTPStorage(panelTitle)
 	if(ExistsWithCorrectLayoutVersion(wv, versionOfNewWave))
 		return wv
 	elseif(WaveExists(wv))
-		Redimension/N=(-1, NUM_HEADSTAGES, 30)/D wv
+		Redimension/N=(-1, NUM_HEADSTAGES, 31)/D wv
 
 		if(WaveVersionIsSmaller(wv, 10))
 			wv[][][17]    = NaN
@@ -2404,10 +2405,10 @@ Function/Wave GetTPStorage(panelTitle)
 			wv[][][24] = NaN
 		endif
 		if(WaveVersionIsSmaller(wv, 15))
-			wv[][][25, 29] = NaN
+			wv[][][25, 30] = NaN
 		endif
 	else
-		Make/N=(MINIMUM_WAVE_SIZE_LARGE, NUM_HEADSTAGES, 30)/D dfr:TPStorage/Wave=wv
+		Make/N=(MINIMUM_WAVE_SIZE_LARGE, NUM_HEADSTAGES, 31)/D dfr:TPStorage/Wave=wv
 
 		wv = NaN
 
@@ -2446,6 +2447,7 @@ Function/Wave GetTPStorage(panelTitle)
 	SetDimLabel LAYERS, 27, AutoTPBaselineRangeExceeded , wv
 	SetDimLabel LAYERS, 28, AutoTPCycleID               , wv
 	SetDimLabel LAYERS, 29, AutoTPBaselineFitResult     , wv
+	SetDimLabel LAYERS, 30, AutoTPDeltaV                , wv
 
 	SetNumberInWaveNote(wv, AUTOBIAS_LAST_INVOCATION_KEY, 0)
 	SetNumberInWaveNote(wv, DIMENSION_SCALING_LAST_INVOC, 0)
@@ -2580,6 +2582,7 @@ End
 /// - 6: Auto TP Baseline: Pass/Fail
 /// - 7: Auto TP Baseline range exceeded: True/False
 /// - 8: Auto TP Baseline fit result: One of @ref TPBaselineFitResults
+/// - 9: Auto TP Delta V: [mV]
 ///
 /// Columns:
 /// - NUM_HEADSTAGES
@@ -2592,10 +2595,10 @@ Function/Wave GetTPResults(string panelTitle)
 	if(ExistsWithCorrectLayoutVersion(wv, version))
 		return wv
 	elseif(WaveExists(wv))
-		Redimension/D/N=(9, NUM_HEADSTAGES) wv
+		Redimension/D/N=(10, NUM_HEADSTAGES) wv
 		wv = NaN
 	else
-		Make/D/N=(9, NUM_HEADSTAGES) dfr:results/Wave=wv
+		Make/D/N=(10, NUM_HEADSTAGES) dfr:results/Wave=wv
 		wv = NaN
 
 		// initialize with the old 1D waves
@@ -2611,7 +2614,7 @@ Function/Wave GetTPResults(string panelTitle)
 		KillOrMoveToTrash(wv = SSResistance)
 	endif
 
-	SetDimensionLabels(wv, "ResistanceInst;BaselineSteadyState;ResistanceSteadyState;ElevatedSteadyState;ElevatedInst;AutoTPAmplitude;AutoTPBaseline;AutoTPBaselineRangeExceeded;AutoTPBaselineFitResult", ROWS)
+	SetDimensionLabels(wv, "ResistanceInst;BaselineSteadyState;ResistanceSteadyState;ElevatedSteadyState;ElevatedInst;AutoTPAmplitude;AutoTPBaseline;AutoTPBaselineRangeExceeded;AutoTPBaselineFitResult;AutoTPDeltaV", ROWS)
 
 	SetWaveVersion(wv, version)
 

commit 7985b799bde710313ff2f0f70c198e0f07e7e7fd

TP_AutoTPDisableIfAppropriate: Remove duplicated code
We already call DAP_TPSettingsToGUI a few lines above.

Bug introduced in abe6323227 (Auto TP: Publish results via zeromq, 2021-11-12).

Packages/MIES/MIES_TestPulse.ipf
@@ -955,10 +955,6 @@ Function TP_AutoTPDisableIfAppropriate(string device, WAVE TPStorage, [variable 
 		return TPState
 	endif
 
-	if(needsUpdate)
-		DAP_TPSettingsToGUI(device, entry = "autoTPEnable")
-	endif
-
 	return NaN
 End
 