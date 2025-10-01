2d00042a35e3a2ba0ad1f7ba3882ac13a3005201
MIES Deployment <mies-deploy@linux-mint-box.seattle>
2025-10-01 12:42:49 +0000

    Updating documentation to Release_2.9_20250502-374-gf9560fe8f

---
+++ _static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '(main) Release_2.9_20250502-374-gf9560fe8f',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: false,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
