AllenInstitute/MIES

commit fae09cb433abc8cd3249b78b8bad525595ada31a

AI_FindConnectedAmpsNoCache: Factor it out and cache the result
This makes calling AI_FindConnectedAmps() much cheaper. We still preserve
the rescanning of the hardware via the "Query connected Amp(s)" button.

Packages/MIES/MIES_AmplifierInteraction.ipf
@@ -2120,8 +2120,45 @@ Function AI_QueryGainsFromMCC(string device)
 	return numConnAmplifiers
 End
 
+Function AI_FindConnectedAmps([variable rescanHardware])
+
+	string key
+
+	if(ParamIsDefault(rescanHardware))
+		rescanHardware = 0
+	else
+		rescanHardware = !!rescanHardware
+	endif
+
+	key = CA_AmplifierHardwareWavesKey()
+
+	if(rescanHardware)
+		IH_RemoveAmplifierConnWaves()
+		CA_DeleteCacheEntry(key)
+	endif
+
+	WAVE telegraphServers = GetAmplifierTelegraphServers()
+	WAVE ampMCC           = GetAmplifierMultiClamps()
+
+	if(DimSize(telegraphServers, ROWS) == 0 || DimSize(ampMCC, ROWS) == 0)
+
+		WAVE/Z/WAVE cache = CA_TryFetchingEntryFromCache(key)
+
+		if(WaveExists(cache))
+			Duplicate/O cache[0], telegraphServers
+			Duplicate/O cache[1], ampMCC
+		else
+			[WAVE telegraphServers, WAVE ampMCC] = AI_FindConnectedAmpsNoCache()
+
+			Make/FREE/WAVE cache = {telegraphServers, ampMCC}
+
+			CA_StoreEntryIntoCache(key, cache)
+		endif
+	endif
+End
+
 /// @brief Create the amplifier connection waves
-Function AI_FindConnectedAmps()
+static Function [WAVE telegraphServers, WAVE ampMCC] AI_FindConnectedAmpsNoCache()
 
 	string list
 
@@ -2135,6 +2172,7 @@ Function AI_FindConnectedAmps()
 	SortColumns/DIML/KNDX={0, 1} sortWaves={telegraphServers}
 
 	MCC_FindServers/Z=1
+	WAVE ampMCC = GetAmplifierMultiClamps()
 
 	SetDataFolder saveDFR
 
@@ -2146,6 +2184,8 @@ Function AI_FindConnectedAmps()
 	endif
 
 	LOG_AddEntry(PACKAGE_MIES, "amplifiers", keys = {"list"}, values = {list})
+
+	return [telegraphServers, ampMCC]
 End
 
 Function [STRUCT AxonTelegraph_DataStruct tds] AI_GetTelegraphStruct(variable axonSerial, variable channel)
@@ -2243,7 +2283,7 @@ Function AI_QueryGainsFromMCC(string device)
 	DEBUGPRINT("Unimplemented")
 End
 
-Function AI_FindConnectedAmps()
+Function AI_FindConnectedAmps([variable rescanHardware])
 
 	DEBUGPRINT("Unimplemented")
 End

Packages/MIES/MIES_Cache.ipf
@@ -84,6 +84,11 @@ Function/S CA_DACDevicesKey(variable hardwareType)
 	return str
 End
 
+Function/S CA_AmplifierHardwareWavesKey()
+
+	return "Amplifier hardware waves: Version 1"
+End
+
 /// @brief Cache key generator for recreated epochs wave
 Function/S CA_KeyRecreatedEpochs(WAVE numericalValues, WAVE/T textualValues, DFREF sweepDFR, variable sweepNo)
 

Packages/MIES/MIES_DAEphys.ipf
@@ -1791,7 +1791,7 @@ Function DAP_ButtonCtrlFindConnectedAmps(STRUCT WMButtonAction &ba) : ButtonCont
 
 	switch(ba.eventcode)
 		case 2: // mouse up
-			AI_FindConnectedAmps()
+			AI_FindConnectedAmps(rescanHardware = 1)
 			break
 		default:
 			break

commit 374fdb8b313ac73ce886d64cba866da6a130e2b8

AI_GetMCCSerialNumbers: Rescan hardware
This function is used to fetch newly reachable amplifiers after opening the
MultiClamp Commander. So we need to rescan the hardware.

Bug introduced in fae09cb4 (AI_FindConnectedAmpsNoCache: Factor it out and
cache the result, 2025-10-01).

Packages/MIES/MIES_AmplifierInteraction.ipf
@@ -813,7 +813,7 @@ End
 /// @return a 1D FREE wave containing amplifier serial numbers without leading zeroes
 static Function/WAVE AI_GetMCCSerialNumbers()
 
-	AI_FindConnectedAmps()
+	AI_FindConnectedAmps(rescanHardware = 1)
 	WAVE W_TelegraphServers = GetAmplifierTelegraphServers()
 	Duplicate/FREE/R=[][FindDimLabel(W_TelegraphServers, COLS, "SerialNum")] W_TelegraphServers, OpenMCCList
 	return GetUniqueEntries(OpenMCCList)