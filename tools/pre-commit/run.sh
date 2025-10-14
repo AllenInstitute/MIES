AllenInstitute/MIES

commit f5cd00d20af66051836103af2a47b1eedabe5df4

CI: Add pre-commit hooks

tools/pre-commit/run.sh
@@ -1,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+# checks for correct installation
+if [ ! $(docker -v | grep -c -w version) -eq 1 ]; then
+	echo "docker not found."
+	exit 1
+fi
+if [ ! $(groups | grep -c -w docker) -eq 1 ]; then
+	echo "add current user $(whoami) to docker group!"
+	exit 1
+fi
+
+top_level=$(git rev-parse --show-toplevel)
+
+# build containter
+echo "##[group]Build Docker container 'pre-commit'"
+docker build                    \
+    --build-arg UID=$(id -u)    \
+    --build-arg GID=$(id -g)    \
+    -t pre-commit               \
+    $top_level/tools/pre-commit
+echo "##[endgroup]"
+
+echo "##[group]Running pre-commit"
+docker run --rm -u $(id -u):$(id -g)                  \
+    --workdir=$HOME/repo                              \
+    -v "$(pwd)/.cache:$HOME/.cache:rw"                \
+    -v "$top_level:$HOME/repo" pre-commit             \
+    pre-commit run --all-files --show-diff-on-failure
+echo "##[endgroup]"

.github/workflows/build-pr.yml
@@ -73,6 +73,15 @@ jobs:
         uses: actions/checkout@v3
       - name: Code Checks
         run: tools/check-code.sh
+        # straight from the documentation, see https://pre-commit.com/#github-actions-example
+      - name: set PY
+        run: echo "PY=$(python -VV | sha256sum | cut -d' ' -f1)" >> $GITHUB_ENV
+      - uses: actions/cache@v1
+        with:
+          path: ~/.cache/pre-commit
+          key: pre-commit|${{ env.PY }}|${{ hashFiles('.pre-commit-config.yaml') }}
+      - name: Pre commit
+        run: tools/pre-commit/run.sh
 
   CompilationTest:
     name: |

.pre-commit-config.yaml
@@ -1,0 +1,33 @@
+# See https://pre-commit.com for more information
+# See https://pre-commit.com/hooks.html for more hooks
+exclude: |
+         (?x) # verbose re
+         ( # start alternations
+           # not our code
+           ^tools/installer/nsis|
+           ^tools/installer/Shelllink|
+           ^tools/installer/AccessControl|
+           # patch and svg files always have trailing whitespace
+           .*patch$|
+           .*svg$
+         ) # end alternations
+repos:
+- repo: https://github.com/pre-commit/pre-commit-hooks
+  rev: v4.3.0
+  hooks:
+  - id: check-added-large-files
+  - id: fix-byte-order-marker
+  - id: check-case-conflict
+  - id: check-executables-have-shebangs
+  - id: check-json
+  - id: check-merge-conflict
+  - id: check-shebang-scripts-are-executable
+  - id: check-symlinks
+  - id: check-yaml
+  - id: destroyed-symlinks
+  - id: mixed-line-ending
+  - id: trailing-whitespace
+- repo: https://github.com/sirosen/texthooks
+  rev: 0.4.0
+  hooks:
+  - id: forbid-bidi-controls

tools/pre-commit/Dockerfile
@@ -1,0 +1,25 @@
+FROM debian:bullseye
+MAINTAINER Thomas Braun thomas.braun@byte-physics.de
+
+ENV DEBIAN_FRONTEND=noninteractive
+
+RUN apt-get update &&        \
+    apt-get install --yes    \
+        git                  \
+        python3-minimal      \
+        python3-pip          \
+        python-is-python3 && \
+    apt-get clean
+
+RUN pip3 install pre-commit==2.20.0
+
+ARG UID
+ARG GID
+
+# add normal user
+RUN groupadd -g $GID ci
+RUN useradd -u $UID -g $GID -ms /bin/bash ci
+
+USER ci
+
+WORKDIR /home/ci

commit ea75e8677b40083e8eb0b0cbff9a983b6f1d15fb

tools/pre-commit/run.sh: Use the correct path to the .cache folder
We can't use $HOME here as we need to think in terms of the home folder of
the user in the container.

Broken since f5cd00d20a (CI: Add pre-commit hooks, 2023-06-01).

tools/pre-commit/run.sh
@@ -14,6 +14,8 @@ fi
 
 top_level=$(git rev-parse --show-toplevel)
 
+container_home="/home/ci"
+
 # build containter
 echo "##[group]Build Docker container 'pre-commit'"
 docker build                     \
@@ -24,9 +26,9 @@ docker build                     \
 echo "##[endgroup]"
 
 echo "##[group]Running pre-commit"
-docker run --rm -u $(id -u):$(id -g)                  \
-    --workdir=$HOME/repo                              \
-    -v "$(pwd)/.cache:$HOME/.cache:rw"                \
-    -v "$top_level:$HOME/repo" pre-commit             \
+docker run --rm -u $(id -u):$(id -g)                \
+    --workdir=$container_home/repo                  \
+    -v "$(pwd)/.cache:$container_home/.cache:rw"    \
+    -v "$top_level:$container_home/repo" pre-commit \
     pre-commit run --all-files --show-diff-on-failure
 echo "##[endgroup]"