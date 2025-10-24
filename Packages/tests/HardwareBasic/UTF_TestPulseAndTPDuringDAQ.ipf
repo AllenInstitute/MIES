AllenInstitute/MIES

commit 6c7cc38f24ebeb19d920dae9b3e25c8ea8f74f4c

PUB_TPResult: Add amplifier settings as well

Packages/MIES/MIES_Publish.ipf
@@ -690,6 +690,42 @@ threadsafe static Function PUB_AddValueWithUnit(variable jsonId, string path, va
 	JSON_AddString(jsonID, path + "/unit", unit)
 End
 
+threadsafe static Function PUB_AppendAmplifierSettings(variable jsonID, string path, variable clampMode, WAVE ampParamStorageSlice)
+
+	variable idx, value, nameIndex
+	string name, unit
+
+	WAVE funcs = AI_GetFunctionConstantForClampMode(clampMode)
+
+	Make/FREE/T/N=(DimSize(funcs, ROWS)) names
+
+	for(func : funcs)
+
+		name = AI_MapFunctionConstantToName(func, clampMode)
+		idx  = FindDimLabel(ampParamStorageSlice, ROWS, name)
+
+		if(idx < 0)
+			continue
+		endif
+
+		if(GetRowIndex(names, str = name) >= 0)
+			// some function constants map to the same name
+			continue
+		endif
+
+		names[nameIndex] = name
+		nameIndex       += 1
+
+		value = ampParamStorageSlice[idx]
+		unit  = AI_GetUnitForFunctionConstant(func, clampMode)
+		if(JSON_Exists(jsonID, path + "/" + name))
+			ASSERT_TS(0, "dup: " + name)
+		endif
+
+		PUB_AddValueWithUnit(jsonID, path + "/" + name, value, unit)
+	endfor
+End
+
 /// Filter: #ZMQ_FILTER_TPRESULT_NOW
 /// Filter: #ZMQ_FILTER_TPRESULT_1S
 /// Filter: #ZMQ_FILTER_TPRESULT_5S
@@ -702,6 +738,17 @@ End
 /// .. code-block:: json
 ///
 ///    {
+///    {
+///      "amplifier": {
+///        "HoldingPotential": {
+///          "unit": "mV",
+///          "value": 123
+///        },
+///        "HoldingPotentialEnable": {
+///          "unit": "On/Off",
+///          "value": 1
+///        }
+///      },
 ///      "properties": {
 ///        "baseline fraction": {
 ///          "unit": "%",
@@ -786,7 +833,7 @@ End
 ///    }
 ///
 /// \endrst
-threadsafe Function PUB_TPResult(string device, WAVE tpData, WAVE additionalData)
+threadsafe Function PUB_TPResult(string device, WAVE tpData, WAVE ampParamStorageSlice, WAVE additionalData)
 
 	string path
 	variable jsonId = JSON_New()
@@ -822,6 +869,10 @@ threadsafe Function PUB_TPResult(string device, WAVE tpData, WAVE additionalData
 	PUB_AddTPResultEntry(jsonId, path + "/instantaneous", tpData[%ELEVATED_INST], adUnit)
 	PUB_AddTPResultEntry(jsonId, path + "/steady state resistance", tpData[%STEADYSTATERES], "MΩ")
 	PUB_AddTPResultEntry(jsonId, path + "/instantaneous resistance", tpData[%INSTANTRES], "MΩ")
+
+	path = "amplifier"
+	JSON_AddTreeObject(jsonID, path)
+	PUB_AppendAmplifierSettings(jsonID, path, tpData[%CLAMPMODE], ampParamStorageSlice)
 
 	PUB_Publish(jsonID, ZMQ_FILTER_TPRESULT_NOW, releaseJSON = 0)
 	PUB_Publish(jsonID, ZMQ_FILTER_TPRESULT_NOW_WITH_DATA, releaseJSON = 0, additionalData = additionalData)

Packages/tests/HardwareBasic/UTF_TestPulseAndTPDuringDAQ.ipf
@@ -1424,6 +1424,17 @@ static Function TPZerosDAC_REENTRY([STRUCT IUTF_MDATA &md])
 	CHECK_LE_VAR(HW_ReadADC(hardwareType, deviceID, ADC), 0.01)
 End
 
+static Function TestTPPublishing_preAcq(string device)
+
+	// HS 0
+	PGC_SetAndActivateControl(device, "slider_DataAcq_ActiveHeadstage", val = 0)
+	PGC_SetAndActivateControl(device, "setvar_DataAcq_Hold_IC", val = 1.23)
+
+	// HS 1
+	PGC_SetAndActivateControl(device, "slider_DataAcq_ActiveHeadstage", val = 1)
+	PGC_SetAndActivateControl(device, "setvar_DataAcq_Hold_VC", val = 4.56)
+End
+
 /// UTF_TD_GENERATOR DeviceNameGeneratorMD1
 static Function TestTPPublishing([string str])
 
@@ -1445,7 +1456,7 @@ End
 
 static Function TestTPPublishing_REENTRY([string str])
 
-	variable sweepNo, jsonId, var, index, dimMarker, headstage, i, foundHS0, foundHS1
+	variable sweepNo, jsonId, var, index, dimMarker, headstage, i, foundHS0, foundHS1, clampMode
 	string filter, stv, adUnit, daUnit
 
 	CHECK_EQUAL_VAR(GetSetVariable(str, "SetVar_Sweep"), 0)
@@ -1476,8 +1487,9 @@ static Function TestTPPublishing_REENTRY([string str])
 
 		stv = JSON_GetString(jsonID, "/properties/device")
 		CHECK_EQUAL_STR(stv, str)
-		var = JSON_GetVariable(jsonID, "/properties/clamp mode")
-		CHECK_EQUAL_VAR(var, tpStorage[index][headstage][%ClampMode])
+		var       = JSON_GetVariable(jsonID, "/properties/clamp mode")
+		clampMode = tpStorage[index][headstage][%ClampMode]
+		CHECK_EQUAL_VAR(var, clampMode)
 		daUnit = GetDAChannelUnit(var)
 		adUnit = GetADChannelUnit(var)
 
@@ -1561,6 +1573,16 @@ static Function TestTPPublishing_REENTRY([string str])
 		CHECK_EQUAL_VAR(var, tpStorage[index][headstage][%PeakResistance])
 		stv = JSON_GetString(jsonID, "/results/instantaneous resistance/unit")
 		CHECK_EQUAL_STR(stv, "MΩ")
+
+		if(clampMode == I_CLAMP_MODE)
+			var = JSON_GetVariable(jsonID, "/amplifier/BiasCurrent/value")
+			CHECK_EQUAL_VAR(var, 1.23)
+		elseif(clampMode == V_CLAMP_MODE)
+			var = JSON_GetVariable(jsonID, "/amplifier/HoldingPotential/value")
+			CHECK_EQUAL_VAR(var, 4.56)
+		else
+			FAIL()
+		endif
 	endfor
 
 	for(i = 0; i < 10; i += 1)

Packages/MIES/MIES_TestPulse.ipf
@@ -1034,6 +1034,7 @@ threadsafe Function/DF TP_TSAnalysis(DFREF dfrInp)
 	DFREF dfrOut = NewFreeDataFolder()
 
 	WAVE     data                 = ASYNC_FetchWave(dfrInp, "data")
+	WAVE     ampParamStorageSlice = ASYNC_FetchWave(dfrInp, "ampParamStorageSlice")
 	variable clampAmp             = ASYNC_FetchVariable(dfrInp, "clampAmp")
 	variable clampMode            = ASYNC_FetchVariable(dfrInp, "clampMode")
 	variable pulseLengthPointsADC = ASYNC_FetchVariable(dfrInp, "pulseLengthPointsADC")
@@ -1158,7 +1159,7 @@ threadsafe Function/DF TP_TSAnalysis(DFREF dfrInp)
 	tpData[%SAMPLINGINTERVALDAC]   = samplingIntervalDAC
 
 	Make/FREE/WAVE additionalData = {data}
-	PUB_TPResult(device, tpData, additionalData)
+	PUB_TPResult(device, tpData, ampParamStorageSlice, additionalData)
 
 	return dfrOut
 End
@@ -1661,8 +1662,15 @@ Function/DF TP_PrepareAnalysisDF(string device, STRUCT TPAnalysisInput &tpInput)
 
 	wlName = GetWorkLoadName(WORKLOADCLASS_TP, device)
 
+	WAVE ampStorage = GetAmplifierParamStorageWave(device)
+	Duplicate/FREE/RMD=[][][tpInput.headstage] ampStorage, ampParamStorageSlice
+	ASSERT(DimSize(ampParamStorageSlice, COLS) == 1, "Expected only one column")
+	ASSERT(DimSize(ampParamStorageSlice, LAYERS) == 1, "Expected only one layer")
+	Redimension/N=(-1, 0, 0)/E=1 ampParamStorageSlice
+
 	DFREF threadDF = ASYNC_PrepareDF("TP_TSAnalysis", "TP_ROAnalysis", wlName, inOrder = 0)
 	ASYNC_AddParam(threadDF, w = tpInput.data, name = "data")
+	ASYNC_AddParam(threadDF, w = ampParamStorageSlice, name = "ampParamStorageSlice")
 	ASYNC_AddParam(threadDF, var = tpInput.clampAmp, name = "clampAmp")
 	ASYNC_AddParam(threadDF, var = tpInput.clampMode, name = "clampMode")
 	ASYNC_AddParam(threadDF, var = tpInput.pulseLengthPointsADC, name = "pulseLengthPointsADC")

Packages/tests/Basic/UTF_ZeroMQPublishing.ipf
@@ -566,6 +566,25 @@ static Function CheckTPData(variable jsonId)
 	CHECK_EQUAL_VAR(var, 2345)
 	stv = JSON_GetString(jsonID, "/results/instantaneous resistance/unit")
 	CHECK_EQUAL_STR(stv, "MΩ")
+
+	var = JSON_GetVariable(jsonID, "/amplifier/HoldingPotential/value")
+	CHECK_EQUAL_VAR(var, 123)
+	stv = JSON_GetString(jsonID, "/amplifier/HoldingPotential/unit")
+	CHECK_EQUAL_STR(stv, "mV")
+	var = JSON_GetVariable(jsonID, "/amplifier/HoldingPotentialEnable/value")
+	CHECK_EQUAL_VAR(var, 1)
+	stv = JSON_GetString(jsonID, "/amplifier/HoldingPotentialEnable/unit")
+	CHECK_EQUAL_STR(stv, "On/Off")
+End
+
+static Function/WAVE GetFakeAmpStorageSlice()
+
+	Make/N=2/FREE=1 fakeAmpStorage
+	SetDimensionLabels(fakeAmpStorage, "HoldingPotential;HoldingPotentialEnable", ROWS)
+	fakeAmpStorage[%HoldingPotential]       = 123
+	fakeAmpStorage[%HoldingPotentialEnable] = 1
+
+	return fakeAmpStorage
 End
 
 // IUTF_TD_GENERATOR DataGenerators#PUB_TPFiltersWithoutData
@@ -573,11 +592,12 @@ static Function CheckTPPublishing([string str])
 
 	variable jsonId
 
-	WAVE tpData = PrepareTPData()
+	WAVE tpData               = PrepareTPData()
+	WAVE ampParamStorageSlice = GetFakeAmpStorageSlice()
 
 	TUFXOP_Clear/Z/N=(str)
 	Make/FREE/N=0/WAVE additionalData
-	PUB_TPResult("TestDevice", tpData, additionalData)
+	PUB_TPResult("TestDevice", tpData, ampParamStorageSlice, additionalData)
 
 	jsonId = FetchAndParseMessage(str)
 	CheckTPData(jsonId)
@@ -588,12 +608,13 @@ static Function CheckTPPublishingWithData()
 
 	variable jsonId
 
-	WAVE tpData = PrepareTPData()
+	WAVE tpData               = PrepareTPData()
+	WAVE ampParamStorageSlice = GetFakeAmpStorageSlice()
 
 	Make/FREE/N=(2, 3) data = (p + q)^2
 	Make/FREE/WAVE additionalData = {data}
 
-	PUB_TPResult("TestDevice", tpData, additionalData)
+	PUB_TPResult("TestDevice", tpData, ampParamStorageSlice, additionalData)
 
 	Make/FREE/WAVE/N=0 receivedData
 	jsonId = FetchAndParseMessage(ZMQ_FILTER_TPRESULT_NOW_WITH_DATA, additionalData = receivedData)

commit 311854e256c1ed472d262c3e207d2de9da21cb55

AI_GetFunctionConstantForClampMode: Handle I=0 gracefully
For I=0 we now return an invalid wave reference as no amplifier settings
are relevant to report and thus PUB_AppendAmplifierSettings then also does
not append any.

Bug introduced in 6c7cc38f24 (PUB_TPResult: Add amplifier settings as
well, 2025-03-19).

Packages/MIES/MIES_AmplifierInteraction.ipf
@@ -1413,6 +1413,8 @@ threadsafe Function/WAVE AI_GetFunctionConstantForClampMode(variable clampMode)
 		case I_CLAMP_MODE:
 			list = AMPLIFIER_CONTROLS_IC
 			break
+		case I_EQUAL_ZERO_MODE:
+			return $""
 		default:
 			FATAL_ERROR("Invalid clamp mode")
 	endswitch

Packages/MIES/MIES_Publish.ipf
@@ -712,7 +712,11 @@ threadsafe static Function PUB_AppendAmplifierSettings(variable jsonID, string p
 	variable idx, value, nameIndex
 	string name, unit
 
-	WAVE funcs = AI_GetFunctionConstantForClampMode(clampMode)
+	WAVE/Z funcs = AI_GetFunctionConstantForClampMode(clampMode)
+
+	if(!WaveExists(funcs))
+		return NaN
+	endif
 
 	Make/FREE/T/N=(DimSize(funcs, ROWS)) names
 
@@ -844,6 +848,8 @@ End
 ///        }
 ///      }
 ///    }
+///
+/// For I=0 clamp mode the amplifier JSON object will be empty.
 ///
 /// \endrst
 threadsafe Function PUB_TPResult(string device, WAVE tpData, WAVE ampParamStorageSlice, WAVE additionalData)

Packages/tests/HardwareBasic/UTF_TestPulseAndTPDuringDAQ.ipf
@@ -1638,3 +1638,51 @@ static Function TestTPPublishing_REENTRY([string str])
 	CHECK(foundHS0)
 	CHECK(foundHS1)
 End
+
+static Function TestTPPublishingIEqualZero_PreAcq(string device)
+
+	/// @todo workaround inconsistent gain handling with I=0, see https://github.com/AllenInstitute/MIES/issues/1431
+	PGC_SetAndActivateControl(device, "SetVar_DataAcq_TPAmplitudeIC", val = -5)
+End
+
+/// UTF_TD_GENERATOR DataGenerators#DeviceNameGeneratorMD1
+static Function TestTPPublishingIEqualZero([string str])
+
+	TUFXOP_Clear/Z/N=(ZMQ_FILTER_TPRESULT_1S)
+	TUFXOP_Clear/Z/N=(ZMQ_FILTER_TPRESULT_5S)
+	TUFXOP_Clear/Z/N=(ZMQ_FILTER_TPRESULT_10S)
+
+	STRUCT DAQSettings s
+	InitDAQSettingsFromString(s, "MD1_RA0_I0_L0_BKG1_TP1" + \
+	                             "__HS0_DA0_AD0_CM:I=0:")
+
+	AcquireData_NG(s, str)
+
+	PrepareForPublishTest()
+
+	CtrlNamedBackGround StopTPAfterFiveSeconds, start=(ticks + TP_DURATION_S * 60), period=1, proc=StopTP_IGNORE
+End
+
+static Function TestTPPublishingIEqualZero_REENTRY([string str])
+
+	variable sweepNo, objectType, jsonId
+	string filter
+
+	CHECK_EQUAL_VAR(GetSetVariable(str, "SetVar_Sweep"), 0)
+
+	sweepNo = AFH_GetLastSweepAcquired(str)
+	CHECK_EQUAL_VAR(sweepNo, NaN)
+
+	WaitAndCheckStoredTPs_IGNORE(str, 1)
+
+	WAVE/T filters = DataGenerators#PUB_TPFilters()
+	for(filter : filters)
+		jsonId = FetchAndParseMessage(filter)
+
+		objectType = JSON_GetType(jsonID, "/amplifier")
+		CHECK_EQUAL_VAR(objectType, JSON_OBJECT)
+		WAVE/Z/T elements = JSON_GetKeys(jsonID, "/amplifier")
+		CHECK_WAVE(elements, FREE_WAVE | TEXT_WAVE)
+		CHECK_EQUAL_VAR(DimSize(elements, ROWS), 0)
+	endfor
+End