AllenInstitute/MIES

commit 2e582112585e227495bc7b0d756d692dbeeec92e

Correct headstage/ADC mix up. Found out that if(functionA() && functionB()) != if(variableA && variableB) When functions are used in IF statments the second part is not evaluated if the first function fails.
Original-CommitID: 0fe7f15c98495a75d8615c50c9dd178eafacb4d0

Packages/MIES/MIES_PressureControl.ipf
@@ -37,14 +37,12 @@ static Constant		PRESSURE_CHANGE					= 1
 /// A key point is that data acquisition used to run pressure pulses cannot be active if the TP is inactive.
 Function P_PressureControl(panelTitle)
 	string 	panelTitle
-	
-	WAVE 	PressureDataWv = P_GetPressureDataWaveRef(panelTitle)
-	Wave TPStorage = GetTPStorage(panelTitle)
-	variable count = GetNumberFromWaveNote(TPStorage, TP_CYLCE_COUNT_KEY)
-	variable 	headStage, Column
+
+	WAVE PressureDataWv = P_GetPressureDataWaveRef(panelTitle)
+	variable headStage
+
 	for(headStage = 0; headStage < NUM_HEADSTAGES; headStage += 1)
 		if(P_ValidatePressureSetHeadstage(panelTitle, headStage) && !IsITCCollectingData(panelTitle, headStage)) // are headstage settings valid AND is the ITC device inactive (avoids ITC commands while pressure pulse is ongoing).
-
 			switch(PressureDataWv[headStage][%Approach_Seal_BrkIn_Clear])
 				case P_METHOD_neg1_ATM:
 						P_MethodAtmospheric(panelTitle, headstage)
@@ -75,18 +73,38 @@ Function P_PressureControl(panelTitle)
 					P_MethodAtmospheric(panelTitle, headstage)
 					break
 			endswitch
-			// save pressure in TPStorageWave
-			if(P_IsHSActiveAndInVClamp(panelTitle, headStage)) /// @todo this is slow! When Headstage settings are converted from control storage to wave storage this should be updated to avoid control queries. This will fail when a new headstage is turned on.
-				Column = TP_GetTPResultsColOfHS(panelTitle, headStage)
-				if(Column != -1)
-					TPStorage[count][column][%Pressure] = PressureDataWv[headStage][%RealTimePressure][0]
-					if(count > 0) // record pressure change
-						TPStorage[count][column][%PressureChange] = TPStorage[count - 1][column][%Pressure] == PressureDataWv[headStage][%RealTimePressure][0] ? NaN : PRESSURE_CHANGE
-					endif
-				endif
-			endif
 		endif
+
+		P_UpdateTPStorage(panelTitle, headStage)
 	endfor
+End
+
+/// @brief Record pressure in TPStorage wave
+static Function P_UpdateTPStorage(panelTitle, headStage)
+	string panelTitle
+	variable headstage
+
+	WAVE PressureDataWv = P_GetPressureDataWaveRef(panelTitle)
+	WAVE TPStorage      = GetTPStorage(panelTitle)
+	variable count      = GetNumberFromWaveNote(TPStorage, TP_CYLCE_COUNT_KEY)
+	variable column
+
+	if(!P_ValidatePressureSetHeadstage(panelTitle, headStage) || !P_IsHSActiveAndInVClamp(panelTitle, headStage))
+		return NaN
+	endif
+
+	column = TP_GetTPResultsColOfHS(panelTitle, headStage)
+	if(column == -1)
+		return NaN
+	endif
+
+	TPStorage[count][column][%Pressure] = PressureDataWv[headStage][%RealTimePressure][0]
+
+	if(count == 0) // don' record pressure change for first entry
+		return NaN
+	endif
+
+	TPStorage[count][column][%PressureChange] = (TPStorage[count - 1][column][%Pressure] == PressureDataWv[headStage][%RealTimePressure][0] ? NaN : PRESSURE_CHANGE)
 End
 
 /// @brief Sets the pressure to atmospheric

Packages/MIES/MIES_Oscilloscope.ipf
@@ -234,7 +234,7 @@ Function SCOPE_CreateGraph(panelTitle, dataAcqOrTP)
 			if(showSteadyStateResistance)
 				steadyStateTrace = "SteadyStateResistance" + adcStr
 				AppendToGraph/W=$graph/R=$rightAxis/T=top TPStorage[][i][%SteadyStateResistance]/TN=$steadyStateTrace
-				headStage = AFH_GetHeadstageFromADC(panelTitle, i)
+				headStage = AFH_GetHeadstageFromADC(panelTitle, adc)
 				ASSERT(isFinite(headStage), "invalid headStage")
 				if(isFinite(PressureData[headStage][%DAC_DevID])) // Check if pressure is enabled
 					ModifyGraph/W=$graph marker($steadyStateTrace)=19, mode($steadyStateTrace)=4

commit 644f1bc9f22ae2cd6b24645ff0d3f4249f78445f

P_UpdateTPStorage: Ensure TPStorage is large enough
We need to ensure that the row exists before accessing it. Bug
introduced in 2e58211258 (Correct headstage/ADC mix up. Found out that
if(functionA() && functio.., 2016-03-10).

This was never triggered in CI as we don't do testing with pressure
devices.

Packages/MIES/MIES_PressureControl.ipf
@@ -147,7 +147,7 @@ End
 /// @brief Record pressure in TPStorage wave
 static Function P_UpdateTPStorage(string device, variable headStage)
 
-	variable count, old, new
+	variable count, old, new, ret
 
 	if(!P_ValidatePressureSetHeadstage(device, headStage) || !P_IsHSActiveAndInVClamp(device, headStage))
 		return NaN
@@ -157,6 +157,13 @@ static Function P_UpdateTPStorage(string device, variable headStage)
 	WAVE TPStorage      = GetTPStorage(device)
 
 	count = GetNumberFromWaveNote(TPStorage, NOTE_INDEX)
+
+	ret = EnsureLargeEnoughWave(TPStorage, indexShouldExist = count, dimension = ROWS, initialValue = NaN, checkFreeMemory = 1)
+
+	if(ret)
+		HandleOutOfMemory(device, NameOfWave(TPStorage))
+		return NaN
+	endif
 
 	TPStorage[count][headstage][%Pressure] = PressureDataWv[headStage][%RealTimePressure][0]
 