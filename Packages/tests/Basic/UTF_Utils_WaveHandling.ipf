AllenInstitute/MIES

commit 602d37cced103792c1c93ced66210b9dd08014f5

EnsureLargeEnoughWave: Avoid resizing when not needed
We don't need to look at the minimum wave size when the requested index
already fits.

We now also ensure that we double the wave sizes so that we should end up
always with a power of two when we started with one.

Packages/MIES/MIES_Utilities_WaveHandling.ipf
@@ -45,6 +45,8 @@ threadsafe Function EnsureLargeEnoughWave(WAVE wv, [variable indexShouldExist, v
 
 	if(ParamIsDefault(dimension))
 		dimension = ROWS
+	else
+		ASSERT_TS(dimension == ROWS || dimension == COLS || dimension == LAYERS || dimension == CHUNKS, "Invalid dimension")
 	endif
 
 	if(ParamIsDefault(checkFreeMemory))
@@ -53,21 +55,18 @@ threadsafe Function EnsureLargeEnoughWave(WAVE wv, [variable indexShouldExist, v
 		checkFreeMemory = !!checkFreeMemory
 	endif
 
-	ASSERT_TS(dimension == ROWS || dimension == COLS || dimension == LAYERS || dimension == CHUNKS, "Invalid dimension")
 	ASSERT_TS(WaveExists(wv), "Wave does not exist")
 	ASSERT_TS(IsFinite(indexShouldExist) && indexShouldExist >= 0, "Invalid minimum size")
 
 	if(ParamIsDefault(indexShouldExist))
 		indexShouldExist = MINIMUM_WAVE_SIZE - 1
-	else
-		indexShouldExist = max(MINIMUM_WAVE_SIZE - 1, indexShouldExist)
 	endif
 
 	if(indexShouldExist < DimSize(wv, dimension))
 		return 0
 	endif
 
-	indexShouldExist *= 2
+	indexShouldExist = max(max(2^FindNextPower(indexShouldExist, 2), 2 * DimSize(wv, dimension)), MINIMUM_WAVE_SIZE)
 
 	if(checkFreeMemory)
 		if((GetWaveSize(wv) * (indexShouldExist / DimSize(wv, dimension)) / 1024 / 1024 / 1024) >= GetFreeMemory())

Packages/tests/Basic/UTF_Utils_WaveHandling.ipf
@@ -160,6 +160,33 @@ Function ELE_AbortsWithTooLargeValue()
 	endtry
 End
 
+Function ELE_DoesNothingIfIndexIsGood()
+
+	Make/FREE/N=(1) wv
+	EnsurelargeEnoughWave(wv, indexShouldExist = 0)
+	CHECK_EQUAL_VAR(DimSize(wv, ROWS), 1)
+End
+
+Function ELE_ResizesGeometrically()
+
+	Make/FREE/N=(MINIMUM_WAVE_SIZE) wv
+	EnsurelargeEnoughWave(wv, indexShouldExist = MINIMUM_WAVE_SIZE + 1)
+	CHECK_EQUAL_VAR(DimSize(wv, ROWS), 2 * MINIMUM_WAVE_SIZE)
+
+	EnsurelargeEnoughWave(wv, indexShouldExist = 2 * MINIMUM_WAVE_SIZE + 1)
+	CHECK_EQUAL_VAR(DimSize(wv, ROWS), 4 * MINIMUM_WAVE_SIZE)
+
+	EnsurelargeEnoughWave(wv, indexShouldExist = 9 * MINIMUM_WAVE_SIZE)
+	CHECK_EQUAL_VAR(DimSize(wv, ROWS), 16 * MINIMUM_WAVE_SIZE)
+End
+
+Function ELE_WorksWithEmtpyInitialSize()
+
+	Make/FREE/N=(0) wv
+	EnsurelargeEnoughWave(wv, indexShouldExist = 0)
+	CHECK_EQUAL_VAR(DimSize(wv, ROWS), MINIMUM_WAVE_SIZE)
+End
+
 /// @}
 
 // GetWaveSize

commit fd2af936b53f96b05834768f1595f12aeb758e76

EnsureLargeEnoughWave: Fix it when requesting a power of two index
When we request a power of two index which is larger than double the
wave size and MINIMUM_WAVE_SIZE we would calculate an indexShouldExist
which is exactly the same as the requested value.

So let's add one before finding the next power of two.

Bug introduced in 602d37cced (EnsureLargeEnoughWave: Avoid resizing when not needed, 2025-04-16).

Packages/MIES/MIES_Utilities_WaveHandling.ipf
@@ -66,7 +66,7 @@ threadsafe Function EnsureLargeEnoughWave(WAVE wv, [variable indexShouldExist, v
 		return 0
 	endif
 
-	indexShouldExist = max(2^FindNextPower(indexShouldExist, 2), 2 * DimSize(wv, dimension), MINIMUM_WAVE_SIZE)
+	indexShouldExist = max(2^FindNextPower(indexShouldExist + 1, 2), 2 * DimSize(wv, dimension), MINIMUM_WAVE_SIZE)
 
 	if(checkFreeMemory)
 		if((GetWaveSize(wv) * (indexShouldExist / DimSize(wv, dimension)) / 1024 / 1024 / 1024) >= GetFreeMemory())

Packages/tests/Basic/UTF_Utils_WaveHandling.ipf
@@ -145,6 +145,14 @@ Function ELE_Returns1WithCheckMem()
 	CHECK_EQUAL_VAR(DimSize(wv, ROWS), MINIMUM_WAVE_SIZE)
 End
 
+Function ELE_WorksWithPowerOfTwoValue()
+
+	Make/FREE/N=(MINIMUM_WAVE_SIZE) wv
+	CHECK_EQUAL_VAR(MINIMUM_WAVE_SIZE, 64)
+	EnsureLargeEnoughWave(wv, indexShouldExist = 128)
+	CHECK_EQUAL_VAR(DimSize(wv, ROWS), 256)
+End
+
 Function ELE_AbortsWithTooLargeValue()
 
 	Make/FREE/N=(MINIMUM_WAVE_SIZE) wv