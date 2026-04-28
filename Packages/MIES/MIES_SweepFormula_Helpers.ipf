AllenInstitute/MIES

commit 70020f0e77097237845f7abc2528ae034c3fef9b

SFH_GetSweepsForFormula: Store range in wave note
And add SFH_ParseToSelectDataWaveAndRange to read it back.

Packages/MIES/MIES_SweepFormula_Helpers.ipf
@@ -453,6 +453,7 @@ Function/WAVE SFH_GetSweepsForFormula(string graph, WAVE range, WAVE/Z selectDat
 			SFH_ASSERT(rangeEnd == inf || (IsFinite(rangeEnd) && rangeEnd >= leftx(sweep) && rangeEnd < rightx(sweep)), "Specified ending range not inside sweep " + num2istr(sweepNo) + ".")
 			Duplicate/FREE/R=(rangeStart, rangeEnd) sweep, rangedSweepData
 
+			JWN_SetWaveInWaveNote(rangedSweepData, SF_META_RANGE, {rangeStart, rangeEnd})
 			JWN_SetNumberInWaveNote(rangedSweepData, SF_META_SWEEPNO, sweepNo)
 			JWN_SetNumberInWaveNote(rangedSweepData, SF_META_CHANNELTYPE, chanType)
 			JWN_SetNumberInWaveNote(rangedSweepData, SF_META_CHANNELNUMBER, chanNr)
@@ -807,4 +808,18 @@ End
 Function/S SFH_GetBrowserForFormulaGraph(string win)
 
 	return GetUserData(win, "", SFH_USER_DATA_BROWSER)
+End
+
+/// @brief Recreate a **single** select data wave and range stored in the JSON wavenote from SFH_GetSweepsForFormula()
+Function [WAVE selectData, WAVE range] SFH_ParseToSelectDataWaveAndRange(WAVE sweepData)
+
+	WAVE range = JWN_GetNumericWaveFromWaveNote(sweepData, SF_META_RANGE)
+
+	WAVE selectData = SFH_NewSelectDataWave(1, 1)
+
+	selectData[0][%SWEEP]         = JWN_GetNumberFromWaveNote(sweepData, SF_META_SWEEPNO)
+	selectData[0][%CHANNELTYPE]   = JWN_GetNumberFromWaveNote(sweepData, SF_META_CHANNELTYPE)
+	selectData[0][%CHANNELNUMBER] = JWN_GetNumberFromWaveNote(sweepData, SF_META_CHANNELNUMBER)
+
+	return [selectData, range]
 End

Packages/MIES/MIES_Constants.ipf
@@ -1900,6 +1900,7 @@ StrConstant CO_SF_TOO_MANY_TRACES = "SF_tooManyTraces"
 /// @{
 StrConstant SF_META_DATATYPE = "/DataType" // string
 StrConstant SF_META_SWEEPNO = "/SweepNumber" // number
+StrConstant SF_META_RANGE = "/Range" // numeric wave
 StrConstant SF_META_CHANNELTYPE = "/ChannelType" // number
 StrConstant SF_META_CHANNELNUMBER = "/ChannelNumber" // number
 StrConstant SF_META_ISAVERAGED = "/IsAveraged" // number

commit 78cd5224f3583de308c58e679700d8073909e528

SFH_GetSweepsForFormulaImpl: Store range as double precision wave
Broken since 70020f0e77 (SFH_GetSweepsForFormula: Store range in wave note, 2023-03-06).

Packages/MIES/MIES_SweepFormula_Helpers.ipf
@@ -746,7 +746,7 @@ static Function/WAVE SFH_GetSweepsForFormulaImpl(string graph, WAVE/WAVE selectD
 				JWN_SetWaveInWaveNote(rangedSweepData, SF_META_RANGE, entry)
 			else
 				// we write here on purpose the requested range
-				JWN_SetWaveInWaveNote(rangedSweepData, SF_META_RANGE, {rangeStart, rangeEnd})
+				JWN_SetWaveInWaveNote(rangedSweepData, SF_META_RANGE, WaveDouble({rangeStart, rangeEnd}))
 			endif
 
 			JWN_SetNumberInWaveNote(rangedSweepData, SF_META_SWEEPNO, sweepNo)