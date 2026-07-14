AllenInstitute/MIES

commit 43d6c9ad15e1c64f99ccc6a75d586262e6fe0378

BSP_DynamicStartupSettings: Hide the button dynamically
In a future commit we want to add support for restoring the hidden state
of windows and subwindows.

For that to work we need to show the button to restore dynamically as we
can't rely on all windows being shown when the configuration is finished.

Packages/MIES/MIES_BrowserSettingsPanel.ipf
@@ -245,6 +245,8 @@ Function BSP_DynamicStartupSettings(string mainPanel)
 		SetPopupMenuVal(shPanel, "popup_Experiment", func = "LBV_GetExperiments(\"" + shPanel + "\")")
 		EnableControls(shPanel, "popup_Device;popup_Experiment")
 	endif
+
+	BSP_UpdateHideButton(mainPanel)
 End
 
 /// @brief Hook function for the Sweep Formula Notebook
@@ -635,6 +637,35 @@ static Function BSP_HidePanel(string win)
 	DoWindow/F $currentWindow
 
 	BSP_MainPanelButtonToggle(mainPanel, 1)
+End
+
+static Function BSP_UpdateHideButton(string win)
+
+	string   mainPanel
+	variable visible
+
+	mainPanel = GetMainWindow(win)
+	WAVE/T allWindows = ListToTextWave(GetAllWindows(mainPanel), ";")
+
+	for(win : allWindows)
+		// ignore child-child windows
+		if(ItemsInList(win, "#") > 2)
+			continue
+		endif
+
+		GetWindow $win, hide
+
+		if(V_Value)
+			visible = 1
+			break
+		endif
+	endfor
+
+	if(visible)
+		ShowControl(mainPanel, BSP_SHOW_WIN_BUTTON)
+	else
+		HideControl(mainPanel, BSP_SHOW_WIN_BUTTON)
+	endif
 End
 
 /// @brief panel close hook for side panel

Packages/MIES/MIES_Constants.ipf
@@ -19,7 +19,7 @@ Constant DAQ_CONFIG_WAVE_VERSION = 3
 
 /// Used to upgrade the GuiStateWave as well as the DA Ephys panel
 Constant DA_EPHYS_PANEL_VERSION           = 66
-Constant DATA_SWEEP_BROWSER_PANEL_VERSION = 52
+Constant DATA_SWEEP_BROWSER_PANEL_VERSION = 53
 Constant WAVEBUILDER_PANEL_VERSION        = 14
 Constant ANALYSISBROWSER_PANEL_VERSION    = 9
 Constant PSX_PLOT_PANEL_VERSION           = 1

Packages/MIES/MIES_DataBrowser_Macro.ipf
@@ -12,7 +12,7 @@
 Window DataBrowser() : Graph
 	PauseUpdate; Silent 1 // building window...
 	Display/W=(427.5, 292.25, 916.5, 664.25)/K=1
-	Button button_BSP_open, pos={3.00, 3.00}, size={24.00, 24.00}, disable=1, proc=BSP_ButtonProc_Panel
+	Button button_BSP_open, pos={3.00, 3.00}, size={24.00, 24.00}, proc=BSP_ButtonProc_Panel
 	Button button_BSP_open, title="<<", help={"Restore side panels"}
 	Button button_BSP_open, userdata(ResizeControlsInfo)=A"!!,>M!!#8L!!#=#!!#=#z!!#](Aon\"Qzzzzzzzzzzzzzz!!#](Aon\"Qzz"
 	Button button_BSP_open, userdata(ResizeControlsInfo)+=A"zzzzzzzzzzzz!!#u:Du]k<zzzzzzzzzzz"

commit cac868d821ec6a0d51e708e21d8d4349a990b263

BSP_UpdateHideButton: Fix logic
GetWindow XXX, hide returns in the 0th bit the truth that the window is
hidden. The fallout was that the restore button was missing when a
subwindow was hidden.

Broken since 43d6c9ad15 (BSP_DynamicStartupSettings: Hide the button dynamically, 2025-06-11).

Packages/MIES/MIES_BrowserSettingsPanel.ipf
@@ -639,7 +639,7 @@ End
 static Function BSP_UpdateHideButton(string win)
 
 	string   mainPanel
-	variable visible
+	variable showButton
 
 	mainPanel = GetMainWindow(win)
 	WAVE/T allWindows = ListToTextWave(GetAllWindows(mainPanel), ";")
@@ -652,13 +652,13 @@ static Function BSP_UpdateHideButton(string win)
 
 		GetWindow $win, hide
 
-		if(V_Value)
-			visible = 1
+		if(V_Value == 1)
+			showButton = 1
 			break
 		endif
 	endfor
 
-	if(visible)
+	if(showButton)
 		ShowControl(mainPanel, BSP_SHOW_WIN_BUTTON)
 	else
 		HideControl(mainPanel, BSP_SHOW_WIN_BUTTON)