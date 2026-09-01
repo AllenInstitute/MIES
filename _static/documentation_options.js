03088de0bd6a3648fb3fc36eb87f70cafd4174b1
MIES Deployment <mies-deploy@linux-mint-box.seattle>
2026-09-01 19:06:30 +0000

    Updating documentation to Release_3.0_20260715-72-g5f1183671

---
+++ _static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '(main) Release_3.0_20260715-72-g5f1183671',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: false,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
